dd48113e0b6feea60fbfcf59a0a9d7fb1488a4ec
Brendon Page <DEV2\Brendon.Page>
2013-02-07 00:27:25 +0200

    ***NO_CI***

--- Dev/Dev2.Studio.Core/StringResources1.Designer.cs
+++ Dev/Dev2.Studio.Core/StringResources1.Designer.cs
@@ -163,7 +163,7 @@ namespace Dev2.Studio.Core {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.2.7.
+        ///   Looks up a localized string similar to 0.2.7.3.
         /// </summary>
         public static string CurrentVersion {
             get {
