c6ee9b88e2c9e64e29e2f3c79948c285a2def972
Trevor Williams-Ros <DEV2\Trevor.Williams-Ros>
2013-02-26 14:07:13 +0200

    ***NO_CI***

---
+++ Dev/Dev2.Sql.Tests/Resources.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Dev2.Sql.Tests
+{
+    public static class Resources
+    {
+        public static string GetFromResources(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(string.Format("Dev2.Sql.Tests.Resources.{0}", resourceName)))
+            {
+                if (stream != null)
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
