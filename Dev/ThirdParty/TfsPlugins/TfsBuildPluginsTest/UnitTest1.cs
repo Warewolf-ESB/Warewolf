09242f9bf342c35299640454eea483f7b6d5e67e
Travis Frisinger <DEV2\Travis.Frisinger>
2013-03-20 10:33:46 +0200

    ***NO_CI***

---
+++ Dev/ThirdParty/TfsPlugins/TfsBuildPluginsTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TfsBuildPluginsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanQueueBuild()
+        {
+        }
+    }
+}
---
+++ Dev/ThirdParty/TfsPlugins/TfsQueueBuildPlugin/QueueBuildActivity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Client;
+
+namespace Dev2.TfsBuildPlugin
+{
+
+    [BuildActivity(HostEnvironmentOption.Agent)]
+    public sealed class QueueBuildActivity : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<IBuildDetail> BuildDetail { get; set; }
+
+        [RequiredArgument]
+        public InArgument<String> BuildDefinition { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string buildDef = context.GetValue(this.BuildDefinition);
+            IBuildDetail buildDetail = context.GetValue(this.BuildDetail);
+
+            bool queuedBld = false;
+
+            if (!string.IsNullOrEmpty(buildDef) && buildDetail != null)
+            {
+                var workspace = buildDetail.BuildDefinition.Workspace;
+
+                if (workspace != null)
+                {
+                    string proj = buildDetail.BuildDefinition.TeamProject;
+
+                    if (!string.IsNullOrEmpty(proj))
+                    {
+                        string configURI = buildDetail.BuildServer.TeamProjectCollection.Uri.ToString();
+
+                        if (!string.IsNullOrEmpty(configURI))
+                        {
+                            TfsTeamProjectCollection projCollection = new TfsTeamProjectCollection(new Uri(configURI));
+
+                            projCollection.EnsureAuthenticated();
+
+                            IBuildServer bldServer = (IBuildServer)projCollection.GetService(typeof(IBuildServer));
+
+                            IBuildDefinition queueBldDef = bldServer.GetBuildDefinition(proj, buildDef);
+
+                            if (bldServer != null && queueBldDef != null)
+                            {
+                                bldServer.QueueBuild(queueBldDef);
+                                queuedBld = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!queuedBld)
+            {
+                throw new Exception("Failed to queue build");
+            }
+        }
+    }
+}
