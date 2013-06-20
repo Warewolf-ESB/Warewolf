c9e29fc4a2ad2ffb2c6d4c26931edd24ab55d62d
Massimo Guerrera <DEV2\Massimo.Guerrera>
2013-06-20 17:55:56 +0200

    Fixes Hugs stuff

--- Dev/Dev2.Core/Interfaces/IDSFDataObject.cs
+++ Dev/Dev2.Core/Interfaces/IDSFDataObject.cs
@@ -57,6 +57,6 @@ namespace Dev2
 
         ExecutionOrigin ExecutionOrigin { get; set; }
         string ExecutionOriginDescription { get; set; }
-        bool IsFromWebServer { get; set; }
+        
     }
 }
