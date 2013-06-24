578159638f5cbf1325686be466247c2809162340
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-06-24 09:01:30 +0200

    fixed bug with drag and drop for remote workflows and services ***NO_CI***

--- Dev/Dev2.Studio/Views/Workflow/WorkflowDesignerView.xaml.cs
+++ Dev/Dev2.Studio/Views/Workflow/WorkflowDesignerView.xaml.cs
@@ -6,6 +6,7 @@ using Dev2.Studio.Core;
 using Dev2.Studio.Core.AppResources.Enums;
 using Dev2.Studio.Core.Interfaces;
 using Dev2.Studio.Utils.ActivityDesignerUtils;
+using Dev2.Studio.ViewModels.Workflow;
 
 namespace Dev2.Studio.Views.Workflow
 {
@@ -35,8 +36,10 @@ namespace Dev2.Studio.Views.Workflow
             var objectData = e.Data.GetData(modelItemString);
             IContextualResourceModel contextualResourceModel = ResourceHelper.GetContextualResourceModel(objectData);
             if(contextualResourceModel==null) return;
-            if (contextualResourceModel.Environment.IsLocalHost()) return;
-            if(contextualResourceModel.ResourceType == ResourceType.WorkflowService || contextualResourceModel.ServerResourceType==ResourceType.WorkflowService.ToString())
+            //if (contextualResourceModel.Environment.IsLocalHost()) return;
+            WorkflowDesignerViewModel currentViewModel = (WorkflowDesignerViewModel)this.DataContext;
+            if(currentViewModel==null) return;
+            if(contextualResourceModel.ResourceType == ResourceType.WorkflowService || contextualResourceModel.ServerResourceType==ResourceType.WorkflowService.ToString() || currentViewModel.EnvironmentModel.ID==contextualResourceModel.Environment.ID)
             {
                 return;
             }
