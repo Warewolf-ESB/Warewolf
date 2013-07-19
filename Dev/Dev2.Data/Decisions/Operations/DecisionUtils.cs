0c462d2d51013609054447d60861a437387d5e08
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-07-19 11:03:53 +0200

    QA MERGE 9934 ***NO_CI***

---
+++ Dev/Dev2.Data/Decisions/Operations/DecisionUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace Dev2.Data.Decisions.Operations
+{
+    public class DecisionUtils
+    {
+        public static bool IsNumericComparison(string[] cols, out int[] tryGetNumber)
+        {
+            tryGetNumber = new int[2];
+            var isString = false;
+            for(var i = 0; i < 2; i++)
+            {
+                if(!int.TryParse(cols[i], out tryGetNumber[i]))
+                {
+                    isString = true;
+                }
+            }
+            return isString;
+        }
+    }
+}
--- Dev/Dev2.Data/Decisions/Operations/IsGreaterThan.cs
+++ Dev/Dev2.Data/Decisions/Operations/IsGreaterThan.cs
@@ -19,6 +19,16 @@ namespace Dev2.Data.Decisions.Operations
 
             if(!string.IsNullOrEmpty(cols[0]))
             {
+                int[] tryGetNumber;
+                var isString = DecisionUtils.IsNumericComparison(cols, out tryGetNumber);
+
+                //either int compare
+                if(!isString)
+                {
+                    return (tryGetNumber[0].CompareTo(tryGetNumber[1]) > 0);
+                }
+
+                //or string compare
                 return (String.Compare(cols[0], cols[1], StringComparison.Ordinal) > 0);
             }
 
