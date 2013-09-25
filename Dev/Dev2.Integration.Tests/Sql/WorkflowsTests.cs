c6d08bd227d908209578a27ea00fa3013a7be16c
Ashley Lewis <DEV2\Ashley.lewis>
2013-09-25 14:14:09 +0200

    The last two batched of un-ignored integration tests caused server failure. The last integration run didn't crash the server with both batches ignored. Now I'm un-ignoring workflows tests class ***NO_CI***

--- Dev/Dev2.Integration.Tests/Sql/WorkflowsTests.cs
+++ Dev/Dev2.Integration.Tests/Sql/WorkflowsTests.cs
@@ -4,7 +4,7 @@ using Warewolf.Sql;
 
 namespace Dev2.Integration.Tests.Sql
 {
-    [TestClass][Ignore]//Ashley: round 2 hunting the evil test
+    [TestClass]
     public class WorkflowsTests
     {
         [ClassInitialize]
