de828a2865cf074fa4956675a1135ed8c8a91d45
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-10-09 11:11:09 +0200

    some plumbing

---
+++ Dev/Dev2.Activities.Tests/ActivityTests/SqlBulkInsertActivityTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using ActivityUnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Tests.Activities.ActivityTests
+{
+
+    [TestClass]
+    public class SqlBulkInsertActivityTests : BaseActivityUnitTest
+    {
+        public TestContext TestContext { get; set; }
+
+        
+        #region Private Test Methods
+
+        private void SetupArguments(string currentDL, string testData, string sourceString, IList<XPathDTO> resultCollection)
+        {
+            TestStartNode = new FlowStep
+            {
+                //Action = new DsfXPathActivity { SourceString = sourceString, ResultsCollection = resultCollection }
+            };
+
+            CurrentDl = testData;
+            TestData = currentDL;
+        }
+
+        #endregion Private Test Methods
+    }
+}
--- Dev/Dev2.Activities/Activities/DsfSqlBulkInsertActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfSqlBulkInsertActivity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Activities;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using Dev2.TO;
 using Dev2.Util;
 using Unlimited.Applications.BusinessDesignStudio.Activities;
@@ -10,6 +11,8 @@ namespace Dev2.Activities
 {
     public class DsfSqlBulkInsertActivity : DsfActivityAbstract<string>
     {
+        ISqlBulkInserter _sqlBulkInserter;
+
         public DsfSqlBulkInsertActivity()
             : base("SQL Bulk Insert")
         {
@@ -32,7 +35,29 @@ namespace Dev2.Activities
         [FindMissing]
         public new string Result { get; set; }
 
+        public bool CheckConstraints { get; set; }
 
+        public bool FireTriggers { get; set; }
+
+        public bool UserInternalTransaction { get; set; }
+
+        public bool UseDefaultValues { get; set; }
+
+        public bool KeepIdentity { get; set; }
+
+        public bool KeepLock { get; set; }
+        
+        public ISqlBulkInserter SqlBulkInserter
+        {
+            get
+            {
+                return _sqlBulkInserter ?? (_sqlBulkInserter = new SqlBulkInserter());
+            }
+            set
+            {
+                _sqlBulkInserter = value;
+            }
+        }
         #region Overrides of DsfNativeActivity<string>
 
         /// <summary>
@@ -59,6 +84,22 @@ namespace Dev2.Activities
         public override IList<DsfForEachItem> GetForEachOutputs()
         {
             return null;
+        }
+
+        #endregion
+    }
+
+    public interface ISqlBulkInserter
+    {
+        void Insert(SqlBulkCopy sqlBulkCopy);
+    }
+
+    public class SqlBulkInserter : ISqlBulkInserter
+    {
+        #region Implementation of ISqlBulkInserter
+
+        public void Insert(SqlBulkCopy sqlBulkCopy)
+        {
         }
 
         #endregion
