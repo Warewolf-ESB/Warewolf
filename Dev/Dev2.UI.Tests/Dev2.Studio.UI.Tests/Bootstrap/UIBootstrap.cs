b0e7b95f84c65a59a918a36f68f229ded00ddf49
Ashley Lewis <DEV2\Ashley.lewis>
2013-10-17 16:13:48 +0200

    null

--- Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/Bootstrap/UIBootstrap.cs
+++ Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/Bootstrap/UIBootstrap.cs
@@ -14,14 +14,12 @@ namespace Dev2.Studio.UI.Tests.Bootstrap
         private const string _exeRoot = @"C:\CodedUI";
         private const string _cleanupFileName = "exit.bat";
         private const string _runFileName = "run.bat";
+        private const string StagingLocation = @"\\RSAKLFSVRTFSBLD\Automated Builds\DevMergeStaging";
 
-        /// <summary>
-        /// Setup for coded ui if setup file found
-        /// </summary>
-        /// <param name="textCtx">The text CTX.</param>
         [AssemblyInitialize()]
         public static void Init(TestContext textCtx)
         {
+            DirectoryCopy(StagingLocation, @"C:\CodedUI\Merge");
             const string runBatFileDir = _exeRoot + "\\" + _runFileName;
             if(File.Exists(runBatFileDir))
             {
@@ -50,6 +48,44 @@ namespace Dev2.Studio.UI.Tests.Bootstrap
                 proc.StartInfo.RedirectStandardOutput = true;
                 proc.StartInfo.UseShellExecute = false;
                 proc.Start();
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs = true)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if(!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if(!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach(FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if(copySubDirs)
+            {
+                foreach(DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
             }
         }
     }
