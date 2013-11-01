dba50d294ee5e5d701b45ec03c9a6c11796c619b
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-11-01 10:04:36 +0200

    did work on TU issues with datalist in studio being invalid causing execution errors.

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/SecurityRead.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using Dev2.DynamicServices;
+using Dev2.Workspaces;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    /// <summary>
+    /// Checks a users permissions on the local file system
+    /// </summary>
+    public class SecurityRead : IEsbManagementEndpoint
+    {
+        public string Execute(IDictionary<string, string> values, IWorkspace theWorkspace)
+        {
+            var result = new StringBuilder();
+
+            return result.ToString();
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            var dynamicService = new DynamicService
+            {
+                Name = HandlesType(),
+                DataListSpecification = "<DataList><Security/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>"
+            };
+
+            var serviceAction = new ServiceAction
+            {
+                Name = HandlesType(),
+                ActionType = enActionType.InvokeManagementDynamicService,
+                SourceMethod = HandlesType()
+            };
+
+            dynamicService.Actions.Add(serviceAction);
+
+            return dynamicService;
+        }
+
+        public string HandlesType()
+        {
+            return "SecurityReadService";
+        }
+    }
+}
---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/SecurityWrite.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+using System.Text;
+using Dev2.Common;
+using Dev2.DynamicServices;
+using Dev2.Runtime.Security;
+using Dev2.Workspaces;
+using Newtonsoft.Json;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    /// <summary>
+    /// Checks a users permissions on the local file system
+    /// </summary>
+    public class SecurityWrite : IEsbManagementEndpoint
+    {
+        public string Execute(IDictionary<string, string> values, IWorkspace theWorkspace)
+        {
+            var result = "Success";
+            var byteConverter = new ASCIIEncoding();
+            var hostSecureConfig = new HostSecureConfig();
+            var jsonData = JsonConvert.SerializeObject(new object());
+            byte[] dataToEncrypt = byteConverter.GetBytes(jsonData);
+            var encryptedData = hostSecureConfig.ServerKey.Encrypt(dataToEncrypt,false);
+            File.WriteAllBytes("secure.config", encryptedData);
+            try
+            {
+                var accessRule = new FileSystemAccessRule("",
+                                     fileSystemRights: FileSystemRights.Modify,
+                                     inheritanceFlags: InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit,
+                                     propagationFlags: PropagationFlags.InheritOnly,
+                                     type: AccessControlType.Allow);
+
+                var fileInfo = new FileInfo("secure.config");
+
+                // Get a DirectorySecurity object that represents the current security settings.
+                var accessControl = fileInfo.GetAccessControl();
+
+                // Add the FileSystemAccessRule to the security settings. 
+                accessControl.AddAccessRule(accessRule);
+
+                // Set the new access settings.
+                fileInfo.SetAccessControl(accessControl);
+            }
+            catch(Exception ex)
+            {
+                ServerLogger.LogError(ex);
+                result = "Error writing security configuration.";
+            }
+
+            return result;
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            var dynamicService = new DynamicService
+            {
+                Name = HandlesType(),
+                DataListSpecification = "<DataList><Security/><Result/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>"
+            };
+
+            var serviceAction = new ServiceAction
+            {
+                Name = HandlesType(),
+                ActionType = enActionType.InvokeManagementDynamicService,
+                SourceMethod = HandlesType()
+            };
+
+            dynamicService.Actions.Add(serviceAction);
+
+            return dynamicService;
+        }
+
+        public string HandlesType()
+        {
+            return "SecurityWriteService";
+        }
+    }
+}
