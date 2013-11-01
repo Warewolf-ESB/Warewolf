4bb6203543e9865e353005915899fdefbfe8ccea
Trevor Williams-Ros <DEV2\Trevor.Williams-Ros>
2013-11-01 16:09:24 +0200

    null

--- Dev/Dev2.Studio/Settings/SettingsViewModel.cs
+++ Dev/Dev2.Studio/Settings/SettingsViewModel.cs
@@ -1,15 +1,23 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Caliburn.Micro;
 using Dev2.Services.Events;
+using Dev2.Studio.Core.Controller;
+using Dev2.Studio.Core.Interfaces;
+using Dev2.Studio.Core.ViewModels.Base;
 using Dev2.Studio.ViewModels.WorkSurface;
 
 namespace Dev2.Settings
 {
     public class SettingsViewModel : BaseWorkSurfaceViewModel
     {
+        bool _isLoading;
+        bool _selectionChanging;
+
         bool _showLogging;
         bool _showSecurity = true;
-        bool _selectionChanging;
+
+        IPopupController _popupController;
 
         public SettingsViewModel()
             : this(EventPublishers.Aggregator)
@@ -19,6 +27,25 @@ namespace Dev2.Settings
         public SettingsViewModel(IEventAggregator eventPublisher)
             : base(eventPublisher)
         {
+            ServerChangedCommand = new RelayCommand(OnServerChanged, o => true);
+        }
+
+        public ICommand ServerChangedCommand { get; private set; }
+
+        public IEnvironmentModel CurrentEnvironment { get; private set; }
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                if(value.Equals(_isLoading))
+                {
+                    return;
+                }
+                _isLoading = value;
+                NotifyOfPropertyChange(() => IsLoading);
+            }
         }
 
         public bool ShowLogging
@@ -76,6 +103,43 @@ namespace Dev2.Settings
                     break;
             }
             _selectionChanging = false;
+        }
+
+        void OnServerChanged(object obj)
+        {
+            var server = obj as IServer;
+            if(server == null || server.Environment == null)
+            {
+                return;
+            }
+
+            if(!server.Environment.IsConnected)
+            {
+                server.Environment.CanStudioExecute = false;
+                _popupController.ShowNotConnected();
+                return;
+            }
+
+            IsLoading = true;
+
+            try
+            {
+                CurrentEnvironment = server.Environment;
+                LoadSettings();
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        void LoadSettings()
+        {
+            //CurrentEnvironment.Connection.ExecuteCommand()
+        }
+
+        void SaveSettings()
+        {
         }
     }
 }
