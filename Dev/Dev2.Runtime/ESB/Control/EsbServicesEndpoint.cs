69ca5c972fe00591e1f95c751bb4d1c9af73d018
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-11-05 10:52:15 +0200

    fix for bug 10674

--- Dev/Dev2.Runtime/ESB/Control/EsbServicesEndpoint.cs
+++ Dev/Dev2.Runtime/ESB/Control/EsbServicesEndpoint.cs
@@ -308,6 +308,11 @@ namespace Dev2.DynamicServices
                     compiler.Shape(dataObject.DataListID, enDev2ArgumentType.Output, outputDefs, out invokeErrors);
                     errors.MergeErrors(invokeErrors);
                 }
+                if(dataObject.IsDebugMode()) // This is crazy but is needed to make the DebugOutput work. No idea why this works. It needs to be removed and the core issue resolved.
+                {
+                    var currentData = compiler.ConvertFrom(dataObject.DataListID, DataListFormat.CreateFormat(GlobalConstants._XML), enTranslationDepth.Data, out errors);
+                    var dataInOldID = compiler.ConvertFrom(oldID, DataListFormat.CreateFormat(GlobalConstants._XML), enTranslationDepth.Data, out errors);
+                }
             }
 
             return result;
