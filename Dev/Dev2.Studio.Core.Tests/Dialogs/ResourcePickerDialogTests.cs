1916ccf1c267935902193529bdc38c4d0fe71c60
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-11-11 11:14:38 +0200

    fixed ignored tests

--- Dev/Dev2.Studio.Core.Tests/Dialogs/ResourcePickerDialogTests.cs
+++ Dev/Dev2.Studio.Core.Tests/Dialogs/ResourcePickerDialogTests.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Primitives;
 using Caliburn.Micro;
+using Dev2.Composition;
+using Dev2.Core.Tests.Environments;
 using Dev2.Dialogs;
 using Dev2.Studio.Core;
 using Dev2.Studio.Core.Interfaces;
+using Dev2.Studio.Core.Models;
 using Dev2.Studio.Enums;
 using Dev2.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -72,14 +77,14 @@ namespace Dev2.Core.Tests.Dialogs
         [TestMethod]
         [Owner("Trevor Williams-Ros")]
         [TestCategory("ResourcePickerDialog_Constructor")]
-        [Ignore]
         public void ResourcePickerDialog_Constructor_EnvironmentModelIsNotNull_EnvironmentRepositoryIsNew()
         {
             //------------Setup for test-------------------------
+            SetupMef();
             var dialogWindow = new Mock<IDialog>();
 
             //------------Execute Test---------------------------
-            var dialog = new TestResourcePickerDialog(enDsfActivityType.All, null) { CreateDialogResult = dialogWindow.Object };
+            var dialog = new TestResourcePickerDialog(enDsfActivityType.All, new Mock<IEnvironmentModel>().Object) { CreateDialogResult = dialogWindow.Object };
             // Need to invoke ShowDialog in order to get the DsfActivityDropViewModel
             dialog.ShowDialog();
 
@@ -87,18 +92,27 @@ namespace Dev2.Core.Tests.Dialogs
             Assert.AreNotSame(EnvironmentRepository.Instance, dialog.CreateDialogDataContext.ExplorerViewModel.EnvironmentRepository);
         }
 
+        static void SetupMef()
+        {
+            var importServiceContext = new ImportServiceContext();
+            ImportService.CurrentContext = importServiceContext;
+            ImportService.Initialize(new List<ComposablePartCatalog>());
+            ImportService.AddExportedValueToContainer<IFrameworkSecurityContext>(new MockSecurityProvider(""));
+        }
+
         [TestMethod]
         [Owner("Trevor Williams-Ros")]
         [TestCategory("ResourcePickerDialog_Constructor")]
-        [Ignore]
         public void ResourcePickerDialog_Constructor_Properties_Initialized()
         {
             //------------Setup for test-------------------------
+            SetupMef();
             var dialogWindow = new Mock<IDialog>();
             const enDsfActivityType ActivityType = enDsfActivityType.All;
             const bool IsFromActivityDrop = true;
 
-            var envRepo = EnvironmentRepository.Create(new Mock<IEnvironmentModel>().Object);
+            var envRepo = new TestLoadEnvironmentRespository();
+            //var envRepo = EnvironmentRepository.Create(new Mock<IEnvironmentModel>().Object);
 
             //------------Execute Test---------------------------
             var dialog = new TestResourcePickerDialog(ActivityType, envRepo, new Mock<IEventAggregator>().Object, new Mock<IAsyncWorker>().Object, IsFromActivityDrop)
