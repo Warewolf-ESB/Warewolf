58cd74301b1a5fdb7e3901d55894661e68cc2b6b
Ashley Lewis <DEV2\Ashley.lewis>
2013-11-21 10:10:13 +0200

    gate testing checkin

--- Dev/Dev2.Data.Tests/Operations/Dev2DecisionOperationTests.cs
+++ Dev/Dev2.Data.Tests/Operations/Dev2DecisionOperationTests.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Dev2.Data.Decisions.Operations;
-using Microsoft.VisualStudio.TestTools.UnitTesting;using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dev2.Data.Tests.Operations
 {
-    [TestClass][ExcludeFromCodeCoverage]
+    [TestClass]
+    [ExcludeFromCodeCoverage]
     public class Dev2DecisionOperationTests
     {
         #region Comparing Integers
