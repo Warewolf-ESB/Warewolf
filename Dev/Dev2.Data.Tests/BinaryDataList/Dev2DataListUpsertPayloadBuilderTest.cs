321c3dfde4930504fd93443c7f9776d043de6eee
Ashley Lewis <DEV2\Ashley.lewis>
2013-11-22 11:42:32 +0200

    gate testing checkin

--- Dev/Dev2.Data.Tests/BinaryDataList/Dev2DataListUpsertPayloadBuilderTest.cs
+++ Dev/Dev2.Data.Tests/BinaryDataList/Dev2DataListUpsertPayloadBuilderTest.cs
@@ -1,11 +1,12 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Dev2.Data.Factories;
 using Dev2.DataList.Contract.Builders;
-using Microsoft.VisualStudio.TestTools.UnitTesting;using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dev2.Data.Tests.BinaryDataList
 {
-    [TestClass][ExcludeFromCodeCoverage]
+    [TestClass]
+    [ExcludeFromCodeCoverage]
     public class Dev2DataListUpsertPayloadBuilderTest
     {
         [TestMethod]
