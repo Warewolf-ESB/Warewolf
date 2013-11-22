bd2b2d020affad7e57312760c51f4f04d73c9bc6
Ashley Lewis <DEV2\Ashley.lewis>
2013-11-22 13:15:41 +0200

    gate testing checkin

--- Dev/Dev2.Infrastructure.Tests/Communication/MemoTests.cs
+++ Dev/Dev2.Infrastructure.Tests/Communication/MemoTests.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using Dev2.Communication;
-using Microsoft.VisualStudio.TestTools.UnitTesting;using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dev2.Infrastructure.Tests.Communication
 {
-    [TestClass][ExcludeFromCodeCoverage]
+    [TestClass]
+    [ExcludeFromCodeCoverage]
     public class MemoTests
     {
         [TestMethod]
