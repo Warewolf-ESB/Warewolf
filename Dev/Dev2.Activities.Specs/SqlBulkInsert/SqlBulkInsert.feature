fc305431feb7f14171962d1cbee2b9a006dcc710
Tshepo Ntlhokoa <DEV2\Tshepo.Ntlhokoa>
2013-11-27 17:06:31 +0200

    BDD - SQL Bulk Insert

---
+++ Dev/Dev2.Activities.Specs/SqlBulkInsert/SqlBulkInsert.feature
@@ -0,0 +1,107 @@
+﻿Feature: SqlBulkInsert
+	In order to quickly insert large amounts of data in a sql server database
+	As a Warewolf user
+	I want a tool that performs this action
+
+Scenario: Import data into table with check contraint disabled
+	Given I have this data
+		| Col1 | Col2     | Col3                                 |
+		| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 1    | TestData | b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	And Check constraints is not selected
+	When the tool is executed
+	Then the new table will have
+		| Col1 | Col2     | Col3                                 |
+		| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 2    | TestData | b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+
+Scenario: Import data into Table with check contraint enabled
+	Given I have this data
+		| Col1 | Col2     | Col3                                 |
+		| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 1    | TestData | b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	And Check constraints is selected
+	When the tool is executed
+	Then the new table will will have 0 of rows
+
+Scenario: Import data into Table with keep identity disabled
+	Given I have this data
+		| Col1 | Col2     | Col3                                 |
+		| 4    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 6    | TestData | bc7a9611-102e-4899-82b8-97ff1517d268 |
+		| 8    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Keep identity is not selected
+	When the tool is executed
+	Then the new table will have
+		| Col1 | Col2     | Col3                                 |
+		| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 2    | TestData | bc7a9611-102e-4899-82b8-97ff1517d268 |
+		| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |		
+
+Scenario: Import data into Table with keep identity enabled
+	Given I have this data
+		| Col1 | Col2     | Col3                                 |
+		| 4    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 6    | TestData | bc7a9611-102e-4899-82b8-97ff1517d268 |
+		| 8    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Keep identity is selected
+	When the tool is executed
+	Then the new table will have
+		| Col1 | Col2     | Col3                                 |
+		| 4    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+		| 6    | TestData | bc7a9611-102e-4899-82b8-97ff1517d268 |
+		| 8    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |		
+
+Scenario: Import data into Table with skip blank rows disabled
+#Note the second row is blank from the source data
+Given I have this data
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	|      |          |										 |
+	| 2    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Skip rows is not selected
+	When the tool is executed
+	Then the new table will will have 0 of rows
+
+Scenario: Import data into Table with skip blank rows enabled
+#Note the second row is blank from the source data
+Given I have this data
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	|      |          |										 |
+	| 2    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Skip rows is selected
+	When the tool is executed
+	Then the new table will will have 3 of rows		
+
+Scenario: Import data into Table with fire triggers disabled
+#A trigger exists in the table [SqlBulkInsertSpecFlowTestTable] against the column [Col2] to add a default value of XXXXXXXX.
+Given I have this data
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 2    |          |	b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Fire triggers is not selected
+	When the tool is executed
+	Then the new table will have
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 2    |          |	b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+
+Scenario: Import data into Table with fire triggers enabled
+#A trigger exists in the table [SqlBulkInsertSpecFlowTestTable] against the column [Col2] to add a default value of XXXXXXXX.
+Given I have this data
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 2    |          |	b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
+	And Fire triggers is selected
+	When the tool is executed
+	Then the new table will have
+	| Col1 | Col2     | Col3                                 |
+	| 1    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |
+	| 2    | XXXXXXXX |	b89416b9-5b24-4f95-bd11-25d9db8160a2 |
+	| 3    | TestData | 279c690e-3304-47a0-8bde-5d3ca2520a34 |	
---
+++ Dev/Dev2.Activities.Specs/SqlBulkInsert/SqlBulkInsertSteps.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using ActivityUnitTests;
+using Dev2.Common;
+using Dev2.DataList.Contract;
+using Dev2.Integration.Tests.Services.Sql;
+using Dev2.Runtime.ServiceModel.Data;
+using Dev2.TO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Dev2.Activities.Specs.SqlBulkInsert
+{
+    [Binding]
+    public class SqlBulkInsertSteps : BaseActivityUnitTest
+    {
+        Guid _dlID;
+        DsfSqlBulkInsertActivity _sqlBulkInsert;
+        IDataListCompiler _compiler;
+        DbSource _dbSource;
+        private string _dataShape;
+
+        public void SetupScenerio()
+        {
+            _sqlBulkInsert = new DsfSqlBulkInsertActivity();
+            _dbSource = SqlServerTests.CreateDev2TestingDbSource();
+            _sqlBulkInsert.Database = _dbSource;
+            _sqlBulkInsert.TableName = "SqlBulkInsertSpecFlowTestTable";
+            var dataColumnMappings = new List<DataColumnMapping>
+            {
+                new DataColumnMapping
+                {
+                    InputColumn = "[[rs(*).Col1]]",
+                    OutputColumn = new DbColumn {ColumnName = "Col1",
+                    DataType = typeof(Int32),
+                    MaxLength = 100},
+                }, new DataColumnMapping
+                {
+                    InputColumn = "[[rs(*).Col2]]",
+                    OutputColumn = new DbColumn { ColumnName = "Col2",
+                    DataType = typeof(String),
+                    MaxLength = 100 }
+                }, new DataColumnMapping
+                {
+                    InputColumn = "[[rs(*).Col3]]",
+                    OutputColumn = new DbColumn { ColumnName = "Col3",
+                    DataType = typeof(Guid),
+                    MaxLength = 100 }
+                }
+            };
+            _sqlBulkInsert.InputMappings = dataColumnMappings;
+            TestStartNode = new FlowStep
+            {
+                Action = _sqlBulkInsert
+            };
+            ErrorResultTO errors;
+            var data = _compiler.ConvertFrom(_dlID, DataListFormat.CreateFormat(GlobalConstants._XML), enTranslationDepth.Data, out errors);
+            CurrentDl = _dataShape;
+            TestData = data;
+        }
+
+        [Given(@"I have this data")]
+        public void GivenIHaveThisData(Table table)
+        {
+            AddTableToDataList(table);
+            SetupScenerio();
+        }
+
+        void AddTableToDataList(Table table)
+        {
+            _compiler = DataListFactory.CreateDataListCompiler();
+            // build up DataTable
+            var dbData = new DataTable("rs");
+            foreach(var columnName in table.Header)
+            {
+                dbData.Columns.Add(columnName);
+            }
+            foreach(TableRow row in table.Rows)
+            {
+                dbData.Rows.Add(row[0],row[1],row[2]);    
+            }
+            // Execute Translator
+            ErrorResultTO errors;
+            _dataShape = "<root><rs><Col1/><Col2/><Col3/></rs></root>";
+            _dlID = _compiler.ConvertTo(DataListFormat.CreateFormat(GlobalConstants._DATATABLE), dbData, _dataShape, out errors);
+
+        }
+        
+        [Given(@"Check constraints is not selected")]
+        public void GivenCheckConstraintsIsNotSelected()
+        {
+            _sqlBulkInsert.CheckConstraints = false;
+        }
+        
+        [When(@"the tool is executed")]
+        public void WhenTheToolIsExecuted()
+        {
+            ExecuteProcess();
+        }
+
+        [Then(@"the new table will have")]
+        public void ThenTheNewTableWillHave(Table table)
+        {
+            var t1 = new DataTable();
+            using(var connection = new SqlConnection(_dbSource.ConnectionString))
+            {
+                connection.Open();
+                const string Query = "SELECT * FROM SqlBulkInsertSpecFlowTestTable";
+
+                using(var cmd = new SqlCommand(Query, connection))
+                {
+                    using(var a = new SqlDataAdapter(cmd))
+                    {
+                        a.Fill(t1);
+                    }
+                }
+            }
+
+
+            List<DataRow> dataRows = t1.Rows.Cast<DataRow>().ToList();
+            List<TableRow> tableRows = table.Rows.ToList();
+
+            using (var connection = new SqlConnection(_dbSource.ConnectionString))
+            {
+                connection.Open();
+                string q1 = "truncate table SqlBulkInsertSpecFlowTestTable";
+
+                using (var cmd = new SqlCommand(q1, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Assert.AreEqual(dataRows.Count, tableRows.Count);
+           
+            for (int i = 0; i < dataRows.Count; i++)
+            {
+                Assert.AreEqual(dataRows[i][0].ToString(), tableRows[i][0]);
+                Assert.AreEqual(dataRows[i][1].ToString(), tableRows[i][1]);
+                Assert.AreEqual(dataRows[i][2].ToString(), tableRows[i][2]);
+            }
+        }
+
+        [Given(@"Check constraints is selected")]
+        public void GivenCheckConstraintsIsSelected()
+        {
+            _sqlBulkInsert.CheckConstraints = true;
+        }
+
+        [Given(@"Keep identity is not selected")]
+        public void GivenKeepIdentityIsNotSelected()
+        {
+            _sqlBulkInsert.KeepIdentity = false;
+        }
+
+        [Given(@"Keep identity is selected")]
+        public void GivenKeepIdentityIsSelected()
+        {
+            _sqlBulkInsert.KeepIdentity = true;
+        }
+
+        [Then(@"the new table will will have (.*) of rows")]
+        public void ThenTheNewTableWillWillHaveOfRows(int numberOfRows)
+        {
+            object numberOfRowsInDb;
+            using (var connection = new SqlConnection(_dbSource.ConnectionString))
+            {
+                connection.Open();
+                const string q1 = "SELECT Count(*) FROM SqlBulkInsertSpecFlowTestTable";
+                using (var cmd = new SqlCommand(q1, connection))
+                {
+                   numberOfRowsInDb = cmd.ExecuteScalar();
+                }
+            }
+
+            using (var connection = new SqlConnection(_dbSource.ConnectionString))
+            {
+                connection.Open();
+                const string q2 = "truncate table SqlBulkInsertSpecFlowTestTable";
+                using (var cmd = new SqlCommand(q2, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Assert.AreEqual(numberOfRows, numberOfRowsInDb);
+        }
+
+        [Given(@"Skip rows is selected")]
+        public void GivenSkipRowsIsSelected()
+        {
+            _sqlBulkInsert.IgnoreBlankRows = true;
+        }
+
+        [Given(@"Skip rows is not selected")]
+        public void GivenSkipRowsIsNotSelected()
+        {
+            _sqlBulkInsert.IgnoreBlankRows = false;
+        }
+
+        [Given(@"Fire triggers is not selected")]
+        public void GivenFireTriggersIsNotSelected()
+        {
+            _sqlBulkInsert.FireTriggers = false;
+        }
+
+        [Given(@"Fire triggers is selected")]
+        public void GivenFireTriggersIsSelected()
+        {
+            _sqlBulkInsert.FireTriggers = true;
+        }
+    }
+}
