d40aa5a7e713bcf5980f44c58a7f56063ffcae8e
Ashley Lewis <DEV2\Ashley.lewis>
2013-11-29 13:49:22 +0200

    null

--- Dev/Dev2.Runtime.Tests/NetworkTest.cs
+++ Dev/Dev2.Runtime.Tests/NetworkTest.cs
@@ -17,26 +17,6 @@ namespace Dev2.DynamicServices.Test
     [ExcludeFromCodeCoverage]
     public class NetworkTest
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
         #region Additional test attributes
         //
         // You can use the following additional attributes as you write your tests:
