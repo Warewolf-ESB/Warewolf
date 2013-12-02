9da0d7c873b814db97e01e2fc445d219c8af1135
Ashley Lewis <DEV2\Ashley.lewis>
2013-12-02 18:13:02 +0200

    Rollback botched merge from UIGroomingBranch

---
+++ Dev/Dev2.Studio.Core/Models/DecisionModels/BetweenOperatorType.cs
@@ -0,0 +1,11 @@
+﻿namespace Dev2.Studio.Core.Models {
+    public class BetweenOperatorType : OperatorType {
+        public BetweenOperatorType() {
+
+        }
+
+        public BetweenOperatorType(string operatorName, string friendlyName, string operatorSymbol, DecisionType parent) : base(operatorName, friendlyName, operatorSymbol, parent) {
+                
+        }
+    }
+}
