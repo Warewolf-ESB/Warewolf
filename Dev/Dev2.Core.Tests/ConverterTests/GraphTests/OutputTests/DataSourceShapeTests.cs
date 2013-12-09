6f472bf3341493bfaae83436f62992c483428fb0
Ashley Lewis <DEV2\Ashley.lewis>
2013-12-09 22:08:03 +0200

    gate testing checkin

--- Dev/Dev2.Core.Tests/ConverterTests/GraphTests/OutputTests/DataSourceShapeTests.cs
+++ Dev/Dev2.Core.Tests/ConverterTests/GraphTests/OutputTests/DataSourceShapeTests.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Unlimited.UnitTest.Framework.ConverterTests.GraphTests.OutputTests
 {
-    //[TestClass][ExcludeFromCodeCoverage]
+    //[TestClass]
+    [ExcludeFromCodeCoverage]
     public class DataSourceShapeTests
     {
         //Here for future extension
