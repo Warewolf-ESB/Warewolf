76b0444b1a7cfc4e2868fea221f4646551512b0e
Ashley Lewis <DEV2\Ashley.lewis>
2013-12-10 17:35:09 +0200

    gate testing checkin

--- Dev/Dev2.Core.Tests/ConverterTests/GraphTests/StringTests/XmlTests/XmlNavigatorTests.cs
+++ Dev/Dev2.Core.Tests/ConverterTests/GraphTests/StringTests/XmlTests/XmlNavigatorTests.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Unlimited.Framework.Converters.Graph.Interfaces;
 using Unlimited.Framework.Converters.Graph.String.Xml;
 
 namespace Unlimited.UnitTest.Framework.ConverterTests.GraphTests.StringTests.XmlTests
 {
-    [TestClass][ExcludeFromCodeCoverage]
+    [TestClass]
+    [ExcludeFromCodeCoverage]
     public class XmlNavigatorTests
     {
         internal string Given()
