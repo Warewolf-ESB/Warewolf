d1f1f57fd2e5f71a9aed0df87d8969dca23de0a7
Ashley Lewis <DEV2\Ashley.lewis>
2013-12-11 18:04:05 +0200

    gate testing checkin

--- Dev/Dev2.Core.Tests/ConverterTests/GraphTests/OutputTests/OutputDescriptionTests.cs
+++ Dev/Dev2.Core.Tests/ConverterTests/GraphTests/OutputTests/OutputDescriptionTests.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
 namespace Unlimited.UnitTest.Framework.ConverterTests.GraphTests.OutputTests
 {
-    //[TestClass][ExcludeFromCodeCoverage]
+    //[TestClass]
+    [ExcludeFromCodeCoverage]
     public class OutputDescriptionTests
     {
         //Here for future extension
