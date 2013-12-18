f86cd1957c5a2cfd2f76dec741ded9737686cf4d
Hagashen Naidu <DEV2\Hagashen.Naidu>
2013-12-18 17:16:27 +0200

    Added test for quick debug

--- Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/DebugUITests.cs
+++ Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/DebugUITests.cs
@@ -68,7 +68,7 @@ namespace Dev2.Studio.UI.Tests
             var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).Replace("Roaming", "");
             var settingPath = Path.Combine(appData, @"Local\Warewolf\DebugData\PersistSettings.dat");
 
-            if (File.Exists(settingPath))
+            if(File.Exists(settingPath))
             {
                 File.Delete(settingPath);
             }
@@ -93,11 +93,11 @@ namespace Dev2.Studio.UI.Tests
 
             DebugUIMap.ClickExecute();
             OutputUIMap.WaitForExecution();
-                
+
             //------------Assert Results-------------------------
 
             // Check for valid input in the input boxes ;)
-            for (int i = 0; i < 9; i++)
+            for(int i = 0; i < 9; i++)
             {
                 RibbonUIMap.ClickRibbonMenuItem("Debug");
                 PopupDialogUIMap.WaitForDialog();
@@ -109,7 +109,7 @@ namespace Dev2.Studio.UI.Tests
                 Assert.AreEqual("2", getInput.Text, "After executing " + i + " times the debug input dialog did not persist");
 
                 DebugUIMap.ClickExecute();
-                OutputUIMap.WaitForExecution();                   
+                OutputUIMap.WaitForExecution();
             }
 
             //Now check the XML tab works ;)
@@ -133,7 +133,7 @@ namespace Dev2.Studio.UI.Tests
             SendKeys.SendWait(KeyboardCommands.CopyCommand);
             var actualXML = Clipboard.GetData(DataFormats.Text);
 
-            actualXML = actualXML.ToString().Replace(Environment.NewLine, "").Replace(" ","");
+            actualXML = actualXML.ToString().Replace(Environment.NewLine, "").Replace(" ", "");
 
             //close the window ;)
             DebugUIMap.CloseDebugWindow_ByCancel();
@@ -145,6 +145,55 @@ namespace Dev2.Studio.UI.Tests
         }
 
         [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("Debug_QuickDebug")]
+        public void Debug_WhenUsingQuickDebugCommand_ExpectSavedInputsUsedInExecution()
+        {
+
+            // Remove the PersistSettings.dat ;)
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).Replace("Roaming", "");
+            var settingPath = Path.Combine(appData, @"Local\Warewolf\DebugData\PersistSettings.dat");
+
+            if(File.Exists(settingPath))
+            {
+                File.Delete(settingPath);
+            }
+
+            //------------Setup for test--------------------------
+            //Open the correct workflow
+            ExplorerUIMap.EnterExplorerSearchText("Bug9394");
+            ExplorerUIMap.DoubleClickOpenProject("localhost", "WORKFLOWS", "BUGS", "Bug9394");
+
+            // prime the values ;)
+            RibbonUIMap.ClickRibbonMenuItem("Debug");
+            PopupDialogUIMap.WaitForDialog();
+
+            Playback.Wait(100);
+            SendKeys.SendWait("{TAB}");
+            Playback.Wait(200);
+            SendKeys.SendWait("1");
+            Playback.Wait(200);
+            SendKeys.SendWait("{TAB}");
+            Playback.Wait(200);
+            SendKeys.SendWait("2");
+
+            DebugUIMap.ClickExecute();
+            OutputUIMap.WaitForExecution();
+            RibbonUIMap.ClickRibbonMenuItem("Debug");
+            PopupDialogUIMap.WaitForDialog();
+            DebugUIMap.CloseDebugWindow_ByCancel();
+            //---------------Execute------------------------------                
+            SendKeys.SendWait("{F6}");
+            //------------Assert Results-------------------------
+            OutputUIMap.WaitForExecution();
+            UITestControlCollection uiTestControlCollection = OutputUIMap.GetOutputWindow();
+            Assert.AreEqual(4, uiTestControlCollection.Count);
+            UITestControl executionStep = uiTestControlCollection[uiTestControlCollection.Count - 1];
+            string workflowStepName = OutputUIMap.GetStepName(executionStep);
+            Assert.AreEqual("Bug9394", workflowStepName);
+        }
+
+        [TestMethod]
         [Owner("Massimo Guerrera")]
         [TestCategory("DebugOutput_whenRun10Time")]
         public void DebugOutput_WhenRun10Times_NormalExecution_CloseTagsReturned10Times()
@@ -152,7 +201,7 @@ namespace Dev2.Studio.UI.Tests
             //------------Setup for test--------------------------
             //Open the correct workflow
             ExplorerUIMap.EnterExplorerSearchText("TravsTestFlow");
-            ExplorerUIMap.DoubleClickOpenProject("localhost", "WORKFLOWS", "TRAV", "TravsTestFlow");                   
+            ExplorerUIMap.DoubleClickOpenProject("localhost", "WORKFLOWS", "TRAV", "TravsTestFlow");
 
             //------------Assert Results-------------------------
 
@@ -160,10 +209,10 @@ namespace Dev2.Studio.UI.Tests
             for(int i = 0; i < 9; i++)
             {
                 RibbonUIMap.ClickRibbonMenuItem("Debug");
-                PopupDialogUIMap.WaitForDialog();               
+                PopupDialogUIMap.WaitForDialog();
 
                 DebugUIMap.ClickExecute();
-                OutputUIMap.WaitForExecution();                
+                OutputUIMap.WaitForExecution();
                 UITestControlCollection uiTestControlCollection = OutputUIMap.GetOutputWindow();
                 Assert.IsTrue(uiTestControlCollection.Count > 1);
                 UITestControl lastStep = uiTestControlCollection[uiTestControlCollection.Count - 1];
