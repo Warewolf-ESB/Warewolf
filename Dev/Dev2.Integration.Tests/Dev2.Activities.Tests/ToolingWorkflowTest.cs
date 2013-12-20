03a7ca910fbc19f62b7afb6f00bbfdb587ee56a4
Travis Frisinger <DEV2\Travis.Frisinger>
2013-12-20 13:01:18 +0200

    null

--- Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/ToolingWorkflowTest.cs
+++ Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/ToolingWorkflowTest.cs
@@ -11,54 +11,13 @@ namespace Dev2.Integration.Tests.Dev2.Activities.Tests
     [TestClass]
     public class ToolingWorkflowTest
     {
-        string WebserverURI = ServerSettings.WebserverURI;
-
-        public ToolingWorkflowTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
+        readonly string WebserverURI = ServerSettings.WebserverURI;
 
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+        public TestContext TestContext { get; set; }
 
         [TestMethod]
         public void AllToolsTestExpectPass()
