5b6a91fbb968a488cb8ee3dd198f485b2b5091d7
Travis Frisinger <DEV2\Travis.Frisinger>
2014-01-08 13:38:52 +0200

    Updated Release Bat Files

---
+++ WarewolfInstaller-SharpSetup/Release Tool Chain/version.bat
@@ -0,0 +1,5 @@
+cd "C:\Development\WarewolfInstaller-SharpSetup\ProductBuild\Server"
+updatever-server.bat "%1%"
+
+cd "C:\Development\WarewolfInstaller-SharpSetup\ProductBuild\Studio"
+updatever-studio.bat "%1%"
