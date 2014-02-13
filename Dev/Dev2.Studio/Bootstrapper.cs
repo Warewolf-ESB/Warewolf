b531811652b35ed635868ef87dc2ebe08a7f7b75
Travis Frisinger <DEV2\Travis.Frisinger>
2014-02-13 07:40:39 +0200

    null

--- Dev/Dev2.Studio/Bootstrapper.cs
+++ Dev/Dev2.Studio/Bootstrapper.cs
@@ -10,10 +10,13 @@ using System.Windows;
 using Caliburn.Micro;
 using Dev2.Composition;
 using Dev2.Studio;
+using Dev2.Studio.AppResources.ExtensionMethods;
 using Dev2.Studio.Controller;
 using Dev2.Studio.Core.AppResources.WindowManagers;
+using Dev2.Studio.Core.Controller;
 using Dev2.Studio.Core.Diagnostics;
 using Dev2.Studio.Core.Helpers;
+using Dev2.Studio.Core.Services;
 using Dev2.Studio.StartupResources;
 using Dev2.Studio.ViewModels;
 using Dev2.Workspaces;
@@ -126,6 +129,56 @@ namespace Dev2
 
         #region Private Methods
 
+        /*
+         * DELETE THIS METHOD AND LOOSE A VERY IMPORTANT PART OF YOU
+         * 
+         * IT IS REQUIRED FOR UPDATES IN RELEASE MODE. 
+         * REMOVING IT MEANS IT IS NOT POSSIBLE FOR ME TO BUILD AND INSTALLER
+         */
+        private bool CheckWindowsService()
+        {
+            IWindowsServiceManager windowsServiceManager = ImportService.GetExportValue<IWindowsServiceManager>();
+            IPopupController popup = ImportService.GetExportValue<IPopupController>();
+
+            if(windowsServiceManager == null)
+            {
+                throw new Exception("Unable to instantiate the windows service manager.");
+            }
+
+            if(popup == null)
+            {
+                throw new Exception("Unable to instantiate the popup manager.");
+            }
+
+            if(!windowsServiceManager.Exists())
+            {
+                popup.Show("The Warewolf service isn't installed. Please re-install the Warewolf server.", "Server Missing", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if(!windowsServiceManager.IsRunning())
+            {
+                MessageBoxResult promptResult = popup.Show("The Warewolf service isn't running would you like to start it?", "Service not Running", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+
+                if(promptResult == MessageBoxResult.Cancel)
+                {
+                    return false;
+                }
+
+                if(promptResult == MessageBoxResult.No)
+                {
+                    return true;
+                }
+
+                if(!windowsServiceManager.Start())
+                {
+                    popup.Show("A time out occurred while trying to start the Warewolf server service. Please try again.", "Timeout", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
         private void PreloadReferences()
         {
