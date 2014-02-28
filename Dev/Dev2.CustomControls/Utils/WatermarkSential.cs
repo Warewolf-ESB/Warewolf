6f91831a1ab341691b2b62b1d0105c5c2550b726
Travis Frisinger <DEV2\Travis.Frisinger>
2014-02-28 14:02:22 +0200

    10999 QA Feedback ;)

--- Dev/Dev2.CustomControls/Utils/WatermarkSential.cs
+++ Dev/Dev2.CustomControls/Utils/WatermarkSential.cs
@@ -3,6 +3,12 @@ namespace Dev2.CustomControls.Utils
 {
     public static class WatermarkSential
     {
+        /// <summary>
+        /// This value is only set when scrolling a designer
+        /// The reason this exist is due to the fact that when a designer is loaded not
+        /// all water marks are updated. Only on scroll, hence why this is static. 
+        /// You can only scroll 1 designer at a time ;)
+        /// </summary>
         public static bool IsWatermarkBeingApplied { get; set; }
     }
 }
