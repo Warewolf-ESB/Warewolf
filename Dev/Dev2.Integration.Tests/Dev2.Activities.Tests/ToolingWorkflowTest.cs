9d41bfe7a4fc6c7646bebcd0a3a11d3d85d2c6bc
Hagashen Naidu <DEV2\Hagashen.Naidu>
2014-03-19 15:39:32 +0200

    changed assert

--- Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/ToolingWorkflowTest.cs
+++ Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/ToolingWorkflowTest.cs
@@ -39,8 +39,8 @@ namespace Dev2.Integration.Tests.Dev2.Activities.Tests
 
             // Find Record Index: PASS
             const string Expected = "<DataList><Test rowID=\"1\"><Result>ForEach: PASS</Result></Test><Test rowID=\"2\"><Result>Switch: PASS</Result></Test><Test rowID=\"3\"><Result>Decision: PASS</Result></Test><Test rowID=\"4\"><Result>Count Records: PASS</Result></Test><Test rowID=\"5\"><Result>Delete Record: PASS</Result></Test><Test rowID=\"6\"><Result>Sort Records: PASS</Result></Test><Test rowID=\"7\"><Result>Find Record Index: PASS</Result></Test><Test rowID=\"8\"><Result>Assign: PASS</Result></Test><Test rowID=\"9\"><Result>Base Conversion: PASS</Result></Test><Test rowID=\"10\"><Result>Case Convert: PASS</Result></Test><Test rowID=\"11\"><Result>Data Merge: FAIL</Result></Test><Test rowID=\"12\"><Result>Data Split: PASS</Result></Test><Test rowID=\"13\"><Result>Date and Time: PASS</Result></Test><Test rowID=\"14\"><Result>Date and Time Difference: PASS</Result></Test><Test rowID=\"15\"><Result>Find Index: PASS</Result></Test><Test rowID=\"16\"><Result>Format Number: INCONCLUSIVE</Result></Test><Test rowID=\"17\"><Result>Javascript:PASS</Result></Test><Test rowID=\"18\"><Result>Unique: PASS</Result></Test><Test rowID=\"19\"><Result>XPath Tool: PASS</Result></Test><Test rowID=\"20\"><Result>GetWebRequest: PASS</Result></Test><Test rowID=\"21\"><Result>File Copy: Pass</Result></Test><Test rowID=\"22\"><Result>Success</Result></Test><Test rowID=\"23\"><Result>Find Record Index: PASS</Result></Test></DataList>";
-
-            Assert.IsTrue(responseData.Contains(Expected), "Expected [ " + Expected + ", But Got [ " + responseData + " ]");
+            StringAssert.Contains(responseData, Expected);
+            //Assert.IsTrue(responseData.Contains(Expected), "Expected [ " + Expected + ", But Got [ " + responseData + " ]");
         }
 
         [TestMethod]
