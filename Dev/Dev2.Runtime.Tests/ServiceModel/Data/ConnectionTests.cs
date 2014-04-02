c90aaed06a6955b692a04490b40e8edeecd6f31e
Travis Frisinger <DEV2\Travis.Frisinger>
2014-04-02 08:01:41 +0200

    Bug 11583 Fixes

--- Dev/Dev2.Runtime.Tests/ServiceModel/Data/ConnectionTests.cs
+++ Dev/Dev2.Runtime.Tests/ServiceModel/Data/ConnectionTests.cs
@@ -68,6 +68,102 @@ namespace Dev2.Tests.Runtime.ServiceModel.Data
             Assert.AreEqual(errorMessage, result.ErrorMessage);
         }
 
+
+        [TestMethod]
+        [Owner("Travis Frisinger")]
+        [TestCategory("Connection_Save")]
+        public void Connection_Save_WhenBlankUserAndPass_ExpectConnectionWithSingleSlashAsUser()
+        {
+            //------------Setup for test--------------------------
+            var connections = CreateConnection();
+            const string conStr = @"<Source ID=""2aa3fdba-e0c3-47dd-8dd5-e6f24aaf5c7a"" Name=""test server"" Type=""Dev2Server"" ConnectionString=""AppServerUri=http://178.63.172.163:3142/dsf;WebServerPort=3142;AuthenticationType=User;UserName=;Password="" Version=""1.0"" ResourceType=""Server"" ServerID=""51a58300-7e9d-4927-a57b-e5d700b11b55"">
+  <TypeOf>Dev2Server</TypeOf>
+  <DisplayName>test server</DisplayName>
+  <Category>WAREWOLF SERVERS</Category>
+  <Signature xmlns=""http://www.w3.org/2000/09/xmldsig#"">
+    <SignedInfo>
+      <CanonicalizationMethod Algorithm=""http://www.w3.org/TR/2001/REC-xml-c14n-20010315"" />
+      <SignatureMethod Algorithm=""http://www.w3.org/2000/09/xmldsig#rsa-sha1"" />
+      <Reference URI="""">
+        <Transforms>
+          <Transform Algorithm=""http://www.w3.org/2000/09/xmldsig#enveloped-signature"" />
+        </Transforms>
+        <DigestMethod Algorithm=""http://www.w3.org/2000/09/xmldsig#sha1"" />
+        <DigestValue>1ia51dqx+BIMQ4QgLt+DuKtTBUk=</DigestValue>
+      </Reference>
+    </SignedInfo>
+    <SignatureValue>Wqd39EqkFE66XVETuuAqZveoTk3JiWtAk8m1m4QykeqY4/xQmdqRRSaEfYBr7EHsycI3STuILCjsz4OZgYQ2QL41jorbwULO3NxAEhu4nrb2EolpoNSJkahfL/N9X5CvLNwpburD4/bPMG2jYegVublIxE50yF6ZZWG5XiB6SF8=</SignatureValue>
+  </Signature>
+</Source>";
+
+            var xe = XElement.Parse(conStr);
+            var conObj = new Connection(xe);
+            var jsonObj = JsonConvert.SerializeObject(conObj);
+
+            //------------Execute Test---------------------------
+            var savedConObj = connections.Save(jsonObj, Guid.Empty, Guid.Empty);
+            //------------Assert Results-------------------------
+            var connection = JsonConvert.DeserializeObject<Connection>(savedConObj);
+
+            Assert.AreEqual("\\", connection.UserName);
+            Assert.AreEqual(string.Empty, connection.Password);
+
+        }
+
+        [TestMethod]
+        [Owner("Travis Frisinger")]
+        [TestCategory("Connection_Save")]
+        public void Connection_Get_WhenBlankUserAndPass_ExpectConnectionWithSingleSlashAsUser()
+        {
+            //------------Setup for test--------------------------
+            const string fetchGuid = "2aa3fdba-e0c3-47dd-8dd5-e6f24aaf5c7a";
+
+            var connections = CreateConnection();
+
+            const string conStr = @"<Source ID=""2aa3fdba-e0c3-47dd-8dd5-e6f24aaf5c7a"" Name=""test server"" Type=""Dev2Server"" ConnectionString=""AppServerUri=http://178.63.172.163:3142/dsf;WebServerPort=3142;AuthenticationType=User;UserName=;Password="" Version=""1.0"" ResourceType=""Server"" ServerID=""51a58300-7e9d-4927-a57b-e5d700b11b55"">
+  <TypeOf>Dev2Server</TypeOf>
+  <DisplayName>test server</DisplayName>
+  <Category>WAREWOLF SERVERS</Category>
+  <Signature xmlns=""http://www.w3.org/2000/09/xmldsig#"">
+    <SignedInfo>
+      <CanonicalizationMethod Algorithm=""http://www.w3.org/TR/2001/REC-xml-c14n-20010315"" />
+      <SignatureMethod Algorithm=""http://www.w3.org/2000/09/xmldsig#rsa-sha1"" />
+      <Reference URI="""">
+        <Transforms>
+          <Transform Algorithm=""http://www.w3.org/2000/09/xmldsig#enveloped-signature"" />
+        </Transforms>
+        <DigestMethod Algorithm=""http://www.w3.org/2000/09/xmldsig#sha1"" />
+        <DigestValue>1ia51dqx+BIMQ4QgLt+DuKtTBUk=</DigestValue>
+      </Reference>
+    </SignedInfo>
+    <SignatureValue>Wqd39EqkFE66XVETuuAqZveoTk3JiWtAk8m1m4QykeqY4/xQmdqRRSaEfYBr7EHsycI3STuILCjsz4OZgYQ2QL41jorbwULO3NxAEhu4nrb2EolpoNSJkahfL/N9X5CvLNwpburD4/bPMG2jYegVublIxE50yF6ZZWG5XiB6SF8=</SignatureValue>
+  </Signature>
+</Source>";
+
+            // create it save it and finally fetch it to ensure it has th
+
+            var xe = XElement.Parse(conStr);
+            var conObj = new Connection(xe);
+            var jsonObj = JsonConvert.SerializeObject(conObj);
+            var savedObj = connections.Save(jsonObj, Guid.Empty, Guid.Empty);
+            var connection = JsonConvert.DeserializeObject<Connection>(savedObj);
+
+            //Pre-asserts
+            Assert.AreEqual("\\", connection.UserName);
+            Assert.AreEqual(String.Empty, connection.Password);
+
+            //------------Execute Test---------------------------
+            var fetchedObj = connections.Get(fetchGuid, Guid.Empty, Guid.Empty);
+
+            //------------Assert Results-------------------------
+
+            // ensure it was returned with empty and empty
+
+            Assert.AreEqual(String.Empty, fetchedObj.UserName);
+            Assert.AreEqual(string.Empty, fetchedObj.Password);
+
+        }
+
         [TestMethod]
         [Owner("Hagashen Naidu")]
         [TestCategory("Connections_Search")]
