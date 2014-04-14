89ab5363495895549e058a9df2b8d544ac52a7a1
Travis Frisinger <DEV2\Travis.Frisinger>
2014-04-14 11:56:47 +0200

    11445 QA Corrections

--- Dev/Dev2.Common.Test/Utils/TextUtilsTests.cs
+++ Dev/Dev2.Common.Test/Utils/TextUtilsTests.cs
@@ -14,8 +14,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_ReplaceWackNWithWackRWackN_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "warewolf\r\n is\\n awesome\r\n";
-            string stringToReplace = "warewolf\n is\\n awesome\r\n";
+            const string expected = "warewolf\r\n is\\n awesome\r\n";
+            const string stringToReplace = "warewolf\n is\\n awesome\r\n";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
@@ -28,8 +28,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_WackNFirstChars_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "\r\nwarewolf\r\n is\\n awesome\r\n";
-            string stringToReplace = "\nwarewolf\n is\\n awesome\r\n";
+            const string expected = "\r\nwarewolf\r\n is\\n awesome\r\n";
+            const string stringToReplace = "\nwarewolf\n is\\n awesome\r\n";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
@@ -42,8 +42,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_WackNSecondChars_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "t\r\nwarewolf\r\n is\\n awesome\r\n";
-            string stringToReplace = "t\nwarewolf\n is\\n awesome\r\n";
+            const string expected = "t\r\nwarewolf\r\n is\\n awesome\r\n";
+            const string stringToReplace = "t\nwarewolf\n is\\n awesome\r\n";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
@@ -56,8 +56,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_WackNLastChars_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "t\r\nwarewolf\r\n is\\n awesome\r\n";
-            string stringToReplace = "t\nwarewolf\n is\\n awesome\n";
+            const string expected = "t\r\nwarewolf\r\n is\\n awesome\r\n";
+            const string stringToReplace = "t\nwarewolf\n is\\n awesome\n";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
@@ -70,8 +70,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_WackWackNFirstChars_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "\\nwarewolf\r\n is\\n awesome\r\ntest";
-            string stringToReplace = "\\nwarewolf\n is\\n awesome\ntest";
+            const string expected = "\\nwarewolf\r\n is\\n awesome\r\ntest";
+            const string stringToReplace = "\\nwarewolf\n is\\n awesome\ntest";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
@@ -84,8 +84,8 @@ namespace Dev2.Common.Tests.Utils
         public void TextUtils_ReplaceWorkflowNewLinesWithEnvironmentNewLines_WackkWackNFirstChars_ReplaceOccurs()
         {
             //------------Setup for test--------------------------
-            string expected = "\\4\\\r\n\r\n\\\\\\\r\n\r\n\\n\r\n";
-            string stringToReplace = "\\4\\\n\n\\\\\\\n\n\\n\n";
+            const string expected = "\\4\\\r\n\r\n\\\\\\\r\n\r\n\\n\r\n";
+            const string stringToReplace = "\\4\\\n\n\\\\\\\n\n\\n\n";
             //------------Execute Test---------------------------
             string actual = TextUtils.ReplaceWorkflowNewLinesWithEnvironmentNewLines(stringToReplace);
             //------------Assert Results-------------------------
