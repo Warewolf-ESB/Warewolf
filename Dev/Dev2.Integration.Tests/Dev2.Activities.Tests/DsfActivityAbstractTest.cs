8519b97ae4a8945efec096ea29580fa8db7f5e6f
Leon Rajindrapersadh <DEV2\Leon.Rajindrapersadh>
2014-04-17 07:22:38 +0200

    null

--- Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/DsfActivityAbstractTest.cs
+++ Dev/Dev2.Integration.Tests/Dev2.Activities.Tests/DsfActivityAbstractTest.cs
@@ -27,24 +27,7 @@ namespace Dev2.Integration.Tests.Dev2.Activities.Tests
             StringAssert.Contains(ResponseData, expected);
         }
 
-        [TestMethod]
-        [Owner("Leon Rajindrapersadh")]
-        [TestCategory("DsfActivity_DispatchDebugOutPut")]
-        public void DsfActivity_MethodName_Scenerio_Result()
-        {
-            string PostData = String.Format("{0}{1}", WebserverURI, "LastRecordSetNotationUpdatesEntry");
-            string expected = @"<nameSetindex=""1""><Name>Michael</Name><Surname>Cullen</Surname></nameSet>
-                                <nameSetindex=""2""><Name>Massimo</Name><Surname>Guerrera</Surname></nameSet>
-                                <nameSetindex=""3""><Name>MASSIMO</Name><Surname></Surname></nameSet>
-                                <nameSetindex=""4""><Name>0x4d415353494d4f</Name><Surname></Surname></nameSet>";
 
-            string ResponseData = TestHelper.PostDataToWebserver(PostData);
-
-            // Standardize the outputs (Remove newlines, etc)
-            expected = TestHelper.CleanUp(expected);
-            ResponseData = TestHelper.CleanUp(ResponseData);
-            StringAssert.Contains(ResponseData, expected);
-        }
 
     }
 }
