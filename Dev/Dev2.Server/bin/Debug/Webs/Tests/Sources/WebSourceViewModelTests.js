534ac4d635df95061f88c3d29d09884488188eee
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-09 15:29:55 +0200

    source wizards javascript unit tests

--- Dev/Dev2.Server/bin/Debug/Webs/Tests/Sources/WebSourceViewModelTests.js
+++ Dev/Dev2.Server/bin/Debug/Webs/Tests/Sources/WebSourceViewModelTests.js
@@ -1,44 +1,45 @@
-﻿///// <reference path="../../wwwroot/Scripts/_references.js" />
-///// <reference path="../../wwwroot/Scripts/Sources/WebSourceViewModel.js" />
-///// <reference path="../../wwwroot/Scripts/Dialogs/SaveViewModel.js" />
+﻿/// <reference path="../../wwwroot/Scripts/_references.js" />
+/// <reference path="../../wwwroot/Scripts/Sources/WebSourceViewModel.js" />
+/// <reference path="../../wwwroot/Scripts/Dialogs/SaveViewModel.js" />
 
-//module("Model Passes Validation");
+module("Model Passes Validation");
 
-//test("IsFormValidWithBareMinimumHttpAddressExpectedPassesValidation", function () {
+test("IsFormValidWithBareMinimumHttpAddressExpectedPassesValidation", function () {
 
-//    var model = new WebSourceViewModel();
-//    model.data.address("http:/");
-//    ok(model.isFormValid(), "Valid web address was declared invalid by web source model");
-//});
+    var model = new WebSourceViewModel();
+    model.data.address("http:/");
+    ok(model.isFormValid(), "Valid web address was declared invalid by web source model");
+});
 
-//test("IsFormValidWithBareMinimumFtpAddressExpectedPassesValidation", function () {
+test("IsFormValidWithBareMinimumFtpAddressExpectedPassesValidation", function () {
 
-//    var model = new WebSourceViewModel();
-//    model.data.address("ftp:/");
-//    ok(model.isFormValid(), "Valid web address was declared invalid by web source model");
-//});
+    var model = new WebSourceViewModel();
+    model.data.address("ftp:/");
+    ok(model.isFormValid(), "Valid web address was declared invalid by web source model");
+});
 
-//module("Model Fails Validation");
+module("Model Fails Validation");
 
-//test("FormValidationWithBlankAddressExpectedFailsValidation", function () {
+test("FormValidationWithBlankAddressExpectedFailsValidation", function () {
 
-//    var model = new WebSourceViewModel();
-//    model.data.address("");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new WebSourceViewModel();
+    model.data.address("");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("FormValidationWithInvalidAddressExpectedFailsValidation", function () {
+test("FormValidationWithInvalidAddressExpectedFailsValidation", function () {
 
-//    var model = new WebSourceViewModel();
-//    model.data.address("http::/www.google.co.za/");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new WebSourceViewModel();
+    model.data.address("http::/www.google.co.za/");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-///*
-//test("CanUserViewResultsInBrowser", function () {
+/*
+Ashley: I removed address validation because it is not possible. There is no standard format that satisfies all web addresses.
+test("CanUserViewResultsInBrowser", function () {
 
-//    var model = new WebSourceViewModel();
-//    model.data.address("http::/www.google.co.za/");
-//    ok(!model.viewInBrowser(), "Did Form Fail Validation");
-//});
-//*/
+    var model = new WebSourceViewModel();
+    model.data.address("http::/www.google.co.za/");
+    ok(!model.viewInBrowser(), "Did Form Fail Validation");
+});
+*/
