d5639df004452162edc2d06cbdc534decccbe6bc
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-09 15:21:10 +0200

    the rest of the db source tests

--- Dev/Dev2.Server/bin/Debug/Webs/Tests/Sources/DbSourceViewModelTests.js
+++ Dev/Dev2.Server/bin/Debug/Webs/Tests/Sources/DbSourceViewModelTests.js
@@ -11,195 +11,195 @@ test("ConstructorWithNoParametersExpectedIsEditingIsFalse", function () {
     equal(model.title(), "New Database Source", "Did Title Initialize");
 });
 
-//test("ConstructorWithNoParametersAndNewResourceNameExpectedResourceNameChanged", function() {
+test("ConstructorWithNoParametersAndNewResourceNameExpectedResourceNameChanged", function() {
 
-//    var dbSourceModel = new DbSourceViewModel();
-//    dbSourceModel.data.resourceName("new resource name");
-//    equal(dbSourceModel.data.resourceName(), "new resource name", "Resource Name Changed");
-//});
+    var dbSourceModel = new DbSourceViewModel();
+    dbSourceModel.data.resourceName("new resource name");
+    equal(dbSourceModel.data.resourceName(), "new resource name", "Resource Name Changed");
+});
 
-//module("DbSource Model Form Passes Validation");
+module("DbSource Model Form Passes Validation");
 
-//test("AuthentificationTypeSubscriberWithChangingAuthentificationTypeToUserExpectedUserInputMadeVisible", function() {
+test("AuthentificationTypeSubscriberWithChangingAuthentificationTypeToUserExpectedUserInputMadeVisible", function() {
 
-//    var model = new DbSourceViewModel();
-//    model.data.authenticationType("User");
-//    ok(model.isUserInputVisible());
-//});
+    var model = new DbSourceViewModel();
+    model.data.authenticationType("User");
+    ok(model.isUserInputVisible());
+});
 
-//test("IsFormValidWithUserAuthentificationAndDatabaseNameExpectedFormIsValid", function () {
+test("IsFormValidWithUserAuthentificationAndDatabaseNameExpectedFormIsValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("User");
-//    model.data.userID("user id");
-//    model.data.password("password");
-//    model.data.databaseName("database name");
-//    ok(model.isFormValid(), "Did Form Pass Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("User");
+    model.data.userID("user id");
+    model.data.password("password");
+    model.data.databaseName("database name");
+    ok(model.isFormValid(), "Did Form Pass Validation");
+});
 
-//test("IsFormValidWithWindowsAuthentificationAndDatabaseNameExpectedFormIsValid", function () {
+test("IsFormValidWithWindowsAuthentificationAndDatabaseNameExpectedFormIsValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("Windows");
-//    model.data.databaseName("database name");
-//    ok(model.isFormValid(), "Did Form Pass Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("Windows");
+    model.data.databaseName("database name");
+    ok(model.isFormValid(), "Did Form Pass Validation");
+});
 
-//module("DbSource Model Form Fails Validation");
+module("DbSource Model Form Fails Validation");
 
-//test("IsFormValidWithUserAuthentificationAndNoDatabaseNameExpectedFormIsNotValid", function() {
+test("IsFormValidWithUserAuthentificationAndNoDatabaseNameExpectedFormIsNotValid", function() {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("User");
-//    model.data.userID("user id");
-//    model.data.password("password");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("User");
+    model.data.userID("user id");
+    model.data.password("password");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("IsFormValidWithUserAuthentificationAndNoPasswordExpectedFormIsNotValid", function() {
+test("IsFormValidWithUserAuthentificationAndNoPasswordExpectedFormIsNotValid", function() {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("User");
-//    model.data.userID("user id");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("User");
+    model.data.userID("user id");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("IsFormValidWithUserAuthentificationAndNoUsernameExpectedFormIsNotValid", function () {
+test("IsFormValidWithUserAuthentificationAndNoUsernameExpectedFormIsNotValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("User");
-//    model.data.password("password");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("User");
+    model.data.password("password");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("IsFormValidWithWindowsAuthentificationAndNoDatabaseNameExpectedFormIsNotValid", function () {
+test("IsFormValidWithWindowsAuthentificationAndNoDatabaseNameExpectedFormIsNotValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address");
-//    model.data.serverType("some server type");
-//    model.data.authenticationType("Windows");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address");
+    model.data.serverType("some server type");
+    model.data.authenticationType("Windows");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("IsFormValidWithNoServerTypeExpectedFormIsNotValid", function () {
+test("IsFormValidWithNoServerTypeExpectedFormIsNotValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("set server address only");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("set server address only");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//test("IsFormValidWithNoServerAddressExpectedFormIsNotValid", function () {
+test("IsFormValidWithNoServerAddressExpectedFormIsNotValid", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.serverType("some server type only");
-//    ok(!model.isFormValid(), "Did Form Fail Validation");
-//});
+    var model = new DbSourceViewModel();
+    model.data.serverType("some server type only");
+    ok(!model.isFormValid(), "Did Form Fail Validation");
+});
 
-//module("DbSource to Save Data Model Binding");
+module("DbSource to Save Data Model Binding");
 
-//test("SaveDialogConstructorExpectedResourceTypeIsDbSource", function () {
+test("SaveDialogConstructorExpectedResourceTypeIsDbSource", function () {
 
-//    var model = new DbSourceViewModel();
-//    equal(model.saveViewModel.data.resourceType(), 'DbSource', "Is Resource Type Db Source");
-//});
+    var model = new DbSourceViewModel();
+    equal(model.saveViewModel.data.resourceType(), 'DbSource', "Is Resource Type Db Source");
+});
 
-//test("ChangeResourceIDExpectedSaveModelResourceIDChanged", function () {
+test("ChangeResourceIDExpectedSaveModelResourceIDChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.resourceID($.Guid.New());
-//    equal(model.saveViewModel.data.resourceID(), model.data.resourceID(), "Did Save Model Resource ID Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.resourceID($.Guid.New());
+    equal(model.saveViewModel.data.resourceID(), model.data.resourceID(), "Did Save Model Resource ID Change");
+});
 
-//test("ChangeResourceNameExpectedSaveModelResourceNameChanged", function () {
+test("ChangeResourceNameExpectedSaveModelResourceNameChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.resourceName("change resource name");
-//    equal(model.saveViewModel.data.resourceName(), model.data.resourceName(), "Did Save Model Resource Name Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.resourceName("change resource name");
+    equal(model.saveViewModel.data.resourceName(), model.data.resourceName(), "Did Save Model Resource Name Change");
+});
 
-//test("ChangeResourcePathExpectedSaveModelResourcePathChanged", function () {
+test("ChangeResourcePathExpectedSaveModelResourcePathChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.resourcePath("change resource path");
-//    equal(model.saveViewModel.data.resourcePath(), model.data.resourcePath(), "Did Save Model Resource Path Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.resourcePath("change resource path");
+    equal(model.saveViewModel.data.resourcePath(), model.data.resourcePath(), "Did Save Model Resource Path Change");
+});
 
-//test("ChangeServerExpectedSaveModelServerNameChanged", function () {
+test("ChangeServerExpectedSaveModelServerNameChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.server("change server name");
-//    equal(model.saveViewModel.data.server(), model.data.server(), "Did SaveModel Server Name Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.server("change server name");
+    equal(model.saveViewModel.data.server(), model.data.server(), "Did SaveModel Server Name Change");
+});
 
-//test("ChangeServerTypeExpectedSaveModelServerTypeChanged", function () {
+test("ChangeServerTypeExpectedSaveModelServerTypeChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.serverType("change server type");
-//    equal(model.saveViewModel.data.serverType(), model.data.serverType(), "Did SaveModel Server Type Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.serverType("change server type");
+    equal(model.saveViewModel.data.serverType(), model.data.serverType(), "Did SaveModel Server Type Change");
+});
 
-//test("ChangeDatabaseNameExpectedSaveModelDatabaseNameChanged", function () {
+test("ChangeDatabaseNameExpectedSaveModelDatabaseNameChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.databaseName("change database name");
-//    equal(model.saveViewModel.data.databaseName(), model.data.databaseName(), "Did SaveModel Database Name Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.databaseName("change database name");
+    equal(model.saveViewModel.data.databaseName(), model.data.databaseName(), "Did SaveModel Database Name Change");
+});
 
-//test("ChangeAuthenticationTypeExpectedSaveModelAuthenticationTypeChanged", function () {
+test("ChangeAuthenticationTypeExpectedSaveModelAuthenticationTypeChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.authenticationType("change authentication type");
-//    equal(model.saveViewModel.data.authenticationType(), model.data.authenticationType(), "Did SaveModel Authentication Type Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.authenticationType("change authentication type");
+    equal(model.saveViewModel.data.authenticationType(), model.data.authenticationType(), "Did SaveModel Authentication Type Change");
+});
 
-//test("ChangeUserNameExpectedSaveModeluserIDChanged", function () {
+test("ChangeUserNameExpectedSaveModeluserIDChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.userID("change user name");
-//    equal(model.saveViewModel.data.userID(), model.data.userID(), "Did SaveModel User Name Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.userID("change user name");
+    equal(model.saveViewModel.data.userID(), model.data.userID(), "Did SaveModel User Name Change");
+});
 
-//test("ChangePasswordExpectedSaveModelpasswordChanged", function () {
+test("ChangePasswordExpectedSaveModelpasswordChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.password("change password");
-//    equal(model.saveViewModel.data.password(), model.data.password(), "Did SaveModel Password Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.password("change password");
+    equal(model.saveViewModel.data.password(), model.data.password(), "Did SaveModel Password Change");
+});
 
-//test("ChangePortExpectedSaveModelportChanged", function () {
+test("ChangePortExpectedSaveModelportChanged", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.port("change port");
-//    equal(model.saveViewModel.data.port(), model.data.port(), "Did SaveModel Port Change");
-//});
+    var model = new DbSourceViewModel();
+    model.data.port("change port");
+    equal(model.saveViewModel.data.port(), model.data.port(), "Did SaveModel Port Change");
+});
 
-//module("DbSource Model Cancel Test");
+module("DbSource Model Cancel Test");
 
-//test("DbSource_RegressionTest_CancelTest_isTestResultsLoadingIsFalseAndLastTestTimeIsSetCorrectly", function () {
+test("DbSource_RegressionTest_CancelTest_isTestResultsLoadingIsFalseAndLastTestTimeIsSetCorrectly", function () {
 
-//    var model = new DbSourceViewModel();
-//    var testTimeTestValue = new Date().valueOf();
-//    model.cancelTest();
-//    ok(!model.isTestResultsLoading(), "Did Test Results Stop Loading");
-//    ok(model.testTime == testTimeTestValue, "Is The Last Test Time Now");
-//});
+    var model = new DbSourceViewModel();
+    var testTimeTestValue = new Date().valueOf();
+    model.cancelTest();
+    ok(!model.isTestResultsLoading(), "Did Test Results Stop Loading");
+    ok(model.testTime == testTimeTestValue, "Is The Last Test Time Now");
+});
 
-//module("DbSource Model Test Source");
+module("DbSource Model Test Source");
 
-//test("DbSource_RegressionTest_TestFunction_DatabaseNameNotClearedAfterTest", function () {
+test("DbSource_RegressionTest_TestFunction_DatabaseNameNotClearedAfterTest", function () {
 
-//    var model = new DbSourceViewModel();
-//    model.data.databaseName("Test Database");
-//    model.test();
-//    ok(model.data.databaseName() == "Test Database", " Did testing a Db Source clear the database selection");
-//});
+    var model = new DbSourceViewModel();
+    model.data.databaseName("Test Database");
+    model.test();
+    ok(model.data.databaseName() == "Test Database", " Did testing a Db Source clear the database selection");
+});
