ef1b7082ec0f3f9710338a4b73952e04e3419e58
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-10 09:02:25 +0200

    checking in through new WEBS gate...

---
+++ Dev/Dev2.Server/bin/Debug/Webs/Tests/Services/ServiceViewModelTests.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../../wwwroot/Scripts/_references.js" />
+/// <reference path="../../wwwroot/Scripts/Services/ServiceData.js" />
+/// <reference path="../../wwwroot/Scripts/Services/PluginServiceViewModel.js" />
+/// <reference path="../../wwwroot/Scripts/Sources/PluginSourceViewModel.js" />
+/// <reference path="../../wwwroot/Scripts/Services/WebServiceViewModel.js" />
+/// <reference path="../../wwwroot/Scripts/Sources/WebSourceViewModel.js" />
+/// <reference path="../../wwwroot/Scripts/Dialogs/SaveViewModel.js" />
+
+//module("ServiceViewModelTests", {
+//    setup: function() {
+//        $.mockjaxSettings = {
+//            contentType: "text/json",
+//            dataType: "json"
+//        };
+//    },
+//    teardown: function () {
+//        $.mockjaxClear();
+//    }
+//});
+
+module("Constructor Tests");
+
+test("ConstructorExpectedModelResourceWebServiceOkDisabledOnLoad", function () {
+
+    var model = new WebServiceViewModel();
+    ok(!model.isFormValid(), "WebService", "Ok Enabled By Default");
+});
