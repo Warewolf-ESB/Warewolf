6b8735503f7a8a15941b48b0a8ab3d70df4e46c6
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-18 08:04:51 +0200

    local test settings

---
+++ Dev/TestSettings/CI Environments/Scripts/TestAutomationScripts/Cleanup - With SFTPServer.bat
@@ -0,0 +1,4 @@
+%DeploymentDirectory%\Dev2.Server.exe -x
+taskkill /im "Warewolf Server.exe"
+taskkill /im "SFTPServer.exe"
+exit 0
---
+++ Dev/TestSettings/CI Environments/Scripts/TestAutomationScripts/Cleanup.bat
@@ -0,0 +1,3 @@
+%DeploymentDirectory%\Dev2.Server.exe -x
+taskkill /im "Warewolf Server.exe"
+exit 0
