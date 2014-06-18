ef7bef0da44bd4ef3bf51c288406e624e26204d8
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-18 10:44:49 +0200

    local test settings

--- Dev/TestSettings/CI Environments/Scripts/A Unit Test Assembly.bat
+++ Dev/TestSettings/CI Environments/Scripts/A Unit Test Assembly.bat
@@ -1,6 +1,9 @@
 @echo off
 @echo Assembly Name eg: Dev2.Infrustructer.Tests.dll:
 set /P assembly=
-cd %CD%\..
-"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /testcontainer:"..\..\TestBinaries\%assembly%" /testSettings:"UnitTestsWithCoverage.testsettings"
+cd %CD%\..\..\..
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" AllProjects.sln /Build Test
+copy /Y "%CD%\TestSettings\CI Environments\UnitTestsWithCoverage.testsettings" "%CD%\UnitTestsWithCoverage.testsettings"
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /testcontainer:"%CD%\TestBinaries\%assembly%" /testSettings:"UnitTestsWithCoverage.testsettings"
+DEL /F /Q "%CD%\UnitTestsWithCoverage.testsettings"
 pause
--- Dev/TestSettings/CI Environments/Scripts/All Integration Tests.bat
+++ Dev/TestSettings/CI Environments/Scripts/All Integration Tests.bat
@@ -1,4 +1,7 @@
 @echo off
-cd %CD%\..
-"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /testcontainer:"..\..\TestBinaries\Dev2.IntegrationTests.dll" /testSettings:"Integration.testsettings"
+cd %CD%\..\..\..
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" AllProjects.sln /Build Test
+copy /Y "%CD%\TestSettings\CI Environments\Integration.testsettings" "%CD%\Integration.testsettings"
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\MSTest.exe" /testcontainer:"%CD%\TestBinaries\Dev2.IntegrationTest.dll" /testSettings:"Integration.testsettings"
+DEL /F /Q "%CD%\Integration.testsettings"
 pause
