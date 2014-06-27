4b13a34d80e3e43645262987e60c6ff514519519
Ashley Lewis <DEV2\Ashley.lewis>
2014-06-27 11:38:48 +0200

    Ignoring all remote tests pending work item 12095. They are interupting coded UI test runs since the explorer upgrade.

--- Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/RemoteServerUITests.cs
+++ Dev/Dev2.UI.Tests/Dev2.Studio.UI.Tests/RemoteServerUITests.cs
@@ -14,6 +14,7 @@ namespace Dev2.Studio.UI.Tests
     /// These are UI tests based on using a remote server
     /// </summary>
     [CodedUITest]
+    [Ignore]//Ashley: Pending work item 12095
     public class RemoteServerUiTests : UIMapBase
     {
         #region Const
