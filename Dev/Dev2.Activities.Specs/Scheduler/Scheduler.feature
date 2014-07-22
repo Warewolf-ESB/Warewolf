0b59bcd41153f41f795ba8912d04155d5ab0ea0b
Leon Rajindrapersadh <DEV2\Leon.Rajindrapersadh>
2014-07-22 13:56:54 +0200

    null

---
+++ Dev/Dev2.Activities.Specs/Scheduler/Scheduler.feature
@@ -0,0 +1,19 @@
+﻿Feature: Scheduler
+	In order to schedule workflows
+	As a Warewolf user
+	I want to setup schedules
+	
+
+#Scenario: Schedule with history
+#      Given I have a schedule "ScheduleWithHistory"
+#	  And "ScheduleWithHistory" executes an Workflow "Assign" 
+#	  And "ScheduleWithHistory" has a username of "UserName" and a Password of "Password"
+#	  And "ScheduleWithHistory" has a Schedule of
+#	  | ScheduleType  | Interval | StartDate  | StartTime | Recurs | RecursInterval | Delay | DelayInterval | Repeat | RepeatInterval | ExpireDate | ExpireTime |
+#	  | On a schedule | "Daily"  | 2014/01/01 | 15:40:44  | 1      | day            | 1     | hour          | 1      | hour           | 2014/01/02 | 15:40:15   |
+#	  When the "ScheduleWithHistory" is executed
+#	  Then the schedule status is "Success"
+#	  And "ScheduleWithHistory" has "1" row of history	   
+#	  And the history debug output for 'ScheduleWithHistory' for row "1" is 
+#		| # |                     |
+#		| 1 | [[rec(2).a]] = Test |
---
+++ Dev/Dev2.Activities.Specs/Scheduler/UnknownSteps.cs
@@ -0,0 +1,58 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Dev2.Activities.Specs
+{
+    [Binding]
+    public class UnknownSteps
+    {
+        [Given(@"I have a schedule ""(.*)""")]
+        public void GivenIHaveASchedule(string scheduleName)
+        {
+            ScenarioContext.Current.Add("ScheduleName",scheduleName);
+        }
+        
+        [Given(@"""(.*)"" executes an Workflow ""(.*)""")]
+        public void GivenExecutesAnWorkflow(string scheduleName, string workFlow)
+        {
+            ScenarioContext.Current.Add("WorkFlow", workFlow);
+        }
+        
+        [Given(@"""(.*)"" has a username of ""(.*)"" and a Password of ""(.*)""")]
+        public void GivenHasAUsernameOfAndAPasswordOf(string scheduleName, string userName, string password)
+        {
+            ScenarioContext.Current.Add("WorkFlow", scheduleName);
+            ScenarioContext.Current.Add("UserName", userName);
+            ScenarioContext.Current.Add("Password", password);
+        }
+        
+        [Given(@"""(.*)"" has a Schedule of")]
+        public void GivenHasAScheduleOf(string scheduleName, Table table)
+        {
+            //SchedulerViewModel scheduler = new SchedulerViewModel(); 
+        }
+        
+        [When(@"the ""(.*)"" is executed")]
+        public void WhenTheIsExecuted(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the schedule status is ""(.*)""")]
+        public void ThenTheScheduleStatusIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"""(.*)"" has ""(.*)"" row of history")]
+        public void ThenHasRowOfHistory(string p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the history debug output for '(.*)' for row ""(.*)"" is")]
+        public void ThenTheHistoryDebugOutputForForRowIs(string p0, int p1, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
