a61062aceeb389038b673445a6f013ae32810c0c
Ashley Lewis <DEV2\Ashley.lewis>
2014-08-20 09:20:02 +0200

    Automation IDs for Multi assign rows

--- Dev/Dev2.Activities.Designers/Designers2/MultiAssign/Large.xaml.cs
+++ Dev/Dev2.Activities.Designers/Designers2/MultiAssign/Large.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Dev2.Activities.Designers2.MultiAssign
 {
@@ -14,6 +15,11 @@ namespace Dev2.Activities.Designers2.MultiAssign
         protected override IInputElement GetInitialFocusElement()
         {
             return DataGrid.GetFocusElement(0);
+        }
+
+        private void DataGrid_LoadingRow(System.Object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Tag = e.Row.GetIndex();
         }
     }
 }
