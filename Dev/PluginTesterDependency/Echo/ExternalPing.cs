f071a77a7e9879f7f8f10b611692167eb93d9b3c
Ashley Lewis <DEV2\Ashley.lewis>
2014-08-29 13:38:23 +0200

    plugin tester solution dependency patch***NO_CI***

---
+++ Dev/PluginTesterDependency/Echo/ExternalPing.cs
@@ -0,0 +1,16 @@
+﻿namespace PluginTesterDependency.Echo
+{
+    public class ExternalPing
+    {
+
+        public string Pong(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                return "Pong [ " + text + " ]";    
+            }
+
+            return "Pong [ Null or Empty String ]";
+        }
+    }
+}
