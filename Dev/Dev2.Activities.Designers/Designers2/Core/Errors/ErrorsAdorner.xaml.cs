18171ad0383184baf3d5425a7b0b6caf4d530ac7
Ashley Lewis <DEV2\Ashley.lewis>
2014-09-02 09:01:56 +0200

    Auto ids for errors adorner

--- Dev/Dev2.Activities.Designers/Designers2/Core/Errors/ErrorsAdorner.xaml.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Core/Errors/ErrorsAdorner.xaml.cs
@@ -1,13 +1,31 @@
 ﻿using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
 
 namespace Dev2.Activities.Designers2.Core.Errors
-{  
+{
     public partial class ErrorsAdorner
     {
+        int ErrorsCounter = 0;
+
         public ErrorsAdorner(FrameworkElement adornedElement)
             : base(adornedElement)
         {
             InitializeComponent();
+        }
+
+        void ErrorTextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            var textBlock = sender as TextBlock;
+            if(textBlock != null)
+            {
+                textBlock.SetValue(AutomationProperties.AutomationIdProperty, "UI_Error" + ErrorsCounter++ + "_AutoID");
+            }
+        }
+
+        void ErrorsGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ErrorsCounter = 0;
         }
     }
 }
