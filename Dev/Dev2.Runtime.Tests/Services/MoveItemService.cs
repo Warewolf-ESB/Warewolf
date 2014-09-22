32da0880c1c0b3fe932c04d08913e7d7363994b6
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2014-09-22 11:56:59 +0200

    drag drop missing file
    Related Work Items: #11830

---
+++ Dev/Dev2.Runtime.Tests/Services/MoveItemService.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Dev2.Common.Interfaces.Data;
+using Dev2.Common.Interfaces.Explorer;
+using Dev2.Common.Interfaces.Hosting;
+using Dev2.Common.Interfaces.Infrastructure;
+using Dev2.Common.Interfaces.Security;
+using Dev2.Communication;
+using Dev2.Explorer;
+using Dev2.Runtime.ESB.Management.Services;
+using Dev2.Runtime.Hosting;
+using Dev2.Workspaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Dev2.Tests.Runtime.Services
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    // ReSharper disable InconsistentNaming
+    public class MoveItemsServiceTest
+    {
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItem_HandlesType")]
+        public void MoveItem_HandlesType_ExpectName()
+        {
+            //------------Setup for test--------------------------
+            var MoveItemService = new MoveItemService();
+
+
+            //------------Execute Test---------------------------
+
+            //------------Assert Results-------------------------
+            Assert.AreEqual("MoveItemService", MoveItemService.HandlesType());
+        }
+
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItemService_Execute")]
+        public void MoveItem_Execute_NullValues_ErrorResult()
+        {
+            //------------Setup for test--------------------------
+            var MoveItemService = new MoveItemService();
+            var serializer = new Dev2JsonSerializer();
+            //------------Execute Test---------------------------
+            StringBuilder jsonResult = MoveItemService.Execute(null, null);
+            IExplorerRepositoryResult result = serializer.Deserialize<IExplorerRepositoryResult>(jsonResult);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(ExecStatus.Fail, result.Status);
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItemService_Execute")]
+        public void MoveItem_Execute_ItemToRenameNotInValues_ErrorResult()
+        {
+            //------------Setup for test--------------------------
+            var values = new Dictionary<string, StringBuilder> { { "item", new StringBuilder() } };
+            var MoveItemService = new MoveItemService();
+            var serializer = new Dev2JsonSerializer();
+            //------------Execute Test---------------------------
+            StringBuilder jsonResult = MoveItemService.Execute(values, null);
+            IExplorerRepositoryResult result = serializer.Deserialize<IExplorerRepositoryResult>(jsonResult);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(ExecStatus.Fail, result.Status);
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItemService_Execute")]
+        public void MoveItem_Execute_NewNameNotInDictionary_ErrorResult()
+        {
+            //------------Setup for test--------------------------
+            var values = new Dictionary<string, StringBuilder> { { "itemToRename", new StringBuilder("This is not deserializable to ServerExplorerItem") } };
+            var MoveItemService = new MoveItemService();
+            var serializer = new Dev2JsonSerializer();
+            //------------Execute Test---------------------------
+            StringBuilder jsonResult = MoveItemService.Execute(values, null);
+            IExplorerRepositoryResult result = serializer.Deserialize<IExplorerRepositoryResult>(jsonResult);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(ExecStatus.Fail, result.Status);
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItemervice_Execute")]
+        public void MoveItem_Execute_ItemToRenameNotServerExplorerItem_ErrorResult()
+        {
+            //------------Setup for test--------------------------
+            var values = new Dictionary<string, StringBuilder> { { "itemToRename", new StringBuilder("This is not deserializable to ServerExplorerItem") }, { "newName", new StringBuilder("new name") } };
+            var MoveItemService = new MoveItemService();
+            var serializer = new Dev2JsonSerializer();
+            //------------Execute Test---------------------------
+            StringBuilder jsonResult = MoveItemService.Execute(values, null);
+            IExplorerRepositoryResult result = serializer.Deserialize<IExplorerRepositoryResult>(jsonResult);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(ExecStatus.Fail, result.Status);
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItem_Execute")]
+        public void MoveItem_Execute_ExpectRename()
+        {
+            //------------Setup for test--------------------------
+            var MoveItemService = new MoveItemService();
+
+            ServerExplorerItem item = new ServerExplorerItem("a", Guid.NewGuid(), ResourceType.Folder, null, Permissions.DeployFrom, "");
+            var repo = new Mock<IExplorerServerResourceRepository>();
+            var ws = new Mock<IWorkspace>();
+            repo.Setup(a => a.MoveItem(It.IsAny<IExplorerItem>(), It.IsAny<string>(), It.IsAny<Guid>())).Returns(new ExplorerRepositoryResult(ExecStatus.Success, "")).Verifiable();
+
+            var serializer = new Dev2JsonSerializer();
+            var inputs = new Dictionary<string, StringBuilder>
+                {
+                    {
+                        "itemToMove", serializer.SerializeToBuilder(item)
+                    },
+                    {
+                        "newPath", new StringBuilder("bob")
+                    }
+                };
+            ws.Setup(a => a.ID).Returns(Guid.Empty);
+            MoveItemService.ServerExplorerRepo = repo.Object;
+            //------------Execute Test---------------------------
+            MoveItemService.Execute(inputs, ws.Object);
+            //------------Assert Results-------------------------
+            repo.Verify(a => a.MoveItem(It.IsAny<IExplorerItem>(), It.IsAny<string>(), It.IsAny<Guid>()));
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("MoveItem_HandlesType")]
+        public void MoveItem_CreateServiceEntry_ExpectProperlyFormedDynamicService()
+        {
+            //------------Setup for test--------------------------
+            var MoveItem = new MoveItemService();
+
+
+            //------------Execute Test---------------------------
+            var a = MoveItem.CreateServiceEntry();
+            //------------Assert Results-------------------------
+            var b = a.DataListSpecification;
+            Assert.AreEqual("<DataList><itemToMove ColumnIODirection=\"Input\"/><newPath ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>", b);
+        }
+    }
+}
