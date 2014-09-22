0f91b77cc101aeab126534244612ae5d12c47a9e
Ashley Lewis <Ashley.lewis@dev2.co.za>
2014-09-22 09:35:41 +0200

    test settings and scripts for ci updated to always use "Server" folder in deploy dir instead of "ServerbinDebug"

--- Dev/TestScripts/UI/Startup.bat
+++ Dev/TestScripts/UI/Startup.bat
@@ -27,29 +27,14 @@ taskkill /im "Warewolf Studio.exe"
 REM  Wait 10 seconds ;)
 ping -n 10 127.0.0.1 > nul
 
-IF EXIST %DeploymentDirectory% GOTO RunDeployed
-
-:RunLocal
-REM ** Start Warewolf server from local binaries built in debug config **
-START "..\..\Dev2.Server\bin\Debug\Warewolf Server.exe" /D "..\..\Dev2.Server\bin\Debug" "Warewolf Server.exe"
-
-REM  Wait 10 seconds ;)
-ping -n 10 127.0.0.1 > nul
-
-REM ** Start Warewolf studio from local binaries built in debug config **
-START "..\..\Dev2.Studio\bin\Debug\Warewolf Studio.exe" /D "..\..\Dev2.Studio\bin\Debug" "Warewolf Studio.exe"
-GOTO exit
-
-:RunDeployed
 REM ** Start Warewolf server from deployed binaries **
-START "%DeploymentDirectory%\ServerbinDebug\Warewolf Server.exe" /D "%DeploymentDirectory%\ServerbinDebug" "Warewolf Server.exe"
+START "%DeploymentDirectory%\Server\Warewolf Server.exe" /D "%DeploymentDirectory%\Server" "Warewolf Server.exe"
 
 REM  Wait 10 seconds ;)
 ping -n 10 127.0.0.1 > nul
 
 REM ** Start Warewolf studio from deployed binaries **
-START "%DeploymentDirectory%\StudiobinDebug\Warewolf Studio.exe" /D "%DeploymentDirectory%\StudiobinDebug" "Warewolf Studio.exe"
-:exit
+START "%DeploymentDirectory%\Studio\Warewolf Studio.exe" /D "%DeploymentDirectory%\Studio" "Warewolf Studio.exe"
 
 REM  Wait 30 seconds ;)
 ping -n 30 127.0.0.1 > nul
