dde968ee0d2e0e4faf449b2b1fa70751213d9272
Robin Grant van den Heever <robin.vandenHeever@dev2.co.za>
2014-11-05 15:04:43 +0200

    FIle Name dogfooding issue fixed

--- Dev/Dev2.Activities.Designers/Designers2/Core/FileActivityDesignerViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Core/FileActivityDesignerViewModel.cs
@@ -55,7 +55,7 @@ namespace Dev2.Activities.Designers2.Core
             get { return (bool)GetValue(IsInputPathFocusedProperty); }
             set
             {
-                if(IsInputPathFocusedProperty != null)
+                if (IsInputPathFocusedProperty != null)
                 {
                     SetValue(dp: IsInputPathFocusedProperty, value: value);
                 }
@@ -70,7 +70,7 @@ namespace Dev2.Activities.Designers2.Core
             get { return (bool)GetValue(IsOutputPathFocusedProperty); }
             set
             {
-                if(IsOutputPathFocusedProperty != null)
+                if (IsOutputPathFocusedProperty != null)
                 {
                     SetValue(dp: IsOutputPathFocusedProperty, value: value);
                 }
@@ -115,7 +115,7 @@ namespace Dev2.Activities.Designers2.Core
             IsValidExpressionRule isValidExpressionRule = new IsValidExpressionRule(() => path, DataListSingleton.ActiveDataList.Resource.DataList);
             fileActivityRuleSet.Add(isValidExpressionRule);
             errors.AddRange(fileActivityRuleSet.ValidateRules(label, onError));
-             
+
 
             string pathValue;
             path.TryParseVariables(out pathValue, onError, variableValue: ValidUriSchemes[0] + "://temp");
@@ -128,26 +128,17 @@ namespace Dev2.Activities.Designers2.Core
                     DoError = onError
                 };
 
+                IsValidFileNameRule isValidFileNameRule = new IsValidFileNameRule(() => path)
+                {
+                    LabelText = label,
+                    DoError = onError
+                };
+
                 fileActivityRuleSet.Add(isStringEmptyOrWhiteSpaceRuleUserName);
+                fileActivityRuleSet.Add(isValidExpressionRule);
+                
                 errors.AddRange(fileActivityRuleSet.ValidateRules(label, onError));
 
-                var pathBlankError = isStringEmptyOrWhiteSpaceRuleUserName.Check();
-                if (pathBlankError != null)
-                {
-                    errors.Add(new ActionableErrorInfo(onError) { ErrorType = ErrorType.Critical, Message = label + " must have a value" });
-                }
-                else
-                {
-                        Uri uriResult;
-                        var isValid = Uri.TryCreate(pathValue, UriKind.Absolute, out uriResult)
-                            ? ValidUriSchemes.Contains(uriResult.Scheme)
-                            : File.Exists(pathValue);
-
-                        if (!isValid)
-                        {
-                            errors.Add(new ActionableErrorInfo(onError) { ErrorType = ErrorType.Critical, Message = "Please supply a valid " + label });
-                        }
-                }
             }
 
             UpdateErrors(errors);
