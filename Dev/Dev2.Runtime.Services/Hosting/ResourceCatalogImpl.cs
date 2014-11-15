1fe0aea452f1db1d1259aec0f49507d519c0d3f4
ChasDigal <bbuchan@premierhq.com>
2014-11-15 15:03:19 +0200

    For code cleanup

---
+++ Dev/Dev2.Runtime.Services/Hosting/ResourceCatalogImpl.cs
@@ -0,0 +1,14 @@
+namespace Dev2.Runtime.Hosting
+{
+    partial class ResourceCatalogImpl
+    {
+    }
+
+    partial class ResourceCatalogImpl : ResourceCatalog
+    {
+    }
+
+    partial class ResourceCatalogImpl
+    {
+    }
+}
