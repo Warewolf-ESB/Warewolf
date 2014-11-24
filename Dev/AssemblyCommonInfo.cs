f6fb21e018f15a050a0b4d4a5829147796e3c6f8
Ashley Lewis <Ashley.lewis@dev2.co.za>
2014-11-24 08:35:14 +0200

    Let C# version assemblies automatically

--- Dev/AssemblyCommonInfo.cs
+++ Dev/AssemblyCommonInfo.cs
@@ -31,5 +31,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.4.20.1")]
-[assembly: AssemblyFileVersion("0.4.20.1")]
+[assembly: AssemblyVersion("0.4.*")]
