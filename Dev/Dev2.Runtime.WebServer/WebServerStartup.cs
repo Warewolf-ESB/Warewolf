15d40c06a8e915a803c9156e142575101a99832c
Hagashen Naidu <Hagashen.Naidu@dev2.co.za>
2014-11-26 13:42:34 +0200

    worked on authentication selection

--- Dev/Dev2.Runtime.WebServer/WebServerStartup.cs
+++ Dev/Dev2.Runtime.WebServer/WebServerStartup.cs
@@ -9,13 +9,15 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Hosting;
+using Microsoft.Owin.Security;
 using Owin;
 
 namespace Dev2.Runtime.WebServer
@@ -52,9 +54,11 @@ namespace Dev2.Runtime.WebServer
         public void Configuration(IAppBuilder app)
         {
             var listener = (HttpListener)app.Properties[typeof(HttpListener).FullName];
-            listener.AuthenticationSchemes = AuthenticationSchemes.Ntlm;  // Enable NTLM auth
+           // listener.AuthenticationSchemes = AuthenticationSchemes.Basic | AuthenticationSchemes.Negotiate;  // Enable NTLM auth
+            //listener.AuthenticationSchemes = AuthenticationSchemes.Basic;  // Enable NTLM auth
             listener.IgnoreWriteExceptions = true;  // ignore errors written to disconnected clients.
-
+            listener.AuthenticationSchemeSelectorDelegate+=AuthenticationSchemeSelectorDelegate;
+            app.Use(typeof(AuthenticationMiddleware));
             // Enable cross-domain calls
             app.UseCors(CorsOptions.AllowAll);
 
@@ -74,5 +78,93 @@ namespace Dev2.Runtime.WebServer
             app.UseWebApi(config);
         }
 
+        AuthenticationSchemes AuthenticationSchemeSelectorDelegate(HttpListenerRequest httpRequest)
+        {
+            var header = httpRequest.Headers["Authorization"];
+            if (!String.IsNullOrEmpty(header))
+            {
+                return AuthenticationSchemes.Negotiate;
+            }
+            return AuthenticationSchemes.Anonymous;
+        }
+    }
+
+    public class AuthenticationMiddleware : OwinMiddleware
+    {
+        public AuthenticationMiddleware(OwinMiddleware next) :
+            base(next) { }
+//
+//        public async Task Invoke(OwinRequest request,
+//                                                OwinResponse response)
+//        {
+//
+////            request.OnSendingHeaders(state =>
+////            {
+////                var resp = (OwinResponse)state;
+////
+////                if (resp.StatusCode == 401)
+////                    resp.SetHeader("WWW-Authenticate", "Basic");
+////            }, response);
+//
+//            var header = request.Headers["Authorization"];
+//
+//            if (!String.IsNullOrWhiteSpace(header))
+//            {
+//                var authHeader = System.Net.Http.Headers
+//                                   .AuthenticationHeaderValue.Parse(header);
+//
+//                if ("Basic".Equals(authHeader.Scheme,
+//                                         StringComparison.OrdinalIgnoreCase))
+//                {
+//                    string parameter = Encoding.UTF8.GetString(
+//                                          Convert.FromBase64String(
+//                                                authHeader.Parameter));
+//                    var parts = parameter.Split(':');
+//
+//                    string userName = parts[0];
+//                    string password = parts[1];
+//
+//                    if (userName == password) // Just a dumb check
+//                    {
+//                        var claims = new[]
+//                    {
+//                        new Claim(ClaimTypes.Name, "Badri")
+//                    };
+//                        var identity = new ClaimsIdentity(claims, "Basic");
+//
+//                        request.User = new ClaimsPrincipal(identity);
+//                    }
+//                }
+//            }
+//
+//            await Next.Invoke(request, response);
+//        }
+
+        #region Overrides of OwinMiddleware
+
+        /// <summary>
+        /// Process an individual request.
+        /// </summary>
+        /// <param name="context"/>
+        /// <returns/>
+        public override Task Invoke(IOwinContext context)
+        {
+            if (context.Request.User != null)
+            {
+               return Next.Invoke(context);
+            }
+            
+            context.Response.OnSendingHeaders(state =>
+            {
+                var resp = (OwinResponse)state;
+
+                if (resp.StatusCode == 401)
+                    resp.Headers["WWW-Authenticate"] = "Negotiate";
+            }, context.Response);
+
+            return Next.Invoke(context);
+        }
+
+        #endregion
     }
 }
