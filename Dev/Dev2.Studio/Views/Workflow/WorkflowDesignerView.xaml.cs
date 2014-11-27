2e28ad52c7c79680d4c8e22d61a6387849a02dde
Robin Grant van den Heever <robin.vandenHeever@dev2.co.za>
2014-11-27 11:51:12 +0200

    #Wolf-322 Check in for Hugs

--- Dev/Dev2.Studio/Views/Workflow/WorkflowDesignerView.xaml.cs
+++ Dev/Dev2.Studio/Views/Workflow/WorkflowDesignerView.xaml.cs
@@ -1,4 +1,3 @@
-
 /*
 *  Warewolf - The Easy Service Bus
 *  Copyright 2014 by Warewolf Ltd <alpha@warewolf.io>
@@ -8,12 +7,11 @@
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
-
+using System.Reactive;
 using System.Windows;
 using System.Windows.Input;
 using Dev2.Interfaces;
 using Dev2.Studio.ViewModels.Workflow;
-
 // ReSharper disable CheckNamespace
 namespace Dev2.Studio.Views.Workflow
 {
@@ -24,7 +22,6 @@ namespace Dev2.Studio.Views.Workflow
     {
         readonly DragDropHelpers _dragDropHelpers;
         //IDisposable _subscription;
-
         public WorkflowDesignerView()
         {
             InitializeComponent();
@@ -32,33 +29,52 @@ namespace Dev2.Studio.Views.Workflow
             PreviewDragOver += DropPointOnDragEnter;
             PreviewMouseDown += WorkflowDesignerView_PreviewMouseDown;
             _dragDropHelpers = new DragDropHelpers(this);
-
+            //            var pattern = Observable.FromEventPattern<KeyEventArgs>(this,"KeyUp");
+            //            pattern.Throttle(TimeSpan.FromMilliseconds(50000))
+            //                .ObserveOn(SynchronizationContext.Current);
+            //
+            //            pattern.Subscribe(PerformOnDispatcher);
         }
-
-
+        void PerformOnDispatcher(EventPattern<KeyEventArgs> eventPattern)
+        {
+            //Dispatcher.BeginInvoke(DispatcherPriority.Background,new Action(()=>PerfromAction(eventPattern.EventArgs)));
+        }
         void WorkflowDesignerView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             var vm = (DataContext as WorkflowDesignerViewModel);
-            if(vm != null)
+            if (vm != null)
             {
                 CustomContainer.Get<IMainViewModel>().AddWorkSurfaceContext(vm.ResourceModel);
             }
         }
-
         //a return from here without settings handled to true and DragDropEffects.None implies that the item drop is allowed
         void DropPointOnDragEnter(object sender, DragEventArgs e)
         {
             var dataObject = e.Data;
-            if(_dragDropHelpers.PreventDrop(dataObject))
+            if (_dragDropHelpers.PreventDrop(dataObject))
             {
                 e.Effects = DragDropEffects.None;
                 e.Handled = true;
             }
         }
+        void UIElement_OnKeyUp(object sender, KeyEventArgs e)
+        {       
+            //PerfromAction(e);
+        }
+        void PerfromAction(KeyEventArgs e)
+        {
 
-       
+            if (e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Up || e.Key == Key.Down || e.Key == Key.PageDown || e.Key == Key.PageUp || e.Key == Key.Left || e.Key == Key.Right)
+            {
+                return;
+            }
+            var workflowDesignerViewModel = DataContext as WorkflowDesignerViewModel;
+            if (workflowDesignerViewModel != null)
+            {
+                workflowDesignerViewModel.AddMissingWithNoPopUpAndFindUnusedDataListItems();
+            }
+        }
     }
-
     public interface IWorkflowDesignerView
     {
         object DataContext { get; set; }
