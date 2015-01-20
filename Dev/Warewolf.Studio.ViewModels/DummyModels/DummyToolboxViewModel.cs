5be6b7f74f64e434e297189223c8f92957c719b8
Robin Grant van den Heever <robin.vandenheever@dev2.co.za>
2015-01-20 07:22:15 +0200

    Toolbox template item added

--- Dev/Warewolf.Studio.ViewModels/DummyModels/DummyToolboxViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/DummyModels/DummyToolboxViewModel.cs
@@ -30,11 +30,16 @@ namespace Warewolf.Studio.ViewModels.DummyModels
         void SetupTools()
         {
             Tools = new ObservableCollection<IToolDescriptorViewModel>();
-            ResourceDictionary rd = new ResourceDictionary { Source = new Uri("/Warewolf.Studio.ViewModels;component/DummyModels/Icons.xaml", UriKind.RelativeOrAbsolute) };
-            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "name", (DrawingImage)rd["TaskScheduler-32"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "tools", ToolType.Native), true));
-            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "name", (DrawingImage)rd["ToolDataSplit-32"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "tools", ToolType.Native), true));
-            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "name", (DrawingImage)rd["ToolSystemInformation-32"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "tools", ToolType.Native), true));
-        }
+            ResourceDictionary rd = new ResourceDictionary { Source = new Uri("/Warewolf.Studio.Themes.Luna;component/Theme.xaml", UriKind.RelativeOrAbsolute) };
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Copy Records", (DrawingImage)rd["RecordSet-CopyRecord"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Count Records", (DrawingImage)rd["RecordSet-CountRecords"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Delete", (DrawingImage)rd["RecordSet-Delete"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Find Records", (DrawingImage)rd["RecordSet-FindRecords"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Length", (DrawingImage)rd["RecordSet-Length"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Sort Records", (DrawingImage)rd["RecordSet-SortRecords"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "SQL Bulk Insert", (DrawingImage)rd["RecordSet-SQLBulkInsert"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+            Tools.Add(new ToolDescriptorViewModel(new ToolDescriptor(Guid.NewGuid(), typeof(string), typeof(string), typeof(Vector), "Unique Records", (DrawingImage)rd["RecordSet-UniqueRecords"], new Version(1, 0, 1), new Mock<IHelpDescriptor>().Object, true, "Recordset", ToolType.Native), true));
+             }
 
         #endregion
     }
