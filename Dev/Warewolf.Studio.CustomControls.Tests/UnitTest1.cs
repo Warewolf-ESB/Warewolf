0e850d997c4e0980eed0409069cd8e57925eb6ee
Robin Grant van den Heever <robin.vandenheever@dev2.co.za>
2015-01-21 15:14:27 +0200

    Textbox Watermark Code added

---
+++ Dev/Warewolf.Studio.CustomControls.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.Studio.CustomControls.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Dev/Warewolf.Studio.CustomControls/WatermarkTextBox.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Warewolf.Studio.CustomControls
+{
+    public class TextBoxHelper
+    {
+        static bool _isInistialised = false;
+
+        public static string GetWatermark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+
+        public static readonly DependencyProperty WatermarkProperty =
+            DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new UIPropertyMetadata(null, WatermarkChanged));
+
+
+
+        public static bool GetShowWatermark(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ShowWatermarkProperty);
+        }
+
+        public static void SetShowWatermark(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ShowWatermarkProperty, value);
+        }
+
+        public static readonly DependencyProperty ShowWatermarkProperty =
+            DependencyProperty.RegisterAttached("ShowWatermark", typeof(bool), typeof(TextBoxHelper), new UIPropertyMetadata(false));
+
+
+
+        static void WatermarkChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var box = obj as TextBox;
+
+            CheckShowWatermark(box);
+
+            if (!_isInistialised)
+            {
+                if(box != null)
+                {
+                    box.TextChanged += BoxTextChanged;
+                    box.Unloaded += BoxUnloaded;
+                }
+                _isInistialised = true;
+            }
+        }
+
+        private static void CheckShowWatermark(TextBox box)
+        {
+            box.SetValue(ShowWatermarkProperty, box.Text == string.Empty);
+        }
+
+        static void BoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var box = sender as TextBox;
+            CheckShowWatermark(box);
+        }
+
+        static void BoxUnloaded(object sender, RoutedEventArgs e)
+        {
+            var box = sender as TextBox;
+            if(box != null)
+            {
+                box.TextChanged -= BoxTextChanged;
+            }
+        }
+    }
+}
