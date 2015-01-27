aa3f69d2bbd163753b433f432201c800af4af509
Hagashen Naidu <Hagashen.Naidu@dev2.co.za>
2015-01-27 07:46:49 +0200

    fixes for some stuff

--- Dev/Warewolf.Studio.Views/Converters/CustomMenuIcons.cs
+++ Dev/Warewolf.Studio.Views/Converters/CustomMenuIcons.cs
@@ -206,6 +206,40 @@ namespace Warewolf.Studio.Views.Converters
 
         #endregion
     }
+    
+    public class ResourceTypeToIconVisibility : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            ResourceType resourceType;
+            if (Enum.TryParse(value.ToString(), out resourceType))
+            {
+                if (resourceType != ResourceType.Folder)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        #endregion
+    }
 
     /// <summary>
     /// Returns the exact length of the requred menu node based on its tree position
