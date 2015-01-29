632000428c1cda4e9dc80d8b81aa88b7a5bd15c9
Robin Grant van den Heever <robin.vandenheever@dev2.co.za>
2015-01-29 05:55:47 +0200

    Testconnection added

--- Dev/Warewolf.Studio.ViewModels/TestConnection.cs
+++ Dev/Warewolf.Studio.ViewModels/TestConnection.cs
@@ -1,17 +1,19 @@
-﻿using Dev2.Common.Interfaces.Communication;
+﻿using System;
+using Dev2.Common.Interfaces.Communication;
 using Dev2.Common.Interfaces.ServerDialogue;
 
 namespace Warewolf.Studio.ViewModels
 {
     class TestConnection : IServerConnectionTest
     {
-       
+
 
         public string Test(IServerSource server)
         {
-            
 
-            return result;
+
+            return String.Empty;
         }
 
-     
+    }
+}
