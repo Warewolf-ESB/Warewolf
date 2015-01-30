f42de08771d01eae3b5696f4c237a9dbe0ed4527
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2015-01-30 07:10:56 +0200

    ServerSource

---
+++ Dev/Dev2.Common.Interfaces/Core/ServerSource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+using Dev2.Common.Interfaces.Runtime.ServiceModel;
+using Dev2.Common.Interfaces.ServerDialogue;
+
+namespace Dev2.Common.Interfaces.Core
+{
+    public class ServerSource : IServerSource
+    {
+        #region Implementation of IServerSource
+
+        /// <summary>
+        /// The server address that we are trying to connect to
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        ///  Windows or user or publlic
+        /// </summary>
+        public AuthenticationType AuthenticationType { get; set; }
+        /// <summary>
+        /// User Name
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Test if connection is successful
+        /// </summary>
+        public ICommand TestCommand { get; set; }
+        /// <summary>
+        /// The message that will be set if the test is either successful or not
+        /// </summary>
+        public string TestMessage { get; set; }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string ResourcePath { get; set; }
+
+        #endregion
+    }
+}
