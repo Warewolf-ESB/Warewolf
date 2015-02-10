57f4018e51f13eac58af8faf6a2c0d71c9958a45
Hagashen Naidu <Hagashen.Naidu@dev2.co.za>
2015-02-10 16:25:29 +0200

    basic setup for being able to do accpt testing on the toolbox

---
+++ Dev/Warewolf.AcceptanceTesting.Explorer/ToolboxSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Dev2.Common.Interfaces.Studio.ViewModels;
+using Dev2.Common.Interfaces.Toolbox;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+using Warewolf.Studio.Core.View_Interfaces;
+using Warewolf.Studio.Views;
+
+namespace Warewolf.AcceptanceTesting.Explorer
+{
+    [Binding]    
+    public class ToolboxSteps
+    {
+        [BeforeFeature("Toolbox")]
+        public static void SetupToolboxDependencies()
+        {
+            var bootstrapper = new UnityBootstrapperForToolboxTesting();
+            bootstrapper.Run();
+            FeatureContext.Current.Add("container", bootstrapper.Container);
+            var view = bootstrapper.Container.Resolve<IToolboxView>();
+            var window = new Window();
+            window.Content = view;
+            var app = Application.Current;
+            app.MainWindow = window;
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
+            {
+                var viewWindow = (ExplorerView)Application.Current.MainWindow.Content;
+                Assert.IsNotNull(viewWindow);
+                Assert.IsNotNull(viewWindow.DataContext);
+                Assert.IsInstanceOfType(viewWindow.DataContext, typeof(IToolboxViewModel));
+                Application.Current.Shutdown();
+            }));
+            
+            Application.Current.Run(Application.Current.MainWindow);
+        }
+
+
+        [BeforeScenario("Toolbox")]
+        public void SetupForExplorer()
+        {
+            var container = FeatureContext.Current.Get<IUnityContainer>("container");
+            var view = container.Resolve<IToolboxView>();
+            ScenarioContext.Current.Add("toolBoxView", view);
+            
+        }
+
+    }
+}
--- Dev/Warewolf.AcceptanceTesting.Explorer/UnityBootstrapperForExplorerTesting.cs
+++ Dev/Warewolf.AcceptanceTesting.Explorer/UnityBootstrapperForExplorerTesting.cs
@@ -1,14 +1,8 @@
-using System.Windows;
 using Dev2.Common.Interfaces;
 using Dev2.Common.Interfaces.Studio.ViewModels;
-using Dev2.Util;
-using Infragistics.Themes;
 using Microsoft.Practices.Prism.PubSubEvents;
-using Microsoft.Practices.Prism.Regions;
-using Microsoft.Practices.Unity;
 using Warewolf.AcceptanceTesting.Core;
 using Warewolf.Studio.Core.View_Interfaces;
-using Warewolf.Studio.Themes.Luna;
 using Warewolf.Studio.ViewModels;
 using Warewolf.Studio.Views;
 
@@ -19,14 +13,7 @@ namespace Warewolf.AcceptanceTesting.Explorer
         protected override void ConfigureContainer()
         {
             base.ConfigureContainer();
-            AppSettings.LocalHost = "http://myserver:3124/";
-            // ReSharper disable ObjectCreationAsStatement
-            new Application();
-            // ReSharper restore ObjectCreationAsStatement
-            ThemeManager.ApplicationTheme = new LunaTheme();
-
-            Container.RegisterType<IServer, ServerForTesting>(new InjectionConstructor());
-            Container.RegisterInstance<IShellViewModel>(new ShellViewModel(Container,Container.Resolve<IRegionManager>(), Container.Resolve<IEventAggregator>()));
+            
             Container.RegisterInstance<IExplorerViewModel>(new ExplorerViewModel(Container.Resolve<IShellViewModel>(),
                 Container.Resolve<IEventAggregator>()));
 
