04c427175a2b40039a24d2957922a86304f43e05
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2015-02-23 07:46:55 +0200

    missing

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/GetComputeNames.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dev2.Common;
+using Dev2.Common.Interfaces.Core.DynamicServices;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.DynamicServices.Objects;
+using Dev2.Runtime.ServiceModel;
+using Dev2.Workspaces;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    public class GetComputeNames : IEsbManagementEndpoint
+    {
+        public StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+        {
+            ExecuteMessage msg = new ExecuteMessage();
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+            try
+            {
+
+                Dev2Logger.Log.Info("Get Computer Names");
+    
+
+
+                var con = new Connections();
+                var result = con.GetNames();
+
+                msg.HasError = false;
+                msg.Message = serializer.SerializeToBuilder(result) ;
+          
+
+            }
+            catch (Exception err)
+            {
+                msg.HasError = true;
+                msg.Message = new StringBuilder(err.Message);
+                Dev2Logger.Log.Error(err);
+
+            }
+
+            return serializer.SerializeToBuilder(msg);
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            DynamicService newDs = new DynamicService { Name = HandlesType(), DataListSpecification = new StringBuilder("<DataList><Roles ColumnIODirection=\"Input\"/><WorkspaceID ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>") };
+            ServiceAction sa = new ServiceAction { Name = HandlesType(), ActionType = enActionType.InvokeManagementDynamicService, SourceMethod = HandlesType() };
+            newDs.Actions.Add(sa);
+
+            return newDs;
+        }
+
+        public string HandlesType()
+        {
+            return "GetComputerNamesService";
+        }
+    }
+}
