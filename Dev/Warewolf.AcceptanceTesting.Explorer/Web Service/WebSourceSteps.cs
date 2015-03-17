96f5a1e930f502d14150def946befa96996210bc
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-03-17 16:36:04 +0200

    added steps for the web source

---
+++ Dev/Warewolf.AcceptanceTesting.Explorer/Web Service/WebSourceSteps.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Warewolf.AcceptanceTesting.Explorer.Web_Service
+{
+    [Binding]
+    public class WebSourceSteps
+    {
+        [Given(@"I open New Web Source")]
+public void GivenIOpenNewWebSource()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I type Address as ""(.*)""")]
+public void GivenITypeAddressAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I type Default Query as ""(.*)""")]
+public void GivenITypeDefaultQueryAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I open ""(.*)"" web source")]
+public void GivenIOpenWebSource(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"Address is ""(.*)""")]
+public void GivenAddressIs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"Default Query is ""(.*)""")]
+public void GivenDefaultQueryIs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"Address as ""(.*)""")]
+public void GivenAddressAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"Default Query as ""(.*)""")]
+public void GivenDefaultQueryAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"Validation message is thrown")]
+public void WhenValidationMessageIsThrown()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"Validation message is Not thrown")]
+public void WhenValidationMessageIsNotThrown()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"Username field is ""(.*)""")]
+public void WhenUsernameFieldIs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"Password field is ""(.*)""")]
+public void WhenPasswordFieldIs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I change Address to ""(.*)""")]
+public void WhenIChangeAddressTo(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I change Default Query to ""/GetCountries\.ashx\?extensio")]
+public void WhenIChangeDefaultQueryToGetCountries_AshxExtensio()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I edit Authentication Type as ""(.*)""")]
+public void WhenIEditAuthenticationTypeAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"Select Authentication Type as ""(.*)""")]
+public void ThenSelectAuthenticationTypeAs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+    }
+}
