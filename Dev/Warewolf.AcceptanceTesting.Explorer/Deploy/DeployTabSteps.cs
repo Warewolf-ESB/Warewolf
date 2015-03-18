e36ec918bed343f7b3895e118ec519d6bf95380a
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-03-18 14:03:47 +0200

    added deploy steps

---
+++ Dev/Warewolf.AcceptanceTesting.Explorer/Deploy/DeployTabSteps.cs
@@ -0,0 +1,188 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Warewolf.AcceptanceTesting.Explorer.Deploy
+{
+    [Binding]
+    public class DeployTabSteps
+    {
+        [Given(@"I have deploy tab opened")]
+        public void GivenIHaveDeployTabOpened()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"selected Source Server is ""(.*)""")]
+        public void GivenSelectedSourceServerIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"""(.*)"" is visible")]
+        public void GivenIsVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"selected Destination Server is ""(.*)""")]
+        public void GivenSelectedDestinationServerIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I select ""(.*)"" from Source Server")]
+        public void GivenISelectFromSourceServer(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I deploy")]
+        public void GivenIDeploy()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"selected ""(.*)"" is ""(.*)""")]
+        public void GivenSelectedIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"selected Destination Server is ""(.*)""")]
+        public void WhenSelectedDestinationServerIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"""(.*)"" is visible")]
+        public void WhenIsVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I select ""(.*)"" from Source Server")]
+        public void WhenISelectFromSourceServer(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I deploy")]
+        public void WhenIDeploy()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click OK on ""(.*)"" popup")]
+        public void WhenIClickOKOnPopup(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click Cancel on ""(.*)"" popup")]
+        public void WhenIClickCancelOnPopup(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click ""(.*)""")]
+        public void WhenIClick(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I Select All Dependecies""")]
+        public void WhenISelectAllDependecies()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I type ""(.*)"" in Destination Server filter")]
+        public void WhenITypeInDestinationServerFilter(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I clear filter on Destination Server")]
+        public void WhenIClearFilterOnDestinationServer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I type ""(.*)"" in Source Server filter")]
+        public void WhenITypeInSourceServerFilter(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I clear filter on Source Server")]
+        public void WhenIClearFilterOnSourceServer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Destination Server edit is ""(.*)""")]
+        public void ThenDestinationServerEditIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)"" is visibe")]
+        public void ThenIsVisibe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the validation message as ""(.*)""")]
+        public void ThenTheValidationMessageAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"deploy is successfull")]
+        public void ThenDeployIsSuccessfull()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)"" is visible on Destination Server")]
+        public void ThenIsVisibleOnDestinationServer(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)"" popup is shown")]
+        public void ThenPopupIsShown(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"deploy is not successfull")]
+        public void ThenDeployIsNotSuccessfull()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)"" from Source Server is ""(.*)""")]
+        public void ThenFromSourceServerIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)"" from Destination Server is ""(.*)""")]
+        public void ThenFromDestinationServerIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I select ""(.*)"" from Destination Server")]
+        public void ThenISelectFromDestinationServer(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I select ""(.*)"" from Source Server")]
+        public void ThenISelectFromSourceServer(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
