390b31e4250bc69f497c042fe10519a3de27a791
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-03-18 09:15:48 +0200

    fixed language of server source specs

--- Dev/Warewolf.AcceptanceTesting.Explorer/Web Service/WebServiceSteps.cs
+++ Dev/Warewolf.AcceptanceTesting.Explorer/Web Service/WebServiceSteps.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TechTalk.SpecFlow;
+﻿using TechTalk.SpecFlow;
 
 namespace Warewolf.AcceptanceTesting.Explorer.Web_Service
 {
@@ -7,99 +6,99 @@ namespace Warewolf.AcceptanceTesting.Explorer.Web_Service
     public class WebServiceSteps
     {
         [Given(@"I click New Web Service Connector")]
-public void GivenIClickNewWebServiceConnector()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIClickNewWebServiceConnector()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I click ""(.*)""")]
-public void GivenIClick(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIClick(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I open ""(.*)"" webservice")]
-public void GivenIOpenWebservice(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIOpenWebservice(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I click ""(.*)"" webservice")]
-public void GivenIClickWebservice(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIClickWebservice(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I select ""(.*)"" as Method")]
-public void WhenISelectAsMethod(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenISelectAsMethod(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I Test Connection")]
-public void WhenITestConnection()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenITestConnection()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I save the webservice")]
-public void WhenISaveTheWebservice()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenISaveTheWebservice()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I type ""(.*)"" as")]
-public void WhenITypeAs(string p0, Table table)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenITypeAs(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"""(.*)"" are")]
-public void WhenAre(string p0, Table table)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenAre(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I change request url to ""(.*)""")]
-public void WhenIChangeRequestUrlTo(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenIChangeRequestUrlTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"Select Request Method & Source is focused")]
-public void ThenSelectRequestMethodSourceIsFocused()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenSelectRequestMethodSourceIsFocused()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"response is loaded")]
-public void ThenResponseIsLoaded()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenResponseIsLoaded()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"""(.*)"" are")]
-public void ThenAre(string p0, Table table)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenAre(string p0, Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"method is selected as ""(.*)""")]
-public void ThenMethodIsSelectedAs(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenMethodIsSelectedAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"""(.*)"" selected as data source")]
-public void ThenSelectedAsDataSource(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenSelectedAsDataSource(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"request URL has ""(.*)"" and ""(.*)""")]
-public void ThenRequestURLHasAnd(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenRequestURLHasAnd(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
     }
 }
