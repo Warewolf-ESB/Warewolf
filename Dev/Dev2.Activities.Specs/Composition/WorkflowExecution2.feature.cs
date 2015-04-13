6763605804b9f8a2a9b4e7456c3aae894ae28e5e
supasam17 <Sameshan.Naidoo@dev2.co.za>
2015-04-13 15:32:37 +0200

    Added Specs for Workflow mappings

--- Dev/Dev2.Activities.Specs/Composition/WorkflowExecution2.feature.cs
+++ Dev/Dev2.Activities.Specs/Composition/WorkflowExecution2.feature.cs
@@ -1,1 +1,6 @@
-﻿#error Generation error: The example sets must provide the same parameters.
+﻿#error Number of cells in the row does not match the number of cells in the header.
+#error Number of cells in the row does not match the number of cells in the header.
+#error Number of cells in the row does not match the number of cells in the header.
+#error Number of cells in the row does not match the number of cells in the header.
+#error Number of cells in the row does not match the number of cells in the header.
+#error Number of cells in the row does not match the number of cells in the header.
