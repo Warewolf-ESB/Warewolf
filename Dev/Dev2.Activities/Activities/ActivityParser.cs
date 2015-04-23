4d4a275bde578335acd221b3bf25365beecd547e
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-04-23 07:00:15 +0200

    duplicate unique id

--- Dev/Dev2.Activities/Activities/ActivityParser.cs
+++ Dev/Dev2.Activities/Activities/ActivityParser.cs
@@ -15,35 +15,33 @@ namespace Dev2.Activities
     {
         #region Implementation of IActivityParser
 
-        public IDev2Activity Parse(DynamicActivity dynamicActivity,Dictionary<string,IDev2Activity> seenActivities )
+        public IDev2Activity Parse(DynamicActivity dynamicActivity, List<IDev2Activity> seenActivities)
         {
             try
             {
+                var chart = WorkflowInspectionServices.GetActivities(dynamicActivity).FirstOrDefault() as Flowchart;
+                if (chart != null)
+                {
+                    if (chart.StartNode == null)
+                    {
+                        return null;
+                    }
+                    var start = (chart.StartNode) as FlowStep;
 
-    
-            var chart = WorkflowInspectionServices.GetActivities(dynamicActivity).FirstOrDefault() as Flowchart;
-            if(chart != null)
-            {
-                if (chart.StartNode == null)
-                {
-                    return null;
+                    if (start != null)
+                    {
+                        var tool = ParseTools(start, seenActivities);
+                        return tool.FirstOrDefault();
+                    }
+                    var flowstart = (chart.StartNode) as FlowSwitch<string>;
+                    if (flowstart != null)
+                    {
+                        return ParseSwitch(flowstart, seenActivities).FirstOrDefault();
+                    }
+                    var flowdec = (chart.StartNode) as FlowDecision;
+                    return ParseDecision(flowdec, seenActivities).FirstOrDefault();
                 }
-                var start = (chart.StartNode) as FlowStep;
-                
-                if(start != null)
-                {
-                    var tool = ParseTools(start,seenActivities);
-                    return tool.FirstOrDefault();
-                }
-                var flowstart = (chart.StartNode) as FlowSwitch<string>;
-                if(flowstart != null)
-                {
-                    return ParseSwitch(flowstart, seenActivities).FirstOrDefault();
-                }
-                var flowdec = (chart.StartNode) as FlowDecision;
-                return ParseDecision(flowdec, seenActivities).FirstOrDefault();
-            }
-            return null;
+                return null;
             }
             catch (InvalidWorkflowException e)
             {
@@ -53,7 +51,7 @@ namespace Dev2.Activities
             }
         }
 
-        IEnumerable<IDev2Activity> ParseTools(FlowNode startNode, Dictionary<string, IDev2Activity> seenActivities)
+        IEnumerable<IDev2Activity> ParseTools(FlowNode startNode, List<IDev2Activity> seenActivities)
         {
           
             if (startNode == null)
@@ -82,22 +80,22 @@ namespace Dev2.Activities
             // ReSharper restore RedundantIfElseBlock
         }
 
-        IEnumerable<IDev2Activity> ParseSwitch(FlowSwitch<string> switchFlowSwitch, Dictionary<string, IDev2Activity> seenActivities)
+        IEnumerable<IDev2Activity> ParseSwitch(FlowSwitch<string> switchFlowSwitch, List<IDev2Activity> seenActivities)
         {
             var activity = switchFlowSwitch.Expression as DsfFlowSwitchActivity;
             if (activity != null)
             {
-                if (seenActivities.ContainsKey(activity.UniqueID))
+                if (seenActivities.Contains(activity))
                 {
-                    return new List<IDev2Activity> { seenActivities[activity.UniqueID] };
+                    return new List<IDev2Activity> { activity };
                 }
 
                 var val = new StringBuilder(Dev2DecisionStack.ExtractModelFromWorkflowPersistedData(activity.ExpressionText));
                  Dev2Switch ds = new Dev2Switch { SwitchVariable = val.ToString() };
                  var swi = new DsfSwitch();
-                 if (!seenActivities.ContainsKey(activity.UniqueID))
+                 if (!seenActivities.Contains(activity))
                  {
-                     seenActivities.Add(activity.UniqueID, swi);
+                     seenActivities.Add(swi);
                  }
                 swi.Switches = switchFlowSwitch.Cases.Select(a => new Tuple<string, IDev2Activity>(a.Key, ParseTools(a.Value, seenActivities).FirstOrDefault())).ToDictionary((a => a.Item1), a => a.Item2);
                 swi.Default = ParseTools(switchFlowSwitch.Default, seenActivities);
@@ -112,15 +110,15 @@ namespace Dev2.Activities
           
         }
 
-        IEnumerable<IDev2Activity> ParseDecision(FlowDecision decision, Dictionary<string, IDev2Activity> seenActivities)
+        IEnumerable<IDev2Activity> ParseDecision(FlowDecision decision, List<IDev2Activity> seenActivities)
         {
             
             var activity = decision.Condition as DsfFlowDecisionActivity;
             if(activity != null)
             {
-                if( seenActivities.ContainsKey( activity.UniqueID))
+                if( seenActivities.Contains(activity))
                 {
-                    return new List<IDev2Activity> { seenActivities[activity.UniqueID]};
+                    return new List<IDev2Activity> { activity};
                 }
                 var rawText = activity.ExpressionText;
                 // ReSharper disable MaximumChainedReferences
@@ -129,9 +127,9 @@ namespace Dev2.Activities
                 var activityText = Dev2DecisionStack.FromVBPersitableModelToJSON(activityTextjson);
                 var decisionStack =  JsonConvert.DeserializeObject<Dev2DecisionStack>(activityText);
                 var dec = new DsfDecision();
-                if (!seenActivities.ContainsKey(activity.UniqueID))
+                if (!seenActivities.Contains(activity))
                 {
-                    seenActivities.Add(activity.UniqueID, dec);
+                    seenActivities.Add( dec);
                 }
                 dec.TrueArm = ParseTools(decision.True, seenActivities);
                 dec.FalseArm = ParseTools(decision.False, seenActivities);
@@ -145,16 +143,16 @@ namespace Dev2.Activities
             throw new Exception("Invalid activity");
         }
 
-        IEnumerable<IDev2Activity> ParseFlowStep(FlowStep startNode, Dictionary<string, IDev2Activity> seenActivities)
+        IEnumerable<IDev2Activity> ParseFlowStep(FlowStep startNode, List<IDev2Activity> seenActivities)
         {
             var action = startNode.Action as IDev2Activity;
             if (action == null)
                 return null;
-            if (seenActivities.ContainsKey(action.UniqueID))
-                return new List<IDev2Activity> { seenActivities[action.UniqueID]};
-            if (!seenActivities.ContainsKey(action.UniqueID))
+            if (seenActivities.Contains(action))
+                return new List<IDev2Activity> { action};
+            if (!seenActivities.Contains(action))
             {
-                seenActivities.Add(action.UniqueID, action);
+                seenActivities.Add(action);
             }
             var tools = ParseTools(startNode.Next, seenActivities);
 
@@ -169,7 +167,7 @@ namespace Dev2.Activities
 
         public IDev2Activity Parse(DynamicActivity dynamicActivity)
         {
-            return Parse(dynamicActivity, new Dictionary<string, IDev2Activity>());
+            return Parse(dynamicActivity, new List<IDev2Activity>());
         }
 
         #endregion
