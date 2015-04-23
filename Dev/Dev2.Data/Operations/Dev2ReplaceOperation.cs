808d1ba6638705d3325e011568b91f4ae12e9361
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-04-23 15:45:29 +0200

    cleaned up the replace operation

--- Dev/Dev2.Data/Operations/Dev2ReplaceOperation.cs
+++ Dev/Dev2.Data/Operations/Dev2ReplaceOperation.cs
@@ -9,16 +9,9 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Dev2.Common.Interfaces.DataList.Contract;
 using Dev2.Data.Interfaces;
-using Dev2.Data.Util;
 using Dev2.DataList.Contract;
-using Dev2.DataList.Contract.Binary_Objects;
-using Dev2.DataList.Contract.Builders;
-using Dev2.DataList.Contract.Value_Objects;
 
 namespace Dev2.Data.Operations
 {
@@ -38,76 +31,7 @@ namespace Dev2.Data.Operations
 
         #region Methods
 
-        public IDev2DataListUpsertPayloadBuilder<string> Replace(Guid exIdx, string expression, string oldString, string newString, bool caseMatch, IDev2DataListUpsertPayloadBuilder<string> payloadBuilder, out ErrorResultTO errors, out int replaceCount, out IBinaryDataListEntry entryToReplaceIn)
-        {
-            replaceCount = 0;
-            oldString = oldString.Replace("\\", "\\\\");
-            // ReSharper disable RedundantAssignment
-            ErrorResultTO allErrors = new ErrorResultTO();
-            errors = new ErrorResultTO();
-            // ReSharper restore RedundantAssignment
-            IDataListCompiler compiler = DataListFactory.CreateDataListCompiler();
-            entryToReplaceIn = compiler.Evaluate(exIdx, enActionType.User, expression, false, out errors);
-            allErrors.MergeErrors(errors);
-
-            if (entryToReplaceIn != null)
-            {
-                RegexOptions regexOptions;
-                if (caseMatch)
-                {
-                    regexOptions = NoneCompiled;
-                }
-                else
-                {
-                    regexOptions = IgnoreCaseCompiled;
-                }                
-                
-                //Massimo Guerrera -  Added the Regex.Escape to escape certain characters - Bug 9937
-                Regex regex = new Regex(Regex.Escape(oldString), regexOptions);                
-
-                IDev2DataListEvaluateIterator itr = Dev2ValueObjectFactory.CreateEvaluateIterator(entryToReplaceIn);
-                while (itr.HasMoreRecords())
-                {
-                    IList<IBinaryDataListItem> rowList = itr.FetchNextRowData();
-                    if(rowList != null)
-                    {
-                        foreach(IBinaryDataListItem binaryDataListItem in rowList)
-                        {
-                            int tmpCount = replaceCount;
-                            string tmpVal = binaryDataListItem.TheValue;
-                            if (tmpVal.Contains("\\") && oldString.Contains("\\"))
-                            {
-                                tmpVal = tmpVal.Replace("\\","\\\\");
-                            }
-
-                            replaceCount += regex.Matches(tmpVal).Count;
-                            if(replaceCount > tmpCount)
-                            {
-                                if(entryToReplaceIn.IsRecordset)
-                                {
-                                    string recsetDisplayValue =
-                                    DataListUtil.CreateRecordsetDisplayValue(entryToReplaceIn.Namespace,binaryDataListItem.FieldName,binaryDataListItem.ItemCollectionIndex.ToString());
-                                    expression = string.Concat("[[", recsetDisplayValue, "]]");
-                                }
-
-                                var replaceValue = regex.Replace(tmpVal, newString);
-                                payloadBuilder.Add(expression, replaceValue);
-                            }
-                        }
-                    }
-                }
-                payloadBuilder.FlushIterationFrame();
-            }
-            else
-            {
-                allErrors.AddError("DataList entry is null.");
-            }
-
-            errors = allErrors;
-
-            return payloadBuilder;
-        }
-
+        
         /// <summary>
         /// Replaces a value in and entry with a new value.
         /// </summary>
@@ -122,15 +46,12 @@ namespace Dev2.Data.Operations
         {
 
             var oldString = stringToSearch;
-            // ReSharper disable RedundantAssignment
             ErrorResultTO allErrors = new ErrorResultTO();
             errors = new ErrorResultTO();
-            // ReSharper restore RedundantAssignment
             allErrors.MergeErrors(errors);
 
             var regexOptions = caseMatch ? NoneCompiled : IgnoreCaseCompiled;
 
-            //Massimo Guerrera -  Added the Regex.Escape to escape certain characters - Bug 9937
             Regex regex = new Regex(Regex.Escape(findString), regexOptions);
             string tmpVal = oldString;
             replaceCount += regex.Matches(tmpVal).Count;
