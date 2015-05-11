5d41f6f8c3ff1570b500a6e968f6038d220eab7d
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-05-11 14:52:15 +0200

    resolved issue with JS errors happening on the new version pop up

--- Dev/Dev2.Studio/Views/Dialogs/WebLatestVersionDialog.xaml.cs
+++ Dev/Dev2.Studio/Views/Dialogs/WebLatestVersionDialog.xaml.cs
@@ -10,7 +10,10 @@
 */
 
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Navigation;
 
 namespace Dev2.Views.Dialogs
@@ -23,13 +26,49 @@ namespace Dev2.Views.Dialogs
         public WebLatestVersionDialog()
         {
             InitializeComponent();
+            Browser.Navigated += Navigated;
             Browser.Navigate(new Uri("http://www.warewolf.io/start_new.php"));
         }
 
-        void wb_LoadCompleted(object sender, NavigationEventArgs e)
+        void Navigated(object sender, NavigationEventArgs e)
         {
-            Browser.Width = Browser.ActualWidth + 32; 
+            SetSilent(Browser,true);
+        }
+
+        void WbLoadCompleted(object sender, NavigationEventArgs e)
+        {
+            Browser.Width = Browser.ActualWidth + 64;
             Browser.Height = Browser.ActualHeight + 32;
         }
+
+
+        public static void SetSilent(WebBrowser browser, bool silent)
+        {
+            if (browser == null)
+                throw new ArgumentNullException("browser");
+
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider sp = browser.Document as IOleServiceProvider;
+            if (sp != null)
+            {
+                Guid iidIWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid iidIWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+                sp.QueryService(ref iidIWebBrowserApp, ref iidIWebBrowser2, out webBrowser);
+                if (webBrowser != null)
+                {
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
+                }
+            }
+        }
     }
+
+    [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IOleServiceProvider
+    {
+        [PreserveSig]
+        int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
+    }
+
 }
