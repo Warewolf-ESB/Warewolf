047c407b2bd1ad0e49edc49c5b88e25abb910a71
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-05-13 15:54:39 +0200

    Started work on CreateJSON designer

---
+++ Dev/Dev2.Activities.Designers/Designers2/CreateJSON/Large.xaml.cs
@@ -0,0 +1,35 @@
+
+/*
+*  Warewolf - The Easy Service Bus
+*  Copyright 2014 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dev2.Activities.Designers2.MultiAssign
+{
+    public partial class Large
+    {
+        public Large()
+        {
+            InitializeComponent();
+            DataGrid = LargeDataGrid;
+        }
+
+        protected override IInputElement GetInitialFocusElement()
+        {
+            return DataGrid.GetFocusElement(0);
+        }
+
+        private void DataGrid_LoadingRow(System.Object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Tag = e.Row.GetIndex();
+        }
+    }
+}
