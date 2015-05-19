5ac34ab7e79af97a27e62a78f1088b02081c7d8f
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-05-19 13:24:19 +0200

    work on the backend piece for webservice source

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/TestWebserviceSourceService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dev2.Common;
+using Dev2.Common.Interfaces.Core;
+using Dev2.Common.Interfaces.Core.DynamicServices;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.DynamicServices.Objects;
+using Dev2.Runtime.ServiceModel;
+using Dev2.Runtime.ServiceModel.Data;
+using Dev2.Workspaces;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    /// <summary>
+    /// Adds a resource
+    /// </summary>
+    public class TestWebserviceSourceService : IEsbManagementEndpoint
+    {
+        public StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+        {
+            ExecuteMessage msg = new ExecuteMessage();
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+            try
+            {
+
+                Dev2Logger.Log.Info("Test WebserviceSource");
+                StringBuilder resourceDefinition;
+
+                values.TryGetValue("WebserviceSource", out resourceDefinition);
+
+                var src = serializer.Deserialize<WebServiceSourceDefinition>(resourceDefinition);
+                var con = new WebSources();
+                var result = con.Test(new WebSource
+                {
+                    Address = src.HostName,
+                    DefaultQuery = src.DefaultQuery,
+                    AuthenticationType = src.AuthenticationType,
+                    UserName = src.UserName,
+                    Password = src.Password
+                });
+                
+
+                msg.HasError = false;
+                msg.Message = new StringBuilder(result.IsValid ? serializer.Serialize( result.Result) : result.ErrorMessage);
+                msg.HasError = !result.IsValid;
+
+            }
+            catch (Exception err)
+            {
+                msg.HasError = true;
+                msg.Message = new StringBuilder(err.Message);
+                Dev2Logger.Log.Error(err);
+
+            }
+
+            return serializer.SerializeToBuilder(msg);
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            DynamicService newDs = new DynamicService { Name = HandlesType(), DataListSpecification = new StringBuilder("<DataList><Roles ColumnIODirection=\"Input\"/><WebserviceSource ColumnIODirection=\"Input\"/><WorkspaceID ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>") };
+            ServiceAction sa = new ServiceAction { Name = HandlesType(), ActionType = enActionType.InvokeManagementDynamicService, SourceMethod = HandlesType() };
+            newDs.Actions.Add(sa);
+
+            return newDs;
+        }
+
+        public string HandlesType()
+        {
+            return "TestWebserviceSource";
+        }
+    }
+}
