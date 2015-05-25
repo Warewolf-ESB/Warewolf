ed78bc8bd22f80c3e8976eff24b08330472fa94f
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2015-05-25 16:13:23 +0200

    files in commit

---
+++ Dev/Dev2.Common.Interfaces/WebService/IWebServiceService.cs
@@ -0,0 +1,13 @@
+﻿namespace Dev2.Common.Interfaces.WebService
+{
+    //public interface IWebService
+    //{
+    //    string Name { get; set; }
+    //    string Path { get; set; }
+    //    IWebServiceSource Source { get; set; }
+    //    IList<IWebserviceInputs> Inputs { get; set; }
+    //    IList<IWebserviceOutputs> OutputMappings { get; set; }
+    //    string QueryString { get; set; }
+    //    Guid Id { get; set; }
+    //}
+}
