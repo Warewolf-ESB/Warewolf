385ef85104bfc555575be594a9b72b4a43de19de
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2015-06-23 13:15:35 +0200

    Fix SplashView expert help button style. remove Button pressed style

--- Dev/Warewolf.Studio.Views/SplashPage.xaml.cs
+++ Dev/Warewolf.Studio.Views/SplashPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using Dev2.Common.Interfaces;
 
 namespace Warewolf.Studio.Views
@@ -9,28 +10,33 @@ namespace Warewolf.Studio.Views
     /// <summary>
     /// Interaction logic for SplashPage.xaml
     /// </summary>
-    public partial class SplashPage : Window, ISplashView
+    public partial class SplashPage : ISplashView
     {
         Grid _blackoutGrid;
         bool _isDialog;
 
         public SplashPage()
         {
-
-            //var content = Application.Current.MainWindow.Content as Grid;
-            //_blackoutGrid = new Grid();
-            //_blackoutGrid.Background = new SolidColorBrush(Colors.Black);
-            //_blackoutGrid.Opacity = 0.75;
-            //if (content != null)
-            //{
-            //    content.Children.Add(_blackoutGrid);
-            //}
+            if (_isDialog)
+            {
+                var content = Application.Current.MainWindow.Content as Grid;
+                _blackoutGrid = new Grid();
+                _blackoutGrid.Background = new SolidColorBrush(Colors.Black);
+                _blackoutGrid.Opacity = 0.75;
+                if (content != null)
+                {
+                    content.Children.Add(_blackoutGrid);
+                }
+            }
             InitializeComponent();
         }
 
         void SplashPage_OnMouseUp(object sender, MouseButtonEventArgs e)
         {
-            //RemoveBlackOutEffect();
+            if (_isDialog)
+            {
+                RemoveBlackOutEffect();
+            }
             CloseSplash();
         }
 
