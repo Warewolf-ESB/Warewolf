c4f2e55bff863979ac0492127ace65ee0ce899bf
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-06-24 14:36:31 +0200

    fixed breaking test

--- Dev/Dev2.Data/ServiceModel/ResourceTypeConverter.cs
+++ Dev/Dev2.Data/ServiceModel/ResourceTypeConverter.cs
@@ -45,7 +45,7 @@ namespace Dev2.Data.ServiceModel
                     break;
 
                 case TypeSource:
-                    resourceTypes = new[] { ResourceType.Server, ResourceType.DbSource, ResourceType.PluginSource, ResourceType.WebSource, ResourceType.EmailSource,ResourceType.OauthSource, ResourceType.ServerSource  };
+                    resourceTypes = new[] { ResourceType.Server, ResourceType.DbSource, ResourceType.PluginSource, ResourceType.WebSource, ResourceType.EmailSource,ResourceType.OauthSource,ResourceType.SharepointServerSource, ResourceType.ServerSource  };
                     break;
 
                 case TypeReservedService:
@@ -90,6 +90,7 @@ namespace Dev2.Data.ServiceModel
                 case ResourceType.EmailSource:
                 case ResourceType.WebSource:
                 case ResourceType.ServerSource:
+                case ResourceType.SharepointServerSource:
                 case ResourceType.OauthSource:
                     return TypeSource;
 
