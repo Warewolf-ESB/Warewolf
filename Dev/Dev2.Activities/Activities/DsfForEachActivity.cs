e5255ea77a5b4b668d2dab28152481698f0df046
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2015-07-24 16:05:44 +0200

    merge errors

--- Dev/Dev2.Activities/Activities/DsfForEachActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfForEachActivity.cs
@@ -826,9 +826,9 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                         DispatchDebugState(dataObject, StateType.After, update);
                     }
                     exePayload.InnerActivity = innerA;
-                    var ind = itr.MaxIndex();
-                    var count = 0;
-                    while (itr.HasMore() && count < ind)
+ 
+           
+                    while (itr.HasMore() )
                         {
 
                             operationalData = exePayload;
@@ -836,13 +836,13 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                         int innerupdate = 0;
                         if (exePayload.ForEachType != enForEachType.NumOfExecution)
                             {
-                            //IterateIOMapping(idx);
+            
                             innerupdate = idx;
                             }
 
-                        exeAct.Execute(dataObject, innerupdate);
+                            exeAct.Execute(dataObject, innerupdate);
 
-                        count++;
+            
                             operationalData.IncIterationCount();
                         }
                     if (errors.HasErrors())
