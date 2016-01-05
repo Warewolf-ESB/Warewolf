82538696c382e81e248ed32411e8334d1b7d62bc
Ashley Lewis <ultrakiff@gmail.com>
2016-01-05 10:24:27 +0200

    fix compile break

--- Dev/Dev2.Activities.Specs/Composition/WorkflowExecutionSteps.cs
+++ Dev/Dev2.Activities.Specs/Composition/WorkflowExecutionSteps.cs
@@ -1010,7 +1010,7 @@ namespace Dev2.Activities.Specs.Composition
                 }
                 else
                 {
-                    throw System.InvalidOperationException("SpecFlow broke.");
+                    throw new InvalidOperationException("SpecFlow broke.");
                 }
             }
 
