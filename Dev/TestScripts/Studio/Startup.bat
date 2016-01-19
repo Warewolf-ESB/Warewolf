94e113c8c63446f6abf0e93540235f74446f0474
Ashley Lewis <ultrakiff@gmail.com>
2016-01-19 15:55:59 +0200

    start studio under test with elevated perssions

--- Dev/TestScripts/Studio/Startup.bat
+++ Dev/TestScripts/Studio/Startup.bat
@@ -51,7 +51,7 @@ REM Init paths to Warewolf studio under test
 IF NOT EXIST "%DeploymentDirectory%\..\Studio\Warewolf Studio.exe" IF EXIST "%~dp0..\..\Dev2.Studio\bin\Debug\Warewolf Studio.exe" SET DeploymentDirectory=%~dp0..\..\Dev2.Studio\bin\Debug
 IF EXIST "%DeploymentDirectory%\..\Studio\Warewolf Studio.exe" SET DeploymentDirectory=%DeploymentDirectory%\..\Studio
 REM ** Start Warewolf studio from deployed binaries **
-START "%DeploymentDirectory%\Warewolf Studio.exe" /D "%DeploymentDirectory%" "Warewolf Studio.exe"
+IF EXIST %windir%\nircmd.exe nircmd elevate "%DeploymentDirectory%\Warewolf Studio.exe" else START "%DeploymentDirectory%\Warewolf Studio.exe" /D "%DeploymentDirectory%" "Warewolf Studio.exe"
 
 REM  Wait 1 minute ;)
 ping -n 60 127.0.0.1 > nul
