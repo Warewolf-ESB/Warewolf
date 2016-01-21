f6b2e45e221fbec5085ad124550c09bfc42677c7
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2016-01-21 15:46:51 +0200

    plugin specs

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/FetchPluginActions.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/FetchPluginActions.cs
@@ -36,21 +36,34 @@ namespace Dev2.Runtime.ESB.Management.Services
                 PluginServices services = new PluginServices();
                 var src = ResourceCatalog.Instance.GetResource<PluginSource>(GlobalConstants.ServerWorkspaceID, pluginSource.Id);
                 //src.AssemblyName = ns.FullName;
-                PluginService svc = new PluginService { Namespace = ns.FullName, Source = src };
+                if(ns != null)
+                {
+                    PluginService svc = new PluginService { Namespace = ns.FullName, Source = src };
 
-                var methods = services.Methods(svc, Guid.Empty, Guid.Empty).Select(a => new PluginAction()
+                    var methods = services.Methods(svc, Guid.Empty, Guid.Empty).Select(a => new PluginAction()
+                    {
+                        FullName = ns.FullName,
+                        Inputs = a.Parameters.Select(x => new ServiceInput(x.Name, x.DefaultValue ?? "") { Name = x.Name, EmptyIsNull = x.EmptyToNull, RequiredField = x.IsRequired, TypeName = x.Type } as IServiceInput).ToList(),
+                        Method = a.Name,
+                        Variables = a.Parameters.Select(x => new NameValue() { Name = x.Name + " (" + x.TypeName + ")", Value = "" } as INameValue).ToList(),
+                    } as IPluginAction
+                        ).ToList();
+                    return serializer.SerializeToBuilder(new ExecuteMessage()
+                    {
+                        HasError = false,
+                        Message = serializer.SerializeToBuilder(methods)
+                    });
+                }
+                // ReSharper disable once RedundantIfElseBlock
+                else
                 {
-                    FullName = ns.FullName,
-                    Inputs = a.Parameters.Select(x => new ServiceInput(x.Name, x.DefaultValue ?? "") { Name = x.Name, EmptyIsNull = x.EmptyToNull, RequiredField = x.IsRequired, TypeName = x.Type } as IServiceInput).ToList(),
-                    Method = a.Name,
-                    Variables = a.Parameters.Select(x => new NameValue() { Name = x.Name + " (" + x.TypeName + ")", Value = "" } as INameValue).ToList(),
-                } as IPluginAction
-                    ).ToList();
-                return serializer.SerializeToBuilder(new ExecuteMessage()
-                {
-                    HasError = false,
-                    Message = serializer.SerializeToBuilder(methods)
-                });
+                    return serializer.SerializeToBuilder(new ExecuteMessage()
+                    {
+                        HasError = false,
+                        Message = serializer.SerializeToBuilder(new List<IPluginAction>())
+                    });
+                }
+
                 // ReSharper restore MaximumChainedReferences
             }
             catch (Exception e)
