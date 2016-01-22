831e401871397063dc0ac4ce6666421af4e3a959
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2016-01-22 14:42:56 +0200

    #WOLF-1265 UI changes

--- Dev/Dev2.Activities.Designers/Designers2/.Net DLL/Large.xaml.cs
+++ Dev/Dev2.Activities.Designers/Designers2/.Net DLL/Large.xaml.cs
@@ -9,6 +9,7 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using System;
 using System.Windows;
 using Dev2.Activities.Designers2.Core.Controls;
 using Infragistics.Controls.Grids;
@@ -34,14 +35,13 @@ namespace Dev2.Activities.Designers2.Net_DLL
             MinHeight = 220;
             Height = 220;
         }
-        void SetNewHeight()
+        void SetOutputInitialHeight()
         {
-            var height = 220 + MainGrid.RowDefinitions[3].ActualHeight + MainGrid.RowDefinitions[5].ActualHeight;
-
-            MinHeight = height;
-            Height = height;
-
-            MainGrid.RowDefinitions[5].Height = new GridLength(10, GridUnitType.Star);
+            if (Height > 220)
+            {
+                MinHeight = 290;
+                Height = 290;
+            }
         }
 
         #region Overrides of ActivityDesignerTemplate
@@ -60,7 +60,7 @@ namespace Dev2.Activities.Designers2.Net_DLL
 
         void OutputsMappingDataGrid_OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            SetInitialHeight();
+            SetOutputInitialHeight();
             var grid = sender as XamGrid;
             if (grid != null)
             {
@@ -68,25 +68,42 @@ namespace Dev2.Activities.Designers2.Net_DLL
                 var items = context as DotNetDllViewModel;
                 if (items != null)
                 {
-                    if (items.TestComplete)
+                    if (items.Outputs != null && items.OutputsVisible)
                     {
-                        SetNewHeight();
+                        if (items.Outputs.Count == 0)
+                        {
+                            SetToolHeight(350);
+                            SetOutputGridHeight(grid, 60);
+                        }
+                        else if (items.Outputs.Count > 0 && items.Outputs.Count < 5)
+                        {
+                            double gridHeight = 30 * (items.Outputs.Count + 1);
+                            SetToolHeight(290 + gridHeight);
+                            SetOutputGridHeight(grid, gridHeight);
+                        }
+                        else
+                        {
+                            const double GridHeight = 30 * 6;
+                            SetToolHeight(290 + GridHeight);
+                            SetOutputGridHeight(grid, GridHeight);
+                            SetOutputNewHeight();
+                        }
                     }
                 }
             }
         }
+        void SetOutputNewHeight()
+        {
+            MainGrid.RowDefinitions[5].Height = new GridLength(10, GridUnitType.Star);
+        }
         void SetInputNewHeight()
         {
-            var height = 220 + MainGrid.RowDefinitions[3].ActualHeight;
-
-            MinHeight = height;
-            Height = height;
-
             MainGrid.RowDefinitions[3].Height = new GridLength(10, GridUnitType.Star);
         }
 
         void LargeDataGrid_OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            SetInitialHeight();
             var grid = sender as Dev2DataGrid;
             if (grid != null)
             {
@@ -94,50 +111,44 @@ namespace Dev2.Activities.Designers2.Net_DLL
                 var items = context as DotNetDllViewModel;
                 if (items != null)
                 {
-                    if (items.Inputs != null)
+                    if (items.Inputs != null && items.InputsVisible)
                     {
-                        if (items.Inputs.Count > 0)
+                        if (items.Inputs.Count == 0)
                         {
-                            var rowCount = grid.CountRows();
-                            if (rowCount > 0)
-                            {
-                                if (rowCount > 5)
-                                {
-                                    grid.MaxHeight = grid.RowHeight * 5;
-                                }
-                                else
-                                {
-                                    grid.MaxHeight = rowCount * grid.RowHeight;
-                                }
-                            }
-                            SetInputNewHeight();
+                            SetToolHeight(290);
+                            SetInputGridHeight(grid, 60);
+                        }
+                        else if (items.Inputs.Count > 0 && items.Inputs.Count < 5)
+                        {
+                            double gridHeight = 30 * (items.Inputs.Count + 1);
+                            SetToolHeight(230 + gridHeight);
+                            SetInputGridHeight(grid, gridHeight);
                         }
                         else
                         {
-                            MaxHeight = 230;
-                            grid.MinHeight = 80;
-                            grid.MaxHeight = 80;
+                            const double GridHeight = 30 * 6;
+                            SetToolHeight(230 + GridHeight);
+                            SetInputGridHeight(grid, GridHeight);
+                            SetInputNewHeight();
                         }
                     }
                 }
             }
         }
 
-        void LargeDataGrid_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        void SetToolHeight(double height)
         {
-            var grid = sender as Dev2DataGrid;
-            if (grid != null)
-            {
-                var context = grid.DataContext;
-                var items = context as DotNetDllViewModel;
-                if (items != null)
-                {
-                    if (items.Inputs != null)
-                    {
+            Height = height;
+            MinHeight = height;
+        }
 
-                    }
-                }
-            }
+        static void SetInputGridHeight(Dev2DataGrid grid, double height)
+        {
+            grid.MinHeight = height;
+        }
+        static void SetOutputGridHeight(XamGrid grid, double height)
+        {
+            grid.MinHeight = height;
         }
     }
 }
