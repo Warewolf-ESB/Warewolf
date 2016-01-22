6e6418dc3294e8e663dd3ce317b49fc9754334eb
Ashley Lewis <ultrakiff@gmail.com>
2016-01-22 09:14:29 +0200

    support script for ui tests solution

--- Dev/Warewolf.Studio.UISpecs/Properties/Compile Run Server Studio.bat
+++ Dev/Warewolf.Studio.UISpecs/Properties/Compile Run Server Studio.bat
@@ -1,4 +1,4 @@
-"%vs120comntools%..\IDE\devenv.exe" "%~dp0..\..\Server.sln" /Build Debug
-"%vs120comntools%..\IDE\devenv.exe" "%~dp0..\..\Studio.sln" /Build Debug
+"%vs120comntools%..\IDE\devenv.com" "%~dp0..\..\Server.sln" /Build Debug
+"%vs120comntools%..\IDE\devenv.com" "%~dp0..\..\Studio.sln" /Build Debug
 "%~dp0..\..\TestScripts\Studio\Startup.bat"
 if not %errorlevel%==0 pause
