e9aa583037bc230ac7d81581f7f44b0642a51c81
Ashley Lewis <ultrakiff@gmail.com>
2016-01-22 09:24:04 +0200

    support script updates

--- Dev/Warewolf.Studio.UISpecs/Properties/Compile Run Server Studio.bat
+++ Dev/Warewolf.Studio.UISpecs/Properties/Compile Run Server Studio.bat
@@ -1,4 +1,6 @@
 "%vs120comntools%..\IDE\devenv.com" "%~dp0..\..\Server.sln" /Build Debug
+if not %errorlevel%==0 pause
 "%vs120comntools%..\IDE\devenv.com" "%~dp0..\..\Studio.sln" /Build Debug
+if not %errorlevel%==0 pause
 "%~dp0..\..\TestScripts\Studio\Startup.bat"
 if not %errorlevel%==0 pause
