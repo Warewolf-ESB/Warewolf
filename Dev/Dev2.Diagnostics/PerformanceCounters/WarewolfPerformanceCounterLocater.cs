5aaca185bc5ef2541804a87a775cbe75c1c5fb3f
Leon Rajindrapersadh <Leon.Rajindrapersadh@dev2.co.za>
2016-01-27 08:14:04 +0200

    1332

---
+++ Dev/Dev2.Diagnostics/PerformanceCounters/WarewolfPerformanceCounterLocater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dev2.Common.Interfaces.Monitoring;
+
+namespace Dev2.Diagnostics.PerformanceCounters
+{
+    public class WarewolfPerformanceCounterLocater : IWarewolfPerformanceCounterLocater
+    {
+        private readonly IList<IPerformanceCounter> _counters;
+
+        public WarewolfPerformanceCounterLocater(IList<IPerformanceCounter> counters)
+        {
+            _counters = counters;
+        }
+
+        public IPerformanceCounter GetCounter(string name)
+        {
+            return _counters.First(a => a.Name == name);
+        }
+
+    }
+}
