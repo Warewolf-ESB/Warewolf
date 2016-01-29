129f6ae8a0cadd9d16b5c94b6c3d71a2e2365bb5
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-01-29 10:46:17 +0200

    #WOLF-1265: Plugin code review and cleanup

--- Dev/Dev2.Activities/Activities/DsfPluginActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfPluginActivity.cs
@@ -9,43 +9,19 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
 using Dev2.DataList.Contract;
-using Dev2.Services.Execution;
 using Unlimited.Applications.BusinessDesignStudio.Activities;
 
 namespace Dev2.Activities
 {
     public class DsfPluginActivity : DsfActivity
     {
-        ErrorResultTO _errorsTo;
 
         #region Overrides of DsfActivity
 
         protected override void ExecutionImpl(IEsbChannel esbChannel, IDSFDataObject dataObject, string inputs, string outputs, out ErrorResultTO tmpErrors, int update)
         {
-            _errorsTo = new ErrorResultTO();
-            var pluginServiceExecution = GetNewPluginServiceExecution(dataObject);
-            pluginServiceExecution.InstanceInputDefinitions = inputs;
-            pluginServiceExecution.InstanceOutputDefintions = outputs;
             tmpErrors = new ErrorResultTO();
-            tmpErrors.MergeErrors(_errorsTo);
-            ExecutePluginService(pluginServiceExecution, update);
-            tmpErrors.MergeErrors(_errorsTo);
-        }
-
-        #endregion
-
-        #region Protected Helper Functions
-
-        protected virtual Guid ExecutePluginService(PluginServiceExecution container, int update)
-        {
-            return container.Execute(out _errorsTo, update);
-        }
-
-        protected virtual PluginServiceExecution GetNewPluginServiceExecution(IDSFDataObject context)
-        {
-            return new PluginServiceExecution(context, true);
         }
 
         #endregion
