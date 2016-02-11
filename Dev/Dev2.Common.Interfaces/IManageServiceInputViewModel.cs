86d9a1348f26d90166f2e23d7457b831ef1532cd
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2016-02-11 13:04:14 +0200

    #WOLF-1369 Change validation windows

--- Dev/Dev2.Common.Interfaces/IManageServiceInputViewModel.cs
+++ Dev/Dev2.Common.Interfaces/IManageServiceInputViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 using Dev2.Common.Interfaces.DB;
 
 namespace Dev2.Common.Interfaces
@@ -14,11 +15,15 @@ namespace Dev2.Common.Interfaces
         bool TestResultsAvailable { get; set; }
         bool IsTestResultsEmptyRows { get; set; }
         bool IsTesting { get; set; }
+        ImageSource TestIconImageSource { get; set; }
         ICommand CloseCommand { get; }
         ICommand OkCommand { get; }
         Action OkAction { get; set; }
         T Model { get; set; }
+        string TestHeader { get; set; }
+
         void ShowView();
+        void CloseView();
     }
 
     public interface IManageDatabaseInputViewModel : IManageServiceInputViewModel<IDatabaseService>
