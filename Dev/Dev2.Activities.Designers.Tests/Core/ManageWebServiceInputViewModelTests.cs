9a8b6ecc463fa4c8dd0f96032476c74c4a53e3eb
Leroy <Leroy.warner@Dev2.co.za>
2016-02-12 15:15:21 +0200

    1264 input view tests

---
+++ Dev/Dev2.Activities.Designers.Tests/Core/ManageWebServiceInputViewModelTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dev2.Activities.Designers.Tests.WebGetTool;
+using Dev2.Activities.Designers2.Core;
+using Dev2.Activities.Designers2.Web_Service_Get;
+using Dev2.Common.Interfaces;
+using Dev2.Common.Interfaces.DB;
+using Dev2.Studio.Core.Activities.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warewolf.Core;
+
+namespace Dev2.Activities.Designers.Tests.Core
+{
+    [TestClass]
+    public class ManageWebServiceInputViewModelTests
+    {
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("OutputsRegion_Ctor")]
+        public void ManageWebServiceInputViewModel_Ctor()
+        {
+            ManageWebServiceInputViewModel vm = new ManageWebServiceInputViewModel();
+            Assert.IsNotNull(vm.CloseCommand);
+            Assert.IsNotNull(vm.PasteResponseCommand);
+            Assert.IsNotNull(vm.CloseCommand);
+
+
+            //------------Assert Results-------------------------
+        }
+
+
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("OutputsRegion_Ctor")]
+        public void ManageWebServiceInputViewModel_TestAction()
+        {
+            bool called=false;
+            bool calledOk=false;
+            ManageWebServiceInputViewModel vm = new ManageWebServiceInputViewModel();
+            vm.TestAction = () => { called = true; };
+            vm.OkAction = () =>
+            {
+                calledOk = true;
+            };
+            vm.TestAction();
+            vm.OkAction();
+
+
+            //------------Assert Results-------------------------
+
+            Assert.IsTrue(called);
+            Assert.IsTrue(calledOk);
+        }
+
+        [TestMethod]
+        [Owner("Leon Rajindrapersadh")]
+        [TestCategory("OutputsRegion_Ctor")]
+        public void ManageWebServiceInputViewModel_Properties()
+        {
+
+            ManageWebServiceInputViewModel vm = new ManageWebServiceInputViewModel();
+            var lst = new List<IServiceInput>();
+            vm.Inputs = lst;
+            Assert.AreEqual(lst,vm.Inputs);
+            var lsto = new List<IServiceOutputMapping>();
+            vm.OutputMappings = lsto;
+            Assert.AreEqual(lst, vm.Inputs);
+            vm.TestResults = "bob";
+            Assert.AreEqual("bob",vm.TestResults);
+            vm.TestResultsAvailable = true;
+            Assert.IsTrue(vm.TestResultsAvailable);
+            vm.OkSelected = true;
+            Assert.IsTrue(vm.OkSelected);
+            vm.IsTestResultsEmptyRows = true;
+            Assert.IsTrue(vm.IsTestResultsEmptyRows);
+            vm.IsTesting = true;
+            Assert.IsTrue(vm.IsTesting);
+
+            var b = new WebServiceDefinition() { Headers = new List<NameValue>() { new NameValue("a", "b") } };
+            vm.Model = b;
+            Assert.IsNotNull(vm.Model);
+
+        }
+
+
+    }
+}
