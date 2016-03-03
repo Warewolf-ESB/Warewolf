2db6cb4ce9d156f924b4b89e081f0a3d75113619
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2016-03-03 15:18:07 +0200

    #WOLF-1324 Fix Web UI template heights

--- Dev/Dev2.Activities.Designers/Designers2/Core/Web/Delete/WebDeleteInputRegion.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Core/Web/Delete/WebDeleteInputRegion.cs
@@ -10,6 +10,7 @@ using Dev2.Common.Interfaces;
 using Dev2.Common.Interfaces.ServerProxyLayer;
 using Dev2.Common.Interfaces.ToolBase;
 using Dev2.Studio.Core.Activities.Utils;
+// ReSharper disable NotAccessedField.Local
 
 namespace Dev2.Activities.Designers2.Core.Web.Delete
 {
@@ -27,7 +28,7 @@ namespace Dev2.Activities.Designers2.Core.Web.Delete
         private double _headersHeight;
         double _maxHeadersHeight;
         bool _isVisible;
-        private const double BaseHeight = 165;
+        private const double BaseHeight = 60;
 
         public WebDeleteInputRegion()
         {
@@ -249,28 +250,17 @@ namespace Dev2.Activities.Designers2.Core.Web.Delete
             MaxHeadersHeight = HeadersHeight;
             if (Headers.Count >= 3)
             {
-                MinHeight = BaseHeight + GlobalConstants.RowHeaderHeight + GlobalConstants.RowHeight;
-                MaxHeight = BaseHeight + GlobalConstants.RowHeaderHeight + GlobalConstants.RowHeight;
-                MaxHeadersHeight = 120;
+                MinHeight = 115;
+                MaxHeight = 115;
+                MaxHeadersHeight = 115;
                 CurrentHeight = MinHeight;
             }
             else
             {
-                var count = 0;
-                if (Headers.Count > 0)
-                {
-                    // Remove the header from the count
-                    count = Headers.Count - 1;
-                }
-                CurrentHeight = GlobalConstants.RowHeaderHeight + count * GlobalConstants.RowHeight;
+                CurrentHeight = GlobalConstants.RowHeaderHeight + Headers.Count * GlobalConstants.RowHeight;
                 if (CurrentHeight < BaseHeight)
                 {
                     CurrentHeight = BaseHeight;
-                    // Check if the count is greater than 0 before adding the RowHeight
-                    if (count > 0)
-                    {
-                        CurrentHeight += GlobalConstants.RowHeight;
-                    }
                 }
                 MinHeight = CurrentHeight;
                 MaxHeight = CurrentHeight;
