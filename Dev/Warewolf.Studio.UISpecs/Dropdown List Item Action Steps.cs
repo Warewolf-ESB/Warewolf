c4585f7b1e8c75e3f1d6433bd6b9837440e8e8be
SalamiArmy <ultrakiff@gmail.com>
2016-03-10 15:08:12 +0200

    Add custom action for asserting an item exists in the currently open dropdownlist

--- Dev/Warewolf.Studio.UISpecs/Dropdown List Item Action Steps.cs
+++ Dev/Warewolf.Studio.UISpecs/Dropdown List Item Action Steps.cs
@@ -20,6 +20,14 @@ namespace Warewolf.Studio.UISpecs
             Mouse.Click(AllItems.FirstOrDefault(listitem => listitem.Name == ListItem), new Point(396, 11));
         }
 
+        [Given("'(.*)' exists in the dropdown list")]
+        [Then("'(.*)' exists in the dropdown list")]
+        public void AssertExistsInTheDropdownList(string ListItem)
+        {
+            var AllItems = Uimap.MainStudioWindow.GetChildren().Where(child => child.ClassName == "Uia.ComboEditorItemControl");
+            Assert.IsTrue(AllItems.Any(listitem => listitem.Name == ListItem), ListItem + " does not appear as an option in the drop down list.");
+        }
+
         #region Properties and Fields
 
         OutsideWorkflowDesignSurfaceUIMap Uimap
