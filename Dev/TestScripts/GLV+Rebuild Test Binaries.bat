6493317c375f7902bbe2cd1e6f3c5c000023b555
SalamiArmy <ultrakiff@gmail.com>
2016-03-11 13:17:37 +0200

    new feature for convenience script to checkout older versions

--- Dev/TestScripts/GLV+Rebuild Test Binaries.bat
+++ Dev/TestScripts/GLV+Rebuild Test Binaries.bat
@@ -2,7 +2,7 @@ git -C "%~dp0..\.." reset --hard
 if not %errorlevel%==0 pause & exit 1
 git -C "%~dp0..\.." clean -xdf
 if not %errorlevel%==0 pause & exit 1
-git -C "%~dp0..\.." pull
+if "%1"=="" (git -C "%~dp0..\.." pull) else (git -C "%~dp0..\.." checkout %1)
 if not %errorlevel%==0 pause & exit 1
 "%~dp0..\.nuget\nuget.exe" restore "%~dp0..\AcceptanceTesting.sln"
 if not %errorlevel%==0 pause & exit 1
