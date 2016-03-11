6026254cbc42c038ee86aa4e061cbee5bc6570b5
SalamiArmy <ultrakiff@gmail.com>
2016-03-11 09:23:28 +0200

    Add variable list custom action steps

--- Dev/Warewolf.Studio.UISpecs/StudioUISpecs.feature.cs
+++ Dev/Warewolf.Studio.UISpecs/StudioUISpecs.feature.cs
@@ -1964,12 +1964,8 @@ this.ScenarioSetup(scenarioInfo);
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Testing New Custom Action Recordings", ((string[])(null)));
 #line 1186
 this.ScenarioSetup(scenarioInfo);
-#line 1187
- testRunner.Given("I \'Assert_Database_Source_Dropdownlist_Exists\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line 1188
- testRunner.When("I \'Select_Data_Source_Droplist\'", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 1189
- testRunner.When("I select \'Customer\' from the dropdown list", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+ testRunner.When("I toggle \'somevar\' Input checkbox in the variable list", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
             this.ScenarioCleanup();
         }
