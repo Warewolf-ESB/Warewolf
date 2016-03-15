b2dd74febcd47b4a58b2280fa9e0103a846b43fe
SalamiArmy <ultrakiff@gmail.com>
2016-03-15 10:23:44 +0200

    updated decrypt server resources script to also decrypt resources in program data

--- Dev/Dev2.Server/Decrypt Server Resources.ps1
+++ Dev/Dev2.Server/Decrypt Server Resources.ps1
@@ -3,16 +3,16 @@ $Invocation = (Get-Variable MyInvocation -Scope 0).Value
 $SolutionDirectory = (get-item (Split-Path $Invocation.MyCommand.Path)).parent.FullName
 Write-Host Got solution path as `"$SolutionDirectory`".
 
-Write-Host Starting nuget package restore using nuget.exe at "$SolutionDirectory\.nuget\nuget.exe".
-[System.Diagnostics.Process]::Start("""" + $SolutionDirectory + ".nuget\nuget.exe""", "restore """ + $SolutionDirectory + "\..\Server.sln""")
+Write-Host Starting nuget package restore using nuget.exe at `"$SolutionDirectory\.nuget\nuget.exe`".
+[System.Diagnostics.Process]::Start("""" + $SolutionDirectory + "\.nuget\nuget.exe""", "restore """ + $SolutionDirectory + "\Server.sln""")
 Write-Host Nuget package restore finished.
 
-Write-Host Starting compile using compiler at "$env:vs120comntools..\IDE\devenv.com".
-[System.Diagnostics.Process]::Start("""" + $env:vs120comntools + "..\IDE\devenv.com""", """" + $SolutionDirectory + "\..\Server.sln"" /Build ""Debug""")
+Write-Host Starting compile using compiler at `"$env:vs120comntools..\IDE\devenv.com`".
+[System.Diagnostics.Process]::Start("""" + $env:vs120comntools + "..\IDE\devenv.com""", """" + $SolutionDirectory + "\Server.sln"" /Build ""Debug""")
 Write-Host Compile finished.
 
-Write-Host Loading assembly at "$SolutionDirectory\..\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll".
-Add-Type -Path "$SolutionDirectory\..\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll"
+Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll`".
+Add-Type -Path "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll"
 Write-Host Assembly loaded.
 
 Write-Host Loading type.
@@ -20,7 +20,7 @@ $ResourceHandler = New-Object Dev2.Runtime.ESB.Management.Services.FetchResource
 Write-Host Type loaded.
 
 Write-Host Recursing through resources.
-get-childitem "$SolutionDirectory\Resources - Debug" -recurse | where {$_.extension -eq ".xml"} | % {
+get-childitem "$SolutionDirectory\Resources - Debug","$env:ProgramData\Warewolf\Resources" -recurse | where {$_.extension -eq ".xml"} | % {
 
 	Write-Host Resource found at $_.FullName.
 	$sb = New-Object System.Text.StringBuilder
