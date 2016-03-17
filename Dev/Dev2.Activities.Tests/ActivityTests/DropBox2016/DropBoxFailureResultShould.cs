98570dc4c50677206afe553f387869d91d48d959
malotho <mnatiris@gmail.com>
2016-03-17 07:55:01 +0200

    WOLF-1490: dropBox Specs Done

--- Dev/Dev2.Activities.Tests/ActivityTests/DropBox2016/DropBoxFailureResultShould.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DropBox2016/DropBoxFailureResultShould.cs
@@ -30,7 +30,7 @@ namespace Dev2.Tests.Activities.ActivityTests.DropBox2016
             var dpExc = new Exception("Message");
             var failureResult = new DropboxFailureResult(dpExc);
             //---------------Assert Precondition----------------
-
+            Assert.IsNotNull(failureResult);
             //---------------Execute Test ----------------------
             var exception = failureResult.GetException();
             //---------------Test Result -----------------------
