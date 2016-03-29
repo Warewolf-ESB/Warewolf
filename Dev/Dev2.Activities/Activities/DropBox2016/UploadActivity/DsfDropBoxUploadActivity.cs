7c3cfbb0c6f0a57974b125a213710eae281cdcd6
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2016-03-29 11:42:13 +0200

    #WOLF-1440 Change dropbox region name

--- Dev/Dev2.Activities/Activities/DropBox2016/UploadActivity/DsfDropBoxUploadActivity.cs
+++ Dev/Dev2.Activities/Activities/DropBox2016/UploadActivity/DsfDropBoxUploadActivity.cs
@@ -15,7 +15,7 @@ using Warewolf.Core;
 
 namespace Dev2.Activities.DropBox2016.UploadActivity
 {
-    [ToolDescriptorInfo("DropBoxLogo", "Upload", ToolType.Native, "8999E59A-38A3-43BB-A98F-6090C8C9EA2E", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Dropbox", "/Warewolf.Studio.Themes.Luna;component/Images.xaml")]
+    [ToolDescriptorInfo("DropBoxLogo", "UPLOAD", ToolType.Native, "8999E59A-38A3-43BB-A98F-6090C8C9EA2E", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Storage", "/Warewolf.Studio.Themes.Luna;component/Images.xaml")]
     public class DsfDropBoxUploadActivity : DsfBaseActivity
     {
         private DropboxClient _client;
@@ -28,7 +28,7 @@ namespace Dev2.Activities.DropBox2016.UploadActivity
         public DsfDropBoxUploadActivity()
         {
             // ReSharper disable once VirtualMemberCallInContructor
-            DisplayName = "Upload";
+            DisplayName = "UPLOAD to Dropbox";
             OverWriteMode = true;
         }
 
