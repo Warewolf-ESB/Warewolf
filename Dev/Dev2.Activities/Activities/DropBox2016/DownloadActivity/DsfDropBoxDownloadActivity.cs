4d208d98afa609a74a71d8cde1a811432ea0480c
malotho <mnatiris@gmail.com>
2016-03-30 10:29:19 +0200

    WOLF-1535: Fixing test failures

--- Dev/Dev2.Activities/Activities/DropBox2016/DownloadActivity/DsfDropBoxDownloadActivity.cs
+++ Dev/Dev2.Activities/Activities/DropBox2016/DownloadActivity/DsfDropBoxDownloadActivity.cs
@@ -18,7 +18,7 @@ using Warewolf.Core;
 
 namespace Dev2.Activities.DropBox2016.DownloadActivity
 {
-    [ToolDescriptorInfo("DropBoxLogo", "Download", ToolType.Native, "8999E59A-38A3-43BB-A98F-6090D8C8EA1E", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Storage", "/Warewolf.Studio.Themes.Luna;component/Images.xaml")]
+    [ToolDescriptorInfo("DropBoxLogo", "DOWNLOAD", ToolType.Native, "8999E59A-38A3-43BB-A98F-6090D8C8EA1E", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Storage", "/Warewolf.Studio.Themes.Luna;component/Images.xaml")]
     public class DsfDropBoxDownloadActivity : DsfBaseActivity
     {
         private DropboxClient _client;
@@ -29,7 +29,7 @@ namespace Dev2.Activities.DropBox2016.DownloadActivity
         public DsfDropBoxDownloadActivity()
         {
             // ReSharper disable once VirtualMemberCallInContructor
-            DisplayName = "Download";
+            DisplayName = "DOWNLOAD from Dropbox";
         }
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Global
@@ -73,6 +73,11 @@ namespace Dev2.Activities.DropBox2016.DownloadActivity
 
         protected override void ExecuteTool(IDSFDataObject dataObject, int update)
         {
+            if (string.IsNullOrEmpty(FromPath))
+            {
+                dataObject.Environment.AddError("Please confirm that the correct file location has been entered");
+                return;
+            }
             if (string.IsNullOrEmpty(ToPath))
             {
                 dataObject.Environment.AddError("Please confirm that the correct file destination has been entered");
@@ -98,16 +103,6 @@ namespace Dev2.Activities.DropBox2016.DownloadActivity
                 if (Response.Response.IsFile)
                 {
                     File.WriteAllBytes(FromPath, bytes);
-                }
-                //This option does not make sense since only files can be downloaded anyway.
-                if (Response.Response.IsFolder)
-                {
-                    Stream result = Response.GetContentAsStreamAsync().Result;
-                    using(var a = new StreamReader(result))
-                    {
-                        Directory.CreateDirectory(FromPath);
-                        var folderMetadata = Response.Response.AsFolder;
-                    }
                 }
                 return Response.Response.Name;
             }
