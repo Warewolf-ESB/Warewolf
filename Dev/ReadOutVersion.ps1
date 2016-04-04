e753fb95194b762d526228954cf14e0b7ea01a37
SalamiArmy <ultrakiff@gmail.com>
2016-04-04 16:26:29 +0200

    fix versioning script to be compatable with bamboo

--- Dev/ReadOutVersion.ps1
+++ Dev/ReadOutVersion.ps1
@@ -1,12 +1,22 @@
 $WarewolfGitRepoDirectory = "$PSScriptRoot"
-Write-Host Reading Warewolf assembly versions...
+Write-Host Reading Warewolf assembly files...
 
 # Foreach Warewolf assembly.
-$AllReadVersions = @()
+$HighestReadVersion = "0.0.0.0"
 foreach ($file in Get-ChildItem -recurse $WarewolfGitRepoDirectory) {
 	if (($file.Name.EndsWith(".dll") -or ($file.Name.EndsWith(".exe") -and -Not $file.Name.EndsWith(".vshost.exe"))) -and ($file.Name.StartsWith("Dev2.") -or $file.Name.StartsWith("Warewolf.") -or $file.Name.StartsWith("WareWolf"))) {
 		# Get version.
 		$ReadVersion = [system.diagnostics.fileversioninfo]::GetVersionInfo($file.FullName).FileVersion
+		
+		# Find highest version
+		$SeperateVersionNumbers = $ReadVersion.split(".")
+		$SeperateVersionNumbersHighest = $HighestReadVersion.split(".")
+		if ($SeperateVersionNumbers[0] -gt $SeperateVersionNumbersHighest[0]`
+		-or $SeperateVersionNumbers[1] -gt $SeperateVersionNumbersHighest[1]`
+		-or $SeperateVersionNumbers[2] -gt $SeperateVersionNumbersHighest[2]`
+		-or $SeperateVersionNumbers[3] -gt $SeperateVersionNumbersHighest[3]){
+			$HighestReadVersion = $ReadVersion
+		}
 
         # Check for invalid.
         if ($ReadVersion.StartsWith("0.0.") -or $ReadVersion.EndsWith(".0")) {
@@ -15,5 +25,5 @@ foreach ($file in Get-ChildItem -recurse $WarewolfGitRepoDirectory) {
         }
 	}
 }
-"FullVersionString=$AllReadVersionsConcatinated" | Out-File -LiteralPath FullVersionString -Encoding utf8
+"FullVersionString=$HighestReadVersion" | Out-File -LiteralPath FullVersionString -Encoding utf8
 Write-Host Compile has completed successfully! For more info about this script see: http://warewolf.io/ESB-blog/artefact-sharing-efficient-ci/
