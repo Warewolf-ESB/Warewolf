79acc6e9ee819b10c51f7d5450141ad4d3eb364b
SalamiArmy <ultrakiff@gmail.com>
2016-04-07 09:29:31 +0200

    refactor convenience scripts

---
+++ Dev/TestScripts/Regenerate All SpecFlow Feature Files.bat
@@ -0,0 +1,11 @@
+cd /d "%~dp0.."
+.nuget\nuget.exe restore AcceptanceTesting.sln
+if not %errorlevel%==0 pause & exit 1
+for /d %%d in (*.Specs) do (
+	packages\SpecFlow.1.9.0\tools\specflow.exe generateAll "%%d\%%d.csproj" /force /verbose
+)
+for /d %%d in (Warewolf.AcceptanceTesting.*) do (
+	packages\SpecFlow.1.9.0\tools\specflow.exe generateAll "%%d\%%d.csproj" /force /verbose
+)
+packages\SpecFlow.1.9.0\tools\specflow.exe generateAll Dev2.Studio.UISpecs\Dev2.Studio.UI.Specs.csproj /force /verbose
+packages\SpecFlow.1.9.0\tools\specflow.exe generateAll Dev2.Installer.Specs\Warewolf.AcceptanceTesting.Installer.csproj /force /verbose
