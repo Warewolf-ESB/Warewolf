4655ccdad61ebc099f560a7ae7857060b0f3a7e8
Pete_Terblanche <pete.terblanche@gmail.com>
2016-04-14 11:56:49 +0200

    #WOLF-1604 Execution and tests

---
+++ Dev/Dev2.Activities.Tests/ActivityTests/DropBox2016/Delete/DropBoxDeleteTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Dev2.Activities.DropBox2016.DeleteActivity;
+using Dev2.Activities.DropBox2016.Result;
+using Dropbox.Api;
+using Dropbox.Api.Files;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedVariable
+
+namespace Dev2.Tests.Activities.ActivityTests.DropBox2016.Delete
+{
+    [TestClass]
+    public class DropBoxDeleteTests
+    {
+        private Mock<IDropBoxDelete> CreateDropboxDeleteMock()
+        {
+            var mock = new Mock<IDropBoxDelete>();
+            var fileMetadata = new DropboxDeleteSuccessResult(new FileMetadata());
+            mock.Setup(upload => upload.ExecuteTask(It.IsAny<DropboxClient>()))
+                 .Returns(fileMetadata);
+            return mock;
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DropboxDelete_CreateDropboxActivity")]
+        public void DropboxDelete_CreateDropboxActivity_GivenIsNew_ShouldNotBeNull()
+        {
+            //------------Setup for test--------------------------
+            var dropBoxDelete = CreateDropboxDeleteMock().Object;
+            
+            //------------Execute Test---------------------------
+
+            //------------Assert Results-------------------------
+            Assert.IsNotNull(dropBoxDelete);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DropboxDelete_ExecuteTask")]
+        public void DropboxDelete_ExecuteTask_GivenDropBoxDelete_ShouldReturnFileMetadata()
+        {
+            //------------Setup for test--------------------------
+            var dropBoxDelete = CreateDropboxDeleteMock();
+
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(dropBoxDelete);
+            //------------Execute Test---------------------------
+            dropBoxDelete.Object.ExecuteTask(It.IsAny<DropboxClient>());
+            //------------Assert Results-------------------------
+            dropBoxDelete.Verify(upload => upload.ExecuteTask(It.IsAny<DropboxClient>()));
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DropboxDelete_CreateDropboxActivity")]
+        public void DropboxDelete_CreateDropboxActivity_GivenParameters_ShouldBeValid()
+        {
+            //---------------Set up test pack-------------------
+            var dropBoxDelete = new DropboxDelete("random.txt");
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(dropBoxDelete);
+            //---------------Execute Test ----------------------
+            dropBoxDelete.Validate();
+            //---------------Test Result -----------------------
+            Assert.IsTrue(dropBoxDelete.IsValid);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DropboxDelete_CreateDropboxActivity")]
+        public void DropboxDelete_CreateDropboxActivity_GivenMissingDeletePath_ShouldBeInValid()
+        {
+            bool valid = true;
+            try
+            {
+                //---------------Set up test pack-------------------
+                var dropBoxDelete = new DropboxDelete("");
+            }
+            catch(Exception)
+            {
+                valid = false;
+            }
+            //---------------Execute Test ----------------------
+            
+            //---------------Test Result -----------------------
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DropboxDelete_CreateDropboxActivity")]
+        public void DropboxDelete_CreateDropboxActivity_GivenInvalidThenExecute_ShouldReturnFailureResult()
+        {
+            bool valid = true;
+            try
+            {
+                //---------------Set up test pack-------------------
+                var dropBoxDelete = new DropboxDelete("");
+            }
+            catch (Exception)
+            {
+                valid = false;
+            }
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.IsFalse(valid);
+        }
+    }
+}
