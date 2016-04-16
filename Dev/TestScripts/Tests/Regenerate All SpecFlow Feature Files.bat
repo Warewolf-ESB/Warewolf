51c0805dc3bbd3dc141f3117c3d76a13601ecf4e
SalamiArmy <ultrakiff@gmail.com>
2016-04-16 09:50:38 +0200

    fixed specflow regenerate feature files script to work with latest .net framework version

--- Dev/TestScripts/Tests/Regenerate All SpecFlow Feature Files.bat
+++ Dev/TestScripts/Tests/Regenerate All SpecFlow Feature Files.bat
@@ -1,6 +1,14 @@
 cd /d "%~dp0..\.."
 .nuget\nuget.exe restore AcceptanceTesting.sln
 if not %errorlevel%==0 pause & exit 1
+if exist packages\SpecFlow.1.9.0\tools\specflow.exe.config del packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo ^<?xml version="1.0" encoding="utf-8" ?^>  >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo ^<configuration^>  >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo     ^<startup^>  >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo         ^<supportedRuntime version="v4.0.30319" /^>  >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo     ^</startup^>  >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+echo ^</configuration^> >>packages\SpecFlow.1.9.0\tools\specflow.exe.config
+if not %errorlevel%==0 pause & exit 1
 for /d %%d in (*.Specs) do (
 	packages\SpecFlow.1.9.0\tools\specflow.exe generateAll "%%d\%%d.csproj" /force /verbose
 )
