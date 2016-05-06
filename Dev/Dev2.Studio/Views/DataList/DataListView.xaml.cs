86b8092d169efe96cf5099315d236d6a28e035d4
Pete_Terblanche <pete.terblanche@gmail.com>
2016-05-06 14:16:23 +0200

    #WOLF-1593 Show all records expanded. Fix Input output not selecting all

--- Dev/Dev2.Studio/Views/DataList/DataListView.xaml.cs
+++ Dev/Dev2.Studio/Views/DataList/DataListView.xaml.cs
@@ -258,5 +258,10 @@ namespace Dev2.Studio.Views.DataList
                 }
             }
         }
+
+        private void Xtg_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Xtg.Records.ExpandAll(true);
+        }
     }
 }
