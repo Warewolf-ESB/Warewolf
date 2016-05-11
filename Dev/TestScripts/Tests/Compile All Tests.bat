e2ec38c5bc2f67e596cdf885184a4e2af2dedbfb
unknown <IntegrationTester@TST-CI1.dev2.local>
2016-05-11 10:50:51 +0200

    script can compile warewolf without vs

--- Dev/TestScripts/Tests/Compile All Tests.bat
+++ Dev/TestScripts/Tests/Compile All Tests.bat
@@ -14,6 +14,11 @@ if not %errorlevel%==0 pause & exit 1
 ) else IF EXIST MSBuild (
 MSBuild "%~dp0..\..\UITesting.sln" /p:Platform="Any CPU";Configuration="Debug"
 if not %errorlevel%==0 pause & exit 1
-MSBuild "%~dp0..\..\AcceptanceTesting.sln" /p:Platform="Any CPU";Configuration="Debug" )
+MSBuild "%~dp0..\..\AcceptanceTesting.sln" /p:Platform="Any CPU";Configuration="Debug"
+) else IF EXIST "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" (
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0..\..\UITesting.sln" /p:Platform="Any CPU";Configuration="Debug"
+if not %errorlevel%==0 pause & exit 1
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0..\..\AcceptanceTesting.sln" /p:Platform="Any CPU";Configuration="Debug"
+)
 
 if not %errorlevel%==0 pause & exit 1
