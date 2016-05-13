b52ca503757a331fd0082f376329d726cdeb7430
SalamiArmy <ultrakiff@gmail.com>
2016-05-13 08:25:31 +0200

    create empty step definition for sharepoint tool specs

--- Dev/Warewolf.ToolsSpecs/Toolbox/Sharepoint/SharepointReadSteps.cs
+++ Dev/Warewolf.ToolsSpecs/Toolbox/Sharepoint/SharepointReadSteps.cs
@@ -7,22 +7,22 @@ using TechTalk.SpecFlow;
 namespace Warewolf.ToolsSpecs.Toolbox.Sharepoint
 {
     [Binding]
-    public sealed class SharepointCreateSteps
+    public sealed class SharepointReadSteps
     {
-        [Given(@"I drag Sharepoint Create Tool onto the design surface")]
-        public void GivenIDragSharepointCreateToolOntoTheDesignSurface()
+        [Given(@"I drag Sharepoint Read Tool onto the design surface")]
+        public void GivenIDragSharepointReadToolOntoTheDesignSurface()
         {
             throw new NotImplementedException("This step definition is not yet implemented and is required for this test to pass. - Ashley");
         }
 
-        [Then(@"the Sharepoint Create Tool is refreshed")]
-        public void ThenTheSharepointCreateToolIsRefreshed()
+        [Then(@"the Sharepoint Read Tool is refreshed")]
+        public void ThenTheSharepointReadToolIsRefreshed()
         {
             throw new NotImplementedException("This step definition is not yet implemented and is required for this test to pass. - Ashley");
         }
 
-        [When(@"Sharepoint Create Input variables are")]
-        public void WhenSharepointCreateInputVariablesAre()
+        [When(@"Sharepoint Read variables are")]
+        public void WhenSharepointReadVariablesAre(Table table)
         {
             throw new NotImplementedException("This step definition is not yet implemented and is required for this test to pass. - Ashley");
         }
