d2245df35e50b7efec6c7cf6d9f61a534433f6cf
mnatiris <mnatiris@gmail.com>
2016-05-16 15:53:08 +0200

    WOLF-1688: Breaking ResourceBuilder Fixed, Fixed the ItemDragged Instance on the SurfaceDesigner, Tools large view toggle fixed

--- Dev/Dev2.Activities.Designers/Designers2/Web Service Delete/WebServiceDeleteViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Web Service Delete/WebServiceDeleteViewModel.cs
@@ -58,6 +58,15 @@ namespace Dev2.Activities.Designers2.Web_Service_Delete
             Model = model;
 
             SetupCommonProperties();
+            if (IsItemDragged.Instance.IsDragged)
+            {
+                Expand();
+                IsItemDragged.Instance.IsDragged = false;
+            }
+            else
+            {
+                ShowLarge = false;
+            }
         }
         Guid UniqueID { get { return GetProperty<Guid>(); } }
         private void SetupCommonProperties()
