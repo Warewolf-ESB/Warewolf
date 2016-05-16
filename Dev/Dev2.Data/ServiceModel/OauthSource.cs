04875baf95ce723037dff8b31edd5f0358d0bfb9
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-05-16 13:32:26 +0200

    WIP OAuthSource

--- Dev/Dev2.Data/ServiceModel/OauthSource.cs
+++ Dev/Dev2.Data/ServiceModel/OauthSource.cs
@@ -9,49 +9,20 @@ using Warewolf.Security.Encryption;
 namespace Dev2.Data.ServiceModel
 {
     // ReSharper disable once UnusedMember.Global
-    public class OauthSource : Resource, IResourceSource, IOAuthSource
+    public abstract class OauthSource : Resource, IResourceSource, IOAuthSource
     {
-        #region Properties
-
-        // ReSharper disable once MemberCanBePrivate.Global
-        public string AccessToken { get; set; }
-        // ReSharper disable once MemberCanBePrivate.Global
-        public string AppKey { get; set; }
-
-
-        #endregion
 
         #region CTOR
 
-        public OauthSource()
+        protected OauthSource()
         {
-            ResourceID = Guid.Empty;
-            ResourceType = "OauthSource";
-            AccessToken = string.Empty;
-            AppKey = string.Empty;
+            ResourceID = Guid.Empty;            
         }
 
-        public OauthSource(XElement xml)
+        protected OauthSource(XElement xml)
             : base(xml)
         {
-            ResourceType = "OauthSource";
-
-
-            var properties = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-            {
-                { "AccessToken", string.Empty },
-                { "AppKey", string.Empty }
-            };
-
-            var conString = xml.AttributeSafe("ConnectionString");
-            var connectionString = conString.CanBeDecrypted() ? DpapiWrapper.Decrypt(conString) : conString;
-            ParseProperties(connectionString, properties);
-            AccessToken = properties["AccessToken"];
-            AppKey = properties["AppKey"];
-   
-
         }
-
        
         #endregion
 
@@ -60,10 +31,7 @@ namespace Dev2.Data.ServiceModel
         public override XElement ToXml()
         {
             var result = base.ToXml();
-            var connectionString = string.Join(";",
-                string.Format("AccessToken={0}", AccessToken),
-                string.Format("AppKey={0}", AppKey)
-                );
+            var connectionString = GetConnectionString();
 
             result.Add(
                 new XAttribute("ConnectionString", DpapiWrapper.Encrypt(connectionString)),
@@ -73,6 +41,8 @@ namespace Dev2.Data.ServiceModel
 
             return result;
         }
+
+        protected abstract string GetConnectionString();
 
         public override bool IsSource
         {
@@ -117,7 +87,74 @@ namespace Dev2.Data.ServiceModel
             }
         }
 
-        #endregion
+        #endregion    
 
+        public abstract string AppKey { get; set; }
+        public abstract string AccessToken { get; set; }
+
+        #region Implementation of IEquatable<IOAuthSource>
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public abstract bool Equals(IOAuthSource other);
+
+        #endregion
+    }
+
+    public class DropBoxSource : OauthSource
+    {
+        public DropBoxSource()
+        {
+            ResourceType = "DropBoxSource";
+        }
+
+        public DropBoxSource(XElement xml)
+            : base(xml)
+        {
+            ResourceType = "DropBoxSource";
+
+            var properties = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "AccessToken", string.Empty },
+                { "AppKey", string.Empty }
+            };
+
+            var conString = xml.AttributeSafe("ConnectionString");
+            var connectionString = conString.CanBeDecrypted() ? DpapiWrapper.Decrypt(conString) : conString;
+            ParseProperties(connectionString, properties);
+            AccessToken = properties["AccessToken"];
+            AppKey = properties["AppKey"];
+        }
+
+        public sealed override string AccessToken { get; set; }
+        public sealed override string AppKey { get; set; }
+
+
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public override bool Equals(IOAuthSource other)
+        {
+            return AppKey == other.AppKey && AccessToken == other.AccessToken;
+        }
+
+        protected override string GetConnectionString()
+        {
+            var connectionString = string.Join(";",
+                string.Format("AccessToken={0}", AccessToken),
+                string.Format("AppKey={0}", AppKey)
+                );
+            return connectionString;
+        }
     }
 }
