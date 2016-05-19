40cffaae157167417f9892d623bd21082eaca32d
mnatiris <mnatiris@gmail.com>
2016-05-19 13:20:22 +0200

    WOLF-1679: Dropbox Delete updated to refresh sources

---
+++ Dev/Dev2.Common/Wrappers/ExternalProcessWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Dev2.Common.Interfaces.Wrappers;
+
+namespace Dev2.Common.Wrappers
+{
+    public class ExternalProcessWrapper:IExternalProsses
+    {
+        #region Implementation of IExternalProsses
+
+        public Process Start(string fileName)
+        {
+            return Process.Start(fileName);
+        }
+
+        #endregion
+    }
+}
