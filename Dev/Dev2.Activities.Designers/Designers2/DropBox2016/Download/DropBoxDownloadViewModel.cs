853791283580796bfb048f5f18cc7a1cb5178324
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-05-20 09:06:27 +0200

    fixed resources and using

--- Dev/Dev2.Activities.Designers/Designers2/DropBox2016/Download/DropBoxDownloadViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/DropBox2016/Download/DropBoxDownloadViewModel.cs
@@ -13,7 +13,6 @@ using Dev2.Common.Interfaces;
 using Dev2.Data.ServiceModel;
 using Dev2.Interfaces;
 using Dev2.Runtime.Configuration.ViewModels.Base;
-using Dev2.Runtime.Hosting;
 using Dev2.Services.Events;
 using Dev2.Studio.Core.Messages;
 // ReSharper disable ClassWithVirtualMembersNeverInherited.Global
