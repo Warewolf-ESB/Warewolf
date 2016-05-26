123ea6ee7a94166d108eb42feb0eee3f3790c6c4
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-05-26 11:01:12 +0200

    Added functionality to get the JSON string that a complex object represents

--- Dev/Dev2.Studio.Core/Interfaces/IDataMappingViewModel.cs
+++ Dev/Dev2.Studio.Core/Interfaces/IDataMappingViewModel.cs
@@ -11,7 +11,6 @@
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Windows.Input;
 using Dev2.Common.Interfaces.Data;
 
 // ReSharper disable once CheckNamespace
--- Dev/Dev2.Studio.Core/Models/DataList/ComplexObjectItemModel.cs
+++ Dev/Dev2.Studio.Core/Models/DataList/ComplexObjectItemModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Text;
 using Dev2.Data.Binary_Objects;
 using Dev2.Studio.Core.Interfaces.DataList;
 
@@ -10,7 +11,7 @@ namespace Dev2.Studio.Core.Models.DataList
         private IComplexObjectItemModel _parent;
         private bool _isParentObject;
 
-        public ComplexObjectItemModel(string displayname, enDev2ColumnArgumentDirection dev2ColumnArgumentDirection = enDev2ColumnArgumentDirection.None, string description = "", IComplexObjectItemModel parent = null, OptomizedObservableCollection<IComplexObjectItemModel> children = null, bool hasError = false, string errorMessage = "", bool isEditable = true, bool isVisible = true, bool isSelected = false, bool isExpanded = true) 
+        public ComplexObjectItemModel(string displayname, IComplexObjectItemModel parent = null, enDev2ColumnArgumentDirection dev2ColumnArgumentDirection = enDev2ColumnArgumentDirection.None, string description = "", OptomizedObservableCollection<IComplexObjectItemModel> children = null, bool hasError = false, string errorMessage = "", bool isEditable = true, bool isVisible = true, bool isSelected = false, bool isExpanded = true) 
             : base(displayname, dev2ColumnArgumentDirection, description, hasError, errorMessage, isEditable, isVisible, isSelected, isExpanded)
         {
             Children = children;
@@ -139,5 +140,74 @@ namespace Dev2.Studio.Core.Models.DataList
         //    return name;
         //}
 
+        public string GetJson()
+        {
+            var jsonString = new StringBuilder();
+            if (!string.IsNullOrEmpty(DisplayName))
+            {
+                if (Parent == null)
+                {
+                    AppendObjectOpenChar(jsonString);
+                }
+                jsonString.Append("\"" + DisplayName + "\"");
+                jsonString.Append(":");
+                if (Children.Count > 0)
+                {
+                    AppendOpenArrayChar(jsonString);
+                    AppendObjectOpenChar(jsonString);
+                    BuildJsonForChildren(jsonString);
+                    AppendObjectCloseChar(jsonString);
+                    AppendCloseArrayChar(jsonString);
+                }
+                if (Parent == null)
+                {
+                    AppendObjectCloseChar(jsonString);
+                }
+            }
+            return jsonString.ToString();
+        }
+
+        private void AppendCloseArrayChar(StringBuilder jsonString)
+        {
+            if(IsArray)
+            {
+                jsonString.Append("]");
+            }
+        }
+
+        private static void AppendObjectCloseChar(StringBuilder jsonString)
+        {
+            jsonString.Append("}");
+        }
+
+        private void AppendOpenArrayChar(StringBuilder jsonString)
+        {
+            if(IsArray)
+            {
+                jsonString.Append("[");
+            }
+        }
+
+        private static void AppendObjectOpenChar(StringBuilder jsonString)
+        {
+            jsonString.Append("{");
+        }
+
+        private void BuildJsonForChildren(StringBuilder jsonString)
+        {
+            for(int index = 0; index < Children.Count; index++)
+            {
+                var complexObjectItemModel = Children[index];
+                jsonString.Append(complexObjectItemModel.GetJson());
+                if(complexObjectItemModel.Children.Count == 0)
+                {
+                    jsonString.Append("\"\"");
+                }
+                if(index != Children.Count - 1)
+                {
+                    jsonString.Append(",");
+                }
+            }
+        }
     }
 }
