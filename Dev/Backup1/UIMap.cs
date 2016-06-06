5d418818f7e3ed6f2355684958554fc2d74dd8c3
SalamiArmy <ultrakiff@gmail.com>
2016-06-06 10:13:42 +0200

    still fixing compile break

---
+++ Dev/Backup1/UIMap.cs
@@ -0,0 +1,23 @@
+﻿namespace Warewolf.Studio.UISpecs
+{
+    using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+    using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+    using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
+    using System;
+    using System.Collections.Generic;
+    using System.CodeDom.Compiler;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.Text.RegularExpressions;
+    
+    
+    public partial class UIMap
+    {
+    }
+}
