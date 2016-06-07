8b420a599cf13003972e4bb5ece4bdbd246afb81
Barney <barney.buchan@dev2.co.za>
2016-06-07 15:45:51 +0200

    Removed redundant methods

--- Dev/Dev2.Data/ListOfIndex.cs
+++ Dev/Dev2.Data/ListOfIndex.cs
@@ -45,30 +45,6 @@ namespace Dev2.Data.Binary_Objects
             return result;
         }
 
-        public int GetMinIndex()
-        {
-            int result = -1;
-
-            if(Indexes != null)
-            {
-                result = Indexes.Min();
-            }
-
-            return result;
-        }
-
-        public bool Contains(int idx)
-        {
-            bool result = false;
-
-            if(Indexes != null)
-            {
-                result = Indexes.Contains(idx);
-            }
-
-            return result;
-        }
-
         public int Count()
         {
             int result = -1;
@@ -77,11 +53,6 @@ namespace Dev2.Data.Binary_Objects
                 result = Indexes.Count;
             }
             return result;
-        }
-
-        public IIndexIterator FetchIterator()
-        {
-            return new ListIndexIterator(Indexes);
         }
     }
 }
