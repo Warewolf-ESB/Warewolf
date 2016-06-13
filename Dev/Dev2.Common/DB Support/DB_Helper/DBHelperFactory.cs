dd59b8129fbbfc94743ef3b913e61a43e3725970
Barney <barney.buchan@dev2.co.za>
2016-06-13 11:56:25 +0200

    Cleanup

--- Dev/Dev2.Common/DB Support/DB_Helper/DBHelperFactory.cs
+++ Dev/Dev2.Common/DB Support/DB_Helper/DBHelperFactory.cs
@@ -1,17 +1,18 @@
 /*
 *  Warewolf - The Easy Service Bus
 *  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
-*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using Dev2.Common.Interfaces.DB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Dev2.Common.Interfaces.DB;
+
 // ReSharper disable InconsistentNaming
 
 // ReSharper disable once CheckNamespace
@@ -25,12 +26,11 @@ namespace Dev2.Common.DB
         private static readonly Dictionary<enSupportedDBTypes, Type> _options
             = new Dictionary<enSupportedDBTypes, Type>();
 
-
         static DBHelperFactory()
         {
-            Type type = typeof (IDBHelper);
+            Type type = typeof(IDBHelper);
 
-            List<Type> types = typeof (IDBHelper).Assembly.GetTypes()
+            List<Type> types = typeof(IDBHelper).Assembly.GetTypes()
                 .Where(t => type.IsAssignableFrom(t)).ToList();
 
             foreach (Type t in types)
