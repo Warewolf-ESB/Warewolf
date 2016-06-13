9dd2b01634286e92625ef176fe56f96e3161edc6
Barney <barney.buchan@dev2.co.za>
2016-06-13 11:51:58 +0200

    Cleanup

--- Dev/Dev2.Common/Wrappers/DirectoryWrapper.cs
+++ Dev/Dev2.Common/Wrappers/DirectoryWrapper.cs
@@ -1,15 +1,15 @@
 /*
 *  Warewolf - The Easy Service Bus
 *  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
-*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System.IO;
 using Dev2.Common.Interfaces.Wrappers;
+using System.IO;
 
 namespace Dev2.Common.Wrappers
 { // not required for code coverage this is simply a pass through required for unit testing
