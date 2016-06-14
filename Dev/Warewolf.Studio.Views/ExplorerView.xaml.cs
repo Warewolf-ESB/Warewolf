ff3586cf210433627064e69ec237545572da4f57
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-06-14 09:58:13 +0200

    completed map complex objects from the browser into workflow

--- Dev/Warewolf.Studio.Views/ExplorerView.xaml.cs
+++ Dev/Warewolf.Studio.Views/ExplorerView.xaml.cs
@@ -2,7 +2,6 @@
 using System.Activities.Presentation;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
