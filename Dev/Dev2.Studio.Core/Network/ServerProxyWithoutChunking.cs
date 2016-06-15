b3209f01e4782d8b98fd889073efb9cba607e48f
Barney <barney.buchan@dev2.co.za>
2016-06-15 12:42:17 +0200

    Cleanup

--- Dev/Dev2.Studio.Core/Network/ServerProxyWithoutChunking.cs
+++ Dev/Dev2.Studio.Core/Network/ServerProxyWithoutChunking.cs
@@ -1,5 +1,4 @@
-﻿
-/*
+﻿/*
 *  Warewolf - Once bitten, there's no going back
 *  Copyright 2014 by Warewolf Ltd <alpha@warewolf.io>
 *  Licensed under GNU Affero General Public License 3.0 or later. 
