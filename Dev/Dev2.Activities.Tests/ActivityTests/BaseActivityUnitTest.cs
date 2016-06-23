55e9a5d0ac9264fa31346a23604748622f13de32
SalamiArmy <ultrakiff@gmail.com>
2016-06-23 14:48:07 +0200

    tests get better errors from the environment when resolving recordsets

--- Dev/Dev2.Activities.Tests/ActivityTests/BaseActivityUnitTest.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/BaseActivityUnitTest.cs
@@ -384,7 +384,7 @@ namespace ActivityUnitTests
                 {
                     variableName = DataListUtil.CreateRecordsetDisplayValue(recordSet, fieldNameToRetrieve, "*");
                 }
-                var warewolfEvalResult = environment.Eval(DataListUtil.AddBracketsToValueIfNotExist(variableName), 0);
+                var warewolfEvalResult = environment.Eval(DataListUtil.AddBracketsToValueIfNotExist(variableName), 0, true);
 
                 if (warewolfEvalResult == null)
                 {
