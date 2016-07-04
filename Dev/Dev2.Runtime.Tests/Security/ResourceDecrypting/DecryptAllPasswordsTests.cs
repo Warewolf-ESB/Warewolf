e29921c9d0e0f40cd2edba28262d3aced873be94
SalamiArmy <ultrakiff@gmail.com>
2016-07-04 08:40:56 +0200

    add failing unit test for decrypting server resources

---
+++ Dev/Dev2.Runtime.Tests/Security/ResourceDecrypting/DecryptAllPasswordsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text;
+using Dev2.Common.Common;
+using Dev2.Runtime.ESB.Management.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2.Tests.Runtime.Security
+{
+    [TestClass]
+    public class DecryptAllPasswordsTests
+    {
+        [TestMethod]
+        [Owner("Ashley Lewis")]
+        [TestCategory("FetchResourceDefinition_DecryptAllPasswords")]
+        public void FetchResourceDefinition_DecryptAllPasswords_ValidResource_PasswordHasBeenRemoved()
+        {
+            //------------Setup for test--------------------------
+            var fetchResourceDefinition = new FetchResourceDefinition();
+            var resourceDefinition = File.ReadAllText(@"Security\ResourceDecrypting\GenDev.xml");
+            var stringBuilder = new StringBuilder(resourceDefinition);
+
+            //------------Execute Test---------------------------
+            var plaintextResourceDefinition = fetchResourceDefinition.DecryptAllPasswords(stringBuilder);
+
+            //------------Assert Results-------------------------
+            Assert.IsTrue(plaintextResourceDefinition.Contains("rsaklfsvrgendev"), "Cannot decrypt resource definition.");
+        }
+    }
+}
