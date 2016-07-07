b35ed6c1433f8140092ed6e589db6dcac31adb6d
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-07-07 20:36:53 +0200

    WOLF-1903: Fixed resource upgrader

--- Dev/Dev2.Runtime.Tests/ResourceUpgraders/BaseUpgradeConvertorTests.cs
+++ Dev/Dev2.Runtime.Tests/ResourceUpgraders/BaseUpgradeConvertorTests.cs
@@ -20,15 +20,15 @@ namespace Dev2.Tests.Runtime.ResourceUpgraders
         [TestMethod]
         [Owner("Leon Rajindrapersadh")]
         [TestCategory("BaseResourceUpgrader_Upgrade")]
-// ReSharper disable InconsistentNaming
+        // ReSharper disable InconsistentNaming
         public void BaseResourceUpgrader_Upgrade_HasMatchin_ExpectReplace()
 
         {
             //------------Setup for test--------------------------
             var baseResourceUpgrader = new BaseResourceUpgrader();
-            
+
             //------------Execute Test---------------------------
-            Assert.AreEqual("<a>clr-namespace:Dev2.Common.Interfaces.Infrastructure.Providers.Errors;assembly=Dev2.CommonDataUtils.Interfaces</a>", baseResourceUpgrader.UpgradeFunc(XElement.Parse("<a>clr-namespace:Dev2.Providers.Errors;assembly=Dev2.Infrastructure</a>")).ToString());
+            Assert.AreEqual("<a>clr-namespace:Dev2.Common.Interfaces.Infrastructure.Providers.Errors;assembly=Dev2.Common.Interfaces</a>", baseResourceUpgrader.UpgradeFunc(XElement.Parse("<a>clr-namespace:Dev2.Providers.Errors;assembly=Dev2.Infrastructure</a>")).ToString());
             //------------Assert Results-------------------------
         }
 
@@ -42,7 +42,7 @@ namespace Dev2.Tests.Runtime.ResourceUpgraders
             var baseResourceUpgrader = new BaseResourceUpgrader();
 
             //------------Execute Test---------------------------
-            Assert.AreEqual("<a>clr-namespace:Dev2.Common.Interfaces.Core.Convertors.Case;assembly=Dev2.CommonDataUtils.Interfaces</a>", baseResourceUpgrader.UpgradeFunc(XElement.Parse("<a>clr-namespace:Dev2.Interfaces;assembly=Dev2.Core</a>")).ToString());
+            Assert.AreEqual("<a>clr-namespace:Dev2.Common.Interfaces.Core.Convertors.Case;assembly=Dev2.Common.Interfaces</a>", baseResourceUpgrader.UpgradeFunc(XElement.Parse("<a>clr-namespace:Dev2.Interfaces;assembly=Dev2.Core</a>")).ToString());
             //------------Assert Results-------------------------
         }
 
@@ -56,7 +56,7 @@ namespace Dev2.Tests.Runtime.ResourceUpgraders
             var baseResourceUpgrader = new BaseResourceUpgrader();
 
             //------------Execute Test---------------------------
-            Assert.AreEqual("<a>bob</a>", baseResourceUpgrader.UpgradeFunc( XElement.Parse("<a>bob</a>")).ToString());
+            Assert.AreEqual("<a>bob</a>", baseResourceUpgrader.UpgradeFunc(XElement.Parse("<a>bob</a>")).ToString());
             //------------Assert Results-------------------------
         }
         // ReSharper restore InconsistentNaming
