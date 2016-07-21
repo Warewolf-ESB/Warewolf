b3d5ae801cc93301cbdc1a08e032d57e8812c461
Barney <barney.buchan@dev2.co.za>
2016-07-21 15:57:22 +0200

    #WOLF-1857 Removed items only used by tests

--- Dev/Dev2.Studio.Core/Activities/Utils/ModelItemUtils.cs
+++ Dev/Dev2.Studio.Core/Activities/Utils/ModelItemUtils.cs
@@ -46,18 +46,6 @@ namespace Dev2.Studio.Core.Activities.Utils
             }
         }
 
-        public static ModelItem CreateModelItem(object parent, object objectToMakeModelItem)
-        {
-            EditingContext ec = new EditingContext();
-            ModelTreeManager mtm = new ModelTreeManager(ec);
-
-            return mtm.CreateModelItem(CreateModelItem(parent), objectToMakeModelItem);
-        }
-
-        public static ModelItem CreateModelItem()
-        {
-            return CreateModelItem(new object());
-        }
         public static ModelItem CreateModelItem(object objectToMakeModelItem)
         {
             EditingContext ec = new EditingContext();
