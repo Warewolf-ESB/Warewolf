4f8d0804663f4de0679935220cb09a7b1f23d6cf
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-07-22 10:59:12 +0200

    WOLF-1645: Designer

--- Dev/Dev2.Common/ExtMethods/StringExtension.cs
+++ Dev/Dev2.Common/ExtMethods/StringExtension.cs
@@ -50,7 +50,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsAlpha(this string payload)
         {
-            if (String.IsNullOrEmpty(payload))
+            if (string.IsNullOrEmpty(payload))
             {
                 return false;
             }
@@ -83,7 +83,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsWholeNumber(this string payload, out int value)
         {
-            if (Int32.TryParse(payload, out value))
+            if (int.TryParse(payload, out value))
             {
                 if (value >= 0)
                 {
@@ -103,7 +103,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsRealNumber(this string payload, out int value)
         {
-            return Int32.TryParse(payload, out value);
+            return int.TryParse(payload, out value);
         }
 
         /// <summary>
@@ -133,7 +133,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsNumeric(this string payload, out decimal value)
         {
-            if (String.IsNullOrEmpty(payload))
+            if (string.IsNullOrEmpty(payload))
             {
                 value = 0;
                 return false;
@@ -147,14 +147,14 @@ namespace Dev2.Common.ExtMethods
             }
 
             NumberFormatInfo current = CultureInfo.CurrentCulture.NumberFormat;
-            if (evalString.Any(c => !Char.IsDigit(c)
+            if (evalString.Any(c => !char.IsDigit(c)
                                     && c != current.NumberDecimalSeparator[0]))
             {
                 value = 0;
                 return false;
             }
 
-            return Decimal.TryParse(payload, out value);
+            return decimal.TryParse(payload, out value);
         }
 
         /// <summary>
@@ -166,7 +166,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsAlphaNumeric(this string payload)
         {
-            return !String.IsNullOrEmpty(payload) &&
+            return !string.IsNullOrEmpty(payload) &&
                    (IsAlpha(payload) || IsNumeric(payload) || IsAlphaNumericRegex.IsMatch(payload));
         }
 
@@ -179,7 +179,7 @@ namespace Dev2.Common.ExtMethods
         /// </returns>
         public static bool IsEmail(this string payload)
         {
-            if (String.IsNullOrEmpty(payload))
+            if (string.IsNullOrEmpty(payload))
             {
                 return false;
             }
