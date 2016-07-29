f68e5eaae46740855e09e9341d97b3b9b1a1a5d3
Barney <barney.buchan@dev2.co.za>
2016-07-29 14:43:46 +0200

    Revert "Removed Unused Classes 4"
    This reverts commit adf162c919b3b33451d8a91e71be7b00f465e6a5.

--- Dev/Dev2.Studio.Core/InterfaceImplementors/NetworkShareInformation.cs
+++ Dev/Dev2.Studio.Core/InterfaceImplementors/NetworkShareInformation.cs
@@ -29,5 +29,76 @@ namespace Dev2.InterfaceImplementors
 
     #endregion
 
+    #region Share
 
+    /// <summary>
+    ///     Information about a local share
+    /// </summary>
+    public class Share
+    {
+        #region Private data
+
+        readonly string _networkName;
+        readonly string _shareServer;
+        readonly ShareType _sharpType;
+
+        #endregion
+
+        #region Constructor
+
+
+        public Share(string server, string netName, ShareType shareType)
+        {
+            if(ShareType.Special == shareType && "IPC$" == netName)
+            {
+                shareType |= ShareType.Ipc;
+            }
+
+            _shareServer = server;
+            _networkName = netName;
+            _sharpType = shareType;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsFileSystem
+        {
+            get
+            {
+                if(0 != (_sharpType & ShareType.Device))
+                {
+                    return false;
+                }
+
+                if(0 != (_sharpType & ShareType.Ipc))
+                {
+                    return false;
+                }
+
+                if(0 == (_sharpType & ShareType.Special))
+                {
+                    return true;
+                }
+
+                return ShareType.Special == _sharpType && !string.IsNullOrEmpty(_networkName);
+            }
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format(@"\\{0}\{1}", string.IsNullOrEmpty(_shareServer)
+                    ? Environment.MachineName
+                    : _shareServer, _networkName);
+        }
+    }
+
+    #endregion
+
+    #region Share Collection
+
+    #endregion
 }
--- Dev/Warewolf.Studio.Views/ManageDatabaseSourceControl.xaml.cs
+++ Dev/Warewolf.Studio.Views/ManageDatabaseSourceControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -196,5 +197,22 @@ namespace Warewolf.Studio.Views
         {
             WindowsRadioButton.Focus();
         }
+    }
+
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
