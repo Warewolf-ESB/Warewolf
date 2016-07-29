d4c48abd6e62a22bb9724cca129e3605cee2c132
Barney <barney.buchan@dev2.co.za>
2016-07-29 13:36:56 +0200

    Removed Unused Classes 4

--- Dev/Dev2.Infrastructure/VerifyArgument.cs
+++ Dev/Dev2.Infrastructure/VerifyArgument.cs
@@ -206,66 +206,6 @@ namespace Dev2
     }
 
     /// <summary>
-    /// Should be used on attributes and causes ReSharper to not mark symbols marked with such attributes as unused (as well as by other usage inspections)
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public sealed class MeansImplicitUseAttribute : Attribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MeansImplicitUseAttribute"/> class.
-        /// </summary>
-        [UsedImplicitly]
-        public MeansImplicitUseAttribute()
-            : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MeansImplicitUseAttribute"/> class.
-        /// </summary>
-        /// <param name="useKindFlags">The use kind flags.</param>
-        /// <param name="targetFlags">The target flags.</param>
-        [UsedImplicitly]
-        public MeansImplicitUseAttribute(ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
-        {
-            UseKindFlags = useKindFlags;
-            TargetFlags = targetFlags;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MeansImplicitUseAttribute"/> class.
-        /// </summary>
-        /// <param name="useKindFlags">The use kind flags.</param>
-        [UsedImplicitly]
-        public MeansImplicitUseAttribute(ImplicitUseKindFlags useKindFlags)
-            : this(useKindFlags, ImplicitUseTargetFlags.Default)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MeansImplicitUseAttribute"/> class.
-        /// </summary>
-        /// <param name="targetFlags">The target flags.</param>
-        [UsedImplicitly]
-        public MeansImplicitUseAttribute(ImplicitUseTargetFlags targetFlags)
-            : this(ImplicitUseKindFlags.Default, targetFlags)
-        {
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [UsedImplicitly]
-        public ImplicitUseKindFlags UseKindFlags { get; private set; }
-
-        /// <summary>
-        /// Gets value indicating what is meant to be used
-        /// </summary>
-        [UsedImplicitly]
-        public ImplicitUseTargetFlags TargetFlags { get; private set; }
-    }
-
-    /// <summary>
     /// 
     /// </summary>
     [Flags]
