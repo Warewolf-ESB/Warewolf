2f16d4630675f2cd6ef96972d0c6dccd3800bcbf
Barney <barney.buchan@dev2.co.za>
2016-07-29 08:36:56 +0200

    #WOLF-1857 Removed unused methods and files

--- Dev/Dev2.Studio/Webs/Callbacks/Dev2DecisionCallbackHandler.cs
+++ Dev/Dev2.Studio/Webs/Callbacks/Dev2DecisionCallbackHandler.cs
@@ -8,7 +8,6 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
 using System.Windows;
 using Dev2.Interfaces;
 using Dev2.Studio.Core.Interfaces;
@@ -23,60 +22,6 @@ namespace Dev2.Webs.Callbacks
 
         public ILayoutObjectViewModel SelectedLayoutObject => null;
 
-        public void Save(string value, bool closeBrowserWindow = true)
-        {
-        }
-
-        public void OpenPropertyEditor()
-        {
-
-        }
-
-        public void Dev2Set(string data, string uri)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dev2SetValue(string value)
-        {
-            ModelData = value;
-            Close();
-        }
-
-        public void Dev2Done()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dev2ReloadResource(Guid resourceID, string resourceType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Close()
-        {
-            if(Owner != null)
-            {
-                Owner.Close();
-            }
-
-        }
-
-        public void Cancel()
-        {
-            Close();
-        }
-
-        public string GetIntellisenseResults(string searchTerm, int caretPosition)
-        {
-            return WebsiteCallbackHandler.GetJsonIntellisenseResults(searchTerm, caretPosition);
-        }
-
         public event NavigateRequestedEventHandler NavigateRequested;
-
-        public string FetchData(string args)
-        {
-            return ModelData;
-        }
     }
 }
