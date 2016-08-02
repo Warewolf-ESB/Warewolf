8e2bccc754b7a419d3cf2c8fa39a0faf01198969
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-08-02 11:48:40 +0200

    clean up and fixes from removal of code WOLF-1959

---
+++ Dev/Dev2.Activities/Activities/DsfWorkflowActivity.cs
@@ -0,0 +1,25 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+
+// ReSharper disable CheckNamespace
+
+using Dev2.Common.Interfaces.Toolbox;
+using Warewolf.Core;
+
+namespace Unlimited.Applications.BusinessDesignStudio.Activities
+// ReSharper restore CheckNamespace
+{
+    [ToolDescriptorInfo("Resources-Service", "Service", ToolType.Native, "8999E59A-38A3-43BB-A98F-6090C5C9EA1E", "Dev2.Acitivities", "1.0.0.0", "Legacy", "Resources", "/Warewolf.Studio.Themes.Luna;component/Images.xaml")]
+    public class DsfWorkflowActivity : DsfActivity
+    {        
+    }
+}
