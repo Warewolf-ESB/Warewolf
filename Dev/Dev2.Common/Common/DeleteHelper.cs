aa988f5350911fb87ac5ca74420e33b24f0c41ae
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-08-02 11:48:40 +0200

    clean up and fixes from removal of code WOLF-1959

---
+++ Dev/Dev2.Common/Common/DeleteHelper.cs
@@ -0,0 +1,80 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.IO;
+
+namespace Dev2.Common.Common
+{
+    public static class DeleteHelper
+    {
+        public static bool Delete(string path)
+        {
+            if (path == null)
+            {
+                return false;
+            }
+
+            string dirRoot = Path.GetDirectoryName(path);
+            string pattern = Path.GetFileName(path);
+            // directory
+            if (IsDirectory(path))
+            {
+                DirectoryHelper.CleanUp(path);
+                return true;
+            }
+
+            // wild-card char
+            if (path.IndexOf("*", StringComparison.Ordinal) >= 0)
+            {
+                if (dirRoot != null)
+                {
+                    string[] fileList = Directory.GetFileSystemEntries(dirRoot, pattern, SearchOption.TopDirectoryOnly);
+                    foreach (string file in fileList)
+                    {
+                        if (IsDirectory(file))
+                        {
+                            //it's a directory
+                            Directory.Delete(file, true);
+                        }
+                        else
+                        {
+                            // we can before, we want to avoid deleting an already deleted file in sub-directory
+                            File.Delete(file);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // single file delete
+                File.Delete(path);
+            }
+
+            return true;
+        }
+
+        private static bool IsDirectory(string path)
+        {
+            if (path.IndexOf("*", StringComparison.Ordinal) >= 0)
+            {
+                return false;
+            }
+
+            FileAttributes attr = File.GetAttributes(path);
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Dev/Dev2.Runtime.Services/Hosting/ResourceUpgrade.cs
@@ -0,0 +1,37 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Dev2.Common.Interfaces.Data;
+
+namespace Dev2.Runtime.Hosting
+{
+    public class ResourceUpgrade:IResourceUpgrade
+    {
+        public ResourceUpgrade( Func<XElement, XElement> upgradeFunc)
+        {
+            VerifyArgument.AreNotNull(new Dictionary<string, object> { { "upgradeFunc", upgradeFunc } });
+            UpgradeFunc = upgradeFunc;
+  
+        }
+
+        public Func<XElement, XElement> UpgradeFunc { get; private set; }
+
+        #region Implementation of IResourceUpgrade
+       
+
+
+
+
+        #endregion
+    }
+}
