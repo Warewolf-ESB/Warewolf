2eb9f2b9eba95860c9af41d0359d70bc3fe8e200
SalamiArmy <ultrakiff@gmail.com>
2016-08-04 08:45:18 +0200

    fix some paths in studio startup convenience script

--- Dev/Studio.Run.bat
+++ Dev/Studio.Run.bat
@@ -1,23 +1,23 @@
-"%~dp0TestScripts\Tests\NuGet.exe" restore "%~dp0Dev2.Server\Dev2.Server.csproj" -SolutionDirectory "%~dp0"
-"%~dp0TestScripts\Tests\NuGet.exe" restore "%~dp0Dev2.Studio\Dev2.Studio.csproj" -SolutionDirectory "%~dp0"
+"%~dp0TestScripts\Tests\NuGet.exe" restore "%~dp0Server.sln" -SolutionDirectory "%~dp0."
+"%~dp0TestScripts\Tests\NuGet.exe" restore "%~dp0Studio.sln" -SolutionDirectory "%~dp0."
 if not %errorlevel%==0 pause
 
 IF EXIST MSBuild (
-	MSBuild "%~dp0Dev2.Server\Dev2.Server.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	MSBuild "%~dp0Server.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 	if not %errorlevel%==0 pause & exit 1
-	MSBuild "%~dp0Dev2.Studio\Dev2.Studio.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	MSBuild "%~dp0Studio.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 ) else IF EXIST "%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" (
-	"%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0Dev2.Server\Dev2.Server.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0Server.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 	if not %errorlevel%==0 pause & exit 1
-	"%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0Dev2.Studio\Dev2.Studio.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0Studio.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 ) else IF EXIST "%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" (
-	"%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0Dev2.Server\Dev2.Server.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0Server.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 	if not %errorlevel%==0 pause & exit 1
-	"%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0Dev2.Studio\Dev2.Studio.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "%~dp0Studio.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 ) else IF EXIST "%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" (
-	"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0Dev2.Server\Dev2.Server.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0Server.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 	if not %errorlevel%==0 pause & exit 1
-	"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0Dev2.Studio\Dev2.Studio.csproj" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+	"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0Studio.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 )
 if not %errorlevel%==0 pause
 
