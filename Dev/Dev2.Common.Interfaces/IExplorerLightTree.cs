fbbd63bb24d24b85076bfa9faaf8f5306f37d7b1
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-10 15:40:23 +0200

    WOLF-1972: Added source and management services

---
+++ Dev/Dev2.Common.Interfaces/IExplorerLightTree.cs
@@ -0,0 +1,10 @@
+﻿namespace Dev2.Common.Interfaces
+{
+    public interface IExplorerLightTree
+    {
+        string IconPath { get; set; }
+        string ParentId { get; set; }
+        string ResourceId { get; set; }
+        string ResourceName { get; set; }
+    }
+}
