ffb1e4577b97a9fbb9c195bef7d45cbfc2cb1a85
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-11 12:16:42 +0200

    WIP: WOLF-1756

---
+++ Dev/Dev2.Runtime.Services/FileResource.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Dev2.Runtime
+{
+    public class FileResource
+    {
+        public string ResourcePath { get; set; }
+        public string ParentPath { get; set; }
+        public string Name { get; set; }
+        public List<FileResource> Children { get; set; }
+    }
+}
