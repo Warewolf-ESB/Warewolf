372bae10c7a10bd055505145f747464caaba8901
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-08-15 16:05:37 +0200

    Green Run tests

--- Dev/Dev2.Common/DuplicateResource.cs
+++ Dev/Dev2.Common/DuplicateResource.cs
@@ -1,9 +1,20 @@
 namespace Dev2.Common
 {
-    public class DuplicateResource
+
+    public interface IDuplicateResource
     {
+        string ResourceName { get; set; }
+        string FilePath { get; set; }
+        string FilePath2 { get; set; }
+    }
+    public class DuplicateResource:IDuplicateResource
+    {
+        #region Implementation of IDuplicateResource
+
         public string ResourceName { get; set; }
         public string FilePath { get; set; }
         public string FilePath2 { get; set; }
+
+        #endregion
     }
 }
