e7bbba12f3cc9675fd6cec7c4c7670b0fedaf206
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-08-18 16:14:28 +0200

    dont use custom error handler during test cleanup

--- Dev/Warewolf.UITests/Tools/Data/Assign.cs
+++ Dev/Warewolf.UITests/Tools/Data/Assign.cs
@@ -46,6 +46,7 @@ namespace Warewolf.UITests.Tools.Data
         [TestCleanup]
         public void MyTestCleanup()
         {
+            Playback.PlaybackError -= Uimap.OnError;
             Uimap.TryCloseHangingSaveDialog();
             Uimap.TryRemoveFromExplorer(WorkflowName);
             Uimap.TryClearToolboxFilter();
