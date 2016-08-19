b7d165a87db37599362c6efb9457837e6a03def7
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-08-19 10:25:17 +0200

    #WOLF-1715 Move code for duplicated resources check to App initialize

--- Dev/Warewolf.Studio.ViewModels/EnvironmentViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/EnvironmentViewModel.cs
@@ -725,12 +725,6 @@ namespace Warewolf.Studio.ViewModels
             {
                 IsConnecting = true;
                 var explorerItems = await Server.LoadExplorer(reloadCatalogue);
-                var loadExplorerDuplicates = await Server.LoadExplorerDuplicates();
-                if(loadExplorerDuplicates?.Count > 0)
-                {
-                    var controller = CustomContainer.Get<IPopupController>();
-                    controller.ShowResourcesConflict(loadExplorerDuplicates);
-                }
                 CreateExplorerItemsSync(explorerItems.Children, Server, this, selectedPath != null, Children.Any(a => AllowResourceCheck));
                 IsLoaded = true;
                 IsConnecting = false;
