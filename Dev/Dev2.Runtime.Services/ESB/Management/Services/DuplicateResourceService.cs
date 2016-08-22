8be677ffde623dd9a387598d780998090f372dd6
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-22 22:32:29 +0200

    WOLF-2004: Done For Non Folder resources

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/DuplicateResourceService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Dev2.Common;
+using Dev2.Common.Interfaces.Core.DynamicServices;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.DynamicServices.Objects;
+using Dev2.Runtime.Hosting;
+using Dev2.Runtime.Interfaces;
+using Dev2.Runtime.ServiceModel.Data;
+using Dev2.Workspaces;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    public class DuplicateResourceService : IEsbManagementEndpoint
+    {
+        private readonly IResourceCatalog _resourceCatalog;
+
+        public DuplicateResourceService(IResourceCatalog resourceCatalog)
+        {
+            _resourceCatalog = resourceCatalog;
+        }
+
+        public DuplicateResourceService()
+            : this(ResourceCatalog.Instance)
+        {
+
+        }
+
+        public StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+        {
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+
+            StringBuilder tmp;
+            StringBuilder newResourceName;
+            values.TryGetValue("ResourceID", out tmp);
+            values.TryGetValue("NewResourceName", out newResourceName);
+            var resourceId = Guid.Empty;
+
+            if (tmp != null)
+            {
+                if (Guid.TryParse(tmp.ToString(), out resourceId))
+                {
+                    if (!string.IsNullOrEmpty(newResourceName?.ToString()))
+                    {
+                        var resource = _resourceCatalog.GetResource(GlobalConstants.ServerWorkspaceID, resourceId);
+                        if (!resource.IsFolder)
+                        {
+                            var xElement = resource.ToXml();
+                            var newResourceXml = new XElement(xElement);
+                            //Allocate new ID
+                            var newGuid = Guid.NewGuid();
+                            var newResource = new Resource(newResourceXml)
+                            {
+                                ResourceName = newResourceName.ToString(),
+                                ResourceID = newGuid
+                            };
+                            //Allocate new Path
+                            _resourceCatalog.SaveResource(GlobalConstants.ServerWorkspaceID, newResource);
+                        }
+                    }
+                }
+            }
+            var result = new ExecuteMessage { HasError = false };
+            return serializer.SerializeToBuilder(result);
+        }
+
+        public string HandlesType()
+        {
+            return "DuplicateResourceService";
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            var deleteResourceService = new DynamicService
+            {
+                Name = HandlesType(),
+                DataListSpecification = new StringBuilder("<DataList><ResourceName ColumnIODirection=\"Input\"/><ResourceType ColumnIODirection=\"Input\"/><Roles ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>")
+            };
+
+            var deleteResourceAction = new ServiceAction
+            {
+                Name = HandlesType(),
+                ActionType = enActionType.InvokeManagementDynamicService,
+                SourceMethod = HandlesType()
+            };
+
+            deleteResourceService.Actions.Add(deleteResourceAction);
+
+            return deleteResourceService;
+        }
+    }
+}
---
+++ Dev/Dev2.Runtime.Tests/Services/DuplicateResourceServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dev2.Common;
+using Dev2.Common.Interfaces.Data;
+using Dev2.Runtime.ESB.Management.Services;
+using Dev2.Runtime.Interfaces;
+using Dev2.Runtime.ServiceModel.Data;
+using Dev2.Tests.Activities.XML;
+using Dev2.Workspaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Dev2.Tests.Runtime.Services
+{
+    [TestClass]
+    public class DuplicateResourceServiceTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void HandlesType_GivenServiceIsCreated_ShouldHandleCorrectly()
+        {
+            //---------------Set up test pack-------------------
+            var resourceCatalog = new Mock<IResourceCatalog>();
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(resourceService);
+            //---------------Execute Test ----------------------
+            var handlesType = resourceService.HandlesType();
+            //---------------Test Result -----------------------
+            Assert.AreEqual("DuplicateResourceService", handlesType);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void CreateServiceEntry_GivenServiceIsCreated_ShouldCreateCorrectDynamicService()
+        {
+            //---------------Set up test pack-------------------
+            var resourceCatalog = new Mock<IResourceCatalog>();
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(resourceService);
+            //---------------Execute Test ----------------------
+            var handlesType = resourceService.CreateServiceEntry();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, handlesType.Actions.Count);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Execute_GivenResourcePayLoad_ShouldExctactPayLoad()
+        {
+            //---------------Set up test pack-------------------
+            var resourceCatalog = new Mock<IResourceCatalog>();
+            var workScpace = new Mock<IWorkspace>();
+            var xElement = XML.XmlResource.Fetch("PluginService");
+            var pluginSource = new PluginSource(xElement);
+            var guid = "7B71D6B8-3E11-4726-A7A0-AC924977D6E5";
+            resourceCatalog.Setup(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, Guid.Parse(guid))).Returns(pluginSource);
+            resourceCatalog.Setup(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(resourceService);
+            //---------------Execute Test ----------------------
+
+            resourceService.Execute(new Dictionary<string, StringBuilder>
+            {
+                {"ResourceID", new StringBuilder(guid) },
+                {"NewResourceName", new StringBuilder("NewName") },
+            }, workScpace.Object);
+            //---------------Test Result -----------------------
+            resourceCatalog.Verify(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, Guid.Parse(guid)));
+            resourceCatalog.Setup(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
+        }
+    }
+}
