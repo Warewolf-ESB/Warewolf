48a94f177b616c32a3299b6c1faab6d6f6e6a19d
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-23 15:56:51 +0200

    WOLF-2003: Added DuplicateResourceViewModel for the View

--- Dev/Warewolf.Studio.Views/CreateDuplicateResourceDialog.xaml.cs
+++ Dev/Warewolf.Studio.Views/CreateDuplicateResourceDialog.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Warewolf.Studio.Views
+﻿namespace Warewolf.Studio.Views
 {
     /// <summary>
     /// Interaction logic for CreateDuplicateResourceDialog.xaml
     /// </summary>
-    public partial class CreateDuplicateResourceDialog : UserControl
+    public partial class CreateDuplicateResourceDialog 
     {
         public CreateDuplicateResourceDialog()
         {
