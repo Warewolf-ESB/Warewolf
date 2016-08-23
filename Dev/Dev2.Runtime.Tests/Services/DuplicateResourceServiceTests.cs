9fd4d527fb8bc1e1d068db4e5d128163750b690b
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-23 09:24:19 +0200

    WOLF-2004: WIP Duplicate Resources

--- Dev/Dev2.Runtime.Tests/Services/DuplicateResourceServiceTests.cs
+++ Dev/Dev2.Runtime.Tests/Services/DuplicateResourceServiceTests.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dev2.Common;
+using Dev2.Common.ExtMethods;
 using Dev2.Common.Interfaces.Data;
+using Dev2.Common.Interfaces.Explorer;
+using Dev2.Common.Interfaces.Infrastructure;
+using Dev2.Communication;
 using Dev2.Runtime.ESB.Management.Services;
 using Dev2.Runtime.Interfaces;
 using Dev2.Runtime.ServiceModel.Data;
@@ -24,7 +28,8 @@ namespace Dev2.Tests.Runtime.Services
         {
             //---------------Set up test pack-------------------
             var resourceCatalog = new Mock<IResourceCatalog>();
-            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            var serverExploer = new Mock<IExplorerServerResourceRepository>();
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object, serverExploer.Object);
             //---------------Assert Precondition----------------
             Assert.IsNotNull(resourceService);
             //---------------Execute Test ----------------------
@@ -39,7 +44,8 @@ namespace Dev2.Tests.Runtime.Services
         {
             //---------------Set up test pack-------------------
             var resourceCatalog = new Mock<IResourceCatalog>();
-            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            var serverExploer = new Mock<IExplorerServerResourceRepository>();
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object, serverExploer.Object);
             //---------------Assert Precondition----------------
             Assert.IsNotNull(resourceService);
             //---------------Execute Test ----------------------
@@ -54,13 +60,14 @@ namespace Dev2.Tests.Runtime.Services
         {
             //---------------Set up test pack-------------------
             var resourceCatalog = new Mock<IResourceCatalog>();
+            var serverExploer = new Mock<IExplorerServerResourceRepository>();
             var workScpace = new Mock<IWorkspace>();
             var xElement = XML.XmlResource.Fetch("PluginService");
             var pluginSource = new PluginSource(xElement);
             var guid = "7B71D6B8-3E11-4726-A7A0-AC924977D6E5";
             resourceCatalog.Setup(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, Guid.Parse(guid))).Returns(pluginSource);
             resourceCatalog.Setup(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
-            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object);
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object, serverExploer.Object);
             //---------------Assert Precondition----------------
             Assert.IsNotNull(resourceService);
             //---------------Execute Test ----------------------
@@ -73,6 +80,49 @@ namespace Dev2.Tests.Runtime.Services
             //---------------Test Result -----------------------
             resourceCatalog.Verify(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, Guid.Parse(guid)));
             resourceCatalog.Setup(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
+        }
+        Guid guid = "7B71D6B8-3E11-4726-A7A0-AC924977D6E5".ToGuid();
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Execute_GivenResourcePayLoadIFolder_ShouldSaveFolderWithNewName()
+        {
+            //---------------Set up test pack-------------------
+            var resourceCatalog = new Mock<IResourceCatalog>();
+            var serverExploer = new Mock<IExplorerServerResourceRepository>();
+            var explorerItem = new Mock<IExplorerItem>();
+            explorerItem.Setup(item => item.IsFolder).Returns(true);
+            explorerItem.Setup(item => item.ResourceId).Returns(guid);
+            var childXplorerItem = new Mock<IExplorerItem>();
+            childXplorerItem.Setup(item => item.IsFolder).Returns(true);
+            childXplorerItem.Setup(item => item.ResourceId).Returns(guid);
+            explorerItem.Setup(item => item.Children).Returns(new List<IExplorerItem>() { childXplorerItem.Object });
+            serverExploer.Setup(repository => repository.Find(It.IsAny<Guid>())).Returns(explorerItem.Object);
+            var workScpace = new Mock<IWorkspace>();
+            var folderResource = new Mock<IResource>();
+            folderResource.SetupGet(resource => resource.IsFolder).Returns(true);
+
+            resourceCatalog.Setup(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, guid)).Returns(folderResource.Object);
+            resourceCatalog.Setup(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
+            resourceCatalog.Setup(catalog => catalog.GetResourceList(GlobalConstants.ServerWorkspaceID, It.IsAny<Dictionary<string, string>>()))
+                .Returns(new List<Resource>());
+            DuplicateResourceService resourceService = new DuplicateResourceService(resourceCatalog.Object, serverExploer.Object);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(resourceService);
+            //---------------Execute Test ----------------------
+
+            var stringBuilder = resourceService.Execute(new Dictionary<string, StringBuilder>
+            {
+                {"ResourceID", new StringBuilder(guid.ToString()) },
+                {"NewResourceName", new StringBuilder("NewName") },
+            }, workScpace.Object);
+            //---------------Test Result -----------------------
+            resourceCatalog.Verify(catalog => catalog.GetResource(GlobalConstants.ServerWorkspaceID, guid));
+            resourceCatalog.Verify(catalog => catalog.SaveResource(GlobalConstants.ServerWorkspaceID, It.IsAny<IResource>(), It.IsAny<string>(), It.IsAny<string>()));
+            resourceCatalog.Verify(catalog => catalog.GetResourceList(GlobalConstants.ServerWorkspaceID, It.IsAny<Dictionary<string, string>>()));
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+            var executeMessage = serializer.Deserialize<ExecuteMessage>(stringBuilder);
+            Assert.IsNotNull(executeMessage);
+            Assert.IsFalse(executeMessage.HasError);
         }
     }
 }
