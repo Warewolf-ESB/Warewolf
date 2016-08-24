bdb62be32902d9828d17db170d27ac1a32a01781
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-08-24 10:31:07 +0200

    Code cleanup

--- Dev/Dev2.Runtime.Services/Hosting/ExplorerItemFactory.cs
+++ Dev/Dev2.Runtime.Services/Hosting/ExplorerItemFactory.cs
@@ -47,7 +47,7 @@ namespace Dev2.Runtime.Hosting
                 var res = new StringBuilder();
                 foreach (var path in duplicateResource.ResourcePath)
                     res.AppendLine(path);
-                var format = string.Format("ResourceName: {0}"+ Environment.NewLine + "{1}", duplicateResource.ResourceName, res);
+                var format = string.Format("Resource: {0}"+ Environment.NewLine + "{1}", duplicateResource.ResourceName, res);
                 duplicateList.Add(format);
             }
             return duplicateList;
