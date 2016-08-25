41b799270873f97b93f85f5297a78b34fc82954b
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-08-25 10:41:53 +0200

    WOLF-2003: Added view model tests

--- Dev/Warewolf.Studio.ViewModels/DuplicateResourceViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/DuplicateResourceViewModel.cs
@@ -6,13 +6,15 @@ using Dev2.Communication;
 using Dev2.Controller;
 using Dev2.Runtime.Configuration.ViewModels.Base;
 using Dev2.Studio.Core;
+using Dev2.Studio.Core.Interfaces;
 using Microsoft.Practices.Prism.Mvvm;
 
 namespace Warewolf.Studio.ViewModels
 {
     public class DuplicateResourceViewModel : BindableBase, IDuplicateResourceViewModel
     {
-        private ICreateDuplicateResourceView _createDuplicateResourceView;
+        private readonly ICommunicationController _controller;
+        private readonly ICreateDuplicateResourceView _createDuplicateResourceView;
 
         private string _newResourceName;
         public string NewResourceName
@@ -21,7 +23,8 @@ namespace Warewolf.Studio.ViewModels
             {
                 return _newResourceName;
             }
-            private set
+            // ReSharper disable once UnusedMember.Local
+            set
             {
                 _newResourceName = value;
                 OnPropertyChanged(() => NewResourceName);
@@ -36,7 +39,8 @@ namespace Warewolf.Studio.ViewModels
             {
                 return _fixReferences;
             }
-            private set
+            // ReSharper disable once UnusedMember.Local
+            set
             {
                 _fixReferences = value;
                 OnPropertyChanged(() => FixReferences);
@@ -48,26 +52,37 @@ namespace Warewolf.Studio.ViewModels
 
 
         public DuplicateResourceViewModel(ICreateDuplicateResourceView createDuplicateResourceView, Guid resourceId)
+            : this(new CommunicationController { ServiceName = "DuplicateResourceService" })
         {
             _createDuplicateResourceView = createDuplicateResourceView;
             _resourceId = resourceId;
+
+        }
+
+        public DuplicateResourceViewModel(ICommunicationController controller)
+        {
+            _controller = controller;
+
             CancelCommand = new DelegateCommand(CancelAndClose);
             CreateCommand = new DelegateCommand(CreateDuplicates);
         }
 
-
         private void CreateDuplicates(object obj)
         {
-            var comsController = new CommunicationController { ServiceName = "DuplicateResourceService" };
 
-            comsController.AddPayloadArgument("ResourceID", _resourceId.ToString());
-            comsController.AddPayloadArgument("NewResourceName", NewResourceName);
-            comsController.AddPayloadArgument("FixRefs", FixReferences.ToString());
-            var environmentConnection = EnvironmentRepository.Instance.ActiveEnvironment.Connection;
+            _controller.AddPayloadArgument("ResourceID", _resourceId.ToString());
+            _controller.AddPayloadArgument("NewResourceName", NewResourceName);
+            _controller.AddPayloadArgument("FixRefs", FixReferences.ToString());
+            if(obj == null)
+                obj = EnvironmentRepository.Instance.ActiveEnvironment;
+            var environmentConnection = EnvironmentConnection(obj as IEnvironmentModel);
             // ReSharper disable once UnusedVariable
-            var executeCommand = comsController.ExecuteCommand<ExecuteMessage>(environmentConnection, GlobalConstants.ServerWorkspaceID);
+            var executeCommand = _controller.ExecuteCommand<ExecuteMessage>(environmentConnection, GlobalConstants.ServerWorkspaceID);
             CancelAndClose(null);
+            EnvironmentRepository.Instance.Load();
         }
+
+        private static IEnvironmentConnection EnvironmentConnection(IEnvironmentModel model) => model.Connection;
 
         private void CancelAndClose(object obj)
         {
