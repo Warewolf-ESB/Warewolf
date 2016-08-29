2488d9ce0c634c435183399f823e302671940b60
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-08-29 11:13:04 +0200

    remove on error handler event when getting whether a control exists right now

--- Dev/Warewolf.UITests/UIMap.cs
+++ Dev/Warewolf.UITests/UIMap.cs
@@ -143,7 +143,9 @@ namespace Warewolf.UITests
         {
             Playback.PlaybackSettings.MaximumRetryCount = _strictMaximumRetryCount * int.Parse(Playback.PlaybackSettings.ThinkTimeMultiplier.ToString());
             Playback.PlaybackSettings.SearchTimeout = _strictSearchTimeout * int.Parse(Playback.PlaybackSettings.ThinkTimeMultiplier.ToString());
+            Playback.PlaybackError -= OnError;
             bool controlExists = thisControl.Exists;
+            Playback.PlaybackError += OnError;
             Playback.PlaybackSettings.MaximumRetryCount = _lenientMaximumRetryCount * int.Parse(Playback.PlaybackSettings.ThinkTimeMultiplier.ToString());
             Playback.PlaybackSettings.SearchTimeout = _lenientSearchTimeout * int.Parse(Playback.PlaybackSettings.ThinkTimeMultiplier.ToString());
             return controlExists;
