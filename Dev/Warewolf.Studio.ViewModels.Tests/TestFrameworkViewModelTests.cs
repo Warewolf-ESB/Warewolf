de188c6a02fc1df52d2c54129e5ae7aae97c49fd
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-09-05 14:29:58 +0200

    WOLF-2044: Added ViewModel for the test framework and Commands

---
+++ Dev/Warewolf.Studio.ViewModels.Tests/TestFrameworkViewModelTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable InconsistentNaming
+
+namespace Warewolf.Studio.ViewModels.Tests
+{
+    [TestClass]
+    public class TestFrameworkViewModelTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveRenameCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.RenameCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.RenameCommand.CanExecute());
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveSaveCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.SaveCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.SaveCommand.CanExecute());
+        }
+
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveEnableTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.EnableTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.EnableTestCommand.CanExecute());
+        }
+
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveDisableTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.DisableTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.DisableTestCommand.CanExecute());
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveDeleteTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.DeleteTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.DeleteTestCommand.CanExecute());
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveDuplicateTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.DuplicateTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.DuplicateTestCommand.CanExecute());
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveRunTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.RunTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.RunTestCommand.CanExecute());
+        }
+
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldHaveWebRunTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.WebRunTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.WebRunTestCommand.CanExecute());
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void OnCreation_GivenIsNew_ShouldStopTestCommand()
+        {
+            //---------------Set up test pack-------------------
+            var vm = new TestFrameworkViewModel();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(vm);
+            //---------------Execute Test ----------------------
+            Assert.IsNotNull(vm.StopTestCommand);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(vm.StopTestCommand.CanExecute());
+        }
+    }
+}
---
+++ Dev/Warewolf.Studio.ViewModels/TestFrameworkViewModel.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.Practices.Prism.Commands;
+
+namespace Warewolf.Studio.ViewModels
+{
+    public class TestFrameworkViewModel
+    {
+        public TestFrameworkViewModel()
+        {
+            RenameCommand = new DelegateCommand(RenameAction, () => CanRename);
+            SaveCommand = new DelegateCommand(SaveAction, () => CanSave);
+            EnableTestCommand = new DelegateCommand(EnableTestAction, () => CanEnableTest);
+            DisableTestCommand = new DelegateCommand(DisableTestAction, () => CanDisableTest);
+            DeleteTestCommand = new DelegateCommand(DeleteTestAction, () => CanDeleteTest);
+            DuplicateTestCommand = new DelegateCommand(DuplicateTestAction, () => CanDuplicateTest);
+            RunTestCommand = new DelegateCommand(RunTestAction, () => CanRunTest);
+            WebRunTestCommand = new DelegateCommand(WebRunTestAction, () => CanWebRunTest);
+            StopTestCommand = new DelegateCommand(StopTestAction, () => CanStopTest);
+        }
+
+        public bool CanStopTest { get; set; }
+
+        private void StopTestAction()
+        {
+        }
+
+        public bool CanWebRunTest { get; set; }
+
+        private void WebRunTestAction()
+        {
+        }
+
+        public bool CanRunTest { get; set; }
+
+        private void RunTestAction()
+        {
+        }
+
+        public bool CanDuplicateTest { get; set; }
+
+        private void DuplicateTestAction()
+        {
+        }
+
+        public bool CanDeleteTest { get; set; }
+
+        private void DeleteTestAction()
+        {
+        }
+
+        #region Commands
+
+        public bool CanDisableTest { get; set; }
+
+        private void DisableTestAction()
+        {
+        }
+
+        public bool CanEnableTest { get; set; }
+
+        private void EnableTestAction()
+        {
+        }
+
+        public bool CanSave { get; set; }
+
+        private void SaveAction()
+        {
+        }
+
+        public bool CanRename { get; set; }
+
+        private void RenameAction()
+        {
+        }
+
+
+
+        public DelegateCommand RenameCommand { get; set; }
+        public DelegateCommand SaveCommand { get; set; }
+        public DelegateCommand EnableTestCommand { get; set; }
+        public DelegateCommand DisableTestCommand { get; set; }
+        public DelegateCommand DeleteTestCommand { get; set; }
+        public DelegateCommand DuplicateTestCommand { get; set; }
+        public DelegateCommand RunTestCommand { get; set; }
+        public DelegateCommand WebRunTestCommand { get; set; }
+        public DelegateCommand StopTestCommand { get; set; }
+
+        #endregion
+
+    }
+}
