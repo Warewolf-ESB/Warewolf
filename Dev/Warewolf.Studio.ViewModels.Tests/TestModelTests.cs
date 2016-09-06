a373cc5aa74b6df082fa3d47bbe8d232ae3e1408
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-09-06 16:19:41 +0200

    #WOLF-2050 Change Input and Output controls. Add bool properties

--- Dev/Warewolf.Studio.ViewModels.Tests/TestModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/TestModelTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Dev2.Common.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 // ReSharper disable InconsistentNaming
 
 namespace Warewolf.Studio.ViewModels.Tests
@@ -16,7 +18,7 @@ namespace Warewolf.Studio.ViewModels.Tests
             var _wasCalled = false;
             testModel.PropertyChanged += (sender, args) =>
               {
-                  if (args.PropertyName == "Name")
+                  if (args.PropertyName == "TestName")
                   {
                       _wasCalled = true;
                   }
@@ -38,7 +40,7 @@ namespace Warewolf.Studio.ViewModels.Tests
             var _wasCalled = false;
             testModel.PropertyChanged += (sender, args) =>
             {
-                if (args.PropertyName == "Username")
+                if (args.PropertyName == "UserName")
                 {
                     _wasCalled = true;
                 }
@@ -47,6 +49,138 @@ namespace Warewolf.Studio.ViewModels.Tests
             testModel.UserName = "theUser";
             //------------Assert Results-------------------------
             Assert.AreEqual("theUser", testModel.UserName);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_TestPassed")]
+        public void TestModel_TestPassed_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "TestPassed")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.TestPassed = true;
+            //------------Assert Results-------------------------
+            Assert.AreEqual(true, testModel.TestPassed);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_TestFailing")]
+        public void TestModel_TestFailing_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "TestFailing")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.TestFailing = true;
+            //------------Assert Results-------------------------
+            Assert.AreEqual(true, testModel.TestFailing);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_TestInvalid")]
+        public void TestModel_TestInvalid_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "TestInvalid")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.TestInvalid = true;
+            //------------Assert Results-------------------------
+            Assert.AreEqual(true, testModel.TestInvalid);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_TestPending")]
+        public void TestModel_TestPending_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "TestPending")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.TestPending = true;
+            //------------Assert Results-------------------------
+            Assert.AreEqual(true, testModel.TestPending);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_Inputs")]
+        public void TestModel_Inputs_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Inputs")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.Inputs = new List<ITestInput>();
+            //------------Assert Results-------------------------
+            Assert.AreEqual(0, testModel.Inputs.Count);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("TestModel_Outputs")]
+        public void TestModel_Outputs_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var testModel = new TestModel();
+            var _wasCalled = false;
+            testModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Outputs")
+                {
+                    _wasCalled = true;
+                }
+            };
+            //------------Execute Test---------------------------
+            testModel.Outputs = new List<ITestOutput>();
+            //------------Assert Results-------------------------
+            Assert.AreEqual(0, testModel.Outputs.Count);
             Assert.IsTrue(_wasCalled);
         }
     }
--- Dev/Warewolf.Studio.ViewModels/TestModel.cs
+++ Dev/Warewolf.Studio.ViewModels/TestModel.cs
@@ -9,6 +9,10 @@ namespace Warewolf.Studio.ViewModels
     {
         private string _testName;
         private string _userName;
+        private bool _testPassed;
+        private bool _testFailing;
+        private bool _testPending;
+        private bool _testInvalid;
 
         #region Implementation of ITestModel
 
@@ -35,10 +39,50 @@ namespace Warewolf.Studio.ViewModels
         public string Password { get; set; }
         public DateTime LastRunDate { get; set; }
         public List<ITestInput> Inputs { get; set; }
-        public List<ITestOutput> OutPuts { get; set; }
+        public List<ITestOutput> Outputs { get; set; }
         public bool Error { get; set; }
         public bool IsNewTest { get; set; }
         public bool IsTestSelected { get; set; }
+
+        public bool TestPassed
+        {
+            get { return _testPassed; }
+            set
+            {
+                _testPassed = value;
+                OnPropertyChanged(() => TestPassed);
+            }
+        }
+
+        public bool TestFailing
+        {
+            get { return _testFailing; }
+            set
+            {
+                _testFailing = value;
+                OnPropertyChanged(() => TestFailing);
+            }
+        }
+
+        public bool TestInvalid
+        {
+            get { return _testInvalid; }
+            set
+            {
+                _testInvalid = value;
+                OnPropertyChanged(() => TestInvalid);
+            }
+        }
+
+        public bool TestPending
+        {
+            get { return _testPending; }
+            set
+            {
+                _testPending = value;
+                OnPropertyChanged(() => TestPending);
+            }
+        }
 
         #endregion
     }
