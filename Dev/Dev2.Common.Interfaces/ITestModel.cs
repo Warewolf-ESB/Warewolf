88ff400711519fd24027300f145d89c311a6a06c
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-09-06 16:19:41 +0200

    #WOLF-2050 Change Input and Output controls. Add bool properties

--- Dev/Dev2.Common.Interfaces/ITestModel.cs
+++ Dev/Dev2.Common.Interfaces/ITestModel.cs
@@ -10,10 +10,14 @@ namespace Dev2.Common.Interfaces
         string Password { get; set; }
         DateTime LastRunDate { get; set; }
         List<ITestInput> Inputs { get; set; }
-        List<ITestOutput> OutPuts { get; set; }
+        List<ITestOutput> Outputs { get; set; }
         bool Error { get; set; }
         bool IsNewTest { get; set; }
         bool IsTestSelected { get; set; }
+        bool TestPassed { get; set; }
+        bool TestFailing { get; set; }
+        bool TestInvalid { get; set; }
+        bool TestPending { get; set; }
     }
 
     public interface ITestInput
--- Dev/Warewolf.Studio.Views/TestFrameworkView.xaml.cs
+++ Dev/Warewolf.Studio.Views/TestFrameworkView.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Dev2.Common.Interfaces;
+using Dev2.UI;
+using Infragistics.Controls.Grids;
+using Infragistics.Controls.Grids.Primitives;
 
 namespace Warewolf.Studio.Views
 {
@@ -10,6 +19,87 @@ namespace Warewolf.Studio.Views
         public TestFrameworkView()
         {
             InitializeComponent();
+        }
+
+        private void TestInputs_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (TestInputs?.Rows != null && TestInputs.Rows.Count > 0)
+            {
+                var cellBaseCollection = TestInputs.Rows[0].Cells;
+                if (cellBaseCollection != null)
+                {
+                    var selectedCell = (Cell)cellBaseCollection[1];
+                    TestInputs.ActiveCell = selectedCell;
+                }
+            }
+            FocusOnAddition();
+        }
+
+        private void FocusOnAddition()
+        {
+            try
+            {
+                var row = GetSelectedRow(TestInputs);
+                if (row != null)
+                {
+                    var intelbox = FindByName("txtValue", row) as IntellisenseTextBox;
+                    intelbox?.Focus();
+                }
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+
+        static CellsPanel GetSelectedRow(XamGrid grid)
+        {
+            var row = grid.ActiveCell?.Row;
+            return row?.Control;
+        }
+
+        private static FrameworkElement FindByName(string name, FrameworkElement root)
+        {
+            if (root != null)
+            {
+                var tree = new Stack<FrameworkElement>();
+                tree.Push(root);
+                while (tree.Count > 0)
+                {
+                    var current = tree.Pop();
+                    if (current.Name == name)
+                        return current;
+
+                    var count = VisualTreeHelper.GetChildrenCount(current);
+                    for (var supplierCounter = 0; supplierCounter < count; ++supplierCounter)
+                    {
+                        var child = VisualTreeHelper.GetChild(current, supplierCounter);
+                        var item = child as FrameworkElement;
+                        if (item != null)
+                            tree.Push(item);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void GridPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            UIElement keyboardFocus = Keyboard.FocusedElement as TextBox;
+            if (e.KeyboardDevice.IsKeyDown(Key.LeftShift) && e.KeyboardDevice.IsKeyDown(Key.Tab))
+            {
+                keyboardFocus?.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+            }
+            if (e.KeyboardDevice.IsKeyDown(Key.Tab))
+            {
+                var vm = DataContext as ITestViewModel;
+                //var itemToSelect = vm?.GetNextRow(TestInputs.ActiveItem as ITestModel);
+                //if (itemToSelect != null)
+                //{
+                //    TestInputs.ActiveItem = itemToSelect;
+                //    FocusOnAddition();
+                //}
+            }
         }
     }
 }
