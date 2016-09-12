53c6ee738a147ef7e236d9727ea162dcd7d27beb
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-12 17:29:19 +0200

    Configure/Settings Menu button mappings

---
+++ Dev/Warewolf.UITests/ConfigureSettings/ConfigureSettingsTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests
+{
+    [CodedUITest]
+    public class ConfigureSettingsTests
+    {
+        [TestMethod]
+        public void ConfigureSetting()
+        {
+            Uimap.Click_ConfigureSetting_From_Menu();
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            Uimap.SetGlobalPlaybackSettings();
+            Uimap.WaitForStudioStart();
+            Console.WriteLine("Test \"" + TestContext.TestName + "\" starting on " + System.Environment.MachineName);
+        }
+        
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            Playback.PlaybackError -= Uimap.OnError;
+            Uimap.TryCloseAllTabs();
+        }
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private TestContext testContextInstance;
+
+        UIMap Uimap
+        {
+            get
+            {
+                if (_uiMap == null)
+                {
+                    _uiMap = new UIMap();
+                }
+
+                return _uiMap;
+            }
+        }
+
+        private UIMap _uiMap;
+
+        #endregion
+    }
+}
