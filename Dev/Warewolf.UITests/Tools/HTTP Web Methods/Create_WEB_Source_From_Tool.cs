0c3bb00301337510abbe93d05397207933205a57
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-12 13:47:08 +0200

    Adding WebSource from the tool mapping and test

---
+++ Dev/Warewolf.UITests/Tools/HTTP Web Methods/Create_WEB_Source_From_Tool.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.Tools
+{
+    [CodedUITest]
+    public class Create_WEB_Source_From_Tool
+    {
+        const string WebSourceName = "UITestingWebSourceFromTool";
+        [TestMethod]
+        [TestCategory("Tools")]
+        public void WebSourceFromTool()
+        {
+            Uimap.Drag_GET_Web_Connector_Onto_DesignSurface();
+            Uimap.Open_GET_Web_Connector_Tool_Large_View();
+            Uimap.Click_AddNew_WebSource_From_GET_Large_View();
+            Uimap.Type_TestSite_into_Web_Source_Wizard_Address_Textbox();
+            Uimap.Save_With_Ribbon_Button_And_Dialog(WebSourceName);
+            //Uimap.Click_Close_Web_Source_Wizard_Tab_Button();
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            Uimap.SetGlobalPlaybackSettings();
+            Uimap.WaitForStudioStart();
+            Console.WriteLine("Test \"" + TestContext.TestName + "\" starting on " + System.Environment.MachineName);
+            Uimap.InitializeABlankWorkflow();
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            Playback.PlaybackError -= Uimap.OnError;
+            Uimap.TryCloseHangingSaveDialog();
+            Uimap.TryRemoveFromExplorer(WebSourceName);
+            Uimap.TryCloseWorkflowTabs();
+            Uimap.TryCloseNewWebSourceWizardTab();
+        }
+
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private TestContext testContextInstance;
+
+        UIMap Uimap
+        {
+            get
+            {
+                if ((_uiMap == null))
+                {
+                    _uiMap = new UIMap();
+                }
+
+                return _uiMap;
+            }
+        }
+
+        private UIMap _uiMap;
+
+        #endregion
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
