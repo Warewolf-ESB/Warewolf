36bc75adcbe61410d956a2b38ab6807c83883a15
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-12 13:59:59 +0200

    Code clean up.. Unused references

--- Dev/Warewolf.UITests/UIMap.Designer.cs
+++ Dev/Warewolf.UITests/UIMap.Designer.cs
@@ -10,11 +10,8 @@
 
 namespace Warewolf.UITests
 {
-    using System;
     using System.CodeDom.Compiler;
-    using System.Collections.Generic;
     using System.Drawing;
-    using System.Text.RegularExpressions;
     using System.Windows.Input;
     using Microsoft.VisualStudio.TestTools.UITest.Extension;
     using Microsoft.VisualStudio.TestTools.UITesting;
@@ -25,8 +22,8 @@ namespace Warewolf.UITests
     using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
     using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
     using MouseButtons = System.Windows.Forms.MouseButtons;
-    
-    
+
+
     [GeneratedCode("Coded UITest Builder", "14.0.23107.0")]
     public partial class UIMap
     {
