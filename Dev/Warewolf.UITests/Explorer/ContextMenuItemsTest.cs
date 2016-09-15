e7e734c74c311430566acd323de7d8a6f61ff2b5
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-15 08:22:45 +0200

    Uncomented test steps

--- Dev/Warewolf.UITests/Explorer/ContextMenuItemsTest.cs
+++ Dev/Warewolf.UITests/Explorer/ContextMenuItemsTest.cs
@@ -14,34 +14,30 @@ namespace Warewolf.UITests
         [TestMethod]
         public void ContextMenuItemsUITest()
         {
+            Uimap.CreateAndSave_Dice_Workflow();
+
+            Uimap.Click_New_Workflow_Ribbon_Button();
+            Uimap.Click_Explorer_Refresh_Button();
+            Uimap.Drag_Dice_Onto_DesignSurface();
+            Uimap.Drag_Dice_Onto_Dice_On_The_DesignSurface();
+            Uimap.Click_Workflow_CollapseAll();
+            Uimap.Save_With_Ribbon_Button_And_Dialog(DoubleDice);
+            Uimap.Click_Close_Workflow_Tab_Button();
+            Uimap.Click_Explorer_Refresh_Button();
+            Uimap.RightClick_Dice();
+            Uimap.Click_ViewSwagger_From_ExplorerContextMenu();
+            Uimap.RightClick_Dice();
+            Uimap.Select_Show_Version_History();
             Uimap.RightClick_Dice();
             Uimap.Select_Rename_FromExplorerContextMenu();
             Uimap.Rename_LocalWorkflow_To_SecodWorkFlow();
-
-            //Uimap.CreateAndSave_Dice_Workflow();            
-
-            //Uimap.Click_New_Workflow_Ribbon_Button();
-            //Uimap.Click_Explorer_Refresh_Button();
-            //Uimap.Drag_Dice_Onto_DesignSurface();
-            //Uimap.Drag_Dice_Onto_Dice_On_The_DesignSurface();
-            //Uimap.Click_Workflow_CollapseAll();
-            //Uimap.Save_With_Ribbon_Button_And_Dialog(DoubleDice);
-            //Uimap.Click_Close_Workflow_Tab_Button();
-            //Uimap.Click_Explorer_Refresh_Button();
-            //Uimap.RightClick_Dice();
-            //Uimap.Click_ViewSwagger_From_ExplorerContextMenu();
-            //Uimap.RightClick_Dice();
-            //Uimap.Select_Show_Version_History();
-            ////Uimap.RightClick_Dice();
-            ////Uimap.Select_Rename_FromExplorerContextMenu();
-            ////Uimap.Rename_LocalWorkflow_To_SecodWorkFlow();
-            //Uimap.RightClick_Dice();
-            //Uimap.Click_Duplicate_From_ExplorerContextMenu();
-            //Uimap.Enter_Duplicate_workflow_name();
-            //Uimap.Click_UpdateDuplicateRelationships();
-            //Uimap.Click_Duplicate_From_Duplicate_Dialog();
-            //Uimap.RightClick_Dice();
-            //Uimap.Select_Show_Dependencies_In_Explorer_Context_Menu(DoubleDice);
+            Uimap.RightClick_Dice();
+            Uimap.Click_Duplicate_From_ExplorerContextMenu();
+            Uimap.Enter_Duplicate_workflow_name();
+            Uimap.Click_UpdateDuplicateRelationships();
+            Uimap.Click_Duplicate_From_Duplicate_Dialog();
+            Uimap.RightClick_Dice();
+            Uimap.Select_Show_Dependencies_In_Explorer_Context_Menu(DoubleDice);
             //Uimap.Click_Close_Dependecy_Tab();
         }
 
