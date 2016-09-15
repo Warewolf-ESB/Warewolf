045621676fcc30524d65766cc72fe254a35f0e56
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-15 08:21:09 +0200

    Mappings for Post Click create a new test 2

--- Dev/Warewolf.UITests/WorkflowTesting/WorkflowTestingTests.cs
+++ Dev/Warewolf.UITests/WorkflowTesting/WorkflowTestingTests.cs
@@ -10,6 +10,7 @@ namespace Warewolf.UITests
         [TestMethod]
         public void WorkflowTestingUITests()
         {
+            //Uimap.CreateAndSave_Dice_Wokflow();
             Uimap.Click_Create_New_Tests();
         }
 
