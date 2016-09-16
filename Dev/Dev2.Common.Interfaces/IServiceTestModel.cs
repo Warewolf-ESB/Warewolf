13bd50919534e924ed2bed8c981ba8a33dcc718d
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-09-16 09:11:44 +0200

    twoway mode

--- Dev/Dev2.Common.Interfaces/IServiceTestModel.cs
+++ Dev/Dev2.Common.Interfaces/IServiceTestModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows;
 using Dev2.Common.Interfaces.Diagnostics.Debug;
 using Dev2.Runtime.ServiceModel.Data;
 
