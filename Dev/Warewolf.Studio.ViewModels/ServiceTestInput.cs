ea754c663949dccaeb8c7c628f99a40fc7ec7ce6
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-09-16 12:19:22 +0200

    Fixed inputs and outputs changes

--- Dev/Warewolf.Studio.ViewModels/ServiceTestInput.cs
+++ Dev/Warewolf.Studio.ViewModels/ServiceTestInput.cs
@@ -65,5 +65,20 @@ namespace Warewolf.Studio.ViewModels
         public Action AddNewAction { get; set; }
 
         #endregion
+
+        #region Implementation of ICloneable
+
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a copy of this instance.
+        /// </returns>
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        #endregion
     }
 }
