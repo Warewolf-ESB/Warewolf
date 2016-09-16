38be1098bd269f87e09a66296209bdac5bac2ff3
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-09-16 13:45:53 +0200

    Fixed cloning on duplicates

--- Dev/Dev2.Data/ServiceTestInputTO.cs
+++ Dev/Dev2.Data/ServiceTestInputTO.cs
@@ -7,20 +7,6 @@ namespace Dev2.Data
         public string Variable { get; set; }
         public string Value { get; set; }
         public bool EmptyIsNull { get; set; }
-
-        #region Implementation of ICloneable
-
-        /// <summary>
-        /// Creates a new object that is a copy of the current instance.
-        /// </summary>
-        /// <returns>
-        /// A new object that is a copy of this instance.
-        /// </returns>
-        public object Clone()
-        {
-            return MemberwiseClone();
-        }
-
-        #endregion
+      
     }
 }
