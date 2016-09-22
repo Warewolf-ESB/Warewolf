4d4a363f0ad910cc5a867d7442c0115e15b56247
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-09-22 08:38:18 +0200

    #WOLF-2058 Fix small and large view templates and cleanup

--- Dev/Dev2.Activities.Designers/Designers2/DeleteRecordsNullHandler/DeleteRecordsDesignerViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/DeleteRecordsNullHandler/DeleteRecordsDesignerViewModel.cs
@@ -31,10 +31,7 @@ namespace Dev2.Activities.Designers2.DeleteRecordsNullHandler
         public override void UpdateHelpDescriptor(string helpText)
         {
             var mainViewModel = CustomContainer.Get<IMainViewModel>();
-            if (mainViewModel != null)
-            {
-                mainViewModel.HelpViewModel.UpdateHelpText(helpText);
-            }
+            mainViewModel?.HelpViewModel.UpdateHelpText(helpText);
         }
     }
 }
