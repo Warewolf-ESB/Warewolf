5c9b426f08d8b9e0609990173ff65975e7230ccd
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-09-22 11:37:30 +0200

    WOLF-2105: Added ruby tool

---
+++ Dev/Dev2.Activities.Designers/Designers2/Script_Ruby/RubyDesignerViewModel.cs
@@ -0,0 +1,75 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2016 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System.Activities.Presentation.Model;
+using System.Windows;
+using System.Windows.Input;
+using Caliburn.Micro;
+using Dev2.Activities.Designers2.Core;
+using Dev2.Interfaces;
+using Dev2.Runtime.Configuration.ViewModels.Base;
+using Dev2.Services.Events;
+
+namespace Dev2.Activities.Designers2.Script_Ruby
+{
+    public class RubyDesignerViewModel : ActivityDesignerViewModel
+    {
+        readonly IEventAggregator _eventPublisher;
+        readonly IScriptChooser _scriptChooser;
+        public RubyDesignerViewModel(ModelItem modelItem)
+            : base(modelItem)
+        {
+            _eventPublisher = EventPublishers.Aggregator;
+            _eventPublisher.Subscribe(this);
+
+            EscapeScript = true;
+            ChooseScriptSourceCommand = new DelegateCommand(o => ChooseScriptSources());
+            AddTitleBarLargeToggle();
+            _scriptChooser = new ScriptChooser();
+        }
+
+        public string IncludeFile
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
+
+        public bool EscapeScript { get; private set; }
+
+
+        public ICommand ChooseScriptSourceCommand { get; private set; }
+
+        public string ScriptTypeDefaultText
+        {
+            get { return (string)GetValue(ScriptTypeTextProperty); }
+            set { SetValue(ScriptTypeTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScriptTypeTextProperty = DependencyProperty.Register("ScriptTypeDefaultText", typeof(string), typeof(RubyDesignerViewModel), new PropertyMetadata("Ruby Syntax"));
+
+
+        public override void Validate()
+        {
+        }
+
+        public override void UpdateHelpDescriptor(string helpText)
+        {
+            var mainViewModel = CustomContainer.Get<IMainViewModel>();
+            mainViewModel?.HelpViewModel.UpdateHelpText(helpText);
+        }
+
+        public void ChooseScriptSources()
+        {            
+            var fileChooserMessage = _scriptChooser.ChooseScriptSources(IncludeFile);
+            _eventPublisher.Publish(fileChooserMessage);
+        }
+
+    }
+}
