dc67c833c4cbf3a98339b7c1f6129769669a3a54
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-09-23 11:00:28 +0200

    commented out code in my last commit by mistake

--- Dev/Warewolf.UITests/Explorer/RemoteSubworkflow.cs
+++ Dev/Warewolf.UITests/Explorer/RemoteSubworkflow.cs
@@ -6,27 +6,27 @@ namespace Warewolf.UITests
     [CodedUITest]
     public class RemoteSubworkflow
     {
-        //const string ServerSourceName = "TSTCIREMOTE";        
+        const string ServerSourceName = "TSTCIREMOTE";
         const string LocalWorkflowName = "RemoteServerUITestWorkflow";
-        //const string RemoteSubWorkflowName = "workflow1";
+        const string RemoteSubWorkflowName = "workflow1";
         const string WindowsGroup = "Domain Users";
-        //private const string ServerAddress = "tst-ci-";
+        private const string ServerAddress = "tst-ci-";
 
         [TestMethod]
         public void BigRemoteSubworkflowUITest()
         {
-            //Uimap.Click_New_Workflow_Ribbon_Button();
-            //Uimap.Select_NewRemoteServer_From_Explorer_Server_Dropdownlist();
-            //Uimap.CreateRemoteServerSource(ServerSourceName, ServerAddress);
-            //Uimap.Enter_Text_Into_Explorer_Filter(ServerSourceName);
-            //Uimap.WaitForSpinner(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.Checkbox.Spinner);
-            //Assert.IsTrue(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.Exists);
-            //Uimap.Select_From_Explorer_Remote_Server_Dropdown_List(Uimap.MainStudioWindow.ComboboxListItemAsTSTCIREMOTE);
-            //Uimap.Click_Explorer_RemoteServer_Connect_Button();
-            //Uimap.WaitForControlNotVisible(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.FirstRemoteServer.Checkbox.Spinner);
-            //Uimap.Enter_Text_Into_Explorer_Filter(RemoteSubWorkflowName);
-            //Uimap.WaitForControlNotVisible(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.FirstRemoteServer.Checkbox.Spinner);
-            //Uimap.TryRefreshExplorerUntilOneItemOnly();
+            Uimap.Click_New_Workflow_Ribbon_Button();
+            Uimap.Select_NewRemoteServer_From_Explorer_Server_Dropdownlist();
+            Uimap.CreateRemoteServerSource(ServerSourceName, ServerAddress);
+            Uimap.Enter_Text_Into_Explorer_Filter(ServerSourceName);
+            Uimap.WaitForSpinner(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.Checkbox.Spinner);
+            Assert.IsTrue(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.Exists);
+            Uimap.Select_From_Explorer_Remote_Server_Dropdown_List(Uimap.MainStudioWindow.ComboboxListItemAsTSTCIREMOTE);
+            Uimap.Click_Explorer_RemoteServer_Connect_Button();
+            Uimap.WaitForControlNotVisible(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.FirstRemoteServer.Checkbox.Spinner);
+            Uimap.Enter_Text_Into_Explorer_Filter(RemoteSubWorkflowName);
+            Uimap.WaitForControlNotVisible(Uimap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.FirstRemoteServer.Checkbox.Spinner);
+            Uimap.TryRefreshExplorerUntilOneItemOnly();
             Uimap.Drag_Explorer_Remote_workflow1_Onto_Workflow_Design_Surface();
             Uimap.Save_With_Ribbon_Button_And_Dialog(LocalWorkflowName);
             Uimap.Click_Debug_Ribbon_Button();
