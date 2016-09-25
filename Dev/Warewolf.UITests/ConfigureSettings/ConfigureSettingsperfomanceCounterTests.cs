5914417e5030e17645866838700b484df00a1050
SalamiArmy <ultrakiff@gmail.com>
2016-09-25 10:07:11 +0200

    split explorer context menu up into explorer environment context menu

--- Dev/Warewolf.UITests/ConfigureSettings/ConfigureSettingsperfomanceCounterTests.cs
+++ Dev/Warewolf.UITests/ConfigureSettings/ConfigureSettingsperfomanceCounterTests.cs
@@ -16,7 +16,7 @@ namespace Warewolf.UITests
             Uimap.Select_PerfomanceCounterTab();
             Uimap.Click_Reset_Perfomance_Counter();
             Uimap.Click_Select_Resource_Button();
-            Uimap.Select_Service_From_Service_Picker(_serviceName);
+            Uimap.Select_Service_From_Service_Picker(_serviceName, true);
             Assert.AreEqual("My Category\\" + _serviceName, Uimap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.SettingsTab.WorksurfaceContext.SettingsView.TabList.PerfomanceCounterTab.PerfmonViewContent.ResourceTable.Row1.ResourceCell.ResourceTextBox.DisplayText, "Resource Name is not set to Dice after selecting Dice from Service picker");
             Uimap.Click_Save_Ribbon_Button_With_No_Save_Dialog();
             Uimap.Click_Close_Settings_Tab_Button();
