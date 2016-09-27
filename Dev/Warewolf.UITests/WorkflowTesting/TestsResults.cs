fb68ad872b6888e5f6d7ddb97080e91567b1d646
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-27 09:52:50 +0200

    Manual Coded UI  test fixes

--- Dev/Warewolf.UITests/WorkflowTesting/TestsResults.cs
+++ Dev/Warewolf.UITests/WorkflowTesting/TestsResults.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,92 +9,92 @@ namespace Warewolf.UITests
     {
         public static void GetSelectedTestPendingResult(WpfListItem test, int instance = 1)
         {
-            Point point;
+            //Point point;
             switch (instance)
             {
                 case 2:
                     var test2 = test as Test2;
                     Assert.IsNotNull(test2);
-                    Assert.IsTrue(test2.Pending.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test2.Pending.Exists);
                     break;
                 case 3:
                     var test3 = test as Test3;
                     Assert.IsNotNull(test3);
-                    Assert.IsTrue(test3.Pending.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test3.Pending.Exists);
                     break;
                 default:
                     var test1 = test as Test1;
                     Assert.IsNotNull(test1);
-                    Assert.IsTrue(test1.Pending.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test1.Pending.Exists);
                     break;
             }
         }
 
         public static void GetSelectedTestInvalidResult(WpfListItem test, int instance = 1)
         {
-            Point point;
+            //Point point;
             switch (instance)
             {
                 case 2:
                     var test2 = test as Test2;
                     Assert.IsNotNull(test2);
-                    Assert.IsTrue(test2.Invalid.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test2.Invalid.Exists);
                     break;
                 case 3:
                     var test3 = test as Test3;
                     Assert.IsNotNull(test3);
-                    Assert.IsTrue(test3.Invalid.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test3.Invalid.Exists);
                     break;
                 default:
                     var test1 = test as Test1;
                     Assert.IsNotNull(test1);
-                    Assert.IsTrue(test1.Invalid.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test1.Invalid.Exists);
                     break;
             }
         }
         public static void GetSelectedTestPassingResult(WpfListItem test, int instance = 1)
         {
-            var point = new Point();
+            //var point = new Point();
             if (instance == 2)
             {
                 var test2 = test as Test2;
                 Assert.IsNotNull(test2);
-                Assert.IsTrue(test2.Passing.TryGetClickablePoint(out point));
+                Assert.IsTrue(test2.Passing.Exists);
             }
             if (instance == 3)
             {
                 var test3 = test as Test3;
                 Assert.IsNotNull(test3);
-                Assert.IsTrue(test3.Passing.TryGetClickablePoint(out point));
+                Assert.IsTrue(test3.Passing.Exists);
             }
             if (instance == 1)
             {
                 var test1 = test as Test1;
                 Assert.IsNotNull(test1);
-                Assert.IsTrue(test1.Passing.TryGetClickablePoint(out point));
+                Assert.IsTrue(test1.Passing.Exists);
             }
-            Assert.IsNotNull(point);
+            //Assert.IsNotNull(point);
         }
 
         public static void GetSelectedTestFailingResult(WpfListItem test, int instance)
         {
-            Point point;
+            //Point point;
             switch (instance)
             {
                 case 2:
                     var test2 = test as Test2;
                     Assert.IsNotNull(test2);
-                    Assert.IsTrue(test2.Failing.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test2.Failing.Exists);
                     break;
                 case 3:
                     var test3 = test as Test3;
                     Assert.IsNotNull(test3);
-                    Assert.IsTrue(test3.Failing.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test3.Failing.Exists);
                     break;
                 default:
                     var test1 = test as Test1;
                     Assert.IsNotNull(test1);
-                    Assert.IsTrue(test1.Failing.TryGetClickablePoint(out point));
+                    Assert.IsTrue(test1.Failing.Exists);
                     break;
             }
         }
