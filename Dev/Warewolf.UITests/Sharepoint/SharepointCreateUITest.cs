4572a208bda360b2bdf766e043ad56b70fe317ed
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-28 06:29:12 +0200

    Sharepoint Create mappings

---
+++ Dev/Warewolf.UITests/Sharepoint/SharepointCreateUITest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests
+{
+    [CodedUITest]
+    public class SharepointCreateUITest
+    {
+        [TestMethod]
+        public void Sharepoint_Create_UITest()
+        {
+            
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            Uimap.SetPlaybackSettings();
+#if !DEBUG
+            Uimap.CloseHangingDialogs();
+#endif
+        }
+
+        UIMap Uimap
+        {
+            get
+            {
+                if (_uiMap == null)
+                {
+                    _uiMap = new UIMap();
+                }
+
+                return _uiMap;
+            }
+        }
+
+        private UIMap _uiMap;
+
+        #endregion
+    }
+}
