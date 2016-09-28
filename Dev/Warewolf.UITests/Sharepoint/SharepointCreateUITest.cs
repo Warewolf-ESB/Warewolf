b69615c7d553a82f11aa7d1115e66f62d01e00bb
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-09-28 07:43:01 +0200

    Removed redundant Asserts

--- Dev/Warewolf.UITests/Sharepoint/SharepointCreateUITest.cs
+++ Dev/Warewolf.UITests/Sharepoint/SharepointCreateUITest.cs
@@ -7,9 +7,22 @@ namespace Warewolf.UITests
     public class SharepointCreateUITest
     {
         [TestMethod]
+        public void Sharepoint_CreateNewSource_UITest()
+        {
+            Uimap.Click_New_Workflow_Ribbon_Button();
+            Uimap.Drag_Toolbox_Sharepoint_Create_Onto_DesignSurface();
+            Uimap.Select_NewSharepointSource_FromServer_Lookup();
+            Uimap.Click_Close_SharepointSource_Tab_Button();
+            Uimap.Click_Close_Workflow_Tab_Button();
+            Uimap.Click_MessageBox_No();
+        }
+
+        [TestMethod]
         public void Sharepoint_Create_UITest()
         {
-            
+            Uimap.Click_New_Workflow_Ribbon_Button();
+            Uimap.Drag_Toolbox_Sharepoint_Create_Onto_DesignSurface();
+            Uimap.Select_SharepointTestServer();
         }
 
         #region Additional test attributes
--- Dev/Warewolf.UITests/WorkflowTesting/TestsResults.cs
+++ Dev/Warewolf.UITests/WorkflowTesting/TestsResults.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,26 +9,22 @@ namespace Warewolf.UITests
     {
         public static void GetSelectedTestPendingResult(WpfListItem test, int instance = 1)
         {
-            Point point;
             switch (instance)
             {
                 case 2:
                     var test2 = test as Test2;
                     Assert.IsNotNull(test2);
                     Assert.IsTrue(test2.Pending.Exists);
-                    Assert.IsTrue(test2.Pending.TryGetClickablePoint(out point));
                     break;
                 case 3:
                     var test3 = test as Test3;
                     Assert.IsNotNull(test3);
                     Assert.IsTrue(test3.Pending.Exists);
-                    Assert.IsTrue(test3.Pending.TryGetClickablePoint(out point));
                     break;
                 default:
                     var test1 = test as Test1;
                     Assert.IsNotNull(test1);
                     Assert.IsTrue(test1.Pending.Exists);
-                    Assert.IsTrue(test1.Pending.TryGetClickablePoint(out point));
                     break;
             }
         }
@@ -79,26 +74,22 @@ namespace Warewolf.UITests
 
         public static void GetSelectedTestFailingResult(WpfListItem test, int instance)
         {
-            //Point point;
             switch (instance)
             {
                 case 2:
                     var test2 = test as Test2;
                     Assert.IsNotNull(test2);
                     Assert.IsTrue(test2.Failing.Exists);
-                    //Assert.IsTrue(test2.Failing.TryGetClickablePoint(out point));
                     break;
                 case 3:
                     var test3 = test as Test3;
                     Assert.IsNotNull(test3);
                     Assert.IsTrue(test3.Failing.Exists);
-                    //Assert.IsTrue(test3.Failing.TryGetClickablePoint(out point));
                     break;
                 default:
                     var test1 = test as Test1;
                     Assert.IsNotNull(test1);
                     Assert.IsTrue(test1.Failing.Exists);
-                    //Assert.IsTrue(test1.Failing.TryGetClickablePoint(out point));
                     break;
             }
         }
