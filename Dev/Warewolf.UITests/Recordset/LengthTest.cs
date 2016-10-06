9519da9d0e09a5c26566133a7bb6321113564ea9
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-10-06 14:17:41 +0200

    Close hanging Recordset tabs

--- Dev/Warewolf.UITests/Recordset/LengthTest.cs
+++ Dev/Warewolf.UITests/Recordset/LengthTest.cs
@@ -12,6 +12,9 @@ namespace Warewolf.UITests
             UIMap.Click_New_Workflow_Ribbon_Button();
             UIMap.Drag_Toolbox_Length_Onto_DesignSurface();
             UIMap.Open_Length_Tool_Large_View();
+
+            UIMap.Click_Close_Workflow_Tab_Button();
+            UIMap.Click_MessageBox_No();
         }
 
         #region Additional test attributes
