f0f6acd1a63305937621b980473959aead4dd997
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-10-07 16:15:32 +0200

    add tests for unpinning and repinning all other tabs

--- Dev/Warewolf.UITests/Tabs/PinUnpinPanes.cs
+++ Dev/Warewolf.UITests/Tabs/PinUnpinPanes.cs
@@ -20,8 +20,81 @@ namespace Warewolf.UITests.Tabs
         public void UnpinAndRepinNewWorkflowTab()
         {
             UIMap.Click_New_Workflow_Ribbon_Button();
-            UIMap.Unpin_New_Workflow_Tab_With_Drag();
-            UIMap.Pin_New_Workflow_Tab_To_Default_Position();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.WorkflowTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinDeployTab()
+        {
+            UIMap.Click_Deploy_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.DeployTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinServerSourceWizardTab()
+        {
+            UIMap.Select_NewRemoteServer_From_Explorer_Server_Dropdownlist();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.ServerSourceWizardTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinSettingsTab()
+        {
+            UIMap.Click_Settings_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.SettingsTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinDBSourceWizardTab()
+        {
+            UIMap.Click_New_Database_Source_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.DBSourceWizardTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinDependencyGraphTab()
+        {
+            UIMap.Select_Show_Dependencies_In_Explorer_Context_Menu("Hello World");
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.DependencyGraphTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinPluginSourceWizardTab()
+        {
+            UIMap.Click_NewPluginSource_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.PluginSourceWizardTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinWebSourceWizardTab()
+        {
+            UIMap.Click_New_Web_Source_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.WebSourceWizardTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinTestsTabPage()
+        {
+            UIMap.Filter_Explorer("Hello World");
+            UIMap.Open_Explorer_First_Item_Tests_With_Context_Menu();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.TestsTabPage);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
+        }
+
+        [TestMethod]
+        public void UnpinAndRepinSchedulerTab()
+        {
+            UIMap.Click_Scheduler_Ribbon_Button();
+            UIMap.Unpin_Tab_With_Drag(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabMan.SchedulerTab);
+            UIMap.Pin_Unpinned_Pane_To_Default_Position();
         }
 
         #region Additional test attributes
