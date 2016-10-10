dabaa5ce31db345ec0d1734f503b138068fca7d5
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-10-10 07:43:46 +0200

    Utitlity tools mappings

---
+++ Dev/Warewolf.UITests/Tools/Utility/RabbitMQConsume.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.Tools.Utility
+{
+    [CodedUITest]
+    public class RabbitMQConsume
+    {
+        [TestMethod]
+		[TestCategory("Tools")]
+        public void RabbitMQConsumeToolUITest()
+        {
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+#if !DEBUG
+            UIMap.CloseHangingDialogs();
+#endif
+            UIMap.InitializeABlankWorkflow();
+        }
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_UIMap == null)
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        #endregion
+    }
+}
