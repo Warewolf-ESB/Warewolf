9cb9c4d80222beaf7a917b41306843c52aac498f
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-10-14 11:27:15 +0200

    strip sharepoint tools down to just one basic test that opens the sharepoint tools large views

--- Dev/Warewolf.UITests/Tools/Sharepoint/Read Folder.cs
+++ Dev/Warewolf.UITests/Tools/Sharepoint/Read Folder.cs
@@ -4,23 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Warewolf.UITests
 {
     [CodedUITest]
-    public class SharepointReadUITest
+    public class Read_Folder
     {
         [TestMethod]
         [TestCategory("Tools")]
-        public void Sharepoint_Create_UITest()
+        public void Sharepoint_Read_Folder_UITest()
         {
-            UIMap.Click_New_Workflow_Ribbon_Button();
-            UIMap.Drag_Toolbox_Sharepoint_Read_Onto_DesignSurface();
-            UIMap.Select_SharepointTestServer_From_SharepointRead_Tool();
-            UIMap.Click_EditSharepointSource_Button_FromSharePointRead();
-            UIMap.Click_Sharepoint_Server_Source_TestConnection();
-            UIMap.Click_Close_SharepointSource_Tab_Button();
-            UIMap.WaitForSpinner(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.Spinner);
-            UIMap.Select_AppData_From_MethodList_From_ReadTool();
-            UIMap.WaitForSpinner(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.Spinner);
-            UIMap.Click_Sharepoint_RefreshButton_From_SharepointRead();
-            UIMap.WaitForSpinner(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.Spinner);
             UIMap.Open_Sharepoint_Read_Tool_Large_View();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.LargeViewContent.DataGridTableVariables.ItemRow1.FileNameCell.FieldComboBox.Exists);
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.LargeViewContent.DataGridTableVariables.ItemRow1.MatchTypeCell.MatchTypeComboBox.Exists);
@@ -28,8 +17,6 @@ namespace Warewolf.UITests
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.LargeViewContent.DataGridTableValues.ItemRow1.FileNameCell.FieldComboBox.Exists);
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.LargeViewContent.DataGridTableValues.ItemRow1.MatchTypeCell.MatchTypeComboBox.Exists);
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointRead.LargeViewContent.DataGridTableValues.ItemRow1.ValueCell.ValueComboBox.Exists);
-            UIMap.Click_Close_Workflow_Tab_Button();
-            UIMap.Click_MessageBox_No();
         }
 
         #region Additional test attributes
@@ -41,6 +28,8 @@ namespace Warewolf.UITests
 #if !DEBUG
             UIMap.CloseHangingDialogs();
 #endif
+            UIMap.Click_New_Workflow_Ribbon_Button();
+            UIMap.Drag_Toolbox_Sharepoint_Read_Onto_DesignSurface();
         }      
 
         UIMap UIMap
--- Dev/Warewolf.UITests/Tools/Sharepoint/Upload File.cs
+++ Dev/Warewolf.UITests/Tools/Sharepoint/Upload File.cs
@@ -4,26 +4,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Warewolf.UITests
 {
     [CodedUITest]
-    public class SharepointUploadUITest
+    public class Upload_File
     {
         [TestMethod]
         [TestCategory("Tools")]
         public void Sharepoint_Upload_UITest()
         {
-            UIMap.Click_New_Workflow_Ribbon_Button();
-            UIMap.Drag_Toolbox_Sharepoint_UploadFile_Onto_DesignSurface();
-            UIMap.Select_NewSharepointSource_FromServer_Lookup_On_SharepointUpload_Tool();
-            UIMap.Enter_Sharepoint_ServerSource_ServerName();
-            UIMap.Click_UserButton_OnSharepointSource();
-            UIMap.Enter_Sharepoint_ServerSource_User_Credentials();
-            UIMap.Click_Sharepoint_Server_Source_TestConnection();
-            UIMap.Click_Close_SharepointSource_Tab_Button();
             UIMap.Open_Sharepoint_Upload_Tool_Large_View();
-            UIMap.Enter_Sharepoint_Server_Path_From_OnUpload_Tool();
-            UIMap.Enter_Sharepoint_Server_Path_To_OnUpload_Tool();
-            UIMap.Click_Close_Workflow_Tab_Button();
-            UIMap.Click_MessageBox_No();
         }
+
         #region Additional test attributes
 
         [TestInitialize()]
@@ -33,6 +22,8 @@ namespace Warewolf.UITests
 #if !DEBUG
             UIMap.CloseHangingDialogs();
 #endif
+            UIMap.Click_New_Workflow_Ribbon_Button();
+            UIMap.Drag_Toolbox_Sharepoint_UploadFile_Onto_DesignSurface();
         }      
 
         UIMap UIMap
