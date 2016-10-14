8e45c69c1b5163495779a20b044a6ff91cfb05b5
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-10-14 07:02:15 +0200

    Database tools tests

---
+++ Dev/Warewolf.UITests/Tools/Database/OracleDatabaseTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.Tools
+{
+    [CodedUITest]
+    public class OracleDatabaseTests
+    {
+        [TestMethod]
+        [TestCategory("Tools")]
+        public void OracleDatabaseToolUITest()
+        {
+            UIMap.Drag_Toolbox_Oracle_Database_Onto_DesignSurface();
+            UIMap.Open_Oracle_Tool_Large_View();
+            UIMap.Click_NewSource_Button_FromOracle_Tool();
+            UIMap.Click_Close_DB_Source_Wizard_Tab_Button();
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+#if !DEBUG
+            UIMap.CloseHangingDialogs();
+#endif
+            UIMap.InitializeABlankWorkflow();
+        }
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            UIMap.Click_Close_Workflow_Tab_Button();
+            UIMap.Click_MessageBox_No();
+        }
+        UIMap UIMap
+        {
+            get
+            {
+                if ((_UIMap == null))
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        #endregion
+    }
+}
---
+++ Dev/Warewolf.UITests/Tools/Database/SqlServerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.Tools
+{
+    [CodedUITest]
+    public class SqlServerTests
+    {
+
+        //Todo:Now, generate the Missing drag actions
+        //Todo:Put more test steps (Dont forget asserts)        
+        
+        [TestMethod]
+        [TestCategory("Tools")]
+        public void SqlServerToolUITest()
+        {
+            UIMap.Drag_Toolbox_SQL_Server_Tool_Onto_DesignSurface();
+            UIMap.Open_Sql_Server_Tool_Large_View();
+            
+        }
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+#if !DEBUG
+            UIMap.CloseHangingDialogs();
+#endif
+            UIMap.InitializeABlankWorkflow();
+        }
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            UIMap.Click_Close_Workflow_Tab_Button();
+            UIMap.Click_MessageBox_No();
+        }
+        UIMap UIMap
+        {
+            get
+            {
+                if ((_UIMap == null))
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        #endregion
+    }
+}
