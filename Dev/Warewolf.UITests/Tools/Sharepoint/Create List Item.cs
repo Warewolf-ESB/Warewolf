f819427597ba8459ae6e45380afbe3d94b28d7ab
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-10-18 11:39:06 +0200

    expand sharepoint tool ui tests

--- Dev/Warewolf.UITests/Tools/Sharepoint/Create List Item.cs
+++ Dev/Warewolf.UITests/Tools/Sharepoint/Create List Item.cs
@@ -8,7 +8,18 @@ namespace Warewolf.UITests
     {
         [TestMethod]
         [TestCategory("Tools")]
-        public void Sharepoint_Create_UITest()
+        public void Sharepoint_Create_Small_View_UITest()
+        {
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.SmallView.Server.Exists, "Server combobox does not exist on small view after tool has been dragged from the toolbox.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.SmallView.EditSourceButton.Exists, "Edit Source Button does not exist on small view after tool has been dragged from the toolbox.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.SmallView.List.Exists, "List does not exist on small view after tool has been dragged from the toolbox.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.SmallView.MethodList.Exists, "Method List does not exist on small view after tool has been dragged from the toolbox.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.SmallView.RefreshButton.Exists, "Refresh Button does not exist on small view after tool has been dragged from the toolbox.");
+        }
+
+        [TestMethod]
+        [TestCategory("Tools")]
+        public void Sharepoint_Create_Large_View_UITest()
         {
             UIMap.Open_Sharepoint_Create_Tool_Large_View();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SharepointCreateListItem.LargeView.DataGridTable.ContentTypeRow.ItemContentTypeCell.ContentTypeText.Exists);
