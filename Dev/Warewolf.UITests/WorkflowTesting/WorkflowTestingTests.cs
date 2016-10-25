1097908ba909b85b9152c2b7358657639ea9fd55
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-10-25 09:52:41 +0200

    Code Cleanup

--- Dev/Warewolf.UITests/WorkflowTesting/WorkflowTestingTests.cs
+++ Dev/Warewolf.UITests/WorkflowTesting/WorkflowTestingTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
+﻿using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Warewolf.UITests.Common;
