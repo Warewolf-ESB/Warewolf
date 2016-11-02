d2fc622a4e133abb8b5d523f22a6547d254632bc
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-11-02 12:24:01 +0200

    Revert back scheduler viewmodel

--- Dev/Dev2.Studio/Settings/Scheduler/SchedulerViewModel.cs
+++ Dev/Dev2.Studio/Settings/Scheduler/SchedulerViewModel.cs
@@ -45,7 +45,6 @@ using Dev2.Studio.Core.Models;
 using Dev2.Studio.ViewModels.Diagnostics;
 using Dev2.Studio.ViewModels.WorkSurface;
 using Dev2.Threading;
-using Newtonsoft.Json;
 using Warewolf.Studio.AntiCorruptionLayer;
 using Warewolf.Studio.Resources.Languages;
 using Warewolf.Studio.ViewModels;
@@ -79,8 +78,6 @@ namespace Dev2.Settings.Scheduler
         private Func<IServer, IEnvironmentModel> _toEnvironmentModel;
         private bool _errorShown;
         private DebugOutputViewModel _debugOutputViewModel;
-        private string _displayName;
-        private SchedulerViewModel _item;
 
         // ReSharper disable once MemberCanBeProtected.Global
         public SchedulerViewModel()
@@ -93,7 +90,7 @@ namespace Dev2.Settings.Scheduler
         {
         }
 
-        public SchedulerViewModel(IEventAggregator eventPublisher, DirectoryObjectPickerDialog directoryObjectPicker, IPopupController popupController, IAsyncWorker asyncWorker, IServer server, Func<IServer, IEnvironmentModel> toEnvironmentModel,Task<IResourcePickerDialog> getResourcePicker = null)
+        public SchedulerViewModel(IEventAggregator eventPublisher, DirectoryObjectPickerDialog directoryObjectPicker, IPopupController popupController, IAsyncWorker asyncWorker, IServer server, Func<IServer, IEnvironmentModel> toEnvironmentModel, Task<IResourcePickerDialog> getResourcePicker = null)
             : base(eventPublisher)
         {
             SchedulerTaskManager = new SchedulerTaskManager(this, getResourcePicker);
@@ -121,8 +118,6 @@ namespace Dev2.Settings.Scheduler
 
             Server = server;
             SetupServer(server);
-            DisplayName = "Scheduler - " + Server.ResourceName;
-            SetItem(this);
         }
 
         public override bool HasVariables => false;
@@ -132,52 +127,16 @@ namespace Dev2.Settings.Scheduler
         {
             get
             {
-                return _displayName;
-            }
-            set
-            {
-                _displayName = value;
-                NotifyOfPropertyChange(() => DisplayName);
-            }
-        }
-
-        private void SetDisplayName()
-        {
-            if (IsDirty)
-            {
-                if (!DisplayName.EndsWith(" *"))
+                if (Server != null)
                 {
-                    DisplayName += " *";
+                    return "Scheduler - " + Server.ResourceName;
                 }
+                return "Scheduler";
             }
-            else
-            {
-                DisplayName = _displayName.Replace("*", "").TrimEnd(' ');
-            }
-        }
-
-        [JsonIgnore]
-        public SchedulerViewModel Item
-        {
-            private get { return _item; }
             set
             {
-                _item = value;
-                NotifyOfPropertyChange(() => Item);
+
             }
-        }
-
-        public void SetItem(SchedulerViewModel model)
-        {
-            Item = Clone(model);
-        }
-
-        public SchedulerViewModel Clone(SchedulerViewModel model)
-        {
-            var resolver = new ShouldSerializeContractResolver();
-            var ser = JsonConvert.SerializeObject(model, new JsonSerializerSettings { ContractResolver = resolver });
-            SchedulerViewModel clone = JsonConvert.DeserializeObject<SchedulerViewModel>(ser);
-            return clone;
         }
 
         public Func<IServer, IEnvironmentModel> ToEnvironmentModel
@@ -347,7 +306,7 @@ namespace Dev2.Settings.Scheduler
                     ClearError(Core.SchedulerDuplicateNameErrorMessage);
                 }
                 SelectedTask.Name = value;
-                SelectedTask.IsDirty = !Equals(this);
+                SelectedTask.IsDirty = true;
                 NotifyOfPropertyChange(() => Name);
             }
         }
@@ -367,7 +326,7 @@ namespace Dev2.Settings.Scheduler
                         return;
                     }
                     SelectedTask.RunAsapIfScheduleMissed = value;
-                    SelectedTask.IsDirty = !Equals(this);
+                    SelectedTask.IsDirty = true;
                     NotifyOfPropertyChange(() => RunAsapIfScheduleMissed);
                 }
             }
@@ -390,7 +349,7 @@ namespace Dev2.Settings.Scheduler
                     if (string.IsNullOrEmpty(value))
                     {
                         SelectedTask.NumberOfHistoryToKeep = 0;
-                        SelectedTask.IsDirty = !Equals(this);
+                        SelectedTask.IsDirty = true;
                     }
                     else
                     {
@@ -398,7 +357,7 @@ namespace Dev2.Settings.Scheduler
                         if (value.IsWholeNumber(out val))
                         {
                             SelectedTask.NumberOfHistoryToKeep = val;
-                            SelectedTask.IsDirty = !Equals(this);
+                            SelectedTask.IsDirty = true;
                         }
                     }
                     NotifyOfPropertyChange(() => NumberOfRecordsToKeep);
@@ -413,11 +372,11 @@ namespace Dev2.Settings.Scheduler
                 if (ScheduledResourceModel != null && SelectedTask != null && _history == null && !SelectedTask.IsNewItem)
                 {
                     _asyncWorker.Start(() =>
-                   {
-                       IsHistoryTabVisible = false;
-                       IsProgressBarVisible = true;
-                       _history = ScheduledResourceModel.CreateHistory(SelectedTask).ToList();
-                   }
+                    {
+                        IsHistoryTabVisible = false;
+                        IsProgressBarVisible = true;
+                        _history = ScheduledResourceModel.CreateHistory(SelectedTask).ToList();
+                    }
                    , () =>
                    {
                        IsHistoryTabVisible = true;
@@ -538,7 +497,7 @@ namespace Dev2.Settings.Scheduler
                     }
                     ClearError(Core.SchedulerLoginErrorMessage);
                     SelectedTask.UserName = value;
-                    SelectedTask.IsDirty = !Equals(this);
+                    SelectedTask.IsDirty = true;
                     NotifyOfPropertyChange(() => AccountName);
                 }
             }
@@ -560,7 +519,7 @@ namespace Dev2.Settings.Scheduler
                     }
                     ClearError(Core.SchedulerLoginErrorMessage);
                     SelectedTask.Password = value;
-                    SelectedTask.IsDirty = !Equals(this);
+                    SelectedTask.IsDirty = true;
                     NotifyOfPropertyChange(() => Password);
                 }
             }
@@ -678,7 +637,7 @@ namespace Dev2.Settings.Scheduler
                        (_deleteCommand = new DelegateCommand(param =>
                        {
                            var taskToBeDeleted = param as IScheduledResource;
-                           if(taskToBeDeleted == null) return;
+                           if (taskToBeDeleted == null) return;
                            SelectedTask = taskToBeDeleted;
                            SchedulerTaskManager.DeleteTask();
                        }));
@@ -823,7 +782,6 @@ namespace Dev2.Settings.Scheduler
                         _errorShown = true;
                     }
                 }
-                SetDisplayName();
                 return false;
             }
         }
@@ -835,7 +793,7 @@ namespace Dev2.Settings.Scheduler
                 if (SelectedTask != null && SelectedTask.IsDirty)
                 {
                     MessageBoxResult showSchedulerCloseConfirmation = _popupController.ShowSchedulerCloseConfirmation();
-                    switch(showSchedulerCloseConfirmation)
+                    switch (showSchedulerCloseConfirmation)
                     {
                         case MessageBoxResult.Cancel:
                         case MessageBoxResult.None:
@@ -854,7 +812,7 @@ namespace Dev2.Settings.Scheduler
         protected internal virtual void ShowSaveErrorDialog(string error)
         {
             _popupController.ShowSaveErrorDialog(error);
-        }       
+        }
 
         public string HelpText
         {
@@ -896,28 +854,6 @@ namespace Dev2.Settings.Scheduler
         public void UpdateScheduleWithResourceDetails(string resourcePath, Guid id, string resourceName)
         {
             SchedulerTaskManager.UpdateScheduleWithResourceDetails(resourcePath, id, resourceName);
-        }
-
-        private bool Equals(SchedulerViewModel other)
-        {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-
-            return EqualsSeq(other);
-        }
-
-        private bool EqualsSeq(SchedulerViewModel other)
-        {
-            return string.Equals(_selectedTask.Name, other._selectedTask.Name) &&
-                   string.Equals(TriggerText, other.TriggerText) &&
-                   string.Equals(_selectedTask.Status.ToString(), other._selectedTask.Status.ToString()) &&
-                   string.Equals(_selectedTask.WorkflowName, other._selectedTask.WorkflowName) &&
-                   string.Equals(_selectedTask.RunAsapIfScheduleMissed.ToString(), other._selectedTask.RunAsapIfScheduleMissed.ToString()) &&
-                   string.Equals(NumberOfRecordsToKeep, other.NumberOfRecordsToKeep) &&
-                   string.Equals(AccountName, other.AccountName) &&
-                   string.Equals(Password, other.Password);
         }
     }
 
