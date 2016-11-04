8cf3866986a71c059d9c4b3213e5eb2bb997c0fb
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-11-04 10:16:29 +0200

    Added cleanup method

--- Dev/Warewolf.UITests/Tools/Data/Case Convert.cs
+++ Dev/Warewolf.UITests/Tools/Data/Case Convert.cs
@@ -32,7 +32,12 @@ namespace Warewolf.UITests.Tools.Data
             Uimap.Click_New_Workflow_Ribbon_Button();
             Uimap.Drag_Toolbox_Case_Conversion_Onto_DesignSurface();
         }
-        
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            Uimap.Click_Close_Workflow_Tab_Button();
+            Uimap.Click_MessageBox_No();
+        }
         UIMap Uimap
         {
             get
