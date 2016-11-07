a453c881dca9868525c43f31f40f32ed7217d330
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-11-07 15:44:32 +0200

    #WOLF-2114 Fix test creation from debug output not consistant

--- Dev/Dev2.Studio.Core/Interfaces/IServiceTestViewModel.cs
+++ Dev/Dev2.Studio.Core/Interfaces/IServiceTestViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
@@ -43,5 +44,6 @@ namespace Dev2.Studio.Core.Interfaces
         void RefreshCommands();
 
         void AddDuplicateTestUsingDebug(NewTestFromDebugMessage message);
+        void PrepopulateTestsUsingDebug(List<IDebugTreeViewItemViewModel> models);
     }
 }
