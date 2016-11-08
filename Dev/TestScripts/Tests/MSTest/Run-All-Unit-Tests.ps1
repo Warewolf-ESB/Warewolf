3cdf8071858c7b6ca779e92b44136e8ca330f2ea
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-08 11:51:51 +0200

    new convenience scripts for running seperate unit test assemblies

--- Dev/TestScripts/Tests/MSTest/Run-All-Unit-Tests.ps1
+++ Dev/TestScripts/Tests/MSTest/Run-All-Unit-Tests.ps1
@@ -28,10 +28,14 @@ if ($Args.Count -gt 0) {
 # Create assemblies list.
 $TestAssembliesList = ''
 foreach ($file in Get-ChildItem $SolutionDir -Filter Warewolf.*.Tests ) {
-    $TestAssembliesList = $TestAssembliesList + " /testcontainer:`"" + $file.FullName + "\bin\Debug\" + $file.Name + ".dll`""
+	if ($file.Name -ne "Warewolf.Studio.ViewModels.Tests.dll")) {
+		$TestAssembliesList = $TestAssembliesList + " /testcontainer:`"" + $file.FullName + "\bin\Debug\" + $file.Name + ".dll`""
+	}
 }
 foreach ($file in Get-ChildItem $SolutionDir -Filter Dev2.*.Tests ) {
-    $TestAssembliesList = $TestAssembliesList + " /testcontainer:`"" + $file.FullName + "\bin\Debug\" + $file.Name + ".dll`""
+	if (($file.Name -ne "Dev2.Activities.Designers.Tests.dll" -and $file.Name -ne "Dev2.Activities.Tests.dll") {
+		$TestAssembliesList = $TestAssembliesList + " /testcontainer:`"" + $file.FullName + "\bin\Debug\" + $file.Name + ".dll`""
+	}
 }
 
 # Create full VSTest argument string.
