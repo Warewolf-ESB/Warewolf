3735bb0522103b452c9a89356b91fc23b7657344
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-11-08 09:19:59 +0200

    Merges after pull from development

--- Dev/Warewolf.UITests/Tools/Storage Dropbox/List Contents.cs
+++ Dev/Warewolf.UITests/Tools/Storage Dropbox/List Contents.cs
@@ -39,7 +39,7 @@ namespace Warewolf.UITests.Tools.Storage_Dropbox
             UIMap.CloseHangingDialogs();
 #endif
             UIMap.Click_New_Workflow_Ribbon_Button();
-            UIMap.Drag_Toolbox_Dropbox_FileList_Onto_DesignSurface();
+            //UIMap.Drag_Toolbox_Dropbox_FileList_Onto_DesignSurface();
         }
 
         UIMap UIMap
