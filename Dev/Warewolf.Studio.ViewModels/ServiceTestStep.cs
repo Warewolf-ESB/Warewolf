6ce9dbf0228dc2380e8663adb8176382e8aadd1b
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-11-08 13:10:45 +0200

    Coverage

--- Dev/Warewolf.Studio.ViewModels/ServiceTestStep.cs
+++ Dev/Warewolf.Studio.ViewModels/ServiceTestStep.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Windows.Media;
 using Dev2.Common.Interfaces;
@@ -56,6 +57,7 @@ namespace Warewolf.Studio.ViewModels
         }
 
         [JsonIgnore]
+        [ExcludeFromCodeCoverage]
         public ImageSource StepIcon
         {
             get { return _stepIcon; }
@@ -276,8 +278,7 @@ namespace Warewolf.Studio.ViewModels
                     if (StepOutputs != null)
                         foreach (var serviceTestOutput in StepOutputs)
                         {
-                            var item = serviceTestOutput as ServiceTestOutput;
-                            item?.OnSearchTypeChanged();
+                            serviceTestOutput?.OnSearchTypeChanged();
                         }
                 }
                 OnPropertyChanged(() => AssertSelected);
@@ -337,12 +338,8 @@ namespace Warewolf.Studio.ViewModels
                 }
                 else
                 {
-                    var serviceTestOutput = new ServiceTestOutput(varName,"","","");
-                    serviceTestOutput.AddNewAction = ()=>AddNewOutput(varName);
-                    //if (StepOutputs.FirstOrDefault(output => output.Variable.Equals(varName, StringComparison.InvariantCultureIgnoreCase)) == null)
-                    {
-                        StepOutputs.Add(serviceTestOutput);
-                    }
+                    var serviceTestOutput = new ServiceTestOutput(varName, "", "", "") { AddNewAction = () => AddNewOutput(varName) };
+                    StepOutputs.Add(serviceTestOutput);
                 }
             }
         }
