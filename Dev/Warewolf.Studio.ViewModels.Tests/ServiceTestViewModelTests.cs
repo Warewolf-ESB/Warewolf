02ac7a22e0a6433c17c18bc565acf98a35ea5fc2
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-11-08 16:32:02 +0200

    Covergae up on the Vm

--- Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.cs
@@ -57,6 +57,8 @@ namespace Warewolf.Studio.ViewModels.Tests
             //------------Assert Results-------------------------
         }
 
+        
+
         [TestMethod]
         [Owner("Nkosinathi Sangweni")]
         [TestCategory("TestFrameworkViewModel_Constructor")]
