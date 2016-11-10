b30b1bc105e2d47473c45e69a1897f4c709f9fa9
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-11-10 11:29:50 +0200

    TestinFramework uispecs in progress

--- Dev/Warewolf.UITests/WorkflowTesting/TestFrameworkMockingTests.cs
+++ Dev/Warewolf.UITests/WorkflowTesting/TestFrameworkMockingTests.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Warewolf.UITests.Common;
 
 namespace Warewolf.UITests
 {
     [CodedUITest]
     public class TestFrameworkMockingTests
     {
-        private const string HelloWorld = "Hello World";
-        private const string Message = "Hello There World";
-                
+        private const string RandomWorkFlow = "RandomTool WorkFlow";
+
         [TestMethod]
         public void StepsWithoutOutputsShouldBeMarkedInvalid()
         {
@@ -28,6 +26,33 @@ namespace Warewolf.UITests
             UIMap.Click_Create_Test_From_Debug();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.TestsTabPage.WorkSurfaceContext.ServiceTestView.StepTestDataTreeTree.AssignToNameTreeItem.Exists);
             UIMap.Click_SaveDialog_Save_Button();
+        }
+        
+        [TestMethod]
+        public void CreateTestFromDebugButtonDisabledForUnsavedWorkflows()
+        {
+            UIMap.Click_New_Workflow_Ribbon_Button();
+            UIMap.Drag_Toolbox_Random_Onto_DesignSurface();
+            Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.CreateTestFromDebugButton.Enabled);
+        }
+
+        [TestMethod]
+        public void CreateNewTestThenCreateTestFromDebugOutput()
+        {
+            UIMap.Click_New_Workflow_Ribbon_Button();
+            UIMap.Drag_Toolbox_Random_Onto_DesignSurface();
+            UIMap.Enter_Dice_Roll_Values();
+            UIMap.Save_With_Ribbon_Button_And_Dialog(RandomWorkFlow);
+            UIMap.Filter_Explorer(RandomWorkFlow);
+            UIMap.Open_Explorer_First_Item_Tests_With_Context_Menu();
+            UIMap.Click_Create_New_Tests(true);
+            UIMap.Click_New_Workflow_Tab();
+            UIMap.Press_F6();
+            UIMap.Click_Create_Test_From_Debug();
+            Assert.IsTrue(UIMap.MessageBoxWindow.Exists);
+            Assert.IsTrue(UIMap.MessageBoxWindow.UIPleasesavecurrentlyeText.Exists);
+            UIMap.Click_MessageBox_OK();
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.TestsTabPage.WorkSurfaceContext.ServiceTestView.Exists);
         }
         
 
