9acb8f88057ad8f8d8ff7d622dc5bc6dd6745d05
Barney <barney.buchan@dev2.co.za>
2016-11-15 09:12:27 +0200

    Removed unused strings

--- Dev/Dev2.Activities.Designers/Designers2/.Net DLL/DotNetDllViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/.Net DLL/DotNetDllViewModel.cs
@@ -48,16 +48,9 @@ namespace Dev2.Activities.Designers2.Net_DLL
         const string DoneText = "Done";
         const string FixText = "Fix";
         const string OutputDisplayName = " - Outputs";
-        // ReSharper disable UnusedMember.Local
+
         readonly string _sourceNotFoundMessage = Warewolf.Studio.Resources.Languages.Core.DatabaseServiceSourceNotFound;
-
-        readonly string _sourceNotSelectedMessage = Warewolf.Studio.Resources.Languages.Core.DatabaseServiceSourceNotSelected;
-        readonly string _methodNotSelectedMessage = Warewolf.Studio.Resources.Languages.Core.PluginServiceMethodNotSelected;
-        readonly string _serviceExecuteOnline = Warewolf.Studio.Resources.Languages.Core.DatabaseServiceExecuteOnline;
-        readonly string _serviceExecuteLoginPermission = Warewolf.Studio.Resources.Languages.Core.DatabaseServiceExecuteLoginPermission;
-        readonly string _serviceExecuteViewPermission = Warewolf.Studio.Resources.Languages.Core.DatabaseServiceExecuteViewPermission;
-        // ReSharper restore UnusedMember.Local
-
+        
         public DotNetDllViewModel(ModelItem modelItem)
             : base(modelItem)
         {
