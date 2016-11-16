7a1531c704c01982fd2aee0cd526b2b69788f167
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-16 11:55:49 +0200

    convert some more ui binding tests to be normal ui tests

--- Dev/Warewolf.UITests/Tools/Control Flow/Decision.cs
+++ Dev/Warewolf.UITests/Tools/Control Flow/Decision.cs
@@ -11,54 +11,58 @@ namespace Warewolf.UITests.Tools.Control_Flow
         [TestCategory("Tools")]
         public void DecisionTool_LargeView_UITest()
         {
-            Uimap.Open_Decision_Large_View();
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.LargeView.Exists);
-            Uimap.Click_Decision_Dialog_Cancel_Button();
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.LargeView.Table.Exists, "Table does not exist on decision large view after dragging tool in from the toolbox.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.LargeView.RequireAllDecisionsToBeTrueCheckbox.Exists, "Require All Decisions To Be True Checkbox does not exist on decision large view after dragging tool in from the toolbox.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.LargeView.DisplayTextbox.Exists, "Display Textbox does not exist on decision large view after dragging tool in from the toolbox.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.LargeView.TrueArmTextbox.Exists, "True Arm Textbox does not exist on decision large view after dragging tool in from the toolbox.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.LargeView.FalseArmTextbox.Exists, "False Arm Textbox does not exist on decision large view after dragging tool in from the toolbox.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.DoneButton.Exists, "Done button does not exist on decision large view after dragging tool in from the toolbox.");
+            UIMap.Click_Decision_Dialog_Cancel_Button();
         }
 
         [TestMethod]
         [TestCategory("Tools")]
         public void DecisionTool_MatchType_Combobox_ListItems_UITest()
         {
-            Uimap.Open_Decision_Large_View();
-            Mouse.Click(Uimap.DecisionOrSwitchDialog.LargeView.Table.Row1.MatchTypeCell.MatchTypeCombobox, new Point(5, 5));
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsContains.Exists, "Contains match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntContain.Exists, "Doesnt Contains match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntEndWith.Exists, "Doesnt End With match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntStartWith.Exists, "Doesnt Start With match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsEndsWith.Exists, "EndsWith match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsEquals.Exists, "Equals match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsGreaterThan.Exists, "Greater Than match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsGreaterThanOrEqualTo.Exists, "Greater Than Or Equal To match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsAlphanumeric.Exists, "Alphanumeric match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntEndWith.Exists, "Doesnt End With match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntStartWith.Exists, "Doesnt Start With match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsBase64.Exists, "Is Base 64 match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsBetween.Exists, "Is Between match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsBinary.Exists, "IsB inary match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsDate.Exists, "Is Date match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsEmail.Exists, "Is Email match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsHex.Exists, "Is Hex match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsNULL.Exists, "Is NULL match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsNumeric.Exists, "Is Numeric match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsBase64.Exists, "Is Base64 match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsNotNULL.Exists, "Is Not NULL match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsText.Exists, "Is Text match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsRegex.Exists, "Is Regex match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsIsXML.Exists, "Is XML match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsNotEmail.Exists, "Not Email match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsNotAlphanumeric.Exists, "Not Alphanumeric match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsNotBase64.Exists, "Not Base64 match type combobox list item does not exist.");
-            Assert.IsTrue(Uimap.DecisionOrSwitchDialog.ComboboxListItemAsNotBetween.Exists, "Not Between match type combobox list item does not exist.");
-            Uimap.Click_Decision_Dialog_Cancel_Button();
+            UIMap.Click_Decision_Large_View_Match_Type_Combobox();
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsContains.Exists, "Contains match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntContain.Exists, "Doesnt Contains match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntEndWith.Exists, "Doesnt End With match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntStartWith.Exists, "Doesnt Start With match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsEndsWith.Exists, "EndsWith match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsEquals.Exists, "Equals match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsGreaterThan.Exists, "Greater Than match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsGreaterThanOrEqualTo.Exists, "Greater Than Or Equal To match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsAlphanumeric.Exists, "Alphanumeric match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntEndWith.Exists, "Doesnt End With match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsDoesntStartWith.Exists, "Doesnt Start With match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsBase64.Exists, "Is Base 64 match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsBetween.Exists, "Is Between match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsBinary.Exists, "IsB inary match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsDate.Exists, "Is Date match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsEmail.Exists, "Is Email match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsHex.Exists, "Is Hex match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsNULL.Exists, "Is NULL match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsNumeric.Exists, "Is Numeric match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsBase64.Exists, "Is Base64 match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsNotNULL.Exists, "Is Not NULL match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsText.Exists, "Is Text match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsRegex.Exists, "Is Regex match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsIsXML.Exists, "Is XML match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsNotEmail.Exists, "Not Email match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsNotAlphanumeric.Exists, "Not Alphanumeric match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsNotBase64.Exists, "Not Base64 match type combobox list item does not exist.");
+            Assert.IsTrue(UIMap.DecisionOrSwitchDialog.ComboboxListItemAsNotBetween.Exists, "Not Between match type combobox list item does not exist.");
+            UIMap.Click_Decision_Dialog_Cancel_Button();
         }
 
         [TestMethod]
 		[TestCategory("Tools")]
         public void CopyDecisionsWithContextMenuAndPasteExpectedNoWizardsDisplayed_UITest()
         {
-            Uimap.CopyAndPaste_Decision_Tool_On_The_Designer();
-            Assert.IsFalse(Uimap.DecisionOrSwitchDialog.Exists);
+            UIMap.Click_Decision_Dialog_Done_Button();
+            UIMap.CopyAndPaste_Decision_Tool_On_The_Designer();
+            Assert.IsFalse(UIMap.ControlExistsNow(UIMap.DecisionOrSwitchDialog), "Decision large view dialog openned after copy and paste.");
         }
 
         #region Additional test attributes
@@ -66,17 +70,15 @@ namespace Warewolf.UITests.Tools.Control_Flow
         [TestInitialize]
         public void MyTestInitialize()
         {
-            Uimap.SetPlaybackSettings();
+            UIMap.SetPlaybackSettings();
 #if !DEBUG
-            Uimap.CloseHangingDialogs();
+            UIMap.CloseHangingDialogs();
 #endif
-            Uimap.InitializeABlankWorkflow();
-            Uimap.Drag_Toolbox_Decision_Onto_DesignSurface();
-            Uimap.Click_Decision_Dialog_Done_Button();
+            UIMap.InitializeABlankWorkflow();
+            UIMap.Drag_Toolbox_Decision_Onto_DesignSurface();
         }
-        
 
-        UIMap Uimap
+        public UIMap UIMap
         {
             get
             {
