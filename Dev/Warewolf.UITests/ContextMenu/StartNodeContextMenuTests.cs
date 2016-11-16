07b7d9b91ba29c26227ae4efd9990109f0a2d815
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-16 10:09:58 +0200

    fix ui tests expecting content menu items to be enabled when they shouldn't actually be

--- Dev/Warewolf.UITests/ContextMenu/StartNodeContextMenuTests.cs
+++ Dev/Warewolf.UITests/ContextMenu/StartNodeContextMenuTests.cs
@@ -4,38 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Warewolf.UITests.ContextMenu
 {
-    /// <summary>
-    /// Summary description for CodedUITest1
-    /// </summary>
     [CodedUITest]
     public class StartNodeContextMenuTests
     {
         [TestMethod]
-        public void CodedUIShowStartNode()
+        public void CodedUIShowStartNodeContextMenuItems()
         {
             UIMap.Click_New_Workflow_Ribbon_Button();
             UIMap.DisplayStartNodeContextMenu();
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugInputsMenuItem.Exists, "Debug Inputs does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugStudioMenuItem.Exists, "Debug Studio does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugBrowserMenuItem.Exists, "Debug Browser does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ScheduleMenuItem.Exists, "Schedule does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.TestEditorMenuItem.Exists, "Test Editor does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.TestEditorMenuItem.Enabled, "Test Editor must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.TestEditorMenuItem.Enabled, "Test Editor must be disabled on a new workflow");
             Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugInputsMenuItem.Enabled, "Debug Inputs must be enabled on a new workflow");
             Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugStudioMenuItem.Enabled, "Debug Studio must be enabled on a new workflow");
             Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DebugBrowserMenuItem.Enabled, "Debug Browser must be enabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ScheduleMenuItem.Enabled, "Schedule must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.RunAllTestsMenuItem.Exists, "Run All Tests does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.RunAllTestsMenuItem.Enabled, "Run All Tests must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DuplicateMenuItem.Enabled, "Duplicate must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DuplicateMenuItem.Exists, "Duplicate does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DeployMenuItem.Exists, "Deploy does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DeployMenuItem.Enabled, "Deploy must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ShowDependenciesMenuItem.Enabled, "Show Dependencies must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ShowDependenciesMenuItem.Exists, "Show Dependencies does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ViewSwaggerMenuItem.Exists, "View Swagger does not exist on the Start Node context menu");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ViewSwaggerMenuItem.Enabled, "View Swagger must be disabled on a new workflow");
-            Assert.IsTrue(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.CopyURLtoClipboardMenuItem.Enabled, "Copy Url to Clipboard must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ScheduleMenuItem.Enabled, "Schedule must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.RunAllTestsMenuItem.Enabled, "Run All Tests must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DuplicateMenuItem.Enabled, "Duplicate must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.DeployMenuItem.Enabled, "Deploy must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ShowDependenciesMenuItem.Enabled, "Show Dependencies must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.ViewSwaggerMenuItem.Enabled, "View Swagger must be disabled on a new workflow");
+            Assert.IsFalse(UIMap.StartNodePopupWindow.CustomWindow.StartNodeItemMenu.CopyURLtoClipboardMenuItem.Enabled, "Copy Url to Clipboard must be disabled on a new workflow");
         }
 
         #region Additional test attributes
--- Dev/Warewolf.UITests/Tools/File, FTP, FTPS + SFTP/Copy.cs
+++ Dev/Warewolf.UITests/Tools/File, FTP, FTPS + SFTP/Copy.cs
@@ -11,6 +11,11 @@ namespace Warewolf.UITests.Tools
         public void CopyTool_OpenLargeViewUITest()
         {            
             UIMap.Open_Copy_Tool_Large_View();
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCopy.DoneButton.Exists, "DoneButton on the design surface does not exist");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCopy.LargeViewContentCustom.FileOrFolderComboBox.Exists, "FileOrFolderComboBox on the design surface does not exist");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCopy.LargeViewContentCustom.DestinationComboBox.Exists, "DestinationComboBox on the design surface does not exist");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCopy.LargeViewContentCustom.OnErrorCustom.Exists, "OnErrorCustom group on the design surface does not exist");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCopy.LargeViewContentCustom.OverwriteCheckBox.Exists, "OverwriteCheckBox on the design surface does not exist");
         }
 
         #region Additional test attributes
