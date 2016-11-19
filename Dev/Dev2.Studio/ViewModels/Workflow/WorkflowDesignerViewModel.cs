e255b3af5b1551fc2ec111f3c2e284167cd7ce2f
Pieter.Terblanche <pete.terblanche@gmail.com>
2016-11-19 14:12:48 +0200

    #WOLF-2256 Fix tools not dropping in large view

--- Dev/Dev2.Studio/ViewModels/Workflow/WorkflowDesignerViewModel.cs
+++ Dev/Dev2.Studio/ViewModels/Workflow/WorkflowDesignerViewModel.cs
@@ -2236,6 +2236,7 @@ namespace Dev2.Studio.ViewModels.Workflow
                 if (isWorkflow != null)
                 {
                     handled = WorkflowDropFromResourceToolboxItem(dataObject, isWorkflow, true, false);
+                    ApplyIsDraggedInstance(isWorkflow);
                 }
                 else
                 {
@@ -2243,6 +2244,19 @@ namespace Dev2.Studio.ViewModels.Workflow
                 }
             }
             return handled;
+        }
+
+        private static void ApplyIsDraggedInstance(string isWorkflow)
+        {
+            if (isWorkflow.Contains("DsfSqlServerDatabaseActivity") || isWorkflow.Contains("DsfMySqlDatabaseActivity")
+                || isWorkflow.Contains("DsfODBCDatabaseActivity") || isWorkflow.Contains("DsfOracleDatabaseActivity")
+                || isWorkflow.Contains("DsfPostgreSqlActivity") || isWorkflow.Contains("DsfWebDeleteActivity")
+                || isWorkflow.Contains("DsfWebGetActivity") || isWorkflow.Contains("DsfWebPostActivity")
+                || isWorkflow.Contains("DsfWebPutActivity") || isWorkflow.Contains("DsfComDllActivity")
+                || isWorkflow.Contains("DsfDotNetDllActivity") || isWorkflow.Contains("DsfWcfEndPointActivity"))
+            {
+                IsItemDragged.Instance.IsDragged = true;
+            }
         }
 
         [ExcludeFromCodeCoverage]
