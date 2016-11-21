6beaa31abca0424a6b53f5b51d672a43a89526fa
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-11-21 16:12:02 +0200

    New Toolbox tests

---
+++ Dev/Warewolf.UITests/Toolbox/ToolboxTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.Toolbox
+{
+    [CodedUITest]
+    public class ToolboxTests
+    {
+        [TestMethod]
+        [TestCategory("ToolBox")]
+        public void ClickToolboxClearButtonRemovesText()
+        {
+            UIMap.Drag_Toolbox_MultiAssign_Onto_DesignSurface();
+            UIMap.Click_Clear_Toolbox_Filter_Clear_Button();
+            Assert.IsTrue(string.IsNullOrEmpty(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.ToolBox.SearchTextBox.Text));
+        }
+
+        #region Additional test attributes
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+            UIMap.CloseHangingDialogs();
+            UIMap.Click_New_Workflow_Ribbon_Button();
+        }
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_uiMap == null)
+                {
+                    _uiMap = new UIMap();
+                }
+
+                return _uiMap;
+            }
+        }
+
+        private UIMap _uiMap;
+
+        #endregion
+    }
+}
