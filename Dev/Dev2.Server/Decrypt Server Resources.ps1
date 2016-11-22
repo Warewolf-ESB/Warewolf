9e085269d298bb28eb983480a6a297ea554e70fd
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-22 10:18:22 +0200

    start reverting recent decrypt server resources script changes

--- Dev/Dev2.Server/Decrypt Server Resources.ps1
+++ Dev/Dev2.Server/Decrypt Server Resources.ps1
@@ -3,34 +3,24 @@ $Invocation = (Get-Variable MyInvocation -Scope 0).Value
 $SolutionDirectory = (get-item (Split-Path $Invocation.MyCommand.Path)).parent.FullName
 Write-Host Got solution path as `"$SolutionDirectory`".
 
+Write-Host Starting nuget package restore using nuget.exe at `"$SolutionDirectory\.nuget\nuget.exe`".
+[System.Diagnostics.Process]::Start("""" + $SolutionDirectory + "\.nuget\nuget.exe""", "restore """ + $SolutionDirectory + "\Server.sln""")
+Write-Host Nuget package restore finished.
+
+Write-Host Starting compile using compiler at `"$env:vs120comntools..\IDE\devenv.com`".
+[System.Diagnostics.Process]::Start("""" + $env:vs120comntools + "..\IDE\devenv.com""", """" + $SolutionDirectory + "\Server.sln"" /Build ""Debug""")
+Write-Host Compile finished.
+
 Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll`".
-if (Test-Path "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll") {
-    Add-Type -Path "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll"
-} else {
-    Write-Host Cannot find assembly at "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll", please compile that before running this tool.
-    pause
-    exit 1
-}
+Add-Type -Path "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll"
 Write-Host Assembly loaded.
 
 Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll`".
-if (Test-Path "$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll") {
-    Add-Type -Path "$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll"
-} else {
-    Write-Host Cannot find assembly at "$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll", please compile that before running this tool.
-    pause
-    exit 1
-}
+Add-Type -Path "$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll"
 Write-Host Assembly loaded.
 
 Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll`".
-if (Test-Path "$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll") {
-    Add-Type -Path "$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll"
-} else {
-    Write-Host Cannot find assembly at "$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll", please compile that before running this tool.
-    pause
-    exit 1
-}
+Add-Type -Path "$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll"
 Write-Host Assembly loaded.
 
 Write-Host Loading types.
@@ -49,20 +39,19 @@ get-childitem "$SolutionDirectory\Resources - Debug\Resources","$env:ProgramData
     $Dev2SourceStringTransform = New-Object Dev2.Common.Utils.StringTransform
     $Dev2SourceStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "<Source ID=`"[a-fA-F0-9\-]+`" .*ConnectionString=`"([^`"]+)`" .*>"
     $Dev2SourceStringTransform.GroupNumbers = @(1)
-    $TransformFunction = [Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
-    $Dev2SourceStringTransform.TransformFunction = $TransformFunction
+    $Dev2SourceStringTransform.TransformFunction = [Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
     $Dev2AbstractFileActivityStringTransform = New-Object Dev2.Common.Utils.StringTransform
     $Dev2AbstractFileActivityStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfFileWrite|DsfFileRead|DsfFolderRead|DsfPathCopy|DsfPathCreate|DsfPathDelete|DsfPathMove|DsfPathRename|DsfZip|DsfUnzip) .*?Password=`"([^`"]+)`" .*?&gt;"
     $Dev2AbstractFileActivityStringTransform.GroupNumbers = @(1)
-    $Dev2AbstractFileActivityStringTransform.TransformFunction = $TransformFunction
+    $Dev2AbstractFileActivityStringTransform.TransformFunction = [Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
     $Dev2AbstractMultipleFilesActivityStringTransform = New-Object Dev2.Common.Utils.StringTransform
     $Dev2AbstractMultipleFilesActivityStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfPathCopy|DsfPathMove|DsfPathRename|DsfZip|DsfUnzip) .*?DestinationPassword=`"([^`"]+)`" .*?&gt;"
     $Dev2AbstractMultipleFilesActivityStringTransform.GroupNumbers = @(1)
-    $Dev2AbstractMultipleFilesActivityStringTransform.TransformFunction = $TransformFunction
+    $Dev2AbstractMultipleFilesActivityStringTransform.TransformFunction = [Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
     $Dev2ZipStringTransform = New-Object Dev2.Common.Utils.StringTransform
     $Dev2ZipStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfZip|DsfUnzip) .*?ArchivePassword=`"([^`"]+)`" .*?&gt;"
     $Dev2ZipStringTransform.GroupNumbers = @(1)
-    $Dev2ZipStringTransform.TransformFunction = $TransformFunction
+    $Dev2ZipStringTransform.TransformFunction = [Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
 
 	Write-Host Decrypting resource.
     $replacements = New-Object 'system.collections.generic.dictionary[[string],[Dev2.Common.Utils.StringTransform]]'
