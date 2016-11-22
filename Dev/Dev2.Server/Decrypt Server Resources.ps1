c61e7b05197f3e9b785b26d65308dae330f69038
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-22 10:19:41 +0200

    finished reverting this script

--- Dev/Dev2.Server/Decrypt Server Resources.ps1
+++ Dev/Dev2.Server/Decrypt Server Resources.ps1
@@ -15,17 +15,9 @@ Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Runtime.Services\bin\De
 Add-Type -Path "$SolutionDirectory\Dev2.Runtime.Services\bin\Debug\Dev2.Runtime.Services.dll"
 Write-Host Assembly loaded.
 
-Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll`".
-Add-Type -Path "$SolutionDirectory\Dev2.Warewolf.Security\bin\Debug\Warewolf.Security.dll"
-Write-Host Assembly loaded.
-
-Write-Host Loading assembly at `"$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll`".
-Add-Type -Path "$SolutionDirectory\Dev2.Common\bin\Debug\Dev2.Common.dll"
-Write-Host Assembly loaded.
-
-Write-Host Loading types.
+Write-Host Loading type.
 $ResourceHandler = New-Object Dev2.Runtime.ESB.Management.Services.FetchResourceDefinition
-Write-Host Types loaded.
+Write-Host Type loaded.
 
 Write-Host Recursing through resources.
 get-childitem "$SolutionDirectory\Resources - Debug\Resources","$env:ProgramData\Warewolf\Resources","$SolutionDirectory\Dev2.Server\bin\Debug\Resources" -recurse | where {$_.extension -eq ".xml"} | % {
@@ -35,37 +27,8 @@ get-childitem "$SolutionDirectory\Resources - Debug\Resources","$env:ProgramData
 	[void]$sb.Append([string](Get-Content $_.FullName))
 	Write-Host Resource read.
 	
-    Write-Host Creating Dev2 String Transforms
-    $Dev2SourceStringTransform = New-Object Dev2.Common.Utils.StringTransform
-    $Dev2SourceStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "<Source ID=`"[a-fA-F0-9\-]+`" .*ConnectionString=`"([^`"]+)`" .*>"
-    $Dev2SourceStringTransform.GroupNumbers = @(1)
-    $Dev2SourceStringTransform.TransformFunction = function:[Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
-    $Dev2AbstractFileActivityStringTransform = New-Object Dev2.Common.Utils.StringTransform
-    $Dev2AbstractFileActivityStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfFileWrite|DsfFileRead|DsfFolderRead|DsfPathCopy|DsfPathCreate|DsfPathDelete|DsfPathMove|DsfPathRename|DsfZip|DsfUnzip) .*?Password=`"([^`"]+)`" .*?&gt;"
-    $Dev2AbstractFileActivityStringTransform.GroupNumbers = @(1)
-    $Dev2AbstractFileActivityStringTransform.TransformFunction = function:[Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
-    $Dev2AbstractMultipleFilesActivityStringTransform = New-Object Dev2.Common.Utils.StringTransform
-    $Dev2AbstractMultipleFilesActivityStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfPathCopy|DsfPathMove|DsfPathRename|DsfZip|DsfUnzip) .*?DestinationPassword=`"([^`"]+)`" .*?&gt;"
-    $Dev2AbstractMultipleFilesActivityStringTransform.GroupNumbers = @(1)
-    $Dev2AbstractMultipleFilesActivityStringTransform.TransformFunction = function:[Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
-    $Dev2ZipStringTransform = New-Object Dev2.Common.Utils.StringTransform
-    $Dev2ZipStringTransform.SearchRegex = New-Object System.Text.RegularExpressions.Regex "&lt;([a-zA-Z0-9]+:)?(DsfZip|DsfUnzip) .*?ArchivePassword=`"([^`"]+)`" .*?&gt;"
-    $Dev2ZipStringTransform.GroupNumbers = @(1)
-    $Dev2ZipStringTransform.TransformFunction = function:[Warewolf.Security.Encryption.DpapiWrapper]::DecryptIfEncrypted
-
 	Write-Host Decrypting resource.
-    $replacements = New-Object 'system.collections.generic.dictionary[[string],[Dev2.Common.Utils.StringTransform]]'
-    $replacements.Add("Source", $Dev2SourceStringTransform)
-    $replacements.Add("DsfAbstractFileActivity", $Dev2AbstractFileActivityStringTransform)
-    $replacements.Add("DsfAbstractMultipleFilesActivity", $Dev2AbstractMultipleFilesActivityStringTransform)
-    $replacements.Add("Zip", $Dev2ZipStringTransform)
-    $ReplacementValues = New-Object 'system.collections.generic.list[Dev2.Common.Utils.StringTransform]'
-    $ReplacementValues.Add($Dev2SourceStringTransform)
-    $ReplacementValues.Add($Dev2AbstractFileActivityStringTransform)
-    $ReplacementValues.Add($Dev2AbstractMultipleFilesActivityStringTransform)
-    $ReplacementValues.Add($Dev2ZipStringTransform)
-    $xml = [Dev2.Common.Utils.StringTransform]::TransformAllMatches($sb.ToString(), $ReplacementValues)
-    $sb.Append($xml)
+	$sb = $ResourceHandler.DecryptAllPasswords($sb)
 	Write-Host Resource decrypted.
 
 	Write-Host Writing decrypted resource.
