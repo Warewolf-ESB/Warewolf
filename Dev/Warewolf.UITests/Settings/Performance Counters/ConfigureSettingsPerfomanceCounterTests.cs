2a62b10b79d801a886314f0dc314c4b37de98723
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-11-24 16:06:51 +0200

    action recording for selecting an iteom from the service picker can pick items in sub folders automatically

--- Dev/Warewolf.UITests/Settings/Performance Counters/ConfigureSettingsPerfomanceCounterTests.cs
+++ Dev/Warewolf.UITests/Settings/Performance Counters/ConfigureSettingsPerfomanceCounterTests.cs
@@ -19,7 +19,7 @@ namespace Warewolf.UITests
         {
             UIMap.Click_Select_Resource_Button();
             var serviceName = "Hello World";
-            UIMap.Select_Service_From_Service_Picker(serviceName, true);
+            UIMap.Select_Service_From_Service_Picker(serviceName);
             Assert.AreEqual(serviceName, UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.SettingsTab.WorksurfaceContext.SettingsView.TabList.PerfomanceCounterTab.PerfmonViewContent.ResourceTable.Row1.ResourceCell.ResourceTextBox.DisplayText, "Resource Name is not set to Dice after selecting Dice from Service picker");
         }
 
