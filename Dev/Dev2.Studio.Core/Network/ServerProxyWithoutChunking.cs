21ada93dbc7afbf3daa1edc5d855a1455530d195
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-11-24 11:22:22 +0200

    resolved WOLF-2221

--- Dev/Dev2.Studio.Core/Network/ServerProxyWithoutChunking.cs
+++ Dev/Dev2.Studio.Core/Network/ServerProxyWithoutChunking.cs
@@ -596,7 +596,7 @@ namespace Dev2.Network
 
             Dev2Logger.Debug("Execute Command Payload [ " + payload + " ]");
 
-            var result = Task.Run(async () => await ExecuteCommandAsync(payload, workspaceId)).ConfigureAwait(false).GetAwaiter().GetResult();
+            var result = Task.Run(async () => await ExecuteCommandAsync(payload, workspaceId)).Result;
             return result;
 
         }
