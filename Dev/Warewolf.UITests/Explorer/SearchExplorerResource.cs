895f8132694d60548ebd7faafd1eda33d4b5a923
nkosinathi.sangweni <nkosinathi.sangweni@Dev2.co.za>
2016-11-25 09:26:23 +0200

    new Spes and explorer tests

---
+++ Dev/Warewolf.UITests/Explorer/SearchExplorerResource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable InconsistentNaming
+
+namespace Warewolf.UITests
+{
+    [CodedUITest]
+    public class SearchExplorerResource
+    {
+        [TestMethod]
+        [TestCategory("Explorer")]
+        public void Search_ExplorerResource()
+        {
+            UIMap.Filter_Explorer("Error WF");
+            UIMap.ExplorerItemCountEquals();
+        }
+
+        [TestMethod]
+        [TestCategory("Explorer")]
+        public void Search_ExplorerFolder()
+        {
+            UIMap.Filter_Explorer("Examples");
+            UIMap.ExplorerItemCountEquals();
+        }
+      
+
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+            UIMap.CloseHangingDialogs();
+        }
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_UIMap == null)
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        #endregion
+    }
+}
