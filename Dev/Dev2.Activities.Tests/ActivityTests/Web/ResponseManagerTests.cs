657c209c0c3aa8600aa5c9651024da7a52c402c0
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2016-11-30 15:16:18 +0200

    resolve issue with empty response returning error

--- Dev/Dev2.Activities.Tests/ActivityTests/Web/ResponseManagerTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/Web/ResponseManagerTests.cs
@@ -80,7 +80,7 @@ namespace Dev2.Tests.Activities.ActivityTests.Web
 
         [TestMethod]
         [Owner("Nkosinathi Sangweni")]
-        [ExpectedException(typeof(NullReferenceException))]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void PushResponseIntoEnvironment_GivenNoDataObject_ShouldCatchObjectNullException()
         {
             //---------------Set up test pack-------------------
@@ -191,56 +191,7 @@ namespace Dev2.Tests.Activities.ActivityTests.Web
             }
             //---------------Test Result -----------------------
         }
-
-        [TestMethod]
-        [Owner("Nkosinathi Sangweni")]
-        public void PushXmlIntoEnvironment_GivenNoResponse_ShouldAddEnvironmentError()
-        {
-            //---------------Set up test pack-------------------
-
-            const string response = "<CurrentWeather>" +
-                                   "<Location>&lt;Paris&gt;</Location>" +
-                                   "<Time>May 29, 2013 - 09:00 AM EDT / 2013.05.29 1300 UTC</Time>" +
-                                   "<Wind>from the NW (320 degrees) at 10 MPH (9 KT) (direction variable):0</Wind>" +
-                                   "<Visibility>&lt;greater than 7 mile(s):0&gt;</Visibility>" +
-                                   "<Temperature> 59 F (15 C)</Temperature>" +
-                                   "<DewPoint> 41 F (5 C)</DewPoint>" +
-                                   "<RelativeHumidity> 51%</RelativeHumidity>" +
-                                   "<Pressure> 29.65 in. Hg (1004 hPa)</Pressure>" +
-                                   "<Status>Success</Status>" +
-                                   "</CurrentWeather>";
-            var environment = new ExecutionEnvironment();
-            environment.Assign("[[City]]", "PMB", 0);
-            environment.Assign("[[CountryName]]", "South Africa", 0);
-            var serviceOutputs = new List<IServiceOutputMapping> { new ServiceOutputMapping("Location", "[[weather().Location]]", "weather"), new ServiceOutputMapping("Time", "[[weather().Time]]", "weather"), new ServiceOutputMapping("Wind", "[[weather().Wind]]", "weather"), new ServiceOutputMapping("Visibility", "[[Visibility]]", "") };
-
-            var serviceXml = XmlResource.Fetch("WebService");
-            var service = new WebService(serviceXml) { RequestResponse = response };
-            var outPutDesc = service.GetOutputDescription();
-            var responseManager = new ResponseManager()
-            {
-                OutputDescription = outPutDesc,
-                Outputs = serviceOutputs
-            };
-            var dataObjectMock = new Mock<IDSFDataObject>();
-            dataObjectMock.Setup(o => o.Environment).Returns(environment);
-            dataObjectMock.Setup(o => o.EsbChannel).Returns(new Mock<IEsbChannel>().Object);
-            dataObjectMock.Setup(o => o.Environment.AddError("No Web Response received"));
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(responseManager);
-            //---------------Execute Test ----------------------
-            try
-            {
-                responseManager.PushResponseIntoEnvironment(string.Empty, 0, dataObjectMock.Object);
-                dataObjectMock.Verify(o => o.Environment.AddError("No Web Response received"));
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(e.Message);
-            }
-            //---------------Test Result -----------------------
-        }
-
+        
         [TestMethod]
         [Owner("Nkosinathi Sangweni")]
         public void PushXmlIntoEnvironment_GivenNull_ShouldLoggError()
