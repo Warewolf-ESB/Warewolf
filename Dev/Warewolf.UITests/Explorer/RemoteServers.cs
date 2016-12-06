7ee6e3e21294a2b1fb1ad913d73a0679ad61b64e
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-12-06 11:17:04 +0200

    Remote Servers Spec prep

--- Dev/Warewolf.UITests/Explorer/RemoteServers.cs
+++ Dev/Warewolf.UITests/Explorer/RemoteServers.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Warewolf.UITests
@@ -36,6 +34,17 @@ namespace Warewolf.UITests
             UIMap.Click_Explorer_RemoteServer_Connect_Button();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ConnectControl.ServerComboBox.SelectedItemAsRemoteConnectionIntegration.Exists, "Remote server name still ends with (Connected) in explorer remote server dropdown list after clicking the disconnect button.");
             UIMap.Select_localhost_From_Explorer_Remote_Server_Dropdown_List();
+        }
+        [TestMethod]
+        [TestCategory("Explorer")]
+        public void Disconnected_Remote_Server_Seperately_UITest()
+        {
+            UIMap.Filter_Explorer("workflow1");
+            UIMap.Select_RemoteConnectionIntegration_From_Explorer();
+            UIMap.Click_Explorer_RemoteServer_Connect_Button();
+            UIMap.WaitForSpinner(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.FirstRemoteServer.Checkbox.Spinner);
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ConnectControl.ServerComboBox.SelectedItemAsRemoteConnectionIntegrationConnected.Exists, "Remote server name does not end in (Connected) in explorer remote server dropdown list after clicking the connect button and waiting for the spinner.");
+
         }
 
         #region Additional test attributes
