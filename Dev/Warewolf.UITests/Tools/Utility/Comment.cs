b19288884a85bd37490e9e7dc971c6bc7d7f0b23
Sanele.Mthembu <Sanele.Mthembu@RSAKLFSANELE>
2016-12-08 09:24:58 +0200

    Removed Invalid Comment Tool Test

--- Dev/Warewolf.UITests/Tools/Utility/Comment.cs
+++ Dev/Warewolf.UITests/Tools/Utility/Comment.cs
@@ -24,21 +24,6 @@ namespace Warewolf.UITests.Tools.Utility
             Assert.AreEqual("Comment", UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.CommentTreeItem.CommentButton.DisplayText);
         }
 
-        [TestMethod]
-        [TestCategory("Utility Tools")]
-        public void ToolDesigners_ResizeComment_MakesTheWorkflowDirty_UITest()
-        {            
-            UIMap.Enter_Text_Into_CommentTool("Some comment, some comment, some comment,");
-            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.TabDescription.DisplayText.Contains("*"));
-            UIMap.Save_With_Ribbon_Button_And_Dialog(CommentToolWf);
-            Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.TabDescription.DisplayText.Contains("*"));
-            var newHeight = UIMap.Expand_Comment_Tool_Size();            
-            UIMap.Click_Close_Workflow_Tab_Button();
-            UIMap.Filter_Explorer(CommentToolWf);
-            UIMap.Open_Explorer_First_Item_With_Context_Menu();
-            Assert.AreEqual(newHeight, UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.Comment.LargeViewContentCustom.Height);
-        }
-
         #region Additional test attributes
 
         [TestInitialize]
