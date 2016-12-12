a3ee5030c26e752070bc7e1c1e075ba4ca444c72
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-12-12 10:09:12 +0200

    consolidate *.Tests project build outputs into one folder, same for *.Specs and Warewolf.UIBinding.* projects into two more respective folders

---
+++ Dev/Warewolf.Specs/Properties/TestScripts/Tests/MSTest/Coded UI/Run-Data-Tools-UI-Tests.ps1
@@ -0,0 +1,53 @@
+﻿if ([string]::IsNullOrEmpty($PSScriptRoot)) {
+	$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$SolutionDir = (Get-Item $PSScriptRoot).parent.parent.parent.parent.FullName
+# Read playlists and args.
+$TestList = ""
+if ($Args.Count -gt 0) {
+    $TestList = $Args.ForEach({ "," + $_ })
+} else {
+    Get-ChildItem "$PSScriptRoot" -Filter *.playlist | `
+    Foreach-Object{
+	    [xml]$playlistContent = Get-Content $_.FullName
+	    if ($playlistContent.Playlist.Add.count -gt 0) {
+	        foreach( $TestName in $playlistContent.Playlist.Add) {
+		        $TestList += " /test:" + $TestName.Test.SubString($TestName.Test.LastIndexOf(".") + 1)
+	        }
+	    } else {        
+            if ($playlistContent.Playlist.Add.Test -ne $null) {
+                $TestList = " /test:" + $playlistContent.Playlist.Add.Test.SubString($playlistContent.Playlist.Add.Test.LastIndexOf(".") + 1)
+            } else {
+	            Write-Host Error parsing Playlist.Add from playlist file at $_.FullName
+	            Continue
+            }
+        }
+    }
+}
+if ($TestList.StartsWith(",")) {
+	$TestList = $TestList -replace "^.", " /Tests:"
+}
+
+# Create test settings.
+$TestSettingsFile = "$PSScriptRoot\LocalToolsUITesting.testsettings"
+[system.io.file]::WriteAllText($TestSettingsFile,  @"
+<?xml version=`"1.0`" encoding="UTF-8"?>
+<TestSettings
+  id=`"3264dd0f-6fc1-4cb9-b44f-c649fef29609`"
+  name=`"ExampleWorkflowExecutionSpecs`"
+  enableDefaultDataCollectors=`"false`"
+  xmlns=`"http://microsoft.com/schemas/VisualStudio/TeamTest/2010`">
+  <Description>Run example workflow execution specs.</Description>
+  <Deployment enabled=`"false`" />
+  <NamingScheme baseName=`"UI`" appendTimeStamp=`"false`" useDefault=`"false`" />
+  <Execution>
+    <Timeouts testTimeout=`"300000`" />
+  </Execution>
+</TestSettings>
+"@)
+
+# Create full VSTest argument string.
+$FullArgsList = "/testcontainer:`"" + $SolutionDir + "\Warewolf.UITests\bin\Debug\Warewolf.UITests.dll`" /resultsfile:TestResults\DataToolUITestsResults.trx /testsettings:`"" + $TestSettingsFile + "`"" + $TestList + " /category:`"Data Tools`""
+
+# Write full command including full argument string.
+Out-File -LiteralPath $PSScriptRoot\RunTests.bat -Encoding default -InputObject `"$env:vs140comntools..\IDE\MSTest.exe`"$FullArgsList
