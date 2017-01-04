65ba9eef077ce52a75bc8e2ae4665651031ac5b4
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-04 08:19:45 +0200

    convert this batch script to powershell

---
+++ Dev/TestScripts/Tests/Regenerate All SpecFlow Feature Files.ps1
@@ -0,0 +1,24 @@
+﻿if ([string]::IsNullOrEmpty($PSScriptRoot)) {
+	$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$RepoDir = (Get-Item $PSScriptRoot ).parent.parent.parent.FullName
+
+Set-Location "$RepoDir"
+Dev\TestScripts\Tests\nuget.exe restore Dev\AcceptanceTesting.sln
+if (-not $env:errorlevel -eq 0) {
+    pause
+    exit 1
+}
+foreach ($ProjectDir in get-ChildItem "Dev\*.Specs") {
+    $FullPath = $ProjectDir.FullName
+    $ProjectName = $ProjectDir.Name
+	Dev\packages\SpecFlow.2.1.0\tools\specflow.exe generateAll "$FullPath\$ProjectName.csproj" /force /verbose
+}
+foreach ($ProjectDir in get-ChildItem "Dev\Warewolf.UIBindingTests.*") {
+    $FullPath = $ProjectDir.FullName
+    $ProjectName = $ProjectDir.Name
+	Dev\packages\SpecFlow.2.1.0\tools\specflow.exe generateAll "$FullPath\$ProjectName.csproj" /force /verbose
+}
+Dev\packages\SpecFlow.2.1.0\tools\specflow.exe generateAll Dev\Warewolf.ToolsSpecs\Warewolf.ToolsSpecs.csproj /force /verbose
+Dev\packages\SpecFlow.2.1.0\tools\specflow.exe generateAll Dev\Warewolf.SecuritySpecs\Warewolf.SecuritySpecs.csproj /force /verbose
+Dev\packages\SpecFlow.2.1.0\tools\specflow.exe generateAll Dev\Warewolf.UISpecs\Warewolf.UISpecs.csproj /force /verbose
