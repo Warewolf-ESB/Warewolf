45c0067827227e28298313efda567d98c32f55fb
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-05 15:31:43 +0200

    change url to download msbuild tools to 2015 tools in compile script

--- Dev/TestScripts/Tests/Compile All.bat
+++ Dev/TestScripts/Tests/Compile All.bat
@@ -11,7 +11,7 @@ if not %errorlevel%==0 pause & exit 1
 "%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0..\..\UITesting.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 ) else (
 set errorlevel=1
-echo MSBuild not found. Download from: https://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe
+echo MSBuild not found. Download from: https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe
 )
 
 if not %errorlevel%==0 pause & exit 1
