8b70c02561a0e4278017901058bcb86dfa00b2e3
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-05 15:26:38 +0200

    update compile script to show error message and download location of msbuild tools if they are not found

--- Dev/TestScripts/Tests/Compile All.bat
+++ Dev/TestScripts/Tests/Compile All.bat
@@ -9,6 +9,9 @@ MSBuild "%~dp0..\..\UITesting.sln" /p:Platform="Any CPU";Configuration="Debug" /
 "%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0..\..\AcceptanceTesting.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
 if not %errorlevel%==0 pause & exit 1
 "%programfiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" "%~dp0..\..\UITesting.sln" /p:Platform="Any CPU";Configuration="Debug" /maxcpucount
+) else (
+set errorlevel=1
+echo MSBuild not found. Download from: https://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe
 )
 
 if not %errorlevel%==0 pause & exit 1
