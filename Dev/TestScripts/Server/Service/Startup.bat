28c517b5b73da3b477472973da13aaed8c4cc7ac
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-06 09:41:49 +0200

    still patching server and studio startup scripts to use different resources

--- Dev/TestScripts/Server/Service/Startup.bat
+++ Dev/TestScripts/Server/Service/Startup.bat
@@ -84,12 +84,10 @@ GOTO StartService
 
 :StartService
 REM ** Try Refresh Warewolf Server Bin Resources and Tests
-IF EXIST "%~dp0..\..\..\Resources - ServerTests\Resources" echo d | xcopy /S /Y "%~dp0..\..\..\Resourses - ServerTests\Resources" "%DeploymentDirectory%\Resources - ServerTests"
-IF EXIST "%~dp0..\..\..\Resources - ServerTests\Tests" echo d | xcopy /S /Y "%~dp0..\..\..\Resources - ServerTests\Tests" "%DeploymentDirectory%\Tests - ServerTests"
+IF EXIST "%~dp0..\..\..\Resources - ServerTests" echo d | xcopy /S /Y "%~dp0..\..\..\Resourses - ServerTests" "%DeploymentDirectory%\Resources - ServerTests"
 
 REM ** Try Refresh Warewolf ProgramData Resources and Tests
-IF NOT EXIST "%ProgramData%\Warewolf\Resources" IF EXIST "%DeploymentDirectory%\Resources - ServerTests" echo d | xcopy /S /Y "%DeploymentDirectory%\Resources - ServerTests" "%ProgramData%\Warewolf\Resources"
-IF NOT EXIST "%ProgramData%\Warewolf\Tests" IF EXIST "%DeploymentDirectory%\Tests - ServerTests" echo d | xcopy /S /Y "%DeploymentDirectory%\Tests - ServerTests" "%ProgramData%\Warewolf\Tests"
+IF NOT EXIST "%ProgramData%\Warewolf\Resources" IF EXIST "%DeploymentDirectory%\Resources - ServerTests" echo d | xcopy /S /Y "%DeploymentDirectory%\Resources - ServerTests" "%ProgramData%\Warewolf"
 
 REM ** Start the server service
 IF EXIST %windir%\nircmd.exe (nircmd elevate sc start "Warewolf Server") else (sc start "Warewolf Server")
