dbf2d6c082daef8e864581e7370d3ab539c93a36
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-09 14:42:30 +0200

    bug in test runner scripts with running lists of test names

--- Dev/TestScripts/Tests/VSTest/Coded UI/Run-DB-Connector-UI-Specs.ps1
+++ Dev/TestScripts/Tests/VSTest/Coded UI/Run-DB-Connector-UI-Specs.ps1
@@ -49,7 +49,11 @@ $TestSettingsFile = "$PSScriptRoot\DBConnectorUISpecs.testsettings"
 "@)
 
 # Create full VSTest argument string.
-$FullArgsList = " `"" + $SolutionDir + "\Warewolf.UISpecs\bin\Debug\Warewolf.UISpecs.dll`" /logger:trx /Settings:`"" + $TestSettingsFile + "`"" + $TestList + " /TestCaseFilter:`"TestCategory=DBConnector`""
+if ($TestList -eq "") {
+	$FullArgsList = " `"" + $SolutionDir + "\Warewolf.UISpecs\bin\Debug\Warewolf.UISpecs.dll`" /logger:trx /Settings:`"" + $TestSettingsFile + "`"" + " /TestCaseFilter:`"TestCategory=DBConnector`""
+} else {
+	$FullArgsList = " `"" + $SolutionDir + "\Warewolf.UISpecs\bin\Debug\Warewolf.UISpecs.dll`" /logger:trx /Settings:`"" + $TestSettingsFile + "`"" + $TestList
+}
 
 # Display full command including full argument string.
 Write-Host $SolutionDir> `"$env:vs140comntools..\IDE\CommonExtensions\Microsoft\TestWindow\VSTest.console.exe`" $FullArgsList
