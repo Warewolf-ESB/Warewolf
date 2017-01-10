1f80a202a8eaec03f234777c90a457dff425853c
Dev One <d1@dev2.co.za>
2017-01-10 13:17:55 +0200

    WOLF-2336 - Temporarily Removed MergeFolders_InUnfileredExplorer_UITest

--- Dev/Warewolf.UITests/Explorer/Folders.cs
+++ Dev/Warewolf.UITests/Explorer/Folders.cs
@@ -9,15 +9,15 @@ namespace Warewolf.UITests
     {
         const string HelloWorld = "Hello World";
         const string ResourceCreatedInFolder = "Resource Created In Folder";
-        [TestMethod]
-        [TestCategory("Explorer")]
-        public void MergeFolders_InUnfileredExplorer_UITest()
-        {
-            UIMap.Filter_Explorer("DragAndDropMergeFolder");
-            UIMap.Drag_Explorer_First_Item_Onto_Second_Item();
-            UIMap.Filter_Explorer("DragAndDropMergeResource1");
-            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.FirstSubItem.FirstItem.Exists, "Resource did not merge into folder after drag and drop in the explorer UI.");
-        }
+        //[TestMethod]
+        //[TestCategory("Explorer")]
+        //public void MergeFolders_InUnfileredExplorer_UITest()
+        //{
+        //    UIMap.Filter_Explorer("DragAndDropMergeFolder");
+        //    UIMap.Drag_Explorer_First_Item_Onto_Second_Item();
+        //    UIMap.Filter_Explorer("DragAndDropMergeResource1");
+        //    Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.FirstSubItem.FirstItem.Exists, "Resource did not merge into folder after drag and drop in the explorer UI.");
+        //}
 
         [TestMethod]
         [TestCategory("Explorer")]
