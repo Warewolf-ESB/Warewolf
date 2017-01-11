68e061275d66910b7205c7693c979d05cb561d20
DDelport <Dylan.Delport@dev2.co.za>
2017-01-11 10:12:44 +0200

    Updated tests for ODBC, Oracle and PostgreSQL

--- Dev/Warewolf.UITests/PostgreSQLSource/PostgreSQLSourceTests.cs
+++ Dev/Warewolf.UITests/PostgreSQLSource/PostgreSQLSourceTests.cs
@@ -15,8 +15,14 @@ namespace Warewolf.UITests
             UIMap.Select_NewPostgreSQLSource_FromExplorerContextMenu();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.ManageDatabaseSourceControl.ServerComboBox.Enabled, "PostgreSQL Server Address combobox is disabled new PostgreSQL Source wizard tab");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.UserRadioButton.Enabled, "User authentification rabio button is not enabled.");
-            Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.WindowsRadioButton.Exists, "Windows authentification type radio button not enabled.");
+            //Assert.IsFalse(UIMap.ControlExistsNow(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.WindowsRadioButton), "Windows authentification radio button is visible.");
             Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.TestConnectionButton.Enabled, "Test Connection Button is enabled.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.UserNameTextBox.Enabled, "Username textbox is not enabled.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.PasswordTextBox.Enabled, "Password textbos is not enabled.");
+            UIMap.Enter_Text_Into_DatabaseServer_Tab();
+            UIMap.IEnterRunAsUserUsernameAndPasswordOnDatabaseSource();
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.TestConnectionButton.Enabled, "Test Connection Button is not enabled.");
+            UIMap.Click_DB_Source_Wizard_Test_Connection_Button();
         }
 
         #region Additional test attributes
