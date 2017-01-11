5e0aec662811fbeb78a95cbd3fa3ef41b95a2278
DDelport <Dylan.Delport@dev2.co.za>
2017-01-11 10:12:44 +0200

    Updated tests for ODBC, Oracle and PostgreSQL

--- Dev/Warewolf.UITests/ODBCSource/ODBCSourceTests.cs
+++ Dev/Warewolf.UITests/ODBCSource/ODBCSourceTests.cs
@@ -13,6 +13,10 @@ namespace Warewolf.UITests
         public void ODBCSource_CreateSourceUITests()
         {
             UIMap.Select_NewODBCSource_FromExplorerContextMenu();
+            Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.ManageDatabaseSourceControl.ServerComboBox.Enabled, "ODBC server combobox is enabled");
+            //Assert.IsFalse(UIMap.ControlExistsNow(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.UserRadioButton), "User authentification radio button is visible.");
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DBSourceWizardTab.WorkSurfaceContext.TestConnectionButton.Enabled, "Test Connection Button is enabled.");
+            UIMap.Click_DB_Source_Wizard_Test_Connection_Button();
         }
 
         #region Additional test attributes
