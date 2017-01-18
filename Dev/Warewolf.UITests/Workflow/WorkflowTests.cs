5d1b0c946fd80252709a15962a22d61e9e2c3525
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-01-18 10:42:02 +0200

    do not comment out tests. ever.

--- Dev/Warewolf.UITests/Workflow/WorkflowTests.cs
+++ Dev/Warewolf.UITests/Workflow/WorkflowTests.cs
@@ -9,21 +9,21 @@ namespace Warewolf.UITests.Workflow
         private const string Folder = "Acceptance Tests";
         private const string HelloWorld = "Hello World";
 
-        //[TestMethod]
-        //[TestCategory("Tabs and Panes")]
-        //public void Workflow_Name_Counter()
-        //{
-        //    UIMap.Create_New_Workflow_Using_Shortcut();
-        //    Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.UIUnsaved2Text.Exists, "Second new workflow tab is not Unsaved 2");
-        //}
+        [TestMethod]
+        [TestCategory("Tabs and Panes")]
+        public void Workflow_Name_Counter()
+        {
+            UIMap.Create_New_Workflow_Using_Shortcut();
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.UIUnsaved2Text.Exists, "Second new workflow tab is not Unsaved 2");
+        }
 
-        //[TestMethod]
-        //[TestCategory("Tabs and Panes")]
-        //public void Unsaved_Workflow_Name_Asterisk()
-        //{
-        //    UIMap.Create_New_Workflow_Using_Shortcut();
-        //    UIMap.Make_Workflow_Savable_By_Dragging_Start();
-        //}
+        [TestMethod]
+        [TestCategory("Tabs and Panes")]
+        public void Unsaved_Workflow_Name_Asterisk()
+        {
+            UIMap.Create_New_Workflow_Using_Shortcut();
+            UIMap.Make_Workflow_Savable_By_Dragging_Start();
+        }
 
         [TestMethod]
         public void Shortcut_Control_W_Opens_NewWorkflow()
