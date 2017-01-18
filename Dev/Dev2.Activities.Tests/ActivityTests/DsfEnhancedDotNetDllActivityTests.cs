0dd184cb708b66acf485b05991b8d60ea4c25be3
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-01-18 09:50:43 +0200

    WOLF-2357 Coverage

--- Dev/Dev2.Activities.Tests/ActivityTests/DsfEnhancedDotNetDllActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DsfEnhancedDotNetDllActivityTests.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Dev2.Activities;
 using Dev2.Common;
 using Dev2.Common.ExtMethods;
 using Dev2.Common.Interfaces;
+using Dev2.Common.Interfaces.DB;
 using Dev2.Data.TO;
+using Dev2.Data.Util;
+using Dev2.DataList.Contract;
 using Dev2.Interfaces;
 using Dev2.Runtime.ServiceModel.Data;
 using Dev2.Runtime.ServiceModel.Esb.Brokers.Plugin;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TestingDotnetDllCascading;
+using Warewolf.Core;
 using Warewolf.Storage;
+using WarewolfParserInterop;
 
 // ReSharper disable InconsistentNaming
 
@@ -108,15 +114,66 @@ namespace Dev2.Tests.Activities.ActivityTests
 
         [TestMethod]
         [Owner("Nkosinathi Sangweni")]
-        public void Execute_GivenNoConstructor_ShouldCreateEmptyConstructor()
+        public void Execute_GivenIsExistingObject_ShouldEvalFromEnvironment()
         {
             //---------------Set up test pack-------------------
             var type = typeof(Human);
             var activity = new DsfEnhancedDotNetDllActivityMock();
             var mock = new Mock<IDSFDataObject>();
             var esbChannel = new Mock<IEsbChannel>();
+            var env = new Mock<IExecutionEnvironment>();
+            var human = new Human("Micky", "Mouse", new Food());
+            var humanString = DataListUtil.ConvertModelToJson(human).ToString();
+            var newWarewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString(humanString));
+            env.Setup(environment => environment.EvalForJson(It.IsAny<string>(), It.IsAny<bool>()))
+                .Returns(newWarewolfAtomResult);
             mock.SetupGet(o => o.EsbChannel).Returns(esbChannel.Object);
-            mock.SetupGet(o => o.Environment).Returns(new ExecutionEnvironment());
+
+            mock.SetupGet(o => o.Environment).Returns(env.Object);
+            activity.Namespace = new NamespaceItem
+            {
+                FullName = type.FullName,
+                AssemblyLocation = type.Assembly.Location,
+                AssemblyName = type.Assembly.FullName,
+                MethodName = "ToString"
+            };
+            activity.MethodsToRun = new List<Dev2MethodInfo>();
+            activity.Constructor = new PluginConstructor()
+            {
+                IsExistingObject = true,
+                ConstructorName = "@name"
+            };
+            //---------------Assert Precondition----------------
+            Assert.AreEqual("DotNet DLL Connector", activity.Type.Expression.ToString());
+            Assert.AreEqual("DotNet DLL", activity.DisplayName);
+            Assert.IsNotNull(activity.ConstructorInputs);
+            //---------------Execute Test ----------------------
+            ErrorResultTO err;
+            activity.ExecuteMock(esbChannel.Object, mock.Object, string.Empty, string.Empty, out err);
+            //---------------Test Result -----------------------
+            env.Verify(environment => environment.EvalForJson("[[@name]]", It.IsAny<bool>()));
+            Assert.AreEqual(0,activity.MethodsToRun.Count);
+        }
+
+       
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Execute_GivenNoConstructor_ShouldDefaultEmptyConstructor()
+        {
+            //---------------Set up test pack-------------------
+            var type = typeof(Human);
+            var activity = new DsfEnhancedDotNetDllActivityMock();
+            var mock = new Mock<IDSFDataObject>();
+            var esbChannel = new Mock<IEsbChannel>();
+            var env = new Mock<IExecutionEnvironment>();
+            var human = new Human("Micky", "Mouse", new Food());
+            var humanString = DataListUtil.ConvertModelToJson(human).ToString();
+            var newWarewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString(humanString));
+            env.Setup(environment => environment.EvalForJson(It.IsAny<string>(), It.IsAny<bool>()))
+                .Returns(newWarewolfAtomResult);
+            mock.SetupGet(o => o.EsbChannel).Returns(esbChannel.Object);
+            mock.SetupGet(o => o.Environment).Returns(env.Object);
             activity.Namespace = new NamespaceItem
             {
                 FullName = type.FullName,
@@ -129,15 +186,15 @@ namespace Dev2.Tests.Activities.ActivityTests
             //---------------Assert Precondition----------------
             Assert.AreEqual("DotNet DLL Connector", activity.Type.Expression.ToString());
             Assert.AreEqual("DotNet DLL", activity.DisplayName);
+            Assert.IsNotNull(activity.ConstructorInputs);
             Assert.IsNull(activity.Constructor);
-            Assert.IsNotNull(activity.Inputs);
             //---------------Execute Test ----------------------
             ErrorResultTO err;
 
             activity.ExecuteMock(esbChannel.Object, mock.Object, string.Empty, string.Empty, out err);
             //---------------Test Result -----------------------
             Assert.IsNotNull(activity.Constructor);
-            Assert.IsNotNull(activity.Inputs);
+            Assert.IsNotNull(activity.ConstructorInputs);
         }
 
         [TestMethod]
@@ -190,15 +247,82 @@ namespace Dev2.Tests.Activities.ActivityTests
             var human = new Human("Micky", "Mouse", new Food { FoodName = "Lettuce" });
             var knownBinder = new KnownTypesBinder();
             knownBinder.KnownTypes.Add(type);
-            var serializeToJsonString = human.SerializeToJsonString(knownBinder);
             var activity = new DsfEnhancedDotNetDllActivityMock();
             var mock = new Mock<IDSFDataObject>();
             var esbChannel = new Mock<IEsbChannel>();
             var executionEnv = new Mock<IExecutionEnvironment>();
-            executionEnv.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                        .Returns(CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString(serializeToJsonString)));
+            var humanString = DataListUtil.ConvertModelToJson(human).ToString();
+            var newWarewolfAtomResult = CommonFunctions.WarewolfEvalResult
+                .NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString(humanString));
+            executionEnv.Setup(environment => environment.EvalForJson(It.IsAny<string>(), It.IsAny<bool>()))
+               .Returns(newWarewolfAtomResult);
             mock.SetupGet(o => o.EsbChannel).Returns(esbChannel.Object);
             mock.Setup(o => o.Environment).Returns(executionEnv.Object);
+            activity.Namespace = new NamespaceItem
+            {
+                FullName = type.FullName,
+                AssemblyLocation = type.Assembly.Location,
+                AssemblyName = type.Assembly.FullName,
+                MethodName = "ToString"
+            };
+            activity.MethodsToRun = new List<Dev2MethodInfo>();
+            activity.Constructor = new PluginConstructor
+            {
+                IsExistingObject = true,
+                ConstructorName = "@Human"
+            };
+            activity.MethodsToRun = new List<Dev2MethodInfo>
+            {
+                new Dev2MethodInfo
+                {
+                    Method = "ToString",
+                    Parameters = new List<MethodParameter>(),
+                }
+            };
+            //---------------Assert Precondition----------------
+            Assert.AreEqual("DotNet DLL Connector", activity.Type.Expression.ToString());
+            Assert.AreEqual("DotNet DLL", activity.DisplayName);
+            //---------------Execute Test ----------------------
+            ErrorResultTO err;
+            activity.ExecuteMock(esbChannel.Object, mock.Object, string.Empty, string.Empty, out err);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(0, err.FetchErrors().Count);
+            var methodResult = activity.MethodsToRun.Single().MethodResult;
+            Assert.AreEqual("\"Name:Micky, Surname:Mouse, FoodName:Lettuce\"", methodResult);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Execute_GivenParameters_ShouldUseExistingObject()
+        {
+            //---------------Set up test pack-------------------
+            var type = typeof(Human);
+            var human = new Human("Micky", "Mouse", new Food { FoodName = "Lettuce" });
+            var knownBinder = new KnownTypesBinder();
+            knownBinder.KnownTypes.Add(type);
+            var activity = new DsfEnhancedDotNetDllActivityMock();
+            var mock = new Mock<IDSFDataObject>();
+            var esbChannel = new Mock<IEsbChannel>();
+            var executionEnv = new Mock<IExecutionEnvironment>();
+            var humanString = DataListUtil.ConvertModelToJson(human).ToString();
+            var newWarewolfAtomResult = CommonFunctions.WarewolfEvalResult
+                .NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString(humanString));
+            executionEnv.Setup(environment => environment.EvalForJson(It.IsAny<string>(), It.IsAny<bool>()))
+               .Returns(newWarewolfAtomResult);
+            var johnResult = CommonFunctions.WarewolfEvalResult
+                .NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("John"));
+            executionEnv.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .Returns(johnResult);
+            mock.SetupGet(o => o.EsbChannel).Returns(esbChannel.Object);
+            mock.Setup(o => o.Environment).Returns(executionEnv.Object);
+            activity.ConstructorInputs = new List<IServiceInput>()
+            {
+                new ServiceInput("name","John")
+                {
+                    TypeName = typeof(string).FullName,
+                    RequiredField = true
+                }
+            };
             activity.Namespace = new NamespaceItem
             {
                 FullName = type.FullName,
