9462749374fc2a0255943f633e41bef5f7a67cee
Ashley Lewis <ultrakiff@gmail.com>
2017-01-22 15:11:34 +0200

    Update Run-All-Other-Unit-Tests.ps1

--- Dev/TestScripts/Tests/VSTest/CI/Run-All-Other-Unit-Tests.ps1
+++ Dev/TestScripts/Tests/VSTest/CI/Run-All-Other-Unit-Tests.ps1
@@ -24,7 +24,7 @@ if ($TestList.StartsWith(",")) {
 }
 
 # Create assemblies list.
-$TestAssembliesList = ''
+$TestAssembliesPath = ''
 if (Test-Path "$PSScriptRoot\Warewolf.Tests\Warewolf.*.Tests.dll") {
 	$TestAssembliesPath = "$PSScriptRoot\Warewolf.Tests"
 }
@@ -58,6 +58,7 @@ elseif (Test-Path "$PSScriptRoot\..\..\..\..\Warewolf.*.Tests.dll") {
 elseif (Test-Path "$PSScriptRoot\..\..\..\..\..\Warewolf.*.Tests.dll") {
 	$TestAssembliesPath = "$PSScriptRoot\..\..\..\..\.."
 }
+$TestAssembliesList = ''
 foreach ($file in Get-ChildItem $TestAssembliesPath -Filter Warewolf.*.Tests.dll ) {
 	if ($file.Name -ne "Warewolf.Studio.ViewModels.Tests.dll") {
 		$TestAssembliesList = $TestAssembliesList + " `"" + $file.FullName + "`""
