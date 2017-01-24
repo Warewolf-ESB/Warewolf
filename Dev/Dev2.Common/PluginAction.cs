373c311a5f231e1fa526cd6506dd6a1e519b5567
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-01-24 09:19:55 +0200

    WOLF-2242 Fix Method selection not showing the selected item

--- Dev/Dev2.Common/PluginAction.cs
+++ Dev/Dev2.Common/PluginAction.cs
@@ -8,97 +8,92 @@ using Dev2.Common.Interfaces.DB;
 // ReSharper disable once CheckNamespace
 namespace Dev2.Common.Interfaces
 {
-    public class PluginAction : IPluginAction, INotifyPropertyChanged
+    public class PluginAction : IPluginAction, INotifyPropertyChanged, IEquatable<PluginAction>
     {
-        string _fullName;
-        string _method;
-        IList<IServiceInput> _inputs;
-        Type _returnType;
-        IList<INameValue> _variables;
-        
-        #region Implementation of IPluginAction
-
-        public string FullName
-        {
-            get
-            {
-                return _fullName;
-            }
-            set
-            {
-                _fullName = value;
-            }
-        }
-        public string Method
-        {
-            get
-            {
-                return _method;
-            }
-            set
-            {
-                _method = value;
-            }
-        }
-        public IList<IServiceInput> Inputs
-        {
-            get
-            {
-                return _inputs;
-            }
-            set
-            {
-                _inputs = value;
-            }
-        }
-        public Type ReturnType
-        {
-            get
-            {
-                return _returnType;
-            }
-            set
-            {
-                _returnType = value;
-            }
-        }
-        public IList<INameValue> Variables
-        {
-            get
-            {
-                return _variables;
-            }
-            set
-            {
-                _variables = value;
-            }
-        }
+        public string FullName { get; set; }
+        public string Method { get; set; }
+        public IList<IServiceInput> Inputs { get; set; }
+        public Type ReturnType { get; set; }
+        public IList<INameValue> Variables { get; set; }
         public string Dev2ReturnType { get; set; }
-
-        public string GetIdentifier()
-        {
-            //Rather add SourceId instead of full name
-            return FullName + Method;
-        }
-
+        
         public string MethodResult { get; set; }
         public string OutputVariable { get; set; }
         public bool IsObject { get; set; }
         public bool IsVoid { get; set; }
-
-        #endregion
-
         public event PropertyChangedEventHandler PropertyChanged;
-
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        public bool Equals(PluginAction other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if (GetHashCode() == other.GetHashCode())
+                return true;
 
+            return string.Equals(Method, other.Method);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((PluginAction)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Inputs?.GetHashCode() ?? 0) * 397) ^ (Method?.GetHashCode() ?? 0);
+            }
+        }
+        public string GetIdentifier()
+        {
+            return FullName + Method;
+        }
+        public static bool operator ==(PluginAction left, PluginAction right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(PluginAction left, PluginAction right)
+        {
+            return !Equals(left, right);
+        }
+
+        #region Overrides of Object
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
         public override string ToString()
         {
             return Method;
         }
+
+        #endregion
     }
 }
