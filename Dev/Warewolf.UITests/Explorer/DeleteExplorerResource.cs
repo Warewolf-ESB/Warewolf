bc059d1079eb39a51cbbf6cf6c10a49bd249ddaf
Dev One <d1@dev2.co.za>
2017-01-25 14:15:29 +0200

    Added new resource "DeleteAnywayResourceFolder"

--- Dev/Warewolf.UITests/Explorer/DeleteExplorerResource.cs
+++ Dev/Warewolf.UITests/Explorer/DeleteExplorerResource.cs
@@ -14,7 +14,7 @@ namespace Warewolf.UITests
         const string flowSwitch = "DeleteExplorerResourceTestFile";
         const string flowSequence = "DeleteResourceRemovalTestFile";
         const string uiTestDependencyOne = "UITestDependencyOne";
-        const string uiTestDependencyFolder = "UITestDependency";
+        const string DeleteAnywayResourceFolder = "DeleteAnyway";
 
         [TestMethod]
         [TestCategory("Explorer")]
@@ -55,21 +55,6 @@ namespace Warewolf.UITests
             Assert.IsTrue(UIMap.MessageBoxWindow.OKButton.Exists, "OK button does not exist.");
             Assert.IsTrue(UIMap.MessageBoxWindow.DeleteAnywayText.Exists, "Error Deleting Confirmation MessageBox does not exist");
             UIMap.Click_DeleteAnyway_MessageBox_OK();
-        }
-
-        [TestMethod]
-        [TestCategory("Explorer")]
-        public void DeletedFolderShowDependencies()
-        {
-            UIMap.Filter_Explorer(uiTestDependencyFolder);
-            UIMap.Delete_FirstResource_From_ExplorerContextMenu();
-            UIMap.Click_MessageBox_Yes();
-            Assert.IsTrue(UIMap.MessageBoxWindow.Applytoall.Exists, "Apply To All button does not exist.");
-            Assert.IsTrue(UIMap.MessageBoxWindow.DeleteAnyway.Exists, "Delete Anyway button does not exist.");
-            Assert.IsTrue(UIMap.MessageBoxWindow.ShowDependencies.Exists, "Show Dependencies button does not exist.");
-            Assert.IsTrue(UIMap.MessageBoxWindow.OKButton.Exists, "OK button does not exist.");
-            UIMap.Click_MessageBox_DeleteAnyway();
-            Assert.IsFalse(UIMap.ControlExistsNow(UIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem), "Item did not delete");
         }
 
         #region Additional test attributes
