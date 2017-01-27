7b2d14b19ad2c76a3c821c95e343ef3696d20c19
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-01-27 09:02:23 +0200

    WOLF-2242 resolved constructor visibility state

---
+++ Dev/Warewolf.UITests/WorkflowTesting/DotnetTestingTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.UITests.WorkflowTesting
+{
+    /// <summary>
+    /// Summary description for DotnetTestingTests
+    /// </summary>
+    [CodedUITest]
+    public class DotnetTestingTests
+    {
+        #region Additional test attributes
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+            UIMap.AssertStudioIsRunning();
+            //UIMap.Filter_Explorer(HelloWorld);
+            UIMap.DoubleClick_Explorer_Localhost_First_Item();
+            UIMap.Press_F6();
+            UIMap.Click_Create_Test_From_Debug();
+        }
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_UIMap == null)
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        #endregion
+    }
+}
