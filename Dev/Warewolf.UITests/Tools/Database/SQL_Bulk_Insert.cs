376b54693cb5ab963d83faa1791a2d3f3196aa5f
Dev One <d1@dev2.co.za>
2017-02-01 09:02:29 +0200

    Changed CreateNewTestThenCreateTestFromDebugOutput to use existing Resource instead of creating a new one

--- Dev/Warewolf.UITests/Tools/Database/SQL_Bulk_Insert.cs
+++ Dev/Warewolf.UITests/Tools/Database/SQL_Bulk_Insert.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Warewolf.UITests.Tools
