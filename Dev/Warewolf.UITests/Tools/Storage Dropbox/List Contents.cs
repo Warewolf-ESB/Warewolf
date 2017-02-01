e74fd13ba00ac73a82a16646ec02b08f4d0d9a3b
DDelport <Dylan.Delport@dev2.co.za>
2017-02-01 10:58:01 +0200

    Completed Dropbox Tool Restructure. 8 tests removed.

--- Dev/Warewolf.UITests/Tools/Storage Dropbox/List Contents.cs
+++ Dev/Warewolf.UITests/Tools/Storage Dropbox/List Contents.cs
@@ -8,16 +8,12 @@ namespace Warewolf.UITests.Tools.Storage_Dropbox
     {
         [TestMethod]
         [TestCategory("Dropbox Tools")]
-        public void Dropbox_ListContentsTool_SmallView()
+        public void DropboxListContentsTool_Small_And_LargeView_Then_NewSource_UITest()
         {
+            //Small View
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.SmallViewContent.DropboxPathIntellisenseCombobox.Textbox.Exists, "Dropbox file path textbox does not exist on Dropbox FileList tool small view after dragging in from the toolbox.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.SmallViewContent.ResultsIntellisenseCombobox.Textbox.Exists, "Result textbox does not exist on Dropbox FileList tool small view after dragging in from the toolbox.");
-        }
-
-        [TestMethod]
-        [TestCategory("Dropbox Tools")]
-        public void Dropbox_ListContentsTool_LargeView()
-        {
+            //Large View
             UIMap.DropboxListContentsTool_ChangeView_With_DoubleClick();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.LargeViewContent.SourceCombobox.Exists, "Source combobox does not exist on Dropbox FileList tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.LargeViewContent.EditSourceButton.Exists, "Edit Source Button does not exist on Dropbox FileList tool large view after openning the large view with a double click.");
@@ -27,13 +23,7 @@ namespace Warewolf.UITests.Tools.Storage_Dropbox
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.LargeViewContent.SourceCombobox.Exists, "Source combobox does not exist on Dropbox FileList tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.LargeViewContent.OnErrorPane.OnErrorGroup.Exists, "OnError Group does not exist on Dropbox FileList tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.DoneButton.Exists, "Done Button does not exist on Dropbox FileList tool large view after openning the large view with a double click.");
-        }
-
-        [TestMethod]
-        [TestCategory("Dropbox Tools")]
-        public void NewSource_From_DropboxListContentsTool_UITest()
-        {
-            UIMap.DropboxListContentsTool_ChangeView_With_DoubleClick();
+            //New Source
             UIMap.Click_NewSourceButton_From_DropboxListContentsTool();
             Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.OAuthSourceWizardTab.WorkSurfaceContext.AuthoriseButton.Enabled, "Authorise button is enabled");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.OAuthSourceWizardTab.WorkSurfaceContext.ServerTypeComboBox.Enabled, "Server Type Combobox is not enabled");
--- Dev/Warewolf.UITests/Tools/Storage Dropbox/Upload.cs
+++ Dev/Warewolf.UITests/Tools/Storage Dropbox/Upload.cs
@@ -8,16 +8,12 @@ namespace Warewolf.UITests.Tools.Storage_Dropbox
     {
         [TestMethod]
         [TestCategory("Dropbox Tools")]
-        public void Dropbox_UploadTool_SmallView()
+        public void DropboxUploadTool_Small_And_LargeView_Then_NewSource_UITest()
         {
+            //Small View
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.SmallViewContent.LocalFileIntellisenseCombobox.Textbox.Exists, "Local file path textbox does not exist on Dropbox Upload tool small view after dragging in from the toolbox.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.SmallViewContent.DropboxFileIntellisenseCombobox.Textbox.Exists, "Dropbox file path textbox does not exist on Dropbox Upload tool small view after dragging in from the toolbox.");
-        }
-
-        [TestMethod]
-        [TestCategory("Dropbox Tools")]
-        public void Dropbox_UploadTool_LargeView()
-        {
+            //Large View
             UIMap.DropboxUploadTool_ChangeView_With_DoubleClick();
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.LargeViewContent.SourceCombobox.Exists, "Source combobox does not exist on Dropbox Upload tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.LargeViewContent.EditSourceButton.Exists, "Edit Source Button does not exist on Dropbox Upload tool large view after openning the large view with a double click.");
@@ -27,13 +23,7 @@ namespace Warewolf.UITests.Tools.Storage_Dropbox
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.LargeViewContent.SourceCombobox.Exists, "Source combobox does not exist on Dropbox Upload tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.LargeViewContent.OnErrorPane.OnErrorGroup.Exists, "OnError Group does not exist on Dropbox Upload tool large view after openning the large view with a double click.");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.DoneButton.Exists, "Done Button does not exist on Dropbox Upload tool large view after openning the large view with a double click.");
-        }
-
-        [TestMethod]
-        [TestCategory("Dropbox Tools")]
-        public void NewSource_From_DropboxUploadTool_UITest()
-        {
-            UIMap.DropboxUploadTool_ChangeView_With_DoubleClick();
+            //New Source
             UIMap.Click_NewSourceButton_From_DropboxUploadTool();
             Assert.IsFalse(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.OAuthSourceWizardTab.WorkSurfaceContext.AuthoriseButton.Enabled, "Authorise button is enabled");
             Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.OAuthSourceWizardTab.WorkSurfaceContext.ServerTypeComboBox.Enabled, "Server Type Combobox is not enabled");
