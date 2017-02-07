3fc4ed96302d02c21264e26c91150183a6b08e12
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-02-07 09:08:20 +0200

    WOLF-WIP resolving tets failures

--- Dev/Warewolf.Studio.ViewModels.Tests/ManageRabbitMQSourceViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ManageRabbitMQSourceViewModelTests.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
+using Dev2.Common.Interfaces.Threading;
 using Dev2.Threading;
 
 // ReSharper disable InconsistentNaming
@@ -28,7 +29,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         private List<string> _changedProperties;
         private ManageRabbitMQSourceViewModel _manageRabbitMQSourceViewModelWithTask;
         private ManageRabbitMQSourceViewModel _manageRabbitMQSourceViewModelWithRabbitMQServiceSourceDefinition;
-
+        private Mock<IAsyncWorker> _asyncWorkerMock;
         #endregion Fields
 
         #region Test initialize
@@ -43,7 +44,19 @@ namespace Warewolf.Studio.ViewModels.Tests
             _changedProperties = new List<string>();
             _manageRabbitMQSourceViewModelWithTask = new ManageRabbitMQSourceViewModel(_rabbitMQSourceModel.Object, _requestServiceNameViewModelTask);
             _manageRabbitMQSourceViewModelWithTask.PropertyChanged += (sender, e) => { _changedProperties.Add(e.PropertyName); };
-            _manageRabbitMQSourceViewModelWithRabbitMQServiceSourceDefinition = new ManageRabbitMQSourceViewModel(_rabbitMQSourceModel.Object, _rabbitMQServiceSourceDefinition.Object, new SynchronousAsyncWorker());
+            _asyncWorkerMock = new Mock<IAsyncWorker>();
+            _rabbitMQSourceModel.Setup(model => model.FetchSource(It.IsAny<Guid>()))
+            .Returns(_rabbitMQServiceSourceDefinition.Object);
+            _asyncWorkerMock.Setup(worker =>
+                                   worker.Start(
+                                            It.IsAny<Func<IRabbitMQServiceSourceDefinition>>(),
+                                            It.IsAny<Action<IRabbitMQServiceSourceDefinition>>()))
+                            .Callback<Func<IRabbitMQServiceSourceDefinition>, Action<IRabbitMQServiceSourceDefinition>>((func, action) =>
+                            {
+                                var dbSource = func.Invoke();
+                                action(dbSource);
+                            });
+            _manageRabbitMQSourceViewModelWithRabbitMQServiceSourceDefinition = new ManageRabbitMQSourceViewModel(_rabbitMQSourceModel.Object, _rabbitMQServiceSourceDefinition.Object, _asyncWorkerMock.Object);
         }
 
         #endregion Test initialize
