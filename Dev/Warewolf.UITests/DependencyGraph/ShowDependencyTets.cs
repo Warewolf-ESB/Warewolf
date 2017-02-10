fd355a70208fc4298c93095ff1c062f8310b6b8e
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-02-10 10:02:59 +0200

    WOLF-2474 tests for source dependencies and opening dependencies from source tabs.

--- Dev/Warewolf.UITests/DependencyGraph/ShowDependencyTets.cs
+++ Dev/Warewolf.UITests/DependencyGraph/ShowDependencyTets.cs
@@ -14,12 +14,40 @@ namespace Warewolf.UITests.DependencyGraph
         public void Explorer_GivenSources_ShouldHaveShowDependencyMenuItem()
         {
             //---------------Set up test pack-------------------
-            const string Source = "NewDotNetPluginSource";
+            const string Source = "DotNetPluginSource";
             UIMap.Filter_Explorer(Source);
             UIMap.RightClick_Explorer_Localhost_FirstItem();
             Mouse.Click(UIMap.MainStudioWindow.ExplorerContextMenu.ShowDependencies, new Point(50, 15));
             var displayText = UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DependencyGraphTab.WorksurfaceContext.DependencyView.ScrollViewer.NodesCustom.DotnetWorkflowForTesText.DisplayText;
-            Assert.AreEqual("NewDotnetWorkflowForTesting", displayText);
+            Assert.AreEqual("DotnetWorkflowForTesting", displayText);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Explorer_GivenDotnetWorkflowAsDependencyIsDoubleClicked_ShouldOpenWorkflowTabWithToolsInside()
+        {
+            //---------------Set up test pack-------------------
+            const string Source = "DotNetPluginSource";
+            UIMap.Filter_Explorer(Source);
+            UIMap.RightClick_Explorer_Localhost_FirstItem();
+            Mouse.Click(UIMap.MainStudioWindow.ExplorerContextMenu.ShowDependencies, new Point(50, 15));
+            Mouse.DoubleClick(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DependencyGraphTab.WorksurfaceContext.DependencyView.ScrollViewer.NodesCustom.DotnetWorkflowForTesText);
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.Exists);
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DotNetDll.Exists);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Explorer_GivenSourceIsDoubleClicked_ShouldOpenSourceTab()
+        {
+            //---------------Set up test pack-------------------
+            const string Source = "DotNetPluginSource";
+            UIMap.Filter_Explorer(Source);
+            UIMap.RightClick_Explorer_Localhost_FirstItem();
+            Mouse.Click(UIMap.MainStudioWindow.ExplorerContextMenu.ShowDependencies, new Point(50, 15));
+            Mouse.DoubleClick(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DependencyGraphTab.WorksurfaceContext.DependencyView.ScrollViewer.DotnetSourceNode.DotNetPluginSourceText);
+            Assert.IsTrue(UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DotNetPluginSourceTab.Exists);
+            Assert.AreEqual(@"C:\ProgramData\Warewolf\Resources\TestingDotnetDllCascading.dll", UIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DotNetPluginSourceTab.WorkSurfaceContext.AssemblyComboBox.TextEdit.Text);
         }
 
         
