0c0079b83c46c4b3bdc29687b5994c106096f744
DDelport <Dylan.Delport@dev2.co.za>
2017-02-15 13:51:49 +0200

    WOLF-2373 - ViewDebugInBrowser updated tests and created BrowserDebugSteps.cs.
    Removed two resources from server tests as feature now creates resources.

---
+++ Dev/Dev2.Activities.Specs/BrowserDebug/BrowserDebugSteps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dev2.Common.Interfaces.Diagnostics.Debug;
+using Dev2.Communication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Dev2.Activities.Specs.BrowserDebug
+{
+    [Binding]
+    public sealed class BrowserDebugSteps
+    {
+        private SpecExternalProcessExecutor _externalProcessExecutor = new SpecExternalProcessExecutor();
+
+        [Then(@"I Debug ""(.*)"" in Browser")]
+        [When(@"I Debug ""(.*)"" in Browser")]
+        [Given(@"I Debug ""(.*)"" in Browser")]
+        public void ThenIDebugInBrowser(string urlString)
+        {
+            _externalProcessExecutor.OpenInBrowser(new Uri(urlString));
+        }
+
+        [Then(@"The Debug in Browser content contains ""(.*)""")]
+        public void ThenTheDebugInBrowserContentContains(string containedText)
+        {
+            Assert.IsTrue(_externalProcessExecutor.WebResult.First().Contains(containedText),
+                _externalProcessExecutor.WebResult.First());
+        }
+
+        private List<IDebugState> GetDebugStates()
+        {
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+            var deserialize = serializer.Deserialize<List<IDebugState>>(_externalProcessExecutor.WebResult.First());
+            return deserialize.ToList();
+        }
+
+        [Given(@"The Debug in Browser content contains has error messagge ""(.*)""")]
+        [When(@"The Debug in Browser content contains has error messagge ""(.*)""")]
+        [Then(@"The Debug in Browser content contains has error messagge ""(.*)""")]
+        public void WhenTheDebugInBrowserContentContainsHasErrorMessagge(string p0)
+        {
+            var debugStates = GetDebugStates();
+            var msg = debugStates[1].ErrorMessage;
+            Assert.IsTrue(debugStates.Last().HasError);
+            Assert.AreEqual("invalid variable assigned to d@teMonth", msg);
+        }
+
+        [Given(@"The Debug in Browser content contains has children with no Inputs and Ouputs")]
+        [When(@"The Debug in Browser content contains has children with no Inputs and Ouputs")]
+        [Then(@"The Debug in Browser content contains has children with no Inputs and Ouputs")]
+        public void TheDebugInBrowserContentHaveGivenVariable()
+        {
+            var deserialize = GetDebugStates();
+            Assert.IsNotNull(deserialize);
+            foreach (var debugState in deserialize)
+            {
+                Assert.AreEqual(0, debugState.Inputs.Count);
+                Assert.AreEqual(0, debugState.Outputs.Count);
+            }
+        }
+
+        [Given(@"The Debug in Browser content contains has inputs and outputs")]
+        [When(@"The Debug in Browser content contains has inputs and outputs")]
+        [Then(@"The Debug in Browser content contains has inputs and outputs")]
+        public void ThenTheDebugInBrowserContentContainsHasInputsAndOutputs()
+        {
+            var allDebugStates = GetDebugStates();
+            foreach (var debugState in allDebugStates)
+            {
+                if (debugState.IsFirstStep())
+                    continue;
+                if (!debugState.IsFinalStep())
+                    Assert.IsTrue(debugState.Inputs.Count > 0);
+                Assert.IsTrue(debugState.Outputs.Count > 0);
+            }
+        }
+    }
+}
