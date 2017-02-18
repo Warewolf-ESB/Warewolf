8ce15ab2ba223f257fceef22a24869fe48bdee60
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-02-18 12:45:00 +0200

    WOLF-2373 changed DebugState parent Id to benullable to cater allow creation

---
+++ Dev/Dev2.Runtime.Tests/JsonResource.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Dev2.Tests.Runtime
+{
+    public class JsonResource
+    {
+        /// <summary>
+        /// Fetches the contents of the embedded XML file with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the XML file excluding extension.</param>
+        /// <returns>The contents of the embedded XML file.</returns>
+        public static string Fetch(string name)
+        {
+            var resourceName = $"Dev2.Tests.Runtime.Json.{name}.json";
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    return string.Empty;
+                }
+
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
