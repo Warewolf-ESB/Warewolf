145979d8f2c7995963aee81e03ea244e9b9fc808
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-02-18 12:40:30 +0200

    WOLF2373 Stash pop

--- Dev/Dev2.Common/DebugStateTreeBuilder.cs
+++ Dev/Dev2.Common/DebugStateTreeBuilder.cs
@@ -12,31 +12,67 @@ namespace Dev2.Common
         public static IEnumerable<IDebugState> BuildTree(IEnumerable<IDebugState> source)
         {
 
-            var groups = source.GroupBy(i => i.ParentID);
-            var roots = groups.First(g => g.Key == Guid.Empty).ToList();
 
-            if (roots.Any())
+            //var groups = source.GroupBy(i => i.ParentID);
+
+            //var roots = groups.First(g => g.Key == Guid.Empty).ToList();
+
+            //if (roots.Any())
+            //{
+            //    var dict = groups.Where(g => g.Key != Guid.Empty).ToDictionary(g => g.Key.ToString(), g => g.ToList());
+            //    for (var i = 0; i < roots.Count(); i++)
+            //        AddChildren(roots[i], dict);
+            //}
+            //var debugStates = roots.GroupBy(state => state.Children).Select(states => states.First());
+
+            return BuildTreeAndReturnRootNodes(source);
+        }
+
+        private static IEnumerable<IDebugState> BuildTreeAndReturnRootNodes(IEnumerable<IDebugState> flatItems)
+        {
+            var debugStates = flatItems.ToList();
+            var byIdLookup = debugStates.ToLookup(i => i.ID);
+            foreach (var item in debugStates)
             {
-                var dict = groups.Where(g => g.Key.HasValue).ToDictionary(g => g.Key.Value, g => g.ToList());
-                for (var i = 0; i < roots.Count(); i++)
-                    AddChildren(roots[i], dict);
+                if (item.ParentID != Guid.Empty)
+                {
+                    var parent = byIdLookup[item.ParentID].First();
+                    if (parent.Children == null)
+                    {
+                        parent.Children = new List<IDebugState>();
+                    }
+                    parent.Children.Add(item);
+                }
             }
-            var debugStates = roots.GroupBy(state => state.Children).Select(states => states.First());
+            var clone = debugStates.ToArray().Clone();
+            var states = clone as IDebugState[];
+            if (states != null)
+
+                foreach (var debugState in states)
+                {
+                    var any = debugStates.Any(state => state.Children?.Any(p => p.ID == debugState.ID) ?? false);
+                    if (any)
+                    {
+                        debugStates.Remove(debugState);
+                    }
+                }
+
+
             return debugStates;
         }
 
-        private static void AddChildren(IDebugState node, IDictionary<Guid, List<IDebugState>> source)
+        private static void AddChildren(IDebugState node, IDictionary<string, List<IDebugState>> source)
         {
-            if (source.ContainsKey(node.ID))
-            {
-                node.Children = source[node.ID];
-                for (var i = 0; i < node.Children.Count; i++)
-                    AddChildren(node.Children[i], source);
-            }
-            else
-            {
-                node.Children = new List<IDebugState>();
-            }
+            //if (source.ContainsKey(node.ID.ToString()))
+            //{
+            //    node.Children = source[node.ID.ToString()];
+            //    for (var i = 0; i < node.Children.Count; i++)
+            //        AddChildren(node.Children[i], source);
+            //}
+            //else
+            //{
+            //    node.Children = new List<IDebugState>();
+            //}
         }
     }
 }
