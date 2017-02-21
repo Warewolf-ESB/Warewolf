91d5b035f5bc47ebf1b4b55abb29b8a7cdaec874
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-02-21 16:02:20 +0200

    update script to skip param properties

--- Dev/Warewolf.UITests/AssertExistsOnAllUIMapPropertyGets.ps1
+++ Dev/Warewolf.UITests/AssertExistsOnAllUIMapPropertyGets.ps1
@@ -4,7 +4,7 @@
         $fileName = $_
         (Get-Content $fileName) | 
             Foreach-Object {
-                if ($_.ToString().Contains("if ((this.") -and $_.ToString().Contains("== null))")) 
+                if ($_.ToString().Contains("if ((this.") -and $_.ToString().Contains("== null))") -and !($_.ToString().Contains("Params == null))")))
                 {
                     $ObjectName = $_.ToString().Substring($_.ToString().IndexOf("(this.")+"(this.".Length,$_.ToString().IndexOf(" == null)")-$_.ToString().IndexOf("(this.")-"(this.".Length)
                     #Add Or Not Exists
