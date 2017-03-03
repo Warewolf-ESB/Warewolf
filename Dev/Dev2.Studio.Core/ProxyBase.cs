179df7a294f435b2301a6c58268ef6f5756b372b
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2017-03-03 16:17:27 +0200

    Added missing cs files

---
+++ Dev/Dev2.Studio.Core/ProxyBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Dev2.Controller;
+using Dev2.Studio.Interfaces;
+
+namespace Dev2.Studio.Core
+{
+    public abstract class ProxyBase
+    {
+
+        public ICommunicationControllerFactory CommunicationControllerFactory { get; private set; }
+        // ReSharper disable once NotAccessedField.Local
+        protected readonly IEnvironmentConnection Connection;
+
+        protected ProxyBase(ICommunicationControllerFactory communicationControllerFactory, IEnvironmentConnection connection)
+        {
+            VerifyArgument.AreNotNull(new Dictionary<string, object>{{"communicationControllerFactory",communicationControllerFactory},{"connection",connection}});
+            CommunicationControllerFactory = communicationControllerFactory;
+            Connection = connection;
+        }
+
+    }
+}
