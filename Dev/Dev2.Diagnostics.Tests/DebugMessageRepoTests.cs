98d0df1b88c040f02b5b8d0742bac319824f208d
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-03 14:30:14 +0200

    WOLF-2373 added new test class

---
+++ Dev/Dev2.Diagnostics.Tests/DebugMessageRepoTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Dev2.Diagnostics.Debug;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2.Diagnostics.Test
+{
+    [TestClass]
+    public class DebugMessageRepoTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Constructor_GivenIsNew_ShouldInitiase()
+        {
+            //---------------Set up test pack-------------------
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(webDebugMessageRepo);
+            //---------------Execute Test ----------------------
+            var instance = WebDebugMessageRepo.Instance;
+            Assert.IsNotNull(instance);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(ReferenceEquals(instance, webDebugMessageRepo));
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void AddDebugItem_GivenValidArgs_ShouldAdd1Item()
+        {
+            //---------------Set up test pack-------------------
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(webDebugMessageRepo);
+            //---------------Execute Test ----------------------
+            var debugState = new DebugState();
+            var clientId = Guid.NewGuid();
+            var sessionId = Guid.NewGuid();
+            webDebugMessageRepo.AddDebugItem(clientId, sessionId, debugState);
+            //---------------Test Result -----------------------
+            var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
+            Assert.AreEqual(1, fetchDebugItems.Count);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void AddDebugItem_GivenValidArgsAndSessions_ShouldNotMixUpDebugStates()
+        {
+            //---------------Set up test pack-------------------
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(webDebugMessageRepo);
+            //---------------Execute Test ----------------------
+            var id = Guid.NewGuid();
+            var debugState = new DebugState() { ID = id };
+            var clientId = Guid.NewGuid();
+            var sessionId = Guid.NewGuid();
+            webDebugMessageRepo.AddDebugItem(clientId, sessionId, debugState);
+            var clientId1 = Guid.NewGuid();
+            var sessionId1 = Guid.NewGuid();
+            var id1 = Guid.NewGuid();
+            var debugState1 = new DebugState() { ID = id1 };
+            webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, debugState1);
+            //---------------Test Result -----------------------
+            var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
+            Assert.AreEqual(1, fetchDebugItems.Count);
+            fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId1, sessionId1);
+            Assert.AreEqual(1, fetchDebugItems.Count);
+        }
+    }
+}
