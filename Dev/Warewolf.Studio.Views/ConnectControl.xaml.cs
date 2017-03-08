707fcc1608b7844af01430a38ca8cef1d3a49cd5
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-03-08 15:31:44 +0200

    WOLF-2511 Remove unused code

--- Dev/Warewolf.Studio.Views/ConnectControl.xaml.cs
+++ Dev/Warewolf.Studio.Views/ConnectControl.xaml.cs
@@ -8,11 +8,7 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
-using System.ComponentModel;
 using System.Windows;
-using Dev2.ConnectionHelpers;
-using Dev2.Studio.Interfaces;
 
 namespace Warewolf.Studio.Views
 {
@@ -39,7 +35,6 @@ namespace Warewolf.Studio.Views
         public static readonly DependencyProperty ServerComboBoxAutomationIDProperty =
             DependencyProperty.Register("ServerComboBoxAutomationID", typeof(string), typeof(ConnectControl), new PropertyMetadata("UI_ServerCbx_AutoID"));
 
-
         public string EditButtonAutomationID
         {
             get { return (string)GetValue(EditButtonAutomationIDProperty); }
@@ -56,56 +51,11 @@ namespace Warewolf.Studio.Views
             get { return (string)GetValue(ConnectButtonAutomationIDProperty); }
             set { SetValue(ConnectButtonAutomationIDProperty, value); }
         }
-        public IServer SelectedServer
-        {
-            get
-            {
-                return (IServer)TheServerComboBox.SelectedItem;
-            }
-            set
-            {
-                _selectedServer = value;
-            }
-        }
 
         // Using a DependencyProperty as the backing store for ConnectButtonAutomationID.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ConnectButtonAutomationIDProperty =
             DependencyProperty.Register("ConnectButtonAutomationID", typeof(string), typeof(ConnectControl),
                 new PropertyMetadata("UI_ConnectServerBtn_AutoID"));
-        IServer _selectedServer;
-
-        #endregion
-
-        public IConnectControlEnvironment SelectServer(string server)
-        {
-            foreach(var item in TheServerComboBox.Items)
-            {
-                var env = item as IConnectControlEnvironment;
-                if (env != null && env.DisplayName == server)
-                    try
-                    {
-                        TheServerComboBox.SelectedItem = env;
-                        return env;
-                    }
-                    catch(Exception)
-                    {
-                        return env;
-                       
-                    }
-                  
-            }
-            return null;
-        }
-
-        #region Implementation of IComponentConnector
-
-        /// <summary>
-        /// Attaches events and names to compiled content. 
-        /// </summary>
-        /// <param name="connectionId">An identifier token to distinguish calls.</param><param name="target">The target to connect events and names to.</param>
-        public void Connect(int connectionId, object target)
-        {
-        }
 
         #endregion
 
@@ -118,36 +68,6 @@ namespace Warewolf.Studio.Views
             if (e.OldValue.Equals(true) && e.NewValue.Equals(false))
             {
                 TheServerComboBox.IsEnabled = true;
-            }
-        }
-
-        void TheServerComboBox_OnDropDownClosing(object sender, CancelEventArgs e)
-        {
-            if (SelectedServer != null)
-            {
-                if (EditButton != null)
-                {
-                    EditButton.IsEnabled = SelectedServer.AllowEdit ;
-                }
-
-                if (SelectedServer.DisplayName.Contains("New Remote Server"))
-                {
-                    foreach (var item in TheServerComboBox.Items)
-                    {
-                        var env = item.Data as IServer;
-                        if (env != null && env.DisplayName.Contains("localhost"))
-                        {
-                            try
-                            {
-                                TheServerComboBox.SelectedItem = env;
-                            }
-                            catch (Exception)
-                            {
-                                // ignored
-                            }
-                        }
-                    }
-                }
             }
         }
     }
