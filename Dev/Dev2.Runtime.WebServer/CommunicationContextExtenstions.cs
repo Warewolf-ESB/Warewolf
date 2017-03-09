3f7356de907189c545cd41605b253ae08cb83b98
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-09 12:06:45 +0200

    WOLF-2373 WIP refactoring

---
+++ Dev/Dev2.Runtime.WebServer/CommunicationContextExtenstions.cs
@@ -0,0 +1,52 @@
+﻿using Dev2.Common;
+
+namespace Dev2.Runtime.WebServer
+{
+    internal static class CommunicationContextExtenstions
+    {
+        public static string GetServiceName(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["servicename"];
+        }
+        // ReSharper disable InconsistentNaming
+        public static string GetWorkspaceID(this ICommunicationContext ctx)
+        {
+            return ctx.Request.QueryString["wid"];
+        }
+
+        public static string GetDataListID(this ICommunicationContext ctx)
+        {
+            return ctx.Request.QueryString[GlobalConstants.DLID];
+        }
+
+        public static string GetBookmark(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["bookmark"];
+        }
+
+        public static string GetInstanceID(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["instanceid"];
+        }
+
+        public static string GetWebsite(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["website"];
+        }
+
+        public static string GetPath(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["path"];
+        }
+
+        public static string GetClassName(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["name"];
+        }
+
+        public static string GetMethodName(this ICommunicationContext ctx)
+        {
+            return ctx.Request.BoundVariables["action"];
+        }
+    }
+}
