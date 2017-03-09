f7f8c1443323ce818f7f2a3e50bbb44e627bbfbc
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-09 12:57:44 +0200

    WOLF-2373 AbstractWebRequestHandler breakdown

--- Dev/Dev2.Runtime.WebServer/ServiceTestExecutor.cs
+++ Dev/Dev2.Runtime.WebServer/ServiceTestExecutor.cs
@@ -1,6 +1,45 @@
-﻿namespace Dev2.Runtime.WebServer
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Dev2.Common.Interfaces;
+using Dev2.Communication;
+using Dev2.Data;
+using Dev2.Data.Decision;
+using Dev2.Data.TO;
+using Dev2.Interfaces;
+using Dev2.Runtime.ESB.Control;
+
+namespace Dev2.Runtime.WebServer
 {
-    class ServiceTestExecutor
+    internal static class ServiceTestExecutor
     {
+        public static async Task GetTaskForTestExecution(string serviceName, IPrincipal userPrinciple, Guid workspaceGuid, Dev2JsonSerializer serializer, ICollection<IServiceTestModelTO> testResults, IDSFDataObject dataObjectClone)
+        {
+            var lastTask = Task.Run(() =>
+            {
+                var interTestRequest = new EsbExecuteRequest { ServiceName = serviceName };
+                var dataObjectToUse = dataObjectClone;
+                Common.Utilities.PerformActionInsideImpersonatedContext(userPrinciple, () =>
+                {
+                    var esbEndpointClone = new EsbServicesEndpoint();
+                    ErrorResultTO errs;
+                    esbEndpointClone.ExecuteRequest(dataObjectToUse, interTestRequest, workspaceGuid, out errs);
+                });
+                var result = serializer.Deserialize<ServiceTestModelTO>(interTestRequest.ExecuteResult);
+                if (result == null)
+                {
+                    if (interTestRequest.ExecuteResult != null)
+                    {
+                        var r = serializer.Deserialize<TestRunResult>(interTestRequest.ExecuteResult.ToString()) ?? new TestRunResult { TestName = dataObjectToUse.TestName };
+                        result = new ServiceTestModelTO { Result = r, TestName = r.TestName };
+                    }
+                }
+                Dev2DataListDecisionHandler.Instance.RemoveEnvironment(dataObjectToUse.DataListID);
+                dataObjectToUse.Environment = null;
+                testResults.Add(result);
+            });
+            await lastTask;
+        }
     }
 }
