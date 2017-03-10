90f086e2b5dd581b951c3fe15d083f301d1eadbd
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-10 17:09:37 +0200

    WOLF-2541 resolved issues with GAC dll loading

--- Dev/Dev2.Common/AssemblyWrapper.cs
+++ Dev/Dev2.Common/AssemblyWrapper.cs
@@ -1,13 +1,39 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Dev2.Common.Interfaces;
 
 namespace Dev2.Common
 {
     public class AssemblyWrapper : IAssemblyWrapper
     {
-        public Assembly Load( string assemblyString)
+        public Assembly Load(string assemblyString)
         {
             return Assembly.Load(assemblyString);
+        }
+
+        public Assembly LoadFrom(string assemblyString)
+        {
+            return Assembly.LoadFrom(assemblyString);
+        }
+
+        public Assembly Load(AssemblyName toLoad)
+        {
+            return Assembly.Load(toLoad);
+        }
+
+        public Assembly UnsafeLoadFrom(string assemblyLocation)
+        {
+            return Assembly.UnsafeLoadFrom(assemblyLocation);
+        }
+
+        public Assembly GetAssembly(Type getType)
+        {
+            return Assembly.GetAssembly(getType);
+        }
+
+        public AssemblyName[] GetReferencedAssemblies(Assembly asm)
+        {
+            return asm.GetReferencedAssemblies();
         }
     }
 }
