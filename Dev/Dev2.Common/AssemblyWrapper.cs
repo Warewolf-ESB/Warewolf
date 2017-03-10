3007bbb0d1dab3023986e8d77fe824de4e29bb68
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-10 17:09:14 +0200

    WOLF-2541 new files

---
+++ Dev/Dev2.Common/AssemblyWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Dev2.Common.Interfaces;
+
+namespace Dev2.Common
+{
+    public class AssemblyWrapper : IAssemblyWrapper
+    {
+        public Assembly Load( string assemblyString)
+        {
+            return Assembly.Load(assemblyString);
+        }
+    }
+}
