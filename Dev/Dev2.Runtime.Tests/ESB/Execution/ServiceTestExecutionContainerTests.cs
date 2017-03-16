78dc915fd8fd56563fd9ccb9c43b23be62dc8b6a
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-16 10:35:45 +0200

    WOLF-2577 wip coverage

---
+++ Dev/Dev2.Runtime.Tests/ESB/Execution/ServiceTestExecutionContainerTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2.Tests.Runtime.ESB.Execution
+{
+    [TestClass]
+    public class ServiceTestExecutionContainerTests
+    {
+    }
+}
