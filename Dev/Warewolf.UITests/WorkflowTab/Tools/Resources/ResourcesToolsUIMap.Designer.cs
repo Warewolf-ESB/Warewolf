23667294773a19b4a6877adbfa63937b2ab3cda7
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-03-29 14:00:35 +0200

    WOLF-2599 resolve namespace for COM maps

--- Dev/Warewolf.UITests/WorkflowTab/Tools/Resources/ResourcesToolsUIMap.Designer.cs
+++ Dev/Warewolf.UITests/WorkflowTab/Tools/Resources/ResourcesToolsUIMap.Designer.cs
@@ -10,21 +10,13 @@
 
 namespace Warewolf.UITests.WorkflowTab.Tools.Resources.ResourcesToolsUIMapClasses
 {
-    using System;
     using System.CodeDom.Compiler;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Text.RegularExpressions;
-    using System.Windows.Input;
     using Microsoft.VisualStudio.TestTools.UITest.Extension;
     using Microsoft.VisualStudio.TestTools.UITesting;
     using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
-    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
-    using MouseButtons = System.Windows.Forms.MouseButtons;
-    
-    
+
+
     [GeneratedCode("Coded UITest Builder", "14.0.23107.0")]
     public partial class ResourcesToolsUIMap
     {
