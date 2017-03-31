2316078dcae30ebb17714f5130f01ef66f8bf15c
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-03-31 14:56:26 +0200

    WOLF-2599 Add coded ui mappings and test for the COM plugin tool

---
+++ Dev/Warewolf.UITests/WorkflowServiceTesting/ComDllTestingTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warewolf.UITests.WorkflowTab.Tools.Resources.ResourcesToolsUIMapClasses;
+using Warewolf.UITests.WorkflowTab.WorkflowTabUIMapClasses;
+
+
+namespace Warewolf.UITests.WorkflowServiceTesting
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class ComDllTestingTests
+    {
+        [TestMethod]
+        [TestCategory("Workflow Select Com Source")]
+        public void ChooseComPluginSelection()
+        {
+            UIMap.Click_NewWorkflow_RibbonButton();
+            WorkflowTabUIMap.Drag_ComDLLConnector_Onto_DesignSurface();
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.NamespaceCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            ResourcesToolsUIMap.Select_Second_Item_From_Com_DLL_Large_View_Source_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.NamespaceCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            ResourcesToolsUIMap.Select_First_Item_From_Com_DLL_Large_View_Namespace_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.GenerateOutputsButton.Enabled);
+            ResourcesToolsUIMap.Select_First_Item_From_Com_DLL_Large_View_Action_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.GenerateOutputsButton.Enabled);
+            ResourcesToolsUIMap.Click_ComDll_Generate_Outputs();
+        }
+
+        [TestMethod]
+        [TestCategory("Workflow Select Com Source")]
+        public void ChangeComPluginSelection()
+        {
+            UIMap.Click_NewWorkflow_RibbonButton();
+            WorkflowTabUIMap.Drag_ComDLLConnector_Onto_DesignSurface();
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.NamespaceCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            ResourcesToolsUIMap.Select_Second_Item_From_Com_DLL_Large_View_Source_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.NamespaceCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            ResourcesToolsUIMap.Select_First_Item_From_Com_DLL_Large_View_Namespace_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.GenerateOutputsButton.Enabled);
+            ResourcesToolsUIMap.Select_First_Item_From_Com_DLL_Large_View_Action_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.GenerateOutputsButton.Enabled);
+            ResourcesToolsUIMap.Select_First_Item_From_Com_DLL_Large_View_Source_Combobox();
+            Assert.IsTrue(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.NamespaceCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.ActionsCombobox.Enabled);
+            Assert.IsFalse(ResourcesToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.ComDll.LargeView.GenerateOutputsButton.Enabled);
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:COMPluginUITestSource
+
+        public const string DotnetWorkflowForTesting = "ComWorkflowForTesting";
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+            UIMap.AssertStudioIsRunning();
+        }
+
+        #endregion
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_UIMap == null)
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        WorkflowTabUIMap WorkflowTabUIMap
+        {
+            get
+            {
+                if (_WorkflowTabUIMap == null)
+                {
+                    _WorkflowTabUIMap = new WorkflowTabUIMap();
+                }
+
+                return _WorkflowTabUIMap;
+            }
+        }
+
+        private WorkflowTabUIMap _WorkflowTabUIMap;
+        ResourcesToolsUIMap ResourcesToolsUIMap
+        {
+            get
+            {
+                if (_ResourcesToolsUIMap == null)
+                {
+                    _ResourcesToolsUIMap = new ResourcesToolsUIMap();
+                }
+
+                return _ResourcesToolsUIMap;
+            }
+        }
+
+        private ResourcesToolsUIMap _ResourcesToolsUIMap;
+    }
+}
