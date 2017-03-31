474eada742e37acfd9f091078e107859860e1cdb
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-03-31 13:10:02 +0200

    duplicate this xml resource so these ui tests can use different resources

--- Dev/Warewolf.UITests/Explorer/ResourceVersion.cs
+++ Dev/Warewolf.UITests/Explorer/ResourceVersion.cs
@@ -15,7 +15,7 @@ namespace Warewolf.UITests
         [TestCategory("Explorer")]
         public void ShowVersionHistory_ForResource()
         {
-            ExplorerUIMap.Filter_Explorer("VersionTestWorkflow");
+            ExplorerUIMap.Filter_Explorer("ShowVersionsTestWorkflow");
             ExplorerUIMap.DoubleClick_Explorer_Localhost_First_Item();
             DataToolsUIMap.MainStudioWindow.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.WorkflowDesigner_Custom.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.MultiAssign.SmallView.DataGrid.Row1.ValueCell.IntellisenseCombobox.Textbox.Text = "Bobby";
             WorkflowTabUIMap.Save_Workflow_Using_Shortcut();
@@ -37,9 +37,9 @@ namespace Warewolf.UITests
         [TestCategory("Explorer")]
         public void OpenVersionHistory_ForResource()
         {
-            ExplorerUIMap.Filter_Explorer("VersionTestWorkflow");
+            ExplorerUIMap.Filter_Explorer("OpenVersionsTestWorkflow");
             ExplorerUIMap.Select_ShowVersionHistory_From_ExplorerContextMenu();
-            Assert.IsTrue(ExplorerUIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.FirstSubItem.Exists);
+            Assert.IsTrue(ExplorerUIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.FirstSubItem.Exists, "No version history found for workflow 'OpenVersionsTestWorkflow'. This UI test expects version history for this resource to start with.");
             ExplorerUIMap.DoubleClick_Explorer_Localhost_First_Item();
             DataToolsUIMap.MainStudioWindow.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.WorkflowDesigner_Custom.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.MultiAssign.SmallView.DataGrid.Row1.ValueCell.IntellisenseCombobox.Textbox.Text = "Batman";
             WorkflowTabUIMap.Save_Workflow_Using_Shortcut();
