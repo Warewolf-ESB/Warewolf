6570e8b8457b689bcdce3242c5a895b5b0506461
Salami Army <ultrakiff@gmail.com>
2017-04-02 11:28:40 +0200

    rmeove existing verison history for this ui test resource and create in test

--- Dev/Warewolf.UITests/Explorer/ResourceVersion.cs
+++ Dev/Warewolf.UITests/Explorer/ResourceVersion.cs
@@ -38,6 +38,9 @@ namespace Warewolf.UITests
         public void OpenVersionHistory_ForResource()
         {
             ExplorerUIMap.Filter_Explorer("OpenVersionsTestWorkflow");
+            ExplorerUIMap.DoubleClick_Explorer_Localhost_First_Item();
+            DataToolsUIMap.MainStudioWindow.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.WorkflowDesigner_Custom.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.MultiAssign.SmallView.DataGrid.Row1.ValueCell.IntellisenseCombobox.Textbox.Text = "Bobby";
+            WorkflowTabUIMap.Save_Workflow_Using_Shortcut();
             ExplorerUIMap.Select_ShowVersionHistory_From_ExplorerContextMenu();
             Assert.IsTrue(ExplorerUIMap.MainStudioWindow.DockManager.SplitPaneLeft.Explorer.ExplorerTree.localhost.FirstItem.FirstSubItem.Exists, "No version history found for workflow 'OpenVersionsTestWorkflow'. This UI test expects version history for this resource to start with.");
             ExplorerUIMap.DoubleClick_Explorer_Localhost_First_Item();
@@ -49,7 +52,6 @@ namespace Warewolf.UITests
             UIMap.Click_Close_Workflow_Tab_Button();
             ExplorerUIMap.DoubleClick_Explorer_Localhost_First_Item();
             Assert.AreEqual("Bobby", DataToolsUIMap.MainStudioWindow.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.WorkflowDesigner_Custom.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.MultiAssign.SmallView.DataGrid.Row1.ValueCell.IntellisenseCombobox.Textbox.Text);
-            ExplorerUIMap.Select_ShowVersionHistory_From_ExplorerContextMenu();
         }
 
         #region Additional test attributes
