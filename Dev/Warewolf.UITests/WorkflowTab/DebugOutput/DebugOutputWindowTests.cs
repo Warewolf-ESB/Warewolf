562923c9cb8c305b465db8b918507435d1ce7cfd
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-04-04 08:30:09 +0200

    WOLF-2557 Dropbox CodedUI selection highlight

--- Dev/Warewolf.UITests/WorkflowTab/DebugOutput/DebugOutputWindowTests.cs
+++ Dev/Warewolf.UITests/WorkflowTab/DebugOutput/DebugOutputWindowTests.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Warewolf.UITests.Explorer.ExplorerUIMapClasses;
 using Warewolf.UITests.WorkflowTab.Tools.Data.DataToolsUIMapClasses;
+using Warewolf.UITests.WorkflowTab.Tools.LoopConstructs.LoopConstructToolsUIMapClasses;
+using Warewolf.UITests.WorkflowTab.Tools.StorageDropbox.DropboxToolsUIMapClasses;
 using Warewolf.UITests.WorkflowTab.WorkflowTabUIMapClasses;
 
 namespace Warewolf.UITests.DebugOutputTests
@@ -42,6 +44,67 @@ namespace Warewolf.UITests.DebugOutputTests
             Assert.IsTrue(assignHasNoFocus);
         }
 
+        [TestMethod]
+        [TestCategory("Debug Input")]
+        public void SelectAndApplyWorkFlowSelection_Validation_UITest()
+        {
+            UIMap.InitializeABlankWorkflow();
+            WorkflowTabUIMap.Drag_Toolbox_SelectAndApply_Onto_DesignSurface();
+            UIMap.Press_F6();
+            var selectAndApplyFocus = LoopConstructToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.SelectAndApply.ItemStatus.Contains("IsPrimarySelection=True IsSelection=True");
+            Assert.IsTrue(selectAndApplyFocus);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Debug Input")]
+        public void DropboxDownloadWorkFlowSelection_Validation_UITest()
+        {
+            UIMap.InitializeABlankWorkflow();
+            WorkflowTabUIMap.Drag_Toolbox_Dropbox_Download_Onto_DesignSurface();
+            UIMap.Press_F6();
+            var dropboxDownloadHasFocus = DropboxToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxDownload.ItemStatus.Contains("IsPrimarySelection=True IsSelection=True");
+            Assert.IsTrue(dropboxDownloadHasFocus);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Debug Input")]
+        public void DropboxDeleteWorkFlowSelection_Validation_UITest()
+        {
+            UIMap.InitializeABlankWorkflow();
+            WorkflowTabUIMap.Drag_Toolbox_Dropbox_Delete_Onto_DesignSurface();
+            UIMap.Press_F6();
+            var dropboxDeleteHasFocus = DropboxToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxDelete.ItemStatus.Contains("IsPrimarySelection=True IsSelection=True");
+            Assert.IsTrue(dropboxDeleteHasFocus);
+
+
+        }
+        [TestMethod]
+        [TestCategory("Debug Input")]
+        public void DropboxUploadWorkFlowSelection_Validation_UITest()
+        {
+            UIMap.InitializeABlankWorkflow();
+            DropboxToolsUIMap.Drag_Toolbox_Dropbox_Upload_Onto_DesignSurface();
+            UIMap.Press_F6();
+            var dropboxUploadHasFocus = DropboxToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxUpload.ItemStatus.Contains("IsPrimarySelection=True IsSelection=True");
+            Assert.IsTrue(dropboxUploadHasFocus);
+
+
+        }
+        [TestMethod]
+        [TestCategory("Debug Input")]
+        public void DropboxListWorkFlowSelection_Validation_UITest()
+        {
+            UIMap.InitializeABlankWorkflow();
+            WorkflowTabUIMap.Drag_Toolbox_Dropbox_FileList_Onto_DesignSurface();
+            UIMap.Press_F6();
+            var dropboxListHasFocus = DropboxToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.DropboxFileList.ItemStatus.Contains("IsPrimarySelection=True IsSelection=True");
+            Assert.IsTrue(dropboxListHasFocus);
+
+        }
+
+
         #region Additional test attributes
 
         [TestInitialize]
@@ -49,7 +112,7 @@ namespace Warewolf.UITests.DebugOutputTests
         {
             UIMap.SetPlaybackSettings();
             UIMap.AssertStudioIsRunning();
-            
+
         }
 
         UIMap UIMap
@@ -111,6 +174,36 @@ namespace Warewolf.UITests.DebugOutputTests
         }
 
         private DataToolsUIMap _DataToolsUIMap;
+
+        LoopConstructToolsUIMap LoopConstructToolsUIMap
+        {
+            get
+            {
+                if (_LoopConstructToolsUIMap == null)
+                {
+                    _LoopConstructToolsUIMap = new LoopConstructToolsUIMap();
+                }
+
+                return _LoopConstructToolsUIMap;
+            }
+        }
+
+        private LoopConstructToolsUIMap _LoopConstructToolsUIMap;
+
+        DropboxToolsUIMap DropboxToolsUIMap
+        {
+            get
+            {
+                if (_DropboxToolsUIMap == null)
+                {
+                    _DropboxToolsUIMap = new DropboxToolsUIMap();
+                }
+
+                return _DropboxToolsUIMap;
+            }
+        }
+
+        private DropboxToolsUIMap _DropboxToolsUIMap;
 
         #endregion
     }
