e8859a6355ebfbcabfe690cd78d94dd151d93680
Dev One <d1@dev2.co.za>
2017-04-10 15:32:04 +0200

    Wolf-2511 : Permissions issue fixed without refactor

--- Dev/Warewolf.Studio.ViewModels/DeploySourceExplorerViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/DeploySourceExplorerViewModel.cs
@@ -25,7 +25,7 @@ namespace Warewolf.Studio.ViewModels
         IEnumerable<IExplorerTreeItem> _preselected;
         private Version _serverVersion;
         private object _serverInformation;
-
+        public System.Action<ICollection<IExplorerItemViewModel>> RunStats { get; set; }
         public DeploySourceExplorerViewModel(IShellViewModel shellViewModel, Microsoft.Practices.Prism.PubSubEvents.IEventAggregator aggregator, IDeployStatsViewerViewModel statsArea)
         {
             if (shellViewModel == null)
@@ -60,6 +60,10 @@ namespace Warewolf.Studio.ViewModels
             RefreshCommand = new Microsoft.Practices.Prism.Commands.DelegateCommand(() => RefreshEnvironment(SelectedEnvironment.ResourceId));
             ConnectControlViewModel.SelectedEnvironmentChanged += DeploySourceExplorerViewModelSelectedEnvironmentChanged;
             IsDeploy = true;
+            RunStats = models =>
+            {
+                _statsArea.CheckDestinationPersmisions(models);
+            };
         }
 
         async void DeploySourceExplorerViewModelSelectedEnvironmentChanged(object sender, Guid environmentId)
