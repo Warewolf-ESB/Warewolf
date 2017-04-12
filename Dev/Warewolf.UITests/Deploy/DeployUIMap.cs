c2e58c20f20269e5893d55a566bebceda53263d8
Dev One <d1@dev2.co.za>
2017-04-12 10:21:49 +0200

    Fixed issues for Permissions Coded UI test

--- Dev/Warewolf.UITests/Deploy/DeployUIMap.cs
+++ Dev/Warewolf.UITests/Deploy/DeployUIMap.cs
@@ -295,7 +295,7 @@ namespace Warewolf.UITests.Deploy.DeployUIMapClasses
         public void ValidateICanNotDeploy(string resource)
         {
             Filter_Deploy_Source_Explorer(resource);
-            Playback.Wait(2000);
+            Playback.Wait(5000);
             Assert.IsFalse(MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.SourceServerExplorer.ExplorerTree.RemoteServer.FirstRemoteResource.FirstRemoteResourceCheckBox.Enabled, "The Deploy selection checkbox is Enabled");
         }
 
@@ -303,7 +303,7 @@ namespace Warewolf.UITests.Deploy.DeployUIMapClasses
         public void ValidateICanDeploy(string resource)
         {
             Filter_Deploy_Source_Explorer(resource);
-            Playback.Wait(5000);
+            Playback.Wait(2000);
             Assert.IsTrue(MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.SourceServerExplorer.ExplorerTree.RemoteServer.FirstRemoteResource.FirstRemoteResourceCheckBox.Enabled, "The Deploy selection checkbox is not Enabled");
         }
 
