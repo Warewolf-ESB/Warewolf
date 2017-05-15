6c3868b9706a89bdd8b00c42ad8e66eef57f6bb4
nkosinathi.sangweni <mnatiris@gmail.com>
2017-05-15 08:03:58 +0200

    WOLF-2592 Cleanup outputs on the activity execution

--- Dev/Dev2.Activities/Activities/RabbitMQ/Consume/DsfConsumeRabbitMQActivity.cs
+++ Dev/Dev2.Activities/Activities/RabbitMQ/Consume/DsfConsumeRabbitMQActivity.cs
@@ -51,7 +51,6 @@ namespace Dev2.Activities.RabbitMQ.Consume
         public int _timeOut;
         public bool IsObject { get; set; }
         [FindMissing]
-        [Outputs("ObjectName")]
         public string ObjectName { get; set; }
         public DsfConsumeRabbitMQActivity()
             : this(new ResponseManager())
