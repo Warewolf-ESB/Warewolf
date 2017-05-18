cf6a7093833c187997a9c7348763132c33fd631c
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-05-18 14:58:27 +0200

    Remove scripting code to validate studio hang inconsistantly

--- Dev/Warewolf.Studio.Views/HelpView.xaml.cs
+++ Dev/Warewolf.Studio.Views/HelpView.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Reflection;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Navigation;
+﻿using System.Windows.Media;
 using Dev2.Common.Interfaces;
 using Infragistics.Documents.RichText;
 
@@ -15,7 +12,6 @@ namespace Warewolf.Studio.Views
         public HelpView()
         {
             InitializeComponent();
-            HideScriptErrors(WebBrowserHost, true);
             RichTextDocument richTextDocument = XamRichTextEditor.Document;
             // ReSharper disable PossibleNullReferenceException
             Color color = (Color)ColorConverter.ConvertFromString("#FFF4F2EE");
@@ -27,34 +23,5 @@ namespace Warewolf.Studio.Views
             };
             XamRichTextEditor.CaretColor = colorInfo;
         }
-
-        private void HideScriptErrors(WebBrowser wb, bool hide)
-        {
-            FieldInfo fiComWebBrowser = typeof(WebBrowser)
-                .GetField("_axIWebBrowser2",
-                          BindingFlags.Instance | BindingFlags.NonPublic);
-            object objComWebBrowser = fiComWebBrowser?.GetValue(wb);
-            objComWebBrowser?.GetType().InvokeMember(
-                "Silent", BindingFlags.SetProperty, null, objComWebBrowser,
-                new object[] { hide });
-        }
-        void WebBrowserHost_OnLoadCompleted(object sender, NavigationEventArgs e)
-        {
-            var browser = sender as WebBrowser;
-
-            if (browser?.Document == null)
-                return;
-
-            dynamic document = browser.Document;
-
-            if (document.readyState != "complete")
-                return;
-
-            dynamic script = document.createElement("script");
-            script.type = @"text/javascript";
-            script.text = @"window.onerror = function(msg,url,line){return true;}";
-            document.head.appendChild(script);
-        }
-
     }
 }
