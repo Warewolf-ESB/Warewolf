fa36c90f905ead08471a61513889a26f72d11757
nkosinathi.sangweni <mnatiris@gmail.com>
2017-05-26 12:00:52 +0200

    Revert "WOLF-2077 increase timeout for all other specs"
    This reverts commit 7aceb33fde172a45995099d2e0a39678c0c8d72a.

--- Dev/TestScripts/Tests/MSTest/CI/Run-All-Other-Specs.ps1
+++ Dev/TestScripts/Tests/MSTest/CI/Run-All-Other-Specs.ps1
@@ -35,7 +35,7 @@ $TestSettingsFile = "$PSScriptRoot\LocalAcceptanceTesting.testsettings"
   <Description>Run example workflow execution specs.</Description>
   <Deployment enabled=`"false`" />
   <Execution>
-    <Timeouts testTimeout=`"250000`" />
+    <Timeouts testTimeout=`"180000`" />
   </Execution>
 </TestSettings>
 "@)
