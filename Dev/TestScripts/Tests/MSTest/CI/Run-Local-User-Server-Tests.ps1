b755e2fabe5c24f5e502f91bdd34d7b6a5ec2dde
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-05-31 09:24:14 +0200

    rename powershell scripts for running server refresh specs to local user server tests

--- Dev/TestScripts/Tests/MSTest/CI/Run-Local-User-Server-Tests.ps1
+++ Dev/TestScripts/Tests/MSTest/CI/Run-Local-User-Server-Tests.ps1
@@ -8,11 +8,11 @@ if ($Args.Count -gt 0) {
 	    [xml]$playlistContent = Get-Content $_.FullName
 	    if ($playlistContent.Playlist.Add.count -gt 0) {
 	        foreach( $TestName in $playlistContent.Playlist.Add) {
-		        $TestList += "," + $TestName.Test.SubString($TestName.Test.LastIndexOf(".") + 1)
+		        $TestList += " /test:" + $TestName.Test.SubString($TestName.Test.LastIndexOf(".") + 1)
 	        }
 	    } else {        
             if ($playlistContent.Playlist.Add.Test -ne $NULL) {
-                $TestList = " /Tests:" + $playlistContent.Playlist.Add.Test.SubString($playlistContent.Playlist.Add.Test.LastIndexOf(".") + 1)
+                $TestList = " /test:" + $playlistContent.Playlist.Add.Test.SubString($playlistContent.Playlist.Add.Test.LastIndexOf(".") + 1)
             } else {
 	            Write-Host Error parsing Playlist.Add from playlist file at $_.FullName
             }
@@ -20,10 +20,11 @@ if ($Args.Count -gt 0) {
     }
 }
 if ($TestList.StartsWith(",")) {
-	$TestList = $TestList -replace "^.", " /Tests:"
+	$TestList = $TestList -replace "^.", " /test:"
 }
 
-# Create assemblies list.
+# Find test assemblies
+$TestAssemblyPath = ""
 if (Test-Path "$PSScriptRoot\Warewolf.Specs\Dev2.*.Specs.dll") {
 	$TestAssemblyPath = "$PSScriptRoot\Warewolf.Specs"
 } elseif (Test-Path "$PSScriptRoot\Dev2.*.Specs.dll") {
@@ -49,24 +50,27 @@ if (Test-Path "$PSScriptRoot\Warewolf.Specs\Dev2.*.Specs.dll") {
 } elseif (Test-Path "$PSScriptRoot\..\..\..\..\..\Dev2.*.Specs.dll") {
 	$TestAssemblyPath = "$PSScriptRoot\..\..\..\..\.."
 }
-$TestAssembliesList = ''
-foreach ($file in Get-ChildItem $TestAssemblyPath -Filter Warewolf.*.Specs.dll ) {
-    $TestAssembliesList = $TestAssembliesList + " `"" + $file.FullName + "`""
+if ($TestAssemblyPath -eq "") {
+	Write-Host Cannot find Warewolf.*.Specs.dll at $PSScriptRoot or $PSScriptRoot\Warewolf.Specs
+	exit 1
 }
-foreach ($file in Get-ChildItem $TestAssemblyPath -Filter Dev2.*.Specs.dll ) {
-    $TestAssembliesList = $TestAssembliesList + " `"" + $file.FullName + "`""
+if (!(Test-Path $PSScriptRoot\TestResults)) {
+	New-Item -ItemType Directory $PSScriptRoot\TestResults
+}
+
+# Create assemblies list.
+$TestAssembliesList = ''
+foreach ($file in Get-ChildItem $TestAssemblyPath -Include Dev2.*.Specs.dll, Warewolf.*.Specs.dll -Recurse | Where-Object {-not $_.FullName.Contains("\obj\")} | Sort-Object -Property Name -Unique ) {
+    $TestAssembliesList = $TestAssembliesList + " /testcontainer:`"" + $file.FullName + "`""
 }
 
 if ($TestList -eq "") {
-	# Create full VSTest argument string.
-	$FullArgsList = $TestAssembliesList + " /logger:trx" + " /TestCaseFilter:`"(TestCategory=ServerRefresh)`""
+	# Create full MSTest argument string.
+	$FullArgsList = $TestAssembliesList + " /resultsfile:TestResults\OtherSpecsResults.trx" + " /category:`"LocalUserServer`""
 } else {
-	# Create full VSTest argument string.
-	$FullArgsList = $TestAssembliesList + " /logger:trx" + $TestList
+	# Create full MSTest argument string.
+	$FullArgsList = $TestAssembliesList + " /resultsfile:TestResults\OtherSpecsResults.trx" + $TestList
 }
 
-# Display full command including full argument string.
-Write-Host `"$env:vs140comntools..\IDE\CommonExtensions\Microsoft\TestWindow\VSTest.console.exe`"$FullArgsList
-
 # Write full command including full argument string.
-Out-File -LiteralPath $PSScriptRoot\RunTests.bat -Append -Encoding default -InputObject `"$env:vs140comntools..\IDE\CommonExtensions\Microsoft\TestWindow\VSTest.console.exe`"$FullArgsList
+Out-File -LiteralPath $PSScriptRoot\RunTests.bat -Append -Encoding default -InputObject `"$env:vs140comntools..\IDE\MSTest.exe`"$FullArgsList
