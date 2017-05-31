e5751df1c64e45ac454b7e05672ae75dcb9abd75
Peter.Bezuidenhout <icefenix1@gmail.com>
2017-05-31 13:30:53 +0200

    WOLF-2077 Fixed MIssing refrences in views and view names.

--- Dev/Dev2.Web/Startup.cs
+++ Dev/Dev2.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.CodeAnalysis;
 
 namespace Dev2.Web
 {
