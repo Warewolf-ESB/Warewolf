40f1a800caeffe1ff33986139e16d55294ade575
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-05-31 14:07:43 +0200

    WOLF-2713 Remove test not compatible after code changes

--- Dev/Dev2.Runtime.Tests/WebServer/Hubs/EsbHubTests.cs
+++ Dev/Dev2.Runtime.Tests/WebServer/Hubs/EsbHubTests.cs
@@ -27,63 +27,6 @@ namespace Dev2.Tests.Runtime.WebServer.Hubs
     // ReSharper disable InconsistentNaming
     public class EsbHubTests
     {
-
-        [TestMethod]
-        [TestCategory("StudioNetworkServerUnitTest")]
-        [Description("Test for StudioNetworkServer's 'UpdateMappingChangedMemo' method: A valid memo and message is passed to StudioNetworkServer and the memo is updated with the message error")]
-        [Owner("Ashley Lewis")]
-        public void StudioNetworkServer_StudioNetworkServerUnitTest_UpdateMappingChangedMemo_MemoUpdated()
-        {
-            //init
-            var server = new MockEsbHub();
-            var message = new CompileMessageTO { UniqueID = Guid.NewGuid(), ServiceID = Guid.NewGuid(), WorkspaceID = Guid.NewGuid(), MessageType = CompileMessageType.MappingChange, MessagePayload = "Test Error Message", ServiceName = "Test Service" };
-
-            //exe
-            server.TestOnCompilerMessageReceived(new ICompileMessageTO[] { message });
-
-            //asserts
-            Assert.AreEqual(2, server.WriteEventProviderMemos.Count);
-            foreach (var memo1 in server.WriteEventProviderMemos)
-            {
-                var memo = (DesignValidationMemo)memo1;
-                Assert.AreEqual(message.ServiceID, memo.ServiceID, "Memo service name not updated with compiler message service name");
-                Assert.AreEqual(message.WorkspaceID, memo.WorkspaceID, "Memo workspace ID not updated");
-                Assert.IsFalse(memo.IsValid, "Error memo not invalidated");
-                Assert.AreEqual(1, memo.Errors.Count, "The wrong number of errors was added to the memo");
-                Assert.AreEqual(message.MessagePayload, memo.Errors[0].FixData, "The wrong error fix data was added to the memo");
-                Assert.AreEqual(message.UniqueID, memo.Errors[0].InstanceID, "The error instacen ID was added to the memo");
-            }
-
-            var serviceMemo = (DesignValidationMemo)server.WriteEventProviderMemos[0];
-            var unqiueMemo = (DesignValidationMemo)server.WriteEventProviderMemos[1];
-            Assert.AreEqual(message.UniqueID, unqiueMemo.InstanceID, "Memo ID not updated with compiler message unique ID");
-            Assert.AreEqual(message.ServiceID, serviceMemo.InstanceID, "Memo ID not updated with compiler message service ID");
-        }
-
-        [TestMethod]
-        [TestCategory("StudioNetworkServerUnitTest")]
-        [Description("Test for StudioNetworkServer's 'UpdateResourceSavedMemo' method: A valid memo and message is passed to StudioNetworkServer and the memo is updated with the message error")]
-        [Owner("Ashley Lewis")]
-        public void StudioNetworkServer_StudioNetworkServerUnitTest_UpdateResourceSavedMemo_MemoUpdated()
-        {
-            //init
-            var server = new MockEsbHub();
-            var message = new CompileMessageTO { ServiceID = Guid.NewGuid(), WorkspaceID = Guid.NewGuid(), MessageType = CompileMessageType.ResourceSaved, ServiceName = "Test Service" };
-
-            //exe
-            server.TestOnCompilerMessageReceived(new ICompileMessageTO[] { message });
-
-
-            //asserts
-            Assert.AreEqual(1, server.WriteEventProviderMemos.Count);
-
-            var memo = (DesignValidationMemo)server.WriteEventProviderMemos[0];
-            Assert.AreEqual(message.ServiceID, memo.InstanceID, "Memo ID not updated with compiler message service ID");
-            Assert.AreEqual(message.ServiceID, memo.ServiceID, "Memo service name not updated with compiler message service name");
-            Assert.AreEqual(message.WorkspaceID, memo.WorkspaceID, "Memo workspace ID not updated");
-            Assert.IsTrue(memo.IsValid, "Resource saved with invalid memo");
-        }
-
         [TestMethod]
         [Owner("Tshepo Ntlhokoa")]
         [TestCategory("EsbHub_AddItemMessage")]
