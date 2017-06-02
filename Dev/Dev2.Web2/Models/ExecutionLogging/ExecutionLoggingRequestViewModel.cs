7a1f7169664f8c59fcfe6a58c2b9526eef66768c
Peter.Bezuidenhout <icefenix1@gmail.com>
2017-06-02 16:09:36 +0200

    WOLF-2077 Updated to support filters

---
+++ Dev/Dev2.Web2/Models/ExecutionLogging/ExecutionLoggingRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Dev2.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dev2.Web2.Models.ExecutionLogging
+{
+    public class ExecutionLoggingRequestViewModel: LogEntry
+    {
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string Protocol { get; set; }
+        public string SortBy { get; set; }
+    }
+}
