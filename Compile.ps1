974da1755708042afb50faac520c1b2636eb2469
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-06-06 07:27:27 +0200

    merge changes I made on the wrong branch

--- Compile.ps1
+++ Compile.ps1
@@ -30,16 +30,15 @@ if (!(Test-Path "$MSBuildPath" -ErrorAction SilentlyContinue)) {
 
 #Version
 if ($Version.IsPresent) {
-    $WarewolfGitRepoDirectory = "$PSScriptRoot"
     Write-Host Writing C# and F# versioning files...
 
     # Get all the latest version tags from server repo.
-    git -C "$WarewolfGitRepoDirectory" fetch --tags
+    git -C "$PSScriptRoot" fetch --tags
 
     # Generate informational version.
     # (from git commit id and time)
-    $GitCommitID = git -C "$WarewolfGitRepoDirectory" rev-parse HEAD
-    $GitCommitTimeObject = git -C "$WarewolfGitRepoDirectory" show -s --format="%ct" $GitCommitID
+    $GitCommitID = git -C "$PSScriptRoot" rev-parse HEAD
+    $GitCommitTimeObject = git -C "$PSScriptRoot" show -s --format="%ct" $GitCommitID
     if (($GitCommitTimeObject[0]).ToString().length -gt 1){
         $GitCommitTimeString = $GitCommitTimeObject[0]
     } else {
@@ -58,7 +57,7 @@ if ($Version.IsPresent) {
 	    $FullVersionString = "$Version"
     } else {
 	    # Check if this version already tagged.
-	    $FullVersionString = git -C "$WarewolfGitRepoDirectory" tag --points-at HEAD
+	    $FullVersionString = git -C "$PSScriptRoot" tag --points-at HEAD
 	    if (-not [string]::IsNullOrEmpty($FullVersionString))  {
 		    $FullVersionString = $FullVersionString.Trim()
 		    if ($FullVersionString -Match " ") {
@@ -73,7 +72,7 @@ if ($Version.IsPresent) {
 		    # This version is not already tagged.
 		    Write-Host This version is not tagged, generating new tag...
 		    # Get last known version
-		    $FullVersionString = git -C "$WarewolfGitRepoDirectory" describe --abbrev=0 --tags
+		    $FullVersionString = git -C "$PSScriptRoot" describe --abbrev=0 --tags
 		    if ([string]::IsNullOrEmpty($FullVersionString)) {
 			    Write-Host No local tags found in git history. 
 			    exit 1
@@ -88,19 +87,19 @@ if ($Version.IsPresent) {
 				    $FullVersionString = $FullVersionString.Split(".")[0] + "." + $FullVersionString.Split(".")[1] + "." + $FullVersionString.Split(".")[2] + "." + $NewBuildNumber
 				    Write-Host Next version would be `"$FullVersionString`". Checking against Origin repo...
 				    # Check tag against origin
-				    $originTag = git -C "$WarewolfGitRepoDirectory" ls-remote --tags origin $FullVersionString
+				    $originTag = git -C "$PSScriptRoot" ls-remote --tags origin $FullVersionString
 				    if ($originTag.length -ne 0) {
 					    Write-Host Origin has tag `"$originTag`".
 				    } else {
 					    Write-Host Double checking with hard coded integration manager repo...
 					    # Check tag against hard coded integration manager repo
-					    $originTag = git -C "$WarewolfGitRepoDirectory" ls-remote --tags "file:////rsaklfsvrdev/Git-Repositories/Warewolf" $FullVersionString
+					    $originTag = git -C "$PSScriptRoot" ls-remote --tags "file:////rsaklfsvrdev/Git-Repositories/Warewolf" $FullVersionString
 					    if ($originTag.length -ne 0) {
 						    Write-Host Hard coded integration manager repo has tag `"$originTag`".
 					    } else {
 						    Write-Host Double checking with hard coded blessed repo...
 						    # Check tag against hard coded blessed repo
-						    $originTag = git -C "$WarewolfGitRepoDirectory" ls-remote --tags "https://github.com/Warewolf-ESB/Warewolf" $FullVersionString
+						    $originTag = git -C "$PSScriptRoot" ls-remote --tags "https://github.com/Warewolf-ESB/Warewolf" $FullVersionString
 						    if ($originTag.length -ne 0) {
 							    Write-Host Hard coded blessed repo has tag `"$originTag`".
 						    }
@@ -113,7 +112,7 @@ if ($Version.IsPresent) {
 	    }
     }
     # Write version files
-    $CSharpVersionFile = "$WarewolfGitRepoDirectory\AssemblyCommonInfo.cs"
+    $CSharpVersionFile = "$PSScriptRoot\AssemblyCommonInfo.cs"
     Write-Host Writing C Sharp version file to `"$CSharpVersionFile`" as...
     $Line1 = "using System.Reflection;"
     $Line2 = "[assembly: AssemblyCompany(""Warewolf"")]"
@@ -137,7 +136,7 @@ if ($Version.IsPresent) {
     $Line6 | Out-File -LiteralPath $CSharpVersionFile -Encoding utf8 -Append
     Write-Host C Sharp version file written to `"$CSharpVersionFile`".
 
-    $FSharpVersionFile = "$WarewolfGitRepoDirectory\AssemblyCommonInfo.fs"
+    $FSharpVersionFile = "$PSScriptRoot\AssemblyCommonInfo.fs"
     Write-Host Writing F Sharp version file to `"$FSharpVersionFile`" as...
     $Line1 = "namespace Warewolf.FSharp"
     $Line2 = "open System.Reflection;"
@@ -145,7 +144,7 @@ if ($Version.IsPresent) {
     $Line4 = "[<assembly: AssemblyProduct(""Warewolf"")>]"
     $Line5 = "[<assembly: AssemblyCopyright(""Copyright Warewolf " + (Get-Date).year + """)>]"
     $Line6 = "[<assembly: AssemblyVersion(""" + $FullVersionString + """)>]"
-    # Ashley: F# Compile thinks this is invalid for some reason
+    # Ashley: F# Compiler thinks this is invalid for some reason
     #$Line7 = "[<assembly: AssemblyInformationalVersion(""" + $GitCommitTime + " " + $GitCommitID + """)>]"
     $Line7 = "do()"
     Write-Host $Line1
@@ -163,9 +162,7 @@ if ($Version.IsPresent) {
     Write-Host $Line7
     $Line7 | Out-File -LiteralPath $FSharpVersionFile -Encoding utf8 -Append
     Write-Host F Sharp version file written to `"$FSharpVersionFile`".
-
-    Write-Host Version written successfully! For more info about this script see: http://warewolf.io/ESB-blog/artefact-sharing-efficient-ci/
-
+    Write-Host Warewolf version written successfully! For more info about Warewolf versioning see: http://warewolf.io/ESB-blog/artefact-sharing-efficient-ci/
 }
 
 #Compile Solutions
