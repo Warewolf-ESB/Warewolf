94c65bf8902b675bc1797dca3da43786ac91091f
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-06-06 09:10:06 +0200

    fix compile break

--- Dev/Dev2.Activities/Properties/AssemblyInfo.cs
+++ Dev/Dev2.Activities/Properties/AssemblyInfo.cs
@@ -15,17 +15,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dev2.Studio.Core")]
+[assembly: AssemblyTitle("Dev2.Activities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-
-[assembly: InternalsVisibleTo("Dev2.Studio.Core.Tests")]
-[assembly: InternalsVisibleTo("Dev2.Activities.Specs")]
-[assembly: InternalsVisibleTo("Warewolf.Studio.ViewModels.Tests")]
-
+[assembly: InternalsVisibleTo("Dev2.Activities.Tests")]
