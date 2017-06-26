e1021aa2ac4ee0d6ebefcce3a0ec35531abf2940
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-06-26 09:41:54 +0200

    test results locaiton fix

--- Dev/Run Tests.ps1
+++ Dev/Run Tests.ps1
@@ -666,31 +666,33 @@ if ($TotalNumberOfJobsToRun -gt 0) {
     }
 
     if (!$MSTest.IsPresent) {
+        Set-Location -Path "$TestsResultsPath"
         # Read playlists and args.
-        $TestList = ""
-        if ($Args.Count -gt 0) {
-            $TestList = $Args.ForEach({ "," + $_ })
-        } else {
-            Get-ChildItem "$PSScriptRoot" -Filter *.playlist | `
-            Foreach-Object{
-	            [xml]$playlistContent = Get-Content $_.FullName
-	            if ($playlistContent.Playlist.Add.count -gt 0) {
-	                foreach( $TestName in $playlistContent.Playlist.Add) {
-		                $TestList += "," + $TestName.Test.SubString($TestName.Test.LastIndexOf(".") + 1)
-	                }
-	            } else {        
-                    if ($playlistContent.Playlist.Add.Test -ne $null) {
-                        $TestList = " /Tests:" + $playlistContent.Playlist.Add.Test.SubString($playlistContent.Playlist.Add.Test.LastIndexOf(".") + 1)
-                    } else {
-	                    Write-Host Error parsing Playlist.Add from playlist file at $_.FullName
+        if ($TestList = "") {
+            if ($Args.Count -gt 0) {
+                $TestList = $Args.ForEach({ "," + $_ })
+            } else {
+                Get-ChildItem "$PSScriptRoot" -Filter *.playlist | `
+                Foreach-Object{
+	                [xml]$playlistContent = Get-Content $_.FullName
+	                if ($playlistContent.Playlist.Add.count -gt 0) {
+	                    foreach( $TestName in $playlistContent.Playlist.Add) {
+		                    $TestList += "," + $TestName.Test.SubString($TestName.Test.LastIndexOf(".") + 1)
+	                    }
+	                } else {        
+                        if ($playlistContent.Playlist.Add.Test -ne $null) {
+                            $TestList = " /Tests:" + $playlistContent.Playlist.Add.Test.SubString($playlistContent.Playlist.Add.Test.LastIndexOf(".") + 1)
+                        } else {
+	                        Write-Host Error parsing Playlist.Add from playlist file at $_.FullName
+                        }
                     }
                 }
             }
+            if ($TestList.StartsWith(",")) {
+	            $TestList = $TestList -replace "^.", " /Tests:"
+            }
         }
-        if ($TestList.StartsWith(",")) {
-	        $TestList = $TestList -replace "^.", " /Tests:"
-        }
-    } else {        
+    } else {
         if ($TestList = "") {
             Get-ChildItem "$PSScriptRoot" -Filter *.playlist | `
             Foreach-Object{
