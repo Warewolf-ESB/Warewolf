e546ad436354e75068b506fa68924b596be8176d
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-06-30 11:48:28 +0200

    remove more empty classes

--- Dev/Run Tests.ps1
+++ Dev/Run Tests.ps1
@@ -127,20 +127,21 @@ if ($JobName.Contains(" DotCover")) {
 if ($RunAllUnitTests.IsPresent -or $RunAllJobs.IsPresent) {
     $JobName = "Other Unit Tests,COMIPC Unit Tests,Studio View Models Unit Tests,Activity Designers Unit Tests,Activities Unit Tests,Tools Specs,UI Binding Tests"
     if ($RunAllJobs.IsPresent) {
-        Invoke-Expression -Command ($PSCommandPath + " -JobName '$JobName' -DisableTimeouts -Parallelize")
+        Invoke-Expression -Command ("&'$PSCommandPath' -JobName '$JobName' -DisableTimeouts -Parallelize")
     }
 }
 if ($RunAllServerTests.IsPresent -or $RunAllJobs.IsPresent) {
     $JobName = "Other Specs,Example Workflow Execution Specs,Subworkflow Execution Specs,Workflow Execution Specs,Integration Tests,Conflicting Contribute View And Execute Permissions Security Specs,Conflicting Execute Permissions Security Specs,Conflicting View And Execute Permissions Security Specs,Conflicting View Permissions Security Specs,No Conflicting Permissions Security Specs,Overlapping User Groups Permissions Security Specs,Resource Permissions Security Specs,Server Permissions Security Specs"
     if ($RunAllJobs.IsPresent) {
-        Invoke-Expression -Command ($PSCommandPath + " -JobName 'Other Specs,Subworkflow Execution Specs,Workflow Execution Specs,Integration Tests' -StartServer -ResourcesType ServerTests")
-        Invoke-Expression -Command ($PSCommandPath + " -JobName 'Example Workflow Execution Specs,Conflicting Contribute View And Execute Permissions Security Specs,Conflicting Execute Permissions Security Specs,Conflicting View And Execute Permissions Security Specs,Conflicting View Permissions Security Specs,No Conflicting Permissions Security Specs,Overlapping User Groups Permissions Security Specs,Resource Permissions Security Specs,Server Permissions Security Specs' -StartServer -ResourcesType Release")
+        Invoke-Expression -Command ("&'$PSCommandPath' -JobName 'Other Specs,Subworkflow Execution Specs,Workflow Execution Specs,Integration Tests' -StartServer -ResourcesType ServerTests")
+        Invoke-Expression -Command ("&'$PSCommandPath' -JobName 'Example Workflow Execution Specs,Conflicting Contribute View And Execute Permissions Security Specs,Conflicting Execute Permissions Security Specs,Conflicting View And Execute Permissions Security Specs,Conflicting View Permissions Security Specs,No Conflicting Permissions Security Specs,Overlapping User Groups Permissions Security Specs,Resource Permissions Security Specs,Server Permissions Security Specs' -StartServer -ResourcesType Release")
     }
 }
 if ($RunAllCodedUITests.IsPresent -or $RunAllJobs.IsPresent) {
     $JobName = "Other UI Tests,Other UI Specs,Assign Tool UI Tests,Control Flow Tools UI Tests,Database Sources UI Tests,Database Tools UI Tests,Data Tools UI Tests,DB Connector UI Specs,Debug Input UI Tests,Default Layout UI Tests,Dependency Graph UI Tests,Deploy UI Specs,Deploy UI Tests,DotNet Connector Mocking UI Tests,DotNet Connector Tool UI Tests,Dropbox Tools UI Tests,Email Tools UI Tests,Explorer UI Specs,Explorer UI Tests,File Tools UI Tests,Hello World Mocking UI Tests,HTTP Tools UI Tests,Plugin Sources UI Tests,Recordset Tools UI Tests,Resource Tools UI Tests,Save Dialog UI Specs,Save Dialog UI Tests,Server Sources UI Tests,Settings UI Tests,Sharepoint Tools UI Tests,Shortcut Keys UI Tests,Source Wizards UI Tests,Tabs And Panes UI Tests,Tools UI Tests,Utility Tools UI Tests,Variables UI Tests,Web Connector UI Specs,Web Sources UI Tests,Workflow Mocking Tests UI Tests,Workflow Testing UI Tests"
     if ($RunAllJobs.IsPresent) {
-        Invoke-Expression -Command ($PSCommandPath + " -JobName '$JobName' -StartStudio -ResourcesType UITests")
+        Invoke-Expression -Command ("&'$PSCommandPath' -JobName '$JobName' -StartStudio -ResourcesType UITests")
+        exit 0
     }
 }
 
