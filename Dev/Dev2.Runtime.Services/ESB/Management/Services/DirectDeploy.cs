2fa4341eecf0c5e7cd7924cc4ffc283891d9d259
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-07-05 16:38:58 +0200

    WOLF-2743 Deploy speed magic

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/DirectDeploy.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/DirectDeploy.cs
@@ -120,29 +120,28 @@ namespace Dev2.Runtime.ESB.Management.Services
                             values.TryGetValue("resourceIDsToDeploy", out resourceIDsToDeploy);
                             var idsToDeploy = new List<Guid>();
                             idsToDeploy.AddRange(serializer.Deserialize<List<Guid>>(resourceIDsToDeploy));
-
                             if (idsToDeploy.Any())
                             {
-                                HashSet<KeyValuePair<Common.Interfaces.Data.IResource, StringBuilder>> pairs = new HashSet<KeyValuePair<Common.Interfaces.Data.IResource, StringBuilder>>();
-                                Parallel.ForEach(idsToDeploy, id =>
+                                var counter = 0;
+                                var count = idsToDeploy.Count;
+                                var amountToTake = 10;
+                                while (counter < count)
                                 {
-                                    var resourceContent = ResourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, id);
-                                    Common.Interfaces.Data.IResource resource = ResourceCatalog.GetResource(GlobalConstants.ServerWorkspaceID, id);
-                                    if (!resource.IsService)
+                                    int diff = count - counter;
+                                    if (diff < 10)
                                     {
-                                        var fetchResourceService = new FetchResourceDefinition();
-                                        resourceContent = fetchResourceService.DecryptAllPasswords(resourceContent);
+                                        amountToTake = diff;
                                     }
-                                    pairs.Add(new KeyValuePair<Common.Interfaces.Data.IResource, StringBuilder>(resource, resourceContent));
-
-                                });
-
-                                Action<Guid> deployAction = resourceId =>
-                                {
-                                    var pair = pairs.Single(p => p.Key.ResourceID == resourceId);
-                                    toReturn.AddRange(DeployResource(roles, serializer, proxy, doTestDeploy, pair));
-                                };
-                                Parallel.ForEach(idsToDeploy, deployAction);
+                                    var throttledIds = idsToDeploy.Skip(counter).Take(amountToTake);
+                                    var taskList = new List<Task>();
+                                    foreach (var resourceId in throttledIds)
+                                    {
+                                        var lastTask = GetTaskForDeploy(resourceId, roles, serializer, proxy, doTestDeploy, toReturn);
+                                        taskList.Add(lastTask);
+                                    }
+                                    Task.WaitAll(taskList.ToArray());
+                                    counter = counter + 10;
+                                }
                             }
                             else
                             {
@@ -157,63 +156,14 @@ namespace Dev2.Runtime.ESB.Management.Services
         }
 
 
-
-        private IEnumerable<DeployResult> DeployResource(StringBuilder roles, Dev2JsonSerializer serializer, IHubProxy proxy, bool doTestDeploy, KeyValuePair<Common.Interfaces.Data.IResource, StringBuilder> pair)
+        public async Task GetTaskForDeploy(Guid resourceId, StringBuilder roles, Dev2JsonSerializer serializer, IHubProxy proxy, bool doTestDeploy, List<DeployResult> toReturn)
         {
-            var toReturn = new List<DeployResult>();
-            var savePath = new StringBuilder();            
-            savePath.Append( pair.Key.GetSavePath());
-            var esbExecuteRequest = new EsbExecuteRequest { ServiceName = "DeployResourceService" };
-            esbExecuteRequest.AddArgument("savePath", savePath);
-            esbExecuteRequest.AddArgument("ResourceDefinition", pair.Value);
-            esbExecuteRequest.AddArgument("Roles", roles);
-            Envelope envelope = new Envelope
+            var lastTask = Task.Run(async () =>
             {
-                Content = serializer.SerializeToBuilder(esbExecuteRequest).ToString(),
-                PartID = 0,
-                Type = typeof(Envelope)
-            };
-            var messageId = Guid.NewGuid();
-            proxy.Invoke<Receipt>("ExecuteCommand", envelope, true, Guid.Empty, Guid.Empty, messageId).Wait();
-            Task<string> fragmentInvoke = proxy.Invoke<string>("FetchExecutePayloadFragment", new FutureReceipt { PartID = 0, RequestID = messageId });
-            var fragmentInvokeResult = fragmentInvoke.Result;
-            var execResult = serializer.Deserialize<ExecuteMessage>(fragmentInvokeResult) ?? new ExecuteMessage { HasError = true, Message = new StringBuilder("Deploy Fialed") };
-            toReturn.Add(new DeployResult(execResult, pair.Key.ResourceName));
-
-            if (doTestDeploy)
-            {
-                var testsToDeploy = TestCatalog.Fetch(pair.Key.ResourceID);
-                CompressedExecuteMessage message = new CompressedExecuteMessage();
-                message.SetMessage(serializer.Serialize(testsToDeploy));
-                var testDeployRequest = new EsbExecuteRequest { ServiceName = "SaveTests" };
-                testDeployRequest.AddArgument("resourceID", pair.Key.ResourceID.ToString().ToStringBuilder());
-                testDeployRequest.AddArgument("resourcePath", savePath);
-                testDeployRequest.AddArgument("testDefinitions", serializer.SerializeToBuilder(message));
-                Envelope deployEnvelope = new Envelope
-                {
-                    Content = serializer.SerializeToBuilder(testDeployRequest).ToString(),
-                    PartID = 0,
-                    Type = typeof(Envelope)
-                };
-                var deployMessageId = Guid.NewGuid();
-                proxy.Invoke<Receipt>("ExecuteCommand", deployEnvelope, true, Guid.Empty, Guid.Empty, deployMessageId).Wait();
-                Task<string> deployFragmentInvoke = proxy.Invoke<string>("FetchExecutePayloadFragment", new FutureReceipt { PartID = 0, RequestID = deployMessageId });
-                var deployFragmentInvokeResult = deployFragmentInvoke.Result;
-                var deployExecResult = serializer.Deserialize<ExecuteMessage>(deployFragmentInvokeResult) ?? new ExecuteMessage { HasError = true, Message = new StringBuilder("Deploy Fialed") };
-                toReturn.Add(new DeployResult(deployExecResult, $"{pair.Key.ResourceName} Tests"));
-            }
-            return toReturn;
-        }
-
-        public DynamicService CreateServiceEntry()
-        {
-            DynamicService deployResourceDynamicService = new DynamicService { Name = HandlesType(), DataListSpecification = new StringBuilder("<DataList><ResourceDefinition ColumnIODirection=\"Input\"/><Roles ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>") };
-
-            ServiceAction deployResourceServiceAction = new ServiceAction { Name = HandlesType(), ActionType = enActionType.InvokeManagementDynamicService, SourceMethod = HandlesType() };
-
-            deployResourceDynamicService.Actions.Add(deployResourceServiceAction);
-
-            return deployResourceDynamicService;
+                var results = await DeployResourceAsync(resourceId, roles, serializer, proxy, doTestDeploy);
+                toReturn.AddRange(results);
+            });
+            await lastTask;
         }
 
         public IConnections Connections
@@ -252,11 +202,73 @@ namespace Dev2.Runtime.ESB.Management.Services
             }
         }
 
+        private async Task<IEnumerable<DeployResult>> DeployResourceAsync(Guid resourceId, StringBuilder roles, Dev2JsonSerializer serializer, IHubProxy proxy, bool doTestDeploy)
+        {
+            var toReturn = new List<DeployResult>();
+            var savePath = new StringBuilder();
+            var resourceContent = ResourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, resourceId);
+            var resource = ResourceCatalog.GetResource(GlobalConstants.ServerWorkspaceID, resourceId);
+            if (!resource.IsService)
+            {
+                var fetchResourceService = new FetchResourceDefinition();
+                resourceContent = fetchResourceService.DecryptAllPasswords(resourceContent);
+            }
+            savePath.Append(resource.GetSavePath());
+
+            var esbExecuteRequest = new EsbExecuteRequest { ServiceName = "DeployResourceService" };
+            esbExecuteRequest.AddArgument("savePath", savePath);
+            esbExecuteRequest.AddArgument("ResourceDefinition", resourceContent);
+            esbExecuteRequest.AddArgument("Roles", roles);
+            Envelope envelope = new Envelope
+            {
+                Content = serializer.SerializeToBuilder(esbExecuteRequest).ToString(),
+                PartID = 0,
+                Type = typeof(Envelope)
+            };
+            var messageId = Guid.NewGuid();
+            await proxy.Invoke<Receipt>("ExecuteCommand", envelope, true, Guid.Empty, Guid.Empty, messageId);
+            string fragmentInvokeResult = await proxy.Invoke<string>("FetchExecutePayloadFragment", new FutureReceipt { PartID = 0, RequestID = messageId });
+            var execResult = serializer.Deserialize<ExecuteMessage>(fragmentInvokeResult) ?? new ExecuteMessage { HasError = true, Message = new StringBuilder("Deploy Failed") };
+            toReturn.Add(new DeployResult(execResult, resource.ResourceName));
+
+            if (doTestDeploy)
+            {
+                var testsToDeploy = TestCatalog.Fetch(resourceId);
+                CompressedExecuteMessage message = new CompressedExecuteMessage();
+                message.SetMessage(serializer.Serialize(testsToDeploy));
+                var testDeployRequest = new EsbExecuteRequest { ServiceName = "SaveTests" };
+                testDeployRequest.AddArgument("resourceID", resourceId.ToString().ToStringBuilder());
+                testDeployRequest.AddArgument("resourcePath", savePath);
+                testDeployRequest.AddArgument("testDefinitions", serializer.SerializeToBuilder(message));
+                Envelope deployEnvelope = new Envelope
+                {
+                    Content = serializer.SerializeToBuilder(testDeployRequest).ToString(),
+                    PartID = 0,
+                    Type = typeof(Envelope)
+                };
+                var deployMessageId = Guid.NewGuid();
+                await proxy.Invoke<Receipt>("ExecuteCommand", deployEnvelope, true, Guid.Empty, Guid.Empty, deployMessageId);
+                string deployFragmentInvokeResult = await proxy.Invoke<string>("FetchExecutePayloadFragment", new FutureReceipt { PartID = 0, RequestID = deployMessageId });
+                var deployExecResult = serializer.Deserialize<ExecuteMessage>(deployFragmentInvokeResult) ?? new ExecuteMessage { HasError = true, Message = new StringBuilder("Deploy Failed") };
+                toReturn.Add(new DeployResult(deployExecResult, $"{resource.ResourceName} Tests"));
+            }
+            return toReturn;
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            DynamicService deployResourceDynamicService = new DynamicService { Name = HandlesType(), DataListSpecification = new StringBuilder("<DataList><ResourceDefinition ColumnIODirection=\"Input\"/><Roles ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>") };
+
+            ServiceAction deployResourceServiceAction = new ServiceAction { Name = HandlesType(), ActionType = enActionType.InvokeManagementDynamicService, SourceMethod = HandlesType() };
+
+            deployResourceDynamicService.Actions.Add(deployResourceServiceAction);
+
+            return deployResourceDynamicService;
+        }
+
         public string HandlesType()
         {
             return "DirectDeploy";
         }
     }
-
-
 }
