fc78e6f5c57e9f5329311cb970acb95eeb71cc24
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-07-07 13:57:53 +0200

    DO-326: pretty printing warewolf service trx test results

--- Dev/Dev2.Runtime.WebServer/ServiceTestModelTRXResultBuilder.cs
+++ Dev/Dev2.Runtime.WebServer/ServiceTestModelTRXResultBuilder.cs
@@ -2,6 +2,7 @@
 using Dev2.Common.Interfaces;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 
 namespace Dev2.Runtime.WebServer
 {
@@ -16,100 +17,95 @@ namespace Dev2.Runtime.WebServer
                 testIDs.Add(Guid.NewGuid());
                 executionIDs.Add(Guid.NewGuid());
             }
-            var Duration = "00:00:00.0000001";
-            var ServerUsername = "User";
             var TestStartDateTime = DateTime.Now.ToString("o", CultureInfo.InvariantCulture);
             var TestEndDateTime = DateTime.Now.ToString("o", CultureInfo.InvariantCulture);
+            var TestDuration = "00:00:00.0000001";
             var TestListID = Guid.NewGuid().ToString();
-            var TRXFileContents = @"<?xml version=""1.0"" encoding=""UTF-8""?>
-    <TestRun id = """;
-            TRXFileContents += Guid.NewGuid().ToString() + @""" name=""Warewolf Service Tests"" runUser=""";
-            TRXFileContents += ServerUsername + @""" xmlns=""http://microsoft.com/schemas/VisualStudio/TeamTest/2010"">
-    <TestSettings name = ""Default Test Settings"" id = """;
-            TRXFileContents += Guid.NewGuid().ToString() + @""">
-      <Execution>
-        <TestTypeSpecific/>
-      </Execution > 
-      <Deployment enabled=""false"" />  
-      <Properties />  
-    </TestSettings>  
-    <Times creation = """;
-            TRXFileContents += TestStartDateTime + "\" queuing=\"" + TestStartDateTime + "\" start=\"" + TestStartDateTime + "\" finish=\"" + TestEndDateTime + "\"";
-            TRXFileContents += @" />
+            var WarewolfServerVersion = "0.0.0.0";
+            var WarewolfServerHostname = "localhost";
+            var WarewolfServerUsername = "User";
+            var TotalTests = testResults.Count;
+            var TotalPassed = testResults.FindAll((result) => { return result.TestPassed; }).Count;
+            var TotalFailed = testResults.FindAll((result) => { return result.TestFailing; }).Count + testResults.FindAll((result) => { return result.TestInvalid; }).Count;
+
+            StringBuilder TRXFileContents = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            TRXFileContents.Append("<TestRun id=\"" + Guid.NewGuid().ToString() + "\" name=\"Warewolf Service Tests\" ");
+            TRXFileContents.Append("runUser=\"" + WarewolfServerUsername + "\" xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\">");
+            TRXFileContents.Append("\n    <Times creation=\"" + TestStartDateTime + "\" queuing=\"" + TestStartDateTime + "\" start=\"" + TestStartDateTime + "\" finish=\"" + TestEndDateTime + "\"");
+            TRXFileContents.Append(@" />
     <ResultSummary outcome=""Completed""> 
-        <Counters total=""";
-            TRXFileContents += testResults.Count.ToString() + @""" executed=""";
-            TRXFileContents += testResults.Count.ToString() + @""" passed=""";
-            TRXFileContents += testResults.FindAll((result) => { return result.TestPassed; }).Count.ToString() + @""" error=""0"" failed=""";
-            TRXFileContents += testResults.FindAll((result) => { return result.TestFailing; }).Count.ToString() + @""" timeout=""0"" aborted=""0"" inconclusive=""0"" passedButRunAborted=""0"" notRunnable=""0"" notExecuted=""0"" disconnected=""0"" warning=""0"" completed=""0"" inProgress=""0"" pending=""0"" />
+        <Counters total=""" + TotalTests.ToString() + "\" executed=\"" + TotalTests.ToString() + "\" ");
+            TRXFileContents.Append("passed=\"" + TotalPassed.ToString() + "\" error=\"0\" ");
+            TRXFileContents.Append("failed=\"" + TotalFailed.ToString() + "\" timeout=\"0\" aborted=\"0\" inconclusive=\"0\" passedButRunAborted=\"0\" notRunnable=\"0\" notExecuted=\"0\" disconnected=\"0\" warning=\"0\" completed=\"0\" inProgress=\"0\" pending=\"0\" />");
+            TRXFileContents.Append(@"
     </ResultSummary>
-    <TestDefinitions>";
+    <TestDefinitions>");
             int testIDIndex = 0;
-            foreach (var TestResult in testResults) {
-                TRXFileContents += @"
-        <UnitTest name=""";
-                TRXFileContents += TestResult.TestName + @""" storage=""";
-                TRXFileContents += TestResult.TestName + @".dll"" id=""";
-                TRXFileContents += testIDs[testIDIndex] + @""">
-            <Execution id=""";
-                TRXFileContents += executionIDs[testIDIndex] + @""" />
-            <TestMethod codeBase=""";
-                TRXFileContents += TestResult.TestName + @".dll"" adapterTypeName=""Microsoft.VisualStudio.TestTools.TestTypes.Unit.UnitTestAdapter, Microsoft.VisualStudio.QualityTools.Tips.UnitTest.Adapter, Version = 14.0.0.0, Culture = neutral, PublicKeyToken = b03f5f7f11d50a3a"" className=""";
-                TRXFileContents += TestResult.TestName + @", Version=WarewolfServerVersion, Culture=neutral, PublicKeyToken=null"" name=""";
-                TRXFileContents += TestResult.TestName + @""" />
-        </UnitTest>";
+            foreach (var TestResult in testResults)
+            {
+                TRXFileContents.Append(@"
+        <UnitTest name=""");
+                TRXFileContents.Append(TestResult.TestName + "\" storage=\"" + TestResult.TestName.Replace(" ", "_") + ".dll\" id=\"" + testIDs[testIDIndex] + @""">
+            <Execution id=""");
+                TRXFileContents.Append(executionIDs[testIDIndex] + @""" />
+            <TestMethod codeBase=""");
+                TRXFileContents.Append(TestResult.TestName + ".dll\" adapterTypeName=\"Microsoft.VisualStudio.TestTools.TestTypes.Unit.UnitTestAdapter, Microsoft.VisualStudio.QualityTools.Tips.UnitTest.Adapter, Version=14.0.0.0, Culture = neutral, PublicKeyToken=b03f5f7f11d50a3a\" className=\"");
+                TRXFileContents.Append(TestResult.TestName + ", Version=" + WarewolfServerVersion + ", Culture=neutral, PublicKeyToken=null\" name=\"" + TestResult.TestName + @""" />
+        </UnitTest>");
                 testIDIndex += 1;
             }
-            TRXFileContents += @"
+            TRXFileContents.Append(@"
     </TestDefinitions>
     <TestLists>
-        <TestList name=""Results Not in a List"" id=""";
-            TRXFileContents += TestListID + @""" />
-        <TestList name = ""All Loaded Results"" id=""19431567-8539-422a-85d7-44ee4e166bda"" />   
-    </TestLists>   
-    <TestEntries>";
+        <TestList name=""Results Not in a List"" id=""");
+            TRXFileContents.Append(TestListID + @""" />
+        <TestList name = ""All Loaded Results"" id=""19431567-8539-422a-85d7-44ee4e166bda"" />
+    </TestLists>
+    <TestEntries>");
             testIDIndex = 0;
-            foreach (var TestResult in testResults) {
-            TRXFileContents += @"
-        <TestEntry testId=""";
-                TRXFileContents += testIDs[testIDIndex] + @""" executionId=""";
-                TRXFileContents += executionIDs[testIDIndex] + @""" testListId=""";
-                TRXFileContents += TestListID + @""" />";
+            foreach (var TestResult in testResults)
+            {
+                TRXFileContents.Append("\n        <TestEntry testId=\"");
+                TRXFileContents.Append(testIDs[testIDIndex] + "\" executionId=\"");
+                TRXFileContents.Append(executionIDs[testIDIndex] + "\" testListId=\"");
+                TRXFileContents.Append(TestListID + "\" />");
                 testIDIndex += 1;
             }
-        TRXFileContents += @"
+        TRXFileContents.Append(@"
     </TestEntries>
-    <Results>";
+    <Results>");
             testIDIndex = 0;
-            foreach (var TestResult in testResults) {
-                TRXFileContents += "\n    <UnitTestResult executionId=\"";
-                TRXFileContents += executionIDs[testIDIndex] + "\" testId=\"";
-                TRXFileContents += testIDs[testIDIndex] + "\" testName=\"";
-                TRXFileContents += TestResult.TestName + "\" computerName=\"ServerPath\" duration=\"";
-                TRXFileContents += Duration + "\" startTime=\"";
-                TRXFileContents += TestStartDateTime + "\" endTime=\"";
-                TRXFileContents += DateTime.Now.ToString("o", CultureInfo.InvariantCulture) + "\" testType=\"13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b\" outcome=\"";
-                TRXFileContents += TestResult.Result.RunTestResult.ToString().Replace("Test", "") + "\" testListId=\"";
-                TRXFileContents += TestListID + "\" relativeResultsDirectory=\"ca6d373f-8816-4969-8999-3dac700d7626\">";
-                if (TestResult.Result.RunTestResult.ToString().Replace("Test", "") == "Failed") {
-                        var TestResultMessage = System.Web.HttpUtility.HtmlEncode(TestResult.FailureMessage);
-                        TRXFileContents += @"
+            foreach (var TestResult in testResults)
+            {
+                TRXFileContents.Append("\n    <UnitTestResult ");
+                TRXFileContents.Append("executionId=\"" + executionIDs[testIDIndex] + "\" ");
+                TRXFileContents.Append("testId=\"" + testIDs[testIDIndex] + "\" ");
+                TRXFileContents.Append("testName=\"" + TestResult.TestName + "\" computerName=\"" + WarewolfServerHostname + "\" ");
+                TRXFileContents.Append("duration=\"" + TestDuration + "\" ");
+                TRXFileContents.Append("startTime=\"" + TestStartDateTime + "\" ");
+                TRXFileContents.Append("endTime=\"" + DateTime.Now.ToString("o", CultureInfo.InvariantCulture) + "\" ");
+                TRXFileContents.Append("testType =\"13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b\" ");
+                TRXFileContents.Append("outcome=\"" + TestResult.Result.RunTestResult.ToString().Replace("Test", "") + "\" ");
+                TRXFileContents.Append("testListId=\"" + TestListID + "\" ");
+                TRXFileContents.Append("relativeResultsDirectory=\"ca6d373f-8816-4969-8999-3dac700d7626\">\n");
+                if (TestResult.TestFailing)
+                {
+                    var TestResultMessage = System.Web.HttpUtility.HtmlEncode(TestResult.FailureMessage);
+                    TRXFileContents.Append(@"
     <Output>
         <ErrorInfo>
-            <Message>";
-                    TRXFileContents += TestResultMessage;
-                    TRXFileContents += @"</Message>
+            <Message>");
+                    TRXFileContents.Append(TestResultMessage);
+                    TRXFileContents.Append(@"</Message>
             <StackTrace></StackTrace>
         </ErrorInfo>
-    </Output>";
-                    }
-	            TRXFileContents += @"    </UnitTestResult>";
+    </Output>");
+                }
+	            TRXFileContents.Append("    </UnitTestResult>");
                 testIDIndex += 1;
             }
-            TRXFileContents += @"
-    </Results>
-</TestRun>";
-            return TRXFileContents;
+            TRXFileContents.Append("\n    </Results>\n</TestRun>");
+            return TRXFileContents.ToString();
         }
     }
 }
