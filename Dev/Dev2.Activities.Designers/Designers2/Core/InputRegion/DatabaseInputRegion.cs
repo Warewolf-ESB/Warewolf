d90be2c81a6f512e5f5b2e5e23d46abfd5573e29
Nkosinathi <mnatiris@gmail.com>
2017-07-10 12:16:45 +0200

    WOLF-2751 resolved Database inputs reverting

--- Dev/Dev2.Activities.Designers/Designers2/Core/InputRegion/DatabaseInputRegion.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Core/InputRegion/DatabaseInputRegion.cs
@@ -12,6 +12,7 @@ using Dev2.Common.Interfaces.ToolBase;
 using Dev2.Common.Interfaces.ToolBase.Database;
 using Dev2.Studio.Core.Activities.Utils;
 using Microsoft.Practices.Prism;
+using Warewolf.Core;
 
 // ReSharper disable ExplicitCallerInfoArgument
 
@@ -34,13 +35,13 @@ namespace Dev2.Activities.Designers2.Core.InputRegion
 
 
         public DatabaseInputRegion(ModelItem modelItem, IActionToolRegion<IDbAction> action)
-            :this(new ActionInputDatatalistMapper())
+            : this(new ActionInputDatatalistMapper())
         {
             ToolRegionName = "DatabaseInputRegion";
             _modelItem = modelItem;
             _action = action;
             _action.SomethingChanged += SourceOnSomethingChanged;
-            var inputsFromModel = _modelItem.GetProperty<List<IServiceInput>>("Inputs");
+            var inputsFromModel = _modelItem.GetProperty<ICollection<IServiceInput>>("Inputs");
             var serviceInputs = inputsFromModel ?? new List<IServiceInput>();
             var inputs = new ObservableCollection<IServiceInput>();
             inputs.CollectionChanged += InputsCollectionChanged;
@@ -69,9 +70,10 @@ namespace Dev2.Activities.Designers2.Core.InputRegion
             }
         }
 
+
         private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            _modelItem.SetProperty("Inputs", Inputs.ToList());
+            _modelItem.SetProperty("Inputs", Inputs);
         }
 
         private void RemoveItemPropertyChangeEvent(NotifyCollectionChangedEventArgs args)
@@ -121,14 +123,32 @@ namespace Dev2.Activities.Designers2.Core.InputRegion
 
         private void UpdateOnActionSelection()
         {
-            Inputs = new List<IServiceInput>();
             IsEnabled = false;
             if (_action?.SelectedAction != null)
             {
-                Inputs = _action.SelectedAction.Inputs;
-                _datatalistMapper.MapInputsToDatalist(Inputs);
-                IsInputsEmptyRows = Inputs.Count < 1;
-                IsEnabled = true;
+                var selectedActionInputs = _action.SelectedAction.Inputs;
+                var selectedAction = ((DbAction)((ActionRegion.DbActionRegion)_action).SelectedAction).Name;
+                var isTheSameActionWithPrvious = Inputs.All(input => input.ActionName?.Equals(selectedAction) ?? false);
+                if (Inputs.Any() && isTheSameActionWithPrvious)
+                {
+                    if (!Inputs.SequenceEqual(selectedActionInputs, new ServiceInputValueComparer()))
+                    {
+                        var newInputs = Inputs.Except(selectedActionInputs);
+                        foreach (var serviceInput in newInputs)
+                        {
+                            Inputs.Add(serviceInput);
+                        }
+                    }
+                }
+                else
+                {
+
+                    Inputs = selectedActionInputs;
+                    _datatalistMapper.MapInputsToDatalist(Inputs);
+                    IsInputsEmptyRows = Inputs.Count < 1;
+                    IsEnabled = true;
+                }
+
             }
             OnPropertyChanged("Inputs");
         }
@@ -242,10 +262,23 @@ namespace Dev2.Activities.Designers2.Core.InputRegion
                     _modelItem.SetProperty("Inputs", _inputs.ToList());
                     OnPropertyChanged();
                 }
-               
+
             }
         }
 
         #endregion
+    }
+
+    internal class ServiceInputValueComparer : IEqualityComparer<IServiceInput>
+    {
+        public bool Equals(IServiceInput x, IServiceInput y)
+        {
+            return x.Value.Equals(y.Value) && x.Name.Equals(y.Name);
+        }
+
+        public int GetHashCode(IServiceInput obj)
+        {
+            return obj.GetHashCode();
+        }
     }
 }
