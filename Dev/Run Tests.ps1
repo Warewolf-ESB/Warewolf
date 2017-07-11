4d230533eb45bbfd6ad37dea94605213c6cd33b0
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-07-11 11:30:00 +0200

    still changing hardcoded test controller address

--- Dev/Run Tests.ps1
+++ Dev/Run Tests.ps1
@@ -748,7 +748,7 @@ if ($TotalNumberOfJobsToRun -gt 0) {
         } else {
             $TestsTimeout = "180000"
         }
-        $HardcodedTestController = "test-vs2017:6901"
+        $HardcodedTestController = "rsaklfsvrdev:6901"
         if ($RecordScreen.IsPresent) {
             $DataCollectorTags = @"
 
