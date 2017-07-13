f0da81313f264035974bdeef31d9059e18607aef
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-07-13 16:49:12 +0200

    still adding deployment items to unit tests

--- Dev/Run Tests.ps1
+++ Dev/Run Tests.ps1
@@ -754,6 +754,7 @@ if ($TotalNumberOfJobsToRun -gt 0) {
         $ScriptsTag = ""
         $DataCollectorTags = ""
         $NamingSchemeTag = ""
+        $BucketsTag = ""
         $TestRunName = "Run $JobName With Timeout"
         if ($StartStudio.IsPresent) {
             $TestsTimeout = "360000"
@@ -828,6 +829,10 @@ if ($TotalNumberOfJobsToRun -gt 0) {
                     New-Item -Force -Path "$StartupScriptPath" -ItemType File -Value "powershell -Command `"&'%DeploymentDirectory%\Run Tests.ps1' -StartStudio -ResourcesType $ResourcesType$ServerUsernameParam$ServerPasswordParam`""
                     Copy-On-Write $CleanupScriptPath
                     New-Item -Force -Path "$CleanupScriptPath" -ItemType File -Value "powershell -Command `"&'%DeploymentDirectory%\Run Tests.ps1' -Cleanup`""
+                    
+                    $BucketsTag = @"  
+    <Buckets size="1"/>
+"@
                 } else {
                     $DeploymentTags = @"
   <Deployment>
@@ -860,8 +865,8 @@ if ($TotalNumberOfJobsToRun -gt 0) {
   name="$JobName"
   xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010">
   <Description>$TestRunName.</Description>$DeploymentTags$NamingSchemeTag$ScriptsTag$ControllerNameTag
-  <Execution$RemoteExecutionAttribute>
-    <Timeouts testTimeout="$TestsTimeout"/>$TestTypeSpecificTags
+  <Execution$RemoteExecutionAttribute>$BucketsTag
+    <Timeouts testTimeout="$TestsTimeout" deploymentTimeout="600000"/>$TestTypeSpecificTags
     <AgentRule name="$AgentRuleNameValue">$AgentRoleTags$DataCollectorTags
     </AgentRule>
   </Execution>
