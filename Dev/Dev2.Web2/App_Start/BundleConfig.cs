1fcdac444d83da591342630fb2ebe60d6e2adea0
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-08-02 11:50:41 +0200

    WOLF-2077 Add timeout and error reset to localhost option

--- Dev/Dev2.Web2/App_Start/BundleConfig.cs
+++ Dev/Dev2.Web2/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace Dev2.Web2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
