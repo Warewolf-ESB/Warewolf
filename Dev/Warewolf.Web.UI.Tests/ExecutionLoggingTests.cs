d55de16c5294ccea6bc16c333057bebacb52f030
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-02 08:37:37 +0200

    add ffmpeg screen recording to web ui tests

--- Dev/Warewolf.Web.UI.Tests/ExecutionLoggingTests.cs
+++ Dev/Warewolf.Web.UI.Tests/ExecutionLoggingTests.cs
@@ -7,6 +7,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using System.Net;
+using System.Windows.Forms;
+using AutoTestSharedTools.VideoRecorder;
+using System.IO;
 
 namespace SeleniumTests
 {
@@ -17,10 +20,16 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-        
+
+        public TestContext TestContext { get; set; }
+        private FfMpegVideoRecorder screenRecorder = new FfMpegVideoRecorder();
+        private string screenRecordingFilePath;
+
         [TestInitialize]
         public void SetupTest()
         {
+            screenRecordingFilePath = Path.Combine(TestContext.DeploymentDirectory, TestContext.TestName + "_on_" + Environment.MachineName) + "." + FfMpegVideoRecorder.VideoExtention;
+            screenRecorder.StartRecord(screenRecordingFilePath);
             WebRequest.Create("http://localhost:3142/secure/Hello%20World.json?Name=Tester");
             driver = new InternetExplorerDriver();
             baseURL = "http://my.warewolf.io";
@@ -38,11 +47,20 @@ namespace SeleniumTests
             {
                 // Ignore errors if unable to close the browser
             }
+            screenRecorder.StopRecord();
+            if (TestContext.CurrentTestOutcome == UnitTestOutcome.Passed)
+            {
+                File.Delete(screenRecordingFilePath);
+            }
+            else
+            {
+                TestContext.AddResultFile(screenRecordingFilePath);
+            }
             Assert.AreEqual("", verificationErrors.ToString());
         }
         
         [TestMethod]
-        public void TheExecutionLoggingTestsTest()
+        public void ExecutionLoggingTest()
         {
             driver.Navigate().GoToUrl(baseURL + "/ExecutionLogging");
             driver.FindElement(By.Id("updateServer")).Click();
