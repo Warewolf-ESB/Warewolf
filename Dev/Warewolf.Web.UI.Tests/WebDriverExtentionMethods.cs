2855e3ec9699eba1d40c0aac5ce999d10408172a
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-10 09:14:33 +0200

    add tests for other browsers

--- Dev/Warewolf.Web.UI.Tests/WebDriverExtentionMethods.cs
+++ Dev/Warewolf.Web.UI.Tests/WebDriverExtentionMethods.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using OpenQA.Selenium.IE;
+using Warewolf.Web.UI.Tests.ScreenRecording;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace Warewolf.Web.Tests
 {
@@ -15,6 +12,36 @@ namespace Warewolf.Web.Tests
         {
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(" + (scrollDown ? "true" : "false") + ");", Element);
             Assert.IsTrue(Element.Displayed, "Failed to scroll " + Element.TagName + " into view.");
+        }
+
+        public static bool IsAlertPresent(this IWebDriver driver)
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+                wait.Until(ExpectedConditions.AlertIsPresent());
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        public static string CloseAlertAndGetItsText(this IWebDriver driver, bool acceptAlert)
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            string alertText = alert.Text;
+            if (acceptAlert)
+            {
+                alert.Accept();
+            }
+            else
+            {
+                alert.Dismiss();
+            }
+            return alertText;
         }
     }
 }
