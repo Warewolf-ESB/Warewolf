7171805a4bc66d08aa7978703c91e6c5d5a074e6
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-22 15:40:01 +0200

    add coded ui generator for specflow to ui specs project

--- Dev/Warewolf.UI.Specs/UseCodedUIAttribute.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem "$PSScriptRoot\*.feature.cs" |
-    Foreach-Object {
-        $PreviousLine = ""
-        (Get-Content $_) | 
-            Foreach-Object {
-                $_.Replace("UnitTesting.TestClassAttribute", "UITesting.CodedUITestAttribute")
-                $PreviousLine = $_
-    } | Set-Content $_
-}
