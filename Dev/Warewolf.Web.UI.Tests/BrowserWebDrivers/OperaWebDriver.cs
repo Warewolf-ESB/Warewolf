ee70d0acf696aaf53e20f51bf1d3b39d629f03e9
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-08-23 13:42:17 +0200

    Refactor WebUI tests

---
+++ Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/OperaWebDriver.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.Opera;
+
+namespace Warewolf.Web.UI.Tests.BrowserWebDrivers
+{
+    public class OperaWebDriver : BaseWebDriver
+    {
+        public OperaWebDriver() : base(new OperaDriver(new OperaOptions() { BinaryLocation = GetOperaPath() }))
+        {
+            
+        }
+    }
+}
