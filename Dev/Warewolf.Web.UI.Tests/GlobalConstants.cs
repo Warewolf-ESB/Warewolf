53d21c696d86f3b3759c537f33936c5da2cc2824
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-08-24 11:19:08 +0200

    Add no server tests assert

--- Dev/Warewolf.Web.UI.Tests/GlobalConstants.cs
+++ Dev/Warewolf.Web.UI.Tests/GlobalConstants.cs
@@ -7,6 +7,7 @@
         public const string UserCredentialsNotShowingError = "'{0}' is expecting the user credentials dialog";
         public const string IsAlertPresentError = "No alert that local Warewolf server is not running.";
         public const string LocalWarewolfServerError = "Local Warewolf Server Not Found";
+        public const string LocalWarewolfServerExpectedDownError = "Local Warewolf Server is not expected to be running";
         public const string ExpectedUserAuthError = "http://localhost:3142 is requesting your username and password.";
         public const string UserAuthAssertMessage = "'{0}' did not match the user credentials dialog error message.";
         public const string AlertText = "Alert text is not correct";
