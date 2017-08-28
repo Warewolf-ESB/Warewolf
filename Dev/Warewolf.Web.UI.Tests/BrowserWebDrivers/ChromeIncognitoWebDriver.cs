1ae42e3c8ec6fdf1473c0fa31b0688eeb6b60e58
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-28 11:34:28 +0200

    use 3 minute timespan instead of 180 seconds

--- Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/ChromeIncognitoWebDriver.cs
+++ Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/ChromeIncognitoWebDriver.cs
@@ -6,7 +6,7 @@ namespace Warewolf.Web.UI.Tests.BrowserWebDrivers
     public class ChromeIncognitoWebDriver : BaseWebDriver
     {
         static ChromeOptions chromeOptions = new ChromeOptions();
-        public ChromeIncognitoWebDriver() : base(new ChromeDriver(@"C:\Windows", chromeOptions, TimeSpan.FromSeconds(180)))
+        public ChromeIncognitoWebDriver() : base(new ChromeDriver(@"C:\Windows", chromeOptions, TimeSpan.FromMinutes(3)))
         {
             chromeOptions.AddArguments(new[] { "--test-type", "start-maximized", "--incognito" });
         }
