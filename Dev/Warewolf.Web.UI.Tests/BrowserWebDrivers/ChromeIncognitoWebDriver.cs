a89ee26078c23b28c0affa69af9b44eaded7d6c4
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-28 11:13:43 +0200

    increase web driver command timeouts to 3 minutes

--- Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/ChromeIncognitoWebDriver.cs
+++ Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/ChromeIncognitoWebDriver.cs
@@ -1,13 +1,12 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Remote;
+﻿using OpenQA.Selenium.Chrome;
+using System;
 
 namespace Warewolf.Web.UI.Tests.BrowserWebDrivers
 {
     public class ChromeIncognitoWebDriver : BaseWebDriver
     {
         static ChromeOptions chromeOptions = new ChromeOptions();
-        public ChromeIncognitoWebDriver() : base(new ChromeDriver(chromeOptions))
+        public ChromeIncognitoWebDriver() : base(new ChromeDriver(@"C:\Windows", chromeOptions, TimeSpan.FromSeconds(180)))
         {
             chromeOptions.AddArguments(new[] { "--test-type", "start-maximized", "--incognito" });
         }
