0881ab3bf305a1a63c1c02b83bf2f876e02fdd97
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2017-08-31 09:32:50 +0200

    updated the execution manager

--- Dev/Dev2.Runtime.Services/IExecutionManager.cs
+++ Dev/Dev2.Runtime.Services/IExecutionManager.cs
@@ -14,6 +14,6 @@ namespace Dev2.Runtime
 
         void CompleteExecution();
 
-        void AddWait(ManualResetEvent eventPulse);
+        void Wait();        
     }
 }
--- Dev/Dev2.Runtime/ESB/Execution/ExecutionManager.cs
+++ Dev/Dev2.Runtime/ESB/Execution/ExecutionManager.cs
@@ -5,23 +5,16 @@ using System.Threading;
 namespace Dev2.Runtime.ESB.Execution
 {
     public class ExecutionManager : IExecutionManager
-    {
-        static readonly Lazy<ExecutionManager> LazyCat = new Lazy<ExecutionManager>(() =>
-        {
-            var c = new ExecutionManager();
-            return c;
-        }, LazyThreadSafetyMode.PublicationOnly);
-
+    {       
         bool _isRefreshing;
-        int _currentExecutions;
-        readonly List<ManualResetEvent> _waitHandles = new List<ManualResetEvent>();
-
-        ExecutionManager()
+        int _currentExecutions;        
+        static ManualResetEvent EventPulse = new ManualResetEvent(false);
+        static readonly object _executionLock = new object();
+        public ExecutionManager()
         {
             _isRefreshing = false;
             _currentExecutions = 0;          
         }
-        public static ExecutionManager Instance => LazyCat.Value;
 
         public void StartRefresh()
         {
@@ -35,18 +28,31 @@ namespace Dev2.Runtime.ESB.Execution
                 Thread.Sleep(1);
             }
         }
+
+        public void Wait()
+        {
+            lock (_executionLock)
+            {
+                EventPulse.WaitOne();
+            }
+        }
         public void StopRefresh()
         {
-            foreach(var autoResetEvent in _waitHandles)
+            //foreach(var autoResetEvent in _waitHandles)
+            //{
+            //    autoResetEvent.Set();
+            //}
+            //_waitHandles.Clear();
+            lock (_executionLock)
             {
-                autoResetEvent.Set();
-            }
-            _waitHandles.Clear();
-            _isRefreshing = false;
+                EventPulse.Set();
+                _isRefreshing = false;
+            }            
         }
         public void AddExecution()
         {
-            _currentExecutions++;
+            Interlocked.Increment(ref _currentExecutions);
+            //_currentExecutions++;
 
         }
 
@@ -56,13 +62,10 @@ namespace Dev2.Runtime.ESB.Execution
         {
             if (_currentExecutions > 0)
             {
-                _currentExecutions--;
+                Interlocked.Decrement(ref _currentExecutions);
+                //_currentExecutions--;
             }
         }
-
-        public void AddWait(ManualResetEvent eventPulse)
-        {
-            _waitHandles.Add(eventPulse);
-        }
+        
     }
 }
