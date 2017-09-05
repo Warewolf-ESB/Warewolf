eb4b61978e0f58763af728c5252d65f889d95e82
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-09-05 09:29:26 +0200

    WOLF2811 added Equality factory

---
+++ Dev/Dev2.Activities.Tests/ActivityComparerTests/Assigns/MultiAssignComparerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Tests.Activities.ActivityComparerTests.Assigns
+
+{
+    [TestClass]
+    public class MultiAssignComparerTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void SetCredentialOnHandler_GivenHandlerIsNotNull_ShouldReturnHttpHandler()
+        {
+            //---------------Set up test pack-------------------
+            DsfMultiAssignActivity multiAssign = new DsfMultiAssignActivity();
+            //---------------Assert Precondition----------------
+            Assert.IsInstanceOfType(multiAssign,typeof(IEqualityComparer<>));
+            //---------------Execute Test ----------------------
+            //---------------Test Result -----------------------
+        }
+    }
+}
