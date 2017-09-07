5f63619e76809b7dd885e0f077da05beee91407f
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-09-07 08:38:59 +0200

    re-introduce gma datastructores project

--- Dev/Dev2.Studio.Core/DataList/Dev2TrieSugggestionProvider.cs
+++ Dev/Dev2.Studio.Core/DataList/Dev2TrieSugggestionProvider.cs
@@ -236,24 +236,6 @@ namespace Dev2.Studio.Core.DataList
             return trie.Retrieve(filter);
         }
 
-        List<string> Permute(string s)
-        {
-            List<string> listPermutations = new List<string>();
-
-            char[] array = s.ToLower().ToCharArray();
-            int iterations = (1 << array.Length) - 1;
-
-            for (int i = 0; i <= iterations; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                    array[j] = (i & (1 << j)) != 0
-                                  ? char.ToUpper(array[j])
-                                  : char.ToLower(array[j]);
-                listPermutations.Add(new string(array));
-            }
-            return listPermutations;
-        }
-
         #endregion Implementation of ISuggestionProvider
     }
 }
