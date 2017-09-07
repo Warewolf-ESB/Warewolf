0bc550e105d824962d435ad326543f7a949d99f0
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-09-07 10:28:51 +0200

    update siffux trie class to trienet 1.0.2

--- Dev/Trie/Gma.DataStructures.StringSearch/_Trie/SuffixTrie.cs
+++ Dev/Trie/Gma.DataStructures.StringSearch/_Trie/SuffixTrie.cs
@@ -9,8 +9,8 @@ namespace Gma.DataStructures.StringSearch
 {
     public class SuffixTrie<T> : ITrie<T>
     {
-        private readonly Trie<T> _mInnerTrie;
-        private readonly int _mMinSuffixLength;
+        private readonly Trie<T> m_InnerTrie;
+        private readonly int m_MinSuffixLength;
 
         public SuffixTrie(int minSuffixLength)
             : this(new Trie<T>(), minSuffixLength)
@@ -20,32 +20,18 @@ namespace Gma.DataStructures.StringSearch
         private SuffixTrie(Trie<T> innerTrie, int minSuffixLength)
         {
 
-            _mInnerTrie = innerTrie;
-            _mMinSuffixLength = minSuffixLength;
+            m_InnerTrie = innerTrie;
+            m_MinSuffixLength = minSuffixLength;
         }
 
         public IEnumerable<T> Retrieve(string query)
         {
             return
-                _mInnerTrie
-                    .Retrieve(query).Distinct(new StringComparer<T>());
+                m_InnerTrie
+                    .Retrieve(query)
+                    .Distinct(new StringComparer<T>());
         }
 
-        
-
-        public void Add(string key, T value)
-        {
-            var original = GetAllSuffixes(_mMinSuffixLength, key);
-            var lowerCasedString = GetAllSuffixes(_mMinSuffixLength, key.ToLower());
-            var upperCasedString = GetAllSuffixes(_mMinSuffixLength, key.ToUpper());
-            var reversedString = GetAllSuffixes(_mMinSuffixLength, ReverseString(key));
-            var suffixes =lowerCasedString?.Union(upperCasedString)?.Union(original.Union(reversedString));
-            foreach (string suffix in suffixes?? new List<string>())
-            {
-                _mInnerTrie.Add(suffix, value);
-            }
-
-        }
         private string ReverseString(string input)
         {
             var array = input?.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)) : c).ToArray();
@@ -53,12 +39,45 @@ namespace Gma.DataStructures.StringSearch
             return reversedCase;
         }
 
-        private static IEnumerable<string> GetAllSuffixes(int minSuffixLength, string word)
+        public void Add(string key, T value, bool caseSensitive = true)
+        {
+            foreach (string suffix in GetAllSuffixes(m_MinSuffixLength, key, caseSensitive) ?? new List<string>())
+            {
+                m_InnerTrie.Add(suffix, value);
+            }
+        }
+
+        private static IEnumerable<string> GetAllSuffixes(int minSuffixLength, string word, bool caseSensitive)
         {
             for (int i = word.Length - minSuffixLength; i >= 0; i--)
             {
                 var partition = new StringPartition(word, i);
-                yield return partition.ToString();
+                if (caseSensitive)
+                {
+                    yield return partition.ToString();
+                }
+                else
+                {
+                    foreach (var str in GetAllCasePermutions(partition.ToString()).Distinct())
+                    {
+                        yield return str;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<string> GetAllCasePermutions(string s)
+        {
+            char[] array = s.ToLower().ToCharArray();
+            int iterations = (1 << array.Length) - 1;
+
+            for (int i = 0; i <= iterations; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                    array[j] = (i & (1 << j)) != 0
+                                  ? char.ToUpper(array[j])
+                                  : char.ToLower(array[j]);
+                yield return new string(array);
             }
         }
     }
