ffa6332a17e0d06f34ae04c521428bf9c61c4b02
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2017-09-08 07:36:52 +0200

    updates and test fixes

--- Dev/Warewolf.MergeParser.Tests/ParseServiceForDifferencesTests.cs
+++ Dev/Warewolf.MergeParser.Tests/ParseServiceForDifferencesTests.cs
@@ -2,6 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Activities.Presentation.Model;
 using Dev2.Studio.Core.Activities.Utils;
+using System.Activities.Statements;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+using Dev2.Activities;
+using System.Linq;
 
 namespace Warewolf.MergeParser.Tests
 {
@@ -20,6 +24,49 @@ namespace Warewolf.MergeParser.Tests
         public void Constructor_NullHead_ShouldThrowException()
         {
             var psd = new ParseServiceForDifferences(ModelItemUtils.CreateModelItem(), null);
+        }
+            
+        [TestMethod]
+        public void GetDifferences_WhenSame_ShouldReturnNoConflictItems()
+        {
+            var randomActivityUniqueId = Guid.NewGuid().ToString();
+            var calculateUniqueId = Guid.NewGuid().ToString();
+            var chart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity { UniqueID = randomActivityUniqueId },
+                    Next =
+                    new FlowStep
+                    {
+                        Action = new DsfCalculateActivity { UniqueID = calculateUniqueId}
+                    }
+                }
+            };
+
+            var otherChart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity { UniqueID = randomActivityUniqueId },
+                    Next =
+                    new FlowStep
+                    {
+                        Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                    }
+                }
+            };
+
+            var psd = new ParseServiceForDifferences(ModelItemUtils.CreateModelItem(chart), ModelItemUtils.CreateModelItem(otherChart));
+            var diffs  = psd.GetDifferences();
+            Assert.IsNotNull(diffs);
+            Assert.AreEqual(2, diffs.Count);
+            Assert.IsTrue(diffs.All(d => !d.conflict));
+            Assert.AreEqual(randomActivityUniqueId, diffs[0].uniqueId.ToString());
+            Assert.AreEqual(calculateUniqueId, diffs[1].uniqueId.ToString());
+
+            Assert.AreEqual(randomActivityUniqueId, diffs[0].activity.UniqueID);
+            Assert.AreEqual(calculateUniqueId, diffs[1].activity.UniqueID);
         }
     }
 }
