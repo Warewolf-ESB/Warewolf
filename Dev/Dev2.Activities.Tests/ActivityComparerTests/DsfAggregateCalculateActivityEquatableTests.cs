934ebb1e83ed47c3787fd7baa4647c36de752857
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-09-11 13:25:44 +0200

    WOLF-2811 AggregateActivity and BaseConvertActivity fully equatable

---
+++ Dev/Dev2.Activities.Tests/ActivityComparerTests/DsfAggregateCalculateActivityEquatableTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Tests.Activities.ActivityComparerTests
+{
+    [TestClass]
+    public class DsfAggregateCalculateActivityEquatableTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void UniqueIDEquals_EmptyAssigns_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void UniqueIDDifferent_EmptyAssigns_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity();
+            var activity1 = new DsfAggregateCalculateActivity();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void DisplayName_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "a" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void DisplayName_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "A" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void DisplayName_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "AAA" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, DisplayName = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Expression_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "a" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Expression_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "A" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Expression_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "AAA" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Expression = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Result_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "a" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Result_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "A" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Result_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var activity = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "AAA" };
+            var activity1 = new DsfAggregateCalculateActivity() { UniqueID = uniqueId, Result = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(activity);
+            //---------------Execute Test ----------------------
+            var @equals = activity.Equals(activity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+    }
+}
