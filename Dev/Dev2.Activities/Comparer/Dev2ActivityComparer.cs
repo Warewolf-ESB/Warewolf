a623a3d130fac1bec61f47b705881730d4360247
Sanele <sanele.mthembu@dev2.co.za>
2017-09-12 14:09:08 +0200

    2811 : Swithc and Unique Equality tests

---
+++ Dev/Dev2.Activities/Comparer/Dev2ActivityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Dev2.TO;
+
+namespace Dev2.Comparer
+{
+    internal class Dev2ActivityComparer : IEqualityComparer<IDev2Activity>
+    {
+        public bool Equals(IDev2Activity x, IDev2Activity y)
+        {
+            return x != null && y != null && x.Equals(y);
+        }
+
+        public int GetHashCode(IDev2Activity obj)
+        {
+            return 1;
+        }
+    }
+}
