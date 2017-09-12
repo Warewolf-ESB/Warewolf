b517be3d5ffaa50c7d7114a2126901e504722c30
Sanele <sanele.mthembu@dev2.co.za>
2017-09-12 11:11:21 +0200

    2811: Webtools equality tests

---
+++ Dev/Dev2.Activities/Comparer/ServiceInputComparer.cs
@@ -0,0 +1,19 @@
+﻿using Dev2.Common.Interfaces.DB;
+using System.Collections.Generic;
+
+namespace Dev2.Comparer
+{
+    public class ServiceInputComparer : IEqualityComparer<IServiceInput>
+    {
+        public bool Equals(IServiceInput x, IServiceInput y)
+        {
+            if (x == null && y == null) return true;
+            return x != null && y != null && x.Equals(y);
+        }
+        
+        public int GetHashCode(IServiceInput obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
