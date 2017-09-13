98ba6a24c5f929bc3fea175146ddca4a1425edd4
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-09-13 07:47:53 +0200

    WOLF-2811 completed DsfDecision equality, including decision stack

--- Dev/Dev2.Data/SystemTemplates/Models/Dev2DecisionStack.cs
+++ Dev/Dev2.Data/SystemTemplates/Models/Dev2DecisionStack.cs
@@ -202,7 +202,7 @@ namespace Dev2.Data.SystemTemplates.Models
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((Dev2DecisionStack) obj);
         }
 
