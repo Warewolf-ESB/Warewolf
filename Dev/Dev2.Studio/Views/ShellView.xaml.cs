25afad71d7719f29b12ad667f2cf546729bcb0e7
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-09-14 16:29:08 +0200

    studio soft reset disconnects from remote servers even if localhost is not connected

--- Dev/Dev2.Studio/Views/ShellView.xaml.cs
+++ Dev/Dev2.Studio/Views/ShellView.xaml.cs
@@ -235,35 +235,35 @@ namespace Dev2.Studio.Views
                         mainViewModel.SetActiveServer(localhostServer.EnvironmentID);
                         mainViewModel.SetActiveServer(localhostServer);
                     }
+                }
 
-                    var explorerViewModel = mainViewModel.ExplorerViewModel;
+                var explorerViewModel = mainViewModel.ExplorerViewModel;
+                if (explorerViewModel != null)
+                {
+                    explorerViewModel.SearchText = string.Empty;
 
-                    if (explorerViewModel != null)
+                    if (explorerViewModel.ConnectControlViewModel != null)
                     {
-                        explorerViewModel.SearchText = string.Empty;
-
-                        if (explorerViewModel.ConnectControlViewModel != null)
+                        foreach (var server in explorerViewModel.ConnectControlViewModel.Servers)
                         {
-                            foreach (var server in explorerViewModel.ConnectControlViewModel.Servers)
+                            if (server != null && server.DisplayName != localhostServer.DisplayName && server.IsConnected)
                             {
-                                if (server != null && server.DisplayName != localhostServer.DisplayName && server.IsConnected)
-                                {
-                                    server.Disconnect();
-                                }
-                            }
-                        }
-
-                        var environmentViewModels = explorerViewModel.Environments;
-                        if (environmentViewModels?.Count > 1)
-                        {
-                            for (var i = 0; i < environmentViewModels.Count - 1; i++)
-                            {
-                                var remoteEnvironment = environmentViewModels.FirstOrDefault(model => model.ResourceId != Guid.Empty);
-                                environmentViewModels.Remove(remoteEnvironment);
+                                server.Disconnect();
                             }
                         }
                     }
+
+                    var environmentViewModels = explorerViewModel.Environments;
+                    if (environmentViewModels?.Count > 1)
+                    {
+                        for (var i = 0; i < environmentViewModels.Count - 1; i++)
+                        {
+                            var remoteEnvironment = environmentViewModels.FirstOrDefault(model => model.ResourceId != Guid.Empty);
+                            environmentViewModels.Remove(remoteEnvironment);
+                        }
+                    }
                 }
+
                 if (mainViewModel.ToolboxViewModel != null)
                 {
                     mainViewModel.ToolboxViewModel.SearchTerm = string.Empty;
