ce21611bb40eb4a901ad291b73d5898b7c2da85d
Sanele <sanele.mthembu@dev2.co.za>
2017-09-14 16:06:11 +0200

    2811 : Added Missing files after Stash Pop commit (All activity comparers)

---
+++ Dev/Dev2.Activities/Comparer/EmailSourceComparer.cs
@@ -0,0 +1,27 @@
+﻿using Dev2.Runtime.ServiceModel.Data;
+using System.Collections.Generic;
+
+namespace Dev2.Comparer
+{
+    internal class EmailSourceComparer : IEqualityComparer<EmailSource>
+    {
+        public bool Equals(EmailSource x, EmailSource y)
+        {
+            if (x == null && y == null) return true;
+            if ((x != null && y == null) || (x == null && y != null)) return false;
+            return string.Equals(x.Host, y.Host)
+                && string.Equals(x.UserName, y.UserName)
+                && string.Equals(x.Password, y.Password)
+                && string.Equals(x.TestFromAddress, y.TestFromAddress)
+                && string.Equals(x.TestToAddress, y.TestToAddress)
+                && string.Equals(x.DataList, y.DataList)
+                && x.EnableSsl.Equals(y.EnableSsl)
+                && x.Timeout.Equals(y.Timeout);
+        }
+
+        public int GetHashCode(EmailSource obj)
+        {
+            return 1;
+        }
+    }
+}
