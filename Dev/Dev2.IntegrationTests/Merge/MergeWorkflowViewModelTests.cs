32c1fba703d38167eb0908d91779f37000d7c878
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-09-15 07:55:39 +0200

    resolve compile failure

--- Dev/Dev2.IntegrationTests/Merge/MergeWorkflowViewModelTests.cs
+++ Dev/Dev2.IntegrationTests/Merge/MergeWorkflowViewModelTests.cs
@@ -1,21 +1,10 @@
-﻿using Dev2.ViewModels.Merge;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dev2.Integration.Tests.Merge
 {
     [TestClass]
     public class MergeWorkflowViewModelIntergrationTests
     {
-        [TestMethod]
-        public void Constructor_GivenIsNew_ShouldPassThrough()
-        {
-            //---------------Set up test pack-------------------
-            var mergeWorkflowViewModel = new MergeWorkflowViewModel();
-            //---------------Assert Precondition----------------
-
-            //---------------Execute Test ----------------------
-
-            //---------------Test Result -----------------------
-        }
+       
     }
 }
