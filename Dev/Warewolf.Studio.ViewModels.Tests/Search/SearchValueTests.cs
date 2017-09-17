e077c747b21e787d385afaf026c0fcc40033fe4d
Pieter.Terblanche <pete.terblanche@gmail.com>
2017-09-17 16:07:38 +0200

    WOLF-165 Add Search view

---
+++ Dev/Warewolf.Studio.ViewModels.Tests/Search/SearchValueTests.cs
@@ -0,0 +1,129 @@
+﻿using Dev2.Studio.Interfaces;
+using Dev2.ViewModels.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace Warewolf.Studio.ViewModels.Tests.Search
+{
+    [TestClass]
+    public class SearchValueTests
+    {
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        public void SearchValue_WhenSet_ShouldFirePropertyChanged()
+        {
+            var _resId = Guid.NewGuid();
+            //------------Setup for test--------------------------
+            var searchValue = CreateSearchValue(_resId);
+
+            var _wasCalled = false;
+            searchValue.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "ResourceId")
+                {
+                    _wasCalled = true;
+                }
+            };
+
+            Assert.IsNotNull(searchValue.ResourceId);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        public void SearchValue_Name_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var _resId = Guid.NewGuid();
+            var _name = "workflowName";
+            var searchValue = CreateSearchValue(_resId, _name);
+
+            var _wasCalled = false;
+            searchValue.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Name")
+                {
+                    _wasCalled = true;
+                }
+            };
+            
+            Assert.AreEqual(_name, searchValue.Name);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        public void SearchValue_Path_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var _resId = Guid.NewGuid();
+            var _path = "resourcePath";
+            var searchValue = CreateSearchValue(_resId, null, _path);
+
+            var _wasCalled = false;
+            searchValue.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Path")
+                {
+                    _wasCalled = true;
+                }
+            };
+            
+            Assert.AreEqual(_path, searchValue.Path);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        public void SearchValue_Type_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var _resId = Guid.NewGuid();
+            var _type = "Workflow";
+            var searchValue = CreateSearchValue(_resId, null, null, _type);
+
+            var _wasCalled = false;
+            searchValue.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Type")
+                {
+                    _wasCalled = true;
+                }
+            };
+            
+            Assert.AreEqual(_type, searchValue.Type);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        public void SearchValue_Match_WhenSet_ShouldFirePropertyChanged()
+        {
+            //------------Setup for test--------------------------
+            var _resId = Guid.NewGuid();
+            var _match = "Input";
+            var searchValue = CreateSearchValue(_resId, null, null, null, _match);
+
+            var _wasCalled = false;
+            searchValue.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Match")
+                {
+                    _wasCalled = true;
+                }
+            };
+            
+            Assert.AreEqual(_match, searchValue.Match);
+            Assert.IsTrue(_wasCalled);
+        }
+
+        private static SearchValue CreateSearchValue(Guid _resId, string _name = null, string _path = null, string _type = null, string _match = null)
+        {
+            var _selectedEnvironment = new Mock<IEnvironmentViewModel>();
+            _selectedEnvironment.Setup(p => p.DisplayName).Returns("someResName");
+
+            return new SearchValue(_resId, _name, _path, _type, _match, _selectedEnvironment.Object);
+        }
+    }
+}
---
+++ Dev/Warewolf.Studio.Views/Search/SearchView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace Dev2.Views.Search
+{
+    /// <summary>
+    /// Interaction logic for SearchView.xaml
+    /// </summary>
+    public partial class SearchView
+    {
+        public SearchView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
