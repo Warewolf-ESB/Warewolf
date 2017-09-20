65a1754cfee0fdf013f69c96668b025a94d107f2
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-09-20 14:17:22 +0200

    still trying to reduce the number of arguments these dev2 datalist item factory methods take

--- Dev/Dev2.Studio.Core/Models/DataList/ItemModel.cs
+++ Dev/Dev2.Studio.Core/Models/DataList/ItemModel.cs
@@ -2,19 +2,74 @@
 {
     public class ItemModel
     {
-        public bool hasError;
-        public string errorMessage;
-        public bool isEditable;
-        public bool isVisable;
-        public bool isSelected;
+        readonly bool hasError;
+        readonly string errorMessage;
+        readonly bool isEditable;
+        readonly bool isVisable;
+        readonly bool isSelected;
 
-        public ItemModel(bool _hasError = false, string _errorMessage = "", bool _isEditable = true, bool _isVisable = true, bool _isSelected = false)
+        public bool HasError => hasError;
+
+        public string ErrorMessage => errorMessage;
+
+        public bool IsEditable => isEditable;
+
+        public bool IsVisable => isVisable;
+
+        public bool IsSelected => isSelected;
+
+        public ItemModel(bool _hasError, string _errorMessage, bool _isEditable, bool _isVisable, bool _isSelected)
         {
             hasError = _hasError;
             errorMessage = _errorMessage;
             isEditable = _isEditable;
             isVisable = _isVisable;
             isSelected = _isSelected;
+        }
+
+        public ItemModel(bool _hasError, string _errorMessage, bool _isEditable, bool _isVisable)
+        {
+            hasError = _hasError;
+            errorMessage = _errorMessage;
+            isEditable = _isEditable;
+            isVisable = _isVisable;
+            isSelected = false;
+        }
+
+        public ItemModel(bool _hasError, string _errorMessage, bool _isEditable)
+        {
+            hasError = _hasError;
+            errorMessage = _errorMessage;
+            isEditable = _isEditable;
+            isVisable = true;
+            isSelected = false;
+        }
+
+        public ItemModel(bool _hasError, string _errorMessage)
+        {
+            hasError = _hasError;
+            errorMessage = _errorMessage;
+            isEditable = true;
+            isVisable = true;
+            isSelected = false;
+        }
+
+        public ItemModel(bool _hasError)
+        {
+            hasError = _hasError;
+            errorMessage = "";
+            isEditable = true;
+            isVisable = true;
+            isSelected = false;
+        }
+
+        public ItemModel()
+        {
+            hasError = false;
+            errorMessage = "";
+            isEditable = true;
+            isVisable = true;
+            isSelected = false;
         }
     }
 }
