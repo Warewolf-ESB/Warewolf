344fb76824646e3528fe78b73272cdf87bd037f8
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-09-26 10:10:32 +0200

    WOLF-2811 covered 1. Same workflows no conflicts 2. Same workflows no conflicts 3. flat tool added on remote 4. flat tool added on local 5. container tool added on remote 6. container tool added on local 7. tool removed on remote/local 8. tool modified on both sides 9. tool  modified on one side

--- Dev/Warewolf.MergeParser.Tests/ParseServiceForDifferencesTests.cs
+++ Dev/Warewolf.MergeParser.Tests/ParseServiceForDifferencesTests.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Activities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Dev2.Studio.Core.Activities.Utils;
 using System.Activities.Statements;
+using System.Collections.Generic;
 using Unlimited.Applications.BusinessDesignStudio.Activities;
 using Dev2.Activities;
 using System.Linq;
 using Dev2;
 using Dev2.Communication;
+using Dev2.Data.SystemTemplates.Models;
 using Dev2.Utilities;
 using Dev2.Studio.Interfaces;
 using Moq;
@@ -86,7 +89,7 @@ namespace Warewolf.MergeParser.Tests
             var currentChart1 = tupleDifference1.current;
             var differenceChart1 = tupleDifference1.difference;
             Assert.IsNotNull(diffs);
-     
+
             var dev2ActivityD = currentChart1.GetCurrentValue<IDev2Activity>();
             var dev2Activity1D = differenceChart1.GetCurrentValue<IDev2Activity>();
             Assert.IsNotNull(dev2ActivityD);
@@ -134,7 +137,6 @@ namespace Warewolf.MergeParser.Tests
             CustomContainer.DeRegister<IActivityParser>();
             CustomContainer.DeRegister<IShellViewModel>();
         }
-
         [TestMethod]
         public void GetDifferences_WhenDifferent_ShouldReturnConflictItems()
         {
@@ -223,6 +225,379 @@ namespace Warewolf.MergeParser.Tests
             //---------------Execute Test ----------------------
 
             //---------------Test Result -----------------------
+
+        }
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void GetDifferences_WhenFlatToolAddedOnRemote_ShouldNullOnLocalChart()
+        {
+            var activityParser = new ActivityParser();
+            var shellView = new Mock<IShellViewModel>();
+            var serverMock = new Mock<IServer>();
+            shellView.Setup(model => model.ActiveServer).Returns(serverMock.Object);
+            CustomContainer.Register(shellView.Object);
+            CustomContainer.Register<IActivityParser>(activityParser);
+
+            var randomActivityUniqueId = Guid.NewGuid().ToString();
+            var calculateUniqueId = Guid.NewGuid().ToString();
+            var chart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId,
+                    },
+                    Next =
+                    new FlowStep
+                    {
+                        Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                    }
+                }
+            };
+            var baseCOnvertId = Guid.NewGuid().ToString();
+            var otherChart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId
+                    },
+                    Next =
+                    new FlowStep()
+                    {
+                        Action = new DsfBaseConvertActivity() { UniqueID = baseCOnvertId }
+                        ,
+                        Next = new FlowStep
+                        {
+                            Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                        }
+                    }
+                }
+
+            };
+
+            var current = CreateContextualResourceModel(chart);
+            var diff = CreateContextualResourceModel(otherChart);
+
+            var psd = new ServiceDifferenceParser();
+            var diffs = psd.GetDifferences(current, diff);
+
+            Assert.AreEqual(3, diffs.Count);
+            Assert.IsTrue(diffs.Any(d => d.conflict));
+            Assert.AreEqual(randomActivityUniqueId, diffs[1].uniqueId.ToString());
+            Assert.AreEqual(calculateUniqueId, diffs[0].uniqueId.ToString());
+
+
+            ////First Node chart
+            var valueTuple = diffs[0];
+            var dev2Activity = valueTuple.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1 = valueTuple.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2Activity);
+            Assert.IsNotNull(dev2Activity1);
+            Assert.AreEqual(calculateUniqueId, dev2Activity1.UniqueID);
+            Assert.AreEqual(calculateUniqueId, dev2Activity.UniqueID);
+
+            //Second chart
+            var valueTuple1 = diffs[1];
+            var dev2ActivityD = valueTuple1.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1D = valueTuple1.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2ActivityD);
+            Assert.IsNotNull(dev2Activity1D);
+            Assert.AreEqual(randomActivityUniqueId, dev2ActivityD.UniqueID);
+            Assert.AreEqual(randomActivityUniqueId, dev2Activity1D.UniqueID);
+
+            //Third node
+            //difference chart
+            var valueTuple2 = diffs[2];
+            var dev3Activity1D = valueTuple2.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNull(valueTuple2.current);
+            Assert.IsNotNull(dev3Activity1D);
+            Assert.AreEqual(baseCOnvertId, dev3Activity1D.UniqueID);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void GetDifferences_WhenFlatToolAddedOnLocal_ShouldNullOnRemoteChart()
+        {
+            var activityParser = new ActivityParser();
+            var shellView = new Mock<IShellViewModel>();
+            var serverMock = new Mock<IServer>();
+            shellView.Setup(model => model.ActiveServer).Returns(serverMock.Object);
+            CustomContainer.Register(shellView.Object);
+            CustomContainer.Register<IActivityParser>(activityParser);
+
+            var randomActivityUniqueId = Guid.NewGuid().ToString();
+            var calculateUniqueId = Guid.NewGuid().ToString();
+            var chart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId,
+                    },
+                    Next =
+                    new FlowStep
+                    {
+                        Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                    }
+                }
+            };
+            var baseCOnvertId = Guid.NewGuid().ToString();
+            var otherChart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId
+                    },
+                    Next =
+                    new FlowStep()
+                    {
+                        Action = new DsfBaseConvertActivity() { UniqueID = baseCOnvertId }
+                        ,
+                        Next = new FlowStep
+                        {
+                            Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                        }
+                    }
+                }
+            };
+
+            var current = CreateContextualResourceModel(otherChart);
+            var diff = CreateContextualResourceModel(chart);
+
+            var psd = new ServiceDifferenceParser();
+            var diffs = psd.GetDifferences(current, diff);
+
+            Assert.AreEqual(3, diffs.Count);
+            Assert.IsTrue(diffs.Any(d => d.conflict));
+            Assert.AreEqual(randomActivityUniqueId, diffs[1].uniqueId.ToString());
+            Assert.AreEqual(calculateUniqueId, diffs[0].uniqueId.ToString());
+
+
+            ////First Node chart
+            var valueTuple = diffs[0];
+            var dev2Activity = valueTuple.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1 = valueTuple.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2Activity);
+            Assert.IsNotNull(dev2Activity1);
+            Assert.AreEqual(calculateUniqueId, dev2Activity1.UniqueID);
+            Assert.AreEqual(calculateUniqueId, dev2Activity.UniqueID);
+
+            //Second chart
+            var valueTuple1 = diffs[1];
+            var dev2ActivityD = valueTuple1.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1D = valueTuple1.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2ActivityD);
+            Assert.IsNotNull(dev2Activity1D);
+            Assert.AreEqual(randomActivityUniqueId, dev2ActivityD.UniqueID);
+            Assert.AreEqual(randomActivityUniqueId, dev2Activity1D.UniqueID);
+
+            //Third node
+            //difference chart
+            var valueTuple2 = diffs[2];
+            var dev3Activity1D = valueTuple2.current.GetCurrentValue<IDev2Activity>();
+            Assert.IsNull(valueTuple2.difference);
+            Assert.IsNotNull(dev3Activity1D);
+            Assert.AreEqual(baseCOnvertId, dev3Activity1D.UniqueID);
+        }
+
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void GetDifferences_WhenDecisionAddedOnLocal_ShouldNullOnRemoteChart()
+        {
+            var activityParser = new ActivityParser();
+            var shellView = new Mock<IShellViewModel>();
+            var serverMock = new Mock<IServer>();
+            shellView.Setup(model => model.ActiveServer).Returns(serverMock.Object);
+            CustomContainer.Register(shellView.Object);
+            CustomContainer.Register<IActivityParser>(activityParser);
+
+            var randomActivityUniqueId = Guid.NewGuid().ToString();
+            var calculateUniqueId = Guid.NewGuid().ToString();
+            var chart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId,
+                    },
+                    Next =
+                    new FlowStep
+                    {
+                        Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                    }
+                }
+            };
+            var dev2DecisionStack = new Dev2DecisionStack()
+            {
+                TheStack = new List<Dev2Decision>() { },
+                TrueArmText = "a",
+                FalseArmText = "a",
+                DisplayText = "a",
+                Mode = Dev2DecisionMode.AND
+            };
+            Dev2JsonSerializer serializer = new Dev2JsonSerializer();
+            var serialize = serializer.Serialize(dev2DecisionStack);
+            var otherChart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfRandomActivity
+                    {
+                        UniqueID = randomActivityUniqueId,
+                    },
+                    Next =
+                        new FlowStep
+                        {
+                            Action = new DsfCalculateActivity { UniqueID = calculateUniqueId }
+                            ,
+                            Next = new FlowDecision()
+                            {
+                                True = new FlowStep()
+                                {
+                                    Action = new DsfMultiAssignActivity()
+                                },
+                                False = new FlowStep()
+                                {
+                                    Action = new DsfActivity()
+                                },
+                                Condition = new DsfFlowDecisionActivity()
+                                {
+                                    ExpressionText = serialize
+                                }
+                            }
+                        }
+                }
+            };
+
+            var current = CreateContextualResourceModel(otherChart);
+            var diff = CreateContextualResourceModel(chart);
+
+            var psd = new ServiceDifferenceParser();
+            var diffs = psd.GetDifferences(current, diff);
+
+            Assert.AreEqual(5, diffs.Count);
+            Assert.IsTrue(diffs.Any(d => d.conflict));
+            Assert.AreEqual(randomActivityUniqueId, diffs[1].uniqueId.ToString());
+            Assert.AreEqual(calculateUniqueId, diffs[0].uniqueId.ToString());
+
+
+            ////First Node chart
+            var valueTuple = diffs[0];
+            var dev2Activity = valueTuple.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1 = valueTuple.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2Activity);
+            Assert.IsNotNull(dev2Activity1);
+            Assert.AreEqual(calculateUniqueId, dev2Activity1.UniqueID);
+            Assert.AreEqual(calculateUniqueId, dev2Activity.UniqueID);
+
+            //Second chart
+            var valueTuple1 = diffs[1];
+            var dev2ActivityD = valueTuple1.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1D = valueTuple1.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2ActivityD);
+            Assert.IsNotNull(dev2Activity1D);
+            Assert.AreEqual(randomActivityUniqueId, dev2ActivityD.UniqueID);
+            Assert.AreEqual(randomActivityUniqueId, dev2Activity1D.UniqueID);
+
+            //Third node
+            var valueTuple2 = diffs[2];
+            var dev3Activity1D = valueTuple2.current.GetCurrentValue<IDev2Activity>();
+            Assert.IsNull(valueTuple2.difference);
+            Assert.IsNotNull(dev3Activity1D);
+            Assert.AreNotEqual(Guid.Empty.ToString(), dev3Activity1D.UniqueID);
+
+            //Fourth node
+            var valueTuple3 = diffs[3];
+            var dev4Activity1D = valueTuple3.current.GetCurrentValue<IDev2Activity>();
+            Assert.IsNull(valueTuple3.difference);
+            Assert.IsNotNull(dev4Activity1D);
+            Assert.AreNotEqual(Guid.Empty.ToString(), dev4Activity1D.UniqueID);
+
+            //Third node
+            var valueTuple4 = diffs[4];
+            var dev5Activity1D = valueTuple4.current.GetCurrentValue<IDev2Activity>();
+            Assert.IsNull(valueTuple4.difference);
+            Assert.IsNotNull(dev3Activity1D);
+            Assert.AreNotEqual(Guid.Empty.ToString(), dev5Activity1D.UniqueID);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void GetDifferences_WhenToolModifiedOnBithSides_ShouldNullOnRemoteChart()
+        {
+            var activityParser = new ActivityParser();
+            var shellView = new Mock<IShellViewModel>();
+            var serverMock = new Mock<IServer>();
+            shellView.Setup(model => model.ActiveServer).Returns(serverMock.Object);
+            CustomContainer.Register(shellView.Object);
+            CustomContainer.Register<IActivityParser>(activityParser);
+
+            var assignId = Guid.NewGuid().ToString();
+            var chart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfMultiAssignActivity()
+                    {
+                        FieldsCollection = new List<ActivityDTO>()
+                        {
+                            new ActivityDTO("field1","field1",1),
+                            new ActivityDTO("field2","field2",4),
+                            new ActivityDTO("field3","field3",3),
+                            new ActivityDTO("field3","field3",2),
+                        },
+                        UniqueID = assignId
+
+                    }
+                }
+            };
+
+            var otherChart = new Flowchart
+            {
+                StartNode = new FlowStep
+                {
+                    Action = new DsfMultiAssignActivity()
+                    {
+                        FieldsCollection = new List<ActivityDTO>()
+                        {
+                            new ActivityDTO("field1","field1",1),
+                            new ActivityDTO("field2","field2",2),
+                            new ActivityDTO("field2","fff",4),
+                            new ActivityDTO("field3","field3",3),
+                        },
+                        UniqueID = assignId
+
+                    }
+                }
+            };
+
+            var current = CreateContextualResourceModel(otherChart);
+            var diff = CreateContextualResourceModel(chart);
+
+            var psd = new ServiceDifferenceParser();
+            var diffs = psd.GetDifferences(current, diff);
+
+            Assert.AreEqual(1, diffs.Count);
+            Assert.IsTrue(diffs.Any(d => d.conflict));
+            Assert.AreEqual(assignId, diffs[0].uniqueId.ToString());
+
+
+            ////First Node chart
+            var valueTuple = diffs[0];
+            var dev2Activity = valueTuple.current.GetCurrentValue<IDev2Activity>();
+            var dev2Activity1 = valueTuple.difference.GetCurrentValue<IDev2Activity>();
+            Assert.IsNotNull(dev2Activity);
+            Assert.IsNotNull(dev2Activity1);
+            Assert.AreEqual(assignId, dev2Activity1.UniqueID);
+            Assert.AreEqual(assignId, dev2Activity.UniqueID);
 
         }
     }
