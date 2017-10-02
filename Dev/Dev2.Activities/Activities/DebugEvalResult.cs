a2cd6bdd58ceab9ca9061890fc316e119fad040f
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-02 10:10:15 +0200

    still trying to fix compiel breaks

--- Dev/Dev2.Activities/Activities/DebugEvalResult.cs
+++ Dev/Dev2.Activities/Activities/DebugEvalResult.cs
@@ -36,6 +36,16 @@ namespace Dev2.Activities
         {
         }
 
+        public DebugEvalResult(string inputVariable, string label, IExecutionEnvironment environment, int update, bool isDataMerge)
+            : this(inputVariable, label, environment, update, isDataMerge, false, false)
+        {
+        }
+
+        public DebugEvalResult(string inputVariable, string label, IExecutionEnvironment environment, int update, bool isDataMerge, bool isCalculate)
+            : this(inputVariable, label, environment, update, isDataMerge, isCalculate, false)
+        {
+        }
+
         public DebugEvalResult(string inputVariable, string label, IExecutionEnvironment environment, int update, bool isDataMerge, bool isCalculate, bool mockSelected)
         {
             _inputVariable = inputVariable?.Trim();
