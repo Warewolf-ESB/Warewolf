ebf5dbc30af0e0da323462df4c4960b4cbbc86bb
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-02 08:44:07 +0200

    remove optional parameters from web request invoker class

--- Dev/Dev2.Activities/Activities/IWebRequestInvoker.cs
+++ Dev/Dev2.Activities/Activities/IWebRequestInvoker.cs
@@ -19,8 +19,11 @@ namespace Dev2.Activities
         string ExecuteRequest(string method, string url, List<Tuple<string, string>> headers);
 
         string ExecuteRequest(string method, string url, List<Tuple<string, string>> headers, int timeoutMilliseconds);
-        string ExecuteRequest(string method, string url, string data, List<Tuple<string, string>> headers = null, Action<string> asyncCallback = null);
-
-        string ExecuteRequest(int timeoutMilliseconds, string method, string url, string data, List<Tuple<string, string>> headers = null, Action<string> asyncCallback = null);
+        string ExecuteRequest(string method, string url, string data);
+        string ExecuteRequest(string method, string url, string data, List<Tuple<string, string>> headers);
+        string ExecuteRequest(string method, string url, string data, List<Tuple<string, string>> headers, Action<string> asyncCallback);
+        string ExecuteRequest(int timeoutMilliseconds, string method, string url, string data);
+        string ExecuteRequest(int timeoutMilliseconds, string method, string url, string data, List<Tuple<string, string>> headers);
+        string ExecuteRequest(int timeoutMilliseconds, string method, string url, string data, List<Tuple<string, string>> headers, Action<string> asyncCallback);
     }
 }
