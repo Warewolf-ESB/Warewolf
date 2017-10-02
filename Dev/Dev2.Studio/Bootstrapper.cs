ae0ce9abb6bb2b011dc704fd556a37c0dfb13faa
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-02 10:36:02 +0200

    start removing optional parameters from studio interfaces project

--- Dev/Dev2.Studio/Bootstrapper.cs
+++ Dev/Dev2.Studio/Bootstrapper.cs
@@ -144,11 +144,8 @@ namespace Dev2
         #endregion Public Methods
 
         #region Private Methods
-
-        /*
-         * You must be in Release config to see the only reference to this function. - Ashley
-         */
         
+#pragma warning disable S1144 // You must be in Release config to see the only reference to this function.
         private bool CheckWindowsService()
         {
             IWindowsServiceManager windowsServiceManager = CustomContainer.Get<IWindowsServiceManager>();
@@ -174,6 +171,7 @@ namespace Dev2
 
             return false;
         }
+#pragma warning restore S1144
 
         private void CheckPath()
         {
@@ -189,8 +187,7 @@ namespace Dev2
                     Header = "Load Error",
                     Description = 
                         $@"The Design Studio could not be launched from a network location.
-                                                    {Environment
-                            .NewLine}Please install the application on your local machine",
+                        {Environment.NewLine}Please install the application on your local machine",
                     Buttons = MessageBoxButton.OK
                 };
 
