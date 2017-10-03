2fb5a60bfc1852c24ba6cceec06424aceca996d3
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-03 08:21:22 +0200

    remove optional parameters from mocked out functions

--- Dev/Dev2.Studio.Core/AppResources/Repositories/ResourceRepository.cs
+++ Dev/Dev2.Studio.Core/AppResources/Repositories/ResourceRepository.cs
@@ -594,8 +594,6 @@ namespace Dev2.Studio.Core.AppResources.Repositories
             return null;
         }
 
-
-
         public Func<string, ICommunicationController> GetCommunicationController = serviveName => new CommunicationController { ServiceName = serviveName };
 
         private ExecuteMessage SaveResource(IServer targetEnvironment, StringBuilder resourceDefinition, Guid workspaceId, string savePath)
