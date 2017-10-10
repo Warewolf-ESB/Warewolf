2ef54ed1417f752d8fa3d6c77ff1805f4d785632
Nkosinathi <Nkosinathi.Sangweni@dev2.co.za>
2017-10-10 14:50:23 +0200

    WOLF-2811 refactor version to resource logic

--- Dev/Warewolf.Studio.ViewModels/VersionViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/VersionViewModel.cs
@@ -1,6 +1,12 @@
 using System;
 using Dev2.Common.Interfaces.Studio.Controller;
 using Dev2.Studio.Interfaces;
+using Dev2;
+using System.Xml.Linq;
+using Dev2.Studio.Core.Models;
+using Dev2.Services.Events;
+using System.Text;
+using Dev2.Common.Interfaces.Security;
 
 namespace Warewolf.Studio.ViewModels
 {
@@ -9,6 +15,47 @@ namespace Warewolf.Studio.ViewModels
         public VersionViewModel(IServer server, IExplorerTreeItem parent, Action<IExplorerItemViewModel> selectAction, IShellViewModel shellViewModel, IPopupController popupController)
             : base(server, parent, selectAction, shellViewModel, popupController)
         {
-        }        
+        }
+    }
+
+    public static class VersionViewModelExtenstions
+    {
+        public static IContextualResourceModel ToContextualResourceModel(this VersionViewModel versionViewModel, IServer server, Guid? Id)
+        {
+            VerifyArgument.IsNotNull(nameof(versionViewModel), versionViewModel);
+            var workflowXaml = server?.ProxyLayer?.GetVersion(versionViewModel.VersionInfo, versionViewModel.ResourceId);
+            if (workflowXaml != null)
+            {
+                var resourceModel = server?.ResourceRepository.LoadContextualResourceModel(versionViewModel.ResourceId);
+                var xamlElement = XElement.Parse(workflowXaml.ToString());
+                var dataList = xamlElement.Element(@"DataList");
+                var dataListString = string.Empty;
+                if (dataList != null)
+                {
+                    dataListString = dataList.ToString();
+                }
+                var action = xamlElement.Element(@"Action");
+
+                var xamlString = string.Empty;
+                var xaml = action?.Element(@"XamlDefinition");
+                if (xaml != null)
+                {
+                    xamlString = xaml.Value;
+                }
+                var resourceVersion = new ResourceModel(server, EventPublishers.Aggregator)
+                {
+                    ResourceType = resourceModel.ResourceType,
+                    ResourceName = versionViewModel.ResourceName,
+                    WorkflowXaml = new StringBuilder(xamlString),
+                    UserPermissions = Permissions.Contribute,
+                    DataList = dataListString,
+                    IsVersionResource = true,
+                    ID = Id ?? Guid.NewGuid()
+                };
+
+                return resourceVersion;
+            }
+            return default(IContextualResourceModel);
+        }
     }
 }
