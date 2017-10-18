728bea08eeb837bc746207ced1fba5de13b04bbf
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-10-18 15:02:18 +0200

    WOLF-2811 resolved failing tests

--- Dev/Dev2.Studio/Factory/CustomGit.cs
+++ Dev/Dev2.Studio/Factory/CustomGit.cs
@@ -29,26 +29,17 @@ namespace Dev2.Factory
             };
 
             var gitExePath = GetGitExePath(processExecutor);
-
-            if (!string.IsNullOrEmpty(gitExePath))
+            if (string.IsNullOrEmpty(gitExePath))
             {
+                gitExePath = "git.exe";
+            }
                 foreach (var item in orderedList1)
                 {
                     try
                     {
                         var quotedExePath = "\"" + gitExePath + "\"";
-                        ProcessStartInfo ProcessInfo = new ProcessStartInfo(quotedExePath, item)
-                        {
-                            CreateNoWindow = true,
-                            UseShellExecute = false
-                        };
-                        Process Process = processExecutor.Start(ProcessInfo);
-                        if (Process != null)
-                        {
-                            Process.WaitForExit(10);
-                            Process.Close();
-                            Process.Dispose();
-                        }
+
+                        ExecuteCommand(processExecutor, quotedExePath, item);
 
                     }
                     catch (Exception ex)
@@ -56,35 +47,34 @@ namespace Dev2.Factory
                         Dev2Logger.Error(ex.Message, "Git Setup error");
                     }
                 }
+        }
+
+        private static string ExecuteCommand(IExternalProcessExecutor processExecutor, string exe, string command)
+        {
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo(exe, command);
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                Process proc = processExecutor.Start(procStartInfo);
+                string result = proc.StandardOutput.ReadToEnd();
+                return result;
+            }
+            catch (Exception)
+            {
+                return "";
             }
         }
         static string GetGitExePath(IExternalProcessExecutor processExecutor)
         {
             try
             {
-                ProcessStartInfo ProcessInfo = new ProcessStartInfo("cmd.exe", "/C where.exe git.exe")
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true
-                };
-                Process Process = processExecutor.Start(ProcessInfo);
-                using (Process)
-                {
-                    Process.WaitForExit(10);
-                    using (StreamReader myOutput = Process.StandardOutput)
-                    {
-                        if (Process.HasExited)
-                        {
-                            string pathToGitExe = myOutput.ReadToEnd();
-                            var gitpath = pathToGitExe.Split(Environment.NewLine.ToArray())
-                                .FirstOrDefault(p => p.EndsWith(@"bin\git.exe", StringComparison.InvariantCultureIgnoreCase));
-                            return gitpath;
-                        }
-
-                        return "";
-                    }
-                }
+                var cmd = "/c where.exe git.exe";
+                var exe = "cmd ";
+                var gitLocation = ExecuteCommand(processExecutor, exe, cmd);
+                var correctPath = gitLocation.Split(Environment.NewLine.ToCharArray()).FirstOrDefault(p => p.EndsWith(@"bin\git.exe", StringComparison.InvariantCultureIgnoreCase));
+                return correctPath;
             }
             catch (Exception ex)
             {
