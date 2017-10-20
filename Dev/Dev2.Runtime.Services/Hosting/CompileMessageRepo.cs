dc004f5196647e29cbf272364d4dedda95d34c67
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-20 15:32:38 +0200

    delete apparently dead code from CompileMessageRepo.cs

--- Dev/Dev2.Runtime.Services/Hosting/CompileMessageRepo.cs
+++ Dev/Dev2.Runtime.Services/Hosting/CompileMessageRepo.cs
@@ -12,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reactive.Subjects;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Timers;
 using Dev2.Common;
 using Dev2.Common.Interfaces.Infrastructure.SharedModels;
@@ -24,7 +23,6 @@ namespace Dev2.Runtime.Hosting
         readonly IDictionary<Guid, IList<ICompileMessageTO>> _messageRepo = new Dictionary<Guid, IList<ICompileMessageTO>>();
         static Subject<IList<ICompileMessageTO>> _allMessages = new Subject<IList<ICompileMessageTO>>();
         private static readonly object Lock = new object();
-        private static bool _changes;
         private static readonly Timer PersistTimer = new Timer(1000 * 5);
         
         public string PersistencePath { get; private set; }
@@ -60,16 +58,6 @@ namespace Dev2.Runtime.Hosting
             {
                 Directory.CreateDirectory(path);
             }
-
-            // Hydrate from disk ;)
-            HydrateFromDisk(path);
-
-            if(activateBackgroundWorker)
-            {
-                // Init Persistence ;)
-                InitPersistence(path);
-            }
-
         }
 
         public bool Ping()
@@ -77,126 +65,6 @@ namespace Dev2.Runtime.Hosting
             return true;
         }
         
-
-        #region Private Methods
-
-        /// <summary>
-        /// Hydrates from disk.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        private void HydrateFromDisk(string path)
-        {
-            lock(Lock)
-            {
-                try
-                {
-                    var files = Directory.GetFiles(path);
-
-                    foreach(var f in files)
-                    {
-                        var fname = Path.GetFileName(f);
-                        if(fname != null)
-                        {
-                            fname = fname.Replace(".msg", "");
-                            BinaryFormatter bf = new BinaryFormatter();
-                            using(Stream s = new FileStream(f, FileMode.OpenOrCreate))
-                            {
-                                try
-                                {
-                                    object obj = bf.Deserialize(s);
-
-
-                                    if (obj is IList<ICompileMessageTO> listOf)
-                                    {
-                                        if (Guid.TryParse(fname, out Guid id))
-                                        {
-                                            _messageRepo[id] = listOf;
-                                            _allMessages.OnNext(listOf);
-                                        }
-                                        else
-                                        {
-                                            Dev2Logger.Error("Failed to parse message ID", GlobalConstants.WarewolfError);
-                                        }
-                                    }
-                                }
-                                catch(Exception e)
-                                {
-                                    Dev2Logger.Error(e, GlobalConstants.WarewolfError);
-                                }
-                            }
-                        }
-                    }
-                }
-                catch(Exception e)
-                {
-                    Dev2Logger.Error(e, GlobalConstants.WarewolfError);
-                }
-            }
-        }
-
-
-
-
-        /// <summary>
-        /// Inits the persistence.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        private void InitPersistence(string path)
-        {
-            PersistTimer.Interval = 1000 * 5; // every 5 seconds
-            PersistTimer.Enabled = true;
-            PersistTimer.Elapsed += (sender, args) => Persist(path);
-        }
-
-        #endregion
-
-        #region Public Methods
-
-
-        /// <summary>
-        /// Persists the specified path.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        public void Persist(string path)
-        {
-            if(_changes)
-            {
-                lock(Lock)
-                {
-                    try
-                    {
-                        // Persistence work ;)
-                        var keys = _messageRepo.Keys;
-                        foreach(var k in keys)
-                        {
-                            if (_messageRepo.TryGetValue(k, out IList<ICompileMessageTO> val))
-                            {
-                                var pPath = Path.Combine(path, k + ".msg");
-                                BinaryFormatter bf = new BinaryFormatter();
-                                using (Stream s = new FileStream(pPath, FileMode.OpenOrCreate))
-                                {
-                                    bf.Serialize(s, val);
-                                }
-                            }
-                        }
-
-                        _changes = false;
-                    }
-                    catch(Exception e)
-                    {
-                        Dev2Logger.Error(e, GlobalConstants.WarewolfError);
-                    }
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// Adds the message.
-        /// </summary>
-        /// <param name="workspaceId">The workspace ID.</param>
-        /// <param name="msgs">The MSGS.</param>
-        /// <returns></returns>
         public bool AddMessage(Guid workspaceId, IList<ICompileMessageTO> msgs)
         {
             if(msgs.Count == 0)
@@ -210,22 +78,17 @@ namespace Dev2.Runtime.Hosting
                     messages = new List<ICompileMessageTO>();
                 }
 
-                // clean up any messages with the same id and add
-
                 for (int i = messages.Count - 1; i >= 0; i--)
                 {
                     messages.Remove(messages[i]);
                 }
-
-                // now add new messages ;)
+                
                 foreach(var msg in msgs)
                 {
                     messages.Add(msg);
                 }
                 _allMessages.OnNext(messages);
                 _messageRepo[workspaceId] = messages;
-
-                _changes = true;
             }
 
             return true;
@@ -247,8 +110,6 @@ namespace Dev2.Runtime.Hosting
                 }
             }
         }
-
-        #endregion
 
         public void ClearObservable()
         {
