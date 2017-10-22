92d036381794478a0875ad285fd55b537e7b66d4
rajendrasingh2121 <rajendra.singh2121@gmail.com>
2017-10-22 21:48:37 +0530

    commented old Tracker usage

--- Dev/Dev2.Studio/ViewModels/WorksurfaceContextManager.cs
+++ Dev/Dev2.Studio/ViewModels/WorksurfaceContextManager.cs
@@ -1272,10 +1272,10 @@ namespace Dev2.Studio.ViewModels
 
             if (exists == null)
             {
-                if (typeof(T) == typeof(SettingsViewModel))
-                {
-                    Tracker.TrackEvent(TrackerEventGroup.Settings, TrackerEventName.Opened);
-                }
+                //if (typeof(T) == typeof(SettingsViewModel))
+                //{
+                //    //Tracker.TrackEvent(TrackerEventGroup.Settings, TrackerEventName.Opened);
+                //}
 
                 return CreateAndActivateUniqueWorkSurface<T>(context);
             }
