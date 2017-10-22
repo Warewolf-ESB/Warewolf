1308fa195e43089682b5870c3a27bd6c6a086a8c
rajendrasingh2121 <rajendra.singh2121@gmail.com>
2017-10-22 21:33:06 +0530

    Added specflow test project

---
+++ Dev/Dev2.Instrumentation.SpecflowTests/TestCustomEvenTrackingSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Dev2.Instrumentation.Factory;
+using TechTalk.SpecFlow;
+
+namespace Dev2.Instrumentation.SpecflowTests
+{
+    [Binding]
+    public class TestCustomEvenTrackingSteps
+    {
+        private IApplicationTracker _applicationTracker;
+        [Given(@"I have application Tracker instance")]
+        public void GivenIHaveApplicationTrackerInstance()
+        {
+            _applicationTracker = ApplicationTrackerFactory.GetApplicationTrackerProvider();
+        }
+        
+        [Given(@"I will enable application tracking by calling method")]
+        public void GivenIWillEnableApplicationTrackingByCallingMethod()
+        {
+            string productVersion = "1.0.0.0";
+            string username = "windows\\raju";
+            _applicationTracker.EnableAppplicationTracker(productVersion, username);
+        }
+
+        [When(@"I will call track custom event method")]
+        public void ThenIwillcalltrackcustomeventmethod()
+        {
+            _applicationTracker.TrackCustomEvent("Test custom Event Category", "Testing Event","custom values");
+
+        }
+
+        [Then(@"I will disable application tracking by calling method")]
+        public void ThenIWillDisableApplicationTrackingByCallingMethod()
+        {
+            _applicationTracker.DisableAppplicationTracker();
+        }
+    }
+}
---
+++ Dev/Dev2.Instrumentation.SpecflowTests/TestEnableRevulyticsTracking.feature
@@ -0,0 +1,8 @@
+﻿Feature: TestEnableRevulyticsTracking
+	This test will chceck the status of revulytics tracker to running
+
+@TestEnableTracking
+Scenario: Test Enable Revulytics tracking
+	Given I have revulytics instance 
+	And I will call the EnableAppplicationTracker method
+	Then I will check the status of revulytics tracker
