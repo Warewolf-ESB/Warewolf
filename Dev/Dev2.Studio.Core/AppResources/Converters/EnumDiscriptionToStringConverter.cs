656f2477ae1b37d40df0aa9b76cd7ed89c8a94c4
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-25 16:06:48 +0200

    re-introduce this converter as it is used by the gather system information tool

---
+++ Dev/Dev2.Studio.Core/AppResources/Converters/EnumDiscriptionToStringConverter.cs
@@ -0,0 +1,50 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using Dev2.Common.ExtMethods;
+using Dev2.Common.Interfaces.Enums.Enums;
+
+
+namespace Dev2.Studio.Core.AppResources.Converters
+{
+    public class EnumDiscriptionToStringConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as Enum).GetDescription();
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Dev2EnumConverter.GetEnumFromStringDiscription(value as string, targetType);
+        }
+
+        #endregion
+    }
+}
