005ff0e77824d530efc9020b00768188b8f3387b
Sanele <sanele.mthembu@dev2.co.za>
2017-10-25 16:22:04 +0200

    2811 : Added merge script to the Repo

---
+++ Dev/Dev2.Studio/MergePowershellScript/customMerge.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Wrapper script for git mergetool
+# [mergetool "customMerge"]
+#	cmd = customMerge \"$LOCAL\" \"$REMOTE\" 
+# Locate this script in your $HOME
+
+echo "Custom Merge $1 $2";
+LOCAL=$1
+REMOTE=$2
+
+WDIFF="\.cs"
+
+if echo "$LOCAL" | grep -q "$WDIFF"; then
+	echo "Using Warewolf to merge";		
+	powershell -command "Start-Process \"C:\PROGRAM FILES (x86)\Warewolf\Studio\Warewolf Studio.exe\" -ArgumentList \"$1 $2\" -Verb runas"
+	
+fi
