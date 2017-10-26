ae2e843c9d7dcc1a80ed6d57f70090870d79b5db
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-26 16:26:43 +0200

    Revert "delete SetCaretIndexAction.cs as it apparently unused"
    This reverts commit b1cb8495d7d867e2cf3672308c7122d6e710df21.

---
+++ Dev/Dev2.Studio.Core/AppResources/TriggerActions/SetCaretIndexAction.cs
@@ -0,0 +1,36 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+
+namespace Dev2.Studio.Core.AppResources.TriggerActions
+{
+    public class SetCaretIndexAction : TriggerAction<TextBox>
+    {
+        public int IndexPosition
+        {
+            get { return (int)GetValue(IndexPositionProperty); }
+            set { SetValue(IndexPositionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IndexPosition.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IndexPositionProperty =
+            DependencyProperty.Register("IndexPosition", typeof(int), typeof(SetCaretIndexAction), new PropertyMetadata(0));
+
+        protected override void Invoke(object parameter)
+        {
+            AssociatedObject.CaretIndex = IndexPosition;
+            AssociatedObject.Focus();
+        }
+    }
+}
