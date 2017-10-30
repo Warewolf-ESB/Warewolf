7aabcaa132d1ec1c3464bcfd8c0536bb8efcc213
Sanele <sanele.mthembu@dev2.co.za>
2017-10-30 11:47:39 +0200

    2811 : Cleaned up OpenBasedOnArguments

--- Dev/Dev2.Studio/Factory/MergeFactory.cs
+++ Dev/Dev2.Studio/Factory/MergeFactory.cs
@@ -15,7 +15,6 @@ namespace Dev2.Factory
 
         public void OpenMergeWindow(IShellViewModel shellViewModel, string item, WarwolfStartupEventArgs args)
         {
-            var strBuilder = new StringBuilder();
             VerifyArgument.IsNotNull(nameof(shellViewModel), shellViewModel);
             var cleanPath = args.Args[1].Replace('^', ' ');
             var mergeHeadPath = cleanPath;
