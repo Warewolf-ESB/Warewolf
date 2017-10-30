27ffad60f91aa4a8914268136a75849547e29b20
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-30 08:44:45 +0200

    remove the implimentation of GetHashCode in this class as it is apparently unused

--- Dev/Dev2.Studio.Core/AppResources/DependencyInjection/EqualityComparers/ContextualResourceModelEqualityComparer.cs
+++ Dev/Dev2.Studio.Core/AppResources/DependencyInjection/EqualityComparers/ContextualResourceModelEqualityComparer.cs
@@ -17,7 +17,6 @@ namespace Dev2.Studio.Core.AppResources.DependencyInjection.EqualityComparers
 {
     public class ContexttualResourceModelEqualityComparer : IEqualityComparer<IContextualResourceModel>
     {
-
         private static readonly Lazy<ContexttualResourceModelEqualityComparer> _current
             = new Lazy<ContexttualResourceModelEqualityComparer>(() => new ContexttualResourceModelEqualityComparer());
 
@@ -29,35 +28,17 @@ namespace Dev2.Studio.Core.AppResources.DependencyInjection.EqualityComparers
 
         public bool Equals(IContextualResourceModel x, IContextualResourceModel y)
         {
-            //Check whether the compared objects reference the same data.
             if(ReferenceEquals(x, y))
             {
                 return true;
-            }
-
-            //Check whether any of the compared objects is null.
+            }            
             if (ReferenceEquals(x, null) || ReferenceEquals(y, null))
             {
                 return false;
-            }
-
-            //Check whether the products' properties are equal.
+            }            
             return EnvironmentModelEqualityComparer.Current.Equals(x.Environment, y.Environment) && x.ResourceName == y.ResourceName;
         }
 
-        public int GetHashCode(IContextualResourceModel obj)
-        {
-            //Check whether the object is null
-            if(ReferenceEquals(obj, null))
-            {
-                return 0;
-            }
-
-            //Get hash code for the Name field if it is not null.
-            int hashProductName = obj.ResourceName?.GetHashCode() ?? 0;
-
-            //Get hash code for the Code field.
-            return hashProductName;
-        }
+        public int GetHashCode(IContextualResourceModel obj) => throw new NotImplementedException();
     }
 }
