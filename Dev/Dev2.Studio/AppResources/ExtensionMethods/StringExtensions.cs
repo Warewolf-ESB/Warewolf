6ffc4040aa342209373f934de557264c9f4ce7db
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-01 11:37:04 +0200

    remove unreferenced and apparently unused public method from StringExtensions.cs

--- Dev/Dev2.Studio/AppResources/ExtensionMethods/StringExtensions.cs
+++ Dev/Dev2.Studio/AppResources/ExtensionMethods/StringExtensions.cs
@@ -18,19 +18,6 @@ namespace Dev2.Studio.AppResources.ExtensionMethods
 {
     public static class StringExtensions
     {
-        public static string GetManagementPayload(this string payload)
-        {
-            if(payload.Contains("<Dev2System.ManagmentServicePayload>"))
-            {
-                var startIndx = payload.IndexOf("<Dev2System.ManagmentServicePayload>", StringComparison.Ordinal);
-                var length = "<Dev2System.ManagmentServicePayload>".Length;
-                var endIndx = payload.IndexOf("</Dev2System.ManagmentServicePayload>", StringComparison.Ordinal);
-                var l = endIndx - startIndx - length;
-                return payload.Substring(startIndx + length, l);
-            }
-            return string.Empty;
-        }
-
         public static string ExceptChars(this string str, IEnumerable<char> toExclude)
         {
             StringBuilder sb = new StringBuilder();
