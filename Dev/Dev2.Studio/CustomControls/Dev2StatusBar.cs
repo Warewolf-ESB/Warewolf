5a46ac97d61944e2838b7db9a49cc4ecd9c131c4
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-01 14:48:17 +0200

    remove unused members of Dev2StatusBar.cs

--- Dev/Dev2.Studio/CustomControls/Dev2StatusBar.cs
+++ Dev/Dev2.Studio/CustomControls/Dev2StatusBar.cs
@@ -14,36 +14,14 @@ using System.Windows.Controls;
 
 namespace Dev2.Studio.CustomControls
 {
-    /// <author>Massimo.Guerrera</author>
-    /// <date>2/28/2013</date>
     [TemplatePart(Name = PART_Label, Type = typeof(Label))]
     public class Dev2StatusBar : TextBox
     {
-
         private const string PART_Label = "StatusBarLabel";
-
-        private Label _label;
-
-        public Label StatusBarLabel => _label;
-
-
-        public string StatusBarLabelText
-        {
-            get { return (string)GetValue(StatusBarLabelTextProperty); }
-            set { SetValue(StatusBarLabelTextProperty, value); }
-        }
 
         // Using a DependencyProperty as the backing store for StatusBarLabelText.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty StatusBarLabelTextProperty =
             DependencyProperty.Register("StatusBarLabelText", typeof(string), typeof(Dev2StatusBar), new PropertyMetadata(string.Empty));
-
-
-
-        public Visibility ProgressBarVisiblity
-        {
-            get { return (Visibility)GetValue(ProgressBarVisiblityProperty); }
-            set { SetValue(ProgressBarVisiblityProperty, value); }
-        }
 
         // Using a DependencyProperty as the backing store for ProgressBarVisiblity.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ProgressBarVisiblityProperty =
@@ -53,13 +31,5 @@ namespace Dev2.Studio.CustomControls
         {
             DefaultStyleKey = typeof(Dev2StatusBar);
         }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-
-            _label = GetTemplateChild(PART_Label) as Label;
-        }
-
     }
 }
