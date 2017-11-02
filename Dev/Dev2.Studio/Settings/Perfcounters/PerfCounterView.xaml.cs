cca1fce977beae16c19a80f94a0a5cee2d99d723
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-02 15:25:35 +0200

    Revert "delete ResourceColumnSorting method from PerfCounterView because it doesn't actually get used right now"
    This reverts commit 46be76151d9a2c70a1321d0773e00eb6d73da759.

--- Dev/Dev2.Studio/Settings/Perfcounters/PerfCounterView.xaml.cs
+++ Dev/Dev2.Studio/Settings/Perfcounters/PerfCounterView.xaml.cs
@@ -5,11 +5,41 @@ using Infragistics.Controls.Grids;
 
 namespace Dev2.Settings.Perfcounters
 {
+    /// <summary>
+    /// Interaction logic for PerfCounterView.xaml
+    /// </summary>
     public partial class PerfCounterView : UserControl
     {
         public PerfCounterView()
         {
             InitializeComponent();
+        }
+         ListSortDirection? _previousServerDirection;
+        ListSortDirection? _previousResourceDirection;
+
+        void ResourceColumnSorting(object sender, SortingCancellableEventArgs e)
+        {
+            if (_previousResourceDirection == null)
+            {
+                _previousResourceDirection = ListSortDirection.Ascending;
+            }
+
+            Sort(sender, e);
+        }
+
+        void Sort(object sender, SortingCancellableEventArgs e)
+        {
+            var dataGrid = (XamGrid)sender;
+            var column = e.Column;
+            var direction = _previousServerDirection != ListSortDirection.Ascending ? ListSortDirection.Ascending : ListSortDirection.Descending;
+            _previousServerDirection = direction;
+            var collectionView = CollectionViewSource.GetDefaultView(dataGrid.ItemsSource);
+            var lcv = (ListCollectionView)collectionView;
+
+            var comparer = new CounterByResoureEqualityComparer(direction, column.Key);
+            lcv.CustomSort = comparer;
+            dataGrid.ItemsSource = lcv;
+            e.Cancel = true;
         }
     }
 }
