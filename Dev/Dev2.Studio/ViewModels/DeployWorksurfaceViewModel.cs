33f356e368810f93a238a5d5e3909f44a41d9135
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-03 08:16:22 +0200

    remove apparently unused contructor from DeployWorksurfaceViewModel.cs

--- Dev/Dev2.Studio/ViewModels/DeployWorksurfaceViewModel.cs
+++ Dev/Dev2.Studio/ViewModels/DeployWorksurfaceViewModel.cs
@@ -7,14 +7,12 @@ using Dev2.Studio.ViewModels.WorkSurface;
 using Microsoft.Practices.Prism.Mvvm;
 using Warewolf.Studio.ViewModels;
 using Warewolf.Studio.Views;
+using System;
 
 namespace Dev2.ViewModels
 {
     public class DeployWorksurfaceViewModel : BaseWorkSurfaceViewModel, IHelpSource, IStudioTab
-
     {
-        readonly IPopupController _popupController;
-
         public DeployWorksurfaceViewModel():base(new EventAggregator())
         {
             var dest = new DeployDestinationViewModel(CustomContainer.Get<IShellViewModel>(), CustomContainer.Get<Microsoft.Practices.Prism.PubSubEvents.IEventAggregator>());
@@ -27,26 +25,6 @@ namespace Dev2.ViewModels
             ViewModel.PropertyChanged += (sender, args) =>
             {
                 if (args.PropertyName == "Header")
-                {
-                    OnPropertyChanged("DisplayName");
-                }
-                var mainViewModel = CustomContainer.Get<IShellViewModel>();
-                if (mainViewModel != null)
-                {
-                    ViewModelUtils.RaiseCanExecuteChanged(mainViewModel.SaveCommand);
-                }
-            };
-        }
-
-        public DeployWorksurfaceViewModel(IEventAggregator eventPublisher, SingleExplorerDeployViewModel vm, IPopupController popupController,IView view)
-            : base(eventPublisher)
-        {
-            ViewModel = vm;
-            View = view;
-            _popupController = popupController;
-            ViewModel.PropertyChanged += (sender, args) =>
-            {
-                if(args.PropertyName == "Header")
                 {
                     OnPropertyChanged("DisplayName");
                 }
@@ -94,15 +72,9 @@ namespace Dev2.ViewModels
 
         public bool IsDirty => false;
 
-        public void CloseView()
-        {
-        }
+        public void CloseView() => throw new NotImplementedException();
 
-        public bool DoDeactivate(bool showMessage)
-        {
-
-            return true;
-        }
+        public bool DoDeactivate(bool showMessage) => throw new NotImplementedException();
 
         #endregion
 
