b9b7e6d39f26148f57a64ce358318741a559c7a0
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2017-11-06 07:06:20 +0200

    some cleanup

--- Dev/Dev2.Activities/Activities/DsfDecision.cs
+++ Dev/Dev2.Activities/Activities/DsfDecision.cs
@@ -63,14 +63,14 @@ namespace Dev2.Activities
             {
                 foreach (var next in TrueArm)
                 {
-                    armConnectors.Add(($"{GetDisplayName()}: TRUE -> {next.GetDisplayName()}", "TRUE", UniqueID, next.UniqueID));
+                    armConnectors.Add(($"{GetDisplayName()}: TRUE -> {next.GetDisplayName()}", "True", UniqueID, next.UniqueID));
                 }
             }
             if (FalseArm != null)
             {
                 foreach (var next in FalseArm)
                 {
-                    armConnectors.Add(($"{GetDisplayName()}: FALSE -> {next.GetDisplayName()}", "FALSE", UniqueID, next.UniqueID));
+                    armConnectors.Add(($"{GetDisplayName()}: FALSE -> {next.GetDisplayName()}", "False", UniqueID, next.UniqueID));
                 }
             }
             return armConnectors;
