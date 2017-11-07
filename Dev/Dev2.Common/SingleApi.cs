6a6aa11f0258a11c0000c2a242bc81262d4256a2
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-07 14:46:43 +0200

    remove IEquatable from SingleApi.cs's inheritance hierarchy, it is apparently unused

--- Dev/Dev2.Common/SingleApi.cs
+++ Dev/Dev2.Common/SingleApi.cs
@@ -13,85 +13,8 @@ using System.Collections.Generic;
 
 namespace Dev2.Common
 {
-    public class SingleApi : IEquatable<SingleApi>
+    public class SingleApi
     {
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(SingleApi other)
-        {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-            return string.Equals(Name, other.Name) && string.Equals(Description, other.Description) && string.Equals(Image, other.Image) && string.Equals(HumanUrl, other.HumanUrl) && string.Equals(BaseUrl, other.BaseUrl) && string.Equals(Version, other.Version) && Equals(Tags, other.Tags) && Equals(Properties, other.Properties) && Equals(Contact, other.Contact);
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-            if (obj.GetType() != GetType())
-            {
-                return false;
-            }
-            return Equals((SingleApi)obj);
-        }
-
-        /// <summary>
-        /// Serves as a hash function for a particular type.
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Name?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (Description?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Image?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (HumanUrl?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (BaseUrl?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Version?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Tags?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Properties?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Contact?.GetHashCode() ?? 0);
-                return hashCode;
-            }
-        }
-
-        public static bool operator ==(SingleApi left, SingleApi right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(SingleApi left, SingleApi right)
-        {
-            return !Equals(left, right);
-        }
-
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
