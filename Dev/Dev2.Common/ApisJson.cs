b73483445c06677da01abd2a2aa32b3eb909bdc7
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-07 10:24:10 +0200

    remove all xml method summaries from this class

--- Dev/Dev2.Common/ApisJson.cs
+++ Dev/Dev2.Common/ApisJson.cs
@@ -15,13 +15,6 @@ namespace Dev2.Common
 {
     public class ApisJson : IEquatable<ApisJson>
     {
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
         public bool Equals(ApisJson other)
         {
             if (ReferenceEquals(null, other))
@@ -34,14 +27,7 @@ namespace Dev2.Common
             }
             return string.Equals(Name, other.Name) && string.Equals(Description, other.Description) && string.Equals(Image, other.Image) && string.Equals(Url, other.Url) && Created.Equals(other.Created) && Modified.Equals(other.Modified) && string.Equals(SpecificationVersion, other.SpecificationVersion);
         }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
+        
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj))
@@ -58,13 +44,7 @@ namespace Dev2.Common
             }
             return Equals((ApisJson)obj);
         }
-
-        /// <summary>
-        /// Serves as a hash function for a particular type.
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
+        
         public override int GetHashCode()
         {
             unchecked
