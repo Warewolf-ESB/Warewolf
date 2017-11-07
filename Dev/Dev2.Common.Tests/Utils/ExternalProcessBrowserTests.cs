e8fbc23df46eb6caf7a44392c820f23f19934e03
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-07 11:46:06 +0200

    extra unit test coverage over ExternalProcessBrowserTests

---
+++ Dev/Dev2.Common.Tests/Utils/ExternalProcessBrowserTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Dev2.Common.Tests.Utils
+{
+    [TestClass]
+    public class ExternalProcessBrowserTests
+    {
+        [TestMethod]
+        public void ExternalProcessBrowser_With_ProcessTimout()
+        {
+            var mockedUrl = new Mock<Uri>("https://warewolf.io");
+            mockedUrl.Setup(uri => uri.ToString()).Throws(new TimeoutException());
+            var processExecutor = new ExternalProcessExecutor();
+            processExecutor.OpenInBrowser(mockedUrl.Object);
+        }
+    }
+}
