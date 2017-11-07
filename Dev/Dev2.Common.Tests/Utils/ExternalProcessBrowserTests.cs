d45e25c2b2a2edab4f23c1b36236ee73e2339913
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-07 11:47:16 +0200

    even more test covarege over ExternalProcessBrowser

--- Dev/Dev2.Common.Tests/Utils/ExternalProcessBrowserTests.cs
+++ Dev/Dev2.Common.Tests/Utils/ExternalProcessBrowserTests.cs
@@ -15,5 +15,14 @@ namespace Dev2.Common.Tests.Utils
             var processExecutor = new ExternalProcessExecutor();
             processExecutor.OpenInBrowser(mockedUrl.Object);
         }
+
+        [TestMethod]
+        public void ExternalProcessBrowser_With_ComException()
+        {
+            var mockedUrl = new Mock<Uri>("https://warewolf.io");
+            mockedUrl.Setup(uri => uri.ToString()).Throws(new System.Runtime.InteropServices.COMException());
+            var processExecutor = new ExternalProcessExecutor();
+            processExecutor.OpenInBrowser(mockedUrl.Object);
+        }
     }
 }
