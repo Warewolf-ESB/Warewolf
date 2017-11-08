5601f46e894262928f9e66503c8da795ad012e89
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-08 15:03:15 +0200

    removing apparently unused class WidthConverter

--- Dev/Warewolf.Studio.CustomControls/ServiceTestWidthConverter.cs
+++ Dev/Warewolf.Studio.CustomControls/ServiceTestWidthConverter.cs
@@ -2,37 +2,9 @@ using System;
 using System.Globalization;
 using System.Windows.Controls;
 using System.Windows.Data;
-using Infragistics.Controls.Menus;
 
 namespace Warewolf.Studio.CustomControls
 {
-    /// <summary>
-    /// Returns the exact length of the requred menu node based on its tree position
-    /// </summary>
-    public class WidthConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-
-            try
-            {
-                if (values[0] is XamDataTreeNodeControl xamDataTreeNodeControl)
-                {
-                    return (double)values[1] - xamDataTreeNodeControl.Node.Manager.Level * 21 - 62;
-                }
-                return 22;
-            }
-            catch(Exception)
-            {
-                return 22;
-            }
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public class ServiceTestWidthConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
