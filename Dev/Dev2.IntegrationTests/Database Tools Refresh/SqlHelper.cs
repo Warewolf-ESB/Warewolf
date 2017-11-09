849510e0ded191ed6304f18d9231a2539c31e7ec
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-11-09 11:00:17 +0200

    WOLF-2818 SqlTests for changing inputs

---
+++ Dev/Dev2.IntegrationTests/Database Tools Refresh/SqlHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using System.Transactions;
+
+namespace Dev2.Integration.Tests.Database_Tools_Refresh
+{
+    public static class SqlHelper
+    {
+        public static int RunSqlCommand(string command)
+        {
+            using (var trans = new TransactionScope())
+            {
+                using (var con = new SqlConnection("Data Source=RSAKLFSVRDEV,1433;Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;"))
+                {
+                    var cmd = con.CreateCommand();
+                    cmd.CommandText = command;
+                    con.Open();
+                    var a = cmd.ExecuteNonQuery();
+                    trans.Complete();
+                    return a;
+                }
+            }
+        }
+    }
+}
