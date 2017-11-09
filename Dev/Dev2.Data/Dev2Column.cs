93d935d2dfecece16a8c678561c6f71870eb169a
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-09 08:44:20 +0200

    remove unused empty constructors from Dev2Column.cs

--- Dev/Dev2.Data/Dev2Column.cs
+++ Dev/Dev2.Data/Dev2Column.cs
@@ -17,98 +17,20 @@ namespace Dev2.DataList.Contract.Binary_Objects
     [Serializable]
     public class Dev2Column : IEquatable<Dev2Column>
     {
-
         #region Properties
-
-        /// <summary>
-        /// Gets or sets the name of the column.
-        /// </summary>
-        /// <value>
-        /// The name of the column.
-        /// </value>
+        
         public string ColumnName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the column description.
-        /// </summary>
-        /// <value>
-        /// The column description.
-        /// </value>
+        
         public string ColumnDescription { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this instance is editable.
-        /// </summary>
-        /// <value>
-        /// <c>true</c> if this instance is editable; otherwise, <c>false</c>.
-        /// </value>
+        
         public bool IsEditable { get; set; }
-
-        /// <summary>
-        /// Gets or sets the argument direction.
-        /// </summary>
-        /// <value>
-        /// The argument direction.
-        /// </value>
+        
         public enDev2ColumnArgumentDirection ColumnIODirection { get; set; }
 
         #endregion Properties
         
         #region Ctor
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Dev2Column" /> class.
-        /// </summary>
-        /// <param name="columnName">Name of the column.</param>
-        /// <param name="columnDesciption">The column desciption.</param>
-        internal Dev2Column(string columnName, string columnDesciption)
-            : this(columnName,columnDesciption,true)
-        {          
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Dev2Column" /> class.
-        /// </summary>
-        /// <param name="columnName">Name of the column.</param>
-        /// <param name="ioDir">The io dir.</param>
-        internal Dev2Column(string columnName, enDev2ColumnArgumentDirection ioDir)
-            : this(columnName, string.Empty, true, ioDir)
-        {
-        }
-
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Dev2Column" /> class.
-        /// </summary>
-        /// <param name="columnName">Name of the column.</param>
-        /// <param name="columnDesciption">The column desciption.</param>
-        /// <param name="ioDir">The io dir.</param>
-        internal Dev2Column(string columnName, string columnDesciption, enDev2ColumnArgumentDirection ioDir)
-            : this(columnName,columnDesciption,true, ioDir)
-        {          
-        }
-
-
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Dev2Column" /> class.
-        /// </summary>
-        /// <param name="columnName">Name of the column.</param>
-        /// <param name="columnDesciption">The column desciption.</param>
-        /// <param name="isEditable">if set to <c>true</c> [is editable].</param>
-        internal Dev2Column(string columnName, string columnDesciption,bool isEditable) 
-            : this(columnName, columnDesciption, isEditable, enDev2ColumnArgumentDirection.None)
-        {
-        }
-
-        // <summary>
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Dev2Column" /> class.
-        /// </summary>
-        /// <param name="columnName">Name of the column.</param>
-        /// <param name="columnDesciption">The column desciption.</param>
-        /// <param name="isEditable">if set to <c>true</c> [is editable].</param>
-        /// <param name="dir">The column dir.</param>
+        
         internal Dev2Column(string columnName, string columnDesciption, bool isEditable, enDev2ColumnArgumentDirection dir)
         {
             ColumnName = columnName;
@@ -118,14 +40,7 @@ namespace Dev2.DataList.Contract.Binary_Objects
         }
 
         #region Equality members
-
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
+        
         public bool Equals(Dev2Column other)
         {
             if(ReferenceEquals(null, other))
@@ -137,48 +52,6 @@ namespace Dev2.DataList.Contract.Binary_Objects
                 return true;
             }
             return string.Equals(ColumnName, other.ColumnName) && string.Equals(ColumnDescription, other.ColumnDescription) && IsEditable == other.IsEditable && ColumnIODirection == other.ColumnIODirection;
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
-        public override bool Equals(object obj)
-        {
-            if(ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if(ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-            if(obj.GetType() != GetType())
-            {
-                return false;
-            }
-            return Equals((Dev2Column)obj);
-        }
-
-        /// <summary>
-        /// Serves as a hash function for a particular type. 
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = ColumnName?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (ColumnDescription?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ IsEditable.GetHashCode();
-                hashCode = (hashCode * 397) ^ (int)ColumnIODirection;
-                return hashCode;
-            }
         }
 
         public static bool operator ==(Dev2Column left, Dev2Column right)
