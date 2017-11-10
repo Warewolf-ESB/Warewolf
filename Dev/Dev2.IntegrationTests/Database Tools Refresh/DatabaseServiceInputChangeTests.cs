e6f24b922cef5dab90b327188fccc78ad28d3767
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-11-10 10:18:50 +0200

    WOLF-2818 added a test for source change events

--- Dev/Dev2.IntegrationTests/Database Tools Refresh/DatabaseServiceInputChangeTests.cs
+++ Dev/Dev2.IntegrationTests/Database Tools Refresh/DatabaseServiceInputChangeTests.cs
@@ -4,6 +4,7 @@ using Dev2.Activities.Designers2.Core.ActionRegion;
 using Dev2.Activities.Designers2.Core.InputRegion;
 using Dev2.Activities.Designers2.Core.Source;
 using Dev2.Common.Interfaces.DB;
+using Dev2.Common.Interfaces.ServerProxyLayer;
 using Dev2.Common.Interfaces.ToolBase.Database;
 using Dev2.Controller;
 using Dev2.Studio.Core;
@@ -33,7 +34,7 @@ namespace Dev2.Integration.Tests.Database_Tools_Refresh
 
         [TestMethod]
         [Owner("Nkosinathi Sangweni")]
-        public void SqlDb_Add_A_New_InputOnSqlProcedure_Expect_New_IS_InputAdded()
+        public void Add_A_New_InputOnSqlProcedure_Expect_New_IS_InputAdded()
         {
             var newName = Guid.NewGuid().ToString();
             var cleanProcName = newName.Replace("-", "").Replace(" ", "");
@@ -96,6 +97,55 @@ namespace Dev2.Integration.Tests.Database_Tools_Refresh
             Assert.AreEqual(-1, alterTableResults);
             var resultForDrop = SqlHelper.RunSqlCommand(dropProcedure);
             Assert.AreEqual(-1, resultForDrop);
-        }            
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void change_sql_source_verify_Empty_Inputs()
+        {
+            var newName = Guid.NewGuid().ToString();
+            var cleanProcName = newName.Replace("-", "").Replace(" ", "");
+            var createProcedure = "CREATE procedure [dbo].[" + cleanProcName + "](@ProductId int) as Begin select * from Country select * from City end";
+            var result = SqlHelper.RunSqlCommand(createProcedure);
+            Assert.AreEqual(-1, result);
+            List<IServiceInput> inputs = new List<IServiceInput>()
+                {
+                    new ServiceInput("ProductId","[[ProductId]]"){ActionName = "dbo." + cleanProcName}
+                };
+            var sqlActivity = new DsfSqlServerDatabaseActivity()
+            {
+                Inputs = inputs,
+                ActionName = "dbo." + cleanProcName,
+                ProcedureName = "dbo." + cleanProcName,
+                SourceId = new Guid("b9184f70-64ea-4dc5-b23b-02fcd5f91082")
+            };
+            var modelItem = ModelItemUtils.CreateModelItem(sqlActivity);
+            var environmentModel = ServerRepository.Instance.Source;
+            environmentModel.Connect();
+            var environmentConnection = environmentModel.Connection;
+            var controllerFactory = new CommunicationControllerFactory();
+            var _proxyLayer = new StudioServerProxy(controllerFactory, environmentConnection);
+            var mock = new Mock<IShellViewModel>();
+            ManageDbServiceModel dbServiceModel = new ManageDbServiceModel(new StudioResourceUpdateManager(controllerFactory, environmentConnection)
+                                                                                    , _proxyLayer.QueryManagerProxy
+                                                                                    , mock.Object
+                                                                                    , environmentModel);
+            var source = new DatabaseSourceRegion(dbServiceModel, modelItem, Common.Interfaces.Core.DynamicServices.enSourceType.SqlDatabase);
+            var selectedSource = source.Sources.Single(a => a.Id == sqlActivity.SourceId);
+            source.SelectedSource = selectedSource;
+            var actionRegion = new DbActionRegion(dbServiceModel, modelItem, source, new SynchronousAsyncWorker());
+
+            var mockSource = new Mock<IDbSource>();
+
+            IDatabaseInputRegion databaseInputRegion = new DatabaseInputRegion(modelItem, actionRegion);
+            Assert.AreEqual(1, databaseInputRegion.Inputs.Count);
+            Assert.AreEqual("ProductId", databaseInputRegion.Inputs.Single().Name);
+            Assert.AreEqual("[[ProductId]]", databaseInputRegion.Inputs.Single().Value);
+            //add testing here
+
+            source.SelectedSource = mockSource.Object;
+            Assert.AreEqual(0, databaseInputRegion.Inputs.Count);          
+           
+        }
     }
 }
