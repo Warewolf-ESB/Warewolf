4288424d9dea07928c620251bf292190d5d78c3b
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-15 15:46:42 +0200

    collapse these funtions of WindowsGroupPermissionEqualityComparer to reduce complexity and make this class easier to cover with tests

--- Dev/Dev2.Infrastructure/Services/Security/WindowsGroupPermissionEqualityComparer.cs
+++ Dev/Dev2.Infrastructure/Services/Security/WindowsGroupPermissionEqualityComparer.cs
@@ -8,59 +8,18 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using System;
 using System.Collections.Generic;
 
 namespace Dev2.Services.Security
 {
-
     public class WindowsGroupPermissionEqualityComparer : IEqualityComparer<WindowsGroupPermission>
     {
         #region Implementation of IEqualityComparer<in WindowsGroupPermission>
 
-        
-        /// <summary>
-        /// Determines whether the specified objects are equal.
-        /// </summary>
-        /// <returns>
-        /// true if the specified objects are equal; otherwise, false.
-        /// </returns>
-        /// <param name="x">The first object to compare.</param><param name="y">The second object to compare.</param>
-        
-        public bool Equals(WindowsGroupPermission x, WindowsGroupPermission y)
-        {
-            var isEqual = x.Permissions.Equals(y.Permissions) && x.ResourceID.Equals(y.ResourceID);
-            if(!string.IsNullOrEmpty(x.ResourceName) && !string.IsNullOrEmpty(y.ResourceName))
-            {
-                isEqual = isEqual && x.ResourceName.Equals(y.ResourceName);
-            }
-            if(!string.IsNullOrEmpty(x.WindowsGroup) && !string.IsNullOrEmpty(y.WindowsGroup))
-            {
-                isEqual = isEqual && x.WindowsGroup.Equals(y.WindowsGroup);
-            }
-            return isEqual;
-        }
+        public bool Equals(WindowsGroupPermission x, WindowsGroupPermission y) => x.Permissions.Equals(y.Permissions) && x.ResourceID.Equals(y.ResourceID) && x.WindowsGroup.Equals(y.WindowsGroup);
 
-        /// <summary>
-        /// Returns a hash code for the specified object.
-        /// </summary>
-        /// <returns>
-        /// A hash code for the specified object.
-        /// </returns>
-        /// <param name="obj">The <see cref="T:System.Object"/> for which a hash code is to be returned.</param><exception cref="T:System.ArgumentNullException">The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.</exception>
-        public int GetHashCode(WindowsGroupPermission obj)
-        {
-            var hashCode = 397;
-            hashCode += obj.Permissions.GetHashCode() ^ obj.ResourceID.GetHashCode();
-            if(!string.IsNullOrEmpty(obj.ResourceName))
-            {
-                hashCode += hashCode^ obj.ResourceName.GetHashCode();
-            }
-            if(!string.IsNullOrEmpty(obj.WindowsGroup))
-            {
-                hashCode +=  hashCode^ obj.WindowsGroup.GetHashCode();
-            }
-            return hashCode;
-        }
+        public int GetHashCode(WindowsGroupPermission obj) => throw new NotImplementedException();
 
         #endregion
     }
