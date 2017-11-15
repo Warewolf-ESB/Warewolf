859e529e1eff130619b253aedbff85b504bba4c1
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-15 16:00:34 +0200

    gut ToolDescriptor.cs of apparently unused bloat

--- Dev/Warewolf.Core/ToolDescriptor.cs
+++ Dev/Warewolf.Core/ToolDescriptor.cs
@@ -4,8 +4,6 @@ using Dev2.Common.Interfaces.Toolbox;
 
 namespace Warewolf.Core
 {
- 
-
     public class WarewolfType : IWarewolfType, IEquatable<WarewolfType>
     {
         public WarewolfType(string fullyQualifiedName, Version version, string containingAssemblyPath)
@@ -20,42 +18,12 @@ namespace Warewolf.Core
         public string FullyQualifiedName { get; private set; }
         public Version Version { get; private set; }
         public string ContainingAssemblyPath { get; private set; }
-
         public Type ActualType { get; set; }
 
         #endregion
-
-        #region Overrides of Object
-
+        
         #region Equality members
-
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(WarewolfType other)
-        {
-            if(ReferenceEquals(null, other))
-            {
-                return false;
-            }
-            if(ReferenceEquals(this, other))
-            {
-                return true;
-            }
-            return string.Equals(FullyQualifiedName, other.FullyQualifiedName) && Equals(Version, other.Version) && string.Equals(ContainingAssemblyPath, other.ContainingAssemblyPath);
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
+        
         public override bool Equals(object obj)
         {
             if(ReferenceEquals(null, obj))
@@ -73,22 +41,9 @@ namespace Warewolf.Core
             return Equals((WarewolfType)obj);
         }
 
-        /// <summary>
-        /// Serves as a hash function for a particular type. 
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = FullyQualifiedName?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (Version != null ? Version.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (ContainingAssemblyPath?.GetHashCode() ?? 0);
-                return hashCode;
-            }
-        }
+        public bool Equals(WarewolfType other) => throw new NotImplementedException();
+
+        public override int GetHashCode() => throw new NotImplementedException();
 
         public static bool operator ==(WarewolfType left, WarewolfType right)
         {
@@ -99,50 +54,6 @@ namespace Warewolf.Core
         {
             return !Equals(left, right);
         }
-
-        #endregion
-
-        #region WarewolfTypeEqualityComparer
-
-        sealed class WarewolfTypeEqualityComparer : IEqualityComparer<WarewolfType>
-        {
-            public bool Equals(WarewolfType x, WarewolfType y)
-            {
-                if(ReferenceEquals(x, y))
-                {
-                    return true;
-                }
-                if(ReferenceEquals(x, null))
-                {
-                    return false;
-                }
-                if(ReferenceEquals(y, null))
-                {
-                    return false;
-                }
-                if(x.GetType() != y.GetType())
-                {
-                    return false;
-                }
-                return string.Equals(x.FullyQualifiedName, y.FullyQualifiedName) && Equals(x.Version, y.Version) && string.Equals(x.ContainingAssemblyPath, y.ContainingAssemblyPath);
-            }
-
-            public int GetHashCode(WarewolfType obj)
-            {
-                unchecked
-                {
-                    var hashCode = obj.FullyQualifiedName?.GetHashCode() ?? 0;
-                    hashCode = (hashCode * 397) ^ (obj.Version != null ? obj.Version.GetHashCode() : 0);
-                    hashCode = (hashCode * 397) ^ (obj.ContainingAssemblyPath?.GetHashCode() ?? 0);
-                    return hashCode;
-                }
-            }
-        }
-
-        static readonly IEqualityComparer<WarewolfType> WarewolfTypeComparerInstance = new WarewolfTypeEqualityComparer();
-        public static IEqualityComparer<WarewolfType> WarewolfTypeComparer => WarewolfTypeComparerInstance;
-
-        #endregion
 
         #endregion
     }
