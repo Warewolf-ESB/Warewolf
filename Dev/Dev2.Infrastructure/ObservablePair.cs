556928484cb5a5ede96bb8e413d82d8454cf8325
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-15 15:01:53 +0200

    remove IEquatable from ObservablePair inheritance hierarch as it is apparently unused

--- Dev/Dev2.Infrastructure/ObservablePair.cs
+++ Dev/Dev2.Infrastructure/ObservablePair.cs
@@ -13,7 +13,7 @@ using System.Collections.Generic;
 
 namespace Dev2
 {
-    public class ObservablePair<TKey, TValue> : ObservableObject, IEquatable<ObservablePair<TKey, TValue>>
+    public class ObservablePair<TKey, TValue> : ObservableObject
     {
         TKey _key;
         TValue _value;
@@ -31,44 +31,5 @@ namespace Dev2
         public TKey Key { get => _key; set => OnPropertyChanged(ref _key, value); }
 
         public TValue Value { get => _value; set => OnPropertyChanged(ref _value, value); }
-
-        public bool Equals(ObservablePair<TKey, TValue> other)
-        {
-            return other != null && EqualityComparer<TKey>.Default.Equals(Key, other.Key);
-        }
-
-        public override bool Equals(Object obj)
-        {
-            if(obj == null)
-            {
-                return false;
-            }
-            var other = obj as ObservablePair<TKey, TValue>;
-            return other != null && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return Key.GetHashCode();
-        }
-
-
-        public static bool operator ==(ObservablePair<TKey, TValue> pair1, ObservablePair<TKey, TValue> pair2)
-        {
-            if((object)pair1 == null || (object)pair2 == null)
-            {
-                return Equals(pair1, pair2);
-            }
-            return pair1.Equals(pair2);
-        }
-
-        public static bool operator !=(ObservablePair<TKey, TValue> pair1, ObservablePair<TKey, TValue> pair2)
-        {
-            if(pair1 == null || pair2 == null)
-            {
-                return !Equals(pair1, pair2);
-            }
-            return !pair1.Equals(pair2);
-        }
     }
 }
