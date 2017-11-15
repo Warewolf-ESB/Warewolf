3f88efea0bbcdb35973746a4c364d01afbfef5e8
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-15 13:52:00 +0200

    remove method summaries from IncludeApi.cs

--- Dev/Dev2.Common/IncludeApi.cs
+++ Dev/Dev2.Common/IncludeApi.cs
@@ -14,13 +14,6 @@ namespace Dev2.Common
 {
     public class IncludeApi : IEquatable<IncludeApi>
     {
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
         public bool Equals(IncludeApi other)
         {
             if (ReferenceEquals(null, other))
@@ -33,14 +26,7 @@ namespace Dev2.Common
             }
             return string.Equals(Name, other.Name) && string.Equals(Url, other.Url);
         }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
+        
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj))
@@ -57,13 +43,7 @@ namespace Dev2.Common
             }
             return Equals((IncludeApi)obj);
         }
-
-        /// <summary>
-        /// Serves as a hash function for a particular type.
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
+        
         public override int GetHashCode()
         {
             unchecked
