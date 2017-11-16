5272352fda8ced32be3f15e5a990c90390a8518d
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-16 11:15:55 +0200

    re-introduce custom trace listener because of error when starting the studio

---
+++ Dev/Dev2.Infrastructure.Tests/Logs/CustomTraceListenerTests.cs
@@ -0,0 +1,123 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using Dev2.Providers.Logs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Dev2.Infrastructure.Tests.Logs
+{
+    [TestClass]
+    public class CustomTraceListenerTests
+    {
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_Constructor_WithNullFileName_ShouldUseDefaultFileName()
+        {
+            //------------Setup for test--------------------------
+            new CustomTextWriter();
+            //------------Execute Test---------------------------
+            string loggingFileName = CustomTextWriter.LoggingFileName;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(loggingFileName, "Warewolf Studio.log");   
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_Constructor_WithEmptyFileName_ShouldUseDefaultFileName()
+        {
+            //------------Setup for test--------------------------
+            new CustomTextWriter();
+            //------------Execute Test---------------------------
+            string loggingFileName = CustomTextWriter.LoggingFileName;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(loggingFileName, "Warewolf Studio.log");
+
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_StaticLoggingFileNameAccessed_WithEmptyFileName_ShouldUseDefaultFileName()
+        {
+            //------------Setup for test--------------------------
+            //------------Execute Test---------------------------
+            string loggingFileName = CustomTextWriter.LoggingFileName;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(loggingFileName, "Warewolf Studio.log");
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_WarewolfAppPath_ShouldContainLocalAppDataPathAndWarewolf()
+        {
+            //------------Setup for test--------------------------
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //------------Execute Test---------------------------
+            var warewolfAppPath = CustomTextWriter.WarewolfAppPath;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(warewolfAppPath, localAppDataPath);
+            StringAssert.Contains(warewolfAppPath, "Warewolf");
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_StudioLogPath_ShouldContainLocalAppDataPathAndWarewolfAndStudioLogs()
+        {
+            //------------Setup for test--------------------------
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //------------Execute Test---------------------------
+            var warewolfAppPath = CustomTextWriter.StudioLogPath;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(warewolfAppPath, localAppDataPath);
+            StringAssert.Contains(warewolfAppPath, "Warewolf");
+            StringAssert.Contains(warewolfAppPath, "Studio Logs");
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_StaticLoggingFileNameAccessed_ShouldHaveFullPathAndDefaultFileName()
+        {
+            //------------Setup for test--------------------------
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //------------Execute Test---------------------------
+            string loggingFileName = CustomTextWriter.LoggingFileName;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(loggingFileName, localAppDataPath);
+            StringAssert.Contains(loggingFileName, "Warewolf");
+            StringAssert.Contains(loggingFileName, "Studio Logs");
+            StringAssert.Contains(loggingFileName, "Warewolf Studio.log");
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("CustomTraceListener")]
+        public void CustomTraceListener_StaticLoggingFileNameAccessed_WithFileName_ShouldHaveFullPathAndGivenFileName()
+        {
+            //------------Setup for test--------------------------
+            const string fileName = "Warewolf Studio.log";
+            new CustomTextWriter();
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            //------------Execute Test---------------------------
+            string loggingFileName = CustomTextWriter.LoggingFileName;
+            //------------Assert Results-------------------------
+            StringAssert.Contains(loggingFileName, localAppDataPath);
+            StringAssert.Contains(loggingFileName, "Warewolf");
+            StringAssert.Contains(loggingFileName, "Studio Logs");
+            StringAssert.Contains(loggingFileName, fileName); 
+        }
+    }
+}
---
+++ Dev/Dev2.Infrastructure/Providers/Logs/CustomTraceListener.cs
@@ -0,0 +1,82 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using Dev2.Common;
+
+
+namespace Dev2.Providers.Logs
+{
+    public class CustomTextWriter : TraceListener
+    {
+        public static string LoggingFileName => Path.Combine(StudioLogPath, "Warewolf Studio.log");
+
+        public static string WarewolfAppPath
+        {
+            get
+            {
+                string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                var warewolfAppPath = Path.Combine(appDataFolder, "Warewolf");
+                if(!Directory.Exists(warewolfAppPath))
+                {
+                    Directory.CreateDirectory(warewolfAppPath);
+                }
+                return warewolfAppPath;
+            }
+        }
+
+        public static string StudioLogPath
+        {
+            get
+            {
+                var studioLogPath = Path.Combine(WarewolfAppPath, "Studio Logs");
+                if(!Directory.Exists(studioLogPath))
+                {
+                    Directory.CreateDirectory(studioLogPath);
+                }
+                return studioLogPath;
+            }
+        }
+
+        public override void Write(string value)
+        {
+            try
+            {
+                Dev2Logger.Info(value, GlobalConstants.WarewolfInfo);
+            }
+            catch(ObjectDisposedException)
+            {
+                //ignore this exception
+            }
+        }
+
+        public override void WriteLine(string value)
+        {
+            try
+            {
+
+                Dev2Logger.Info(value, GlobalConstants.WarewolfInfo);
+
+            }
+            catch(ObjectDisposedException)
+            {
+                //ignore this exception
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+      
+        }
+    }
+}
