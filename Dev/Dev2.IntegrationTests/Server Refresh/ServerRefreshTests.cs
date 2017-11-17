8404c2cdc8bcf7bf8ee60ae9c88b521ceb1f40fd
DEV2\nkosinathi.sangweni <mnatiris@gmail.com>
2017-11-17 14:14:12 +0200

    WOLF-2846 WIP fixing tests

--- Dev/Dev2.IntegrationTests/Server Refresh/ServerRefreshTests.cs
+++ Dev/Dev2.IntegrationTests/Server Refresh/ServerRefreshTests.cs
@@ -77,7 +77,7 @@ namespace Dev2.Integration.Tests.Server_Refresh
             }
         }
 
-        private Task<string> ExececuteRequest(Uri url)
+        public Task<string> ExececuteRequest(Uri url)
         {
             try
             {
