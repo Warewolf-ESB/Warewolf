eda2b03860c52021b91b40f8b8616b2081365f45
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-22 10:21:01 +0200

    still adding timeout atttributes

--- Dev/Dev2.Activities.Tests/ActivityTests/DateTimeActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DateTimeActivityTests.cs
@@ -137,7 +137,7 @@ namespace Dev2.Tests.Activities.ActivityTests
         [TestMethod]
         [TestCategory("DateTimeUnitTest")]
         [Owner("Massimo Guerrera")]
-
+        [Timeout(60000)]
         public void DateTime_DateTimeUnitTest_ExecuteWithBlankInput_DateTimeNowIsUsed()
 
         {
@@ -164,7 +164,7 @@ namespace Dev2.Tests.Activities.ActivityTests
         [TestMethod]
         [TestCategory("DateTimeUnitTest")]
         [Owner("Massimo Guerrera")]
-
+        [Timeout(60000)]
         public void DateTime_DateTimeUnitTest_ExecuteWithBlankInputAndSplitSecondsOutput_OutputNotZero()
 
         {
