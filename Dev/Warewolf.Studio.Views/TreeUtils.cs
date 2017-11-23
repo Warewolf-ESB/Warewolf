b3894ed9f117448aa351135e3d19562bd8cecd68
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-23 15:29:00 +0200

    remove apparently unused method from TreeUtils.cs

--- Dev/Warewolf.Studio.Views/TreeUtils.cs
+++ Dev/Warewolf.Studio.Views/TreeUtils.cs
@@ -6,27 +6,6 @@ namespace Warewolf.Studio.Views
 {
     public static class TreeUtils
     {        
-
-        public static IEnumerable<XamDataTreeNode> Descendants(XamDataTreeNode root)
-        {
-            var nodes = new Stack<XamDataTreeNode>(new[] { root });
-            while (nodes.Any())
-            {
-                var node = nodes.Pop();
-                yield return node;
-                if(node != null)
-                {
-                    foreach (var n in node.Nodes)
-                    {
-                        if (n != null)
-                        {
-                            nodes.Push(n);
-                        }
-                    }
-                }
-            }
-        }
-        
         public static IEnumerable<XamDataTreeNode> Descendants(XamDataTreeNode[] roots)
         {
             var nodes = new Stack<XamDataTreeNode>(roots);
