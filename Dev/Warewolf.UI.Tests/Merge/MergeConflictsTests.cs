e9987eef406205bbe3e0ce6469980da60d52f2ce
Sanele <sanele.mthembu@dev2.co.za>
2017-11-23 08:09:15 +0200

    2811 : Introduces MergeConflictsTests

---
+++ Dev/Warewolf.UI.Tests/Merge/MergeConflictsTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warewolf.UI.Tests.Explorer.ExplorerUIMapClasses;
+using Warewolf.UI.Tests.Merge.MergeConflictsUIMapClasses;
+using Warewolf.UI.Tests.Merge.MergeDialogUIMapClasses;
+
+namespace Warewolf.UI.Tests.Merge
+{
+    [CodedUITest]
+    public partial class MergeConflictsTests
+    {
+        public const string MergeAllConflicting = "MergeAllConflicting";
+
+        [TestMethod]
+        [TestCategory("Merge")]
+        public void Select_All_Radio_Buttons_Enables_Save()
+        {
+            MergeConflictsUIMap.MainWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.MergeTab.WorkSurfaceContext.ContentDockManager.MergeWorkflowView.ScrollViewerPane.VariablesExpander.MergeButton.KeepCurrentCheckbox.Selected = true;
+            MergeConflictsUIMap.MainWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.MergeTab.WorkSurfaceContext.ContentDockManager.MergeWorkflowView.ScrollViewerPane.ConflictsTree.MergeTreeItem.MergeItemExpander.MergeButton.FirstAssign_Difference.Selected = true;
+            Assert.IsTrue(UIMap.MainStudioWindow.SideMenuBar.SaveButton.Enabled);
+        }      
+
+        #region Additional test attributes
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            UIMap.SetPlaybackSettings();
+            UIMap.AssertStudioIsRunning();
+            MergeConflictsUIMap.OpenMerge_For_Workflow(MergeAllConflicting);
+        }
+
+        public MergeConflictsUIMap MergeConflictsUIMap
+        {
+            get
+            {
+                if (_MergeConflictsUIMap == null)
+                {
+                    _MergeConflictsUIMap = new MergeConflictsUIMap();
+                }
+
+                return _MergeConflictsUIMap;
+            }
+        }
+
+        private MergeConflictsUIMap _MergeConflictsUIMap;
+
+        UIMap UIMap
+        {
+            get
+            {
+                if (_UIMap == null)
+                {
+                    _UIMap = new UIMap();
+                }
+
+                return _UIMap;
+            }
+        }
+
+        private UIMap _UIMap;
+
+        public ExplorerUIMap ExplorerUIMap
+        {
+            get
+            {
+                if (_ExplorerUIMap == null)
+                {
+                    _ExplorerUIMap = new ExplorerUIMap();
+                }
+
+                return _ExplorerUIMap;
+            }
+        }
+
+        private ExplorerUIMap _ExplorerUIMap;
+
+        public MergeDialogUIMap MergeDialogUIMap
+        {
+            get
+            {
+                if (_MergeDialogUIMap == null)
+                {
+                    _MergeDialogUIMap = new MergeDialogUIMap();
+                }
+
+                return _MergeDialogUIMap;
+            }
+        }
+
+        private MergeDialogUIMap _MergeDialogUIMap;
+
+        #endregion
+    }
+}
