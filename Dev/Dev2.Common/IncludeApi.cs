5371b7beef37ce554889f2ebf343a5691bd38f12
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-28 15:50:02 +0200

    IncludeApi.cs and MaintainerApi.cs apparently don't use the members they inherit from IEquatable

--- Dev/Dev2.Common/IncludeApi.cs
+++ Dev/Dev2.Common/IncludeApi.cs
@@ -8,60 +8,10 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
-
 namespace Dev2.Common
 {
-    public class IncludeApi : IEquatable<IncludeApi>
+    public class IncludeApi
     {
-        public bool Equals(IncludeApi other)
-        {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-            return string.Equals(Name, other.Name) && string.Equals(Url, other.Url);
-        }
-        
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-            if (obj.GetType() != GetType())
-            {
-                return false;
-            }
-            return Equals((IncludeApi)obj);
-        }
-        
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((Name?.GetHashCode() ?? 0) * 397) ^ (Url?.GetHashCode() ?? 0);
-            }
-        }
-
-        public static bool operator ==(IncludeApi left, IncludeApi right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(IncludeApi left, IncludeApi right)
-        {
-            return !Equals(left, right);
-        }
-
         public string Name { get; set; }
         public string Url { get; set; }
     }
