0c4960242bc4fded0dc32ff91e9861c27d049dd8
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-28 15:50:02 +0200

    IncludeApi.cs and MaintainerApi.cs apparently don't use the members they inherit from IEquatable

--- Dev/Dev2.Common/MaintainerApi.cs
+++ Dev/Dev2.Common/MaintainerApi.cs
@@ -12,66 +12,8 @@ using System;
 
 namespace Dev2.Common
 {
-    public class MaintainerApi : IEquatable<MaintainerApi>
+    public class MaintainerApi
     {
-        public bool Equals(MaintainerApi other)
-        {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-            return string.Equals(Fn, other.Fn) && string.Equals(Email, other.Email) && string.Equals(Url, other.Url) && string.Equals(Org, other.Org) && string.Equals(Adr, other.Adr) && string.Equals(Tel, other.Tel) && string.Equals(XTwitter, other.XTwitter) && string.Equals(XGithub, other.XGithub) && string.Equals(Photo, other.Photo) && string.Equals(VCard, other.VCard);
-        }
-        
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-            if (obj.GetType() != GetType())
-            {
-                return false;
-            }
-            return Equals((MaintainerApi)obj);
-        }
-        
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Fn?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (Email?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Url?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Org?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Adr?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Tel?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (XTwitter?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (XGithub?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Photo?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (VCard?.GetHashCode() ?? 0);
-                return hashCode;
-            }
-        }
-
-        public static bool operator ==(MaintainerApi left, MaintainerApi right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(MaintainerApi left, MaintainerApi right)
-        {
-            return !Equals(left, right);
-        }
-
         [Newtonsoft.Json.JsonProperty("FN")]
         public string Fn { get; set; }
 
