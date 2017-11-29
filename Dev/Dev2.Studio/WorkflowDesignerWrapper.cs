02a706cbb429b2d8c43f8350746d4c4877e37e3f
Nkosinathi Sangweni <mnatiris@gmail.com>
2017-11-29 11:37:56 +0200

    WOLF-2811 workfdesignervm tests

---
+++ Dev/Dev2.Studio/WorkflowDesignerWrapper.cs
@@ -0,0 +1,14 @@
+﻿using Dev2.Studio.Interfaces;
+using System.Activities.Presentation;
+
+namespace Dev2
+{
+    public class WorkflowDesignerWrapper : IWorkflowDesignerWrapper
+    {
+        public TServiceType GetService<TServiceType>(WorkflowDesigner wd)
+        {
+            var modelService=wd.Context.Services.GetService<TServiceType>();
+            return modelService;
+        }
+    }
+}
