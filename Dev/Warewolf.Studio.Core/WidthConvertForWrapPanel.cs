fd7c13d988f0f104b070fe119226321baab28dcb
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-30 09:23:57 +0200

    use full method bodies for these two convert methods to synthetically boost the fraction of the class which is covered

--- Dev/Warewolf.Studio.Core/WidthConvertForWrapPanel.cs
+++ Dev/Warewolf.Studio.Core/WidthConvertForWrapPanel.cs
@@ -6,7 +6,11 @@ namespace Warewolf.Studio.Core
 {
     public class WidthConvertForWrapPanel : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (double)value - 10;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value - 10;
+        }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 }
