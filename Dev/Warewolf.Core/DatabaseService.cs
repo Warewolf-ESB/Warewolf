6ad9384a1ac993d571c5609daaf606fc6d11857e
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-12-06 14:56:31 +0200

    remove IEquatable from this classes hierarchy because it is apparently unused

--- Dev/Warewolf.Core/DatabaseService.cs
+++ Dev/Warewolf.Core/DatabaseService.cs
@@ -15,7 +15,7 @@ using Dev2.Common.Interfaces.ServerProxyLayer;
 
 namespace Warewolf.Core
 {
-    public class DatabaseService : IDatabaseService, IEquatable<DatabaseService>
+    public class DatabaseService : IDatabaseService
     {
 
         #region Implementation of IDatabaseService
@@ -30,92 +30,13 @@ namespace Warewolf.Core
 
         #endregion
 
-        #region Equality members
-
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(DatabaseService other)
-        {
-            if (ReferenceEquals(null, other))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-            return Equals(Source, other.Source) && Equals(Action, other.Action);
-        }
-
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(IDatabaseService other)
-        {
-            return Equals(other as DatabaseService);
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
-        /// </summary>
-        /// <returns>
-        /// true if the specified object  is equal to the current object; otherwise, false.
-        /// </returns>
-        /// <param name="obj">The object to compare with the current object. </param>
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
             if (obj.GetType() != GetType())
             {
                 return false;
             }
             return Equals((DatabaseService)obj);
         }
-
-        /// <summary>
-        /// Serves as a hash function for a particular type. 
-        /// </summary>
-        /// <returns>
-        /// A hash code for the current <see cref="T:System.Object"/>.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Source?.GetHashCode() ?? 0;
-                hashCode = (hashCode * 397) ^ (Name?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Path?.GetHashCode() ?? 0);
-                hashCode = (hashCode * 397) ^ (Action?.GetHashCode() ?? 0);
-                return hashCode;
-            }
-        }
-
-        public static bool operator ==(DatabaseService left, DatabaseService right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(DatabaseService left, DatabaseService right)
-        {
-            return !Equals(left, right);
-        }
-
-        #endregion
     }
 }
