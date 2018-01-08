7faf794da6891aec518f1a0d2322199882046fb8
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-01-08 14:51:49 +0200

    WOLF-2915 Change disabled colour. Add Merge to version description

--- Dev/Dev2.Studio.Core.Tests/MergeWorkflowViewModelTests.cs
+++ Dev/Dev2.Studio.Core.Tests/MergeWorkflowViewModelTests.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Dev2.Studio.Interfaces;
-using System.Text;                      
+using System.Text;
 using System.Windows;
 using Dev2.Common.Interfaces.Studio.Controller;
 using Unlimited.Applications.BusinessDesignStudio.Activities;
 using System.Collections.Generic;
-using System;                  
+using System;
 using Warewolf.MergeParser;
 using Dev2.Activities;
 using Dev2.Communication;
@@ -1130,7 +1130,7 @@ namespace Dev2.Core.Tests
                 //---------------Execute Test ----------------------
                 mergeWorkflowViewModel.Save();
                 //---------------Test Result -----------------------
-                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object));
+                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object, It.IsAny<string>()));
             }
         }
 
@@ -1182,7 +1182,7 @@ namespace Dev2.Core.Tests
                 mergeWorkflowViewModel.Save();
                 //---------------Test Result -----------------------
                 currentResourceModel.Verify(p => p.Environment.ExplorerRepository.UpdateManagerProxy.Rename(currentResourceModel.Object.ID, currentResourceModel.Object.ResourceName));
-                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object));
+                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object, It.IsAny<string>()));
             }
         }
 
@@ -1243,7 +1243,7 @@ namespace Dev2.Core.Tests
                 currentFactory.Verify(p => p.DataListViewModel.WriteToResourceModel());
                 currentFactory.Verify(p => p.IsVariablesChecked);
                 currentResourceModel.VerifySet(q => q.DataList = It.IsAny<string>());
-                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object));
+                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object, It.IsAny<string>()));
             }
         }
 
@@ -1303,7 +1303,7 @@ namespace Dev2.Core.Tests
                 diffFactory.Verify(p => p.DataListViewModel.WriteToResourceModel());
                 currentFactory.Verify(p => p.IsVariablesChecked);
                 currentResourceModel.VerifySet(q => q.DataList = It.IsAny<string>());
-                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object));
+                currentResourceModel.Verify(p => p.Environment.ResourceRepository.SaveToServer(currentResourceModel.Object, It.IsAny<string>()));
             }
         }
 
@@ -1582,7 +1582,7 @@ namespace Dev2.Core.Tests
                 Assert.IsTrue(wasCalled);
             }
         }
-        
+
         [TestMethod]
         public void DataListViewModel_Test_propertyChanges()
         {
