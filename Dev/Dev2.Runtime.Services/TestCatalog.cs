6e576af80764d88bb31c7b247a5c1296e602cff5
Sanele <sanele.mthembu@dev2.co.za>
2018-01-16 10:24:53 +0200

    2833 : Sonar clean up

--- Dev/Dev2.Runtime.Services/TestCatalog.cs
+++ Dev/Dev2.Runtime.Services/TestCatalog.cs
@@ -74,7 +74,7 @@ namespace Dev2.Runtime
             }
         }
 
-        void UpdateTestToInvalid(List<IServiceTestModelTO> testsToUpdate)
+        static void UpdateTestToInvalid(List<IServiceTestModelTO> testsToUpdate)
         {
             foreach (var serviceTestModelTO in testsToUpdate)
             {
@@ -118,7 +118,7 @@ namespace Dev2.Runtime
             }
         }
 
-        void UpdateStepOutputsForTest(IServiceTestModelTO serviceTestModelTo)
+        static void UpdateStepOutputsForTest(IServiceTestModelTO serviceTestModelTo)
         {
             if (serviceTestModelTo.TestSteps != null)
             {
@@ -152,7 +152,7 @@ namespace Dev2.Runtime
             Load();
         }
 
-        void UpdateOutputsForTest(IServiceTestModelTO serviceTestModelTO, IList<IDev2Definition> outputDefs)
+        static void UpdateOutputsForTest(IServiceTestModelTO serviceTestModelTO, IList<IDev2Definition> outputDefs)
         {
             if (outputDefs.Count == 0)
             {
@@ -241,7 +241,7 @@ namespace Dev2.Runtime
             }
         }
 
-        void UpdateInputsForTest(IServiceTestModelTO serviceTestModelTO, IList<IDev2Definition> inputDefs)
+        static void UpdateInputsForTest(IServiceTestModelTO serviceTestModelTO, IList<IDev2Definition> inputDefs)
         {
             if (inputDefs.Count == 0)
             {
