4512aa601d04e0d3a1f0dbd10b76cea996ab7221
Sanele <sanele.mthembu@dev2.co.za>
2018-01-17 15:55:58 +0200

    2932 : DataListUtilTests for GetVariableNameToMapOutputTo And Sonar issues Clean up

--- Dev/Dev2.Activities/Activities/ResponseManager.cs
+++ Dev/Dev2.Activities/Activities/ResponseManager.cs
@@ -112,7 +112,7 @@ namespace Dev2.Activities
                 var outputDefs =
                     Outputs.Select(
                         a =>
-                            new Dev2Definition(DataListUtil.VariableNameToMapTo(a.MappedTo), a.MappedTo, "", a.RecordSetName, true, "", true,
+                            new Dev2Definition(DataListUtil.GetVariableNameToMapOutputTo(a.MappedTo), a.MappedTo, "", a.RecordSetName, true, "", true,
                                 a.MappedTo) as IDev2Definition).ToList();
                 TryConvert(children, outputDefs, indexCache, update, dataObj);
             }
