2601a2d6680d6bdceac0ca5721e88f3226d01b92
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-01-24 13:08:28 +0200

    typos

--- Compile.ps1
+++ Compile.ps1
@@ -276,7 +276,6 @@ Catch {
 }
 if (!$TagExists) {
     docker build -t warewolfserver/warewolfserver:$GitCommitID "$PSScriptRoot\Dev\Dev2.Server\bin\Debug"
-    docker tag warewolfserver warewolfserver/warewolfserver:$GitCommitID
     docker push warewolfserver/warewolfserver
 } else {
     Write-Host Already tagged. See: https://index.docker.io/v1/repositories/warewolfserver/warewolfserver/tags/$GitCommitID
