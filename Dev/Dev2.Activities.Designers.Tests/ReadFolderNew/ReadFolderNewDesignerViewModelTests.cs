8d526dc4fb4ffb238321ea02e434d5c766bc9586
Sanele <sanele.mthembu@dev2.co.za>
2018-01-25 14:22:25 +0200

    2933: ReadFolderNew viewmodel tests

---
+++ Dev/Dev2.Activities.Designers.Tests/ReadFolderNew/ReadFolderNewDesignerViewModelTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dev2.Common.Interfaces.Help;
+using Dev2.Studio.Core.Activities.Utils;
+using Dev2.Studio.Interfaces;
+using Moq;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+
+namespace Dev2.Activities.Designers.Tests.ReadFolderNew
+{
+    [TestClass]
+    public class ReadFolderNewDesignerViewModelTests
+    {
+        [TestMethod]
+        [Owner("Trevor Williams-Ros")]
+        [TestCategory("ReadFolderDesignerViewModel_Constructor")]
+        public void ReadFolderDesignerViewModel_Constructor_Properties_Initialized()
+        {
+            //------------Setup for test-------------------------
+
+            //------------Execute Test---------------------------
+            var viewModel = ReadFolderNewViewModel();
+
+            //------------Assert Results-------------------------
+            Assert.AreEqual("Directory", viewModel.InputPathLabel);
+            Assert.AreEqual("", viewModel.OutputPathLabel);
+            Assert.IsNull(viewModel.InputPathValue);
+            Assert.IsNull(viewModel.OutputPathValue);
+            Assert.IsNull(viewModel.Errors);
+            Assert.AreEqual(0, viewModel.TitleBarToggles.Count);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("ReadFolderDesignerViewModel_Handle")]
+        public void ReadFolderDesignerViewModel_UpdateHelp_ShouldCallToHelpViewMode()
+        {
+            //------------Setup for test--------------------------      
+            var mockMainViewModel = new Mock<IShellViewModel>();
+            var mockHelpViewModel = new Mock<IHelpWindowViewModel>();
+            mockHelpViewModel.Setup(model => model.UpdateHelpText(It.IsAny<string>())).Verifiable();
+            mockMainViewModel.Setup(model => model.HelpViewModel).Returns(mockHelpViewModel.Object);
+            CustomContainer.Register(mockMainViewModel.Object);
+            var viewModel = ReadFolderNewViewModel();
+            //------------Execute Test---------------------------
+            viewModel.UpdateHelpDescriptor("help");
+            //------------Assert Results-------------------------
+            mockHelpViewModel.Verify(model => model.UpdateHelpText(It.IsAny<string>()), Times.Once());
+        }
+
+        [TestMethod]
+        [Owner("Trevor Williams-Ros")]
+        [TestCategory("ReadFolderDesignerViewModel_Validate")]
+        public void ReadFolderDesignerViewModel_Validate_CorrectFieldsAreValidated()
+        {
+            //------------Setup for test-------------------------
+            var viewModel = ReadFolderNewViewModel();
+
+            //------------Execute Test---------------------------
+            viewModel.Validate();
+
+            //------------Assert Results-------------------------
+            Assert.AreEqual(0, viewModel.ValidateInputAndOutputPathHitCount);
+            Assert.AreEqual(1, viewModel.ValidateInputPathHitCount);
+            Assert.AreEqual(0, viewModel.ValidateOutputPathHitCount);
+            Assert.AreEqual(1, viewModel.ValidateUserNameAndPasswordHitCount);
+        }
+
+        [TestMethod]
+        [Owner("Tshepo Ntlhokoa")]
+        [TestCategory("ReadFolderDesignerViewModel_Constructor")]
+        public void ReadFolderDesignerViewModel_Constructor_IsFilesSelectedIsSetToTrue()
+        {
+            //------------Setup for test-------------------------
+            var viewModel = ReadFolderNewViewModel();
+            //------------Assert Results-------------------------
+            Assert.IsTrue(viewModel.IsFilesSelected);
+        }
+
+
+        static TestReadFolderNewDesignerViewModel ReadFolderNewViewModel()
+        {
+            var viewModel = new TestReadFolderNewDesignerViewModel(ModelItemUtils.CreateModelItem(new DsfFolderRead()));
+            return viewModel;
+        }
+    }
+}
