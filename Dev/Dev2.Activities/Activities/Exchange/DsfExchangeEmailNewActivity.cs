4061ab283a964dc1c217f8623dac89d8bf1ebc9d
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-01-29 20:30:07 +0200

    fix violations now considered errors

--- Dev/Dev2.Activities/Activities/Exchange/DsfExchangeEmailNewActivity.cs
+++ Dev/Dev2.Activities/Activities/Exchange/DsfExchangeEmailNewActivity.cs
@@ -315,8 +315,16 @@ namespace Dev2.Activities.Exchange
 
         public bool Equals(DsfExchangeEmailNewActivity other)
         {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
             var isSourceEqual = CommonEqualityOps.AreObjectsEqual(SavedSource, other.SavedSource);
             return base.Equals(other)
                 && isSourceEqual
@@ -333,9 +341,21 @@ namespace Dev2.Activities.Exchange
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
             return Equals((DsfExchangeEmailNewActivity)obj);
         }
 
