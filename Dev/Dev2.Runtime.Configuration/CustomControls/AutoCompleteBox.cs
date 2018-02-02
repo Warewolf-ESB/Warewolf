fc6934aa349fdcbf8b8a6217d1fc9138386da6fa
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-02-02 16:31:53 +0200

    violations of S1117 and S108 now break the compile

--- Dev/Dev2.Runtime.Configuration/CustomControls/AutoCompleteBox.cs
+++ Dev/Dev2.Runtime.Configuration/CustomControls/AutoCompleteBox.cs
@@ -900,11 +900,7 @@ namespace System.Windows.Controls
 
             if (_popupHasOpened)
             {
-#if SILVERLIGHT
-                OnDropDownClosed(new RoutedPropertyChangedEventArgs<bool>(true, false));
-#else
                 OnDropDownClosed(new RoutedPropertyChangedEventArgs<bool>(true, false, DropDownClosedEvent));
-#endif
             }
         }
 
@@ -914,11 +910,7 @@ namespace System.Windows.Controls
 
         void FocusChanged(bool hasFocus)
         {
-            if (hasFocus)
-            {
-
-            }
-            else
+            if (!hasFocus)
             {
                 IsDropDownOpen = false;
                 _userCalledPopulate = false;
@@ -928,12 +920,7 @@ namespace System.Windows.Controls
 
         protected bool HasFocus()
         {
-            var focused =
-#if SILVERLIGHT
-                FocusManager.GetFocusedElement() as DependencyObject;
-#else
-                IsKeyboardFocusWithin ? Keyboard.FocusedElement as DependencyObject : FocusManager.GetFocusedElement(this) as DependencyObject;
-#endif
+            var focused = IsKeyboardFocusWithin ? Keyboard.FocusedElement as DependencyObject : FocusManager.GetFocusedElement(this) as DependencyObject;
             while (focused != null)
             {
                 if(ReferenceEquals(focused, this))
