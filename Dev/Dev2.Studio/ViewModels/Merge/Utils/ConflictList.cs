4271e3a39c3b045736c81fd49090213bb03f470a
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-02-06 10:52:39 +0200

    WOLF-2954 refactor ConflictList out of MergeWorkflowViewModel, and checkbox change

---
+++ Dev/Dev2.Studio/ViewModels/Merge/Utils/ConflictList.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dev2.Common.Interfaces;
+using System.Collections;
+
+namespace Dev2.ViewModels.Merge.Utils
+{
+    class ConflictList : IEnumerable<IConflict>
+    {
+        private List<IConflict> conflicts;
+        public List<IConflict> Conflicts
+        {
+            get { return conflicts; }
+            set { conflicts = value; }
+        }
+
+        public IEnumerator<IConflict> GetEnumerator() => conflicts.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => conflicts.GetEnumerator();
+
+        public int Count => conflicts.Count;
+        public int IndexOf(IConflict conflict) => conflicts.IndexOf(conflict);
+
+
+        public IConflict GetNextConflict(IConflict conflict, List<IConflict> _seenConflicts)
+        {
+            var idx = conflicts.IndexOf(conflict);
+            var nextConflict = MoveNext(idx);
+            while (_seenConflicts.Contains(nextConflict) && nextConflict != null)
+            {
+                idx = idx + 1;
+                nextConflict = MoveNext(idx);
+            }
+            _seenConflicts.Add(nextConflict);
+            return nextConflict;
+        }
+
+        public IConflict MoveNext(int index)
+        {
+            var nextIndex = index + 1;
+            if (nextIndex >= conflicts.Count)
+            {
+                return null;
+            }
+            var nextConflict = conflicts[nextIndex];
+            return nextConflict;
+        }
+
+        public IConflict GetNextConlictToUpdate(IConflict container)
+        {
+            var index = conflicts.IndexOf(container) + 1;
+            if (index < conflicts.Count)
+            {
+                var nextConflict = conflicts.ElementAt(index);
+                return nextConflict;
+            }
+            return null;
+        }
+    }
+}
