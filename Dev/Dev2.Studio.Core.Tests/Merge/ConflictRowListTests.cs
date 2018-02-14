3457ff88ac21dc4cb176f11f78376abc48730eb7
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-02-14 16:03:00 +0200

    WOLF-2954 WIP: Tests

--- Dev/Dev2.Studio.Core.Tests/Merge/ConflictRowListTests.cs
+++ Dev/Dev2.Studio.Core.Tests/Merge/ConflictRowListTests.cs
@@ -1,15 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Dev2.Common;
+using Dev2.Core.Tests.Merge.Utils;
+using Dev2.Studio.Interfaces;
+using Dev2.ViewModels.Merge;
+using Dev2.ViewModels.Merge.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Dev2.Core.Tests.Merge
 {
     [TestClass]
-    public class ConflictRowListTests
+    public class ConflictRowListTests : MergeTestUtils
     {
         [TestMethod]
         public void ConflictRowList_Constructor()
         {
-            
+            var modelFactoryCurrent = CreateCurrentConflictModelFactory();
+            var modelFactoryDifferent = CreateDiffConflictModelFactory();
+
+            var conflictNodeCurrent = CreateConflictTreeNode(modelFactoryCurrent.Model);
+            var conflictNodeDiff = CreateConflictTreeNode(modelFactoryDifferent.Model);
+
+            var toolConlfictItem = ToolConflictItem.NewFromActivity(multiAssign, modelItem, location);
+
+            var currentTree = new List<ConflictTreeNode> { conflictNodeCurrent };
+            var diffTree = new List<ConflictTreeNode> { conflictNodeDiff };
+
+            var conflictRowList = new ConflictRowList(modelFactoryCurrent, modelFactoryDifferent, currentTree, diffTree);
+
+            Assert.AreEqual(1, conflictRowList.Count);
 
         }
     }
