1006e1975a4e795fb764708da285c62b0db86f6c
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-02-14 21:19:13 +0200

    WOLF-2954 WIP: Tests fixes and removed

--- Dev/Dev2.Studio.Core.Tests/Merge/ConflictRowListTests.cs
+++ Dev/Dev2.Studio.Core.Tests/Merge/ConflictRowListTests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Windows;
+using Dev2.Activities;
 using Dev2.Common;
 using Dev2.Core.Tests.Merge.Utils;
+using Dev2.Studio.Core.Interfaces;
 using Dev2.Studio.Interfaces;
 using Dev2.ViewModels.Merge;
 using Dev2.ViewModels.Merge.Utils;
@@ -18,18 +21,29 @@ namespace Dev2.Core.Tests.Merge
             var modelFactoryCurrent = CreateCurrentConflictModelFactory();
             var modelFactoryDifferent = CreateDiffConflictModelFactory();
 
-            var conflictNodeCurrent = CreateConflictTreeNode(modelFactoryCurrent.Model);
-            var conflictNodeDiff = CreateConflictTreeNode(modelFactoryDifferent.Model);
-
-            var toolConlfictItem = ToolConflictItem.NewFromActivity(multiAssign, modelItem, location);
-
-            var currentTree = new List<ConflictTreeNode> { conflictNodeCurrent };
-            var diffTree = new List<ConflictTreeNode> { conflictNodeDiff };
+            var (currentTree, diffTree) = CreateConflictTreeNodeList();
 
             var conflictRowList = new ConflictRowList(modelFactoryCurrent, modelFactoryDifferent, currentTree, diffTree);
 
-            Assert.AreEqual(1, conflictRowList.Count);
+            Assert.AreEqual(2, conflictRowList.Count);
 
+            Assert.IsNotNull(conflictRowList[0].Current);
+            Assert.IsNotNull(conflictRowList[0].Different);
+            Assert.IsNotNull(conflictRowList[0].UniqueId);
+            Assert.IsNotNull(conflictRowList[0].Connectors);
+            Assert.IsTrue(conflictRowList[0].HasConflict);
+            Assert.IsTrue(conflictRowList[0].IsMergeVisible);
+            Assert.IsFalse(conflictRowList[0].IsChecked);
+            Assert.IsFalse(conflictRowList[0].ContainsStart);
+
+            Assert.IsNotNull(conflictRowList[1].Current);
+            Assert.IsNotNull(conflictRowList[1].Different);
+            Assert.IsNotNull(conflictRowList[1].UniqueId);
+            Assert.IsNotNull(conflictRowList[1].Connectors);
+            Assert.IsTrue(conflictRowList[1].HasConflict);
+            Assert.IsTrue(conflictRowList[1].IsMergeVisible);
+            Assert.IsFalse(conflictRowList[1].IsChecked);
+            Assert.IsFalse(conflictRowList[1].ContainsStart);
         }
     }
 }
