92618d123f3ca60ac4e57ad6715759761a0513e8
Rory McGuire <rory.mcguire@dev2.co.za>
2018-02-23 14:22:54 +0200

    WOLF-2930 enable disable connectors

--- Dev/Dev2.Studio/ViewModels/Merge/Utils/ConflictListStateApplier.cs
+++ Dev/Dev2.Studio/ViewModels/Merge/Utils/ConflictListStateApplier.cs
@@ -8,6 +8,8 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using System;
+using System.Collections.Generic;
 using Dev2.Common.Interfaces;
 
 namespace Dev2.ViewModels.Merge.Utils
@@ -23,7 +25,51 @@ namespace Dev2.ViewModels.Merge.Utils
         public ConflictListStateApplier(ConflictRowList conflicts)
         {
             this._conflictRowList = conflicts;
+            RegisterToolEventListeners();
         }
+
+        private void RegisterToolEventListeners()
+        {
+            foreach (var row in _conflictRowList)
+            {
+                if (row is IToolConflictRow toolRow)
+                {
+                    if (!(toolRow.CurrentViewModel is ToolConflictItem.Empty))
+                    {
+                        toolRow.CurrentViewModel.NotifyIsCheckedChanged += NotifyIsCheckedChangedHandler;
+                    }
+                    if (!(toolRow.DiffViewModel is ToolConflictItem.Empty))
+                    {
+                        toolRow.DiffViewModel.NotifyIsCheckedChanged += NotifyIsCheckedChangedHandler;
+                    }
+                }
+            }
+        }
+
+        private static void NotifyIsCheckedChangedHandler(IConflictItem conflictItem, bool arg2)
+        {
+            if (conflictItem is IToolConflictItem toolConflictItem)
+            {
+                var connectors = new List<IConnectorConflictItem>();
+                connectors.AddRange(toolConflictItem.InboundConnectors);
+                connectors.AddRange(toolConflictItem.OutboundConnectors);
+                foreach (var connector in connectors)
+                {
+                    var sourceItem = connector.SourceConflictItem();
+                    var destinationItem = connector.DestinationConflictItem();
+
+                    var allow = sourceItem != null && sourceItem.IsInWorkflow
+                            && destinationItem != null && destinationItem.IsInWorkflow;
+
+                    connector.AllowSelection = allow;
+                    if (!connector.AllowSelection)
+                    {
+                        connector.IsChecked = false;
+                    }
+                }
+            }
+        }
+
         public void SetConnectorSelectionsToCurrentState()
         {
             foreach (var row in _conflictRowList)
