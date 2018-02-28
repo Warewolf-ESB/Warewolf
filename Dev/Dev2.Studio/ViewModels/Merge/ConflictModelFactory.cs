7914db42b5bc9be481ec94d1d3962c41eed948ed
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2018-02-28 16:50:00 +0200

    resolved some tests

--- Dev/Dev2.Studio/ViewModels/Merge/ConflictModelFactory.cs
+++ Dev/Dev2.Studio/ViewModels/Merge/ConflictModelFactory.cs
@@ -46,7 +46,7 @@ namespace Dev2.ViewModels.Merge
         public ConflictModelFactory(IToolConflictItem toolConflictItem, IContextualResourceModel resourceModel, IConflictTreeNode conflict)
         {
             _resourceModel = resourceModel;
-            var modelItem = ModelItemUtils.CreateModelItem(conflict.Activity);
+            CreateModelItem(toolConflictItem,conflict);
         }
 
         public ConflictModelFactory()
