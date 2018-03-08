28379e2567fabd8793b8bf79b45c15095ebd42ff
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-03-08 08:31:56 +0200

    Revert "rollback SchedulerSteps.cs"
    This reverts commit dea3cfd86731467c765f15e6ba18619ce9231053.

--- Dev/Dev2.Activities.Specs/Scheduler/SchedulerSteps.cs
+++ Dev/Dev2.Activities.Specs/Scheduler/SchedulerSteps.cs
@@ -129,15 +129,14 @@ namespace Dev2.Activities.Specs.Scheduler
                 if (scheduler.HasErrors)
                 {
                     _scenarioContext["Error"] = scheduler.Error;
+                    Console.WriteLine("Error creating schedule: " + scheduler.Error);
                 }
             }
             catch (Exception e)
             {
-
                 _scenarioContext["Error"] = e.Message;
+                Console.WriteLine("Error creating schedule: " + e.Message);
             }
-
-
         }
 
         void UpdateTrigger(IScheduledResource task, Table table)
@@ -239,7 +238,7 @@ namespace Dev2.Activities.Specs.Scheduler
             var resources = _scenarioContext["History"] as IList<IResourceHistory>;
 
             var debug = resources.First().DebugOutput;
-
+            
             var debugTocompare = debug.Last();
             _commonSteps.ThenTheDebugOutputAs(table, debugTocompare.Outputs.SelectMany(s => s.ResultsList).ToList(), true);
         }
@@ -277,16 +276,14 @@ namespace Dev2.Activities.Specs.Scheduler
         {
             try
             {
-
                 var i = 0;
                 var x = new TaskService();
                 x.GetFolder("Warewolf");
-                var task = x.FindTask(scheduleName);
+                var task = x.FindTask(_scenarioContext["ScheduleName"].ToString());
+                Assert.IsNotNull(task, "Task " + scheduleName + " not found in Warewolf folder");
                 do
                 {
                     task.Run();
-
-
                     const int TimeOut = 10;
                     var time = 0;
                     while (task.State == TaskState.Running && time < TimeOut)
@@ -296,15 +293,12 @@ namespace Dev2.Activities.Specs.Scheduler
                     }
                     i++;
 
-
                 } while (i < times);
             }
             catch (Exception e)
             {
-
                 _scenarioContext["Error"] = e;
             }
-
         }
 
         public static bool AccountExists(string name)
@@ -315,9 +309,9 @@ namespace Dev2.Activities.Specs.Scheduler
                 var id = GetUserSecurityIdentifier(name);
                 accountExists = id.IsAccountSid();
             }
-
+            
             catch (Exception)
-
+            
             {
                 /* Invalid user account */
             }
@@ -367,8 +361,11 @@ namespace Dev2.Activities.Specs.Scheduler
         [AfterScenario("@Scheduler")]
         public static void CleanupAfterTestScheduler()
         {
-            var vm = _scenarioContext["Scheduler"] as SchedulerViewModel;
-            vm?.DeleteCommand.Execute(vm.SelectedTask);
+            if (_scenarioContext.ContainsKey("Scheduler"))
+            {
+                var vm = _scenarioContext["Scheduler"] as SchedulerViewModel;
+                vm?.DeleteCommand.Execute(vm.SelectedTask);
+            }
         }
     }
 }
