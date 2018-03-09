c03c1bf884bde082a7ace203a01e6a7986c5e97f
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-03-09 10:49:17 +0200

    Revert "dont fail build on warnings in tools specs project"
    This reverts commit 46ee9058f15e220d4afd986186d8d2c6c078d3f7.

---
+++ Dev/Warewolf.Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Warewolf.Launcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Warewolf.Launcher")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b669d51-14ce-44a3-9407-8251ba3bad19")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
