6137ed2933cf4499419e16260dacd8ac49ca0ae6
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-03-09 16:31:32 +0200

    still trying to fix compile break

--- Dev/Dev2.IntegrationTests/Load Tests.cs
+++ Dev/Dev2.IntegrationTests/Load Tests.cs
@@ -42,7 +42,7 @@ namespace Dev2.Integration.Tests
         [TestCategory("Load Tests")]
         public void Single_Token_Perfomance_Op()
         {
-            var dtb = new Dev2TokenizerBuilder { ToTokenize = Properties.TestStrings.tokenizerBase.ToStringBuilder() };
+            var dtb = new Dev2TokenizerBuilder { ToTokenize = Properties.TestStrings.tokenizerBase };
 
 
             dtb.AddTokenOp("-", false);
@@ -69,7 +69,7 @@ namespace Dev2.Integration.Tests
         [TestCategory("Load Tests")]
         public void Three_Token_Perfomance_Op()
         {
-            var dtb = new Dev2TokenizerBuilder { ToTokenize = Properties.TestStrings.tokenizerBase.ToStringBuilder() };
+            var dtb = new Dev2TokenizerBuilder { ToTokenize = Properties.TestStrings.tokenizerBase };
 
 
             dtb.AddTokenOp("AB-", false);
