478780687faf6ed511e9f7ee81730cd1efe5b05d
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-03-22 17:13:00 +0200

    WOLF-165 Resolve Code Review feedback

--- Dev/Dev2.Runtime.Services/Search/ActivitySearcher.cs
+++ Dev/Dev2.Runtime.Services/Search/ActivitySearcher.cs
@@ -22,35 +22,38 @@ namespace Dev2.Runtime.Search
             var searchResults = new List<ISearchResult>();
             if (searchParameters.SearchOptions.IsToolTitleSelected)
             {
-                foreach (var resourceActivity in _resourceCatalog.GetResourceActivityCache(GlobalConstants.ServerWorkspaceID).Cache)
+                var activityCache = _resourceCatalog.GetResourceActivityCache(GlobalConstants.ServerWorkspaceID);
+                if (activityCache != null)
                 {
-                    var activity = resourceActivity.Value;
-                    var allNodes = new List<IDev2Activity> { activity };
-                    var seenNodes = new List<string>();
-                    while (allNodes != null)
+                    foreach (var resourceActivity in activityCache.Cache)
                     {
-                        var nextNodes = new List<IDev2Activity>();
-                        foreach (var next in allNodes)
-                        {
-                            if (seenNodes.Contains(next.UniqueID))
-                            {
-                                continue;
-                            }
-                            PerformSearchOnActivity(searchParameters, searchResults, resourceActivity, next);
-                            seenNodes.Add(next.UniqueID);
-                            nextNodes.AddRange(next.GetNextNodes().DistinctBy(t => t.UniqueID));
-                            nextNodes.AddRange(next.GetChildrenNodes().DistinctBy(t => t.UniqueID));
-                        }
-                        if (nextNodes.Count != 0)
-                        {
-                            allNodes = nextNodes;
-                        }
-                        else
-                        {
-                            allNodes = null;
-                        }
+                        searchResults.AddRange(ProcessActivitiesForResource(searchParameters, resourceActivity));
                     }
                 }
+            }
+            return searchResults;
+        }
+
+        private List<ISearchResult> ProcessActivitiesForResource(ISearch searchParameters, KeyValuePair<Guid, IDev2Activity> resourceActivity)
+        {
+            var allNodes = new List<IDev2Activity> { resourceActivity.Value };
+            var seenNodes = new List<string>();
+            var searchResults = new List<ISearchResult>();
+            while (allNodes != null)
+            {
+                var nextNodes = new List<IDev2Activity>();
+                foreach (var next in allNodes)
+                {
+                    if (seenNodes.Contains(next.UniqueID))
+                    {
+                        continue;
+                    }
+                    PerformSearchOnActivity(searchParameters, searchResults, resourceActivity, next);
+                    seenNodes.Add(next.UniqueID);
+                    nextNodes.AddRange(next.GetNextNodes().DistinctBy(t => t.UniqueID));
+                    nextNodes.AddRange(next.GetChildrenNodes().DistinctBy(t => t.UniqueID));
+                }
+                allNodes = nextNodes.Count != 0 ? nextNodes : null;
             }
             return searchResults;
         }
