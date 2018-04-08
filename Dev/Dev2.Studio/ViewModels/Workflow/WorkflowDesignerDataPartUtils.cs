7d0975385c9c0e74728c163ed5f93f7b5ef307a7
SalamiArmy <ultrakiff@gmail.com>
2018-04-08 09:16:06 +0200

    DO-366 studio control flow nesting refactor

--- Dev/Dev2.Studio/ViewModels/Workflow/WorkflowDesignerDataPartUtils.cs
+++ Dev/Dev2.Studio/ViewModels/Workflow/WorkflowDesignerDataPartUtils.cs
@@ -52,77 +52,104 @@ namespace Dev2.ViewModels.Workflow
             }
             else
             {
-                var fieldList = dataPartFieldData.Split('.');
-                string fullyFormattedStringValue;
-                if (fieldList.Length > 1 && !String.IsNullOrEmpty(fieldList[0]))
+                AddDataUniquePart(dataPartFieldData, dataLanguageParser, unique);
+            }
+        }
+
+        static void AddDataUniquePart(string dataPartFieldData, Dev2DataLanguageParser dataLanguageParser, Dictionary<IDataListVerifyPart, string> unique)
+        {
+            IDataListVerifyPart verifyPart = null;
+            var fieldList = dataPartFieldData.Split('.');
+            string fullyFormattedStringValue = "";
+            if (fieldList.Length > 1 && !String.IsNullOrEmpty(fieldList[0]))
+            {
+                AddRecsetContainingAField(dataLanguageParser, unique, ref verifyPart, fieldList, ref fullyFormattedStringValue);
+            }
+            else
+            {
+                if (fieldList.Length == 1 && !String.IsNullOrEmpty(fieldList[0]))
                 {
-                    // If it's a RecordSet Containing a field
-                    var recAdded = false;
+                    AddScalarOrRecset(dataPartFieldData, dataLanguageParser, unique, ref verifyPart, fieldList, ref fullyFormattedStringValue);
+                }
+            }
+        }
 
-                    foreach (var item in fieldList)
+        private static void AddScalarOrRecset(string dataPartFieldData, Dev2DataLanguageParser dataLanguageParser, Dictionary<IDataListVerifyPart, string> unique, ref IDataListVerifyPart verifyPart, string[] fieldList, ref string fullyFormattedStringValue)
+        {
+            // If the workflow field is simply a scalar or a record set without a child
+            if (dataPartFieldData.EndsWith(")") && dataPartFieldData == fieldList[0])
+            {
+                if (dataPartFieldData.Contains("("))
+                {
+                    fullyFormattedStringValue = RemoveRecordSetBrace(fieldList[0]);
+                    var intellisenseResult = dataLanguageParser.ValidateName(fullyFormattedStringValue, "");
+                    if (intellisenseResult == null)
                     {
-                        if (item.EndsWith(")") && item == fieldList[0])
-                        {
-                            if (item.Contains("("))
-                            {
-                                fullyFormattedStringValue = RemoveRecordSetBrace(item);
-                                var intellisenseResult = dataLanguageParser.ValidateName(fullyFormattedStringValue, "");
-                                if (intellisenseResult == null)
-                                {
-                                    recAdded = true;
-                                    verifyPart =
-                                         IntellisenseFactory.CreateDataListValidationRecordsetPart(fullyFormattedStringValue,
-                                             String.Empty);
-                                    AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
-                                }
-
-                            }
-                        }
-                        else if (item == fieldList[1] && !(item.EndsWith(")") && item.Contains(")")))
-                        {
-                            // If it's a field to a record set
-                            var intellisenseResult = dataLanguageParser.ValidateName(item, "");
-                            if (intellisenseResult == null && recAdded)
-                            {
-                                verifyPart = IntellisenseFactory.CreateDataListValidationRecordsetPart(
-                                        RemoveRecordSetBrace(fieldList.ElementAt(0)), item);
-                                AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        verifyPart = IntellisenseFactory.CreateDataListValidationRecordsetPart(fullyFormattedStringValue, String.Empty);
+                        AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
                     }
+                }
+            }
+            else
+            {
+                var intellisenseResult = dataLanguageParser.ValidateName(dataPartFieldData, "");
+                if (intellisenseResult == null)
+                {
+                    verifyPart = IntellisenseFactory.CreateDataListValidationScalarPart(RemoveRecordSetBrace(dataPartFieldData));
+                    AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
+                }
+            }
+        }
+
+        static void AddRecsetContainingAField(Dev2DataLanguageParser dataLanguageParser, Dictionary<IDataListVerifyPart, string> unique, ref IDataListVerifyPart verifyPart, string[] fieldList, ref string fullyFormattedStringValue)
+        {
+            // If it's a RecordSet Containing a field
+            var recAdded = false;
+
+            foreach (var item in fieldList)
+            {
+                if (item.EndsWith(")") && item == fieldList[0])
+                {
+                    AddField(dataLanguageParser, unique, ref verifyPart, ref fullyFormattedStringValue, ref recAdded, item);
+                }
+                else if (item == fieldList[1] && !(item.EndsWith(")") && item.Contains(")")))
+                {
+                    verifyPart = AddField(dataLanguageParser, unique, verifyPart, fieldList, recAdded, item);
                 }
                 else
                 {
-                    if (fieldList.Length == 1 && !String.IsNullOrEmpty(fieldList[0]))
-                    {
-                        // If the workflow field is simply a scalar or a record set without a child
-                        if (dataPartFieldData.EndsWith(")") && dataPartFieldData == fieldList[0])
-                        {
-                            if (dataPartFieldData.Contains("("))
-                            {
-                                fullyFormattedStringValue = RemoveRecordSetBrace(fieldList[0]);
-                                var intellisenseResult = dataLanguageParser.ValidateName(fullyFormattedStringValue, "");
-                                if (intellisenseResult == null)
-                                {
-                                    verifyPart = IntellisenseFactory.CreateDataListValidationRecordsetPart(fullyFormattedStringValue, String.Empty);
-                                    AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            var intellisenseResult = dataLanguageParser.ValidateName(dataPartFieldData, "");
-                            if (intellisenseResult == null)
-                            {
-                                verifyPart = IntellisenseFactory.CreateDataListValidationScalarPart(RemoveRecordSetBrace(dataPartFieldData));
-                                AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
-                            }
-                        }
-                    }
+                    break;
+                }
+            }
+        }
+
+        static IDataListVerifyPart AddField(Dev2DataLanguageParser dataLanguageParser, Dictionary<IDataListVerifyPart, string> unique, IDataListVerifyPart verifyPart, string[] fieldList, bool recAdded, string item)
+        {
+            // If it's a field to a record set
+            var intellisenseResult = dataLanguageParser.ValidateName(item, "");
+            if (intellisenseResult == null && recAdded)
+            {
+                verifyPart = IntellisenseFactory.CreateDataListValidationRecordsetPart(
+                        RemoveRecordSetBrace(fieldList.ElementAt(0)), item);
+                AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
+            }
+
+            return verifyPart;
+        }
+
+        static void AddField(Dev2DataLanguageParser dataLanguageParser, Dictionary<IDataListVerifyPart, string> unique, ref IDataListVerifyPart verifyPart, ref string fullyFormattedStringValue, ref bool recAdded, string item)
+        {
+            if (item.Contains("("))
+            {
+                fullyFormattedStringValue = RemoveRecordSetBrace(item);
+                var intellisenseResult = dataLanguageParser.ValidateName(fullyFormattedStringValue, "");
+                if (intellisenseResult == null)
+                {
+                    recAdded = true;
+                    verifyPart =
+                         IntellisenseFactory.CreateDataListValidationRecordsetPart(fullyFormattedStringValue,
+                             String.Empty);
+                    AddDataVerifyPart(verifyPart, verifyPart.DisplayValue, unique);
                 }
             }
         }
