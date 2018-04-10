431b4918dbc5305e3d12ec75f5c4fba242d6b211
Rory McGuire <rory.mcguire@dev2.co.za>
2018-04-10 15:55:48 +0200

    3043 WIP execution state logger

--- Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
+++ Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
@@ -3,11 +3,12 @@ using System.IO;
 
 namespace Dev2.Interfaces
 {
-    public interface IDev2StateLogger
+    public interface IDev2StateLogger : IDisposable
     {
         void LogPreExecuteState(IDev2Activity nextActivity);
         void LogPostExecuteState(IDev2Activity previousActivity, IDev2Activity nextActivity);
         void LogExecuteException(Exception e, IDev2Activity activity);
         void LogExecuteCompleteState();
+        void Close();
     }
 }
