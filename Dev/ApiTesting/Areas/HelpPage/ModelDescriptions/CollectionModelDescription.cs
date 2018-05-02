db1e62c31875418af0b28cb6da8336396608ef85
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2018-05-02 10:13:15 +0200

    3140: Added a WebAPI solution that will now be used as the project for testing Warewolf against API's. Updated the WebApi to have a api/Error action that returns a 404 with data, this is used for bug WOLF-3410

---
+++ Dev/ApiTesting/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ApiTesting.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
