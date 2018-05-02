b71e9a63b5f516eb9e5eb2b2c91de936f6a8d74f
Rory McGuire <rory.mcguire@dev2.co.za>
2018-05-02 13:06:04 +0200

    fix build

---
+++ Dev/Dev2.Studio.Core/runtests.bat
@@ -0,0 +1,2 @@
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"  ..\Dev2.Studio.Core.Tests\bin\Debug\Dev2.Studio.Core.Tests.dll
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"  ..\Dev2.Studio.Core.Specs\bin\Debug\Dev2.Studio.Core.Specs.dll
