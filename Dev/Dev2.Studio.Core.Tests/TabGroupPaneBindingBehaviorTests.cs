1cfbb99645d0819a2ca6e147dbf5d5ae40eb5a75
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-05-09 15:40:55 +0200

    DO-373 add unit test for TabGroupPaneBindingBehavior

---
+++ Dev/Dev2.Studio.Core.Tests/TabGroupPaneBindingBehaviorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Dev2.Studio.AppResources.Behaviors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2.Core.Tests
+{
+    [TestClass]
+    public class TabGroupPaneBindingBehaviorTests
+    {
+        [TestMethod]
+        public void TabGroupPaneBindingBehavior_SetDocumentHost_CanSetDocumentHost()
+        {
+            //------------Setup for test-------------------------
+            var myTabGroupPaneBindingBehavior = new TabGroupPaneBindingBehavior();
+            //------------Execute Test---------------------------
+            myTabGroupPaneBindingBehavior.DocumentHost = new Infragistics.Windows.DockManager.DocumentContentHost();
+            //------------Assert Results-------------------------
+            Assert.IsNotNull(myTabGroupPaneBindingBehavior.DocumentHost);
+        }
+    }
+}
