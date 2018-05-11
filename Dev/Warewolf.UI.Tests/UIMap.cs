9fad22ec772cb168d88b678122dcf39b5b212a79
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-05-11 16:46:57 +0200

    3178: Add Coded UI tests for UpdatePaneToolWindow in ShellView code behind not covered

--- Dev/Warewolf.UI.Tests/UIMap.cs
+++ Dev/Warewolf.UI.Tests/UIMap.cs
@@ -1,4 +1,9 @@
-﻿using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
 using System.Windows.Input;
 using MouseButtons = System.Windows.Forms.MouseButtons;
 using System;
@@ -69,7 +74,7 @@ namespace Warewolf.UI.Tests
         {
             Assert.IsTrue(MainStudioWindow.Exists, "Warewolf studio is not running. You are expected to run \"Dev\\Run Tests.ps1\" as an administrator and wait for it to complete before running any coded UI tests");
             Keyboard.SendKeys(MainStudioWindow, "^%{F4}");
-            
+
         }
 
         public void TryPin_Unpinned_Pane_To_Default_Position()
@@ -336,7 +341,7 @@ namespace Warewolf.UI.Tests
 
         public void Pin_Unpinned_Pane_To_Another_UnpinnedTab()
         {
-            Mouse.StartDragging(MainStudioWindow.UnpinnedTab, new Point(5,5));
+            Mouse.StartDragging(MainStudioWindow.UnpinnedTab, new Point(5, 5));
             Mouse.StopDragging(MainStudioWindow.UnpinnedTab);
         }
         public void Pin_Unpinned_Pane_To_Default_Position()
@@ -350,8 +355,17 @@ namespace Warewolf.UI.Tests
             Mouse.StopDragging(0, 50);
             Playback.Wait(2000);
         }
+        public void Unpin_Pane_With_Context_Menu(UITestControl Tab)
+        {
+            Mouse.StartDragging(Tab);
+            Mouse.StopDragging(0, 50);
+
+            //Mouse.Click(Tab, MouseButtons.Right, ModifierKeys.None, new Point(106, 28));
+            //Mouse.Click(MainStudioWindow.PaneContextItem.FloatingMenuItem);
+        }
+
         public void Unpin_Tab2_With_Drag(UITestControl Tab)
-        {                        
+        {
             Mouse.StartDragging(Tab);
             Mouse.StopDragging(0, 100);
             Playback.Wait(2000);
@@ -877,7 +891,7 @@ namespace Warewolf.UI.Tests
         [Then(@"The GetCountries Recordset Is Visible in Debug Output")]
         public void ThenTheDebugOutputShowsGetCountriesRecordset()
         {
-            Assert.IsTrue(WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.DebugOutputTree.Step1.RecordsetGroup.RecordsetName.DisplayText.StartsWith("[[dbo_GetCountries(") && 
+            Assert.IsTrue(WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.DebugOutputTree.Step1.RecordsetGroup.RecordsetName.DisplayText.StartsWith("[[dbo_GetCountries(") &&
                 WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.DebugOutputTree.Step1.RecordsetGroup.RecordsetName.DisplayText.EndsWith(").CountryID]]"), "DB connector debug output recordset is not dbo_GetCountries with CountryID field.");
             Assert.IsTrue(!String.IsNullOrEmpty(WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.ContentPane.ContentDockManager.SplitPaneRight.DebugOutput.DebugOutputTree.Step1.RecordsetGroup.RecordsetValue.DisplayText), "No recordset value in debug output for new DB connector.");
         }
@@ -1086,5 +1100,18 @@ namespace Warewolf.UI.Tests
         private UtilityToolsUIMap _UtilityToolsUIMap;
 
         #endregion
+
+        /// <summary>
+        /// RightClickDebugOutput
+        /// </summary>
+        public void RightClickDebugOutput()
+        {
+            #region Variable Declarations
+            WpfCustom uIOUTPUTCustom = this.UIWarewolfDEV2PIETERTEWindow.UIDev2StudioViewModelsCustom.UIContentDockManagerCustom.UIOUTPUTCustom;
+            #endregion
+
+            // Right-Click 'OUTPUT' custom control
+            Mouse.Click(uIOUTPUTCustom, MouseButtons.Right, ModifierKeys.None, new Point(106, 28));
+        }
     }
 }
