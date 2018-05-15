b91a9acb477ae688be8e149ce556de7ddac2eaf7
Rory McGuire <rory.mcguire@dev2.co.za>
2018-05-15 15:10:02 +0200

    3190 converage for DebugItemWarewolfAtomListResult

--- Dev/Dev2.Activities.Tests/ActivityTests/DebugItemWarewolfAtomListResultTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DebugItemWarewolfAtomListResultTests.cs
@@ -35,6 +35,134 @@ namespace Dev2.Tests.Activities.ActivityTests
         }
 
         [TestMethod]
+        [Owner("Rory McGuire")]
+        public void DebugItemWarewolfAtomListResult_IsScalarOldValue_GetDebugItemResult()
+        {
+            //---------------Set up test pack-------------------
+            var env = new Mock<IExecutionEnvironment>();
+            env.Setup(environment => environment.EvalToExpression(It.IsAny<string>(), It.IsAny<int>())).Returns("[[scalar]]");
+            var warewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}"));
+            env.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(warewolfAtomResult);
+            var atomList = new WarewolfAtomList<DataStorage.WarewolfAtom>(DataStorage.WarewolfAtom.NewDataString("Home"));
+            var newWarewolfAtomListresult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomListresult(atomList) as CommonFunctions.WarewolfEvalResult.WarewolfAtomListresult;
+            Assert.IsNotNull(newWarewolfAtomListresult);
+            newWarewolfAtomListresult.Item.AddSomething(DataStorage.WarewolfAtom.NewDataString("KingDom Of The Zulu"));
+            var debugEvalResult = new DebugItemWarewolfAtomListResult(newWarewolfAtomListresult, warewolfAtomResult, "newValue", "Variable", "", "Some right label text", "");
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(debugEvalResult);
+            //---------------Execute Test ----------------------
+            Assert.AreEqual("", debugEvalResult.LabelText);
+            var debugItemResults = debugEvalResult.GetDebugItemResult();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, debugItemResults.Count);
+            Assert.AreEqual("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}", debugItemResults[0].Value);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void DebugItemWarewolfAtomListResult_IsListOldValue_GetDebugItemResult()
+        {
+            //---------------Set up test pack-------------------
+            var env = new Mock<IExecutionEnvironment>();
+            env.Setup(environment => environment.EvalToExpression(It.IsAny<string>(), It.IsAny<int>())).Returns("[[scalar]]");
+            var warewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}"));
+            env.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(warewolfAtomResult);
+            var atomList = new WarewolfAtomList<DataStorage.WarewolfAtom>(DataStorage.WarewolfAtom.NewDataString("Home"));
+            var newWarewolfAtomListresult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomListresult(atomList) as CommonFunctions.WarewolfEvalResult.WarewolfAtomListresult;
+            Assert.IsNotNull(newWarewolfAtomListresult);
+            newWarewolfAtomListresult.Item.AddSomething(DataStorage.WarewolfAtom.NewDataString("KingDom Of The Zulu"));
+            var debugEvalResult = new DebugItemWarewolfAtomListResult(newWarewolfAtomListresult, newWarewolfAtomListresult, "newValue(1)", "Variable", "", "Some right label text", "");
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(debugEvalResult);
+            //---------------Execute Test ----------------------
+            Assert.AreEqual("", debugEvalResult.LabelText);
+            var debugItemResults = debugEvalResult.GetDebugItemResult();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, debugItemResults.Count);
+            Assert.AreEqual("KingDom Of The Zulu", debugItemResults[0].Value);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void DebugItemWarewolfAtomListResult_IsListOldValue_GetDebugItemResult2()
+        {
+            //---------------Set up test pack-------------------
+            var env = new Mock<IExecutionEnvironment>();
+            env.Setup(environment => environment.EvalToExpression(It.IsAny<string>(), It.IsAny<int>())).Returns("[[scalar]]");
+            var warewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}"));
+            env.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(warewolfAtomResult);
+            var atomList = new WarewolfAtomList<DataStorage.WarewolfAtom>(DataStorage.WarewolfAtom.NewDataString("Home"));
+            var newWarewolfAtomListresult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomListresult(atomList) as CommonFunctions.WarewolfEvalResult.WarewolfAtomListresult;
+            Assert.IsNotNull(newWarewolfAtomListresult);
+            newWarewolfAtomListresult.Item.AddSomething(DataStorage.WarewolfAtom.NewDataString("KingDom Of The Zulu"));
+            var debugEvalResult = new DebugItemWarewolfAtomListResult(newWarewolfAtomListresult, newWarewolfAtomListresult, "[[newValue().PolicyNo]]", "Variable", "", "Some right label text", "");
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(debugEvalResult);
+            //---------------Execute Test ----------------------
+            Assert.AreEqual("", debugEvalResult.LabelText);
+            var debugItemResults = debugEvalResult.GetDebugItemResult();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, debugItemResults.Count);
+            Assert.AreEqual("KingDom Of The Zulu", debugItemResults[0].Value);
+
+            Assert.AreEqual("[[newValue(1).PolicyNo]]", debugItemResults[0].Variable);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void DebugItemWarewolfAtomListResult_IsListOldValue_IsCalculate_GetDebugItemResult()
+        {
+            //---------------Set up test pack-------------------
+            const string rightLabel = "Some right label text";
+            var env = new Mock<IExecutionEnvironment>();
+            env.Setup(environment => environment.EvalToExpression(It.IsAny<string>(), It.IsAny<int>())).Returns("[[scalar]]");
+            var warewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}"));
+            env.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(warewolfAtomResult);
+            var atomList = new WarewolfAtomList<DataStorage.WarewolfAtom>(DataStorage.WarewolfAtom.NewDataString("Home"));
+            var newWarewolfAtomListresult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomListresult(atomList) as CommonFunctions.WarewolfEvalResult.WarewolfAtomListresult;
+            Assert.IsNotNull(newWarewolfAtomListresult);
+            newWarewolfAtomListresult.Item.AddSomething(DataStorage.WarewolfAtom.NewDataString("KingDom Of The Zulu"));
+            var debugEvalResult = new DebugItemWarewolfAtomListResult(newWarewolfAtomListresult, newWarewolfAtomListresult, "newValue", "Variable", "", rightLabel, "", true, false);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(debugEvalResult);
+            //---------------Execute Test ----------------------
+            Assert.AreEqual("", debugEvalResult.LabelText);
+            var debugItemResults = debugEvalResult.GetDebugItemResult();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, debugItemResults.Count);
+            Assert.AreEqual("KingDom Of The Zulu", debugItemResults[0].Value);
+            Assert.AreEqual(rightLabel, debugItemResults[0].Label);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void DebugItemWarewolfAtomListResult_IsListOldValue_IsCalculate_GetDebugItemResult2()
+        {
+            //---------------Set up test pack-------------------
+            const string rightLabel = "Some right label text";
+            var env = new Mock<IExecutionEnvironment>();
+            env.Setup(environment => environment.EvalToExpression(It.IsAny<string>(), It.IsAny<int>())).Returns("[[scalar]]");
+            var warewolfAtomResult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("{\"PolicyNo\":\"A0003\",\"DateId\":32,\"SomeVal\":\"Bob\"}"));
+            env.Setup(environment => environment.Eval(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(warewolfAtomResult);
+            var atomList = new WarewolfAtomList<DataStorage.WarewolfAtom>(DataStorage.WarewolfAtom.NewDataString("Home"));
+            var newWarewolfAtomListresult = CommonFunctions.WarewolfEvalResult.NewWarewolfAtomListresult(atomList) as CommonFunctions.WarewolfEvalResult.WarewolfAtomListresult;
+            Assert.IsNotNull(newWarewolfAtomListresult);
+            newWarewolfAtomListresult.Item.AddSomething(DataStorage.WarewolfAtom.NewDataString("KingDom Of The Zulu"));
+            var debugEvalResult = new DebugItemWarewolfAtomListResult(newWarewolfAtomListresult, newWarewolfAtomListresult, "[[newValue]]", "Variable", "", rightLabel, "", true, false);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(debugEvalResult);
+            //---------------Execute Test ----------------------
+            Assert.AreEqual("", debugEvalResult.LabelText);
+            var debugItemResults = debugEvalResult.GetDebugItemResult();
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, debugItemResults.Count);
+            Assert.AreEqual("KingDom Of The Zulu", debugItemResults[0].Value);
+
+            Assert.AreEqual("[[newValue]]", debugItemResults[0].Variable);
+            Assert.AreEqual(rightLabel, debugItemResults[0].Label);
+        }
+
+        [TestMethod]
         [Owner("Nkosinathi Sangweni")]
         public void DebugItemWarewolfAtomListResult_GivenJsonArrayResult_GetDebugItemResult()
         {
