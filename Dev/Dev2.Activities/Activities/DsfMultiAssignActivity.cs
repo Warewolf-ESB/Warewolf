0d16dd086cd480325417c7eb0b2e2658677cab3b
Rory McGuire <rory.mcguire@dev2.co.za>
2018-05-18 08:37:51 +0200

    3190 ignore warnings in legacy classes that should not be altered

--- Dev/Dev2.Activities/Activities/DsfMultiAssignActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfMultiAssignActivity.cs
@@ -37,6 +37,7 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
 {
     public class DsfMultiAssignActivity : DsfActivityAbstract<string>
     {
+#pragma warning disable S3776,S1541,S134,CC0075,S1066,S1067
         public static readonly string CalculateTextConvertPrefix = GlobalConstants.CalculateTextConvertPrefix;
         public static readonly string CalculateTextConvertSuffix = GlobalConstants.CalculateTextConvertSuffix;
         public static readonly string CalculateTextConvertFormat = GlobalConstants.CalculateTextConvertFormat;
@@ -495,6 +496,7 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                 return hashCode;
             }
         }
+#pragma warning restore S3776,S1541,S134,CC0075,S1066,S1067
     }
 
 }
