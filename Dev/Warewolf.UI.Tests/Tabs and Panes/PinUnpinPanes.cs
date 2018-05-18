ab03addcff0e3176e4793c2d77ed3aa5b4c58075
Candice.Daniel <Candice.Daniel@RSAKLFCANDICE>
2018-05-18 10:17:52 +0200

    3178-Removed UnpinAndRepinNewWorkflowWizardTabByDraggingOnly and SwitchTool_EditSwitchCase_With_Three_Tools_UITest. New tickets will be created for them

--- Dev/Warewolf.UI.Tests/Tabs and Panes/PinUnpinPanes.cs
+++ Dev/Warewolf.UI.Tests/Tabs and Panes/PinUnpinPanes.cs
@@ -55,17 +55,6 @@ namespace Warewolf.UI.Tests.Tabs
 
         [TestMethod]
         [TestCategory("Tabs and Panes")]
-        public void UnpinAndRepinNewWorkflowWizardTabByDraggingOnly()
-        {
-            UIMap.Click_NewWorkflow_RibbonButton();
-            UIMap.Unpin_Tab_With_Drag(WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab);
-            UIMap.Pin_Unpinned_Pane_To_Default_Position();
-            Assert.IsFalse(UIMap.ControlExistsNow(UIMap.MainStudioWindow.UnpinnedTab), "Unpinned pane still exists after being dragged onto the central dock indicator.");
-            Assert.IsTrue(WorkflowTabUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.Exists, "Workflow tab did not dock into it's default position after being dragged onto the central dock indicator.");
-        }
-
-        [TestMethod]
-        [TestCategory("Tabs and Panes")]
         public void UnpinnedPaneContextMenuItems()
         {
             UIMap.Click_NewWorkflow_RibbonButton();
