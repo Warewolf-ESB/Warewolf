dda5e49adbd2b00849b99c09c99d57aa65092860
Rory McGuire <rory.mcguire@dev2.co.za>
2018-06-11 09:24:33 +0200

    3221 Command Timeout should be passed to test db calls

--- Dev/Dev2.Activities.Designers/Designers2/MySqlDatabase/MySqlDatabaseDesignerViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/MySqlDatabase/MySqlDatabaseDesignerViewModel.cs
@@ -417,7 +417,8 @@ namespace Dev2.Activities.Designers2.MySqlDatabase
             {
                 Source = SourceRegion.SelectedSource,
                 Action = ActionRegion.SelectedAction,
-                Inputs = new List<IServiceInput>()
+                Inputs = new List<IServiceInput>(),
+                CommandTimeout = InputArea.CommandTimeout
             };
             foreach (var serviceInput in InputArea.Inputs)
             {
