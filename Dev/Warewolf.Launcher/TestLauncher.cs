7dfd30392875e55d622f1c85516295359fc2c049
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-12 11:21:29 +0200

    DO-362 use ci remote resources in the container that the launcher uses

--- Dev/Warewolf.Launcher/TestLauncher.cs
+++ Dev/Warewolf.Launcher/TestLauncher.cs
@@ -348,7 +348,7 @@ namespace Warewolf.Launcher
         {
             try
             {
-                ciRemoteContainerLauncher = new WarewolfServerContainerLauncher("localhost", "", "DO-362_CI_Remote_Containers");
+                ciRemoteContainerLauncher = new WarewolfServerContainerLauncher("localhost", "", "latest", true);
                 CIRemoteOverloading(ciRemoteContainerLauncher.IP);
             }
             catch (Exception e)
