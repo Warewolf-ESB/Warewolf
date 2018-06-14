b880c3d23ee52dbff2e302a5b7d7ccef407fb7f4
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-14 11:01:51 +0200

    DO-362 move this unit test out of it's assembly. it is cross contaminating another test in that assembly

--- Dev/Dev2.Runtime.Tests/Hosting/ResourceCatalogTests.cs
+++ Dev/Dev2.Runtime.Tests/Hosting/ResourceCatalogTests.cs
@@ -46,6 +46,11 @@ using Newtonsoft.Json;
 using Unlimited.Framework.Converters.Graph.Ouput;
 using Warewolf.ResourceManagement;
 using System.Collections.Concurrent;
+using Dev2.Studio.Interfaces;
+using Dev2.Communication;
+using Warewolf.Studio.ViewModels;
+using Dev2.Controller;
+using System.Collections.ObjectModel;
 
 namespace Dev2.Tests.Runtime.Hosting
 {
@@ -3137,6 +3142,71 @@ namespace Dev2.Tests.Runtime.Hosting
             //------------Assert Precondition-----------------
             Assert.AreEqual(2, result.Count);
             Assert.IsNotNull(oldResource);
+
+        }
+
+
+        [TestMethod, Timeout(60000)]
+        [Owner("Nkosinathi Sangweni")]
+        public void CallDuplicateCommand_GivenIsFolder_ShouldAddValidPayloads()
+        {
+            //---------------Set up test pack-------------------
+
+            var envMock = new Mock<IEnvironmentConnection>();
+            var controller = new Mock<ICommunicationController>();
+            var envModel = new Mock<IEnvironmentViewModel>();
+            envModel.Setup(model => model.Children).Returns(new ObservableCollection<IExplorerItemViewModel>());
+            var selectedItemMock = new Mock<IExplorerViewModel>();
+            var itemObj = new Mock<IExplorerItemViewModel>();
+            itemObj.Setup(model => model.IsFolder).Returns(true);
+            var item = new Mock<IExplorerTreeItem>();
+            item.Setup(model => model.ResourceName).Returns("name");
+            item.Setup(model => model.ResourceType).Returns("type");
+            item.Setup(model => model.ResourceName).Returns("name");
+            item.Setup(model => model.IsFolder).Returns(true);
+            selectedItemMock.Setup(sitem => sitem.SelectedItem).Returns(item.Object);
+            var serverRepo = new Mock<IServerRepository>();
+            var connectionObject = new Mock<IEnvironmentConnection>();
+            serverRepo.Setup(repository => repository.ActiveServer.Connection).Returns(connectionObject.Object);
+            CustomContainer.Register(serverRepo.Object);
+            var viewModel = RequestServiceNameViewModel.CreateAsync(envModel.Object, "", "", itemObj.Object).Result;
+
+            controller.Setup(communicationController => communicationController.AddPayloadArgument("ResourceID", It.IsAny<string>()));
+            controller.Setup(communicationController => communicationController.AddPayloadArgument("NewResourceName", It.IsAny<string>()));
+            controller.Setup(communicationController => communicationController.AddPayloadArgument("FixRefs", It.IsAny<string>()));
+            controller.Setup(communicationController => communicationController.ExecuteCommand<ExecuteMessage>(It.IsAny<IEnvironmentConnection>(), It.IsAny<Guid>()));
+            var lazyCon = typeof(RequestServiceNameViewModel).GetField("_lazyCon", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
+
+            lazyCon.SetValue(viewModel, envMock.Object);
+            var lazyComs = typeof(RequestServiceNameViewModel).GetField("_lazyComs", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
+
+            lazyComs.SetValue(viewModel, controller.Object);
+            var selectedItem = typeof(RequestServiceNameViewModel).GetProperty("SingleEnvironmentExplorerViewModel", BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.Public);
+
+            selectedItem.SetValue(viewModel, selectedItemMock.Object);
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(viewModel);
+
+
+            //---------------Execute Test ----------------------
+            var canExecute = viewModel.DuplicateCommand.CanExecute(null);
+            try
+            {
+                viewModel.DuplicateCommand.Execute(null);
+
+            }
+            catch (Exception f) when (f is NullReferenceException)
+            {
+                Console.WriteLine(f);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            //---------------Test Result -----------------------
+            var value = lazyComs.GetValue(viewModel) as ICommunicationController;
+            Assert.IsNotNull(value);
+            Assert.AreEqual("DuplicateFolderService", value.ServiceName);
 
         }
 
