18e7a8c1c2e95161ae2db257ef84bad51d3b022f
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-14 12:04:50 +0200

    DO-362 still trying to fix build hang by moving another unit test out of the warewolf.studio.viewmodel.tests assembly

--- Dev/Warewolf.Studio.ViewModels.Tests/RequestServiceNameViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/RequestServiceNameViewModelTests.cs
@@ -532,60 +532,6 @@ namespace Warewolf.Studio.ViewModels.Tests
 
         [TestMethod,Timeout(60000)]
         [Owner("Nkosinathi Sangweni")]
-        public void CallDuplicateService_GivenValidComsController_ShouldExecuteCorrectly()
-        {
-            //---------------Set up test pack-------------------
-
-            var envMock = new Mock<IEnvironmentConnection>();
-            var controller = new Mock<ICommunicationController>();
-            var envModel = new Mock<IEnvironmentViewModel>();
-            envModel.Setup(model => model.Children).Returns(new ObservableCollection<IExplorerItemViewModel>());
-            var itemObj = new Mock<IExplorerItemViewModel>();
-            var selectedItemMock = new Mock<IExplorerViewModel>();
-            var item = new Mock<IExplorerTreeItem>();
-            item.Setup(model => model.ResourceName).Returns("name");
-            item.Setup(model => model.ResourceType).Returns("type");
-            item.Setup(model => model.ResourceName).Returns("name");
-            selectedItemMock.Setup(sitem => sitem.SelectedItem).Returns(item.Object);
-            var serverRepo = new Mock<IServerRepository>();
-            var connectionObject = new Mock<IEnvironmentConnection>();
-            serverRepo.Setup(repository => repository.ActiveServer.Connection).Returns(connectionObject.Object);
-            CustomContainer.Register(serverRepo.Object);
-            var viewModel = RequestServiceNameViewModel.CreateAsync(envModel.Object, "", "", itemObj.Object).Result;
-
-            controller.Setup(communicationController => communicationController.AddPayloadArgument("ResourceID", It.IsAny<string>()));
-            controller.Setup(communicationController => communicationController.AddPayloadArgument("NewResourceName", It.IsAny<string>()));
-            controller.Setup(communicationController => communicationController.AddPayloadArgument("FixRefs", It.IsAny<string>()));
-            controller.Setup(communicationController => communicationController.ExecuteCommand<ExecuteMessage>(It.IsAny<IEnvironmentConnection>(), It.IsAny<Guid>()));
-            var lazyCon = typeof(RequestServiceNameViewModel).GetField("_lazyCon", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
-            
-            lazyCon.SetValue(viewModel, envMock.Object);
-            var lazyComs = typeof(RequestServiceNameViewModel).GetField("_lazyComs", BindingFlags.Instance | BindingFlags.GetField | BindingFlags.NonPublic);
-            
-            lazyComs.SetValue(viewModel, controller.Object);
-            var selectedItem = typeof(RequestServiceNameViewModel).GetProperty("SingleEnvironmentExplorerViewModel", BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.Public);
-            
-            selectedItem.SetValue(viewModel, selectedItemMock.Object);
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(viewModel);
-            //---------------Execute Test ----------------------
-
-            try
-            {
-                viewModel.DuplicateCommand.Execute(null);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e.Message);
-            }
-            //---------------Test Result -----------------------
-            controller.Verify(communicationController => communicationController.AddPayloadArgument("ResourceID", It.IsAny<string>()));
-            controller.Verify(communicationController => communicationController.AddPayloadArgument("NewResourceName", It.IsAny<string>()));
-            controller.Verify(communicationController => communicationController.ExecuteCommand<ResourceCatalogDuplicateResult>(It.IsAny<IEnvironmentConnection>(), It.IsAny<Guid>()));
-        }
-
-        [TestMethod,Timeout(60000)]
-        [Owner("Nkosinathi Sangweni")]
         public void CallDuplicateCommand_GivenNoItemPassed_ShouldSetCanExecuteFalse()
         {
             //---------------Set up test pack-------------------
