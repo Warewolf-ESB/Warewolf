5f14903e287708c3e86e7286779566b29ebfc0c6
Hagashen Naidu <hagashen.naidu@dev2.co.za>
2018-06-15 11:45:26 +0200

    remove the application tracker from the container for this test

--- Dev/Dev2.Studio.Core.Tests/Workflows/WorkflowDesignerUnitTest.cs
+++ Dev/Dev2.Studio.Core.Tests/Workflows/WorkflowDesignerUnitTest.cs
@@ -52,7 +52,7 @@ using Dev2.Studio.Interfaces.DataList;
 using Dev2.Data.Interfaces.Enums;
 using Dev2.Data.Interfaces;
 using Dev2.Studio.Interfaces.Enums;
-
+using Dev2.Instrumentation;
 
 namespace Dev2.Core.Tests.Workflows
 {
@@ -2182,6 +2182,7 @@ namespace Dev2.Core.Tests.Workflows
         [Owner("Pieter Terblanche")]
         public void WorkflowDesignerViewModel_PerformAddItems_ModelItemWithFlowStepWithServiceName_FlowStepHandled()
         {
+            CustomContainer.DeRegister<IApplicationTracker>();
             var explorerTooltips = new Mock<IExplorerTooltips>();
             CustomContainer.Register(explorerTooltips.Object);
             #region Setup view model constructor parameters
