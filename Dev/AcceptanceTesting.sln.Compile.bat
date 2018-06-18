4924582738fcdbb78872f1b0629322f21ac51822
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-18 14:43:59 +0200

    Revert "DO-382 must change dir here to put the bin directory in the right place"
    This reverts commit b9cb620ec5bacd4f94627584ebed7a953fc39813.

--- Dev/AcceptanceTesting.sln.Compile.bat
+++ Dev/AcceptanceTesting.sln.Compile.bat
@@ -1,2 +1,1 @@
-cd %~dp0..\
-powershell -ExecutionPolicy Bypass -File %~dp0Compile.ps1 --AcceptanceTesting -SolutionWideBuildOutputs %1 %2 %3 %4 %5
+powershell -ExecutionPolicy Bypass -File %~dp0..\Compile.ps1 --AcceptanceTesting -SolutionWideBuildOutputs %1 %2 %3 %4 %5
