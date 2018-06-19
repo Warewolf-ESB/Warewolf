29bebacb2203b61aca8e7c8347876c3007ffa9e4
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-19 10:31:42 +0200

    DO-382 fix compile break caused by merge

--- Dev/AcceptanceTesting.sln.Compile.bat
+++ Dev/AcceptanceTesting.sln.Compile.bat
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -File %~dp0..\Compile.ps1 --AcceptanceTesting -SolutionWideOutputs %1 %2 %3 %4 %5
+powershell -ExecutionPolicy Bypass -File %~dp0..\Compile.ps1 -AcceptanceTesting -SolutionWideOutputs %1 %2 %3 %4 %5
