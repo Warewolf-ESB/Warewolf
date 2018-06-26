32104dd52706d434d2fd584bed016c2f5e3ee9f4
Rory McGuire <rory.mcguire@dev2.co.za>
2018-06-26 15:11:22 +0200

    3043 ability to log additional details

--- Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
+++ Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
@@ -6,6 +6,7 @@ namespace Dev2.Interfaces
     public interface IDev2StateLogger : IDisposable
     {
         void LogPreExecuteState(IDev2Activity nextActivity);
+        void LogAdditionalDetail(object detail, string callerName = null);
         void LogPostExecuteState(IDev2Activity previousActivity, IDev2Activity nextActivity);
         void LogExecuteException(Exception e, IDev2Activity activity);
         void LogExecuteCompleteState();
