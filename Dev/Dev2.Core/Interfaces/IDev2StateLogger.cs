9e1cf73fc590c4de906686ddfbde83c11ae8eb90
Rory McGuire <rory.mcguire@dev2.co.za>
2018-06-28 11:30:07 +0200

    3049 refactor

--- Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
+++ Dev/Dev2.Core/Interfaces/IDev2StateLogger.cs
@@ -5,16 +5,18 @@ namespace Dev2.Interfaces
 {
     public interface IDev2StateLogger : IDisposable
     {
-        void Subscribe(IStateLoggerListener listener);
         void LogPreExecuteState(IDev2Activity nextActivity);
         void LogAdditionalDetail(object detail, string callerName);
         void LogPostExecuteState(IDev2Activity previousActivity, IDev2Activity nextActivity);
         void LogExecuteException(Exception e, IDev2Activity activity);
         void LogExecuteCompleteState();
         void LogStopExecutionState();
-        void Close();
     }
-    public interface IStateLoggerListener
+    public interface IStateNotifier : IDev2StateLogger
+    {
+        void Subscribe(IStateListener listener);
+    }
+    public interface IStateListener
     {
         bool Notify(string type, object payload);
     }
---
+++ Dev/Dev2.Runtime/ESB/Execution/State/StateNotifier.cs
@@ -0,0 +1,81 @@
+﻿using Dev2.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Dev2.Runtime.ESB.Execution.State
+{
+    class StateNotifier : IStateNotifier, IDisposable
+    {
+        public void Dispose()
+        {
+            foreach (var listener in stateListeners)
+            {
+                if (listener is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+
+        public void LogAdditionalDetail(object detail, string callerName)
+        {
+            Notify(nameof(LogAdditionalDetail), new
+            {
+                CallerName = callerName,
+                Detail = detail,
+            });
+        }
+
+        public void LogExecuteCompleteState()
+        {
+            Notify(nameof(LogExecuteCompleteState), null);
+        }
+
+        public void LogExecuteException(Exception e, IDev2Activity activity)
+        {
+            Notify(nameof(LogExecuteException), new
+            {
+                Activity = activity,
+                Exception = e,
+            });
+        }
+
+        public void LogPostExecuteState(IDev2Activity previousActivity, IDev2Activity nextActivity)
+        {
+            Notify(nameof(LogPostExecuteState), new
+            {
+                previousActivity,
+                nextActivity,
+            });
+        }
+
+        public void LogPreExecuteState(IDev2Activity nextActivity)
+        {
+            Notify(nameof(LogPreExecuteState), nextActivity);
+        }
+
+        public void LogStopExecutionState()
+        {
+            Notify(nameof(LogStopExecutionState), null);
+        }
+
+        readonly IList<IStateListener> stateListeners = new List<IStateListener>();
+        public void Subscribe(IStateListener listener)
+        {
+            stateListeners.Add(listener);
+        }
+
+        private void Notify(string callerName, object payload)
+        {
+            foreach (var stateListener in stateListeners)
+            {
+                var result = stateListener.Notify(callerName, payload);
+                if (!result)
+                {
+                    throw new StateLoggerStoppedException();
+                }
+            }
+        }
+        class StateLoggerStoppedException : Exception { }
+    }
+}
