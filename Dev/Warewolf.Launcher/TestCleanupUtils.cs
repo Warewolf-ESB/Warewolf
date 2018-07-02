76a2af26627d526a6cf1a87f3e87178a8b056551
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-02 09:27:44 +0200

    DO-382 introduce WindowUtils to force the launcher window to the front whenever it needs user input

--- Dev/Warewolf.Launcher/TestCleanupUtils.cs
+++ Dev/Warewolf.Launcher/TestCleanupUtils.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace Warewolf.Launcher
