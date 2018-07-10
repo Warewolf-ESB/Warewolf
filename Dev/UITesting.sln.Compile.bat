c1d1b29f32c267dabd40eaeeacfd2c8585e3765a
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-10 08:53:12 +0200

    WOLF-3691 add the new SkipCodeAnalysis arg to the compile shortcuts

--- Dev/UITesting.sln.Compile.bat
+++ Dev/UITesting.sln.Compile.bat
@@ -1,2 +1,2 @@
-powershell -ExecutionPolicy Bypass -File %~dp0..\Compile.ps1 -UITesting -Server -Studio -SolutionWideOutputs %1 %2 %3 %4 %5
+powershell -ExecutionPolicy Bypass -File %~dp0..\Compile.ps1 -UITesting -Server -Studio -SolutionWideOutputs -SkipCodeAnalysis %1 %2 %3 %4 %5
 pause
