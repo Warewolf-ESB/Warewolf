a0a4548e5312ce929ab9ebbe21fb421ccc6cb2db
sanelemthembu <Sanele.Mthembu@RSAKLFSANELEM>
2018-07-19 14:27:58 +0200

    3330 :  DsfAggregateCalculateActivity GetState

--- Dev/Dev2.Activities/Activities/DsfAggregateCalculateActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfAggregateCalculateActivity.cs
@@ -6,6 +6,7 @@ using Dev2.Activities;
 using Dev2.Common;
 using Dev2.Common.Common;
 using Dev2.Common.Interfaces.Diagnostics.Debug;
+using Dev2.Common.State;
 using Dev2.Data;
 using Dev2.Data.TO;
 using Dev2.Diagnostics;
@@ -247,6 +248,23 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                 hashCode = (hashCode * 397) ^ (DisplayName != null ? DisplayName.GetHashCode() : 0);
                 return hashCode;
             }
+        }
+
+        public IEnumerable<StateVariable> GetState()
+        {
+            return new[]
+           {
+                new StateVariable
+                {
+                    Type = StateVariable.StateType.Input,
+                    Value = Expression
+                },
+                new StateVariable
+                {
+                    Type = StateVariable.StateType.Output,
+                    Value = Result
+                }
+            };
         }
     }
 }
