c5f3da2a26956c24869cacc3544dd03274178be9
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-07-19 11:14:58 +0200

    3330: Expected commit break. Add StateVariable to be implemented across activities.

---
+++ Dev/Dev2.Common/State/StateVariable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev2.Common.State
+{
+    public struct StateVariable
+    {
+        public enum StateType { Input, Output, InputOutput };
+        public StateType Type;
+        public string Value;
+    }
+}
