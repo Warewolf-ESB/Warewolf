407e5c13be06bf80cb38558d1be3858a500b78e6
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-19 08:38:47 +0200

    WOLF-3723 refactor adminmode.cs and manualmode.cs

---
+++ Dev/Warewolf.Launcher/Utils/ManualMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Warewolf.Launcher.Utils
+{
+    class ManualMode
+    {
+        public static void Run(TestLauncher build)
+        {
+            build.InstallServer();
+            build.CleanupServerStudio();
+            build.Startmywarewolfio();
+            build.TryStartLocalCIRemoteContainer();
+            if (String.IsNullOrEmpty(build.DomywarewolfioStart))
+            {
+                build.StartServer();
+                if (String.IsNullOrEmpty(build.DoServerStart) && String.IsNullOrEmpty(build.DomywarewolfioStart))
+                {
+                    build.StartStudio();
+                }
+            }
+            Console.WriteLine("Press Enter to Shutdown.");
+            Console.ReadKey();
+            build.CleanupServerStudio();
+        }
+    }
+}
