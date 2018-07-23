462e010776b2a783db9170b0aff2ac6cbd5f20e9
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-23 11:48:01 +0200

    WOLF-3729 still fixing path to embedded resource

--- Dev/Dev2.Runtime.Tests/InitializeAuditDB.cs
+++ Dev/Dev2.Runtime.Tests/InitializeAuditDB.cs
@@ -11,7 +11,7 @@ namespace Dev2.Runtime.Tests
         [AssemblyInitialize]
         public static void AssemblyInit(TestContext context)
         {
-            string inputFilePath = "Dev2.Runtime.Tests.auditDB.db";
+            string inputFilePath = "Dev2.Tests.Runtime.auditDB.db";
             string outputFilePath = Environment.ExpandEnvironmentVariables(@"%programdata%\Warewolf\Audits\auditDB.db");
             if (File.Exists(outputFilePath))
             {
