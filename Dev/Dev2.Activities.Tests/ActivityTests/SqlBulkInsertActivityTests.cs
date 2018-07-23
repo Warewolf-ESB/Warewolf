fe0719a2aedc237d55260db9a57a859de89d05c0
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-23 10:05:46 +0200

    WOLF-3729 fix for activity unit test failures due to Provider Incompatable Error

--- Dev/Dev2.Activities.Tests/ActivityTests/SqlBulkInsertActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/SqlBulkInsertActivityTests.cs
@@ -13,6 +13,8 @@ using System.Activities.Statements;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
 using ActivityUnitTests;
 using Dev2.Activities;
 using Dev2.Activities.SqlBulkInsert;
@@ -1271,9 +1273,40 @@ namespace Dev2.Tests.Activities.ActivityTests
             Assert.AreEqual("Hello", returnedDataTable.Rows[2]["Val"]);
         }
 
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            string inputFilePath = "Dev2.Tests.Activities.auditDB.db";
+            string outputFilePath = Environment.ExpandEnvironmentVariables(@"%programdata%\Warewolf\Audits\auditDB.db");
+            if (File.Exists(outputFilePath))
+            {
+                File.Delete(outputFilePath);
+            }
+            if (!Directory.Exists(Path.GetDirectoryName(outputFilePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+            }
+            Stream inputFile = Assembly.GetExecutingAssembly().GetManifestResourceStream(inputFilePath);
+            Assert.IsNotNull(inputFile, inputFilePath + " file not found in " + string.Join(", ", Assembly.GetExecutingAssembly().GetManifestResourceNames()));
+            using (Stream input = inputFile)
+            {
+                using (Stream output = File.Create(outputFilePath))
+                {
+                    byte[] buffer = new byte[8192];
+
+                    int bytesRead;
+                    while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
         [TestMethod]
         [Owner("Travis Frisinger")]
         [TestCategory("DsfSqlBulkInsertActivity_Execute")]
+        [DeploymentItem(@"x86\SQLite.Interop.dll")]
         public void DsfSqlBulkInsertActivity_Execute_WithInputMappingsWithNonNullableColumnWithDataMappedWhenMultiTable_HasDataTableWithData()
         {
             //------------Setup for test--------------------------
