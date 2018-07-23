a8dd377f8b15b614872c9d269a42a8514d6ee4d8
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-07-23 10:08:12 +0200

    WOLF-3729 move the audit db initialization into it's own class

---
+++ Dev/Dev2.Activities.Tests/InitializeAuditDB.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev2.Tests.Activities
+{
+    [TestClass]
+    static class InitializeAuditDB
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            string inputFilePath = "Dev2.Tests.Activities.auditDB.db";
+            string outputFilePath = Environment.ExpandEnvironmentVariables(@"%programdata%\Warewolf\Audits\auditDB.db");
+            if (File.Exists(outputFilePath))
+            {
+                File.Delete(outputFilePath);
+            }
+            if (!Directory.Exists(Path.GetDirectoryName(outputFilePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+            }
+            Stream inputFile = Assembly.GetExecutingAssembly().GetManifestResourceStream(inputFilePath);
+            Assert.IsNotNull(inputFile, inputFilePath + " file not found in " + string.Join(", ", Assembly.GetExecutingAssembly().GetManifestResourceNames()));
+            using (Stream input = inputFile)
+            {
+                using (Stream output = File.Create(outputFilePath))
+                {
+                    byte[] buffer = new byte[8192];
+
+                    int bytesRead;
+                    while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
