4910e907dc1d1e2a79f87731c4ee3ec8c78829a0
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-07-24 10:23:12 +0200

    3330: Add DsfUnZip GetState and test

--- Dev/Dev2.Activities.Tests/ActivityComparerTests/PathOperations/DsfUnZipComparerTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityComparerTests/PathOperations/DsfUnZipComparerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Dev2.Common.State;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Unlimited.Applications.BusinessDesignStudio.Activities;
 
@@ -363,6 +365,109 @@ namespace Dev2.Tests.Activities.ActivityComparerTests.PathOperations
             var @equals = folderRead.Equals(folderRead1);
             //---------------Test Result -----------------------
             Assert.IsFalse(@equals);
+        }
+
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory("DsfUnZip_GetState")]
+        public void DsfUnZip_GetState_ReturnsStateVariable()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var inputPath = @"c:\OldFile.txt";
+            var username = "[[username]]";
+            var privateKeyFile = "[[KeyFile]]";
+            var outputPath = @"c:\OldFile_Zip.txt";
+            var destinationUsername = "[[destinationUsername]]";
+            var destinationPrivateKeyFile = "[[destinationPrivateKeyFile]]";
+            var overwrite = false;
+            var result = "[[result]]";
+
+            var zip = new DsfUnZip()
+            {
+                UniqueID = uniqueId,
+                InputPath = inputPath,
+                Username = username,
+                PrivateKeyFile = privateKeyFile,
+                OutputPath = outputPath,
+                DestinationUsername = destinationUsername,
+                DestinationPrivateKeyFile = destinationPrivateKeyFile,
+                Overwrite = overwrite,
+                Result = result
+            };
+
+            //------------Execute Test---------------------------
+            var stateItems = zip.GetState();
+            Assert.AreEqual(8, stateItems.Count());
+
+            var expectedResults = new[]
+            {
+               new StateVariable
+                {
+                    Name = "InputPath",
+                    Value = inputPath,
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "Username",
+                    Value = username,
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "PrivateKeyFile",
+                    Value = privateKeyFile,
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "OutputPath",
+                    Value = outputPath,
+                    Type = StateVariable.StateType.Output
+                },
+                new StateVariable
+                {
+                    Name = "DestinationUsername",
+                    Value = destinationUsername,
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "DestinationPrivateKeyFile",
+                    Value = destinationPrivateKeyFile,
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "Overwrite",
+                    Value = overwrite.ToString(),
+                    Type = StateVariable.StateType.Input
+                },
+                new StateVariable
+                {
+                    Name = "Result",
+                    Value = result,
+                    Type = StateVariable.StateType.Output
+                }
+            };
+
+            var iter = zip.GetState().Select(
+                (item, index) => new
+                {
+                    value = item,
+                    expectValue = expectedResults[index]
+                }
+                );
+
+            //------------Assert Results-------------------------
+            foreach (var entry in iter)
+            {
+                Assert.AreEqual(entry.expectValue.Name, entry.value.Name);
+                Assert.AreEqual(entry.expectValue.Type, entry.value.Type);
+                Assert.AreEqual(entry.expectValue.Value, entry.value.Value);
+            }
+
         }
     }
 }
