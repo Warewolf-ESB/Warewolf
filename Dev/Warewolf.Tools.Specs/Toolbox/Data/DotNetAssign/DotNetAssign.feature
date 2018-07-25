0b6f356989fa4a061a98e1bc6cdd33d67675bd72
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-07-25 07:34:01 +0200

    3697: Add new DotNet Assign spec

---
+++ Dev/Warewolf.Tools.Specs/Toolbox/Data/DotNetAssign/DotNetAssign.feature
@@ -0,0 +1,23 @@
+﻿Feature: DotNetAssign
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Assign multiple variables to the same recordset
+	Given I dotnetassign the value "Kim" to a variable "[[person().name]]"
+	And I dotnetassign the value "bob" to a variable "[[person().name]]"
+	And I dotnetassign the value "jack" to a variable "[[person().name]]"
+	When the dotnetassign tool is executed
+	And the execution has "NO" error
+	And the debug inputs as
+	| # | Variable            | New Value |
+	| 1 | [[person().name]] = | Kim       |
+	| 2 | [[person().name]] = | bob       |
+	| 3 | [[person().name]] = | jack      |
+	And the debug output as
+    | # |                     |
+    | 1 | [[rec(1).set]] = 10 |
+    | 2 | [[rec(2).set]] = 20 |
+    | 3 | [[rec(3).set]] = 30 |
+    | 4 | [[value]] = 30      |
---
+++ Dev/Warewolf.Tools.Specs/Toolbox/Data/DotNetAssign/DotNetAssignSteps.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+using Warewolf.Tools.Specs.BaseTypes;
+
+namespace Warewolf.Tools.Specs.Toolbox.Data.DotNetAssign
+{
+    public class DotNetAssignSteps : RecordSetBases
+    {
+        readonly ScenarioContext scenarioContext;
+
+        public DotNetAssignSteps(ScenarioContext scenarioContext)
+            : base(scenarioContext)
+        {
+            if (scenarioContext == null)
+            {
+                throw new ArgumentNullException("scenarioContext");
+            }
+
+            this.scenarioContext = scenarioContext;
+        }
+
+        protected override void BuildDataList()
+        {
+            BuildShapeAndTestData();
+
+            scenarioContext.TryGetValue("fieldCollection", out List<ActivityDTO> fieldCollection);
+
+            var multiAssign = new DsfDotNetMultiAssignActivity();
+
+            TestStartNode = new FlowStep
+            {
+                Action = multiAssign
+            };
+
+            foreach (var field in fieldCollection)
+            {
+                multiAssign.FieldsCollection.Add(field);
+            }
+            scenarioContext.Add("activity", multiAssign);
+        }
+
+        [Given(@"I dotnetassign the value ""(.*)"" to a variable ""(.*)""")]
+        public void GivenIDotnetassignTheValueKimToAVariable(string value, string variable)
+        {
+            value = value.Replace('"', ' ').Trim();
+
+            if (value.StartsWith("="))
+            {
+                value = value.Replace("=", "");
+                value = string.Format("!~calculation~!{0}!~~calculation~!", value);
+            }
+
+            scenarioContext.TryGetValue("variableList", out List<Tuple<string, string>> variableList);
+
+            scenarioContext.TryGetValue("fieldCollection", out List<ActivityDTO> fieldCollection);
+
+            if (variableList == null)
+            {
+                variableList = new List<Tuple<string, string>>();
+                scenarioContext.Add("variableList", variableList);
+            }
+
+            if (fieldCollection == null)
+            {
+                fieldCollection = new List<ActivityDTO>();
+                scenarioContext.Add("fieldCollection", fieldCollection);
+            }
+
+            fieldCollection.Add(new ActivityDTO(variable, value, 1, true));
+        }
+
+        [When(@"the dotnetassign tool is executed")]
+        public void WhenTheDotnetassignToolIsExecuted()
+        {
+            BuildDataList();
+            var result = ExecuteProcess(isDebug: true, throwException: false);
+            scenarioContext.Add("result", result);
+        }
+
+    }
+}
