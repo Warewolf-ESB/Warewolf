ecfaa4922c6ce923b6ba483eaac89bc062ef0c2a
SalamiArmy <ultrakiff@gmail.com>
2018-07-30 06:30:49 +0200

    WOLF-3723 update these two ui test asserts' usage of ci remote container

--- Dev/Warewolf.UI.Tests/WorkflowTab/Tools/File FTP FTPS + SFTP/Create.cs
+++ Dev/Warewolf.UI.Tests/WorkflowTab/Tools/File FTP FTPS + SFTP/Create.cs
@@ -35,7 +35,7 @@ namespace Warewolf.UI.Tests.Tools
         public void PathCreateTool_FileSystemIntellisenseProvider_UITest()
         {
             _containerOps = TestLauncher.TryStartLocalCIRemoteContainer(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestResults"));
-            FileToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCreate.SmallViewContentCustom.FileOrFolderComboBox.TextEdit.Text = @"\\test-remotewarewolf\";
+            FileToolsUIMap.MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.WorkflowTab.WorkSurfaceContext.WorkflowDesignerView.DesignerView.ScrollViewerPane.ActivityTypeDesigner.WorkflowItemPresenter.Flowchart.PathCreate.SmallViewContentCustom.FileOrFolderComboBox.TextEdit.Text = @"\\rsaklfsvrpdc\";
             Assert.IsTrue(UIMap.MainStudioWindow.IntellisenseOptionsList.FirstOption.Exists, "No file system provided intellisense results are showing.");
         }
 
