88d75a0985d9f8471e3a289ae068ee107886d4ba
sanelemthembu <Sanele.Mthembu@RSAKLFSANELEM>
2018-07-31 10:57:23 +0200

    3840 : Created Workflow Resume stub

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/WorkflowManagementEndpointAbstract.cs
@@ -0,0 +1,77 @@
+﻿using Dev2.Common;
+using Dev2.Common.Interfaces.Core.DynamicServices;
+using Dev2.Common.Interfaces.Enums;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.DynamicServices.Objects;
+using Dev2.Workspaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev2.Runtime.ESB.Management
+{
+    public abstract class WorkflowManagementEndpointAbstract : IEsbManagementEndpoint
+    {
+        public Guid GetResourceID(Dictionary<string, StringBuilder> requestArgs)
+        {
+            requestArgs.TryGetValue("resourceID", out StringBuilder tmp);
+            if (tmp != null && Guid.TryParse(tmp.ToString(), out Guid resourceId))
+            {
+                return resourceId;
+            }
+
+            return Guid.Empty;
+        }
+
+        public AuthorizationContext GetAuthorizationContextForService() => AuthorizationContext.Administrator;
+
+        public virtual StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+        {
+            var serializer = new Dev2JsonSerializer();
+            try
+            {
+                Dev2Logger.Info("Workflow Resume Service", GlobalConstants.WarewolfInfo);
+
+                values.TryGetValue("resourceID", out StringBuilder resourceIdString);
+                if (resourceIdString == null)
+                {
+                    throw new InvalidDataContractException("resourceID is missing");
+                }
+                if (!Guid.TryParse(resourceIdString.ToString(), out Guid resourceId))
+                {
+                    throw new InvalidDataContractException("resourceID is not a valid GUID.");
+                }
+                var message = ExecuteImpl(serializer, resourceId);
+
+                return serializer.SerializeToBuilder(message);
+            }
+            catch (Exception err)
+            {
+                Dev2Logger.Error(err, GlobalConstants.WarewolfError);
+                var res = new CompressedExecuteMessage { HasError = true, Message = new StringBuilder(err.Message) };
+                return serializer.SerializeToBuilder(res);
+            }
+        }
+
+        public DynamicService CreateServiceEntry()
+        {
+            var newDs = new DynamicService { Name = HandlesType() };
+            var sa = new ServiceAction { Name = HandlesType(), ActionType = enActionType.InvokeManagementDynamicService, SourceMethod = HandlesType() };
+            newDs.Actions.Add(sa);
+            return newDs;
+        }
+
+
+        abstract protected CompressedExecuteMessage ExecuteImpl(Dev2JsonSerializer serializer, Guid resourceId);
+        /*var tests = TestCatalog.Fetch(resourceId)
+        var message = new CompressedExecuteMessage()
+        message.SetMessage(serializer.Serialize(tests))
+        message.HasError = false
+        return message*/
+        abstract public string HandlesType();
+    }
+}
---
+++ Dev/Dev2.Runtime.Tests/Services/WorkflowResumeTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dev2.Runtime.ESB.Management.Services;
+using Dev2.Common.Interfaces;
+using Dev2.Data;
+using Dev2.Runtime.ServiceModel.Data;
+using Dev2.Communication;
+using Moq;
+using Dev2.Workspaces;
+
+namespace Dev2.Tests.Runtime.Services
+{
+    [TestClass]
+    public class WorkflowResumeTests
+    {
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        [TestCategory("WorkflowResume_Execute")]
+        public void WorkflowResume_Execute_ExpectTestList()
+        {
+            //------------Setup for test--------------------------
+            var workflowResume = new WorkflowResume();
+            var ws = new Mock<IWorkspace>();
+            var resID = Guid.NewGuid();
+            var values = new Dictionary<string, StringBuilder>
+            {
+                { "resourceID", new StringBuilder(resID.ToString()) }
+            };
+            var serializer = new Dev2JsonSerializer();
+            var resourceID = Guid.NewGuid();
+            //------------Execute Test---------------------------
+            var res = workflowResume.Execute(values, ws.Object);
+            var msg = serializer.Deserialize<CompressedExecuteMessage>(res);
+            //------------Assert Results-------------------------
+            Assert.IsNotNull(msg);
+            Assert.IsFalse(msg.HasError);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        [TestCategory("WorkflowResume_Execute")]
+        public void WorkflowResume_Returns_HandleType_WorkflowResume()
+        {
+            //------------Setup for test--------------------------
+            var workflowResume = new WorkflowResume();
+            //------------Execute Test--------------------------- 
+            //------------Assert Results-------------------------
+            Assert.AreEqual("WorkflowResume", workflowResume.HandlesType());
+        }
+    }
+}
