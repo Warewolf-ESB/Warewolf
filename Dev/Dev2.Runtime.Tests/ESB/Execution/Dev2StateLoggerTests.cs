fabcd8d6d0679c89fc17cbbd10d9bd443c4e440f
Candice.Daniel <Candice.Daniel@RSAKLFCANDICE>
2018-08-02 10:03:22 +0200

    3707-Fixed the date Filter. At this stage unable to fix complexity as it is a linq expression

--- Dev/Dev2.Runtime.Tests/ESB/Execution/Dev2StateLoggerTests.cs
+++ Dev/Dev2.Runtime.Tests/ESB/Execution/Dev2StateLoggerTests.cs
@@ -331,7 +331,7 @@ namespace Dev2.Tests.Runtime.ESB.Execution
             _dev2StateAuditLogger.LogPreExecuteState(_activity.Object);
             // verify
             var str = expectedWorkflowId.ToString();
-            var results = Dev2StateAuditLogger.Query(item => true);
+            var results = Dev2StateAuditLogger.Query(a => a.WorkflowID == str);
             _dev2StateAuditLogger.Dispose();
 
             foreach (var item in results)
