330a14a880e7cb73507c7ad01469dcac4138b35d
Candice.Daniel <Candice.Daniel@RSAKLFCANDICE>
2018-08-27 16:04:23 +0200

    3044-change count of servers for test SharepointListDesignerViewModelBase_SharepointServerList_ShouldHaveServerListWithNewSharepointOption

--- Dev/Dev2.Activities.Designers.Tests/Sharepoint/SharepointDesignerViewModelBaseTests.cs
+++ Dev/Dev2.Activities.Designers.Tests/Sharepoint/SharepointDesignerViewModelBaseTests.cs
@@ -114,8 +114,9 @@ namespace Dev2.Activities.Designers.Tests.Sharepoint
             var sharepointServerList = sharepointListDesignerViewModelBase.SharepointServers;
             //------------Assert Results-------------------------
             Assert.IsNotNull(sharepointServerList);
-            Assert.AreEqual(1,sharepointServerList.Count);
-            Assert.AreEqual(sharepointListDesignerViewModelBase.GetNewSharepointSource, sharepointServerList[0]);
+            Assert.AreEqual(2,sharepointServerList.Count);
+            Assert.AreEqual(sharepointListDesignerViewModelBase.GetSelectSharepointSource, sharepointServerList[0]);
+            Assert.AreEqual(sharepointListDesignerViewModelBase.GetNewSharepointSource, sharepointServerList[1]);
         }
 
         static TestSharepointListDesignerViewModelBase CreateSharepointListDesignerViewModel()
