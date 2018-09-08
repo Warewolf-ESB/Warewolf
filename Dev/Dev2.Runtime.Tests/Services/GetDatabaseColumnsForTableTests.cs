f8c7134e731f66df22adeb52e4b6721a73dd92a0
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-09-08 20:35:34 +0200

    WOLF-4017 test still pointing to old container server

--- Dev/Dev2.Runtime.Tests/Services/GetDatabaseColumnsForTableTests.cs
+++ Dev/Dev2.Runtime.Tests/Services/GetDatabaseColumnsForTableTests.cs
@@ -322,7 +322,7 @@ namespace Dev2.Tests.Runtime.Services
                 ResourceID = Guid.NewGuid(),
                 ResourceName = "Dev2TestingDB",
                 DatabaseName = "Dev2TestingDB",
-                Server = "TEST-MSSQL",
+                Server = "rsaklfsvrdev.dev2.local",
                 AuthenticationType = AuthenticationType.User,
                 ServerType = enSourceType.SqlDatabase,
                 ReloadActions = true,
