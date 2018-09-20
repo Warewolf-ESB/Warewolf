8c2b4ed7024ad9dd48426e23281d0b92a3a35913
Rory McGuire <rory.mcguire@dev2.co.za>
2018-09-20 09:20:57 +0200

    4078 change ConfigurationManagerWrapper to use a customer json file serverSettings.json

--- Dev/Dev2.Common/Wrappers/ConfigurationManagerWrapper.cs
+++ Dev/Dev2.Common/Wrappers/ConfigurationManagerWrapper.cs
@@ -1,4 +1,6 @@
-﻿using Dev2.Common.Interfaces.Wrappers;
+﻿using Dev2.Common.Interfaces.Core;
+using Dev2.Common.Interfaces.Wrappers;
+using Newtonsoft.Json;
 using System;
 using System.Configuration;
 
@@ -7,6 +9,11 @@ namespace Dev2.Common.Wrappers
     class ConfigurationManagerWrapper : IConfigurationManager
     {
         private readonly object _settingLock = new object();
+        private readonly FileWrapper _fileWrapper;
+        public ConfigurationManagerWrapper()
+        {
+            _fileWrapper = new FileWrapper();
+        }
         public string this[params string[] args]
         {
             get
@@ -19,7 +26,17 @@ namespace Dev2.Common.Wrappers
                 var defaultValue = args[1];
                 lock (_settingLock)
                 {
-                    var setting = ConfigurationManager.AppSettings.Get(settingName);
+                    ServerSettingsData settings = null;
+                    try
+                    {
+                        var text = _fileWrapper.ReadAllText("serverSettings.json");
+                        settings = JsonConvert.DeserializeObject<ServerSettingsData>(text);
+                    } catch {
+                        settings = new ServerSettingsData();
+                    }
+                    var prop = typeof(ServerSettingsData).GetProperty(settingName);
+                    var setting = prop.GetValue(settings)?.ToString();
+                    //var setting = ConfigurationManager.AppSettings.Get(settingName)
                     return string.IsNullOrWhiteSpace(setting) ? defaultValue : setting;
                 }
             }
@@ -32,17 +49,22 @@ namespace Dev2.Common.Wrappers
                 var settingName = args[0];
                 lock (_settingLock)
                 {
-                    var setting = ConfigurationManager.AppSettings.Get(settingName);
-                    if (setting is null)
-                    {
-                        ConfigurationManager.AppSettings.Add(settingName, value);
-                    }
-                    else
-                    {
-                        ConfigurationManager.AppSettings.Set(settingName, value);
-                    }
+                    var settings = Config.Server.Get();
+                    var prop = typeof(ServerSettingsData).GetProperty(settingName);
+                    prop.SetValue(settings, value);
+                    settings.Save();
                 }
             }
+        }
+    }
+
+    static class ServerSettingsDataExtensionMethods
+    {
+        public static void Save(this ServerSettingsData data)
+        {
+            var fileWrapper = new FileWrapper();
+            var json = JsonConvert.SerializeObject(data);
+            fileWrapper.WriteAllText("serverSettings.json", json);
         }
     }
 }
