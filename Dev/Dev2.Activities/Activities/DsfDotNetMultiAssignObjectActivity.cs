0d3ce30df97235dee7c44d3619c98fa699802f8d
Rory McGuire <rory.mcguire@dev2.co.za>
2018-09-28 11:41:24 +0200

    4112 multi assign tools should not treat json object arrays as recordsets during debug output

--- Dev/Dev2.Activities/Activities/DsfDotNetMultiAssignObjectActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfDotNetMultiAssignObjectActivity.cs
@@ -158,7 +158,7 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                 }
                 if (dataObject.IsDebugMode())
                 {
-                    if (DataListUtil.IsValueRecordset(assignValue.Name) && DataListUtil.GetRecordsetIndexType(assignValue.Name) == enRecordsetIndexType.Blank)
+                    if (DataListUtil.IsValueRecordset(assignValue.Name) && DataListUtil.GetRecordsetIndexType(assignValue.Name) == enRecordsetIndexType.Blank && !assignValue.Name.Contains(DataListUtil.ObjectStartMarker))
                     {
                         var length = dataObject.Environment.GetObjectLength(DataListUtil.ExtractRecordsetNameFromValue(assignValue.Name));
                         assignValue = new AssignValue(DataListUtil.ReplaceObjectBlankWithIndex(assignValue.Name, length), assignValue.Value);
