544e8a74abff20ca31da2264430ad971b980db37
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-10-22 07:25:01 +0200

    WOLF-4148 include job name opencover coverage output file name to help merging many job coverage outputs together

--- Dev/Warewolf.Launcher/ITestCoverageRunner.cs
+++ Dev/Warewolf.Launcher/ITestCoverageRunner.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Warewolf.Launcher
 {
@@ -10,7 +7,7 @@ namespace Warewolf.Launcher
     {
         string CoverageToolPath { get; set; }
         string RunCoverageTool(string TestsResultsPath, string JobName, List<string> TestAssembliesDirectories);
-        string StartServiceWithCoverage(string ServerPath, string OutputDirectory, bool IsExistingService);
-        void StartProcessWithCoverage(string processPath, string OutputDirectory);
+        string StartServiceWithCoverage(string ServerPath, string TestsResultsPath, string JobName, bool IsExistingService);
+        void StartProcessWithCoverage(string processPath, string TestsResultsPath, string JobName);
     }
 }
