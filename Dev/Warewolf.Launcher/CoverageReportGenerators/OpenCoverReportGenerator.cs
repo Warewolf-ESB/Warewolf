837a0f8cac356fd7f150bb637afc813bbe8cbb52
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-10-24 15:16:20 +0200

    WOLF-4148 fix for some server snapshot reports not generating

--- Dev/Warewolf.Launcher/CoverageReportGenerators/OpenCoverReportGenerator.cs
+++ Dev/Warewolf.Launcher/CoverageReportGenerators/OpenCoverReportGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace Warewolf.Launcher.TestCoverageMergers
 {
@@ -13,9 +14,41 @@ namespace Warewolf.Launcher.TestCoverageMergers
         public void GenerateCoverageReport(string DestinationFilePath, string LogFilePath)
         {
             var SnapshotPaths = Directory.GetFiles(Path.GetDirectoryName(DestinationFilePath), "*OpenCover Output.xml", SearchOption.AllDirectories).ToList();
+            foreach (var snapshot in SnapshotPaths)
+            {
+                if (!WaitForFileContent(snapshot))
+                {
+                    throw new Exception($"Snapshot \"{snapshot}\" contains no content.");
+                }
+            }
             var DotCoverSnapshotsString = String.Join("\";\"", SnapshotPaths);
             Console.WriteLine($"Writing coverage report to {DestinationFilePath} with {ToolPath}");
             ProcessUtils.RunFileInThisProcess(ToolPath, $"-reports:\"{DotCoverSnapshotsString}\" -targetdir:\"{DestinationFilePath}\"");
+        }
+
+        bool WaitForFileContent(string snapshot)
+        {
+            if (!File.Exists(snapshot))
+            {
+                return false;
+            }
+            var hasContent = false;
+            var RetryCount = 0;
+            while (!hasContent && RetryCount < 100)
+            {
+                RetryCount++;
+                try
+                {
+                    var content = File.ReadAllBytes(snapshot);
+                    hasContent = content.Length > 0;
+                }
+                catch
+                {
+                    Console.WriteLine($"Still waiting for {snapshot} file to contain something.");
+                    Thread.Sleep(3000);
+                }
+            }
+            return hasContent;
         }
     }
 }
