ae17f01cae4c5d781552a54239b4379b72163fc2
Pieter.Terblanche <pete.terblanche@gmail.com>
2018-11-01 13:48:38 +0200

    4162: Add error dialog specs

--- Dev/warewolfBI/ClientApp/src/app/core/error-dialog/error-dialog.component.ts
+++ Dev/warewolfBI/ClientApp/src/app/core/error-dialog/error-dialog.component.ts
@@ -1,18 +1,23 @@
-import { Component, Inject, Injectable } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+import { Component, Inject, OnInit, Injectable } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-error-dialog',
   templateUrl: './error-dialog.component.html',
   styleUrls: ['./error-dialog.component.scss']
 })
-export class ErrorDialogComponent {
 
-  //constructor(private dialogRef: MatDialogRef, @Inject(MAT_DIALOG_DATA) public data: any) {}
+export class ErrorDialogComponent implements OnInit {
 
-  public closeDialog() {
-    //this.dialogRef.close();
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<ErrorDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {}
+
+  close() {
+    this.dialogRef.close();
   }
-
-
 }
