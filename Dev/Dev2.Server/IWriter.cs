b80d510774726593cd2a615c453932dc36ae8109
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2018-12-19 16:22:51 +0200

    4409-Writer class

---
+++ Dev/Dev2.Server/IWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Dev2
+{
+    public interface IWriter
+    {
+       void Write(string message);
+       void WriteLine(string message);
+    }
+}
---
+++ Dev/Dev2.Server/Writer.cs
@@ -0,0 +1,35 @@
+﻿using Dev2.Common;
+using System;
+
+namespace Dev2
+{
+    public class Writer : IWriter
+    {
+        public void Write(string message)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.Write(message);
+                Dev2Logger.Info(message, GlobalConstants.WarewolfInfo);
+            }
+            else
+            {
+                Dev2Logger.Info(message, GlobalConstants.WarewolfInfo);
+            }
+        }
+
+        public void WriteLine(string message)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine(message);
+                Dev2Logger.Info(message, GlobalConstants.WarewolfInfo);
+            }
+            else
+            {
+                Dev2Logger.Info(message, GlobalConstants.WarewolfInfo);
+            }
+
+        }
+    }
+}
