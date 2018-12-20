f87f7bea9db18d6083e58973d186f9efdb3d3034
Rory McGuire <rory.mcguire@dev2.co.za>
2018-12-20 11:35:36 +0200

    4409 TimerWrapperFactory

--- Dev/Dev2.Common.Interfaces/Wrappers/ITimer.cs
+++ Dev/Dev2.Common.Interfaces/Wrappers/ITimer.cs
@@ -8,10 +8,16 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 using System;
+using System.Threading;
 
 namespace Dev2.Common.Interfaces.Wrappers
 {
     public interface ITimer : IDisposable
     {
+    }
+
+    public interface ITimerFactory
+    {
+        ITimer New(TimerCallback callback, object state, int dueTime, int period);
     }
 }
