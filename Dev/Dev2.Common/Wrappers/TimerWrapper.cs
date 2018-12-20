a6b8bd44757aff425550d7dd814c138878e8f134
Rory McGuire <rory.mcguire@dev2.co.za>
2018-12-20 09:37:55 +0200

    4409 TimerWrapper

---
+++ Dev/Dev2.Common/Wrappers/TimerWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Dev2.Common.Interfaces.Wrappers;
+using System.Threading;
+
+namespace Dev2.Common.Wrappers
+{
+    public class TimerWrapper : ITimer
+    {
+        Timer _timer;
+
+        public TimerWrapper(TimerCallback callback, object state, int dueTime, int period)
+        {
+            _timer = new Timer(callback, state, dueTime, period);
+        }
+
+        public void Dispose()
+        {
+            if (_timer is null)
+            {
+                return;
+            }
+
+            _timer.Dispose();
+            _timer = null;
+        }
+    }
+}
