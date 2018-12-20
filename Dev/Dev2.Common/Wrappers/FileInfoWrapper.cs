b158d6faacc1a575e921365500aed6811acfc0ee
Rory McGuire <rory.mcguire@dev2.co.za>
2018-12-20 09:38:56 +0200

    4409 FileInfoWrapper

---
+++ Dev/Dev2.Common/Wrappers/FileInfoWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Dev2.Common.Interfaces.Wrappers;
+
+namespace Dev2.Common.Wrappers
+{
+    public class FileInfoWrapper : IFileInfo
+    {
+        readonly FileInfo _fileInfo;
+
+        public FileInfoWrapper(FileInfo fileInfo)
+        {
+            _fileInfo = fileInfo;
+        }
+
+        public DateTime CreationTime => _fileInfo.CreationTime;
+
+        public void Delete()
+        {
+            _fileInfo.Delete();
+        }
+    }
+}
