16a02807f317f48b1370b3d6c091a03536cfe0ac
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2018-12-21 07:27:57 +0200

    4409-Code Cleanup

--- Dev/Dev2.Server.Tests/WebServerConfigurationTests.cs
+++ Dev/Dev2.Server.Tests/WebServerConfigurationTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Dev2.Common;
 using System.Configuration;
-using Dev2.Runtime.WebServer;
-using System.Net;
 
 namespace Dev2.Server.Tests
 {
@@ -19,17 +24,12 @@ namespace Dev2.Server.Tests
         public void WebServerConfigurationTests_Execute__IsWebServerEnabled_False()
         {
             //----------------Arrange--------------------
-            bool _isWebServerSslEnabled;
             string expectedUsageStats = "33.33";
             string expectedPort = "44";
             string expectedSslPort = "55";
             ConfigurationManager.AppSettings.Set("CollectUsageStats", "33.33");
             ConfigurationManager.AppSettings.Set("webServerPort", expectedPort);
             ConfigurationManager.AppSettings.Set("webServerSslPort", expectedSslPort);
-
-            //var _isWebServerEnabled = false;
-            //bool.TryParse(ConfigurationManager.AppSettings["webServerEnabled"], out _isWebServerEnabled);
-            //bool.TryParse(ConfigurationManager.AppSettings["webServerSslEnabled"], out _isWebServerSslEnabled);
 
             var mockWriter = new Mock<IWriter>();
             mockWriter.Setup(a => a.Write(It.IsAny<string>())).Verifiable();
@@ -47,26 +47,23 @@ namespace Dev2.Server.Tests
         public void WebServerConfigurationTests_Execute__IsWebServerEnabled_True()
         {
             //----------------Arrange--------------------
-            bool _isWebServerSslEnabled;
             string expectedUsageStats = "33.33";
             string expectedPort = "44";
             string expectedSslPort = "55";
-
-            string expectedHttpEndpoint = "http://*:44/";
+            
             ConfigurationManager.AppSettings.Set("CollectUsageStats", "33.33");
             ConfigurationManager.AppSettings.Set("webServerPort", expectedPort);
             ConfigurationManager.AppSettings.Set("webServerSslPort", expectedSslPort);
-
-           // var _isWebServerEnabled = true;
+            
             ConfigurationManager.AppSettings.Set("webServerEnabled", "True");
             ConfigurationManager.AppSettings.Set("webServerSslEnabled", "True");
 
             var mockWriter = new Mock<IWriter>();
             mockWriter.Setup(a => a.Write(It.IsAny<string>())).Verifiable();
             //----------------Act------------------------
-            var ss = new WebServerConfiguration(mockWriter.Object);
-            ss.Execute();
-            var endPoints = ss.EndPoints;
+            var webServerConfig = new WebServerConfiguration(mockWriter.Object);
+            webServerConfig.Execute();
+            var endPoints = webServerConfig.EndPoints;
             //----------------Assert---------------------
             Assert.AreEqual(expectedPort, GlobalConstants.WebServerPort);
             Assert.AreEqual(expectedSslPort, GlobalConstants.WebServerSslPort);
