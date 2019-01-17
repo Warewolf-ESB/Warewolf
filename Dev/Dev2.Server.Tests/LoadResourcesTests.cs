1e219c4133248db1e0883cead54bfd1e42855f7c
Pieter.Terblanche <pete.terblanche@gmail.com>
2019-01-17 08:56:06 +0200

    4409 Fix for reference issue

--- Dev/Dev2.Server.Tests/LoadResourcesTests.cs
+++ Dev/Dev2.Server.Tests/LoadResourcesTests.cs
@@ -8,13 +8,11 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using Dev2.Common;
 using Dev2.Common.Interfaces.Scheduler.Interfaces;
 using Dev2.Common.Interfaces.Wrappers;
 using Dev2.Runtime.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.IO;
 using System.Reflection;
 
 namespace Dev2.Server.Tests
