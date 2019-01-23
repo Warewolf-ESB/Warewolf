44a5df5e3b219a18db13553d401b4307a88be1c9
Pieter.Terblanche <pete.terblanche@gmail.com>
2019-01-23 15:16:30 +0200

    4429 Add new Warewolf.Studio.Models.Tests project

---
+++ Dev/Warewolf.Studio.Models.Tests/ExchangeServiceModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warewolf.Studio.Models.Tests
+{
+    [TestClass]
+    public class ExchangeServiceModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
