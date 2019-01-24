d7bf7528e777ea87dab57c9acbba832ddf3694eb
Ashley Lewis <ashley.lewis@dev2.co.za>
2019-01-24 13:50:29 +0200

    4447: still fixing test broken by my changes to allow server to start in a container

--- Dev/Dev2.Server.Tests/LoadResourcesTests.cs
+++ Dev/Dev2.Server.Tests/LoadResourcesTests.cs
@@ -14,6 +14,7 @@ using Dev2.Common.Interfaces.Wrappers;
 using Dev2.Runtime.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.IO;
 using System.Reflection;
 
 namespace Dev2.Server.Tests
@@ -279,7 +280,8 @@ namespace Dev2.Server.Tests
             var mockResourceCatalogFactory = new Mock<IResourceCatalogFactory>();
             var mockDirectoryHelper = new Mock<IDirectoryHelper>();
 
-            mockDirectory.Setup(o => o.Exists(It.IsAny<string>())).Returns(true);
+            mockDirectory.Setup(o => o.Exists(EnvironmentVariables.TestPath)).Returns(false);
+            mockDirectory.Setup(o => o.Exists(Path.Combine(EnvironmentVariables.ApplicationPath, "Tests"))).Returns(true);
             mockResourceCatalogFactory.Setup(o => o.New()).Returns(mockResourceCatalog.Object);
             //------------------Act-------------------
             var loadResources = new LoadResources(resourceDirectory, mockWriter.Object, mockDirectory.Object, mockResourceCatalogFactory.Object, mockDirectoryHelper.Object);
