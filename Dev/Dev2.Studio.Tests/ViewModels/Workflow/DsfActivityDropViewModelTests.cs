bc39f26959121a637144114f271637b20044c444
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-01-28 16:17:38 +0200

    4429-DsfActivityDropViewModel tests

--- Dev/Dev2.Studio.Tests/ViewModels/Workflow/DsfActivityDropViewModelTests.cs
+++ Dev/Dev2.Studio.Tests/ViewModels/Workflow/DsfActivityDropViewModelTests.cs
@@ -12,12 +12,9 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
     [TestClass]
     public class DsfActivityDropViewModelTests
     {
-        const string testOwner = "Siphamandla Dube";
-        const string testCategory = nameof(DsfActivityDropViewModel);
-
         [TestMethod]
-        [Owner(testOwner)]
-        [TestCategory(testCategory)]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
         public void DsfActivityDropViewModel_Okay_SelectedItem_NotNull_ExpectDialogResult_Okay()
         {
             //---------------------Arrange---------------------
@@ -42,18 +39,15 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
         }
 
         [TestMethod]
-        [Owner(testOwner)]
-        [TestCategory(testCategory)]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
         public void DsfActivityDropViewModel_Okay_SelectedItem_IsNull_ExpectDialogResult_Cancel()
         {
             //---------------------Arrange---------------------
             var mockServer = new Mock<IServer>();
             var mockExplorerViewModel = new Mock<IExplorerViewModel>();
             var mockExplorerTreeItem = new Mock<IExplorerTreeItem>();
-
-            //mockExplorerViewModel.Setup(o => o.SelectedItem).Returns();
-            //mockExplorerViewModel.Setup(o => o.SelectedItem.Server).Returns(mockServer.Object);
-
+            
             var dialogResult = new ViewModelDialogResults();
             bool closeRequested = false;
             //---------------------Act-------------------------
@@ -69,8 +63,8 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
         }
 
         [TestMethod]
-        [Owner(testOwner)]
-        [TestCategory(testCategory)]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
         public void DsfActivityDropViewModel_SelectedResourceModel_ActivityType_Workflow_ExpectDialogResult_Cancel()
         {
             //---------------------Arrange---------------------
@@ -98,8 +92,8 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
         }
 
         [TestMethod]
-        [Owner(testOwner)]
-        [TestCategory(testCategory)]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
         public void DsfActivityDropViewModel_CanOkay_ActivityType_Workflow_ExpectDialogResult_Cancel()
         {
             //---------------------Arrange---------------------
@@ -125,8 +119,8 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
         }
 
         [TestMethod]
-        [Owner(testOwner)]
-        [TestCategory(testCategory)]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
         public void DsfActivityDropViewModel_CancelCommand_ActivityType_Workflow_ExpectDialogResult_Okay()
         {
             //---------------------Arrange---------------------
@@ -145,8 +139,44 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
             //---------------------Act-------------------------
             using (var dsfActivityDropViewModel = new TestDsfActivityDropViewModel(mockExplorerViewModel.Object, Enums.enDsfActivityType.Workflow, sender, mockExplorerTreeItem.Object))
             {
-                //dsfActivityDropViewModel.TestSingleEnvironmentExplorerViewModel_SelectedItemChanged(sender, mockExplorerTreeItem.Object);
                 cancelCommand = dsfActivityDropViewModel.CancelCommand;
+                cancelCommand.Execute(null);
+                closeRequested = dsfActivityDropViewModel.CloseRequested;
+                tittle = dsfActivityDropViewModel.Title;
+                imageSource = dsfActivityDropViewModel.ImageSource;
+            }
+            //---------------------Assert----------------------
+            Assert.AreEqual(ViewModelDialogResults.Okay, dialogResult);
+            Assert.IsTrue(closeRequested);
+            Assert.AreEqual("Select A Service", tittle);
+            Assert.AreEqual("Workflow-32", imageSource);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(DsfActivityDropViewModel))]
+        public void DsfActivityDropViewModel_Okay_ActivityType_Workflow_ExpectDialogResult_Okayq()
+        {
+            //---------------------Arrange---------------------
+            var mockServer = new Mock<IServer>();
+            var mockExplorerViewModel = new Mock<IExplorerViewModel>();
+            var mockExplorerTreeItem = new Mock<IExplorerTreeItem>();
+            var mockServerRepository = new Mock<IServerRepository>();
+            var mockExplorerItemViewModel = new Mock<IExplorerItemViewModel>();
+
+            //mockExplorerViewModel.Setup(o => o.SelectedItem.Server).Returns(mockServer.Object);
+            //mockExplorerViewModel.Setup(o => o.SelectedItem.Server.EnvironmentID).Returns(null);
+            //mockExplorerTreeItem.Setup(o => o.SelectItem(Guid.NewGuid(), ()=> )).re
+
+            var dialogResult = new ViewModelDialogResults();
+            bool closeRequested = false;
+            var tittle = "";
+            var imageSource = "";
+            object sender = new object();
+            //---------------------Act-------------------------
+            using (var dsfActivityDropViewModel = new TestDsfActivityDropViewModel(mockExplorerViewModel.Object, Enums.enDsfActivityType.Workflow, sender, null, null))
+            {
+                dsfActivityDropViewModel.Okay();
                 closeRequested = dsfActivityDropViewModel.CloseRequested;
                 tittle = dsfActivityDropViewModel.Title;
                 imageSource = dsfActivityDropViewModel.ImageSource;
@@ -156,17 +186,25 @@ namespace Dev2.Studio.Tests.ViewModels.Workflow
             Assert.IsFalse(closeRequested);
             Assert.AreEqual("Select A Service", tittle);
             Assert.AreEqual("Workflow-32", imageSource);
-            //Assert.AreEqual(mockExplorerTreeItem.Object, selectedExplorerItemModel);
         }
 
         class TestDsfActivityDropViewModel : DsfActivityDropViewModel
         {
+            private object sender;
+            private IExplorerTreeItem @object;
+
             public TestDsfActivityDropViewModel(IExplorerViewModel explorerViewModel, enDsfActivityType dsfActivityType) 
                 : base(explorerViewModel, dsfActivityType)
             {
             }
 
-            public TestDsfActivityDropViewModel(IExplorerViewModel explorerViewModel, enDsfActivityType dsfActivityType, object sender, IExplorerTreeItem e) 
+            public TestDsfActivityDropViewModel(IExplorerViewModel explorerViewModel, enDsfActivityType dsfActivityType, object sender, IExplorerTreeItem @object) : this(explorerViewModel, dsfActivityType)
+            {
+                this.sender = sender;
+                this.@object = @object;
+            }
+
+            public TestDsfActivityDropViewModel(IExplorerViewModel explorerViewModel, enDsfActivityType dsfActivityType, object sender, IExplorerTreeItem e, IServerRepository @object) 
                 : base(explorerViewModel, dsfActivityType)
             {
             }
