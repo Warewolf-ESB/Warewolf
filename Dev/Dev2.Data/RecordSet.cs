1556e8c90558866e21588bed2566398b13417d18
Pieter.Terblanche <pete.terblanche@gmail.com>
2019-01-29 14:54:02 +0200

    4429 ExcludeFromCodeCoverage

--- Dev/Dev2.Data/RecordSet.cs
+++ Dev/Dev2.Data/RecordSet.cs
@@ -1,4 +1,15 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Dev2.Data
 {
@@ -7,6 +18,7 @@ namespace Dev2.Data
         Dictionary<int, List<IScalar>> Columns { get; set; }
     }
 
+    [ExcludeFromCodeCoverage]
     public class RecordSet:Scalar, IRecordSet
     {
         public Dictionary<int, List<IScalar>> Columns { get; set; }
