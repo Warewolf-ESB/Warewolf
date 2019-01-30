68ac33bf3b6411ea4f508ef003c768cd3e0f056b
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-01-30 09:02:28 +0200

    4429-ResourceCriteria code cleanup

--- Dev/Dev2.Core.Tests/Common/ResourceCriteriaTests.cs
+++ Dev/Dev2.Core.Tests/Common/ResourceCriteriaTests.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
 using Dev2.Common;
 using Dev2.Common.Interfaces.Versioning;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -62,7 +72,7 @@ namespace Dev2.Core.Tests
             //-----------------Arrange-------------------
             var resourceCriteria = new ResourceCriteria();
 
-            var resourceType = "TestResourcePath";
+            var resourceType = "TestResourceType";
             //-----------------Act-----------------------
             resourceCriteria.ResourceType = resourceType;
             //-----------------Assert--------------------
