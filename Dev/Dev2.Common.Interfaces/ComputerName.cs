3e421f434771f6085ce6f1fa5b42b75518474569
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-01-30 07:52:23 +0200

    4429-ComputerName Tests

--- Dev/Dev2.Common.Interfaces/ComputerName.cs
+++ Dev/Dev2.Common.Interfaces/ComputerName.cs
@@ -8,21 +8,13 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System.Diagnostics.CodeAnalysis;
 
 namespace Dev2.Common.Interfaces
 {
-    [ExcludeFromCodeCoverage]
     public class ComputerName : IComputerName
     {
         public string Name { get; set; }
-
-        /// <summary>
-        /// Returns a string that represents the current object.
-        /// </summary>
-        /// <returns>
-        /// A string that represents the current object.
-        /// </returns>
+        
         public override string ToString() => Name;
     }
 }
