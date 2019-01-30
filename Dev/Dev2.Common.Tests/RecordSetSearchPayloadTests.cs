d3195e43b09acaba12400c783c687bab89b79db2
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-01-30 15:53:36 +0200

    4429-RecordSetSearchPayload Tests

---
+++ Dev/Dev2.Common.Tests/RecordSetSearchPayloadTests.cs
@@ -0,0 +1,51 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2.Common.Tests
+{
+    [TestClass]
+    public class RecordSetSearchPayloadTests
+    {
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(RecordSetSearchPayload))]
+        public void RecordSetSearchPayload_NoParamsConstractor_SetProperty_ExpectSetValue()
+        {
+            //--------------------Arrange--------------------
+            var testIndex = 0;
+            var testPayload = "TestPayload";
+
+            var recordSetSearchPayload = new RecordSetSearchPayload();
+            //--------------------Act------------------------
+            recordSetSearchPayload.Index = testIndex;
+            recordSetSearchPayload.Payload = testPayload;
+            //--------------------Assert---------------------
+            Assert.AreEqual(testIndex, recordSetSearchPayload.Index);
+            Assert.AreEqual(testPayload, recordSetSearchPayload.Payload);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(RecordSetSearchPayload))]
+        public void RecordSetSearchPayload_WithParamsConstractor_SetProperty_ExpectSetValue()
+        {
+            //--------------------Arrange--------------------
+            var testIndex = 0;
+            var testPayload = "TestPayload";
+            //--------------------Act------------------------
+            var recordSetSearchPayload = new RecordSetSearchPayload(testIndex, testPayload);
+            //--------------------Assert---------------------
+            Assert.AreEqual(testIndex, recordSetSearchPayload.Index);
+            Assert.AreEqual(testPayload, recordSetSearchPayload.Payload);
+        }
+    }
+}
