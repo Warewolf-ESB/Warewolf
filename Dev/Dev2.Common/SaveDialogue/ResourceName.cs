b04406fdda1415235569991f1da5be7f6a77639f
Candice.Daniel <Candice.Daniel@RSAKLFCANDICE>
2019-01-31 15:43:47 +0200

    4429-Moved this file from interfaces to common. Had to change the namespace used in 29 files to propagate the change to the solution I also renamed the physical file to the name of the class

---
+++ Dev/Dev2.Common/SaveDialogue/ResourceName.cs
@@ -0,0 +1,27 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+namespace Dev2.Common.SaveDialog
+{
+    public class ResourceName
+    {
+        readonly string _name;
+        readonly string _path;
+
+        public ResourceName(string path, string name)
+        {
+            _path = path;
+            _name = name;
+        }
+
+        public string Name => _name;
+
+        public string Path => _path;
+    }
+}
