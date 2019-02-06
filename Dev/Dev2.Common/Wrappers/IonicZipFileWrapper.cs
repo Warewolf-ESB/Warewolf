edd75b3c4865d5bb2050bf89dc9795ec9767fbaa
Pieter.Terblanche <pete.terblanche@gmail.com>
2019-02-06 15:39:53 +0200

    4429 ZipEntryWrapper

--- Dev/Dev2.Common/Wrappers/IonicZipFileWrapper.cs
+++ Dev/Dev2.Common/Wrappers/IonicZipFileWrapper.cs
@@ -1,5 +1,16 @@
-﻿using System.Collections;
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Dev2.Common.Interfaces.Wrappers;
 using Ionic.Zip;
@@ -48,6 +59,7 @@ namespace Dev2.Common.Wrappers
         public IIonicZipFileWrapper Read(Stream stream) => new IonicZipFileWrapper(ZipFile.Read(stream));
     }
 
+    [ExcludeFromCodeCoverage]
     class ZipEntryWrapper : IZipEntry
     {
         readonly ZipEntry _zipEntry;
