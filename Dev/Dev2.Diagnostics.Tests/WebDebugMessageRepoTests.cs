a61c491680dda96cd3a63443511a252a03049396
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-02-12 10:50:05 +0200

    4494-WebDebugMessageRepo Tests

--- Dev/Dev2.Diagnostics.Tests/WebDebugMessageRepoTests.cs
+++ Dev/Dev2.Diagnostics.Tests/WebDebugMessageRepoTests.cs
@@ -1,6 +1,19 @@
-﻿using System;
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2018 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Threading.Tasks;
+using Dev2.Common.Interfaces.Diagnostics.Debug;
 using Dev2.Diagnostics.Debug;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Dev2.Diagnostics.Test
 {
@@ -8,89 +21,113 @@ namespace Dev2.Diagnostics.Test
     public class WebDebugMessageRepoTests
     {
         [TestMethod]
-        [Owner("Nkosinathi Sangweni")]
-        public void Constructor_GivenIsNew_ShouldInitiase()
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(WebDebugMessageRepo))]
+        public void WebDebugMessageRepo_Constructor_IsNew_ShouldInitialise_OnlyOnce()
         {
-            //---------------Set up test pack-------------------
-            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(webDebugMessageRepo);
-            //---------------Execute Test ----------------------
-            var instance = WebDebugMessageRepo.Instance;
-            Assert.IsNotNull(instance);
-            //---------------Test Result -----------------------
-            Assert.IsTrue(ReferenceEquals(instance, webDebugMessageRepo));
+            //---------------Arrange-------------------
+            //---------------Act------------------------
+            //---------------Assert---------------------
+            Parallel.Invoke(()=> { Assert.IsNotNull(WebDebugMessageRepo.Instance); },
+                            () => { Assert.IsNotNull(WebDebugMessageRepo.Instance); },
+                            () => { Assert.IsNotNull(WebDebugMessageRepo.Instance); }
+                            );
         }
 
         [TestMethod]
-        [Owner("Nkosinathi Sangweni")]
-        public void AddDebugItem_GivenValidArgs_ShouldAdd1Item()
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(WebDebugMessageRepo))]
+        public void WebDebugMessageRepo_AddDebugItem_ValidArgs_ShouldAdd1Item()
         {
             //---------------Set up test pack-------------------
-            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(webDebugMessageRepo);
-            //---------------Execute Test ----------------------
-            var debugState = new DebugState();
+            var mockDebugState = new Mock<IDebugState>();
+
             var clientId = Guid.NewGuid();
             var sessionId = Guid.NewGuid();
-            webDebugMessageRepo.AddDebugItem(clientId, sessionId, debugState);
+
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Execute Test ----------------------
+            webDebugMessageRepo.AddDebugItem(clientId, sessionId, mockDebugState.Object);
             //---------------Test Result -----------------------
             var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
             Assert.AreEqual(1, fetchDebugItems.Count);
         }
 
         [TestMethod]
-        [Owner("Nkosinathi Sangweni")]
-        public void AddDebugItem_GivenValidArgsAndSessions_ShouldNotMixUpDebugStates()
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(WebDebugMessageRepo))]
+        public void WebDebugMessageRepo_AddDebugItem_ValidArgsAndSessions_ShouldNotMixUpDebugStates()
         {
             //---------------Set up test pack-------------------
-            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(webDebugMessageRepo);
-            //---------------Execute Test ----------------------
-            var id = Guid.NewGuid();
-            var debugState = new DebugState() { ID = id };
+            var mockDebugState = new Mock<IDebugState>();
+            var mockDebugState1 = new Mock<IDebugState>();
+
             var clientId = Guid.NewGuid();
-            var sessionId = Guid.NewGuid();
-            webDebugMessageRepo.AddDebugItem(clientId, sessionId, debugState);
             var clientId1 = Guid.NewGuid();
+            var sessionId = Guid.NewGuid();
             var sessionId1 = Guid.NewGuid();
-            var id1 = Guid.NewGuid();
-            var debugState1 = new DebugState() { ID = id1 };
-            webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, debugState1);
-            //---------------Test Result -----------------------
+
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Execute Test ----------------------
+            Parallel.Invoke(() => { webDebugMessageRepo.AddDebugItem(clientId, sessionId, mockDebugState.Object); },
+                           () => { webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, mockDebugState1.Object); }
+                           );
+
             var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
-            Assert.AreEqual(1, fetchDebugItems.Count);
+
             fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId1, sessionId1);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(1, fetchDebugItems.Count);
             Assert.AreEqual(1, fetchDebugItems.Count);
         }
 
         [TestMethod]
-        [Owner("Nkosinathi Sangweni")]
-        public void AddDebugItem_GivenKeyExists_ShouldNotMixUpDebugStates()
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(WebDebugMessageRepo))]
+        public void WebDebugMessageRepo_AddDebugItem_KeyExists_ShouldNotMixUpDebugStates()
         {
             //---------------Set up test pack-------------------
+            var mockDebugState = new Mock<IDebugState>();
+            var mockDebugState1 = new Mock<IDebugState>();
+
+            var clientId = Guid.NewGuid();
+            var clientId1 = Guid.NewGuid();
+            var sessionId = Guid.NewGuid();
+            var sessionId1 = Guid.NewGuid();
+
             var webDebugMessageRepo = WebDebugMessageRepo.Instance;
-            //---------------Assert Precondition----------------
-            Assert.IsNotNull(webDebugMessageRepo);
             //---------------Execute Test ----------------------
-            var id = Guid.NewGuid();
-            var debugState = new DebugState() { ID = id };
+            Parallel.Invoke(() => { webDebugMessageRepo.AddDebugItem(clientId, sessionId, mockDebugState.Object); },
+                            () => { webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, mockDebugState1.Object); },
+                            () => { webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, mockDebugState1.Object); }
+                            );
+
+            var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
+            var fetchDebugItems1 = webDebugMessageRepo.FetchDebugItems(clientId1, sessionId1);
+            //---------------Test Result -----------------------;
+            Assert.AreEqual(1, fetchDebugItems.Count);
+            Assert.AreEqual(2, fetchDebugItems1.Count);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        [TestCategory(nameof(WebDebugMessageRepo))]
+        public void WebDebugMessageRepo_AddDebugItem_StateTypeNone_ParentID_IsNull_ExpectTrue()
+        {
+            //---------------Set up test pack-------------------
+            var mockDebugState = new Mock<IDebugState>();
+
             var clientId = Guid.NewGuid();
             var sessionId = Guid.NewGuid();
-            webDebugMessageRepo.AddDebugItem(clientId, sessionId, debugState);
-            var clientId1 = Guid.NewGuid();
-            var sessionId1 = Guid.NewGuid();
-            var id1 = Guid.NewGuid();
-            var debugState1 = new DebugState() { ID = id1 };
-            webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, debugState1);
-            webDebugMessageRepo.AddDebugItem(clientId1, sessionId1, debugState1);
+
+            mockDebugState.Setup(o => o.StateType).Returns(StateType.None);
+            mockDebugState.Setup(o => o.ParentID).Returns(default(Guid));
+
+            var webDebugMessageRepo = WebDebugMessageRepo.Instance;
+            //---------------Execute Test ----------------------
+            webDebugMessageRepo.AddDebugItem(Guid.NewGuid(), Guid.NewGuid(), mockDebugState.Object);
             //---------------Test Result -----------------------
-            var fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId, sessionId);
-            Assert.AreEqual(1, fetchDebugItems.Count);
-            fetchDebugItems = webDebugMessageRepo.FetchDebugItems(clientId1, sessionId1);
-            Assert.AreEqual(2, fetchDebugItems.Count);
+            mockDebugState.VerifySet(o => o.ParentID = null);
         }
     }
 }
