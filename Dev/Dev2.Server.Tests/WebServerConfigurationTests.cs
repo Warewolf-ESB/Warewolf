6dfe17c103fe17b9db46de6d7b4d677bc16537b8
Ashley Lewis <ashley.lewis@dev2.co.za>
2019-02-18 13:46:15 +0200

    4494 add missing deployment item to this unit test

--- Dev/Dev2.Server.Tests/WebServerConfigurationTests.cs
+++ Dev/Dev2.Server.Tests/WebServerConfigurationTests.cs
@@ -141,6 +141,7 @@ namespace Dev2.Server.Tests
         [TestMethod]
         [Owner("Siphamandla Dube")]
         [TestCategory(nameof(WebServerConfiguration))]
+        [DeploymentItem("WarewolfServer.cer")]
         public void WebServerConfigurationTests_Execute_sslCertificateName_NotNull_ExpectException()
         {
             //----------------Arrange--------------------
