3a904cde09c81c66bd3bf732a8da10151af3b246
Barney <barney.buchan@dev2.co.za>
2019-03-01 08:51:07 +0200

    4800 disable S1541

--- Dev/Dev2.Activities/Activities/DsfEnhancedDotNetDllActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfEnhancedDotNetDllActivity.cs
@@ -431,7 +431,9 @@ namespace Dev2.Activities
 
         #region Overrides of DsfActivity
         [ExcludeFromCodeCoverage] //This does not appear to be used anywhere
+#pragma warning disable S1541 // Methods and properties should not be too complex
         protected override void ChildDebugStateDispatch(IDSFDataObject dataObject)
+#pragma warning restore S1541 // Methods and properties should not be too complex
         {
             if (dataObject.IsDebugMode())
             {
