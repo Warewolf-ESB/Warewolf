b2bd7e476e1bdb4ac1d14491ad76acdecc4ee771
Barney <barney.buchan@dev2.co.za>
2019-03-01 08:50:26 +0200

    4800 disable S1541

--- Dev/Dev2.Activities/Activities/DsfExecuteCommandLineActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfExecuteCommandLineActivity.cs
@@ -140,7 +140,9 @@ namespace Dev2.Activities
             };
         }
 
+#pragma warning disable S1541 // Methods and properties should not be too complex
         protected override void ExecuteTool(IDSFDataObject dataObject, int update)
+#pragma warning restore S1541 // Methods and properties should not be too complex
         {
             var exeToken = dataObject.ExecutionToken;
 
