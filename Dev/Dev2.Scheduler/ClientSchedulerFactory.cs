e4a4061c0e64d70280dc0514c5aff7ea95e3649f
Rory McGuire <rory.mcguire@dev2.co.za>
2019-03-11 12:15:39 +0200

    4801 ignore complexity for switching method

--- Dev/Dev2.Scheduler/ClientSchedulerFactory.cs
+++ Dev/Dev2.Scheduler/ClientSchedulerFactory.cs
@@ -34,7 +34,9 @@ namespace Dev2.Scheduler
 
         public IScheduleTrigger CreateTrigger(TaskState state, ITrigger trigger) => new ScheduleTrigger(state, trigger, _service, _serviceConvertorFactory);
 
+#pragma warning disable S1541 // Methods and properties should not be too complex
         public IScheduleTrigger CreateTrigger(Trigger trigger)
+#pragma warning restore S1541 // Methods and properties should not be too complex
         {
             switch (trigger.TriggerType)
             {
