d5906e17aa527b9fa265f65071b6f3d7e48ebc03
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2019-03-18 15:05:03 +0200

    4805-DebugStateTreeBuilder complexity refactor

--- Dev/Dev2.Common/DebugStateTreeBuilder.cs
+++ Dev/Dev2.Common/DebugStateTreeBuilder.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Dev2.Common.Interfaces.Diagnostics.Debug;
@@ -7,7 +18,6 @@ namespace Dev2.Common
 {
     public static class DebugStateTreeBuilder
     {
-
         public static IEnumerable<IDebugState> BuildTree(IEnumerable<IDebugState> source)
         {
             var groups = source?.GroupBy(i => i.ParentID) ?? new List<IGrouping<Guid?, IDebugState>>();
@@ -28,17 +38,18 @@ namespace Dev2.Common
             var debugStates = roots?.DistinctBy(state => new { state.ID, state.StateType, state.Children }).ToList();
 
             return debugStates;
-
         }
 
         static void AddChildren(IDebugState node, IDictionary<Guid, List<IDebugState>> source)
         {
+            var IsActualTypeContains = (node.ActualType?.Contains("DsfForEachActivity") ?? false);
+
             if (source.ContainsKey(node.ID)
-                && (!node.IsAdded || (node.ActualType?.Contains("DsfForEachActivity") ?? false))
+                && (!node.IsAdded || IsActualTypeContains)
                 && node.StateType != StateType.Duration)//Services have the same Id so, they dont work inside the foreach
             {
                 List<IDebugState> debugStates;
-                if (node.ActualType?.Contains("DsfForEachActivity") ?? false)
+                if (IsActualTypeContains)
                 {
                     var states = source[node.ID].DistinctBy(state => new
                     {
@@ -54,16 +65,21 @@ namespace Dev2.Common
                                             .ToList();
                 }
 
-                node.Children = debugStates ?? new List<IDebugState>();
-                node.IsAdded = true;
-                foreach (var state in node.Children)
-                {
-                    AddChildren(state, source);
-                }
+                AddEachChild(node, source, debugStates);
             }
             else
             {
                 node.Children = new List<IDebugState>();
+            }
+        }
+
+        private static void AddEachChild(IDebugState node, IDictionary<Guid, List<IDebugState>> source, List<IDebugState> debugStates)
+        {
+            node.Children = debugStates ?? new List<IDebugState>();
+            node.IsAdded = true;
+            foreach (var state in node.Children)
+            {
+                AddChildren(state, source);
             }
         }
     }
