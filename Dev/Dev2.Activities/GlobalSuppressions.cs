b35d879c8b8c1510386cb9a036f5cf6b864f5819
Ashley Lewis <ashley.lewis@dev2.co.za>
2019-03-26 11:08:52 +0200

    4949 ignoring or otherwise resolving compile warnings todo with vestris.resourcelib and AD0001 (analyzer exceptions)

--- Dev/Dev2.Activities/GlobalSuppressions.cs
+++ Dev/Dev2.Activities/GlobalSuppressions.cs
@@ -4,5 +4,5 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S104:Files should not have too many lines of code", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S104:Files should not have too many lines of code")]
 
