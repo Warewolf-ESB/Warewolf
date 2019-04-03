ab10c6cf0c4ef6cafe7cd8437b320e7bd50f1455
Pieter.Terblanche <pete.terblanche@gmail.com>
2019-04-03 13:27:47 +0200

    4815 JsonUtils

--- Dev/Dev2.Common.Tests/Utils/JSONUtilsTests.cs
+++ Dev/Dev2.Common.Tests/Utils/JSONUtilsTests.cs
@@ -18,40 +18,39 @@ namespace Dev2.Common.Tests.Utils
     {
         [TestMethod]
         [Owner("Candice Daniel")]
-        [TestCategory(nameof(JSONUtils))]
-
+        [TestCategory(nameof(JsonUtils))]
         public void JSONUtils_ScrubJSON()
         {
             var fetch = JsonResource.Fetch("ForEachWorkFlow");
-            var value = JSONUtils.ScrubJSON(fetch);
+            var value = JsonUtils.ScrubJson(fetch);
 
             Assert.AreEqual(fetch, value);
         }
+
         [TestMethod]
         [Owner("Candice Daniel")]
-        [TestCategory(nameof(JSONUtils))]
-
+        [TestCategory(nameof(JsonUtils))]
         public void JSONUtils_ScrubJSON_Clean()
         {
             var fetch = JsonResource.Fetch("ForEachWorkFlow");
-            Assert.AreEqual(fetch, JSONUtils.ScrubJSON("\"" + fetch + "\""));
+            Assert.AreEqual(fetch, JsonUtils.ScrubJson("\"" + fetch + "\""));
         }
+
         [TestMethod]
         [Owner("Candice Daniel")]
-        [TestCategory(nameof(JSONUtils))]
-
+        [TestCategory(nameof(JsonUtils))]
         public void JSONUtils_Format_Empty()
         {
-            Assert.AreEqual("", JSONUtils.Format(""));
+            Assert.AreEqual("", JsonUtils.Format(""));
         }
+
         [TestMethod]
         [Owner("Candice Daniel")]
-        [TestCategory(nameof(JSONUtils))]
-
+        [TestCategory(nameof(JsonUtils))]
         public void JSONUtils_Format()
         {
             var fetch = JsonResource.Fetch("Test");
-            var formatted = JSONUtils.Format(fetch);
+            var formatted = JsonUtils.Format(fetch);
             var result = "[\r\n\t{\r\n\t\t\"ID\": \"00000000-0000-0000-0000-000000000000\",\r\n\t\t\"ParentID\": null,\r\n\t\t\"SourceResourceID\": \"037b7b8c-be29-4f2a-b648-6569051ca127\",\r\n\t\t\r\n\t}\r\n]";
             Assert.AreEqual(result, formatted);
         }
