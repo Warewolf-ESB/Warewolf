4e0845b71ea063d89dcd85603470a13a26392a5a
Rory McGuire <rory.mcguire@dev2.co.za>
2019-07-09 16:01:17 +0200

    5737 cache the IsAuthorized results based on Identity.Name, AuthorizationContext, and resource.id

--- Dev/Dev2.Runtime.Services/Security/ServerAuthorizationService.cs
+++ Dev/Dev2.Runtime.Services/Security/ServerAuthorizationService.cs
@@ -21,7 +21,7 @@ using Dev2.Common.Interfaces.Enums;
 
 namespace Dev2.Runtime.Security
 {
-    public class ServerAuthorizationService : AuthorizationServiceBase
+    public sealed class ServerAuthorizationService : AuthorizationServiceBase
     {
         static ConcurrentDictionary<Tuple<string, string, AuthorizationContext>, Tuple<bool, DateTime>> _cachedRequests = new ConcurrentDictionary<Tuple<string, string, AuthorizationContext>, Tuple<bool, DateTime>>();
 
@@ -62,7 +62,7 @@ namespace Dev2.Runtime.Security
             _cachedRequests = new ConcurrentDictionary<Tuple<string, string, AuthorizationContext>, Tuple<bool, DateTime>>();
         }
 
-        public override bool IsAuthorized(AuthorizationContext context, string resource)
+        public sealed override bool IsAuthorized(AuthorizationContext context, string resource)
         {
             bool authorized;
 
@@ -96,7 +96,7 @@ namespace Dev2.Runtime.Security
             return authorized;
         }
 
-        public override bool IsAuthorized(IAuthorizationRequest request)
+        public sealed override bool IsAuthorized(IAuthorizationRequest request)
         {
             VerifyArgument.IsNotNull("request", request);
             bool authorized;
