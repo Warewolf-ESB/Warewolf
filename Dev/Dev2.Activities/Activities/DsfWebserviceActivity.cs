b6ab32295903a340a07d006fb6bb8e8a2b260c80
Rory McGuire <rory.mcguire@dev2.co.za>
2019-07-25 13:15:29 +0200

    5781 add WebserviceExecution and DsfWebserviceActivity back into the repo, these are used in some old production workflows

---
+++ Dev/Dev2.Activities/Activities/DsfWebserviceActivity.cs
@@ -0,0 +1,52 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Data.TO;
+using Dev2.Interfaces;
+using Dev2.Services.Execution;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Activities
+{
+    public class DsfWebserviceActivity : DsfActivity
+    {
+        #region Overrides of DsfActivity
+
+        protected override void ExecutionImpl(IEsbChannel esbChannel, IDSFDataObject dataObject, string inputs, string outputs, out ErrorResultTO tmpErrors, int update)
+        {
+            tmpErrors = new ErrorResultTO();
+            var webserviceExecution = GetNewWebserviceExecution(dataObject);
+
+
+            if(webserviceExecution != null && !tmpErrors.HasErrors())
+            {
+                webserviceExecution.InstanceOutputDefintions = outputs; // set the output mapping for the instance ;)
+                webserviceExecution.InstanceInputDefinitions = inputs;
+                webserviceExecution.Execute(out ErrorResultTO invokeErrors, update);
+                var err = invokeErrors.MakeDataListReady();
+                if (!string.IsNullOrEmpty(err))
+                {
+                    dataObject.Environment.AddError(err);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Protected Helper Functions
+
+        protected virtual WebserviceExecution GetNewWebserviceExecution(IDSFDataObject context)
+        {
+            return new WebserviceExecution(context, false);
+        }
+
+        #endregion
+    }
+}
---
+++ Dev/Dev2.Services.Execution/WebserviceExecution.cs
@@ -0,0 +1,61 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2017 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using Dev2.Common.Interfaces.Core.Graph;
+using Dev2.Data.TO;
+using Dev2.Data.Util;
+using Dev2.Interfaces;
+using Dev2.Runtime.ServiceModel;
+using Dev2.Runtime.ServiceModel.Data;
+
+namespace Dev2.Services.Execution
+{
+    public class WebserviceExecution : ServiceExecutionAbstract<WebService, WebSource>
+    {
+        #region Constuctors
+
+        public WebserviceExecution(IDSFDataObject dataObj, bool handlesFormatting)
+            : base(dataObj, handlesFormatting)
+        {
+        }
+
+        #endregion
+
+        #region Execute
+
+        public override void BeforeExecution(ErrorResultTO errors)
+        {
+        }
+
+        public override void AfterExecution(ErrorResultTO errors)
+        {
+        }
+
+        protected virtual void ExecuteWebRequest(WebService service, out ErrorResultTO errors)
+        {
+            WebServices.ExecuteRequest(service, true, out errors);
+        }
+
+        protected object ExecuteService(int update, out ErrorResultTO errors) => ExecuteService(update, out errors, null);
+        protected override object ExecuteService(int update, out ErrorResultTO errors, IOutputFormatter formater)
+        {
+            Service.Source = Source;
+            ExecuteWebRequest(Service, out errors);
+            var result = String.IsNullOrEmpty(Service.JsonPath) || String.IsNullOrEmpty(Service.JsonPathResult)
+                ? Scrubber.Scrub(Service.RequestResponse)
+                : Scrubber.Scrub(Service.JsonPathResult);
+            Service.RequestResponse = null;
+            return result;
+        }
+
+        #endregion
+    }
+}
