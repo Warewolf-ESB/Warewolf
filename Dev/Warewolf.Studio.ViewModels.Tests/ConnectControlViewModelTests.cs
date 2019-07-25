e63c0b278104f74bca54306750c1fafde68927dc
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-07-25 09:17:26 +0200

    Revert "453 ignore this unit test causing stack overflow exception"
    This reverts commit 33358bf7af35718dfc22e5a6326954b1c058e30f.

--- Dev/Warewolf.Studio.ViewModels.Tests/ConnectControlViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ConnectControlViewModelTests.cs
@@ -368,7 +368,6 @@ namespace Warewolf.Studio.ViewModels.Tests
         [TestMethod,Timeout(60000)]
         [TestCategory(nameof(ConnectControlViewModel))]
         [Owner("Sipohamandla Dube")]
-        [Ignore]
         public void ConnectControlViewModel_TestConnectSuccessful()
         {
             //arrange
