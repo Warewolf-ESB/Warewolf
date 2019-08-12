bf794c76e312ad0703c6315df944eb1aab64bf7d
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-08-12 12:08:37 +0200

    5734-RabbitMQDriverTests consuming

---
+++ Dev/RabbitMQDriver/RabbitConnection.cs
@@ -0,0 +1,50 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Common.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using IConnection = RabbitMQ.Client.IConnection;
+
+namespace Warewolf.Driver.RabbitMQ
+{
+    internal class RabbitConnection : IQueueConnection
+    {
+        readonly IConnection _connection;
+
+        public RabbitConnection(IConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public bool IsOpen => _connection.IsOpen;
+
+        public IPublisher NewPublisher(IQueueConfig config)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StartConsuming(IQueueConfig config, IConsumer consumer)
+        {
+            var rconfig = config as RabbitConfig;
+            var channel = rconfig.CreateChannel(_connection);
+
+            var evConsumer = new EventingBasicConsumer(channel);
+            evConsumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body;
+
+                consumer.Consume(body);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+        }
+    }
+}
---
+++ Dev/RabbitMQDriver/RabbitMQSource.cs
@@ -0,0 +1,33 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Common.Interfaces;
+using RabbitMQ.Client;
+
+namespace Warewolf.Driver.RabbitMQ
+{
+    public class RabbitMQSource : IQueueSource
+    {
+        private readonly ConnectionFactory _factory;
+
+        public RabbitMQSource()
+        {
+            _factory = new ConnectionFactory() { HostName = "localhost" };
+
+        }
+
+        public IQueueConnection NewConnection(IQueueConfig config)
+        {
+            return new RabbitConnection(_factory.CreateConnection());
+
+            
+        }
+    }
+}
