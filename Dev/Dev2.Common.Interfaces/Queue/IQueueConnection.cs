6a73fd7c9d2063a7a99f7403160f207031253db1
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-08-12 11:11:09 +0200

    5734-QueueTests

---
+++ Dev/Dev2.Common.Interfaces/Queue/IQueueConnection.cs
@@ -0,0 +1,23 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using Dev2.Common.Interfaces.Queue;
+
+namespace Dev2.Common.Interfaces
+{
+    public interface IQueueConnection
+    {
+        bool IsOpen { get; }
+
+        IPublisher NewPublisher(IQueueConfig config);
+        void StartConsuming(IQueueConfig config, IConsumer consumer);
+    }
+}
