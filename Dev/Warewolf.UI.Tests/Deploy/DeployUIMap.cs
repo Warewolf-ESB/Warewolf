112d47a038ce8819ca0346ded734ad2b47bfafac
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-08-20 11:10:38 +0200

    453 making this assert more lenient will reduce test flicker

--- Dev/Warewolf.UI.Tests/Deploy/DeployUIMap.cs
+++ Dev/Warewolf.UI.Tests/Deploy/DeployUIMap.cs
@@ -113,7 +113,7 @@ namespace Warewolf.UI.Tests.Deploy.DeployUIMapClasses
             MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.DestinationServerConectControl.Combobox.ToggleButton.WaitForControlExist(300000);
             Mouse.Click(MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.DestinationServerConectControl.Combobox.ToggleButton);
             Mouse.Click(UIMap.MainStudioWindow.ComboboxListItemAsLocalServerSource);
-            Assert.IsTrue(MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.DestinationServerConectControl.Combobox.LocalServerSourceText.Exists, "Selected source server in deploy is not Remote Connection Integration (Connected).");
+            Assert.IsTrue(MainStudioWindow.DockManager.SplitPaneMiddle.TabManSplitPane.TabMan.DeployTab.WorkSurfaceContext.DockManager.DeployView.DestinationServerConectControl.Combobox.LocalServerSourceText.Exists, "Selected source server in deploy is not Local Server Source.");
         }
 
         [When(@"I Select LocalhostConnected From Deploy Tab Source Server Combobox")]
