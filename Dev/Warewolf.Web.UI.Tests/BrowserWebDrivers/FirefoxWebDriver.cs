671fc19b97f1ef50894ec274d88fbf1ddac3523c
Ashley.lewis <Ashley.lewis@TEST-AGENT>
2019-08-20 10:46:01 +0200

    453 trying to fick firefox test flicker with -no-sandbox argument

--- Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/FirefoxWebDriver.cs
+++ Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/FirefoxWebDriver.cs
@@ -8,9 +8,11 @@ namespace Warewolf.Web.UI.Tests.BrowserWebDrivers
     {
         static FirefoxDriverService driverService = FirefoxDriverService.CreateDefaultService(Environment.CurrentDirectory);
         static FirefoxOptions firefoxOptions = new FirefoxOptions() { Profile = new FirefoxProfile(Path.Combine(Environment.CurrentDirectory, "WebDriverProfiles", "Firefox")) };
-        public FirefoxWebDriver() : base(new FirefoxDriver(driverService, firefoxOptions, TimeSpan.FromMinutes(3)))
+        public FirefoxWebDriver() : base(new FirefoxDriver(driverService, FirefoxOptions, TimeSpan.FromMinutes(3)))
         {
             driverService.Port = 18406;
         }
+
+        public static FirefoxOptions FirefoxOptions { get { firefoxOptions.AddArgument("-no-sandbox"); return firefoxOptions; } set => firefoxOptions = value; }
     }
 }
