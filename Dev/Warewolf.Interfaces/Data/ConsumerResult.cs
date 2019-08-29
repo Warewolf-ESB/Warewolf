ff226b72b4ab309ec1dce6c0cbf126eba24a375e
Hagashen Naidu <hagashen.naidu@theunlimited.co.za>
2019-08-29 12:32:44 +0200

    5796: Implemented publishing message to the dead letter queue if there is a bad response from the webrequest

---
+++ Dev/Warewolf.Interfaces/Data/ConsumerResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warewolf.Interfaces.Data
+{
+    public enum ConsumerResult
+    {
+        Success,
+        Failed
+    }
+}
