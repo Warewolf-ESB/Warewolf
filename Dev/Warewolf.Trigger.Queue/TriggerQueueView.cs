a04942cbbf6cd4221308786fe9243fd37da4ba5e
Rory McGuire <rory.mcguire@dev2.co.za>
2019-08-29 09:49:18 +0200

    4971 use As<T> to cast to IServiceInput

--- Dev/Warewolf.Trigger.Queue/TriggerQueueView.cs
+++ Dev/Warewolf.Trigger.Queue/TriggerQueueView.cs
@@ -475,12 +475,12 @@ namespace Warewolf.Trigger.Queue
                                 value = obj[sca.DisplayValue.Replace("@", "")].ToString();
                             }
                             var serviceTestInput = new ServiceInput(sca.DisplayValue, value);
-                            return (IServiceInput)serviceTestInput;
+                            return serviceTestInput.As<IServiceInput>();
                         }
                         else
                         {
                             var serviceTestInput = new ServiceInput(sca.DisplayValue, sca.Value);
-                            return (IServiceInput)serviceTestInput;
+                            return serviceTestInput.As<IServiceInput>();
                         }
 
                     }).ToList();
@@ -519,7 +519,7 @@ namespace Warewolf.Trigger.Queue
             Inputs = inputList.Select(sca =>
             {
                 var serviceTestInput = new ServiceInput(sca.DisplayValue, "");
-                return (IServiceInput)serviceTestInput;
+                return serviceTestInput.As<IServiceInput>();
 
             }).ToList();
         }
