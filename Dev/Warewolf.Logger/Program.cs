563374e7a288e5fd78f8ff7feb44d2dd95d2d24d
Candice Daniel <candice.daniel@dev2.co.za>
2019-09-03 15:36:44 +0200

    5732-Created Project and Added CommandLine and set CommandLineParser.ParseArguments to get “-v” to set verbose mode

---
+++ Dev/Warewolf.Logger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CommandLine;
+namespace Warewolf.Logger
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Warewolf.Logger is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Warewolf.Logger");
+                       }
+                   });
+
+        }
+    }
+}
