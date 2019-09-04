12f0d09c0d3f2b2d04834d5fe6a2935b7d93704b
Candice Daniel <candice.daniel@dev2.co.za>
2019-09-04 11:19:15 +0200

    5732-Added LoggerContext and Implementation to Main. Added Test Project and Tests for LoggerContext

---
+++ Dev/Warewolf.Logger.Tests/WarewolfLoggerTests.cs
@@ -0,0 +1,51 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Warewolf.Logger.Tests
+{
+    [TestClass]
+    public class WarewolfLoggerTests
+    {
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(LoggerContext))]
+        public void LoggerContext_Contructor_Verbose_IsTrue()
+        {
+            var args = new string[] { "-v" };
+            var loggerContext = new LoggerContext(args);
+            Assert.IsNotNull(loggerContext);
+            Assert.IsTrue(loggerContext.Verbose);
+        }
+
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(LoggerContext))]
+        public void LoggerContext_Contructor_Verbose_IsFalse()
+        {
+            var args = new string[] { };
+            var loggerContext = new LoggerContext(args);
+            Assert.IsNotNull(loggerContext);
+            Assert.IsFalse(loggerContext.Verbose);
+        }
+
+
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(LoggerContext))]
+        public void LoggerContext_HandleParseError()
+        {
+            var args = new string[] { "-h" };
+            var loggerContext = new LoggerContext(args);
+            Assert.AreEqual(1,loggerContext.Errors.Count());
+        }
+    }
+}
---
+++ Dev/Warewolf.Logger/LoggerContext.cs
@@ -0,0 +1,58 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using Warewolf.Common;
+
+namespace Warewolf.Logger
+{
+    public class LoggerContext : ILoggerContext
+    {
+        public IEnumerable<Error> Errors { get; set; }
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
+        public bool Verbose { get; set; }
+        public LoggerContext(string[] args)
+        {
+            var processArgs = Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed<Options>(opts => IsVerbose(opts))
+                    .WithNotParsed<Options>((errs) => HandleParseError(errs));
+
+        }
+
+        public void IsVerbose(Options opts)
+        {
+            if (opts.Verbose)
+            {
+                Verbose = true;
+                Console.WriteLine("Warewolf.Logger is in Verbose mode!");
+                _ = new ConsoleWindow();
+            }
+            else
+            {
+                Verbose = false;
+            }
+        }
+
+        public void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine("Command Line parameters provided were not valid!");
+            Errors = errs;
+
+            //Environment.Exit(1); //TODO: Removed for now as I am not sure how test just yet
+        }
+    }
+}
