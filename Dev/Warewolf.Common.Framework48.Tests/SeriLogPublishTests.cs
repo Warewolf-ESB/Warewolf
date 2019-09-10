8db5152a41e328d9a50eadd608c3f64676e04df4
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-09-10 11:26:15 +0200

    5732-SeriLogPublishTests WIP

--- Dev/Warewolf.Common.Framework48.Tests/SeriLogPublishTests.cs
+++ Dev/Warewolf.Common.Framework48.Tests/SeriLogPublishTests.cs
@@ -15,6 +15,7 @@ using Serilog.Core;
 using Serilog.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Warewolf.Logging.SeriLog;
 
@@ -115,12 +116,76 @@ namespace Warewolf.Common.Framework48.Tests
 
         }
 
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        [TestCategory("SeriLogPublish")]
+        public void SeriLogPublish_NewPublisher_WriteToSink_UsingAny_ILogEventSink_IPML_Success1()
+        {
+            //-------------------------Arrange------------------------------
+            var testSqlitePath = @"C:\Test\Warewolf\db\testAudits.db";
+            File.Delete(testSqlitePath);
+
+            Assert.IsFalse(File.Exists(testSqlitePath));
+
+            var seriConfig = new TestSeriLogSQLiteConfig(sqlPath:testSqlitePath);
+
+
+            var loggerSource = new SeriLoggerSource();
+
+            var loggerConnection = loggerSource.NewConnection(seriConfig);
+
+            Assert.IsTrue(File.Exists(testSqlitePath));
+
+            var loggerPublisher = loggerConnection.NewPublisher();
+
+            var error = new { ServerName = "testServer", Error = "testError" };
+            var fatal = new { ServerName = "testServer", Error = "testFatalError" };
+
+            var expectedTestInfoMsgTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}";
+            var expectedTestErrorMsgTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+            var expectedTestFatalMsg = "test infomation {testFatalKey}";
+
+            var expectedTestErrorMsg = $"Error From: {@error.ServerName} : {error.Error} ";
+
+            //-------------------------Act----------------------------------
+
+            loggerPublisher.Info("test message");
+            loggerPublisher.Error(expectedTestErrorMsgTemplate, DateTime.Now, LogEventLevel.Error, expectedTestErrorMsg, Environment.NewLine, "test exception");
+            loggerPublisher.Fatal(expectedTestFatalMsg, @fatal);
+
+            //var loggerConsumer = loggerConnection.Consumer();
+
+            //var item1 = actualLogEventList[0].Properties.FirstOrDefault(o => o.Key == "Message");
+            //var item2 = actualLogEventList[1].Properties.First(o => o.Key == "Message");
+
+            ////-------------------------Assert-------------------------------
+            //Assert.IsTrue(actualLogEventList.Count == 3);
+
+            //Assert.AreEqual(expected: LogEventLevel.Information, actual: actualLogEventList[0].Level);
+            //Assert.AreEqual(expected: expectedTestInfoMsgTemplate, actual: actualLogEventList[0].MessageTemplate.Text);
+
+            //Assert.AreEqual(expected: "Message", actual: item1.Key);
+            //Assert.AreEqual(expected: "\"test message\"".ToString(), actual: item1.Value.ToString());
+
+            //Assert.AreEqual(expected: LogEventLevel.Error, actual: actualLogEventList[1].Level);
+            //Assert.AreEqual(expected: expectedTestErrorMsgTemplate, actual: actualLogEventList[1].MessageTemplate.Text);
+            //Assert.AreEqual(expected: "\"Error From: testServer : testError \"", actual: item2.Value.ToString());
+
+            //Assert.AreEqual(expected: LogEventLevel.Fatal, actual: actualLogEventList[2].Level);
+            //Assert.AreEqual(expected: 1, actual: actualLogEventList[2].Properties.Count);
+            //Assert.IsTrue(actualLogEventList[2].Properties.ContainsKey("testFatalKey"));
+
+            //Assert.AreEqual(1, actualLogEventList[2].Properties.Values.Count());
+            //Assert.AreEqual(expected: expectedTestFatalMsg, actual: actualLogEventList[2].MessageTemplate.Text);
+
+        }
+
         class TestLogEventSink : ILogEventSink
         {
             public List<LogEvent> LogData { get; private set; } = new List<LogEvent>();
             public void Emit(LogEvent logEvent)
             {
-               LogData.Add(logEvent);
+                LogData.Add(logEvent);
             }
         }
 
@@ -135,11 +200,42 @@ namespace Warewolf.Common.Framework48.Tests
                 _logEventSink = logEventSink;
             }
 
-            public Logger CreateLogger()
+            private Logger CreateLogger()
             {
-                return new LoggerConfiguration().WriteTo.Sink(logEventSink: _logEventSink).CreateLogger();
+               return new LoggerConfiguration().WriteTo.Sink(logEventSink: _logEventSink).CreateLogger();
+                
             }
         }
 
     }
+
+    internal class TestSeriLogSQLiteConfig : ISeriLogConfig
+    {
+        readonly string _sqlPath;
+
+        public TestSeriLogSQLiteConfig(string sqlPath)
+        {
+            _sqlPath = sqlPath;
+        }
+
+        public Logger Logger { get => CreateLogger(); }
+
+        private Logger CreateLogger()
+        {
+            var config = new SeriLogSQLiteConfig.Config
+            {
+                TableName = "testLogs",
+                RestrictedToMinimumLevel = LogEventLevel.Information,
+            };
+
+            return new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .WriteTo
+                .SQLite(_sqlPath) //, tableName: config.TableName, restrictedToMinimumLevel: config.RestrictedToMinimumLevel, formatProvider: config.FormatProvider, storeTimestampInUtc: config.StoreTimestampInUtc, retentionPeriod: config.RetentionPeriod)
+                .CreateLogger();
+        }
+    }
+
 }
---
+++ Dev/Warewolf.Common.Framework48/SeriLogSQLiteConfig.cs
@@ -0,0 +1,50 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using Warewolf.Logging.SeriLog;
+
+namespace Warewolf.Common.Framework48
+{
+    public class SeriLogSQLiteConfig : ISeriLogConfig
+    {
+
+        readonly Config _config;
+
+        public SeriLogSQLiteConfig(Config config)
+        {
+            _config = config;
+        } 
+
+        public Logger Logger => CreateLogger();
+
+        private Logger CreateLogger()
+        {
+            return new LoggerConfiguration()
+                .WriteTo
+                .SQLite(sqliteDbPath:_config.SqliteDbPath, tableName: _config.TableName, restrictedToMinimumLevel: _config.RestrictedToMinimumLevel, retentionPeriod: _config.RetentionPeriod, storeTimestampInUtc: _config.StoreTimestampInUtc, formatProvider: _config.FormatProvider)
+                .CreateLogger();
+        }
+
+        public class Config
+        {
+            public string SqliteDbPath { get; }
+            public string TableName { get; set; } = "Logs"; 
+            public LogEventLevel RestrictedToMinimumLevel { get; set; } = LogEventLevel.Verbose;
+            public IFormatProvider FormatProvider { get; set; } = null;
+            public bool StoreTimestampInUtc { get; set; } = false;
+            public TimeSpan? RetentionPeriod { get; set; } = null;
+        }
+    }
+}
