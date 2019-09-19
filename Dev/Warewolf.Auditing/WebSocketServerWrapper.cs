94f46d8b5b4255ec0f640b4ecaac16f3c7cc52bb
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-09-19 17:01:00 +0200

    5732-WebSocketServerWrapper

---
+++ Dev/Warewolf.Auditing/WebSocketServerWrapper.cs
@@ -0,0 +1,59 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using System;
+using Fleck;
+
+namespace Warewolf.Auditing
+{
+    public interface IWebSocketServerWrapper
+    {
+        void Dispose();
+        void Start(Action<IWebSocketConnection> config);
+    }
+
+    public class WebSocketServerWrapper : IWebSocketServer, IWebSocketServerWrapper
+    {
+        private readonly IWebSocketServer _webSocketServer;
+
+        public WebSocketServerWrapper(string serverLoggingAddress)
+        {
+            _webSocketServer = new WebSocketServer(serverLoggingAddress)
+            {
+                RestartAfterListenError = true
+            };
+        }
+
+        public void Dispose()
+        {
+            _webSocketServer.Dispose();
+        }
+
+        public void Start(Action<IWebSocketConnection> config)
+        {
+            _webSocketServer.Start(config);
+        }
+
+
+        public interface IWebSocketServerFactory
+        {
+            IWebSocketServerWrapper New(string serverLoggingAddress);
+        }
+
+        public class WebSocketServerFactory : IWebSocketServerFactory
+        {
+            public IWebSocketServerWrapper New(string serverLoggingAddress)
+            {
+               return new WebSocketServerWrapper(serverLoggingAddress);
+            }
+        }
+    }
+}
