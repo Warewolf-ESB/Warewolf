c431bfdce027b7d61cc481b5a486c64dfffc58b6
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2019-09-25 14:08:05 +0200

    5732 Add and start LoggingServiceMonitor

---
+++ Dev/Dev2.Server/JobConfig.cs
@@ -0,0 +1,30 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using Warewolf.OS;
+
+namespace Dev2
+{
+    public class JobConfig : IJobConfig
+    {
+        private Guid guid;
+
+        public JobConfig(Guid guid)
+        {
+            this.guid = guid;
+        }
+
+        public Guid Id => guid;
+
+        public string Name { get; set; }
+        public int Concurrency { get; set; }
+    }
+}
