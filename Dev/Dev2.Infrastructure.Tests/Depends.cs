eaf4074a1841ae8538cad276ea642ccac991dd05
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-10-03 15:32:15 +0200

    395 cherry-pick changes from my old branch

--- Dev/Dev2.Infrastructure.Tests/Depends.cs
+++ Dev/Dev2.Infrastructure.Tests/Depends.cs
@@ -1,40 +1,44 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
-using System.Reflection;
 using System.Threading;
+using System.Web.Script.Serialization;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor)]
-public class Depends: System.Attribute, IDisposable
+public class Depends : System.Attribute, IDisposable
 {
+    readonly string RigOpsHost = "RSAKLFSVRHST1";
+    readonly string RigOpsDomain = "dev2.local";
+
     public enum ContainerType
     {
         MySQL = 0,
         MSSQL = 1,
-        Oracle = 2,
-        PostGreSQL = 3,
-        Warewolf = 4,
-        RabbitMQ = 5
+        PostGreSQL = 2,
+        Warewolf = 3,
+        RabbitMQ = 4,
+        CIRemote = 5
     }
 
     string ConvertToString(ContainerType containerType)
     {
-        switch (_containerType)
+        switch (containerType)
         {
             case ContainerType.MySQL:
                 return "MySQL";
             case ContainerType.MSSQL:
                 return "MSSQL";
-            case ContainerType.Oracle:
-                return "Oracle";
             case ContainerType.PostGreSQL:
                 return "PostGreSQL";
             case ContainerType.Warewolf:
                 return "Warewolf";
             case ContainerType.RabbitMQ:
                 return "RabbitMQ";
+            case ContainerType.CIRemote:
+                return "CIRemote";
         }
         throw new ArgumentOutOfRangeException();
     }
@@ -43,136 +47,158 @@ public class Depends: System.Attribute, IDisposable
 
     public string Hostname;
 
-    public Depends() { throw new ArgumentNullException("Missing type and hostname of the container."); }
+    public Depends() { throw new ArgumentNullException("Missing type of the container."); }
+
+    public Depends(ContainerType type) : this(type, "") { }
 
     public Depends(ContainerType type, string hostname)
     {
         _containerType = type;
         Hostname = hostname;
-        using (var client = new WebClient { Credentials = CredentialCache.DefaultNetworkCredentials })
+        using (var client = new WebClientWithExtendedTimeout { Credentials = CredentialCache.DefaultNetworkCredentials })
         {
-            var result = client.DownloadString($"http://T004124.premier.local:3142/public/Container/Async/Start/{ConvertToString(_containerType)}.json?Hostname={hostname}");
-            if (result != "Success")
-            {
-                throw new Exception($"Cannot start container{(result == string.Empty ? "." : ": " + result)}");
-            }
+            var result = client.DownloadString($"http://{RigOpsHost}.{RigOpsDomain}:3142/public/Container/Async/Start/{ConvertToString(_containerType)}.json?Hostname={hostname}");
+            Hostname = ParseForHostname(result);
+        }
+        switch (_containerType)
+        {
+            case ContainerType.MySQL:
+                StartRemoteMySQLContainer(true);
+                break;
+            case ContainerType.MSSQL:
+                StartRemoteMSSQLContainer(true);
+                break;
+            case ContainerType.RabbitMQ:
+                StartRemoteRabbitMQContainer(true);
+                break;
+            case ContainerType.CIRemote:
+                StartRemoteCIRemoteContainer(true);
+                break;
         }
     }
+
+    string ParseForHostname(string result)
+    {
+        JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+        var JSONObj = javaScriptSerializer.Deserialize<NewContainer>(result);
+        return JSONObj.ID.Substring(0, 12) + RigOpsDomain;
+    }
+
 
     public void Dispose()
     {
         using (var client = new WebClient { Credentials = CredentialCache.DefaultNetworkCredentials })
         {
-            var result = client.DownloadString($"http://T004124.premier.local:3142/public/Container/Async/Stop/{ConvertToString(_containerType)}.json?Hostname={Hostname}");
-            if (result != "Success")
+            var result = client.DownloadString($"http://{RigOpsHost}.{RigOpsDomain}:3142/public/Container/Async/Stop/{ConvertToString(_containerType)}.json");
+            if (result != @"{
+  ""Result"": ""Success""
+}")
             {
                 throw new Exception($"Cannot stop container{(result == string.Empty ? "." : ": " + result)}");
             }
         }
     }
 
-    public static Depends StartRemoteCIRemoteContainer(bool EnableDocker)
+    void StartRemoteCIRemoteContainer(bool EnableDocker)
     {
         var knownServerSources = new List<string>()
             {
+                @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\ChangingServerAuthUITest.bite",
                 @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\ChangingServerAuthUITest.xml",
+                @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\Remote Connection Integration.bite",
                 @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\Remote Connection Integration.xml",
+                @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\Restricted Remote Connection.bite",
                 @"%programdata%\Warewolf\Resources\Acceptance Testing Resources\Restricted Remote Connection.xml",
+                @"%programdata%\Warewolf\Resources\ExistingCodedUITestServerSource.bite",
                 @"%programdata%\Warewolf\Resources\ExistingCodedUITestServerSource.xml",
                 @"%programdata%\Warewolf\Resources\Remote Container.bite",
+                @"%programdata%\Warewolf\Resources\Remote Container.xml",
+                @"%programdata%\Warewolf\Resources\RemoteServerToDelete.bite",
                 @"%programdata%\Warewolf\Resources\RemoteServerToDelete.xml",
+                @"%programdata%\Warewolf\Resources\Remote Connection Integration.bite",
                 @"%programdata%\Warewolf\Resources\Remote Connection Integration.xml"
             };
         if (EnableDocker)
         {
-            var Depends = new Depends(ContainerType.Warewolf, "test-remotewarewolf");
-            UpdateSourcesConnectionStrings($"AppServerUri=http://{Depends.Hostname}:3142/dsf;WebServerPort=3142;AuthenticationType=Windows", knownServerSources);
-            return Depends;
+            UpdateSourcesConnectionStrings($"AppServerUri=http://{Hostname}:3142/dsf;WebServerPort=3142;AuthenticationType=User;UserName=WarewolfAdmin;Password=W@rEw0lf@dm1n;", knownServerSources);
         }
         else
         {
-            var defaultServer = GetIPAddress("tst-ci-remote.premier.local");
+            var defaultServer = GetIPAddress("tst-ci-remote");
             if (defaultServer != null)
             {
                 UpdateSourcesConnectionStrings($"AppServerUri=http://{defaultServer}:3142/dsf;WebServerPort=3142;AuthenticationType=Windows", knownServerSources);
                 Thread.Sleep(30000);
             }
         }
-        return null;
     }
 
-    public static Depends StartRemoteMSSQLContainer(bool EnableDocker)
+    void StartRemoteMSSQLContainer(bool EnableDocker)
     {
         var knownMssqlServerSources = new List<string>()
             {
+                @"%programdata%\Warewolf\Resources\Sources\Database\NewSqlServerSource.bite",
                 @"%programdata%\Warewolf\Resources\Sources\Database\NewSqlServerSource.xml",
                 @"%programdata%\Warewolf\Resources\Sources\Database\TestDb.bite",
+                @"%programdata%\Warewolf\Resources\Sources\Database\TestDb.xml",
+                @"%programdata%\Warewolf\Resources\Sources\Database\NewSqlBulkInsertSource.bite",
                 @"%programdata%\Warewolf\Resources\Sources\Database\NewSqlBulkInsertSource.xml"
             };
         if (EnableDocker)
         {
-            var Depends = new Depends(ContainerType.MSSQL, "test-mssql");
-            UpdateSourcesConnectionStrings($"Data Source={Depends.Hostname},1433;Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;", knownMssqlServerSources);
+            UpdateSourcesConnectionStrings($"Data Source={Hostname},1433;Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;", knownMssqlServerSources);
             Thread.Sleep(30000);
-            return Depends;
         }
         else
         {
-            var defaultServer = GetIPAddress("SVRDEV.premier.local");
+            var defaultServer = GetIPAddress("RSAKLFSVRDEV");
             if (defaultServer != null)
             {
                 UpdateSourcesConnectionStrings($"Data Source={defaultServer},1433;Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;", knownMssqlServerSources);
                 Thread.Sleep(30000);
             }
         }
-        return null;
     }
 
-    public static Depends StartRemoteRabbitMQContainer(bool EnableDocker)
+    void StartRemoteRabbitMQContainer(bool EnableDocker)
     {
         var knownServerSources = new List<string>()
             {
-                @"%programdata%\Warewolf\Resources\Sources\Database\NewSqlServerSource.xml",
-                @"%programdata%\Warewolf\Resources\Sources\Database\TestDb.bite"
+                @"%programdata%\Warewolf\Resources\Sources\RabbitMq\testRabbitMQSource.bite",
+                @"%programdata%\Warewolf\Resources\Sources\RabbitMq\testRabbitMQSource.xml"
             };
         if (EnableDocker)
         {
-            var Depends = new Depends(ContainerType.RabbitMQ, "test-rabbitmq");
-            UpdateSourcesConnectionStrings($"HostName={Depends.Hostname};Port=5672;UserName=guest;Password=guest;VirtualHost=/", knownServerSources);
+            UpdateSourcesConnectionStrings($"HostName={Hostname};Port=5672;UserName=guest;Password=guest;VirtualHost=/", knownServerSources);
             Thread.Sleep(120000);
-            return Depends;
         }
         else
         {
-            var defaultServer = GetIPAddress("SVRDEV.premier.local");
+            var defaultServer = GetIPAddress("rsaklfsvrdev.dev2.local");
             if (defaultServer != null)
             {
                 UpdateSourcesConnectionStrings($"HostName={defaultServer};Port=5672;UserName=test;Password=test;VirtualHost=/", knownServerSources);
                 Thread.Sleep(30000);
             }
         }
-        return null;
     }
 
-    public static Depends StartRemoteMySQLContainer(bool EnableDocker)
+    void StartRemoteMySQLContainer(bool EnableDocker)
     {
         if (EnableDocker)
         {
-            var Depends = new Depends(ContainerType.MySQL, "test-mysql");
-            UpdateSourcesConnectionString(Depends.Hostname, @"%programdata%\Warewolf\Resources\Sources\Database\NewMySqlSource.bite");
+            UpdateSourcesConnectionString(Hostname, @"%programdata%\Warewolf\Resources\Sources\Database\NewMySqlSource.bite");
             Thread.Sleep(30000);
-            return Depends;
         }
         else
         {
-            var defaultServer = GetIPAddress("SVRDEV.premier.local");
+            var defaultServer = GetIPAddress("RSAKLFSVRDEV.dev2.local");
             if (defaultServer != null)
             {
                 UpdateSourcesConnectionString(defaultServer, @"%programdata%\Warewolf\Resources\Sources\Database\NewMySqlSource.bite");
                 Thread.Sleep(30000);
             }
         }
-        return null;
     }
 
     static void UpdateSourcesConnectionString(string defaultServer, string knownServerSource)
@@ -264,4 +290,20 @@ public class Depends: System.Attribute, IDisposable
         }
         return serverSourceXML.Substring(0, startIndex) + newConnectionString + serverSourceXML.Substring(startIndex, serverSourceXML.Length - startIndex);
     }
-} 
+}
+
+class NewContainer
+{
+    public string ID { get; set; }
+    public string IP { get; set; }
+}
+
+class WebClientWithExtendedTimeout : WebClient
+{
+    protected override WebRequest GetWebRequest(Uri uri)
+    {
+        WebRequest w = base.GetWebRequest(uri);
+        w.Timeout = 20 * 60 * 1000;
+        return w;
+    }
+}
