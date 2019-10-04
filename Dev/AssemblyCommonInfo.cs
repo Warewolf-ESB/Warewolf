6c058d1a578cee5cc534a276153ae6c538c2cf2b
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-10-04 11:05:33 +0200

    395 use more verbose versioning for debug builds

--- Dev/AssemblyCommonInfo.cs
+++ Dev/AssemblyCommonInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
 [assembly: InternalsVisibleTo("Dev2.Runtime.Tests")]
 [assembly: InternalsVisibleTo("Dev2.Studio.Core.Tests")]
 [assembly: InternalsVisibleTo("Dev2.TaskScheduler.Wrappers")]
