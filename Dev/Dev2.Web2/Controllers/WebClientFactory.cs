224320a49dda9d5bc90e5a5e145bba9583ab0c34
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-10-07 09:29:07 +0200

    5946-AuditControllerTests UnAuthorizedUser test fix

---
+++ Dev/Dev2.Web2/Controllers/WebClientFactory.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace Dev2.Web2.Controllers
+{
+    public interface IWebClientFactory
+    {
+        IWebClientWrapper New(string userName, string password);
+    }
+
+    public class WebClientFactory : IWebClientFactory
+    {
+        public IWebClientWrapper New(string userName, string password)
+        {
+            return new WebClientWrapper(userName, password);
+        }
+    }
+
+    public interface IWebClientWrapper : IDisposable
+    {
+        byte[] UploadValues(string wareWolfResumeUrl, string method, NameValueCollection nameValueCollection);
+    }
+
+    public class WebClientWrapper : IWebClientWrapper
+    {
+        private readonly WebClient _webClient;
+
+        public WebClientWrapper(string userName, string password)
+        {
+            _webClient = new WebClient
+            {
+                Credentials = new NetworkCredential(userName, password)
+            };
+        }
+
+        public byte[] UploadValues(string wareWolfResumeUrl, string method, NameValueCollection nameValueCollection)
+        {
+            return _webClient.UploadValues(wareWolfResumeUrl, method, nameValueCollection);
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                _webClient.Dispose();
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
