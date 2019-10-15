d47678b12df10626b2f774e489bb8599c1d2b031
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2019-10-15 12:02:56 +0200

    5956 Add Small view to GateDesigner

--- Dev/Dev2.Activities.Designers/Designers2/Gate/Small.xaml.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Gate/Small.xaml.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
 
 namespace Dev2.Activities.Designers2.Gate
 {
-    /// <summary>
-    /// Interaction logic for Small.xaml
-    /// </summary>
-    public partial class Small : UserControl
+    public partial class Small
     {
         public Small()
         {
