52241c3f923f46310bae75eee868749aa5fe1fac
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2019-10-16 10:30:55 +0200

    5956 Missing code for compile break

--- Dev/Dev2.Activities.Designers/Designers2/Gate/GateDesignerViewModel.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Gate/GateDesignerViewModel.cs
@@ -8,11 +8,15 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using Dev2.Activities.Designers2.Core;
+using System.Activities.Presentation.Model;
+
 namespace Dev2.Activities.Designers2.Gate
 {
-    public class GateDesignerViewModel
+    public class GateDesignerViewModel : ActivityDesignerViewModel
     {
-        public GateDesignerViewModel()
+        public GateDesignerViewModel(ModelItem modelItem)
+            : base(modelItem)
         {
             ImagePath = "pack://application:,,,/Warewolf Studio;component/Images/gate-open.png";
             GateFailure = "Retry [Gate]";
@@ -20,5 +24,10 @@ namespace Dev2.Activities.Designers2.Gate
 
         public string ImagePath { get; set; }
         public string GateFailure { get; set; }
+
+        public override void UpdateHelpDescriptor(string helpText)
+        {
+            
+        }
     }
 }
