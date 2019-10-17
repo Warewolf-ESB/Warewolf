389d0c7934964e030d7faba112709b27a7baac5f
Candice Daniel <candice.daniel@dev2.co.za>
2019-10-17 09:15:50 +0200

    5956-Added Enums GateFailureOptions and GateFailureOptionsHelper

--- Dev/Dev2.Activities.Designers.Tests/Gate/GateDesignerViewModelTests.cs
+++ Dev/Dev2.Activities.Designers.Tests/Gate/GateDesignerViewModelTests.cs
@@ -8,11 +8,12 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
-using System.Activities.Presentation.Model;
 using Dev2.Activities.Designers2.Gate;
+using Dev2.Common.Interfaces.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Activities.Presentation.Model;
+using System.Linq;
 
 namespace Dev2.Activities.Designers.Tests.Gate
 {
@@ -26,13 +27,59 @@ namespace Dev2.Activities.Designers.Tests.Gate
         {
             //------------Setup for test--------------------------
             var expectedImagePath = "pack://application:,,,/Warewolf Studio;component/Images/gate-open.png";
-            var expectedGateFailure = "Retry [Gate]";
+            var expectedGateFailure = "Retry";
             var mockModelItem = new Mock<ModelItem>();
             //------------Execute Test----------------------------
             var gateDesignerViewModel = new GateDesignerViewModel(mockModelItem.Object);
             //------------Assert Results--------------------------
             Assert.AreEqual(expectedImagePath, gateDesignerViewModel.ImagePath);
-            Assert.AreEqual(expectedGateFailure, gateDesignerViewModel.GateFailure);
+            Assert.AreEqual(expectedGateFailure, gateDesignerViewModel.GateFailure.ToString());
+        }
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(GateDesignerViewModel))]
+        public void GateDesignerViewModel_GateFailureOptions_Retry()
+        {
+            //------------Setup for test--------------------------
+            var mockModelItem = new Mock<ModelItem>();
+            CustomContainer.Register(mockModelItem.Object);
+            var gateDesignerViewModel = new GateDesignerViewModel(mockModelItem.Object);
+            //------------Execute Test---------------------------
+            gateDesignerViewModel.SelectedGateFailure = "Retry: Retry execution on error";
+            //------------Assert Results-------------------------
+            Assert.AreEqual("Retry: Retry execution on error", gateDesignerViewModel.SelectedGateFailure);
+            Assert.AreEqual(GateFailureOptions.Retry, gateDesignerViewModel.GateFailure);
+        }
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(GateDesignerViewModel))]
+        public void GateDesignerViewModel_GateFailureOptions_StopOnError()
+        {
+            //------------Setup for test--------------------------
+            var mockModelItem = new Mock<ModelItem>();
+            CustomContainer.Register(mockModelItem.Object);
+            var gateDesignerViewModel = new GateDesignerViewModel(mockModelItem.Object);
+            //------------Execute Test---------------------------
+            gateDesignerViewModel.SelectedGateFailure = "StopOnError: Stop execution on error";
+            //------------Assert Results-------------------------
+            Assert.AreEqual("StopOnError: Stop execution on error", gateDesignerViewModel.SelectedGateFailure);
+            Assert.AreEqual(GateFailureOptions.StopOnError, gateDesignerViewModel.GateFailure);
+        }
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(GateDesignerViewModel))]
+        public void GateDesignerViewModel_GateFailureOptions_List()
+        {
+            //------------Setup for test--------------------------
+            var mockModelItem = new Mock<ModelItem>();
+            CustomContainer.Register(mockModelItem.Object);
+            var gateDesignerViewModel = new GateDesignerViewModel(mockModelItem.Object);
+            //------------Execute Test---------------------------
+
+            //------------Assert Results-------------------------
+            var list = gateDesignerViewModel.GateFailureOptions.ToList();
+            Assert.AreEqual("Retry: Retry execution on error", list[0]);
+            Assert.AreEqual("StopOnError: Stop execution on error", list[1]);
         }
     }
 }
