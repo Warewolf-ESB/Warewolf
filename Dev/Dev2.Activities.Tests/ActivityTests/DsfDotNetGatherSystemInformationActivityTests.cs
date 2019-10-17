16db30cc5d15722334b9309c1b6f0f42a221bbf2
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-10-17 08:15:11 +0200

    395 revert retry

--- Dev/Dev2.Activities.Tests/ActivityTests/DsfDotNetGatherSystemInformationActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DsfDotNetGatherSystemInformationActivityTests.cs
@@ -64,129 +64,90 @@ namespace Dev2.Tests.Activities.ActivityTests
             var computer = env.EvalAsListOfStrings(outputs[0], 0);
 
             var computerName = env.EvalAsListOfStrings("[[computerName]]", 0);
-            var operatingSystem = env.EvalAsListOfStrings("[[operatingSystem]]", 0);
-            var operatingSystemVersion = env.EvalAsListOfStrings("[[operatingSystemVersion]]", 0);
-            var servicePack = env.EvalAsListOfStrings("[[servicePack]]", 0);
-            var oSBitValue = env.EvalAsListOfStrings("[[oSBitValue]]", 0);
-            var fullDateTime = env.EvalAsListOfStrings("[[fullDateTime]]", 0);
-            var dateTimeFormat = env.EvalAsListOfStrings("[[dateTimeFormat]]", 0);
-            var diskAvailable = env.EvalAsListOfStrings("[[diskAvailable]]", 0);
-            var diskTotal = env.EvalAsListOfStrings("[[diskTotal]]", 0);
-            var virtualMemoryAvailable = env.EvalAsListOfStrings("[[virtualMemoryAvailable]]", 0);
-            var virtualMemoryTotal = env.EvalAsListOfStrings("[[virtualMemoryTotal]]", 0);
-            var physicalMemoryAvailable = env.EvalAsListOfStrings("[[physicalMemoryAvailable]]", 0);
-            var physicalMemoryTotal = env.EvalAsListOfStrings("[[physicalMemoryTotal]]", 0);
-            var cPUAvailable = env.EvalAsListOfStrings("[[cPUAvailable]]", 0);
-            var cPUTotal = env.EvalAsListOfStrings("[[cPUTotal]]", 0);
-            var language = env.EvalAsListOfStrings("[[language]]", 0);
-            var region = env.EvalAsListOfStrings("[[region]]", 0);
-            var userRoles = env.EvalAsListOfStrings("[[userRoles]]", 0);
-            var userName = env.EvalAsListOfStrings("[[userName]]", 0);
-            var domain = env.EvalAsListOfStrings("[[domain]]", 0);
-            var numberOfServerNICS = env.EvalAsListOfStrings("[[numberOfServerNICS]]", 0);
-            var macAddress = env.EvalAsListOfStrings("[[macAddress]]", 0);
-            var gateWayAddress = env.EvalAsListOfStrings("[[gateWayAddress]]", 0);
-            var dNSAddress = env.EvalAsListOfStrings("[[dNSAddress]]", 0);
-            var iPv4Address = env.EvalAsListOfStrings("[[iPv4Address]]", 0);
-            var iPv6Address = env.EvalAsListOfStrings("[[iPv6Address]]", 0);
-            var warewolfMemory = env.EvalAsListOfStrings("[[warewolfMemory]]", 0);
-            var warewolfCPU = env.EvalAsListOfStrings("[[warewolfCPU]]", 0);
-            var warewolfServerVersion = env.EvalAsListOfStrings("[[warewolfServerVersion]]", 0);
-
-            if (System.Environment.MachineName != computerName[0] || 
-                string.IsNullOrWhiteSpace(operatingSystem[0]) ||
-                string.IsNullOrWhiteSpace(operatingSystemVersion[0]) ||
-                1 != servicePack.Count ||
-                string.IsNullOrWhiteSpace(oSBitValue[0]) ||
-                string.IsNullOrWhiteSpace(fullDateTime[0]) ||
-                string.IsNullOrWhiteSpace(dateTimeFormat[0]) ||
-                string.IsNullOrWhiteSpace(diskAvailable[0]) ||
-                string.IsNullOrWhiteSpace(diskTotal[0]) ||
-                string.IsNullOrWhiteSpace(virtualMemoryAvailable[0]) ||
-                string.IsNullOrWhiteSpace(virtualMemoryTotal[0]) ||
-                string.IsNullOrWhiteSpace(physicalMemoryAvailable[0]) ||
-                string.IsNullOrWhiteSpace(physicalMemoryTotal[0]) ||
-                string.IsNullOrWhiteSpace(cPUAvailable[0]) ||
-                string.IsNullOrWhiteSpace(cPUTotal[0]) ||
-                string.IsNullOrWhiteSpace(language[0]) ||
-                string.IsNullOrWhiteSpace(region[0]) ||
-                string.IsNullOrWhiteSpace(userRoles[0]) ||
-                string.IsNullOrWhiteSpace(userName[0]) ||
-                string.IsNullOrWhiteSpace(domain[0]) ||
-                string.IsNullOrWhiteSpace(numberOfServerNICS[0]) ||
-                string.IsNullOrWhiteSpace(macAddress[0]) ||
-                string.IsNullOrWhiteSpace(gateWayAddress[0]) ||
-                string.IsNullOrWhiteSpace(dNSAddress[0]) ||
-                string.IsNullOrWhiteSpace(iPv4Address[0]) ||
-                string.IsNullOrWhiteSpace(iPv6Address[0]) ||
-                string.IsNullOrWhiteSpace(warewolfMemory[0]) ||
-                string.IsNullOrWhiteSpace(warewolfCPU[0]) ||
-                string.IsNullOrWhiteSpace(warewolfServerVersion[0]))
-            {
-                ob.Execute(data.Object, 0);
-
-                outputs = ob.GetOutputs();
-                computer = env.EvalAsListOfStrings(outputs[0], 0);
-
-                computerName = env.EvalAsListOfStrings("[[computerName]]", 0);
-                operatingSystem = env.EvalAsListOfStrings("[[operatingSystem]]", 0);
-                operatingSystemVersion = env.EvalAsListOfStrings("[[operatingSystemVersion]]", 0);
-                servicePack = env.EvalAsListOfStrings("[[servicePack]]", 0);
-                oSBitValue = env.EvalAsListOfStrings("[[oSBitValue]]", 0);
-                fullDateTime = env.EvalAsListOfStrings("[[fullDateTime]]", 0);
-                dateTimeFormat = env.EvalAsListOfStrings("[[dateTimeFormat]]", 0);
-                diskAvailable = env.EvalAsListOfStrings("[[diskAvailable]]", 0);
-                diskTotal = env.EvalAsListOfStrings("[[diskTotal]]", 0);
-                virtualMemoryAvailable = env.EvalAsListOfStrings("[[virtualMemoryAvailable]]", 0);
-                virtualMemoryTotal = env.EvalAsListOfStrings("[[virtualMemoryTotal]]", 0);
-                physicalMemoryAvailable = env.EvalAsListOfStrings("[[physicalMemoryAvailable]]", 0);
-                physicalMemoryTotal = env.EvalAsListOfStrings("[[physicalMemoryTotal]]", 0);
-                cPUAvailable = env.EvalAsListOfStrings("[[cPUAvailable]]", 0);
-                cPUTotal = env.EvalAsListOfStrings("[[cPUTotal]]", 0);
-                language = env.EvalAsListOfStrings("[[language]]", 0);
-                region = env.EvalAsListOfStrings("[[region]]", 0);
-                userRoles = env.EvalAsListOfStrings("[[userRoles]]", 0);
-                userName = env.EvalAsListOfStrings("[[userName]]", 0);
-                domain = env.EvalAsListOfStrings("[[domain]]", 0);
-                numberOfServerNICS = env.EvalAsListOfStrings("[[numberOfServerNICS]]", 0);
-                macAddress = env.EvalAsListOfStrings("[[macAddress]]", 0);
-                gateWayAddress = env.EvalAsListOfStrings("[[gateWayAddress]]", 0);
-                dNSAddress = env.EvalAsListOfStrings("[[dNSAddress]]", 0);
-                iPv4Address = env.EvalAsListOfStrings("[[iPv4Address]]", 0);
-                iPv6Address = env.EvalAsListOfStrings("[[iPv6Address]]", 0);
-                warewolfMemory = env.EvalAsListOfStrings("[[warewolfMemory]]", 0);
-                warewolfCPU = env.EvalAsListOfStrings("[[warewolfCPU]]", 0);
-                warewolfServerVersion = env.EvalAsListOfStrings("[[warewolfServerVersion]]", 0);
-            }
-
             Assert.AreEqual(System.Environment.MachineName, computerName[0], "[[computerName]]");
+
+            var operatingSystem = env.EvalAsListOfStrings("[[operatingSystem]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(operatingSystem[0]), "[[operatingSystem]]");
+
+            var operatingSystemVersion = env.EvalAsListOfStrings("[[operatingSystemVersion]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(operatingSystemVersion[0]), "[[operatingSystemVersion]]");
+
+            var servicePack = env.EvalAsListOfStrings("[[servicePack]]", 0);
             Assert.AreEqual(1, servicePack.Count, "[[servicePack]]");
+
+            var oSBitValue = env.EvalAsListOfStrings("[[oSBitValue]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(oSBitValue[0]), "[[oSBitValue]]");
+
+            var fullDateTime = env.EvalAsListOfStrings("[[fullDateTime]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(fullDateTime[0]), "[[fullDateTime]]");
+
+            var dateTimeFormat = env.EvalAsListOfStrings("[[dateTimeFormat]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(dateTimeFormat[0]), "[[dateTimeFormat]]");
+
+            var diskAvailable = env.EvalAsListOfStrings("[[diskAvailable]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(diskAvailable[0]), "[[diskAvailable]]");
+
+            var diskTotal = env.EvalAsListOfStrings("[[diskTotal]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(diskTotal[0]), "[[diskTotal]]");
+
+            var virtualMemoryAvailable = env.EvalAsListOfStrings("[[virtualMemoryAvailable]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(virtualMemoryAvailable[0]), "[[virtualMemoryAvailable]]");
+
+            var virtualMemoryTotal = env.EvalAsListOfStrings("[[virtualMemoryTotal]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(virtualMemoryTotal[0]), "[[virtualMemoryTotal]]");
+
+            var physicalMemoryAvailable = env.EvalAsListOfStrings("[[physicalMemoryAvailable]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(physicalMemoryAvailable[0]), "[[physicalMemoryAvailable]]");
+
+            var physicalMemoryTotal = env.EvalAsListOfStrings("[[physicalMemoryTotal]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(physicalMemoryTotal[0]), "[[physicalMemoryTotal]]");
+
+            var cPUAvailable = env.EvalAsListOfStrings("[[cPUAvailable]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(cPUAvailable[0]), "[[cPUAvailable]]");
+
+            var cPUTotal = env.EvalAsListOfStrings("[[cPUTotal]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(cPUTotal[0]), "[[cPUTotal]]");
+
+            var language = env.EvalAsListOfStrings("[[language]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(language[0]), "[[language]]");
+
+            var region = env.EvalAsListOfStrings("[[region]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(region[0]), "[[region]]");
+
+            var userRoles = env.EvalAsListOfStrings("[[userRoles]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(userRoles[0]), "[[userRoles]]");
+
+            var userName = env.EvalAsListOfStrings("[[userName]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(userName[0]), "[[userName]]");
+
+            var domain = env.EvalAsListOfStrings("[[domain]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(domain[0]), "[[domain]]");
+
+            var numberOfServerNICS = env.EvalAsListOfStrings("[[numberOfServerNICS]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(numberOfServerNICS[0]), "[[numberOfServerNICS]]");
+
+            var macAddress = env.EvalAsListOfStrings("[[macAddress]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(macAddress[0]), "[[macAddress]]");
+
+            var gateWayAddress = env.EvalAsListOfStrings("[[gateWayAddress]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(gateWayAddress[0]), "[[gateWayAddress]]");
+
+            var dNSAddress = env.EvalAsListOfStrings("[[dNSAddress]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(dNSAddress[0]), "[[dNSAddress]]");
+
+            var iPv4Address = env.EvalAsListOfStrings("[[iPv4Address]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(iPv4Address[0]), "[[iPv4Address]]");
+
+            var iPv6Address = env.EvalAsListOfStrings("[[iPv6Address]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(iPv6Address[0]), "[[iPv6Address]]");
+
+            var warewolfMemory = env.EvalAsListOfStrings("[[warewolfMemory]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(warewolfMemory[0]), "[[warewolfMemory]]");
+
+            var warewolfCPU = env.EvalAsListOfStrings("[[warewolfCPU]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(warewolfCPU[0]), "[[warewolfCPU]]");
+
+            var warewolfServerVersion = env.EvalAsListOfStrings("[[warewolfServerVersion]]", 0);
             Assert.IsFalse(string.IsNullOrWhiteSpace(warewolfServerVersion[0]), "[[warewolfServerVersion]]");
         }
 
