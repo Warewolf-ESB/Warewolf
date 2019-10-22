00bdb67c325d36134d9740ba94e0bd886b448179
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2019-10-22 11:25:55 +0200

    5988 Redis implementation

--- Dev/Dev2.Common.Interfaces/IRedisServiceSource.cs
+++ Dev/Dev2.Common.Interfaces/IRedisServiceSource.cs
@@ -8,17 +8,19 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System;
-using Dev2.Common.Interfaces.Data;
 using Dev2.Runtime.ServiceModel.Data;
+using System;
 
-namespace Dev2.Common.Interfaces.ServerProxyLayer
+namespace Dev2.Common.Interfaces
 {
-    public interface IRedisSource : IResource
+    public interface IRedisServiceSource : IEquatable<IRedisServiceSource>
     {
         string HostName { get; set; }
         string Password { get; set; }
         AuthenticationType AuthenticationType { get; set; }
         string Port { get; set; }
+        string Name { get; set; }
+        string Path { get; set; }
+        Guid Id { get; set; }
     }
 }
