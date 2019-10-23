621ba68b34b3ebb7bfc2d30db7ba0c727f55e3fc
Candice Daniel <candice.daniel@dev2.co.za>
2019-10-23 11:53:54 +0200

    5956-Added test classes back

---
+++ Dev/Warewolf.Data.Tests/Options/OptionTests.cs
@@ -0,0 +1,285 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Warewolf.Options;
+
+namespace Warewolf.Data.Tests
+{
+    [TestClass]
+    public class OptionTests
+    {
+        [TestMethod]
+        [TestCategory(nameof(OptionBool))]
+        [Owner("Pieter Terblanche")]
+        public void OptionBool_Default()
+        {
+            var optionBool = new OptionBool();
+
+            Assert.IsNull(optionBool.Name);
+            optionBool.Name = "Durable";
+            Assert.AreEqual("Durable", optionBool.Name);
+
+            Assert.IsFalse(optionBool.Value);
+            optionBool.Value = true;
+            Assert.IsTrue(optionBool.Value);
+
+            Assert.IsTrue(optionBool.Default);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionBool))]
+        [Owner("Pieter Terblanche")]
+        public void OptionBool_Clone()
+        {
+            var optionBool = new OptionBool
+            {
+                Name = "Durable",
+                Value = true
+            };
+
+            var cloneOptionBool = optionBool.Clone() as OptionBool;
+            Assert.AreEqual(optionBool.Name, cloneOptionBool.Name);
+            Assert.AreEqual(optionBool.Value, cloneOptionBool.Value);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionBool))]
+        [Owner("Pieter Terblanche")]
+        public void OptionBool_CompareTo()
+        {
+            var optionBool = new OptionBool
+            {
+                Name = "Durable",
+                Value = true
+            };
+
+            var expectedValue = optionBool.CompareTo(null);
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionBool.CompareTo(new object { });
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionBool.CompareTo(optionBool);
+            Assert.AreEqual(0, expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionInt))]
+        [Owner("Pieter Terblanche")]
+        public void OptionInt_Default()
+        {
+            var optionInt = new OptionInt();
+
+            Assert.IsNull(optionInt.Name);
+            optionInt.Name = "MaxAllowed";
+            Assert.AreEqual("MaxAllowed", optionInt.Name);
+
+            Assert.AreEqual(0, optionInt.Value);
+            optionInt.Value = 10;
+            Assert.AreEqual(10, optionInt.Value);
+
+            Assert.AreEqual(0, optionInt.Default);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionInt))]
+        [Owner("Pieter Terblanche")]
+        public void OptionInt_Clone()
+        {
+            var optionInt = new OptionInt
+            {
+                Name = "MaxAllowed",
+                Value = 10
+            };
+
+            var cloneOptionBool = optionInt.Clone() as OptionInt;
+            Assert.AreEqual(optionInt.Name, cloneOptionBool.Name);
+            Assert.AreEqual(optionInt.Value, cloneOptionBool.Value);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionInt))]
+        [Owner("Pieter Terblanche")]
+        public void OptionInt_CompareTo()
+        {
+            var optionInt = new OptionInt
+            {
+                Name = "MaxAllowed",
+                Value = 10
+            };
+
+            var expectedValue = optionInt.CompareTo(null);
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionInt.CompareTo(new object { });
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionInt.CompareTo(optionInt);
+            Assert.AreEqual(0, expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionAutocomplete))]
+        [Owner("Pieter Terblanche")]
+        public void OptionAutocomplete_Default()
+        {
+            var optionAutocomplete = new OptionAutocomplete();
+
+            Assert.IsNull(optionAutocomplete.Name);
+            optionAutocomplete.Name = "Suggestions";
+            Assert.AreEqual("Suggestions", optionAutocomplete.Name);
+
+            Assert.IsNull(optionAutocomplete.Value);
+            optionAutocomplete.Value = "Item1";
+            Assert.AreEqual("Item1", optionAutocomplete.Value);
+
+            Assert.AreEqual(string.Empty, optionAutocomplete.Default);
+            Assert.IsNull(optionAutocomplete.Suggestions);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionAutocomplete))]
+        [Owner("Pieter Terblanche")]
+        public void OptionAutocomplete_Clone()
+        {
+            var optionAutocomplete = new OptionAutocomplete
+            {
+                Name = "Suggestions",
+                Value = "Item1"
+            };
+
+            var cloneOptionBool = optionAutocomplete.Clone() as OptionAutocomplete;
+            Assert.AreEqual(optionAutocomplete.Name, cloneOptionBool.Name);
+            Assert.AreEqual(optionAutocomplete.Value, cloneOptionBool.Value);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionAutocomplete))]
+        [Owner("Pieter Terblanche")]
+        public void OptionAutocomplete_CompareTo()
+        {
+            var optionAutocomplete = new OptionAutocomplete
+            {
+                Name = "Suggestions",
+                Value = "Item1"
+            };
+
+            var expectedValue = optionAutocomplete.CompareTo(null);
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionAutocomplete.CompareTo(new object { });
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionAutocomplete.CompareTo(optionAutocomplete);
+            Assert.AreEqual(0, expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionEnum))]
+        [Owner("Siphamandla Dube")]
+        public void OptionEnum_Default()
+        {
+            var optionEnum = new OptionEnum();
+
+            Assert.IsNull(optionEnum.Name);
+            optionEnum.Name = "MyEnum";
+            Assert.AreEqual("MyEnum", optionEnum.Name);
+
+            Assert.IsNull(optionEnum.Value);
+            optionEnum.Value = MyEnum.Option2;
+            Assert.AreEqual(MyEnum.Option2, optionEnum.Value);
+
+            Assert.IsNull(optionEnum.Default);
+            optionEnum.Default = MyEnum.Option1;
+            Assert.AreEqual(MyEnum.Option1, optionEnum.Default);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionEnum))]
+        [Owner("Siphamandla Dube")]
+        public void OptionEnum_Clone()
+        {
+            var optionEnum = new OptionEnum
+            {
+                Name = "MyEnum",
+                Value = MyEnum.Option2
+            };
+
+            var cloneOptionEnum = optionEnum.Clone() as OptionEnum;
+            Assert.AreEqual(optionEnum.Name, cloneOptionEnum.Name);
+            Assert.AreEqual(optionEnum.Value, cloneOptionEnum.Value);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionEnum))]
+        [Owner("Siphamandla Dube")]
+        public void OptionEnum_CompareTo()
+        {
+            var optionEnum = new OptionEnum
+            {
+                Name = "MyEnum",
+                Value = MyEnum.Option2
+            };
+
+            var expectedValue = optionEnum.CompareTo(null);
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionEnum.CompareTo(new object { });
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionEnum.CompareTo(optionEnum);
+            Assert.AreEqual(0, expectedValue);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionCombobox))]
+        [Owner("Siphamandla Dube")]
+        public void OptionCombobox_Clone()
+        {
+            var optionAutocomplete = new OptionCombobox
+            {
+                Name = "MyEnum",
+                Value = MyEnum.Option1.ToString()
+            };
+
+            Assert.ThrowsException<NotImplementedException>(() => optionAutocomplete.Clone() as OptionCombobox);
+        }
+
+        [TestMethod]
+        [TestCategory(nameof(OptionCombobox))]
+        [Owner("Siphamandla Dube")]
+        public void OptionCombobox_CompareTo()
+        {
+            var optionCombobox = new OptionCombobox
+            {
+                Name = "MyEnum",
+                Value = MyEnum.Option1.ToString()
+            };
+
+            var expectedValue = optionCombobox.CompareTo(null);
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionCombobox.CompareTo(new object { });
+            Assert.AreEqual(-1, expectedValue);
+
+            expectedValue = optionCombobox.CompareTo(optionCombobox);
+            Assert.AreEqual(0, expectedValue);
+        }
+
+        enum MyEnum
+        {
+            Option1,
+            Option2
+        }
+
+    }
+}
