11659e3771eb37efa68410a5062b055824553fb4
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-10-28 09:25:07 +0200

    395 wrong host name used here for remote sql server in container

--- Dev/Dev2.IntegrationTests/Database Tools Refresh/SqlHelper.cs
+++ Dev/Dev2.IntegrationTests/Database Tools Refresh/SqlHelper.cs
@@ -6,11 +6,11 @@ namespace Dev2.Integration.Tests.Database_Tools_Refresh
 {
     public static class SqlHelper
     {
-        public static int RunSqlCommand(string command)
+        public static int RunSqlCommand(string host, string port, string command)
         {
             using (var trans = new TransactionScope())
             {
-                using (var con = new SqlConnection("Data Source=rsaklfsvrdev.dev2.local,1433;Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;"))
+                using (var con = new SqlConnection($"Data Source={host},{port};Initial Catalog=Dev2TestingDB;User ID=testuser;Password=test123;"))
                 {
                     var cmd = con.CreateCommand();
                     cmd.CommandText = command;
@@ -22,11 +22,11 @@ namespace Dev2.Integration.Tests.Database_Tools_Refresh
             }
         }
 
-        public static int RunOracleSqlCommand(string command)
+        public static int RunOracleSqlCommand(string host, string port, string command)
         {
             using (var trans = new TransactionScope())
             {
-                var conStr1 = "User Id=Testuser;Password=test123;Data Source=rsaklfsvrdev.dev2.local;";
+                var conStr1 = $"User Id=Testuser;Password=test123;Data Source={host},{port};";
                 using (var con = new OracleConnection(conStr1))
                 {
                     var cmd = con.CreateCommand();
