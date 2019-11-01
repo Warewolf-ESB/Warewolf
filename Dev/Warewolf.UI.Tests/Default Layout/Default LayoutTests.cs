f228f666068ec3c2aaf5eb4242d2faa6dbf07d9f
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-11-01 11:07:13 +0200

    395 remove unused reference

--- Dev/Warewolf.UI.Tests/Default Layout/Default LayoutTests.cs
+++ Dev/Warewolf.UI.Tests/Default Layout/Default LayoutTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
