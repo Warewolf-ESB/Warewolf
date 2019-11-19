8a02125421e349ab32f7b8a72c7f64a75f0022df
Siphamandla Dube <siphamandla.dube@dev2.co.za>
2019-11-19 12:13:27 +0200

    5988-ToolboxSearchTags Redis Counter toolbox tooltip

--- Dev/Warewolf.Studio.Resources/Languages/ToolboxSearchTags.Designer.cs
+++ Dev/Warewolf.Studio.Resources/Languages/ToolboxSearchTags.Designer.cs
@@ -709,6 +709,15 @@ namespace Warewolf.Studio.Resources.Languages {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to increment/decrement the number stored at key by one.
+        /// </summary>
+        public static string Tool_Utility_RedisCounter {
+            get {
+                return ResourceManager.GetString("Tool_Utility_RedisCounter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to removes the specified key.
         /// </summary>
         public static string Tool_Utility_RedisDelete {
