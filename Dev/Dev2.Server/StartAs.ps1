78813c6be8670f7e8bbb128f1741678edf2f24a9
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-11-20 09:03:39 +0200

    395 patch for "Missing statement block after else keyword." error

--- Dev/Dev2.Server/StartAs.ps1
+++ Dev/Dev2.Server/StartAs.ps1
@@ -13,11 +13,13 @@ if ($WarewolfServerProcess) {
 	Grant-UserRight -Account "$env:SERVER_USERNAME" -Right SeServiceLogonRight
 	if (Test-Path "C:\Server\Warewolf Server.exe") {
 		sc.exe create "Warewolf Server" start= auto binPath= "C:\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
-	} else if (Test-Path "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe") {
-		sc.exe create "Warewolf Server" start= auto binPath= "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
 	} else {
-		Write-Error -Message "This script expects a Warewolf Server at either C:\Server or C:\Program Files (x86)\Warewolf\Server"
-		exit 1
+		if (Test-Path "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe") {
+			sc.exe create "Warewolf Server" start= auto binPath= "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
+		} else {
+			Write-Error -Message "This script expects a Warewolf Server at either C:\Server or C:\Program Files (x86)\Warewolf\Server"
+			exit 1
+		}
 	}
 	sc.exe start "Warewolf Server"
 	if ($NoExit.IsPresent) {
