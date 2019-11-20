8bcc09657909fc79512df642c511b5711630a053
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2019-11-20 08:07:14 +0200

    395 patch start script to check for server exe and wait for server start

--- Dev/Dev2.Server/StartAs.ps1
+++ Dev/Dev2.Server/StartAs.ps1
@@ -7,8 +7,26 @@ NET localgroup "Administrators" "$env:SERVER_USERNAME" /ADD
 NET localgroup "Warewolf Administrators" "$env:SERVER_USERNAME" /ADD
 Import-Module C:\Server\UserRights.psm1
 Grant-UserRight -Account "$env:SERVER_USERNAME" -Right SeServiceLogonRight
-sc.exe create "Warewolf Server" start= auto binPath= "C:\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
+if (Test-Path "C:\Server\Warewolf Server.exe") {
+	sc.exe create "Warewolf Server" start= auto binPath= "C:\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
+} else if (Test-Path "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe") {
+	sc.exe create "Warewolf Server" start= auto binPath= "C:\Program Files (x86)\Warewolf\Server\Warewolf Server.exe" obj= ".\$env:SERVER_USERNAME" password= $env:SERVER_PASSWORD
+} else {
+	Write-Error -Message "This script expect a Warewolf Server at either C:\Server or C:\Program Files (x86)\Warewolf\Server"
+	exit 1
+}
 sc.exe start "Warewolf Server"
 if ($NoExit.IsPresent) {
     ping -t localhost
+} else {
+	$Counter = 0
+	$CounterMax = 300
+	while (!(Test-Path "C:\Server\serverstarted") -and $Counter -lt $CounterMax) {
+		Start-Sleep 1000
+		$Counter = $Counter + 1
+	}
+	if ($Counter -eq $CounterMax) {
+		Write-Error -Message "Timed out waiting for server to start."
+		exit 1
+	}
 }
