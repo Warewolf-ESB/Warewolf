44c5e2c10adbfb6bc1392096d03fa9db0ead802d
Devaji Chotaliya <Devaji.Chotaliya@theunlimited.co.za>
2019-11-27 15:40:18 +0530

    6083: Develop generic object pool implementation

---
+++ Dev/Warewolf.Interfaces/Pooling/IObjectPool.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Warewolf.Interfaces.Pooling
+{
+    public interface IObjectPool<T> : IDisposable where T : class, new()
+    {
+        T AcquireObject();
+        void ReleaseObject(T concrete);
+    }
+}
