a9f63cef623af8d3f5f3ef36b236c1c5be92574f
Candice Daniel <candice.daniel@dev2.co.za>
2020-01-10 13:44:14 +0200

    635-Added the body to the error message for server log

--- Dev/Warewolf.QueueWorker/LoggingConsumerWrapper.cs
+++ Dev/Warewolf.QueueWorker/LoggingConsumerWrapper.cs
@@ -66,7 +66,7 @@ namespace QueueWorker
                 }
                 else
                 {
-                    _logger.Error($"Failed to execute {_resourceId}");
+                    _logger.Error($"Failed to execute {_resourceId + " " + strBody}");
                     CreateExecutionError(requestForwarderResult, executionId, startDate, endDate, duration);
                 }
 
