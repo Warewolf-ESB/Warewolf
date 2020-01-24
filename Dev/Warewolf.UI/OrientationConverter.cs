2706b67342c35a0ebcee4c54b3f7f798d1008edc
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2020-01-24 14:44:07 +0200

    5746 Orientation and Performance improvement

---
+++ Dev/Warewolf.UI/OrientationConverter.cs
@@ -0,0 +1,38 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using Warewolf.Data;
+
+namespace Warewolf.UI
+{
+    public class OrientationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Orientation orientation)
+            {
+                switch (orientation)
+                {
+                    case Orientation.Horizontal:
+                        return System.Windows.Controls.Orientation.Horizontal;
+                    default:
+                        return System.Windows.Controls.Orientation.Vertical;
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+    }
+}
