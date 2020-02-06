00be2eba05d638bb8dad5922c8e3bfd7e95c3f0c
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2020-02-06 11:08:15 +0200

    6213 Add Following Panel

---
+++ Dev/Dev2.Studio/Settings/Clusters/ClusterViewModel.cs
@@ -0,0 +1,34 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Common.Interfaces;
+using Dev2.Studio.Interfaces;
+
+namespace Dev2.Settings.Clusters
+{
+    public class ClusterViewModel : SettingsItemViewModel, IUpdatesHelp
+    {
+        public ClusterViewModel()
+        {
+            
+        }
+
+        protected override void CloseHelp()
+        {
+            
+        }
+
+        public void UpdateHelpDescriptor(string helpText)
+        {
+            var mainViewModel = CustomContainer.Get<IShellViewModel>();
+            mainViewModel?.HelpViewModel.UpdateHelpText(helpText);
+        }
+    }
+}
