837589be16dc788eb50edc3c171da1cb8ad92e72
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2020-02-07 10:26:59 +0200

    6196 User feedback to "Drop activity here" not allowed

--- Dev/Dev2.Activities.Designers/Designers2/Gate/Large.xaml.cs
+++ Dev/Dev2.Activities.Designers/Designers2/Gate/Large.xaml.cs
@@ -9,16 +9,37 @@
 */
 
 using System.Windows;
+using Dev2.Activities.Utils;
 
 namespace Dev2.Activities.Designers2.Gate
 {
     public partial class Large
     {
+        readonly DropEnabledActivityDesignerUtils _dropEnabledActivityDesignerUtils;
         public Large()
         {
             InitializeComponent();
+            DropPoint.PreviewDrop += DropPoint_OnPreviewDrop;
+            _dropEnabledActivityDesignerUtils = new DropEnabledActivityDesignerUtils();
         }
 
         protected override IInputElement GetInitialFocusElement() => null;
+
+        private void DropPoint_OnPreviewDrop(object sender, DragEventArgs e)
+        {
+            if (DataContext is GateDesignerViewModel viewModel)
+            {
+                if (_dropEnabledActivityDesignerUtils != null)
+                {
+                    var dropEnabled = _dropEnabledActivityDesignerUtils.LimitDragDropOptions(e.Data);
+                    if (!dropEnabled)
+                    {
+                        e.Effects = DragDropEffects.None;
+                        e.Handled = true;
+                    }
+                }
+            }
+            DropPoint.Item = null;
+        }
     }
 }
