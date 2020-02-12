af941baa8d24f91572d5d979f313306e013bef91
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-02-12 11:27:55 +0200

    6213-Add WorkflowExecutionLogging.Feature: Workflow execution Entry point detailed log

---
+++ Dev/Dev2.Activities.Specs/Composition/WorkflowExecutionLoggingSteps.cs
@@ -0,0 +1,42 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going bac
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using TechTalk.SpecFlow;
+
+namespace Dev2.Activities.Specs.Composition
+{
+    [Binding]
+    public class WorkflowExecutionLoggingSteps
+    {
+        [Given(@"a valid workflow")]
+        public void GivenAValidWorkflow()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"workflow execution entry point detailed logs are logged")]
+        public void GivenWorkflowExecutionEntryPointDetailedLogsAreLogged()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a workflow stops on error has no logs")]
+        public void GivenAWorkflowStopsOnErrorHasNoLogs()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"a detailed execution completed log entry is created")]
+        public void ThenADetailedExecutionCompletedLogEntryIsCreated(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
