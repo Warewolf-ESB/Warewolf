7c797e66e66eaaebbcb484c5c48a3a0dfd0474ca
Candice Daniel <candice.daniel@dev2.co.za>
2020-02-13 14:35:34 +0200

    6213-WIP

---
+++ Dev/Warewolf.Driver.Serilog.Tests/Sinks/ElasticConfigTests.cs
@@ -0,0 +1,31 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warewolf.Driver.Serilog;
+
+namespace Warewolf.Tests.Sinks
+{ 
+    [TestClass]
+    public class ElasticConfigTests
+    {
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(SeriLogELasticConfig))]
+        public void SerilogElasticConfig_NoParamConstructor_Returns_Default()
+        {
+            //---------------------------------Arrange-----------------------------
+            var elasticConfig = new SeriLogELasticConfig();
+            //---------------------------------Assert------------------------------
+            Assert.IsNotNull(elasticConfig.Logger);
+            Assert.IsNull(elasticConfig.ServerLoggingAddress);
+        }
+    }
+}
---
+++ Dev/Warewolf.Driver.Serilog/Sinks/ElasticConfig.cs
@@ -0,0 +1,80 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Warewolf.Driver.Serilog
+{
+    public class SeriLogELasticConfig : ISeriLogConfig
+    {
+        static readonly Lazy<ILogger> _logger = new Lazy<ILogger>(() => new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    MinimumLogEventLevel = LogEventLevel.Verbose,
+                    AutoRegisterTemplate = true,
+                    CustomFormatter = new ElasticsearchJsonFormatter()
+                })
+                .CreateLogger());
+
+        static readonly Settings _staticSettings = new Settings();
+
+        readonly Settings _config;
+
+        public SeriLogELasticConfig()
+        {
+            _config = new Settings();
+        }
+
+        public SeriLogELasticConfig(Settings elasticConfig)
+        {
+            _config = elasticConfig;
+        }
+
+        public ILogger Logger { get => _logger.Value; }
+        //TODO: this path needs to come the Config.Server.AuditPath which is still tobe moved to project Framework48
+        public string ConnectionString { get => _config.ConnectionString; }  
+
+        public string ServerLoggingAddress { get; set; }
+
+        private ILogger CreateLogger()
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    MinimumLogEventLevel = LogEventLevel.Verbose,
+                    AutoRegisterTemplate = true,
+                    CustomFormatter = new ElasticsearchJsonFormatter()
+                })
+                .CreateLogger();
+        }
+        public class Settings
+        {
+            public Settings()
+            {
+            }
+            public string ConnectionString { get; set; }
+            public string Database { get; set; }
+            public string Path { get; set; }
+            public string TableName { get; set; }
+            public LogEventLevel RestrictedToMinimumLevel { get; set; }
+            public IFormatProvider FormatProvider { get; set; }
+            public bool StoreTimestampInUtc { get; set; }
+            public TimeSpan? RetentionPeriod { get; set; }
+        }
+    }
+}
