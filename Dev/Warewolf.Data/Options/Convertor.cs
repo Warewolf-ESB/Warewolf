7ec5ddbae4727cffa8bfff637fbbdcca74317a29
Rory McGuire <rory.mcguire@dev2.co.za>
2020-02-18 11:53:36 +0200

    build fix: Netstandard libraries do not contain System.Activities, cannot use ModelItem

--- Dev/Warewolf.Data/Options/Convertor.cs
+++ Dev/Warewolf.Data/Options/Convertor.cs
@@ -13,7 +13,6 @@ using System.Reflection;
 using System.Linq;
 using Warewolf.Data;
 using System;
-using System.Activities;
 using Warewolf.Data.Options;
 
 namespace Warewolf.Options
@@ -112,30 +111,6 @@ namespace Warewolf.Options
                     }
                 };
                 return optionWorkflow;
-            }
-            else if (prop.PropertyType.IsAssignableFrom(typeof(System.Activities.Presentation.Model.ModelItem)))
-            {
-                var optionSelectedActivity = new OptionActivity
-                {
-                    Name = prop.Name,
-                    Value = (System.Activities.Presentation.Model.ModelItem)prop.GetValue(instance),
-                };
-                if (helptextAttr != null)
-                {
-                    optionSelectedActivity.HelpText = helptextAttr.Get();
-                }
-                if (tooltipAttr != null)
-                {
-                    optionSelectedActivity.Tooltip = tooltipAttr.Get();
-                }
-                optionSelectedActivity.PropertyChanged += (o, e) =>
-                {
-                    if (e.PropertyName == nameof(OptionActivity.Value))
-                    {
-                        prop.SetValue(instance, ((OptionActivity)o).Value);
-                    }
-                };
-                return optionSelectedActivity;
             }
             else if (prop.PropertyType.IsAssignableFrom(typeof(bool)))
             {
@@ -336,22 +311,11 @@ namespace Warewolf.Options
                     throw FailedMappingException;
                 }
             }
-            if (prop.PropertyType.IsAssignableFrom(typeof(IWorkflow)))
+            else if (prop.PropertyType.IsAssignableFrom(typeof(IWorkflow)))
             {
                 if (option is OptionWorkflow optionWorkflow)
                 {
                     prop.SetValue(parentInstance, optionWorkflow.Workflow);
-                }
-                else
-                {
-                    throw FailedMappingException;
-                }
-            }
-            if (prop.PropertyType.IsAssignableFrom(typeof(System.Activities.Presentation.Model.ModelItem)))
-            {
-                if (option is OptionActivity optionSelectedActivity)
-                {
-                    prop.SetValue(parentInstance, optionSelectedActivity.Value);
                 }
                 else
                 {
