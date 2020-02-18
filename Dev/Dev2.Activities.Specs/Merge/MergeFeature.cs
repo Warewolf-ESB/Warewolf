6c3668f293dc6c04519617506384fc59bc4a5ea2
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2020-02-18 07:56:58 +0200

    478 remove commented out code

--- Dev/Dev2.Activities.Specs/Merge/MergeFeature.cs
+++ Dev/Dev2.Activities.Specs/Merge/MergeFeature.cs
@@ -57,9 +57,6 @@ namespace Dev2.Activities.Specs.Merge
             CustomContainer.Register(pop.Object);
             localHost = environmentModel.Source;
             localHost.ConnectAsync().Wait(60000);
-#pragma warning disable S125 // Sections of code should not be "commented out"
-            //localHost.ResourceRepository.Load(true);
-#pragma warning restore S125 // Sections of code should not be "commented out"
         }
 
         IServer localHost;
