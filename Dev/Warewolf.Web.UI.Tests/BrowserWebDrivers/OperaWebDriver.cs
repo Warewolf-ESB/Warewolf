f26e91245a292226cc2d82d1c1a51962792d1dc6
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2020-02-19 15:10:44 +0200

    Attempting to fix Opera ui tests

--- Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/OperaWebDriver.cs
+++ Dev/Warewolf.Web.UI.Tests/BrowserWebDrivers/OperaWebDriver.cs
@@ -11,6 +11,8 @@ namespace Warewolf.Web.UI.Tests.BrowserWebDrivers
         public OperaWebDriver() : base(new OperaDriver(driverService, operaOptions, TimeSpan.FromMinutes(3)))
         {
             driverService.Port = 18406;
+            operaOptions.AddAdditionalCapability("browserName", "chrome");
+            operaOptions.AddAdditionalCapability("browserVersion", "62.0");
             operaOptions.AddArguments(new[] { "--user-data-dir=" + Path.Combine(Environment.CurrentDirectory, "WebDriverProfiles", "Opera"), "start-maximized" });
         }
     }
