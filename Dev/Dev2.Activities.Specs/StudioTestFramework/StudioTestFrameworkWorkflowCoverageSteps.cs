c0969ffcc9aff49bed33be93709c19f04f807250
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-02-21 13:43:48 +0200

    6158-Add, StudioTestFrameworkWorkflowCoverage:   scenario show partial coverage of nodes

---
+++ Dev/Dev2.Activities.Specs/StudioTestFramework/StudioTestFrameworkWorkflowCoverageSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Dev2.Activities.Specs.StudioTestFramework
+{
+    [Binding]
+    public class StudioTestFrameworkWorkflowCoverageSteps
+    {
+        [Given(@"a saved test is run")]
+        public void GivenASavedTestIsRun()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"generate test coverage is selected")]
+        public void GivenGenerateTestCoverageIsSelected()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"workflow only tests")]
+        public void GivenWorkflowOnlyTests(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"test coverage is generated")]
+        public void WhenTestCoverageIsGenerated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the covered nodes are")]
+        public void ThenTheCoveredNodesAre(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the test coverage is ""(.*)""")]
+        public void ThenTheTestCoverageIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
