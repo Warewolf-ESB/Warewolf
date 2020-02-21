a07c41ba5163d826a588f5d3e9526b94227c8048
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-02-21 14:23:11 +0200

    6148-Refactor, StudioTestFrameworkWorkflowCoverage:  adding scenario: total nodes covered in workflow

--- Dev/Dev2.Activities.Specs/StudioTestFramework/StudioTestFrameworkWorkflowCoverageSteps.cs
+++ Dev/Dev2.Activities.Specs/StudioTestFramework/StudioTestFrameworkWorkflowCoverageSteps.cs
@@ -41,5 +41,30 @@ namespace Dev2.Activities.Specs.StudioTestFramework
         {
             ScenarioContext.Current.Pending();
         }
+
+        [Given(@"two saved tests ""(.*)"" and ""(.*)""")]
+        public void GivenTwoSavedTestsAnd(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I run all the tests")]
+        public void WhenIRunAllTheTests()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the test coverage is")]
+        public void WhenTheTestCoverageIs(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the total workflow test coverage is ""(.*)""")]
+        public void ThenTheTotalWorkflowTestCoverageIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
     }
 }
