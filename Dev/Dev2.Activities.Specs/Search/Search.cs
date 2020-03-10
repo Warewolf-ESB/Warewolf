dc0f10d4a11309b2105094da9bc47dc74ebcb99f
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-03-10 07:19:49 +0200

    6213-Copyright, Search.feature hooks add copyright

--- Dev/Dev2.Activities.Specs/Search/Search.cs
+++ Dev/Dev2.Activities.Specs/Search/Search.cs
@@ -1,4 +1,14 @@
-﻿using Dev2.Activities.Specs.BaseTypes;
+﻿/*
+*  Warewolf - Once bitten, there's no going bac
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Activities.Specs.BaseTypes;
 using Dev2.Common.Interfaces.Search;
 using Dev2.Common.Search;
 using Dev2.Studio.Core;
