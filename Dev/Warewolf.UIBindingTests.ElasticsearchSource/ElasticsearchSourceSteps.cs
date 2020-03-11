b0f0499cf4108da86cc3a3c70006618105e0660b
Candice Daniel <candice.daniel@dev2.co.za>
2020-03-11 13:27:11 +0200

    6321-Created Elasticseach Source Specs Project

---
+++ Dev/Warewolf.UIBindingTests.ElasticsearchSource/ElasticsearchSourceSteps.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using Dev2.Common.Interfaces;
+using Dev2.Common.Interfaces.RabbitMQ;
+using Dev2.Common.SaveDialog;
+using Dev2.Studio.Core;
+using Dev2.Studio.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TechTalk.SpecFlow;
+using Warewolf.Test.Agent;
+using Warewolf.Studio.Core.Infragistics_Prism_Region_Adapter;
+using Warewolf.Studio.ViewModels;
+using Warewolf.Studio.Views;
+using Warewolf.UIBindingTests.Core;
+using Warewolf.UnitTestAttributes;
+
+
+namespace Warewolf.UIBindingTests.ElasticsearchSource
+{
+    [Binding]
+    public sealed class ElasticsearchSourceSteps
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext context;
+
+        public ElasticsearchSourceSteps(ScenarioContext injectedContext)
+        {
+            context = injectedContext;
+        }
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata 
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            context.Pending();
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            //TODO: implement act (action) logic
+
+            context.Pending();
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            context.Pending();
+        }
+    }
+}
