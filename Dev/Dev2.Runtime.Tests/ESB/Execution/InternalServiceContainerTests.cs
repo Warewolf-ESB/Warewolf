1443e12168b13e2a989237886659d23698621d44
Rory McGuire <rory.mcguire@dev2.co.za>
2020-03-16 15:07:48 +0200

    6047 InternalServiceContainer CanExecute should be able to be overridden to false in endpoints

--- Dev/Dev2.Runtime.Tests/ESB/Execution/InternalServiceContainerTests.cs
+++ Dev/Dev2.Runtime.Tests/ESB/Execution/InternalServiceContainerTests.cs
@@ -1,15 +1,21 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using Dev2.Common.Interfaces.Enums;
 using Dev2.Communication;
 using Dev2.Data.TO;
 using Dev2.DynamicServices.Objects;
 using Dev2.Interfaces;
 using Dev2.Runtime.ESB.Execution;
+using Dev2.Runtime.ESB.Management;
 using Dev2.Runtime.ESB.Management.Services;
 using Dev2.Runtime.Interfaces;
 using Dev2.Workspaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Warewolf.Execution;
 using Warewolf.Resource.Errors;
 using Warewolf.Storage;
 
@@ -117,6 +123,80 @@ namespace Dev2.Tests.Runtime.ESB.Execution
             var execute = internalServiceContainer.Execute(out ErrorResultTO errorResultTO, 1);
             //---------------Test Result -----------------------
             locater.VerifyAll();            
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void InternalServiceContainer_CanExecute_GivenAllowedService_ShouldAllowExecute()
+        {
+            var allowCanExecute = true;
+            var endpointAuthorizationLevel = AuthorizationContext.Any;
+
+            var mockEsbManagementEndpoint = new Mock<IEsbManagementEndpoint>();
+            mockEsbManagementEndpoint.Setup(o => o.CanExecute(It.IsAny<CanExecuteArg>())).Returns(allowCanExecute);
+            var resourceID = Guid.NewGuid();
+            var requestArgs = new Dictionary<string, StringBuilder>();
+            requestArgs.Add("ResourceID", new StringBuilder(resourceID.ToString()));
+            mockEsbManagementEndpoint.Setup(o => o.GetResourceID(requestArgs)).Returns(resourceID);
+            mockEsbManagementEndpoint.Setup(o => o.GetAuthorizationContextForService())
+                .Returns(endpointAuthorizationLevel);
+
+            InternalServiceContainer_CanExecute_GivenAllowedService(mockEsbManagementEndpoint.Object);
+
+            mockEsbManagementEndpoint.Verify(o => o.CanExecute(It.IsAny<CanExecuteArg>()), Times.Once);
+            mockEsbManagementEndpoint.Verify(o => o.Execute(It.IsAny<Dictionary<string,StringBuilder>>(), It.IsAny<IWorkspace>()), Times.Once);
+        }
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        public void InternalServiceContainer_CanExecute_GivenAllowedService_ShouldNotAllowExecute()
+        {
+            var allowCanExecute = false;
+            var endpointAuthorizationLevel = AuthorizationContext.Any;
+
+            var mockEsbManagementEndpoint = new Mock<IEsbManagementEndpoint>();
+            mockEsbManagementEndpoint.Setup(o => o.CanExecute(It.IsAny<CanExecuteArg>())).Returns(allowCanExecute);
+            var resourceID = Guid.NewGuid();
+            var requestArgs = new Dictionary<string, StringBuilder>();
+            requestArgs.Add("ResourceID", new StringBuilder(resourceID.ToString()));
+            mockEsbManagementEndpoint.Setup(o => o.GetResourceID(requestArgs)).Returns(resourceID);
+            mockEsbManagementEndpoint.Setup(o => o.GetAuthorizationContextForService())
+                .Returns(endpointAuthorizationLevel);
+
+            InternalServiceContainer_CanExecute_GivenAllowedService(mockEsbManagementEndpoint.Object);
+
+            mockEsbManagementEndpoint.Verify(o => o.CanExecute(It.IsAny<CanExecuteArg>()), Times.Once);
+            mockEsbManagementEndpoint.Verify(o => o.Execute(It.IsAny<Dictionary<string,StringBuilder>>(), It.IsAny<IWorkspace>()), Times.Never);
+        }
+
+        private void InternalServiceContainer_CanExecute_GivenAllowedService(IEsbManagementEndpoint esbManagementEndpoint)
+        {
+            var p = new Mock<IPrincipal>();
+            var i = new Mock<IIdentity>();
+            i.Setup(o => o.Name).Returns("bob");
+            p.Setup(o => o.Identity).Returns(i.Object);
+            Common.Utilities.OrginalExecutingUser = p.Object;
+            //---------------Set up test pack-------------------
+            const string datalist = "<DataList><scalar1 ColumnIODirection=\"Input\"/><persistantscalar ColumnIODirection=\"Input\"/><rs><f1 ColumnIODirection=\"Input\"/><f2 ColumnIODirection=\"Input\"/></rs><recset><field1/><field2/></recset></DataList>";
+            var serviceAction = new ServiceAction() { DataListSpecification = new StringBuilder(datalist) , ServiceName = "name", Name = "Name"};
+            var dsfObj = new Mock<IDSFDataObject>();
+            dsfObj.Setup(o => o.Environment).Returns(new ExecutionEnvironment());
+            dsfObj.Setup(o => o.Environment.Eval(It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(CommonFunctions.WarewolfEvalResult.NewWarewolfAtomResult(DataStorage.WarewolfAtom.NewDataString("Args")));
+            var mockWorkSpace = new Mock<IWorkspace>();
+            var channel = new Mock<IEsbChannel>();
+            var esbExecuteRequest = new EsbExecuteRequest();
+            var locater = new Mock<IEsbManagementServiceLocator>();
+
+            locater.Setup(loc => loc.LocateManagementService("Name")).Returns(esbManagementEndpoint);
+            var workSpace = mockWorkSpace.Object;
+            var internalServiceContainer = new InternalServiceContainer(serviceAction, dsfObj.Object, workSpace, channel.Object, esbExecuteRequest, locater.Object);
+            //---------------Assert Precondition----------------
+            Assert.AreEqual(4, esbExecuteRequest.Args.Count);
+            //---------------Execute Test ----------------------
+            var execute = internalServiceContainer.Execute(out ErrorResultTO errorResultTO, 1);
+            //---------------Test Result -----------------------
+            locater.VerifyAll();
         }
     }
 }
