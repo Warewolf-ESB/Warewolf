cbbb646927ca998c0242c35a14f2a01ef2722472
Candice Daniel <candice.daniel@dev2.co.za>
2020-03-20 15:29:20 +0200

    6316-These were missed in previous commit

---
+++ Dev/Warewolf.Driver.Serilog.Tests/Sinks/SeriLogElasticsearchTests.cs
@@ -0,0 +1,29 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warewolf.Driver.Serilog;
+
+namespace Warewolf.Driver.Serilog.Tests
+{
+    [TestClass]
+    public class SeriLogElasticsearchTests
+    {
+        [TestMethod]
+        [Owner("Candice Daniel")]
+        [TestCategory(nameof(SeriLogElasticsearchConfig))]
+        public void SeriLogELasticsearchConfig_NoParamConstructor_Returns_Default()
+        {
+            var config = new SeriLogElasticsearchConfig();
+            Assert.IsNotNull(config.Logger);
+            Assert.IsNotNull(config.Endpoint);
+        }
+    }
+}
