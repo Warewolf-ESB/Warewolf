31042b888738c13075503604a55880e87ec16f38
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-03-24 10:59:47 +0200

    6158-Refactor, Workflow.cs: so I be able to get workflow nodes from xamlDefinition

--- Dev/Dev2.Runtime.Services/ServiceModel/Data/Workflow.cs
+++ Dev/Dev2.Runtime.Services/ServiceModel/Data/Workflow.cs
@@ -9,14 +9,38 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using System;
+using System.Activities;
+using System.Activities.Presentation.View;
+using System.Activities.Statements;
+using System.Activities.XamlIntegration;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xaml;
 using System.Xml.Linq;
+using Dev2.Common;
 using Dev2.Common.Common;
 
 namespace Dev2.Runtime.ServiceModel.Data
 {
-    public class Workflow : Resource
+    public interface IWorkflow
+    {
+        string Comment { get; set; }
+        XElement DataList { get; set; }
+        Collection<FlowNode> FlowNodes { get; }
+        string HelpLink { get; set; }
+        string IconPath { get; set; }
+        string Tags { get; set; }
+        List<IWorkflowNode> WorkflowNodes { get; }
+        StringBuilder XamlDefinition { get; set; }
+
+        XElement ToXml();
+    }
+
+    public class Workflow : Resource, IWorkflow
     {
         #region CTOR
 
@@ -37,12 +61,83 @@ namespace Dev2.Runtime.ServiceModel.Data
             HelpLink = xml.ElementSafe("HelpLink");
 
             var action = xml.Descendants("Action").FirstOrDefault();
-            if(action == null)
+            if (action == null)
             {
                 return;
             }
 
             XamlDefinition = action.ElementSafeStringBuilder("XamlDefinition");
+            FlowNodes = GetFlowNodes(action);
+            WorkflowNodes = GetWorkflowNodes();
+        }
+
+        private List<IWorkflowNode> GetWorkflowNodes()
+        {
+            var list = new List<IWorkflowNode>();
+            foreach (var node in FlowNodes)
+            {
+                var nodeType = node.GetType().Name;
+                IDev2Activity activity;
+                switch (node.GetType().Name)
+                {
+                    case nameof(FlowStep):
+                        activity = ((FlowStep)node).Action as IDev2Activity;
+                        list.Add(new WorkflowNode
+                        {
+                            ActivityID = activity.ActivityId,
+                            UniqueID = Guid.Parse(activity.UniqueID),
+                            StepDescription = activity.GetDisplayName()
+                        });
+                        break;
+
+                    case nameof(FlowDecision):
+                        activity = ((FlowDecision)node).Condition as IDev2Activity;
+                        list.Add(new WorkflowNode
+                        {
+                            ActivityID = activity.ActivityId,
+                            UniqueID = Guid.Parse(activity.UniqueID),
+                            StepDescription = activity.GetDisplayName()
+                        });
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            return list;
+        }
+
+        private Collection<FlowNode> GetFlowNodes(XElement action)
+        {
+            var builder = ReadXamlDefinition(action.ElementSafe("XamlDefinition").ToStringBuilder());
+
+            return ((Flowchart)builder.Implementation).Nodes;
+        }
+
+        private ActivityBuilder ReadXamlDefinition(StringBuilder xaml)
+        {
+            try
+            {
+                if (xaml != null && xaml.Length != 0)
+                {
+                    using (var sw = new StringReader(xaml.ToString()))
+                    {
+                        var xamlXmlWriterSettings = new XamlXmlReaderSettings
+                        {
+                            LocalAssembly = System.Reflection.Assembly.GetAssembly(typeof(VirtualizedContainerService))
+                        };
+                        var xw = ActivityXamlServices.CreateBuilderReader(new XamlXmlReader(sw, new XamlSchemaContext(), xamlXmlWriterSettings));
+                        var load = XamlServices.Load(xw);
+                        return load as ActivityBuilder;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Dev2Logger.Error("Error loading XAML: ", e, GlobalConstants.WarewolfError);
+            }
+            return null;
         }
 
         #endregion
@@ -54,6 +149,8 @@ namespace Dev2.Runtime.ServiceModel.Data
         public string IconPath { get; set; }
         public string Tags { get; set; }
         public string HelpLink { get; set; }
+        public Collection<FlowNode> FlowNodes { get; private set; }
+        public List<IWorkflowNode> WorkflowNodes { get; }
 
         #region ToXml
 
@@ -67,7 +164,7 @@ namespace Dev2.Runtime.ServiceModel.Data
             result.Add(new XElement("Tags", Tags ?? string.Empty));
             result.Add(new XElement("HelpLink", HelpLink ?? string.Empty));
             result.Add(DataList);
-            result.Add(new XElement("Action", new XAttribute("Name", "InvokeWorkflow"),new XAttribute("Type", "Workflow"),
+            result.Add(new XElement("Action", new XAttribute("Name", "InvokeWorkflow"), new XAttribute("Type", "Workflow"),
                 serviceDefinition)
                 );
             return result;
