bafafb9602908612f5ea2626a2a3301b8e46f18e
Candice Daniel <candice.daniel@dev2.co.za>
2020-03-31 14:28:53 +0200

    6213- Fixed Tests for Sqlite

--- Dev/Warewolf.Driver.Serilog/SeriLogData.cs
+++ Dev/Warewolf.Driver.Serilog/SeriLogData.cs
@@ -1,6 +1,6 @@
 ﻿/*
 *  Warewolf - Once bitten, there's no going back
-*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
 *  Licensed under GNU Affero General Public License 3.0 or later. 
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
@@ -8,17 +8,17 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-
+using System;
+using System.Collections.Generic;
 using Serilog.Events;
 
 namespace Warewolf.Driver.Serilog
 {
-    public class SeriLogData
+    public class SeriLogLogEvent
     {
-        public string Timestamp { get; set; }
-        public string Message { get; set; }
-        public string NewLine { get; set; }
-        public string Exception { get; set; }
-        public string Level { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string Properties { get; set; }
+        public Exception Exception { get; set; }
+        public LogEventLevel Level { get; set; }
     }
 }
