b97bb59c1adbd4ffe08ba4f04e6cd2c7d8d84418
Candice Daniel <candice.daniel@dev2.co.za>
2020-04-07 20:20:40 +0200

    6158-Styling WiP

--- Dev/Dev2.Runtime.WebServer/ServiceTestCoverageModelHTMLResultBuilder.cs
+++ Dev/Dev2.Runtime.WebServer/ServiceTestCoverageModelHTMLResultBuilder.cs
@@ -8,7 +8,6 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-
 using Dev2.Common.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +17,6 @@ namespace Dev2.Runtime.WebServer
 {
     public static class ServiceTestCoverageModelHTMLResultBuilder
     {
-
         public static void SetupNavBarHtml(this HtmlTextWriter writer, string classValue, string resourcePath)
         {
             writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "10px 10px 20px 10px");
@@ -34,41 +32,90 @@ namespace Dev2.Runtime.WebServer
 
         public static void SetupWorkflowReportsHtml(this IServiceTestCoverageModelTo coverageModelTo, HtmlTextWriter writer, string classValue)
         {
+            writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
             writer.AddAttribute(HtmlTextWriterAttribute.Class, classValue);
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "5px 0 15px 0");
             writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-            writer.Write("Report name - " + coverageModelTo.ReportName + "  Coverage: " + coverageModelTo.CoveragePercentage);
+            double testFailing = (100 - (coverageModelTo.CoveragePercentage * 100)) / 100;
+            double testPassed = coverageModelTo.CoveragePercentage;
 
-            writer.RenderEndTag(); 
+            writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
+            writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-width");
+            writer.AddStyleAttribute(HtmlTextWriterStyle.Width, "80px");
+            writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+            if (testPassed > 0)
+            {
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-passed");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.BackgroundColor, "green");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontWeight, "bold");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "10px");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "white");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.TextAlign, "center");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "5px 0px");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Width, testPassed.ToString("0%"));
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                writer.Write(testPassed.ToString("0%"));
+                writer.RenderEndTag();
+            }
+
+            if (testFailing > 0)
+            {
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-failed");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.BackgroundColor, "red");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontWeight, "bold");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.TextAlign, "center");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "10px");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "white");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "5px 0px");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.Width, testFailing.ToString("0%"));
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                writer.Write(testFailing.ToString("0%"));
+                writer.RenderEndTag();
+            }
+
+            writer.RenderEndTag();
+
+            writer.RenderEndTag();
         }
 
-
+       
         public static void SetupWorkflowNodeHtml(this IWorkflowNode workflowNode, HtmlTextWriter writer, string className, List<IWorkflowNode> coveredNodes)
         {
-            writer.AddAttribute(HtmlTextWriterAttribute.Class, className);
-            writer.RenderBeginTag(HtmlTextWriterTag.Div);
+           
 
             if (IsNodeCovered(coveredNodes, workflowNode))
             {
                 writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "green");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "step-description-green");
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
                 writer.Write(workflowNode.StepDescription);
+                writer.RenderEndTag();
             }
             else
             {
                 writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "red");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "step-description-red");
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
                 writer.Write(workflowNode.StepDescription);
+                writer.RenderEndTag();
             }
-
+           
             if (workflowNode.NextNodes?.Count > 0)
             {
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, "next-nodes");
                 writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
                 workflowNode.NextNodes.ForEach(node => SetupNextNodeHTML(writer, coveredNodes, node));
+                writer.RenderEndTag();
             }
 
-            writer.RenderEndTag();
+           
         }
 
         public static void SetupNextNodeHTML(HtmlTextWriter writer, List<IWorkflowNode> coveredNodes, IWorkflowNode node)
@@ -78,69 +125,20 @@ namespace Dev2.Runtime.WebServer
             if (IsNodeCovered(coveredNodes, node))
             {
                 writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "green");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
                 writer.RenderBeginTag(HtmlTextWriterTag.Li);
             }
             else
             {
                 writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "red");
+                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
                 writer.RenderBeginTag(HtmlTextWriterTag.Li);
             }
 
             writer.Write(node.StepDescription);
             writer.RenderEndTag();
-
         }
-        
-        public static void SetupCountProgressSummaryHtml(this List<IServiceTestModelTO> allTests, HtmlTextWriter writer, string className)
-        {
-            var total = allTests.Count();
-            var testFailing = 0;
-            var testPassed = 0;
-            if (allTests.Count(o => o.TestFailing) > 0)
-            {
-                testFailing = (total / allTests.Count(o => o.TestFailing)) * 100;
-            }
-            if (allTests.Count(o => o.TestPassed) > 0)
-            {
-                testPassed = (total / allTests.Count(o => o.TestPassed)) * 100;
-            }
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
-            writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-width");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Width, "150px");
-            writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-            if (allTests.Count(o => o.TestPassed) > 0)
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-passed");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.BackgroundColor, "green");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.FontWeight, "bold");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "white");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.TextAlign, "center");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "5px 3px");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Width, testPassed + "%");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
-                writer.Write(testPassed + "%");
-                writer.RenderEndTag();
-               
-            }
-
-            if (allTests.Count(o => o.TestFailing) > 0)
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "progress-bar-failed");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.BackgroundColor, "red");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.FontWeight, "bold");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.TextAlign, "center");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "12px");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "white");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "5px 3px");
-                writer.AddStyleAttribute(HtmlTextWriterStyle.Width, testFailing + "%");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
-                writer.Write(testFailing + "%");
-                writer.RenderEndTag();
-            }
-            writer.RenderEndTag();
-        }
 
         public static void SetupCountSummaryHtml(this List<IServiceTestModelTO> allTests, HtmlTextWriter writer, string className)
         {
@@ -148,7 +146,7 @@ namespace Dev2.Runtime.WebServer
             writer.AddStyleAttribute(HtmlTextWriterStyle.Margin, "5px");
             writer.AddAttribute(HtmlTextWriterAttribute.Class, className);
             writer.RenderBeginTag(HtmlTextWriterTag.Div);
-            
+
             writer.AddStyleAttribute(HtmlTextWriterStyle.Margin, "0 -15px 0 -15px");
             writer.RenderBeginTag(HtmlTextWriterTag.Table);
             writer.RenderBeginTag(HtmlTextWriterTag.Tr);
@@ -182,7 +180,7 @@ namespace Dev2.Runtime.WebServer
             writer.RenderBeginTag(HtmlTextWriterTag.Td);
             writer.Write("Tests Failed: " + allTests.Count(o => o.TestFailing));
             writer.RenderEndTag();
-            
+
             writer.RenderEndTag();
             writer.RenderEndTag();
             writer.RenderEndTag();
@@ -190,16 +188,7 @@ namespace Dev2.Runtime.WebServer
 
         public static void SetupWorkflowPathHtml(this object resourcePath, HtmlTextWriter writer, string classValue)
         {
-            writer.AddAttribute(HtmlTextWriterAttribute.Class, classValue);
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "#F36F21");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.FontWeight, "bold");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "14px");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Width, "16%");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Padding, "8px 16px 16px 8px");
-            writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "inline-block");
-            writer.RenderBeginTag(HtmlTextWriterTag.Div);
-            writer.Write(resourcePath);
-            writer.RenderEndTag();
+            
         }
 
         private static bool IsNodeCovered(List<IWorkflowNode> coveredNodes, IWorkflowNode node)
