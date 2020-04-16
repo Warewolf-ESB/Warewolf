0cdf6877ea64c7b3b3cce688cba4312433825b39
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2020-04-16 06:38:22 +0200

    6213 cleanup

--- Dev/Warewolf.Auditing/Drivers/AuditQueryableSqlite.cs
+++ Dev/Warewolf.Auditing/Drivers/AuditQueryableSqlite.cs
@@ -1,7 +1,7 @@
 /*
 *  Warewolf - Once bitten, there's no going back
 *  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
-*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
@@ -170,7 +170,7 @@ namespace Warewolf.Auditing.Drivers
         {
             using (var sqlConn = new SQLiteConnection(connectionString: "Data Source=" + _source.ConnectionString + ";"))
             {
-                using (var command = new SQLiteCommand(_query.ToString(), sqlConn))
+                using (var command = new SQLiteCommand(_query, sqlConn))
                 {
                     sqlConn.Open();
                     sqlConn.EnableExtensions(true);
