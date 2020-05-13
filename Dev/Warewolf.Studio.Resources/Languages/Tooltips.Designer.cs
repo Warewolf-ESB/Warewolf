3f453c809f93d7910e3a4e3d552c27e7caa3f4da
doobzin <34076447+doobzin@users.noreply.github.com>
2020-05-13 05:51:32 +0200

    Revert "6393-Bugfix, ReadFile: add result as base64 UI changes"
    This reverts commit 6f48571d403d653f1981704601efba966ce58cff.

--- Dev/Warewolf.Studio.Resources/Languages/Tooltips.Designer.cs
+++ Dev/Warewolf.Studio.Resources/Languages/Tooltips.Designer.cs
@@ -19,7 +19,7 @@ namespace Warewolf.Studio.Resources.Languages {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Tooltips {
@@ -3922,15 +3922,6 @@ namespace Warewolf.Studio.Resources.Languages {
         public static string ReadFileToolTipFilename {
             get {
                 return ResourceManager.GetString("ReadFileToolTipFilename", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to If selected it returns the content of the file in base64 format..
-        /// </summary>
-        public static string ReadFileToolTipIsResultBase64 {
-            get {
-                return ResourceManager.GetString("ReadFileToolTipIsResultBase64", resourceCulture);
             }
         }
         
