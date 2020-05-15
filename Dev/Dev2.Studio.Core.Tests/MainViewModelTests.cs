785e527454e66e99e54c70e7415a6d0dff000746
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-05-15 06:40:54 +0000

    DevOps adding more DoNotParallelize tags here

--- Dev/Dev2.Studio.Core.Tests/MainViewModelTests.cs
+++ Dev/Dev2.Studio.Core.Tests/MainViewModelTests.cs
@@ -209,8 +209,8 @@ namespace Dev2.Core.Tests
             Assert.IsNotNull(expected);
         }
 
-        // PBI 9397 - 2013.06.09 - TWR: added
         [TestMethod]
+        [DoNotParallelize]
         public void MainViewModelConstructorWithWorkspaceItemsInRepositoryExpectedNotLoadsWorkspaceItemsWithDifferentEnvID()
         {
             var workspaceID = Guid.NewGuid();
@@ -764,6 +764,7 @@ namespace Dev2.Core.Tests
         [TestMethod]
         [Owner("Hagashen Naidu")]
         [TestCategory("MainViewModel_ShowStartPageCommand")]
+        [DoNotParallelize]
         public void MainViewModel_ShowStartPageCommand_ShowStartPageActive()
         {
             CreateFullExportsAndVm();
