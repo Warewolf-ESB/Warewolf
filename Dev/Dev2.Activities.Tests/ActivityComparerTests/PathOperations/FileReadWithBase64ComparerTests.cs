c60cd76cbd9d3c49beb99ee4910d11ab72dace78
Rory McGuire <rory.mcguire@dev2.co.za>
2020-05-18 14:45:15 +0200

    6393 Introduce FileReadWithBase64

---
+++ Dev/Dev2.Activities.Tests/ActivityComparerTests/PathOperations/FileReadWithBase64ComparerTests.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Linq;
+using Dev2.Common.State;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Tests.Activities.ActivityComparerTests.PathOperations
+{
+    [TestClass]
+    public class FileReadWithBase64ComparerTests
+    {
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void UniqueIDEquals_EmptyFileRead_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId };
+            var dsfFileRead = new FileReadWithBase64 { UniqueID = uniqueId };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(dsfFileRead);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void UniqueIDDifferent_EmptyFileRead_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64();
+            var fileRead1 = new FileReadWithBase64();
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void IsNotCertVerifiable_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var rabbitMqActivity = new FileReadWithBase64 { UniqueID = uniqueId, Result = "A", };
+            var rabbitMqActivity1 = new FileReadWithBase64 { UniqueID = uniqueId, Result = "A" };
+            //---------------Assert Precondition----------------
+            Assert.IsTrue(rabbitMqActivity.Equals(rabbitMqActivity1));
+            //---------------Execute Test ----------------------
+            rabbitMqActivity.IsNotCertVerifiable = true;
+            rabbitMqActivity1.IsNotCertVerifiable = false;
+            var @equals = rabbitMqActivity.Equals(rabbitMqActivity1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(@equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void IsNotCertVerifiable_Same_Object_Is_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var rabbitMqActivity = new FileReadWithBase64 { UniqueID = uniqueId, Result = "A" };
+            var rabbitMqActivity1 = new FileReadWithBase64 { UniqueID = uniqueId, Result = "A" };
+            //---------------Assert Precondition----------------
+            Assert.IsTrue(rabbitMqActivity.Equals(rabbitMqActivity1));
+            //---------------Execute Test ----------------------
+            rabbitMqActivity.IsNotCertVerifiable = true;
+            rabbitMqActivity1.IsNotCertVerifiable = true;
+            var @equals = rabbitMqActivity.Equals(rabbitMqActivity1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(@equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void InputPath_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "a" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void InputPath_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "A" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void InputPath_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "AAA" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, InputPath = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Equals_Given_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "a" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Equals_Given_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "A" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Equals_Given_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "AAA" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, DisplayName = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Username_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Username = "a" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Username = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Username_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Username = "A" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Username = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Username_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Username = "AAA" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Username = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void PrivateKeyFile_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "a" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void PrivateKeyFile_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "A" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void PrivateKeyFile_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "AAA" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, PrivateKeyFile = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Password_Same_Object_IsEqual()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Password = "a" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Password = "a" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Password_Different_Object_Is_Not_Equal()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Password = "A" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Password = "ass" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        [Owner("Nkosinathi Sangweni")]
+        public void Password_Different_Object_Is_Not_Equal_CaseSensitive()
+        {
+            //---------------Set up test pack-------------------
+            var uniqueId = Guid.NewGuid().ToString();
+            var fileRead = new FileReadWithBase64 { UniqueID = uniqueId, Password = "AAA" };
+            var fileRead1 = new FileReadWithBase64 { UniqueID = uniqueId, Password = "aaa" };
+            //---------------Assert Precondition----------------
+            Assert.IsNotNull(fileRead);
+            //---------------Execute Test ----------------------
+            var @equals = fileRead.Equals(fileRead1);
+            //---------------Test Result -----------------------
+            Assert.IsFalse(equals);
+        }
+
+
+        [TestMethod]
+        [Owner("Rory McGuire")]
+        [TestCategory("FileReadWithBase64Activity_GetState")]
+        public void FileReadWithBase64Activity_GetState_ReturnsStateVariable()
+        {
+            //---------------Set up test pack-------------------
+            var inputPath = "/Path";
+            var result = "[result]";
+
+            //------------Setup for test--------------------------
+            var activity = new FileReadWithBase64 { InputPath = inputPath, Result = result };
+            //------------Execute Test---------------------------
+            var stateItems = activity.GetState();
+            Assert.AreEqual(2, stateItems.Count());
+
+            var expectedResults = new[]
+            {
+                new StateVariable
+                {
+                    Name = "InputPath",
+                    Type = StateVariable.StateType.Input,
+                    Value = inputPath
+                },
+                new StateVariable
+                {
+                    Name = "Result",
+                    Type = StateVariable.StateType.Output,
+                    Value = result
+                }
+            };
+
+            var iter = activity.GetState().Select(
+                (item, index) => new
+                {
+                    value = item,
+                    expectValue = expectedResults[index]
+                }
+                );
+
+            //------------Assert Results-------------------------
+            foreach (var entry in iter)
+            {
+                Assert.AreEqual(entry.expectValue.Name, entry.value.Name);
+                Assert.AreEqual(entry.expectValue.Type, entry.value.Type);
+                Assert.AreEqual(entry.expectValue.Value, entry.value.Value);
+            }
+        }
+    }
+}
---
+++ Dev/Dev2.Activities.Tests/ActivityTests/FileReadWithBase64Tests.cs
@@ -0,0 +1,177 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System;
+using System.Collections.Generic;
+using ActivityUnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Tests.Activities.ActivityTests
+{
+
+    /// <summary>
+    /// Summary description for DateTimeDifferenceTests
+    /// </summary>
+    [TestClass]
+    public class FileReadWithBase64Tests : BaseActivityUnitTest
+    {
+
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        
+
+        
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachInputs")]
+        public void FileReadWithBase64_UpdateForEachInputs_NullUpdates_DoesNothing()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            var inputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt");
+            var act = new FileReadWithBase64 { InputPath = inputPath, Result = "[[CompanyName]]" };
+
+            //------------Execute Test---------------------------
+            act.UpdateForEachInputs(null);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(inputPath, act.InputPath);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachInputs")]
+        public void FileReadWithBase64_UpdateForEachInputs_MoreThan1Updates_DoesNotUpdates()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            var path = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]].txt");
+            var inputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt");
+            var act = new FileReadWithBase64 { InputPath = inputPath, Result = "[[CompanyName]]" };
+
+            var tuple1 = new Tuple<string, string>(inputPath, "Test");
+            var tuple2 = new Tuple<string, string>(path, "Test2");
+            //------------Execute Test---------------------------
+            act.UpdateForEachInputs(new List<Tuple<string, string>> { tuple1, tuple2 });
+            //------------Assert Results-------------------------
+            Assert.AreEqual(inputPath, act.InputPath);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachInputs")]
+        public void FileReadWithBase64_UpdateForEachInputs_1Update_Updates()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            var inputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt");
+            var act = new FileReadWithBase64 { InputPath = inputPath, Result = "[[CompanyName]]" };
+
+            var tuple1 = new Tuple<string, string>(inputPath, "Test");
+            //------------Execute Test---------------------------
+            act.UpdateForEachInputs(new List<Tuple<string, string>> { tuple1 });
+            //------------Assert Results-------------------------
+            Assert.AreEqual("Test", act.InputPath);
+        }
+
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachOutputs")]
+        public void FileReadWithBase64_UpdateForEachOutputs_NullUpdates_DoesNothing()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            const string result = "[[CompanyName]]";
+            var act = new FileReadWithBase64 { InputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt"), Result = result };
+
+            act.UpdateForEachOutputs(null);
+            //------------Assert Results-------------------------
+            Assert.AreEqual(result, act.Result);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachOutputs")]
+        public void FileReadWithBase64_UpdateForEachOutputs_MoreThan1Updates_DoesNothing()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            const string result = "[[CompanyName]]";
+            var act = new FileReadWithBase64 { InputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt"), Result = result };
+
+            var tuple1 = new Tuple<string, string>("Test", "Test");
+            var tuple2 = new Tuple<string, string>("Test2", "Test2");
+            //------------Execute Test---------------------------
+            act.UpdateForEachOutputs(new List<Tuple<string, string>> { tuple1, tuple2 });
+            //------------Assert Results-------------------------
+            Assert.AreEqual(result, act.Result);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_UpdateForEachOutputs")]
+        public void FileReadWithBase64_UpdateForEachOutputs_1Updates_UpdateResult()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            var inputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt");
+            var act = new FileReadWithBase64 { InputPath = inputPath, Result = "[[CompanyName]]" };
+
+            var tuple1 = new Tuple<string, string>("[[CompanyName]]", "Test");
+            //------------Execute Test---------------------------
+            act.UpdateForEachOutputs(new List<Tuple<string, string>> { tuple1 });
+            //------------Assert Results-------------------------
+            Assert.AreEqual("Test", act.Result);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_GetForEachInputs")]
+        public void FileReadWithBase64_GetForEachInputs_WhenHasExpression_ReturnsInputList()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            var inputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt");
+            var act = new FileReadWithBase64 { InputPath = inputPath, Result = "[[CompanyName]]" };
+
+            //------------Execute Test---------------------------
+            var dsfForEachItems = act.GetForEachInputs();
+            //------------Assert Results-------------------------
+            Assert.AreEqual(1, dsfForEachItems.Count);
+            Assert.AreEqual(inputPath, dsfForEachItems[0].Name);
+            Assert.AreEqual(inputPath, dsfForEachItems[0].Value);
+        }
+
+        [TestMethod]
+        [Owner("Hagashen Naidu")]
+        [TestCategory("FileReadWithBase64_GetForEachOutputs")]
+        public void FileReadWithBase64_GetForEachOutputs_WhenHasResult_ReturnsOutputList()
+        {
+            //------------Setup for test--------------------------
+            var newGuid = Guid.NewGuid();
+            const string result = "[[CompanyName]]";
+            var act = new FileReadWithBase64 { InputPath = string.Concat(TestContext.TestRunDirectory, "\\", newGuid + "[[CompanyName]]2.txt"), Result = result };
+
+            //------------Execute Test---------------------------
+            var dsfForEachItems = act.GetForEachOutputs();
+            //------------Assert Results-------------------------
+            Assert.AreEqual(1, dsfForEachItems.Count);
+            Assert.AreEqual(result, dsfForEachItems[0].Name);
+            Assert.AreEqual(result, dsfForEachItems[0].Value);
+        }
+    }
+}
