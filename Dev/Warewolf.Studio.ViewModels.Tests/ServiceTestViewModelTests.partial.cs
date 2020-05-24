815ba0944fe51debe361c283f0afdd141577d3c5
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-05-24 16:55:04 +0000

    DevOps cannot parallelize the rest of this partial class either

--- Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.partial.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.partial.cs
@@ -34,6 +34,7 @@ using Unlimited.Applications.BusinessDesignStudio.Activities;
 
 namespace Warewolf.Studio.ViewModels.Tests
 {
+    [DoNotParallelize]//Due to the type of dependency injection used in this class. Remove CustomContainer.Register from ServiceTestViewModelTests.cs and retry.
     partial class ServiceTestViewModelTests
     {
 
