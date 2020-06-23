854bca2cf272a209154b400f0b3b9fd3883749fd
Rory McGuire <rory.mcguire@dev2.co.za>
2020-06-23 18:30:53 +0200

    6046 remove old string based IsAuthorized from IAuthorizationService

--- Dev/Dev2.Infrastructure/Services/Security/IAuthorizationService.cs
+++ Dev/Dev2.Infrastructure/Services/Security/IAuthorizationService.cs
@@ -1,7 +1,7 @@
 /*
 *  Warewolf - Once bitten, there's no going back
-*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
-*  Licensed under GNU Affero General Public License 3.0 or later. 
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
@@ -23,10 +23,8 @@ namespace Dev2.Services.Security
         event EventHandler<PermissionsModifiedEventArgs> PermissionsModified;
         ISecurityService SecurityService { get; }
 
-        //bool IsAuthorized(AuthorizationContext context, string resource);
         bool IsAuthorized(AuthorizationContext context, Guid resourceId);
         bool IsAuthorized(AuthorizationContext context, IWarewolfResource resource);
-        // bool IsAuthorized(IPrincipal user, AuthorizationContext context, string resource);
         bool IsAuthorized(IPrincipal user, AuthorizationContext context, Guid resourceId);
         bool IsAuthorized(IPrincipal user, AuthorizationContext context, IWarewolfResource resource);
         bool IsAuthorized(IAuthorizationRequest request);
