79047b3d5d7f9fd225d76537ca5563d3dd5ba633
Rory McGuire <rory.mcguire@dev2.co.za>
2020-06-24 10:10:26 +0200

    6046 update copyright headers

--- Dev/Dev2.Runtime.WebServer/ApisJsonBuilder.cs
+++ Dev/Dev2.Runtime.WebServer/ApisJsonBuilder.cs
@@ -1,4 +1,14 @@
 #pragma warning disable
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
