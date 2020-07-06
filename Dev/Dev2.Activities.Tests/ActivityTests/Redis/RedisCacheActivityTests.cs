cac0559aa20641a60e6cbe8eb236741f6e0b14ab
Candice Daniel <candice.daniel@dev2.co.za>
2020-07-06 13:11:41 +0200

    6466-Added Recordset result to Specs

--- Dev/Dev2.Activities.Tests/ActivityTests/Redis/RedisCacheActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/Redis/RedisCacheActivityTests.cs
@@ -8,7 +8,6 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System.Threading;
 using Dev2.Activities.RedisCache;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
