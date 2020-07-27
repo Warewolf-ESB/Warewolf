5aab8eeebb6d32212f419fd791eed5347ba1286b
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-07-27 12:14:47 +0000

    Revert "patch for compile break caused by my last commit"
    This reverts commit 65fe7ca12044cf246083a8bcb7f04cbf650e83e3

--- Dev/Dev2.Activities.Specs/Composition/WorkflowExecutionLoggingSteps.cs
+++ Dev/Dev2.Activities.Specs/Composition/WorkflowExecutionLoggingSteps.cs
@@ -281,6 +281,7 @@ namespace Dev2.Activities.Specs.Composition
             activityParserMock.Setup(o => o.Parse(It.IsAny<DynamicActivity>())).Returns(activityMock.Object);
             CustomContainer.Register<IActivityParser>(activityParserMock.Object);
             _scenarioContext.Add("activityMock", activityMock.Object);
+            _scenarioContext.Add("expectException", expectException);
         }
 
         [When(@"a workflow execution has an exception")]
