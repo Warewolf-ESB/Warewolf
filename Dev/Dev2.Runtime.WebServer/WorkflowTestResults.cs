db28363f9d01dc8eb71541654de24e665e30cec2
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-07-29 15:58:20 +0200

    6359-Refactor, WorkflowTestResults and Workflow: IsTestInValid and All to Any check refactor increase calc speed

--- Dev/Dev2.Runtime.WebServer/WorkflowTestResults.cs
+++ Dev/Dev2.Runtime.WebServer/WorkflowTestResults.cs
@@ -37,31 +37,31 @@ namespace Dev2.Runtime.WebServer
             {
                 OldTestName = result.OldTestName,
                 TestName = result.TestName,
-                TestPassed = IsTestPassed(result),
                 TestInvalid = IsTestInValid(result),
+                TestPassed = IsTestPassed(result),
                 TestFailing = IsTestFailing(result),
                 Result = new TestRunResult
                 {
                     TestName = result.TestName,
-                    Message = IsTestInValid(result) ? "Test has no selected nodes" : result.FailureMessage,
-                    RunTestResult = IsTestInValid(result) ? RunResult.TestInvalid : IsTestFailing(result) ? RunResult.TestFailed : RunResult.TestPassed,
+                    Message = result.TestInvalid ? "Test has no selected nodes" : result.FailureMessage,
+                    RunTestResult = result.TestInvalid ? RunResult.TestInvalid : result.TestFailing ? RunResult.TestFailed : RunResult.TestPassed,
                 }
             });
         }
 
         private static bool IsTestFailing(IServiceTestModelTO test)
         {
-            return IsTestInValid(test) is false && IsTestPassed(test) is false;
+            return test.TestFailing = test.TestInvalid is false && test.TestPassed is false;
         }
 
         private static bool IsTestPassed(IServiceTestModelTO test)
         {
-            return IsTestInValid(test) is false && test.TestFailing is false;
+            return test.TestPassed = test.TestInvalid is false && test.TestFailing is false;
         }
 
         private static bool IsTestInValid(IServiceTestModelTO test)
         {
-            return test.TestSteps is null || test.TestSteps.Count is 0;
+            return test.TestInvalid = test.TestSteps is null || test.TestSteps.Count is 0;
         }
     }
 }
