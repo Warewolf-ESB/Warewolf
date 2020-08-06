9e28ea601e4e729061e7e4f1489f7a31cf9f5456
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-08-06 07:41:16 +0000

    Update Dockerfile.deploy.bat

--- Dev/Dev2.Server/Dockerfile.deploy.bat
+++ Dev/Dev2.Server/Dockerfile.deploy.bat
@@ -1,10 +1,20 @@
 @echo off
-echo.
-echo Please enter any username: (for example: WarewolfAdmin)
-set /p Username=
-echo.
-echo Please enter any password greater than 8 upper, lower, number and symbol characters: (for example: W@rEw0lf@dm1n)
-set /p Password=
+if "%Username%"=="" (
+    echo.
+    echo Please enter any username: (for example: WarewolfAdmin)
+    set /p Username=
+)
+if "%Password%"=="" (
+    echo.
+    echo Please enter any password greater than 8 upper, lower, number and symbol characters: (for example: W@rEw0lf@dm1n)
+    set /p Password=
+)
+if "%Port%"=="" (
+    echo.
+    echo Please enter any valid port number:
+    set /p Port=
+)
+set /A SecurePort=%Port%+1
 @echo on
 
-docker run -d -m 4g -e SERVER_USERNAME=%Username% -e SERVER_PASSWORD=%Password% -p 3142:3142 -p 3143:3143 warewolfserver/warewolfserver
+docker run -d -m 4g -e SERVER_USERNAME=%Username% -e SERVER_PASSWORD=%Password% -p %SecurePort%:3142 -p %Port%:3143 warewolfserver/warewolfserver
