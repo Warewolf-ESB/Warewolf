462aca6a497702d0648b21cd792b200d62b9112e
Candice Daniel <candice.daniel@dev2.co.za>
2020-10-12 10:48:34 +0200

    6581-GetPersistenceSettings Service

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/GetPersistenceSettings.cs
@@ -0,0 +1,40 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using Dev2.Common;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.Workspaces;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Warewolf.Resource.Errors;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+    public class GetPersistenceSettings : DefaultEsbManagementEndpoint
+    {
+        public override StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+        {
+            var serializer = new Dev2JsonSerializer();
+
+            if (values is null)
+            {
+                throw new InvalidDataContractException(ErrorResource.NoParameter);
+            }
+            var settings = Config.Persistence.Get();
+            return serializer.SerializeToBuilder(settings);
+        }
+
+        public override DynamicService CreateServiceEntry() => EsbManagementServiceEntry.CreateESBManagementServiceEntry(HandlesType(), "<DataList><Database ColumnIODirection=\"Input\"/><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>");
+
+        public override string HandlesType() => nameof(Warewolf.Service.GetPersistenceSettings);
+    }
+}
