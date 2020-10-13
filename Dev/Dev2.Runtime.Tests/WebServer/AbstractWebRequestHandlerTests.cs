78a609911e63bf0efe72249a648abc5990b15c53
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-10-13 17:54:00 +0000

    DevOps adding another DoNotParallelize flag

--- Dev/Dev2.Runtime.Tests/WebServer/AbstractWebRequestHandlerTests.cs
+++ Dev/Dev2.Runtime.Tests/WebServer/AbstractWebRequestHandlerTests.cs
@@ -1,4 +1,4 @@
-﻿#pragma warning disable
+#pragma warning disable
 /*
 *  Warewolf - Once bitten, there's no going back
 *  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
@@ -794,6 +794,7 @@ namespace Dev2.Tests.Runtime.WebServer
         [TestMethod]
         [Owner("Siphamandla Dube")]
         [TestCategory(nameof(AbstractWebRequestHandler))]
+        [DoNotParallelize]
         public void AbstractWebRequestHandler_GetPostData_GivenPost_FileStreamData_InContext_ShouldReturnBase64RequestBoundVariables()
         {
             //---------------Set up test pack-------------------
