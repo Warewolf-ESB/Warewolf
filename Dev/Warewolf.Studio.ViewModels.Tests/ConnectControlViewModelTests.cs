719ab2da78537e0e90939c44931a7562b7f74735
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-10-20 14:55:09 +0200

    DevOps setting studio viewmodel unit test timeouts

--- Dev/Warewolf.Studio.ViewModels.Tests/ConnectControlViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ConnectControlViewModelTests.cs
@@ -198,7 +198,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(100)]
+        [Timeout(250)]
         public void TestNewConnectionCommand()
         {
             //arrange
