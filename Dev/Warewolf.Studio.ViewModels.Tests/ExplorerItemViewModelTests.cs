cd6c9fd7ae7942fbaf636fcd410e099c0322f77f
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-10-26 16:33:35 +0200

    DevOps setting studio viewmodel unit test timeout

--- Dev/Warewolf.Studio.ViewModels.Tests/ExplorerItemViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ExplorerItemViewModelTests.cs
@@ -236,7 +236,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(100)]
+        [Timeout(250)]
         public void TestNewOracleSourceCommand()
         {
             //arrange
--- Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ServiceTestViewModelTests.cs
@@ -934,7 +934,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(1000)]
+        [Timeout(2000)]
         [Owner("Pieter Terblanche")]
         [TestCategory("TestFrameworkViewModel_Constructor")]
         public void TestFrameworkViewModel_Constructor_IsDirty_IsFalse()
