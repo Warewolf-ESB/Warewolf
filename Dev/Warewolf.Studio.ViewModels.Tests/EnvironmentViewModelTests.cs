f5e0e847a5998f43daa081021370e2cce8c951f2
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-11-05 19:34:35 +0200

    DevOps bumping timeouts here

--- Dev/Warewolf.Studio.ViewModels.Tests/EnvironmentViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/EnvironmentViewModelTests.cs
@@ -507,7 +507,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(250)]
+        [Timeout(500)]
         public void TestCanCreateFolderTrue()
         {
             //arrange
