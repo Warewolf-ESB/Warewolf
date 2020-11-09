4d3012d18e6a435c98cdb5a2e49695a0c38ed39d
Candice Daniel <candice.daniel@dev2.co.za>
2020-11-09 14:10:39 +0200

    6529-Added Impl class to handle the user feedback on the run. Made changes to tests, but they not passing yet.

--- Dev/Warewolf.HangfireServer.Tests/HangfireServerTests.cs
+++ Dev/Warewolf.HangfireServer.Tests/HangfireServerTests.cs
@@ -8,15 +8,19 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
-using System.Text;
+using System;
 using Dev2.Common;
+using Dev2.Common.ExtMethods;
 using Dev2.Common.Interfaces.Wrappers;
+using Dev2.Communication;
 using Dev2.Runtime.ServiceModel.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Warewolf.Interfaces.Auditing;
 using Warewolf.Streams;
 using HangfireServer;
+using Newtonsoft.Json.Serialization;
+using Warewolf.Data;
 using Warewolf.Execution;
 using static HangfireServer.Program;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
@@ -48,25 +52,31 @@ namespace Warewolf.HangfireServer.Tests
 
             var mockFile = new Mock<IFile>();
             var mockDirectory = new Mock<IDirectory>();
-
-            var persistenceSettings = new PersistenceSettings("", mockFile.Object, mockDirectory.Object)
-            {
-                PersistenceDataSource = {Payload = "test"}
-            };
-            var bytes = UTF8Encoding.UTF8.GetBytes(persistenceSettings.PersistenceDataSource.Payload);
-            var mockDeserializer = new Mock<IDeserializer>();
             var dbSource = new DbSource
             {
                 ConnectionString = "connectionString",
             };
-            mockDeserializer.Setup(o => o.Deserialize<DbSource>(bytes)).Returns(dbSource).Verifiable();
+            var compressedExecuteMessage = new CompressedExecuteMessage();
+            var serializeToJsonString = dbSource.SerializeToJsonString(new DefaultSerializationBinder());
+            compressedExecuteMessage.SetMessage(serializeToJsonString);
 
-            var item = new Implementation(args, implConfig, persistenceSettings, mockDeserializer.Object);
-            var result = item.Run();
+            var mockSerializer = new Mock<Dev2JsonSerializer>();
 
-            Assert.AreEqual(0, result);
+            var persistenceSettings = new PersistenceSettings("some path", mockFile.Object, mockDirectory.Object);
+            persistenceSettings.Enable = false;
+            persistenceSettings.PersistenceDataSource = new NamedGuidWithEncryptedPayload
+            {
+                Name = "Data Source",
+                Value = Guid.Empty,
+                Payload = serializeToJsonString
+            };
+            //  mockSerializer.Setup(o => o.Deserialize<DbSource>(persistenceSettings.PersistenceDataSource.Payload)).Returns(dbSource).Verifiable();
+            var mockContext = new HangfireContext(args);
+            var item = new Implementation(mockContext, implConfig, persistenceSettings, mockSerializer.Object);
+            item.Run();
+
             mockWriter.Verify(o => o.WriteLine("Starting Hangfire server..."), Times.Once);
-            mockDeserializer.Verify(o => o.Deserialize<DbSource>(bytes), Times.Once);
+            mockSerializer.Verify(o => o.Deserialize<DbSource>(persistenceSettings.PersistenceDataSource.Payload), Times.Once);
             mockWriter.Verify(o => o.WriteLine("Hangfire dashboard started..."), Times.Once);
             mockWriter.Verify(o => o.WriteLine("Hangfire server started..."), Times.Once);
             mockPauseHelper.Verify(o => o.Pause(), Times.Never);
@@ -89,21 +99,20 @@ namespace Warewolf.HangfireServer.Tests
 
             mockWriter.Setup(o => o.WriteLine("Starting Hangfire server...")).Verifiable();
             mockWriter.Setup(o => o.WriteLine(
-                        "Fatal Error: Could not find persistence config file. Hangfire server is unable to start.")).Verifiable();
+                "Fatal Error: Could not find persistence config file. Hangfire server is unable to start.")).Verifiable();
             mockWriter.Setup(o => o.Write("Press any key to exit...")).Verifiable();
 
             var mockFile = new Mock<IFile>();
             var mockDirectory = new Mock<IDirectory>();
 
             var persistenceSettings = new PersistenceSettings("", mockFile.Object, mockDirectory.Object);
+            var mockContext = new HangfireContext(args);
+            var item = new Implementation(mockContext, implConfig, persistenceSettings, new Mock<Dev2JsonSerializer>().Object);
+            item.Run();
 
-            var item = new Implementation(args, implConfig, persistenceSettings, new Mock<IDeserializer>().Object);
-            var result = item.Run();
-
-            Assert.AreEqual(0, result);
             mockWriter.Verify(o => o.WriteLine("Starting Hangfire server..."), Times.Once);
             mockWriter.Verify(o => o.WriteLine(
-                    "Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
+                "Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
             mockWriter.Verify(o => o.Write("Press any key to exit..."), Times.Once);
             mockPauseHelper.Verify(o => o.Pause(), Times.Once);
             mockExitHelper.Verify(o => o.Exit(), Times.Once);
