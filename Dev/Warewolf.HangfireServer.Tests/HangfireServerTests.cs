b8abad5a89f44d787c77b46f42c377ea43af0f57
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2020-11-09 16:59:40 +0200

    6529 Update code and resolve tests

--- Dev/Warewolf.HangfireServer.Tests/HangfireServerTests.cs
+++ Dev/Warewolf.HangfireServer.Tests/HangfireServerTests.cs
@@ -11,20 +11,20 @@
 using System;
 using Dev2.Common;
 using Dev2.Common.ExtMethods;
+using Dev2.Common.Interfaces.Communication;
 using Dev2.Common.Interfaces.Wrappers;
 using Dev2.Communication;
 using Dev2.Runtime.ServiceModel.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Warewolf.Interfaces.Auditing;
-using Warewolf.Streams;
 using HangfireServer;
 using Newtonsoft.Json.Serialization;
 using Warewolf.Data;
 using Warewolf.Execution;
 using static HangfireServer.Program;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using static Warewolf.Common.NetStandard20.ExecutionLogger;
+using ISerializer = Warewolf.Streams.ISerializer;
 
 namespace Warewolf.HangfireServer.Tests
 {
@@ -42,16 +42,9 @@ namespace Warewolf.HangfireServer.Tests
                 ShowConsole = true,
             };
 
-            var implConfig = SetupHangfireImplementationConfigs(out Mock<IWriter> mockWriter,
-                out Mock<IExecutionLoggerFactory> mockExecutionLoggerFactory, out Mock<IPauseHelper> mockPauseHelper,
-                out Mock<IExitHelper> mockExitHelper);
+            var implConfig = SetupHangfireImplementationConfigs(out var mockWriter, out var mockPauseHelper,
+                out var mockExitHelper, out var mockExecutionLogPublisher);
 
-            mockWriter.Setup(o => o.WriteLine("Starting Hangfire server...")).Verifiable();
-            mockWriter.Setup(o => o.WriteLine("Hangfire dashboard started...")).Verifiable();
-            mockWriter.Setup(o => o.WriteLine("Hangfire server started...")).Verifiable();
-
-            var mockFile = new Mock<IFile>();
-            var mockDirectory = new Mock<IDirectory>();
             var dbSource = new DbSource
             {
                 ConnectionString = "connectionString",
@@ -60,25 +53,30 @@ namespace Warewolf.HangfireServer.Tests
             var serializeToJsonString = dbSource.SerializeToJsonString(new DefaultSerializationBinder());
             compressedExecuteMessage.SetMessage(serializeToJsonString);
 
-            var mockSerializer = new Mock<Dev2JsonSerializer>();
+            var mockSerializer = new Mock<IBuilderSerializer>();
 
-            var persistenceSettings = new PersistenceSettings("some path", mockFile.Object, mockDirectory.Object);
-            persistenceSettings.Enable = false;
-            persistenceSettings.PersistenceDataSource = new NamedGuidWithEncryptedPayload
+            var mockFile = new Mock<IFile>();
+            var mockDirectory = new Mock<IDirectory>();
+            var persistenceSettings = new PersistenceSettings("some path", mockFile.Object, mockDirectory.Object)
             {
-                Name = "Data Source",
-                Value = Guid.Empty,
-                Payload = serializeToJsonString
+                Enable = false,
+                PersistenceDataSource = new NamedGuidWithEncryptedPayload
+                {
+                    Name = "Data Source", Value = Guid.Empty, Payload = serializeToJsonString
+                },
             };
-            //  mockSerializer.Setup(o => o.Deserialize<DbSource>(persistenceSettings.PersistenceDataSource.Payload)).Returns(dbSource).Verifiable();
+            mockSerializer.Setup(o => o.Deserialize<DbSource>(persistenceSettings.PersistenceDataSource.Payload)).Returns(dbSource).Verifiable();
             var mockContext = new HangfireContext(args);
             var item = new Implementation(mockContext, implConfig, persistenceSettings, mockSerializer.Object);
             item.Run();
 
             mockWriter.Verify(o => o.WriteLine("Starting Hangfire server..."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Info("Starting Hangfire server..."), Times.Once);
             mockSerializer.Verify(o => o.Deserialize<DbSource>(persistenceSettings.PersistenceDataSource.Payload), Times.Once);
             mockWriter.Verify(o => o.WriteLine("Hangfire dashboard started..."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire dashboard started..."), Times.Once);
             mockWriter.Verify(o => o.WriteLine("Hangfire server started..."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire server started..."), Times.Once);
             mockPauseHelper.Verify(o => o.Pause(), Times.Never);
             mockExitHelper.Verify(o => o.Exit(), Times.Never);
         }
@@ -86,46 +84,89 @@ namespace Warewolf.HangfireServer.Tests
         [TestMethod]
         [Owner("Pieter Terblanche")]
         [TestCategory(nameof(HangfireServer))]
-        public void HangfireServer_Program_ConnectionString_Payload_Empty()
+        public void HangfireServer_Program_ConnectionString_Payload_Empty_Pause()
         {
             var args = new Args
             {
-                ShowConsole = false,
+                ShowConsole = true,
             };
 
-            var implConfig = SetupHangfireImplementationConfigs(out Mock<IWriter> mockWriter,
-                out Mock<IExecutionLoggerFactory> mockExecutionLoggerFactory, out Mock<IPauseHelper> mockPauseHelper,
-                out Mock<IExitHelper> mockExitHelper);
-
-            mockWriter.Setup(o => o.WriteLine("Starting Hangfire server...")).Verifiable();
-            mockWriter.Setup(o => o.WriteLine(
-                "Fatal Error: Could not find persistence config file. Hangfire server is unable to start.")).Verifiable();
-            mockWriter.Setup(o => o.Write("Press any key to exit...")).Verifiable();
+            var implConfig = SetupHangfireImplementationConfigs(out var mockWriter, out var mockPauseHelper,
+                out var mockExitHelper, out var mockExecutionLogPublisher);
 
             var mockFile = new Mock<IFile>();
             var mockDirectory = new Mock<IDirectory>();
 
             var persistenceSettings = new PersistenceSettings("", mockFile.Object, mockDirectory.Object);
             var mockContext = new HangfireContext(args);
-            var item = new Implementation(mockContext, implConfig, persistenceSettings, new Mock<Dev2JsonSerializer>().Object);
+            var item = new Implementation(mockContext, implConfig, persistenceSettings, new Mock<IBuilderSerializer>().Object);
             item.Run();
+            item.WaitForExit();
 
-            mockWriter.Verify(o => o.WriteLine("Starting Hangfire server..."), Times.Once);
-            mockWriter.Verify(o => o.WriteLine(
-                "Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Info("Starting Hangfire server..."), Times.Once);
+            mockWriter.Verify(o => o.WriteLine("Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Error("Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
             mockWriter.Verify(o => o.Write("Press any key to exit..."), Times.Once);
             mockPauseHelper.Verify(o => o.Pause(), Times.Once);
-            mockExitHelper.Verify(o => o.Exit(), Times.Once);
+            mockExitHelper.Verify(o => o.Exit(), Times.Never);
+            mockWriter.Verify(o => o.WriteLine("Hangfire dashboard started..."), Times.Never);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire dashboard started..."), Times.Never);
+            mockWriter.Verify(o => o.WriteLine("Hangfire server started..."), Times.Never);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire server started..."), Times.Never);
         }
 
-        private Implementation.ConfigImpl SetupHangfireImplementationConfigs(out Mock<IWriter> mockWriter,
-            out Mock<IExecutionLoggerFactory> mockExecutionLoggerFactory, out Mock<IPauseHelper> mockPauseHelper,
-            out Mock<IExitHelper> mockExitHelper)
+        [TestMethod]
+        [Owner("Pieter Terblanche")]
+        [TestCategory(nameof(HangfireServer))]
+        public void HangfireServer_Program_ConnectionString_Payload_Empty_Exit()
         {
-            var mockExecutionLogPublisher = new Mock<IExecutionLogPublisher>();
+            var args = new Args
+            {
+                ShowConsole = false,
+            };
 
+            var implConfig = SetupHangfireImplementationConfigs(out var mockWriter, out var mockPauseHelper,
+                out var mockExitHelper, out var mockExecutionLogPublisher);
+
+            var mockFile = new Mock<IFile>();
+            var mockDirectory = new Mock<IDirectory>();
+
+            var persistenceSettings = new PersistenceSettings("", mockFile.Object, mockDirectory.Object);
+            var mockContext = new HangfireContext(args);
+            var item = new Implementation(mockContext, implConfig, persistenceSettings, new Mock<IBuilderSerializer>().Object);
+            item.Run();
+            item.WaitForExit();
+
+            mockWriter.Verify(o => o.WriteLine("Starting Hangfire server..."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Info("Starting Hangfire server..."), Times.Once);
+            mockWriter.Verify(o => o.WriteLine("Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
+            mockExecutionLogPublisher.Verify(o => o.Error("Fatal Error: Could not find persistence config file. Hangfire server is unable to start."), Times.Once);
+            mockWriter.Verify(o => o.Write("Press any key to exit..."), Times.Once);
+            mockPauseHelper.Verify(o => o.Pause(), Times.Never);
+            mockExitHelper.Verify(o => o.Exit(), Times.Once);
+            mockWriter.Verify(o => o.WriteLine("Hangfire dashboard started..."), Times.Never);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire dashboard started..."), Times.Never);
+            mockWriter.Verify(o => o.WriteLine("Hangfire server started..."), Times.Never);
+            mockExecutionLogPublisher.Verify(o => o.Info("Hangfire server started..."), Times.Never);
+        }
+
+        private static Implementation.ConfigImpl SetupHangfireImplementationConfigs(out Mock<IWriter> mockWriter,
+            out Mock<IPauseHelper> mockPauseHelper, out Mock<IExitHelper> mockExitHelper, out Mock<IExecutionLogPublisher> mockExecutionLogPublisher)
+        {
             mockWriter = new Mock<IWriter>();
-            mockExecutionLoggerFactory = new Mock<IExecutionLoggerFactory>();
+            mockWriter.Setup(o => o.WriteLine("Starting Hangfire server...")).Verifiable();
+            mockWriter.Setup(o => o.WriteLine("Hangfire dashboard started...")).Verifiable();
+            mockWriter.Setup(o => o.WriteLine("Hangfire server started...")).Verifiable();
+            mockWriter.Setup(o => o.WriteLine("Fatal Error: Could not find persistence config file. Hangfire server is unable to start.")).Verifiable();
+            mockWriter.Setup(o => o.Write("Press any key to exit...")).Verifiable();
+
+            mockExecutionLogPublisher = new Mock<IExecutionLogPublisher>();
+            mockExecutionLogPublisher.Setup(o => o.Info("Starting Hangfire server...")).Verifiable();
+            mockExecutionLogPublisher.Setup(o => o.Info("Hangfire dashboard started...")).Verifiable();
+            mockExecutionLogPublisher.Setup(o => o.Info("Hangfire server started...")).Verifiable();
+            mockExecutionLogPublisher.Setup(o => o.Error("Fatal Error: Could not find persistence config file. Hangfire server is unable to start.")).Verifiable();
+
+            var mockExecutionLoggerFactory = new Mock<IExecutionLoggerFactory>();
             mockExecutionLoggerFactory.Setup(o => o.New(It.IsAny<ISerializer>(), It.IsAny<IWebSocketPool>()))
                 .Returns(mockExecutionLogPublisher.Object);
 
