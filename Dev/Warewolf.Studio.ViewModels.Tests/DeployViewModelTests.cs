3116f8d13ca94914045c3744deb2d7f738e7c89d
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-11-12 15:21:26 +0200

    DevOps increasing a couple more studio view model unit test timeouts

--- Dev/Warewolf.Studio.ViewModels.Tests/DeployViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/DeployViewModelTests.cs
@@ -98,7 +98,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(2000)]
+        [Timeout(5000)]
         [Owner("Leon Rajindrapersadh")]
         [TestCategory("DeploySourceExplorerViewModel_Ctor_valid")]
         public void DeploySourceExplorerViewModel_Ctor_Nulls_ExpectSuccess()
