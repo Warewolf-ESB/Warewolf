98ea8d36e86c625eeebc0b1b5c3b6e9be7172043
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-11-13 04:31:10 +0200

    DevOps bumping a timeout

--- Dev/Warewolf.Studio.ViewModels.Tests/ManagePluginSourceViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ManagePluginSourceViewModelTests.cs
@@ -185,7 +185,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(1000)]
+        [Timeout(2500)]
         public void TestChooseGACDLLCommandCanExecuteDllChooserIsNotNull()
         {
             //arrange
