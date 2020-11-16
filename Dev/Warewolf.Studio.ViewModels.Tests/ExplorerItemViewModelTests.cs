40110b1fca7eca1e35afaff75a31ee8aa920030b
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-11-16 18:03:56 +0200

    DevOps bumping a couple timeouts

--- Dev/Warewolf.Studio.ViewModels.Tests/ExplorerItemViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/ExplorerItemViewModelTests.cs
@@ -1861,7 +1861,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(100)]
+        [Timeout(250)]
         public void TestCreateNewFolderResourceTypeDbService()
         {
             //arrange
