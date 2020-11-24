223792c1dd8757bfa5f09870e633a8098843513c
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-11-24 16:53:40 +0200

    DevOps doubling a timeout

--- Dev/Warewolf.Studio.ViewModels.Tests/DatabaseSourceViewModelBaseTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/DatabaseSourceViewModelBaseTests.cs
@@ -85,7 +85,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(250)]
+        [Timeout(500)]
         public void TestTestDbConnection()
         {
             //arrange
