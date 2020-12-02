8dac8e52512ddf936895296a3ff36439c86db4d7
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2020-12-02 15:08:22 +0000

    Revert "DevOps cannot run these tests in parallel at the moment"
    This reverts commit 2642d9b2514a46a99def263e88986e8cc5a04eb5

--- Dev/Dev2.Activities.Tests/ActivityTests/DateTimeDifferenceTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DateTimeDifferenceTests.cs
@@ -19,7 +19,6 @@ using Unlimited.Applications.BusinessDesignStudio.Activities;
 namespace ActivityUnitTests.ActivityTests
 {
     [TestClass]
-    [DoNotParallelize]
     public class DateTimeDifferenceTests : BaseActivityUnitTest
     {
         [TestInitialize]
