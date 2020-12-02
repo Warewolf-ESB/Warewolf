d027847188c24ba25e54b6c9020d14a063af25fc
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2020-12-02 14:10:28 +0200

    6649-Refactor, DsfWebPostActivityTests: add testing for ResponseManager variable changes

--- Dev/Dev2.Activities/Activities/DsfWebPostActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfWebPostActivity.cs
@@ -100,7 +100,6 @@ namespace Dev2.Activities
 
             tmpErrors.MergeErrors(_errorsTo);
 
-            //TODO: Make sure the below changes are tested
             var  bytes = webRequestResult.Base64StringToByteArray();
             var response = bytes.ReadToString();
 
