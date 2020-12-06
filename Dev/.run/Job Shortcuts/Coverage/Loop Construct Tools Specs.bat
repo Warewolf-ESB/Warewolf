64cd9dcf8173f6ffbcf9aa1bb36b20f95d3cb084
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-12-06 19:59:53 +0200

    DevOps dumping these shortcut scripts I just generated from the build definitions

---
+++ Dev/.run/Job Shortcuts/Coverage/Loop Construct Tools Specs.bat
@@ -0,0 +1,3 @@
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -SolutionWideOutputs
+cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
+powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -Projects Warewolf.Tools.Specs -Category LoopConstructs -Coverage
