c2e631278eb457c50baba4f2858ef709f7888f08
Candice Daniel <candice.daniel@dev2.co.za>
2020-12-07 14:14:27 +0200

    6640-Added currentuserprincipal to WorkflowResume

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/WorkflowResume.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/WorkflowResume.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using System.Web;
@@ -28,7 +29,15 @@ namespace Dev2.Runtime.ESB.Management.Services
     public class WorkflowResume : WorkflowManagementEndpointAbstract
     {
         public override string HandlesType() => nameof(WorkflowResume);
+        static string GetUnqualifiedName(string userName)
+        {
+            if (userName.Contains("\\"))
+            {
+                return userName.Split('\\').Last().Trim();
+            }
 
+            return userName;
+        }
         protected override ExecuteMessage ExecuteImpl(Dev2JsonSerializer serializer, Guid resourceId, Dictionary<string, StringBuilder> values)
         {
             values.TryGetValue("versionNumber", out StringBuilder versionNumber);
@@ -36,20 +45,33 @@ namespace Dev2.Runtime.ESB.Management.Services
             {
                 throw new InvalidDataContractException("no version Number passed");
             }
+
             values.TryGetValue("environment", out StringBuilder environmentString);
             if (environmentString == null)
             {
                 throw new InvalidDataContractException("no environment passed");
             }
+
             values.TryGetValue("startActivityId", out StringBuilder startActivityIdString);
             if (startActivityIdString == null)
             {
                 startActivityIdString = new StringBuilder(resourceId.ToString());
             }
+
             if (!Guid.TryParse(startActivityIdString.ToString(), out Guid startActivityId))
             {
                 throw new InvalidDataContractException("startActivityId is not a valid GUID.");
             }
+
+            values.TryGetValue("currentuserprincipal", out StringBuilder currentuserprincipal);
+            if (currentuserprincipal == null)
+            {
+                throw new InvalidDataContractException("no executing user principal passed");
+            }
+
+            var unqualifiedUserName = GetUnqualifiedName(currentuserprincipal.ToString()).Trim();
+            var executingUser = new WindowsPrincipal(new WindowsIdentity(unqualifiedUserName));
+
             var decodedEnv = HttpUtility.UrlDecode(environmentString.ToString());
             var executionEnv = new ExecutionEnvironment();
             executionEnv.FromJson(decodedEnv);
@@ -58,29 +80,31 @@ namespace Dev2.Runtime.ESB.Management.Services
             var dataObject = new DsfDataObject("", Guid.NewGuid())
             {
                 ResourceID = resourceId,
-                Environment = executionEnv,                                                
+                Environment = executionEnv,
                 VersionNumber = parsedVersionNumber,
-                ExecutingUser = Thread.CurrentPrincipal,
+                ExecutingUser = executingUser,
                 IsDebug = true
             };
             var dynamicService = ResourceCatalog.Instance.GetService(GlobalConstants.ServerWorkspaceID, resourceId, "");
             if (dynamicService is null)
             {
-                return new ExecuteMessage { HasError = true, Message = new StringBuilder($"Error resuming. ServiceAction is null for Resource ID:{resourceId}") };
+                return new ExecuteMessage {HasError = true, Message = new StringBuilder($"Error resuming. ServiceAction is null for Resource ID:{resourceId}")};
             }
+
             var sa = dynamicService.Actions.FirstOrDefault();
             if (sa is null)
             {
-                return new ExecuteMessage { HasError = true, Message = new StringBuilder($"Error resuming. ServiceAction is null for Resource ID:{resourceId}") };
+                return new ExecuteMessage {HasError = true, Message = new StringBuilder($"Error resuming. ServiceAction is null for Resource ID:{resourceId}")};
             }
+
             var container = CustomContainer.Get<IResumableExecutionContainerFactory>().New(startActivityId, sa, dataObject);
             container.Execute(out ErrorResultTO errors, 0);
             if (errors.HasErrors())
             {
-                return new ExecuteMessage { HasError = true, Message = new StringBuilder(errors.MakeDisplayReady()) };
+                return new ExecuteMessage {HasError = true, Message = new StringBuilder(errors.MakeDisplayReady())};
             }
-            return new ExecuteMessage { HasError = false, Message = new StringBuilder("Execution Completed.") };
-        }
 
+            return new ExecuteMessage {HasError = false, Message = new StringBuilder("Execution Completed.")};
+        }
     }
 }
