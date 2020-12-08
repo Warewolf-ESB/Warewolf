6011d3ddc8ced78be3424fdcd605439203162f23
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-12-08 21:57:07 +0200

    DevOps removing redundant tag

--- Dev/.run/Job Shortcuts/Container/Load Tests.bat
+++ Dev/.run/Job Shortcuts/Container/Load Tests.bat
@@ -1,3 +1,3 @@
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -SolutionWideOutputs
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting
 cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
 powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -Projects Dev2.Integration.Tests -Category Load Tests -PreTestRunScript 'StartAs.ps1 -Cleanup -Anonymous -ResourcesPath LoadTests' -RunInDocker
--- Dev/.run/Job Shortcuts/Coverage/Composition Load Tests.bat
+++ Dev/.run/Job Shortcuts/Coverage/Composition Load Tests.bat
@@ -1,3 +1,3 @@
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -SolutionWideOutputs
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting
 cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
 powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -Projects Dev2.Activities.Specs -Category CompositionLoadTests -PreTestRunScript 'StartAs.ps1 -Cleanup -Anonymous -ResourcesPath LoadTests' -Coverage
