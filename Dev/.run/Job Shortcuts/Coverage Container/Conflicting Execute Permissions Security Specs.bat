2e87738702d5ef4c2c64a81be1ae79ff56e0661e
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-12-09 12:26:42 +0200

    DevOps compile in container in test run shortcut

--- Dev/.run/Job Shortcuts/Coverage Container/Conflicting Execute Permissions Security Specs.bat
+++ Dev/.run/Job Shortcuts/Coverage Container/Conflicting Execute Permissions Security Specs.bat
@@ -1,3 +1,3 @@
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -InDockerContainer
 cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
 powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -Projects Dev2.*.Specs,Warewolf.*.Specs -ExcludeProjects Dev2.Activities.Specs,Warewolf.Tools.Specs,Warewolf.UI.Specs,Warewolf.UI.Load.Specs -Category ConflictingExecutePermissionsSecurity -PreTestRunScript "StartAs.ps1 -Cleanup -Anonymous -ResourcesPath ServerTests" -Coverage -RunInDocker
