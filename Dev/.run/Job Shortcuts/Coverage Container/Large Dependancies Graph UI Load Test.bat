43baa347e956d7d421b5015bfed35fa65f9e571a
Ashley Lewis <Ashley.lewis@dev2.co.za>
2020-12-09 12:26:42 +0200

    DevOps compile in container in test run shortcut

--- Dev/.run/Job Shortcuts/Coverage Container/Large Dependancies Graph UI Load Test.bat
+++ Dev/.run/Job Shortcuts/Coverage Container/Large Dependancies Graph UI Load Test.bat
@@ -1,3 +1,3 @@
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -InDockerContainer
 cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
 powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -Projects Warewolf.UI.Load.Specs -Category StudioLargeDependanciesGraphUILoadTest -PreTestRunScript 'StartAs.ps1 -Cleanup -Anonymous -ResourcesPath LoadTests' -Coverage -RunInDocker
