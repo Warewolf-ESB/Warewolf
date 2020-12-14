c4b270d750dd6db2e7b054ef8348832e4be299ac
Candice Daniel <candice.daniel@dev2.co.za>
2020-12-14 13:52:02 +0200

    Return Execute message and throw if there is an error. Also ONLY allow resume is state is Scheduled or Failed.

--- Dev/Warewolf.HangfireServer/ResumptionAttribute.cs
+++ Dev/Warewolf.HangfireServer/ResumptionAttribute.cs
@@ -7,6 +7,7 @@
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -57,21 +58,25 @@ namespace HangfireServer
             {
                 return;
             }
-            var jobArg = context.BackgroundJob.Job.Args[0];
-            var backgroundJobId = context.BackgroundJob.Id;
 
-            OnPerformResume(jobArg, backgroundJobId);
+            OnPerformResume(context);
         }
 
-        public void OnPerformResume(object jobArg, string backgroundJobId)
+        public void OnPerformResume(PerformingContext context)
         {
+            var jobArg = context.BackgroundJob.Job.Args[0];
+            var backgroundJobId = context.BackgroundJob.Id;
             var resumptionFactory = _resumptionFactory ?? new ResumptionFactory();
             var resumption = resumptionFactory.New();
             if (resumption.Connect())
             {
                 var values = jobArg as Dictionary<string, StringBuilder>;
                 LogResumption(values);
-                resumption.Resume(values);
+                var result = resumption.Resume(values);
+                if (result.HasError)
+                {
+                    throw new InvalidOperationException(result.Message?.ToString(),new Exception(result.Message?.ToString()));
+                }
             }
             else
             {
