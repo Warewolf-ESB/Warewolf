d3e84c48b445e91b8d44d8200bceb0b7ff0c37d8
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-01-19 07:24:42 +0200

    DevOps add auto-version to installer shortcut script

--- Dev/.run/Job Shortcuts/Installer.Build.bat
+++ Dev/.run/Job Shortcuts/Installer.Build.bat
@@ -1,7 +1,7 @@
 echo off
 if not exist "%~dp0installer" ( git -C "%~dp0." clone https://gitlab.com/warewolf/installer )
 if not exist "%~dp0installer" ( echo Installer git clone failed. Try cloning, copying or otherwise creating directory at "%~dp0installer" containing https://gitlab.com/warewolf/installer with this command: git -C "%~dp0." clone https://gitlab.com/warewolf/installer )
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\Compile.ps1" -AcceptanceTesting -ProjectSpecificOutputs -Config Release
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\Compile.ps1" -AcceptanceTesting -ProjectSpecificOutputs -Config Release -AutoVersion
 xcopy "%~dp0..\..\Dev2.Server\bin\Release\net48\win" "%~dp0installer\WarewolfInstaller\ProductBuild\Server\" /S /Y
 xcopy "%~dp0..\..\Dev2.Studio\bin\Release\net48\win" "%~dp0installer\WarewolfInstaller\ProductBuild\Studio\" /S /Y
 "%~dp0installer\Build.bat"
