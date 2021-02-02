93b9fe42c436fa22c655cc6b3c350aba2591dbd8
Njabulo Nxele <njabulo.nxele@theunlimited.co.za>
2021-02-02 10:28:35 +0200

    fix folder issue

--- Dev/Dev2.Runtime.WebServer/Handlers/GetSwaggerServiceHandler.cs
+++ Dev/Dev2.Runtime.WebServer/Handlers/GetSwaggerServiceHandler.cs
@@ -2,9 +2,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Dev2.Common;
 using Dev2.Runtime.Hosting;
 using Dev2.Runtime.Interfaces;
@@ -28,63 +31,61 @@ namespace Dev2.Runtime.WebServer.Handlers
 
 #pragma warning disable S3010 // Used by tests for constructor injection
         public GetSwaggerServiceHandler(IResourceCatalog catalog, IAuthorizationService auth)
-            : base(ResourceCatalog.Instance, TestCatalog.Instance, TestCoverageCatalog.Instance, new DefaultEsbChannelFactory(), new SecuritySettings())
+            : base(ResourceCatalog.Instance, TestCatalog.Instance, TestCoverageCatalog.Instance,
+                new DefaultEsbChannelFactory(), new SecuritySettings())
         {
             _resourceCatalog = catalog;
             _authorizationService = auth;
-            
         }
 #pragma warning restore S3010
-      
+
         public override void ProcessRequest(ICommunicationContext ctx)
         {
-            if(ctx == null)
+            if (ctx == null)
             {
                 throw new ArgumentNullException("ctx");
             }
+
             var basePath = ctx.Request.BoundVariables["path"];
             if (!bool.TryParse(ctx.Request.BoundVariables["isPublic"], out bool isPublic))
             {
                 isPublic = false;
             }
-            var result = ResponseWriter(basePath, isPublic, ctx);
-            
-            //ctx.Send(result);
+
+            ResponseWriter(basePath, isPublic, ctx);
         }
 
-        List<IResponseWriter> ResponseWriter(string basePath,bool isPublic, ICommunicationContext ctx)
+        void ResponseWriter(string basePath, bool isPublic, ICommunicationContext ctx)
         {
             var webPath = basePath.Replace("\\", "/");
-            var searchPath = basePath.Replace("/", "\\");
-            var resourceList = ResourceCatalog.Instance.GetResourceList(GlobalConstants.ServerWorkspaceID).Where(resource =>
-                resource.GetResourcePath(GlobalConstants.ServerWorkspaceID).Contains(searchPath) &&
-                resource.ResourceType == "WorkflowService").ToList();
+            var searchPath = basePath.Replace("/", "\\").Replace(".api", "");
+            var resourceList = ResourceCatalog.Instance.GetResourceList(GlobalConstants.ServerWorkspaceID).Where(
+                resource =>
+                    resource.GetResourcePath(GlobalConstants.ServerWorkspaceID).Contains(searchPath) &&
+                    resource.ResourceType == "WorkflowService").ToList();
 
+            var builder = new StringBuilder();
+            foreach (var resource in resourceList)
+            {
+                var requestVariables = new NameValueCollection
+                {
+                    {"servicename", $"{basePath.Replace(".api", "")}/{resource.ResourceName}.api"},
+                    {"isPublic", isPublic.ToString()}
+                };
 
-            var apiBuilder = new SwaggerBuilder(_authorizationService, _resourceCatalog);
-            return apiBuilder.BuildForPath(basePath, isPublic, ctx);
-            
-            var a = new AbstractWebRequestHandler.Executor(_workspaceRepository, _resourceCatalog, _testCatalog,
-                _testCoverageCatalog, _authorizationService, _dataObjectFactory, _esbChannelFactory, _jwtManager);
-            
-            
-            // var converter = new JsonSerializer();
-            // var result = new StringBuilder();
-            // var jsonTextWriter = new JsonTextWriter(new StringWriter(result)) { Formatting = Formatting.Indented };
-            // converter.Serialize(jsonTextWriter, apis);
-            // jsonTextWriter.Flush();
-            // var apisJson = result.ToString();
-            // var stringResponseWriter = new StringResponseWriter(apisJson, ContentTypes.Json,false);
-            // return stringResponseWriter;
+                var uri = new Uri($"{ctx.Request.Uri.ToString().Replace(".api", "")}/{resource.ResourceName}.api");
+                var req = new HttpRequestMessage(HttpMethod.Get, uri) { Content = ctx.Request.Request.Content };
+                var context = new WebServerContext(req, requestVariables) {Request = {User = ctx.Request.User}};
+
+                var request = new WebGetRequestHandler(_resourceCatalog, _testCatalog, _testCoverageCatalog);
+                request.ProcessRequest(context);
+                Task.Run(async () =>
+                {
+                    var val = await context.Response.Response.Content.ReadAsStringAsync();
+                    builder.AppendLine(val);
+                }).Wait();
+            }
+            ctx.Send(new StringResponseWriter(builder.ToString(), "application/json"));
         }
-        
-        // private IResponseWriter CreateForm(WebRequestTO webRequest, string serviceName, string workspaceId,
-        //     NameValueCollection headers, IPrincipal user)
-        // {
-        //     var a = new AbstractWebRequestHandler.Executor(_workspaceRepository, _resourceCatalog, _testCatalog,
-        //         _testCoverageCatalog, _authorizationService, _dataObjectFactory, _esbChannelFactory, _jwtManager);
-        //     var response = a.TryExecute(webRequest, serviceName, workspaceId, headers, user);
-        //     return response ?? a.BuildResponse(webRequest, serviceName);
-        // }
     }
 }
