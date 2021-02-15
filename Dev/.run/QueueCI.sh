4585231c7d0ef3c5ad958d46ac356b0f63c1d192
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2021-02-15 08:33:52 +0000

    DevOps removing unrecognized option

--- Dev/.run/QueueCI.sh
+++ Dev/.run/QueueCI.sh
@@ -7,7 +7,7 @@ DefaultCIBambooPlanKey="WOLF-CI"
 DefaultBranchName="develop"
 
 function QueueBuild {
-	build=$(wget -O - "http://bamboo.opswolf.com/rest/api/latest/queue/$2.json?os_username=$BambooUsername&os_password=$BambooPassword" -q --method=GET)
+	build=$(wget -O - "http://bamboo.opswolf.com/rest/api/latest/queue/$2.json?os_username=$BambooUsername&os_password=$BambooPassword" -q)
 	echo $(echo $build | cut -d '"' -f 20)
 }
 
@@ -29,7 +29,7 @@ if [ "$branch" == "$DefaultBranchName" ]; then
 	QueueBuild $CIBambooPlanKey
 else
 	branch=${branch//\//-}
-	JSONDATA=$(wget -O - "http://bamboo.opswolf.com/rest/api/latest/plan/$CIBambooPlanKey.json?os_username=$BambooUsername&os_password=$BambooPassword&expand=branches&max-result=99" -q --method=GET)
+	JSONDATA=$(wget -O - "http://bamboo.opswolf.com/rest/api/latest/plan/$CIBambooPlanKey.json?os_username=$BambooUsername&os_password=$BambooPassword&expand=branches&max-result=99" -q)
 	FindBranchName=$(echo $JSONDATA | grep -o "$branch.*")
 	FindBranchKey=$(echo $FindBranchName | grep -o "\"key\":\".*")
 	BranchKey=$(echo $FindBranchKey | cut -d '"' -f 4)
