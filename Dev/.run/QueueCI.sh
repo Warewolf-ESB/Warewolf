177b4524f6af58c35c17b6443b50d13421551405
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2021-02-15 07:44:34 +0000

    DevOps adding quotes around this string

--- Dev/.run/QueueCI.sh
+++ Dev/.run/QueueCI.sh
@@ -10,7 +10,7 @@ DefaultCIBambooPlanKey="WOLF-CI"
 DefaultBranchName="develop"
 
 function QueueBuild {
-	build=$(wget -O - "http://$1/rest/api/latest/queue/$2.json?os_authType=basic" --user=$BambooUsername --password=$BambooPassword --post-data="" -q)
+	build=$(wget -O - "http://$1/rest/api/latest/queue/$2.json?os_authType=basic" --user=$BambooUsername --password='$BambooPassword' --post-data="" -q)
 	echo $(echo $build | cut -d '"' -f 20)
 }
 
@@ -32,7 +32,7 @@ if [ "$branch" == "$DefaultBranchName" ]; then
 	QueueBuild $BambooUrl $CIBambooPlanKey
 else
 	branch=${branch//\//-}
-	JSONDATA=$(wget -O - "http://$BambooUrl/rest/api/latest/plan/$CIBambooPlanKey.json?os_authType=basic&expand=branches&max-result=99" --user=$BambooUsername --password=$BambooPassword -q)
+	JSONDATA=$(wget -O - "http://$BambooUrl/rest/api/latest/plan/$CIBambooPlanKey.json?os_authType=basic&expand=branches&max-result=99" --user=$BambooUsername --password='$BambooPassword' -q)
 	FindBranchName=$(echo $JSONDATA | grep -o "$branch.*")
 	FindBranchKey=$(echo $FindBranchName | grep -o "\"key\":\".*")
 	BranchKey=$(echo $FindBranchKey | cut -d '"' -f 4)
