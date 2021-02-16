dd0a7fe6cbe9127d31fa32c560af7a7470618cb8
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-02-16 12:50:25 +0200

    DevOps re-introducing more UNC file path tool specs

--- Dev/.run/Job Shortcuts/Container/Workflow Merge with Assign Tools Conflicting.bat
+++ Dev/.run/Job Shortcuts/Container/Workflow Merge with Assign Tools Conflicting.bat
@@ -1,4 +1,4 @@
 powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0..\..\..\..\Compile.ps1" -AcceptanceTesting -InDockerContainer
 mkdir "%~dp0..\..\..\..\bin\AcceptanceTesting"
 cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
-powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -RetryRebuild -Projects Warewolf.UI.Tests -Category Merge Assign Conflicts -PreTestRunScript 'StartAs.ps1 -Cleanup -Anonymous -ResourcesPath UITests' -RunInDocker
+powershell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -RetryRebuild -Projects Warewolf.UI.Tests -Category Merge Assign Conflicts -PreTestRunScript 'StartAs.ps1 -Cleanup -Anonymous -ResourcesPath UITests' -InContainer
