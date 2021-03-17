5a0e07f7fcc1cc8156a6336d76aa77a72e58bc4a
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2021-03-17 10:40:49 +0200

    6713-Bugfix, WebServiceHeaderBuilder: automatically adds application/json even on empty JSON, this causes header misuse GREEN

--- Dev/Dev2.Common/Common/ExtensionMethods.cs
+++ Dev/Dev2.Common/Common/ExtensionMethods.cs
@@ -1,7 +1,7 @@
 #pragma warning disable
 /*
 *  Warewolf - Once bitten, there's no going back
-*  Copyright 2020 by Warewolf Ltd <alpha@warewolf.io>
+*  Copyright 2021 by Warewolf Ltd <alpha@warewolf.io>
 *  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
@@ -445,8 +445,12 @@ namespace Dev2.Common.Common
             {
                 try
                 {
-                    JToken.Parse(trimmedInput);
-                    return true;
+                    if (trimmedInput != "{}")
+                    {
+                        JToken.Parse(trimmedInput);
+                        return true;
+                    }
+                    return false;
                 }
                 catch (JsonReaderException jex)
                 {
