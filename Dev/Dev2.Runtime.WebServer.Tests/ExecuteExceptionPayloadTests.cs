f39b14099e09845279c496232bd864eb985606ea
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2021-03-24 13:06:18 +0200

    6721-Enhance, Exception Handling: add ExecuteExceptionPayload

---
+++ Dev/Dev2.Runtime.WebServer.Tests/ExecuteExceptionPayloadTests.cs
@@ -0,0 +1,140 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2021 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using Dev2.Interfaces;
+using Dev2.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using Warewolf.Storage;
+
+namespace Dev2.Runtime.WebServer.Tests
+{
+    [TestClass]
+    [TestCategory(nameof(ExecuteExceptionPayload))]
+    public class ExecuteExceptionPayloadTests
+    {
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_ShouldDefaultToXML()
+        {
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+               .Returns(new ExecutionEnvironment());
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual("<ExecutionError><MessageId>NotImplemented</MessageId><Message>Please consult system Administrator.</Message></ExecutionError>", sut, "If the system was able to get this far then this might be a behavior NotImplemented currently by Warewolf");
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_Given_IsDebug_true_ShouldReturnEmptyString()
+        {
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+               .Returns(new ExecutionEnvironment());
+            mockDataObject.Setup(o => o.IsDebug)
+                .Returns(true);
+
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual(string.Empty, sut);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_Given_RemoteInvoke_True_And_EmitionTypes_Cover_ShouldReturnJSON()
+        {
+            var errorMessage = "test error message";
+            var env = new ExecutionEnvironment();
+            env.Errors.Add(errorMessage);
+            env.Errors.Add(errorMessage);
+
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+                .Returns(env);
+            mockDataObject.Setup(o => o.RemoteInvoke)
+                .Returns(true);
+            mockDataObject.Setup(o => o.ReturnType)
+                .Returns(EmitionTypes.Cover);
+
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual("{\"ExecutionError\":{\"MessageId\":\"BadRequest\",\"Message\":\"test error message\"}}", sut);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_Given_RemoteInvoke_True_And_EmitionTypes_XML_ShouldReturnXML()
+        {
+            var errorMessage = "test error message";
+            var env = new ExecutionEnvironment();
+            env.Errors.Add(errorMessage);
+            env.Errors.Add(errorMessage);
+
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+                .Returns(env);
+            mockDataObject.Setup(o => o.RemoteInvoke)
+                .Returns(true);
+            mockDataObject.Setup(o => o.ReturnType)
+                .Returns(EmitionTypes.XML);
+
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual("<ExecutionError><MessageId>BadRequest</MessageId><Message>test error message</Message></ExecutionError>", sut);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_Given_RemoteInvoke_True_And_EmitionTypes_TRX_ShouldReturnXML()
+        {
+            var errorMessage = "test error message";
+            var env = new ExecutionEnvironment();
+            env.Errors.Add(errorMessage);
+            env.Errors.Add(errorMessage);
+
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+                .Returns(env);
+            mockDataObject.Setup(o => o.RemoteInvoke)
+                .Returns(true);
+            mockDataObject.Setup(o => o.ReturnType)
+                .Returns(EmitionTypes.TRX);
+
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual("<ExecutionError><MessageId>BadRequest</MessageId><Message>test error message</Message></ExecutionError>", sut);
+        }
+
+        [TestMethod]
+        [Owner("Siphamandla Dube")]
+        public void ExecuteExceptionPayload_Given_RemoteNonDebugInvoke_True_And_EmitionTypes_OPENAPI_ShouldReturnJSON()
+        {
+            var errorMessage = "test error message";
+            var env = new ExecutionEnvironment();
+
+            var mockDataObject = new Mock<IDSFDataObject>();
+            mockDataObject.Setup(o => o.Environment)
+                .Returns(env);
+            mockDataObject.Setup(o => o.RemoteNonDebugInvoke)
+                .Returns(true);
+            mockDataObject.Setup(o => o.ReturnType)
+                .Returns(EmitionTypes.OPENAPI);
+            mockDataObject.Setup(o => o.ExecutionException)
+                .Returns(new Exception(errorMessage));
+
+            var sut = ExecuteExceptionPayload.Calculate(mockDataObject.Object);
+
+            Assert.AreEqual("{\"ExecutionError\":{\"MessageId\":\"InternalServerError\",\"Message\":\"test error message\"}}", sut);
+        }
+    }
+}
---
+++ Dev/Dev2.Runtime.WebServer/ExecuteExceptionPayload.cs
@@ -0,0 +1,72 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2021 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using Dev2.Interfaces;
+using Dev2.Web;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Dev2.Runtime.WebServer
+{
+
+    public class ExecuteExceptionPayload
+    {
+        private ExecuteExceptionPayload()
+        {
+        }
+
+        public static string Calculate(IDSFDataObject dataObject)
+        {
+            var (statusCode, message) = GetResponseMessage(dataObject);
+            var notDebug = !dataObject.IsDebug || dataObject.RemoteInvoke || dataObject.RemoteNonDebugInvoke;
+            if (notDebug)
+            {
+                switch (dataObject.ReturnType)
+                {
+                    case EmitionTypes.TRX:
+                    case EmitionTypes.XML:
+                        {
+                            return $"<ExecutionError>" +
+                                "<MessageId>" +statusCode+ "</MessageId>" +
+                                "<Message>" + message + "</Message>"+
+                                "</ExecutionError>";   
+                        }
+                    default: 
+                    case EmitionTypes.OPENAPI:
+                    case EmitionTypes.CoverJson:
+                    case EmitionTypes.JSON:
+                        {
+                            return JsonConvert.SerializeObject(new { ExecutionError = new { MessageId = statusCode.ToString(), Message = message } });
+                        }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static (HttpStatusCode statusCode, string message) GetResponseMessage(IDSFDataObject dataObject)
+        {
+            var env = dataObject.Environment;
+            var exception = dataObject.ExecutionException;
+            var hasException = exception != null;
+            if (hasException)
+            {
+                return (HttpStatusCode.InternalServerError, message: exception.Message);
+            }
+            else if (!hasException && env.HasErrors())
+            {
+                return (HttpStatusCode.BadRequest, message: env.FetchErrors());
+            }
+
+            return (HttpStatusCode.NotImplemented, message: "Please consult system Administrator.");
+        }
+    }
+}
