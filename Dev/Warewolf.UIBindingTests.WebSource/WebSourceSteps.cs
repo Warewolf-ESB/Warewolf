dbe0e58f2ab262c8e693893db67ecaee219970fd
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-03-31 14:41:53 +0200

    DevOps still patching compile failure

--- Dev/Warewolf.UIBindingTests.WebSource/WebSourceSteps.cs
+++ Dev/Warewolf.UIBindingTests.WebSource/WebSourceSteps.cs
@@ -308,7 +308,7 @@ namespace Warewolf.UIBindingTests.WebSource
             var manageWebserviceSourceControl = scenarioContext.Get<ManageWebserviceSourceControl>(Utils.ViewNameKey);
             var viewModel = scenarioContext.Get<ManageWebserviceSourceViewModel>("viewModel");
             var username = @"dev2\IntegrationTester";
-            var password = TestEnvironmentVariables.GetVar(username);
+            var password = "I73573r0";
             Assert.AreEqual(password, viewModel.Password);
             Assert.AreEqual(password, manageWebserviceSourceControl.GetPassword());
         }
@@ -321,7 +321,7 @@ namespace Warewolf.UIBindingTests.WebSource
         {
             var manageWebserviceSourceControl = scenarioContext.Get<ManageWebserviceSourceControl>(Utils.ViewNameKey);
             var username = @"dev2\IntegrationTester";
-            var password = TestEnvironmentVariables.GetVar(username);
+            var password = "I73573r0";
             manageWebserviceSourceControl.EnterPassword(password);
             var viewModel = scenarioContext.Get<ManageWebserviceSourceViewModel>("viewModel");
             Assert.AreEqual(password, viewModel.Password);
