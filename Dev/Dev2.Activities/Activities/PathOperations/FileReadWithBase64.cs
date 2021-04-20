92b75c9d3c8a2ad93370581c7369aa5fb2afcbdf
PREMIER\Njabulo.Nxele <njabulo.nxele@theunlimited.co.za>
2021-04-20 11:45:38 +0200

    Revert "allow varialbles to be passed to credentials in read file activity"
    This reverts commit 4b52063b044a64c050f5347979e6a689226f59d1.

--- Dev/Dev2.Activities/Activities/PathOperations/FileReadWithBase64.cs
+++ Dev/Dev2.Activities/Activities/PathOperations/FileReadWithBase64.cs
@@ -72,9 +72,6 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
             //get all the possible paths for all the string variables
             var inputItr = new WarewolfIterator(context.Environment.Eval(InputPath, update));
             colItr.AddVariableToIterateOn(inputItr);
-            
-            var userItr = new WarewolfIterator(context.Environment.Eval(Username, update));
-            colItr.AddVariableToIterateOn(userItr);
 
             var passItr = new WarewolfIterator(context.Environment.Eval(DecryptedPassword, update));
             colItr.AddVariableToIterateOn(passItr);
@@ -102,7 +99,7 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
             {
                 var broker = ActivityIOFactory.CreateOperationsBroker();
                 var ioPath = ActivityIOFactory.CreatePathFromString(colItr.FetchNextValue(inputItr),
-                                                                                colItr.FetchNextValue(userItr),
+                                                                                Username,
                                                                                 colItr.FetchNextValue(passItr),
                                                                                 true, colItr.FetchNextValue(privateKeyItr));
                 var endpoint = ActivityIOFactory.CreateOperationEndPointFromIOPath(ioPath);
