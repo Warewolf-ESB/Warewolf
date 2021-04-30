1fa9ef2c202f0cf4a5b1eb38319bd1c87d4bdf27
Candice Daniel <candice.daniel@dev2.co.za>
2021-04-30 13:13:31 +0200

    6764-Changed the Usagelogger from a minute to an hour.

--- Dev/Dev2.Server/ServerLifecycleManager.cs
+++ Dev/Dev2.Server/ServerLifecycleManager.cs
@@ -144,7 +144,7 @@ namespace Dev2
             _startupResourceCatalogFactory = startupConfiguration.ResourceCatalogFactory;
             _ipcClient = startupConfiguration.IpcClient;
             _assemblyLoader = startupConfiguration.AssemblyLoader;
-            _usageLogger = new UsageLogger(TimeSpan.FromMinutes(1).TotalMilliseconds);
+            _usageLogger = new UsageLogger(TimeSpan.FromHours(1).TotalMilliseconds);
             _pulseLogger = new PulseLogger(60000).Start();
             _pulseTracker = new PulseTracker(TimeSpan.FromDays(1).TotalMilliseconds).Start();
             _serverEnvironmentPreparer.PrepareEnvironment();
