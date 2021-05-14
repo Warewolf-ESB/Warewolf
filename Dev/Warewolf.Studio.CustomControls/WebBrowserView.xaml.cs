a83584cc4b7719de4d7ae60db8e33ce808aa0b2a
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2021-05-14 16:01:29 +0200

    6803 Add WebBrowserView modal for testing

---
+++ Dev/Warewolf.Studio.CustomControls/WebBrowserView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Warewolf.Studio.CustomControls
+{
+    [SuppressMessage("ReSharper", "CC0091")]
+    public partial class WebBrowserView : Window
+    {
+        public WebBrowserView()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var helper = new ScriptHelper(this);
+            this.webBrowser.ObjectForScripting = helper;
+
+            var curDir = Directory.GetCurrentDirectory();
+            var url = new Uri($"file:///{curDir}/ChargeBee.html");
+
+            webBrowser.Source = url;
+            //
+            // webBrowser.InvokeScript("function");
+        }
+
+        private void WebBrowser_OnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            var urlParameters = HttpUtility.ParseQueryString(e.Uri.ToString());
+            // extract the post data from the url.
+            var postdata = urlParameters["http://localhost/AlertDisplay.aspx?txtSomething"];
+            if (postdata != null)
+            {
+                MessageBox.Show(postdata);
+            }
+        }
+    }
+}
