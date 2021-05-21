e8c304cfb63304a6afe7de7db312cd9a3391ee34
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-05-21 05:08:14 +0200

    DevOps patching adding logging to this flakey unit test

--- Dev/Dev2.Common.Tests/Common/GetComputerNamesImplTests.cs
+++ Dev/Dev2.Common.Tests/Common/GetComputerNamesImplTests.cs
@@ -96,7 +96,7 @@ namespace Dev2.Common.Tests
         {
             var result = SecurityIdentityForWindows.GetWindowsDomainOrWorkgroupName("");
 
-            Assert.IsTrue(result == null || result == "WORKGROUP", "Cannot get Windows domain or workgroupname from username.");
+            Assert.IsTrue(result == null || result == "WORKGROUP", "Cannot get Windows domain or workgroupname from username. Instead got  " + result);
         }
     }
 }
