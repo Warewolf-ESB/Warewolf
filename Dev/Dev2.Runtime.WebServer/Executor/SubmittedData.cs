61d47091e8ae790082c649c0fa7e9962a5adcdb4
Ashley Leewis <ashley.lewis@dev2.co.za>
2021-05-31 14:14:56 +0200

    6643-fix unit test failure caused by merge up from develop

--- Dev/Dev2.Runtime.WebServer/Executor/SubmittedData.cs
+++ Dev/Dev2.Runtime.WebServer/Executor/SubmittedData.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml.Linq;
 using Dev2.Common;
@@ -29,236 +30,248 @@ namespace Dev2.Runtime.WebServer.Executor
 {
     public class SubmittedData
     {
-            private readonly IStreamWriterFactory _streamWriterFactory;
-            private readonly IStreamContentFactory _streamContentFactory;
-            private readonly IMultipartMemoryStreamProviderFactory _multipartMemoryStreamProviderFactory;
-            private readonly IMemoryStreamFactory _memoryStreamFactory;
+        private readonly IStreamWriterFactory _streamWriterFactory;
+        private readonly IStreamContentFactory _streamContentFactory;
+        private readonly IMultipartMemoryStreamProviderFactory _multipartMemoryStreamProviderFactory;
+        private readonly IMemoryStreamFactory _memoryStreamFactory;
 
-            public SubmittedData()
-            {
-                _streamContentFactory = new StreamContentFactory();
-                _streamWriterFactory = new StreamWriterFactory();
-                _multipartMemoryStreamProviderFactory = new MultipartMemoryStreamProviderFactory();
-                _memoryStreamFactory = new MemoryStreamFactory();
-            }
+        public SubmittedData()
+        {
+            _streamContentFactory = new StreamContentFactory();
+            _streamWriterFactory = new StreamWriterFactory();
+            _multipartMemoryStreamProviderFactory = new MultipartMemoryStreamProviderFactory();
+            _memoryStreamFactory = new MemoryStreamFactory();
+        }
 
-            public SubmittedData(IStreamWriterFactory streamWriterFactory, IStreamContentFactory streamContentFactory, IMultipartMemoryStreamProviderFactory streamProviderFactory, IMemoryStreamFactory memoryStreamFactory)
-            {
-                _streamWriterFactory = streamWriterFactory;
-                _streamContentFactory = streamContentFactory;
-                _multipartMemoryStreamProviderFactory = streamProviderFactory;
-                _memoryStreamFactory = memoryStreamFactory;
-            }
+        public SubmittedData(IStreamWriterFactory streamWriterFactory, IStreamContentFactory streamContentFactory, IMultipartMemoryStreamProviderFactory streamProviderFactory, IMemoryStreamFactory memoryStreamFactory)
+        {
+            _streamWriterFactory = streamWriterFactory;
+            _streamContentFactory = streamContentFactory;
+            _multipartMemoryStreamProviderFactory = streamProviderFactory;
+            _memoryStreamFactory = memoryStreamFactory;
+        }
 
-            internal string GetPostData(ICommunicationContext ctx)
-            {
-                var baseStr = HttpUtility.UrlDecode(ctx.Request.Uri.ToString());
-                baseStr = HttpUtility.UrlDecode(CleanupXml(baseStr));
-                string payload = null;
-                if (baseStr != null)
+        internal string GetPostData(ICommunicationContext ctx)
+        {
+            var baseStr = HttpUtility.UrlDecode(ctx.Request.Uri.ToString());
+            baseStr = HttpUtility.UrlDecode(CleanupXml(baseStr));
+            string payload = null;
+            if (baseStr != null)
+                {
+                    var startIdx = baseStr.IndexOf("?", StringComparison.Ordinal);
+                if (startIdx > 0)
+                {
+                    payload = baseStr.Substring(startIdx + 1);
+                    if (payload.IsXml() || payload.IsJSON())
                     {
-                        var startIdx = baseStr.IndexOf("?", StringComparison.Ordinal);
-                    if (startIdx > 0)
-                    {
-                        payload = baseStr.Substring(startIdx + 1);
-                        if (payload.IsXml() || payload.IsJSON())
-                        {
-                            return payload;
-                        }
+                        return payload;
                     }
                 }
-
-                if (ctx.Request.Method == "GET")
-                {
-                    return ExtractKeyValuePairForGetMethod(ctx, payload);
-                }
-
-                if (ctx.Request.Method == "POST")
-                {
-                    using (var reader = new StreamReader(ctx.Request.InputStream, ctx.Request.ContentEncoding))
-                    {
-                        try
-                        {
-                            return ExtractKeyValuePairForPostMethod(ctx, reader);
-                        }
-                        catch (Exception ex)    
-                        {
-                            Dev2Logger.Error(nameof(AbstractWebRequestHandler), ex, GlobalConstants.WarewolfError);
-                        }
-                    }
-                }
-
-                return string.Empty;
             }
 
-            internal static string CleanupXml(string baseStr)
+            if (ctx.Request.Method == "GET")
             {
-                if (baseStr.Contains("?"))
-                {
-                    var startQueryString = baseStr.IndexOf("?", StringComparison.Ordinal);
-                    var query = baseStr.Substring(startQueryString + 1);
-                    if (query.IsJSON())
-                    {
-                        return baseStr;
-                    }
-
-                    var args = HttpUtility.ParseQueryString(query);
-                    var url = baseStr.Substring(0, startQueryString + 1);
-                    var results = new List<string>();
-                    foreach (var arg in args.AllKeys)
-                    {
-                        var txt = args[arg];
-                        results.Add(txt.IsXml() ? arg + "=" + string.Format(GlobalConstants.XMLPrefix + "{0}", Convert.ToBase64String(Encoding.UTF8.GetBytes(txt))) : $"{arg}={txt}");
-                    }
-
-                    return url + string.Join("&", results);
-                }
-
-                return baseStr;
+                return ExtractKeyValuePairForGetMethod(ctx, payload);
             }
 
-            internal static string ExtractKeyValuePairForGetMethod(ICommunicationContext ctx, string payload)
+            if (ctx.Request.Method == "POST")
             {
-                if (payload != null)
+                using (var reader = new StreamReader(ctx.Request.InputStream, ctx.Request.ContentEncoding))
                 {
-                    var keyValuePairs = payload.Split(new[] {"&"}, StringSplitOptions.RemoveEmptyEntries).ToList();
-                    foreach (var keyValuePair in keyValuePairs)
+                    try
                     {
-                        if (keyValuePair.StartsWith("wid="))
-                        {
-                            continue;
-                        }
-
-                        if (keyValuePair.IsXml() || keyValuePair.IsJSON() || (keyValuePair.ToLowerInvariant().Contains("<DataList>".ToLowerInvariant()) && keyValuePair.ToLowerInvariant().Contains("</DataList>".ToLowerInvariant())))
-                        {
-                            return keyValuePair;
-                        }
+                        return ExtractKeyValuePairForPostMethod(ctx, reader);
+                    }
+                    catch (Exception ex)    
+                    {
+                        Dev2Logger.Error(nameof(AbstractWebRequestHandler), ex, GlobalConstants.WarewolfError);
                     }
                 }
-
-                var pairs = ctx.Request.QueryString;
-                return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
             }
 
-            string ExtractKeyValuePairForPostMethod(ICommunicationContext ctx, StreamReader reader)
+            return string.Empty;
+        }
+
+        internal static string CleanupXml(string baseStr)
+        {
+            if (baseStr.Contains("?"))
             {
-                NameValueCollection pairs;
-                if (ctx.Request.Content.IsMimeMultipartContent("form-data"))
+                var startQueryString = baseStr.IndexOf("?", StringComparison.Ordinal);
+                var query = baseStr.Substring(startQueryString + 1);
+                if (query.IsJSON())
                 {
-                    pairs = ExtractMultipartFormDataArgumentsFromDataList(ctx, reader);
-                    return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
+                    return baseStr;
                 }
 
-                var data = reader.ReadToEnd();
-
-                if (data.IsXml(out XDocument _))
+                var args = HttpUtility.ParseQueryString(query);
+                var url = baseStr.Substring(0, startQueryString + 1);
+                var results = new List<string>();
+                foreach (var arg in args.AllKeys)
                 {
-                    return data.CleanXmlSOAP();
+                    var txt = args[arg];
+                    txt = CheckForEscapeCharacters(txt);
+                    results.Add(txt.IsXml() ? arg + "=" + string.Format(GlobalConstants.XMLPrefix + "{0}", Convert.ToBase64String(Encoding.UTF8.GetBytes(txt))) : $"{arg}={txt}");
                 }
 
-                if (DataListUtil.IsJson(data))
-                {
-                    return data;
-                }
-
-                pairs = ExtractArgumentsFromDataListOrQueryString(ctx, data);
-                return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
+                return url + string.Join("&", results);
             }
 
-            private NameValueCollection ExtractMultipartFormDataArgumentsFromDataList(ICommunicationContext ctx, StreamReader reader)
+            return baseStr;
+        }
+
+        static string CheckForEscapeCharacters(string text)
+        {
+            var escapeCharacters = new[] { "\\\"" };
+            if (escapeCharacters.Any(text.Contains))
             {
-                var provider = _multipartMemoryStreamProviderFactory.New();
-                var tempStream = _memoryStreamFactory.New();
-
-                var reqContentStream = ctx.Request.Content.ReadAsStreamAsync().Result;
-                reqContentStream.CopyTo(tempStream);
-
-                var byteArray = reader.BaseStream.ToByteArray();
-                var stream = _memoryStreamFactory.New(byteArray);
-                stream.CopyTo(tempStream);
-                
-                tempStream.Seek(0, SeekOrigin.End);
-                var writer = _streamWriterFactory.New(tempStream);
-                writer.WriteLine();
-                writer.Flush();
-                tempStream.Position = 0;
-
-                var streamContent = _streamContentFactory.New(tempStream);
-                var requestContentHeaders = ctx.Request.Content.Headers;
-                foreach (var header in requestContentHeaders.Headers)
-                {
-                    streamContent.Headers.Add(header.Key, header.Value);
-                }
-
-                streamContent.ReadAsMultipartAsync(provider).Wait();
-                var valuePairs = new NameValueCollection();
-                foreach (HttpContent content in provider.Contents)
-                {
-                    var contentDisposition = content.Headers.ContentDisposition;
-                    var name = contentDisposition.Name.Trim('"');
-                    var byteData= content.ReadAsByteArrayAsync().Result;
-
-                    var contentType = content.Headers.ContentType;
-                    var mediaType = contentType?.MediaType;
-                    if (mediaType == null)
-                    {
-                        valuePairs.Add(name, byteData.ReadToString());
-                        continue;
-                    }
-
-                    valuePairs.Add(name, byteData.ToBase64String());
-                }
-
-                return valuePairs;
+                text = Regex.Unescape(text);
             }
+            return text;
+        }
 
-            private static NameValueCollection ExtractArgumentsFromDataListOrQueryString(ICommunicationContext ctx, string data)
+
+        internal static string ExtractKeyValuePairForGetMethod(ICommunicationContext ctx, string payload)
+        {
+            if (payload != null)
             {
-                var pairs = new NameValueCollection(5);
-                var keyValuePairs = data.Split(new[] {"&"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var keyValuePairs = payload.Split(new[] {"&"}, StringSplitOptions.RemoveEmptyEntries).ToList();
                 foreach (var keyValuePair in keyValuePairs)
                 {
-                    var keyValue = keyValuePair.Split(new[] {"="}, StringSplitOptions.RemoveEmptyEntries);
-                    if (keyValue.Length > 1)
-                    {
-                        pairs.Add(keyValue[0], keyValue[1]);
-                    }
-                    else
-                    {
-                        if (keyValue.Length == 1 && (keyValue[0].IsXml() || keyValue[0].IsJSON()))
-                        {
-                            pairs.Add(keyValue[0], keyValue[0]);
-                        }
-                    }
-                }
-
-                if (pairs.Count == 0)
-                {
-                    pairs = ctx.Request.QueryString;
-                }
-
-                return pairs;
-            }
-
-            internal static string ExtractKeyValuePairs(NameValueCollection pairs, NameValueCollection boundVariables)
-            {
-                // Extract request keys
-                foreach (var key in pairs.AllKeys)
-                {
-                    if (key == "wid") //Don't add the Workspace ID to DataList
+                    if (keyValuePair.StartsWith("wid="))
                     {
                         continue;
                     }
 
-                    if (key.IsXml(out XDocument _) || key.IsJSON() || (key.ToLowerInvariant().Contains("<DataList>".ToLowerInvariant()) && key.ToLowerInvariant().Contains("<\\DataList>".ToLowerInvariant())))
+                    if (keyValuePair.IsXml() || keyValuePair.IsJSON() || (keyValuePair.ToLowerInvariant().Contains("<DataList>".ToLowerInvariant()) && keyValuePair.ToLowerInvariant().Contains("</DataList>".ToLowerInvariant())))
                     {
-                        return key; //We have a workspace id and XML DataList
+                        return keyValuePair;
                     }
+                }
+            }
 
-                    boundVariables.Add(key, pairs[key]);
+            var pairs = ctx.Request.QueryString;
+            return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
+        }
+
+        string ExtractKeyValuePairForPostMethod(ICommunicationContext ctx, StreamReader reader)
+        {
+            NameValueCollection pairs;
+            if (ctx.Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                pairs = ExtractMultipartFormDataArgumentsFromDataList(ctx, reader);
+                return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
+            }
+
+            var data = reader.ReadToEnd();
+
+            if (data.IsXml(out XDocument _))
+            {
+                return data.CleanXmlSOAP();
+            }
+
+            if (DataListUtil.IsJson(data))
+            {
+                return data;
+            }
+
+            pairs = ExtractArgumentsFromDataListOrQueryString(ctx, data);
+            return ExtractKeyValuePairs(pairs, ctx.Request.BoundVariables);
+        }
+
+        private NameValueCollection ExtractMultipartFormDataArgumentsFromDataList(ICommunicationContext ctx, StreamReader reader)
+        {
+            var provider = _multipartMemoryStreamProviderFactory.New();
+            var tempStream = _memoryStreamFactory.New();
+
+            var reqContentStream = ctx.Request.Content.ReadAsStreamAsync().Result;
+            reqContentStream.CopyTo(tempStream);
+
+            var byteArray = reader.BaseStream.ToByteArray();
+            var stream = _memoryStreamFactory.New(byteArray);
+            stream.CopyTo(tempStream);
+            
+            tempStream.Seek(0, SeekOrigin.End);
+            var writer = _streamWriterFactory.New(tempStream);
+            writer.WriteLine();
+            writer.Flush();
+            tempStream.Position = 0;
+
+            var streamContent = _streamContentFactory.New(tempStream);
+            var requestContentHeaders = ctx.Request.Content.Headers;
+            foreach (var header in requestContentHeaders.Headers)
+            {
+                streamContent.Headers.Add(header.Key, header.Value);
+            }
+
+            streamContent.ReadAsMultipartAsync(provider).Wait();
+            var valuePairs = new NameValueCollection();
+            foreach (HttpContent content in provider.Contents)
+            {
+                var contentDisposition = content.Headers.ContentDisposition;
+                var name = contentDisposition.Name.Trim('"');
+                var byteData= content.ReadAsByteArrayAsync().Result;
+
+                var contentType = content.Headers.ContentType;
+                var mediaType = contentType?.MediaType;
+                if (mediaType == null)
+                {
+                    valuePairs.Add(name, byteData.ReadToString());
+                    continue;
                 }
 
-                return string.Empty;
+                valuePairs.Add(name, byteData.ToBase64String());
             }
+
+            return valuePairs;
+        }
+
+        private static NameValueCollection ExtractArgumentsFromDataListOrQueryString(ICommunicationContext ctx, string data)
+        {
+            var pairs = new NameValueCollection(5);
+            var keyValuePairs = data.Split(new[] {"&"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            foreach (var keyValuePair in keyValuePairs)
+            {
+                var keyValue = keyValuePair.Split(new[] {"="}, StringSplitOptions.RemoveEmptyEntries);
+                if (keyValue.Length > 1)
+                {
+                    pairs.Add(keyValue[0], keyValue[1]);
+                }
+                else
+                {
+                    if (keyValue.Length == 1 && (keyValue[0].IsXml() || keyValue[0].IsJSON()))
+                    {
+                        pairs.Add(keyValue[0], keyValue[0]);
+                    }
+                }
+            }
+
+            if (pairs.Count == 0)
+            {
+                pairs = ctx.Request.QueryString;
+            }
+
+            return pairs;
+        }
+
+        internal static string ExtractKeyValuePairs(NameValueCollection pairs, NameValueCollection boundVariables)
+        {
+            // Extract request keys
+            foreach (var key in pairs.AllKeys)
+            {
+                if (key == "wid") //Don't add the Workspace ID to DataList
+                {
+                    continue;
+                }
+
+                if (key.IsXml(out XDocument _) || key.IsJSON() || (key.ToLowerInvariant().Contains("<DataList>".ToLowerInvariant()) && key.ToLowerInvariant().Contains("<\\DataList>".ToLowerInvariant())))
+                {
+                    return key; //We have a workspace id and XML DataList
+                }
+
+                boundVariables.Add(key, pairs[key]);
+            }
+
+            return string.Empty;
+        }
     }
 }
