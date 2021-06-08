251a1a0d47c9fa851d7399bdf9d49916342036d4
Pieter Terblanche <pieter.terblanche@dev2.co.za>
2021-06-08 10:54:49 +0200

    6821 Change Connect to ConnectAsync wait for 300ms

--- Dev/Dev2.Studio/App.xaml.cs
+++ Dev/Dev2.Studio/App.xaml.cs
@@ -278,7 +278,8 @@ namespace Dev2.Studio
             // Create the window 
             var repository = ServerRepository.Instance;
             var server = repository.Source;
-            server.Connect();
+            //server.Connect();
+            server.ConnectAsync().Wait(300);
             CustomContainer.Register(server);
             CustomContainer.Register(repository);
 
