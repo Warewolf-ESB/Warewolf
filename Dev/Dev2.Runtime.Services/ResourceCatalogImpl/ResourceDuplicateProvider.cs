724c0048947394b77d6d93e379463ace19b22473
PREMIER\Njabulo.Nxele <njabulo.nxele@theunlimited.co.za>
2021-06-08 16:52:59 +0200

    remove semaphore, file read optimization

--- Dev/Dev2.Runtime.Services/ResourceCatalogImpl/ResourceDuplicateProvider.cs
+++ Dev/Dev2.Runtime.Services/ResourceCatalogImpl/ResourceDuplicateProvider.cs
@@ -30,7 +30,6 @@ using Dev2.Runtime.Interfaces;
 using Dev2.Runtime.ServiceModel.Data;
 using ServiceStack.Common;
 
-
 namespace Dev2.Runtime.ResourceCatalogImpl
 {
     class ResourceDuplicateProvider : IResourceDuplicateProvider
@@ -45,15 +44,20 @@ namespace Dev2.Runtime.ResourceCatalogImpl
 
         public ITestCatalog TestCatalog
         {
-            get { return _testCatalog ?? Runtime.TestCatalog.Instance; }
-            set { _testCatalog = value; }
+            get
+            {
+                return _testCatalog ?? Runtime.TestCatalog.Instance;
+            }
+            set
+            {
+                _testCatalog = value;
+            }
         }
 
         public ResourceCatalogDuplicateResult DuplicateFolder(string sourcePath, string destinationPath, string newName, bool fixRefences)
         {
             try
             {
-
                 var items = DuplicateAndSaveFolders(sourcePath, destinationPath, newName, fixRefences);
                 return new ResourceCatalogDuplicateResult
                 {
@@ -62,7 +66,7 @@ namespace Dev2.Runtime.ResourceCatalogImpl
                     DuplicatedItems = items.ToList()
                 };
             }
-            catch (Exception x)
+            catch(Exception x)
             {
                 Dev2Logger.Error($"resource{sourcePath} ", x, GlobalConstants.WarewolfError);
                 return new ResourceCatalogDuplicateResult
@@ -75,7 +79,6 @@ namespace Dev2.Runtime.ResourceCatalogImpl
 
         public ResourceCatalogDuplicateResult DuplicateResource(Guid resourceId, string sourcePath, string destinationPath)
         {
-
             try
             {
                 var item = SaveResource(resourceId, sourcePath, destinationPath);
@@ -86,12 +89,13 @@ namespace Dev2.Runtime.ResourceCatalogImpl
                     DuplicatedItems = new List<IExplorerItem> { item }
                 };
             }
-            catch (Exception x)
+            catch(Exception x)
             {
                 Dev2Logger.Error($"resource{resourceId} ", x, GlobalConstants.WarewolfError);
                 return null;
             }
         }
+
         IExplorerItem SaveResource(Guid resourceId, string newPath, string newResourceName)
         {
             var result = _resourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, resourceId);
@@ -109,13 +113,13 @@ namespace Dev2.Runtime.ResourceCatalogImpl
             newResource.ResourceName = newResourceName;
             newResource.ResourceID = resourceID;
             xElement.SetElementValue("DisplayName", newResourceName);
-            if (xamlElement != null)
+            if(xamlElement != null)
             {
                 var xamlContent = xamlElement.Value;
-                xamlElement.Value = xamlContent.
-                        Replace("x:Class=\"" + resource?.ResourceName + "\"", "x:Class=\"" + newResourceName + "\"")
-                        .Replace("Flowchart DisplayName=\"" + resource?.ResourceName + "\"", "Flowchart DisplayName=\"" + newResourceName + "\"");
+                xamlElement.Value = xamlContent.Replace("x:Class=\"" + resource?.ResourceName + "\"", "x:Class=\"" + newResourceName + "\"")
+                    .Replace("Flowchart DisplayName=\"" + resource?.ResourceName + "\"", "Flowchart DisplayName=\"" + newResourceName + "\"");
             }
+
             var fixedResource = xElement.ToStringBuilder();
             _resourceCatalog.SaveResource(GlobalConstants.ServerWorkspaceID, newResource, fixedResource, newPath);
             SaveTests(resourceId, resourceID);
@@ -125,12 +129,13 @@ namespace Dev2.Runtime.ResourceCatalogImpl
         void SaveTests(Guid oldResourceId, Guid newResourceId)
         {
             var serviceTestModelTos = _testCatalog?.Fetch(oldResourceId);
-            if (serviceTestModelTos != null && serviceTestModelTos.Count > 0)
+            if(serviceTestModelTos != null && serviceTestModelTos.Count > 0)
             {
-                foreach (var serviceTestModelTo in serviceTestModelTos)
+                foreach(var serviceTestModelTo in serviceTestModelTos)
                 {
                     serviceTestModelTo.ResourceId = newResourceId;
                 }
+
                 TestCatalog.SaveTests(newResourceId, serviceTestModelTos);
             }
         }
@@ -146,31 +151,29 @@ namespace Dev2.Runtime.ResourceCatalogImpl
 
                 DuplicateAndSaveTests(duplicatedResourcesMap);
 
-                if (fixRefences)
+                if(fixRefences)
                 {
                     try
                     {
-                        using (var tx = new TransactionScope(TransactionScopeOption.RequiresNew))
+                        using(var tx = new TransactionScope(TransactionScopeOption.RequiresNew))
                         {
                             FixReferences(duplicatedResourcesMap);
                             tx.Complete();
                         }
-
                     }
-                    catch (TransactionAbortedException e)
+                    catch(TransactionAbortedException e)
                     {
                         //TODO: remove this line as Transation Rollback is not possible here, current returns null
                         //Transaction.Current.Rollback();  
                         throw new TransactionAbortedException("Failure Fixing references", e);
                     }
                 }
-                
             }
-            catch (TransactionAbortedException ex)
+            catch(TransactionAbortedException ex)
             {
                 throw;
             }
-           
+
             return items;
         }
 
@@ -178,59 +181,49 @@ namespace Dev2.Runtime.ResourceCatalogImpl
         {
             var resourceMaps = new List<DuplicateResourceTO>();
 
-            var semaphore = new SemaphoreSlim(10);
-            
+            var semaphore = new SemaphoreSlim(1);
+
             var tasks = new List<Task>();
-            foreach (var oldResource in resourcesToMove)
+            foreach(var oldResource in resourcesToMove)
             {
-                tasks.Add(Task.Run(()=> 
+                try
                 {
-                    semaphore.Wait();
-                    try
-                    {
-                        string savePath = CalculateResourceSavePath(sourceLocation, destFolderPath, oldResource.ResourceName, oldResource);
-                        var itemToAdd = GenerateItemToAdd(savePath);
-                        ServerExplorerRepository.Instance.AddItem(itemToAdd, GlobalConstants.ServerWorkspaceID);
+                    string savePath = CalculateResourceSavePath(sourceLocation, destFolderPath, oldResource.ResourceName, oldResource);
+                    var itemToAdd = GenerateItemToAdd(savePath);
+                    ServerExplorerRepository.Instance.AddItem(itemToAdd, GlobalConstants.ServerWorkspaceID);
 
-                        var result = _resourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, oldResource.ResourceID);
-                        var xElement = result.ToXElement();
-                        var newResource = DuplicateResource(xElement);
+                    var result = _resourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, oldResource.ResourceID);
+                    var xElement = result.ToXElement();
+                    var newResource = DuplicateResource(xElement);
 
-                        resourceMaps.Add(new DuplicateResourceTO
+                    resourceMaps.Add(
+                        new DuplicateResourceTO
                         {
                             OldResourceID = oldResource.ResourceID,
                             NewResource = newResource,
                             DestinationPath = savePath,
                             ResourceContents = xElement.ToStringBuilder()
                         });
-                    }
-                    catch (Exception e)
-                    {
-                        Dev2Logger.Error(e.Message, e, GlobalConstants.WarewolfError);
-                        throw new Exception("Failure Duplicating Folder: " + e.Message);
-                    }
-                    finally
-                    {
-                        semaphore.Release();
-                    }
-                    
-                }));
-
+                }
+                catch(Exception e)
+                {
+                    Dev2Logger.Error(e.Message, e, GlobalConstants.WarewolfError);
+                    throw new Exception("Failure Duplicating Folder: " + e.Message);
+                }
             }
 
-            Task.WaitAll(tasks.ToArray());
             _resourceCatalog.SaveResources(GlobalConstants.ServerWorkspaceID, resourceMaps, overrideExisting: true);
             return resourceMaps;
         }
 
         private void DuplicateAndSaveTests(List<DuplicateResourceTO> resourcesMaps)
         {
-            foreach (var mapper in resourcesMaps)
+            foreach(var mapper in resourcesMaps)
             {
                 var oldResourceId = mapper.OldResourceID;
                 var newResource = mapper.NewResource;
                 var newResourceId = newResource.ResourceID;
-                SaveTests(oldResourceId, newResourceId); 
+                SaveTests(oldResourceId, newResourceId);
             }
         }
 
@@ -264,7 +257,7 @@ namespace Dev2.Runtime.ResourceCatalogImpl
             var actualPath = destPath.TrimStart('\\');
             var trimEnd = destination.TrimStart('\\').TrimEnd('\\');
             var parentItem = ServerExplorerRepository.Instance.Find(item => item.ResourcePath.ToLowerInvariant().TrimEnd('\\').Equals(trimEnd));
-            if (parentItem == null)
+            if(parentItem == null)
             {
                 var itemToAdd = new ServerExplorerItem(newName, Guid.NewGuid(), "Folder", new List<IExplorerItem>(), Permissions.Contribute, actualPath) { IsFolder = true };
                 ServerExplorerRepository.Instance.AddItem(itemToAdd, GlobalConstants.ServerWorkspaceID);
@@ -287,11 +280,12 @@ namespace Dev2.Runtime.ResourceCatalogImpl
         private List<IResource> GetResourcesToMoveFrom(string sourceLocation)
         {
             var resourceList = _resourceCatalog.GetResourceList(GlobalConstants.ServerWorkspaceID);
-            return resourceList.Where(resource =>
-            {
-                var upper = resource.GetResourcePath(GlobalConstants.ServerWorkspaceID).ToUpper();
-                return upper.StartsWith(sourceLocation.ToUpper());
-            }).Where(resource => !(resource is ManagementServiceResource)).ToList();
+            return resourceList.Where(
+                resource =>
+                {
+                    var upper = resource.GetResourcePath(GlobalConstants.ServerWorkspaceID).ToUpper();
+                    return upper.StartsWith(sourceLocation.ToUpper());
+                }).Where(resource => !(resource is ManagementServiceResource)).ToList();
         }
 
         public static string CalculateResourceSavePath(string sourceLocation, string destPath, string resourceName, IResource resource)
@@ -300,17 +294,18 @@ namespace Dev2.Runtime.ResourceCatalogImpl
 
             var savePath = resourcePath;
             var resourceNameIndex = resourcePath.LastIndexOf(resourceName, StringComparison.InvariantCultureIgnoreCase);
-            if (resourceNameIndex >= 0)
+            if(resourceNameIndex >= 0)
             {
                 savePath = resourcePath.Substring(0, resourceNameIndex);
             }
+
             savePath = savePath.ReplaceFirst(sourceLocation, destPath);
             return savePath;
         }
 
         void FixReferences(List<DuplicateResourceTO> resourceMaps)
         {
-            foreach (var mapper in resourceMaps)
+            foreach(var mapper in resourceMaps)
             {
                 var newResource = mapper.NewResource;
                 var oldResourceID = newResource.ResourceID.ToString();
@@ -318,20 +313,20 @@ namespace Dev2.Runtime.ResourceCatalogImpl
 
                 var contents = _resourceCatalog.GetResourceContents(GlobalConstants.ServerWorkspaceID, newResource.ResourceID);
                 contents = contents.Replace(oldResourceID.ToLowerInvariant(), newResourceID.ToLowerInvariant());
-                
+
                 var resPath = newResource.GetResourcePath(GlobalConstants.ServerWorkspaceID);
 
                 var savePath = resPath;
                 var resourceNameIndex = resPath.LastIndexOf(newResource.ResourceName, StringComparison.InvariantCultureIgnoreCase);
-                if (resourceNameIndex >= 0)
+                if(resourceNameIndex >= 0)
                 {
                     savePath = resPath.Substring(0, resourceNameIndex);
                 }
 
-               newResource.LoadDependencies(contents.ToXElement());
+                newResource.LoadDependencies(contents.ToXElement());
             }
-            _resourceCatalog.SaveResources(GlobalConstants.ServerWorkspaceID, resourceMaps, overrideExisting: true);
 
+            _resourceCatalog.SaveResources(GlobalConstants.ServerWorkspaceID, resourceMaps, overrideExisting: true);
         }
     }
 }
