de8331680235a2ff722b413bf09b6f854c9b76e3
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2021-06-24 10:35:48 +0000

    DevOps-removing references to revulytics

--- Dev/Warewolf.Studio.ViewModels/MenuViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/MenuViewModel.cs
@@ -1,4 +1,4 @@
-﻿#pragma warning disable
+#pragma warning disable
 /*
 *  Warewolf - Once bitten, there's no going back
 *  Copyright 2021 by Warewolf Ltd <alpha@warewolf.io>
@@ -19,7 +19,6 @@ using FontAwesome.WPF;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
 using Dev2;
-using Dev2.Instrumentation;
 
 namespace Warewolf.Studio.ViewModels
 {
@@ -56,9 +55,6 @@ namespace Warewolf.Studio.ViewModels
             CheckForNewVersionCommand = new DelegateCommand(_viewModel.DisplayDialogForNewVersion);
             SupportCommand = new DelegateCommand(() =>
             {
-                var applicationTracker = CustomContainer.Get<IApplicationTracker>();
-                applicationTracker?.TrackEvent(Resources.Languages.TrackEventHelp.EventCategory,
-                    Resources.Languages.TrackEventHelp.Help);
                 Process.Start(Resources.Languages.HelpText.WarewolfHelpURL);
             });
 
