809da462a1cc7a66c2f115534f8d009cc36a4284
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-06-30 22:35:44 +0200

    6853-add end-to-end integration test for getting coverage as json

--- Dev/Dev2.IntegrationTests/TestCoverage/TestCoverageCatalogIntegrationTests.cs
+++ Dev/Dev2.IntegrationTests/TestCoverage/TestCoverageCatalogIntegrationTests.cs
@@ -159,6 +159,7 @@ namespace Dev2.Integration.Tests.TestCoverage
         }
         
         [TestMethod]
+        [TestCategory(nameof(TestCoverageCatalog))]
         public void ExecutionWithTest_ExpectedValidJson()
         {
             var result = TestHelper.PostDataToWebserver("http://localhost:3142/secure/.tests");
@@ -167,19 +168,32 @@ namespace Dev2.Integration.Tests.TestCoverage
         }
         
         [TestMethod]
+        [TestCategory(nameof(TestCoverageCatalog))]
         public void ExecutionWithTest_ExpectedValidXml()
         {
-            var result = TestHelper.PostDataToWebserver("http://localhost:3142/secure/.trx");
-            Assert.IsNotNull(result);
+            const string ExpectedXmlStarter = "<TestRun xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010"";
+            var result = TestHelper.PostDataToWebserver("http://localhost:3142/secure/.tests.trx");
+            Assert.IsTrue(result.StartsWith(ExpectedXmlStarter), "Unexpected xml returned from tests:\n" + result);
         }
         
         [TestMethod]
+        [TestCategory(nameof(TestCoverageCatalog))]
         public void ExecutionWithCoverage_ExpectedValidHtml()
         {
             const string ExpectedHtmlStarter = "<div class=\"nav-bar-row\" style=\"padding:10px 10px 20px 10px;margin:5px;font-family:Roboto sans-serif;font-size:28px;font-weight:500;\">\r\n\t";
             TestHelper.PostDataToWebserver("http://localhost:3142/secure/.tests");
             var result = TestHelper.PostDataToWebserver("http://localhost:3142/secure/.coverage");
             Assert.IsTrue(result.StartsWith(ExpectedHtmlStarter + "Coverage Summary"), "Invalid html returned from coverage:\n" + result);
+        }
+        
+        [TestMethod]
+        [TestCategory(nameof(TestCoverageCatalog))]
+        public void ExecutionWithCoverage_ExpectedValidJson()
+        {
+            TestHelper.PostDataToWebserver("http://localhost:3142/secure/.tests");
+            var result = TestHelper.PostDataToWebserver("http://localhost:3142/secure/.coverage.json");
+            var json = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            Assert.IsNotNull(json, "Coverage json is not valid as json.");
         }
 
         public Mock<IResourceCatalog> GetMockResourceCatalog(IWarewolfWorkflow warewolfWorkflow)
