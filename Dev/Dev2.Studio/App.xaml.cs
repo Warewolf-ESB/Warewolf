b274bcf717ccc1525a8a144abd092bf407aebfe8
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2021-07-30 07:14:49 +0000

    DevOps using connect async in App.xaml.cs

--- Dev/Dev2.Studio/App.xaml.cs
+++ Dev/Dev2.Studio/App.xaml.cs
@@ -274,7 +274,7 @@ namespace Dev2.Studio
             // Create the window 
             var repository = ServerRepository.Instance;
             var server = repository.Source;
-            server.Connect();
+            server.ConnectAsync(3000);
             CustomContainer.Register(server);
             CustomContainer.Register(repository);
 
