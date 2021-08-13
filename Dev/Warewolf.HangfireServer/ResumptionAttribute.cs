be97ed9c7c0b94ac679b7abf384e2b19bf672238
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2021-08-13 14:39:41 +0200

    6915-Refactor, ResumptionAttribute: On Scheduler state change logging with repeating status checks

--- Dev/Warewolf.HangfireServer/ResumptionAttribute.cs
+++ Dev/Warewolf.HangfireServer/ResumptionAttribute.cs
@@ -9,13 +9,17 @@
 */
 
 using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hangfire;
 using Hangfire.Client;
 using Hangfire.Common;
 using Hangfire.Logging;
 using Hangfire.Server;
 using Hangfire.States;
 using Hangfire.Storage;
+using Warewolf.Auditing;
 using Warewolf.Driver.Resume;
 using Warewolf.Execution;
 using Warewolf.HangfireServer;
@@ -29,8 +33,7 @@ namespace HangfireServer
         private static readonly ILog _hangfireLogger = LogProvider.GetCurrentClassLogger();
         private readonly IExecutionLogPublisher _logger;
         private readonly IResumptionFactory _resumptionFactory;
-        private readonly SemaphoreSlim throttler = new SemaphoreSlim(Environment.ProcessorCount);
-
+        
         public ResumptionAttribute(IExecutionLogPublisher logger, IResumptionFactory resumptionFactory)
         {
             _logger = logger;
@@ -40,6 +43,7 @@ namespace HangfireServer
         public void OnCreating(CreatingContext context)
         {
             _hangfireLogger.InfoFormat("Creating a job based on method {0}...", context.Job.Method.Name);
+            _logger.Info("Creating a job based on method {"+ context.Job.Method.Name + "}...");
         }
 
         public void OnCreated(CreatedContext context)
@@ -48,6 +52,7 @@ namespace HangfireServer
                 "Job that is based on method {0} has been created with id {1}",
                 context.Job.Method.Name,
                 context.BackgroundJob?.Id);
+            _logger.Info("Job that is based on method {"+ context.Job.Method.Name + "} has been created with id {"+ context.BackgroundJob?.Id + "}");
         }
 
         public void OnPerforming(PerformingContext context)
@@ -70,6 +75,21 @@ namespace HangfireServer
         public void OnPerformed(PerformedContext context)
         {
             _hangfireLogger.InfoFormat("Job {0} has been performed", context.BackgroundJob.Id);
+            _logger.Info("Job {"+ context.BackgroundJob.Id + "} has been performed ");
+
+            if(context.Exception != null)
+            {
+                LogJobPerfomedOnSchedulerException(context.BackgroundJob, context.Exception, "HasException", context.Result.ToString());
+            }
+            if (context.Canceled)
+            {
+                LogJobPerfomedOnSchedulerException(context.BackgroundJob, context.Exception, "WasCanceled", context.Result.ToString());
+            }
+            if (context.ExceptionHandled)
+            {
+                LogJobPerfomedOnSchedulerException(context.BackgroundJob, context.Exception, "ExceptionHandled", context.Result.ToString());
+            }
+
         }
 
         public void OnStateElection(ElectStateContext context)
@@ -80,11 +100,21 @@ namespace HangfireServer
                     "Job {0} has been failed due to an exception {1}",
                     context.BackgroundJob.Id,
                     failedState.Exception);
-                _logger.Warn(
-                    "Job {0} has been failed due to an exception {1}",
-                    context.BackgroundJob.Id,
-                    failedState.Exception);
+                _logger.Warn("Job {" + context.BackgroundJob.Id + "} has been failed due to an exception {" + failedState.Exception + "}");
+
+                if (IsReapiting(context.TraversedStates, "Failed"))
+                {
+                    LogJobPerfomedOnSchedulerException(context.BackgroundJob, failedState.Exception, context.CandidateState.ToString(), context.TraversedStates.ToString());
+                    _logger.Warn("Job {" + context.BackgroundJob.Id + "} has been failed before and again now due to {" + failedState.Exception + "}, TraversedStates: {" + context.TraversedStates.ToString() + "}");
+                }
             }
+            
+            if (IsReapiting(context.TraversedStates, "Enqueued"))
+            {
+                LogJobPerfomedOnSchedulerException(context.BackgroundJob, null, context.CandidateState.ToString(), context.TraversedStates.ToString());
+                _logger.Warn("Job {" + context.BackgroundJob.Id + "} has been enqueued before and again now due to {" + context.CandidateState.Reason + "}, TraversedStates: {"+ context.TraversedStates.ToString() +"}");
+            }
+
         }
 
         public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
@@ -94,14 +124,51 @@ namespace HangfireServer
                 context.BackgroundJob.Id,
                 context.OldStateName,
                 context.NewState.Name);
+
+            _logger.Info("Job {"+ context.BackgroundJob.Id + "} state was changed from {"+ context.OldStateName + "} to {"+ context.NewState.Name + "}");
         }
 
         public void OnStateUnapplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
         {
-            _hangfireLogger.InfoFormat(
+            _hangfireLogger.WarnFormat(
                 "Job {0} state {1} was not applied.",
                 context.BackgroundJob.Id,
                 context.OldStateName);
+
+            _logger.Warn("Job {"+ context.BackgroundJob.Id + "} state {"+ context.OldStateName + "} was not applied.");
+        }
+
+        //Note: this can be done better later
+        private void LogJobPerfomedOnSchedulerException(BackgroundJob backgroundJob, Exception exception, string schedulerState, string schedulerAdditionalDetail)
+        {
+            var jobArg = backgroundJob.Job.Args[0];
+            var values = jobArg as Dictionary<string, StringBuilder>;
+
+            values.TryGetValue("resourceID", out var resourceId);
+            values.TryGetValue("startActivityId", out var startActivityId);
+            values.TryGetValue("versionNumber", out var versionNumber);
+            values.TryGetValue("currentuserprincipal", out var currentuserprincipal);
+
+            _logger.LogResumedExecution(new Audit
+            {
+                AuditDate = DateTime.Now,
+                WorkflowID = resourceId?.ToString(),
+                Environment = string.Empty,
+                VersionNumber = versionNumber?.ToString(),
+                NextActivityId = startActivityId?.ToString(),
+                AuditType = $"LogResumeExecutionState",
+                LogLevel = (Warewolf.Logging.LogLevel)LogLevel.Info,
+                ExecutingUser = currentuserprincipal?.ToString(),
+                Exception = exception,
+                Status = schedulerState,
+                AdditionalDetail = schedulerAdditionalDetail
+            });
+        }
+
+        private static bool IsReapiting(IState[] context, string status)
+        {
+            return context.Where(o => o.Name == status)
+                    .FirstOrDefault() != null;
         }
     }
 
