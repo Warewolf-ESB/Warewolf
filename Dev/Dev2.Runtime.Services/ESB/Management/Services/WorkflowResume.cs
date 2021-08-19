f6dec43436c5aabf8139fbca159fc3010e49d3a5
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2021-08-19 12:04:54 +0200

    6915-Refactor, HangfireScheduler: ResumeWorkflow use CustomContainer.AddToLoadedTypes to initialize objects out of range

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/WorkflowResume.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/WorkflowResume.cs
@@ -21,6 +21,7 @@ using Dev2.Common.Interfaces.Enums;
 using Dev2.Communication;
 using Dev2.Data.TO;
 using Dev2.DynamicServices;
+using Dev2.Runtime.ESB.Management;
 using Dev2.Runtime.Hosting;
 using Dev2.Runtime.Interfaces;
 using Dev2.Runtime.Security;
@@ -35,7 +36,7 @@ namespace Dev2.Runtime.ESB.Management.Services
     {
         private IAuthorizationService _authorizationService;
         private IResourceCatalog _resourceCatalog;
-
+        
         public override string HandlesType() => nameof(WorkflowResume);
 
         protected override ExecuteMessage ExecuteImpl(Dev2JsonSerializer serializer, Guid resourceId, Dictionary<string, StringBuilder> values)
@@ -59,8 +60,6 @@ namespace Dev2.Runtime.ESB.Management.Services
                 IsDebug = true
             };
 
-            //PBI: this might have already been done before this execution and not a good idea to make this call for each execution
-            //temporally disabled for (HOTFIX) investigate to a permanent fix
             if (!CanExecute(dataObject))
             {
                 var errorMessage = string.Format(ErrorResource.AuthenticationError, executingUser.Identity.Name);
@@ -80,14 +79,12 @@ namespace Dev2.Runtime.ESB.Management.Services
                 return new ExecuteMessage {HasError = true, Message = new StringBuilder($"Error resuming. ServiceAction is null for Resource ID:{resourceId}")};
             }
 
-            var errorResultTO = new ErrorResultTO();
-            var container = CustomContainer.Get<IResumableExecutionContainerFactory>().New(startActivityId, sa, dataObject);
+            var container = CustomContainer.CreateInstance<IResumableExecutionContainer>(startActivityId, sa, dataObject);
             container.Execute(out ErrorResultTO errors, 0);
-            errorResultTO = errors;
-
-            if (errorResultTO.HasErrors())
+            
+            if (errors.HasErrors())
             {
-                return new ExecuteMessage {HasError = true, Message = new StringBuilder(errorResultTO.MakeDisplayReady())};
+                return new ExecuteMessage {HasError = true, Message = new StringBuilder(errors.MakeDisplayReady())};
             }
 
             return new ExecuteMessage {HasError = false, Message = new StringBuilder("Execution Completed.")};
---
+++ Dev/Warewolf.Driver.Persistence.Tests/Test Utils/HangfireMocks.cs
@@ -0,0 +1,76 @@
+﻿/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2021 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.Server;
+using Hangfire.Storage;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warewolf.Driver.Drivers.HangfireScheduler.Test_Utils
+{
+    public class HangfireMocks
+    {
+    }
+
+    class PerformContextMock
+    {
+        private readonly Lazy<PerformContext> _context;
+
+        public PerformContextMock(string jobId, Dictionary<string, StringBuilder> values)
+        {
+            Connection = new Mock<IStorageConnection>();
+            BackgroundJob = new BackgroundJobMock(jobId, values);
+            CancellationToken = new Mock<IJobCancellationToken>();
+
+            _context = new Lazy<PerformContext>(
+                () => new PerformContext(Connection.Object, BackgroundJob.Object, CancellationToken.Object));
+        }
+
+        public Mock<IStorageConnection> Connection { get; set; }
+        public BackgroundJobMock BackgroundJob { get; set; }
+        public Mock<IJobCancellationToken> CancellationToken { get; set; }
+
+        public PerformContext Object => _context.Value;
+
+        public static void SomeMethod()
+        {
+        }
+    }
+
+    class BackgroundJobMock
+    {
+        private readonly Lazy<BackgroundJob> _object;
+
+        public BackgroundJobMock(string jobId, Dictionary<string, StringBuilder> values)
+        {
+            Id = jobId;
+            Job = Job.FromExpression(() => ResumeWorkflow(values, null));
+            CreatedAt = DateTime.UtcNow;
+
+            _object = new Lazy<BackgroundJob>(
+                () => new BackgroundJob(Id, Job, CreatedAt));
+        }
+
+        public string Id { get; set; }
+        public Job Job { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public BackgroundJob Object => _object.Value;
+
+        public static void ResumeWorkflow(Dictionary<string, StringBuilder> values, PerformContext context)
+        {
+        }
+    }
+}
