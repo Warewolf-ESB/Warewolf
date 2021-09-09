3f8189573a151ac0210b9560295938a2478933eb
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2021-09-09 03:09:44 +0000

    Revert "DevOps-patching flakey test on premier.local domain"
    This reverts commit a094d4512e4335a3c8fbc002963926085d32d77c

--- Dev/Dev2.Activities.Tests/ActivityTests/DateTimeActivityTests.cs
+++ Dev/Dev2.Activities.Tests/ActivityTests/DateTimeActivityTests.cs
@@ -148,7 +148,7 @@ namespace Dev2.Tests.Activities.ActivityTests
         {
             var startTime = DateTime.Now;
 
-            Thread.Sleep(12000);
+            Thread.Sleep(1200);
 
             const string currDL = @"<root><MyTestResult></MyTestResult></root>";
             SetupArguments(currDL
@@ -164,7 +164,7 @@ namespace Dev2.Tests.Activities.ActivityTests
             GetScalarValueFromEnvironment(result.Environment, "MyTestResult", out string actual, out string error);
             var parsedResult = DateTime.Parse(actual);
 
-            Thread.Sleep(11000);
+            Thread.Sleep(1100);
 
             var endTime = DateTime.Now;
 
