48fac9415544fd740a9e3dc6d6d46257f0b5f26e
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-11-17 20:39:34 +0000

    7028-inject a scenario and a feature context here

--- Dev/Warewolf.UIBindingTests.ComPluginSource/ComPluginSourceSteps.cs
+++ Dev/Warewolf.UIBindingTests.ComPluginSource/ComPluginSourceSteps.cs
@@ -19,16 +19,19 @@ using Warewolf.Studio.Views;
 [Binding]
 public class COMPluginSourceSteps
 {
+    static FeatureContext _featureContext;
+    readonly ScenarioContext _scenarioContext;
+
+    public COMPluginSourceSteps(ScenarioContext scenarioContext) => _scenarioContext = scenarioContext;
 
     [BeforeFeature("ComPluginSource")]
-    public static void SetupForSystem()
+    public static void SetupForSystem(FeatureContext featureContext)
     {
-        
+        _featureContext = featureContext;
         Utils.SetupResourceDictionary();
 
         var sourceControl = CreateSourceControl();
        
-
         var mockStudioUpdateManager = new Mock<IManageComPluginSourceModel>();
         mockStudioUpdateManager.Setup(model => model.ServerName).Returns("localhost");
         mockStudioUpdateManager.Setup(model => model.GetComDllListings(null)).Returns(BuildBaseListing());
@@ -40,17 +43,16 @@ public class COMPluginSourceSteps
         task.Start();
         var viewModel = new ManageComPluginSourceViewModel(mockStudioUpdateManager.Object, task, mockEventAggregator.Object, new SynchronousAsyncWorker());
       
-            sourceControl.DataContext = viewModel;
-            Utils.ShowTheViewForTesting(sourceControl);
+        sourceControl.DataContext = viewModel;
+        Utils.ShowTheViewForTesting(sourceControl);
        
-        FeatureContext.Current.Add(Utils.ViewNameKey, sourceControl);
-        FeatureContext.Current.Add(Utils.ViewModelNameKey, viewModel);
-        FeatureContext.Current.Add("updateManager", mockStudioUpdateManager);
-        FeatureContext.Current.Add("requestServiceNameViewModel", mockRequestServiceNameViewModel);
-        FeatureContext.Current.Add("externalProcessExecutor", mockExecutor);
-        FeatureContext.Current.Add("eventAggregator", mockEventAggregator);
-        FeatureContext.Current.Add("synchronousAsyncWorker", mockSynchronousAsyncWorker);
-
+        _featureContext.Add(Utils.ViewNameKey, sourceControl);
+        _featureContext.Add(Utils.ViewModelNameKey, viewModel);
+        _featureContext.Add("updateManager", mockStudioUpdateManager);
+        _featureContext.Add("requestServiceNameViewModel", mockRequestServiceNameViewModel);
+        _featureContext.Add("externalProcessExecutor", mockExecutor);
+        _featureContext.Add("eventAggregator", mockEventAggregator);
+        _featureContext.Add("synchronousAsyncWorker", mockSynchronousAsyncWorker);
     }
 
     static ManageComPluginSourceControl CreateSourceControl()
@@ -74,65 +76,65 @@ public class COMPluginSourceSteps
     [BeforeScenario("ComPluginSource")]
     public void SetupForPluginSource()
     {
-        ScenarioContext.Current.Add(Utils.ViewNameKey, FeatureContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey));
-        ScenarioContext.Current.Add("updateManager", FeatureContext.Current.Get<Mock<IManageComPluginSourceModel>>("updateManager"));
-        ScenarioContext.Current.Add("requestServiceNameViewModel", FeatureContext.Current.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel"));
-        ScenarioContext.Current.Add(Utils.ViewModelNameKey, FeatureContext.Current.Get<ManageComPluginSourceViewModel>(Utils.ViewModelNameKey));
+        _scenarioContext.Add(Utils.ViewNameKey, _featureContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey));
+        _scenarioContext.Add("updateManager", _featureContext.Get<Mock<IManageComPluginSourceModel>>("updateManager"));
+        _scenarioContext.Add("requestServiceNameViewModel", _featureContext.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel"));
+        _scenarioContext.Add(Utils.ViewModelNameKey, _featureContext.Get<ManageComPluginSourceViewModel>(Utils.ViewModelNameKey));
     }
     [AfterScenario("ComPluginSource")]
     public void Cleanup()
     {
         var mockExecutor = new Mock<IExternalProcessExecutor>();
-        var mockUpdateManager = ScenarioContext.Current.Get<Mock<IManageComPluginSourceModel>>("updateManager");
+        var mockUpdateManager = _scenarioContext.Get<Mock<IManageComPluginSourceModel>>("updateManager");
         mockUpdateManager.Setup(model => model.GetComDllListings(null)).Returns(BuildBaseListing());
-        var mockRequestServiceNameViewModel = ScenarioContext.Current.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
+        var mockRequestServiceNameViewModel = _scenarioContext.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
         var mockEventAggregator = new Mock<Microsoft.Practices.Prism.PubSubEvents.IEventAggregator>();
-        var mockViewModel = ScenarioContext.Current.Get<ManageComPluginSourceViewModel>(Utils.ViewModelNameKey);
+        var mockViewModel = _scenarioContext.Get<ManageComPluginSourceViewModel>(Utils.ViewModelNameKey);
         var task = new Task<IRequestServiceNameViewModel>(() => mockRequestServiceNameViewModel.Object);
         task.Start();
         var viewModel = new ManageComPluginSourceViewModel(mockUpdateManager.Object, task, mockEventAggregator.Object, new SynchronousAsyncWorker(), mockViewModel.DispatcherAction);
-        var managePluginSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var managePluginSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         managePluginSourceControl.DataContext = viewModel;
-        FeatureContext.Current.Remove("viewModel");
-        FeatureContext.Current.Add("viewModel", viewModel);
-        FeatureContext.Current.Remove("externalProcessExecutor");
-        FeatureContext.Current.Add("externalProcessExecutor", mockExecutor);
+        _featureContext.Remove("viewModel");
+        _featureContext.Add("viewModel", viewModel);
+        _featureContext.Remove("externalProcessExecutor");
+        _featureContext.Add("externalProcessExecutor", mockExecutor);
     }
 
     [Given(@"I open New COMPlugin Source")]
     [When(@"I open New COMPlugin Source")]
     public void GivenIOpenNewPluginSource()
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         Assert.IsNotNull(sourceControl);
     }
 
     [Then(@"""(.*)"" tab is opened")]
     public void ThenTabIsOpened(string headerText)
     {
-        var viewModel = ScenarioContext.Current.Get<IDockAware>("viewModel");
+        var viewModel = _scenarioContext.Get<IDockAware>("viewModel");
         Assert.AreEqual(headerText, viewModel.Header);
     }
 
     [Then(@"title is ""(.*)""")]
     public void ThenTitleIs(string title)
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
-        var viewModel = ScenarioContext.Current.Get<ManageComPluginSourceViewModel>("viewModel");
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var viewModel = _scenarioContext.Get<ManageComPluginSourceViewModel>("viewModel");
         Assert.AreEqual(title, viewModel.HeaderText);
         Assert.AreEqual(title, sourceControl.GetHeaderText());
     }
     [Then(@"Footerlabel is ""(.*)""")]
     public void ThenFooterlabelIs(string assembly)
     {
-        var managePluginSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var managePluginSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         Assert.AreEqual(assembly, managePluginSourceControl.GetAssemblyName());
     }
 
     [When(@"I click")]
     public void WhenIClick(Table table)
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         foreach (var row in table.Rows)
         {
             var dllListingModel = sourceControl.SelectItem(row["Clicks"]);
@@ -145,15 +147,15 @@ public class COMPluginSourceSteps
     [Then(@"""(.*)"" is ""(.*)""")]
     public void GivenIs(string controlName, string enabledString)
     {
-        Utils.CheckControlEnabled(controlName, enabledString, ScenarioContext.Current.Get<ICheckControlEnabledView>(Utils.ViewNameKey), Utils.ViewNameKey);
+        Utils.CheckControlEnabled(controlName, enabledString, _scenarioContext.Get<ICheckControlEnabledView>(Utils.ViewNameKey), Utils.ViewNameKey);
     }
 
     [When(@"I change Assembly to ""(.*)""")]
     public void WhenIChangeAssemblyTo(string assemblyName)
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         sourceControl.SetAssemblyName(assemblyName);
-        var viewModel = ScenarioContext.Current.Get<ManageComPluginSourceViewModel>("viewModel");
+        var viewModel = _scenarioContext.Get<ManageComPluginSourceViewModel>("viewModel");
         var assemblyNameOnViewModel = viewModel.AssemblyName;
         var isSameAsViewModel = assemblyName.Equals(assemblyNameOnViewModel, StringComparison.OrdinalIgnoreCase);
         Assert.IsTrue(isSameAsViewModel);
@@ -161,16 +163,16 @@ public class COMPluginSourceSteps
     [When(@"I save as ""(.*)""")]
     public void WhenISaveAs(string resourceName)
     {
-        var mockRequestServiceNameViewModel = ScenarioContext.Current.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
+        var mockRequestServiceNameViewModel = _scenarioContext.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
         mockRequestServiceNameViewModel.Setup(model => model.ResourceName).Returns(new ResourceName("", resourceName));
         mockRequestServiceNameViewModel.Setup(model => model.ShowSaveDialog()).Returns(MessageBoxResult.OK);
-        var manageWebserviceSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var manageWebserviceSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         manageWebserviceSourceControl.PerformSave();
     }
     [Then(@"the save dialog is opened")]
     public void ThenTheSaveDialogIsOpened()
     {
-        var mockRequestServiceNameViewModel = ScenarioContext.Current.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
+        var mockRequestServiceNameViewModel = _scenarioContext.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
         mockRequestServiceNameViewModel.Verify();
     }
 
@@ -187,9 +189,9 @@ public class COMPluginSourceSteps
             SelectedDll = new DllListing() {ClsId = Guid.NewGuid().ToString(), Name = "Object", Is32Bit = false, FullName = "System.Object", IsDirectory = false},
         };
 
-        var managePluginSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
-        var mockStudioUpdateManager = FeatureContext.Current.Get<Mock<IManageComPluginSourceModel>>("updateManager").Object;
-        var mockEventAggregator = FeatureContext.Current.Get<Mock<IEventAggregator>>("eventAggregator").Object;
+        var managePluginSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var mockStudioUpdateManager = _featureContext.Get<Mock<IManageComPluginSourceModel>>("updateManager").Object;
+        var mockEventAggregator = _featureContext.Get<Mock<IEventAggregator>>("eventAggregator").Object;
 
         try
         {
@@ -205,8 +207,8 @@ public class COMPluginSourceSteps
             });
 
             managePluginSourceControl.DataContext = managePluginSourceViewModel;
-            ScenarioContext.Current.Remove("viewModel");
-            ScenarioContext.Current.Add("viewModel", managePluginSourceViewModel);
+            _scenarioContext.Remove("viewModel");
+            _scenarioContext.Add("viewModel", managePluginSourceViewModel);
         }
         catch (Exception)
         {
@@ -219,11 +221,11 @@ public class COMPluginSourceSteps
     [Then(@"FooterLabel value is ""(.*)""")]
     public void ThenAssemblyValueIs(string assemblyName)
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         var assemblyNameOnControl = sourceControl.GetAssemblyName();
         var isSameAsControl = assemblyName.Equals(assemblyNameOnControl, StringComparison.OrdinalIgnoreCase);
         Assert.IsTrue(isSameAsControl);
-        var viewModel = ScenarioContext.Current.Get<ManageComPluginSourceViewModel>("viewModel");
+        var viewModel = _scenarioContext.Get<ManageComPluginSourceViewModel>("viewModel");
         var assemblyNameOnViewModel = viewModel.SelectedDll.FullName;
         var isSameAsViewModel = assemblyName.Equals(assemblyNameOnViewModel, StringComparison.OrdinalIgnoreCase);
         Assert.IsTrue(isSameAsViewModel);
@@ -234,26 +236,26 @@ public class COMPluginSourceSteps
     [Then(@"I click ""(.*)""")]
     public void ThenIClick(string itemName)
     {
-        var sourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var sourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         var dllListingModel = sourceControl.SelectItem(itemName);
         Assert.IsNotNull(dllListingModel);
-        var viewModel = ScenarioContext.Current.Get<ManageComPluginSourceViewModel>("viewModel");
+        var viewModel = _scenarioContext.Get<ManageComPluginSourceViewModel>("viewModel");
         Assert.AreEqual(dllListingModel, viewModel.SelectedDll);
         Assert.IsTrue(viewModel.SelectedDll.IsExpanded);
     }
     [When(@"I save Plugin source")]
     public void WhenISavePluginSource()
     {
-        var mockRequestServiceNameViewModel = ScenarioContext.Current.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
+        var mockRequestServiceNameViewModel = _scenarioContext.Get<Mock<IRequestServiceNameViewModel>>("requestServiceNameViewModel");
         mockRequestServiceNameViewModel.Setup(model => model.ShowSaveDialog()).Verifiable();
-        var managePluginSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var managePluginSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         managePluginSourceControl.PerformSave();
     }
 
     [Given(@"DLLs is ""(.*)""")]
     public void GivenDLLsIs(string p0)
     {
-        var managePluginSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var managePluginSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         managePluginSourceControl.ExecuteRefresh();
     }
    
@@ -268,7 +270,7 @@ public class COMPluginSourceSteps
     [When(@"I ""(.*)"" the filter")]
     public void WhenITheFilter(string p0)
     {
-        var manageWebserviceSourceControl = ScenarioContext.Current.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
+        var manageWebserviceSourceControl = _scenarioContext.Get<ManageComPluginSourceControl>(Utils.ViewNameKey);
         manageWebserviceSourceControl.ClearFilter();
     }
 
