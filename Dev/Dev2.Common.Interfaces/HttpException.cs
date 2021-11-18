45d51440d89b977f6ab956c8fa5a3a948d816995
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-11-18 18:55:04 +0000

    7028-start upgrade to .NET 5.0

---
+++ Dev/Dev2.Common.Interfaces/HttpException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+
+public class HttpException : Exception
+{
+    readonly int httpStatusCode;
+    new string Message;
+
+    public HttpException(string message)
+    {
+        Message = message;
+    }
+
+    public HttpException(int httpStatusCode)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message) : base(message)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message) : base(message)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message, Exception inner) : base(message, inner)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message, Exception inner) : base(message, inner)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public int StatusCode { get { return this.httpStatusCode; } }
+}
