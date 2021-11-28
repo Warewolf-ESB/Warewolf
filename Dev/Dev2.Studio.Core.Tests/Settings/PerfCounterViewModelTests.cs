d0764c8f179901248fd4ac714ce20d080cb78c78
Ashley Lewis <ashley.lewis@dev2.co.za>
2021-11-28 10:39:05 +0000

    7041-deprecate more usages of PrivateType

--- Dev/Dev2.Studio.Core.Tests/Settings/PerfCounterViewModelTests.cs
+++ Dev/Dev2.Studio.Core.Tests/Settings/PerfCounterViewModelTests.cs
@@ -35,7 +35,6 @@ namespace Dev2.Core.Tests.Settings
         [TestInitialize]
         public void Setup()
         {
-
             AppUsageStats.LocalHost = "http://localhost:3142";
             _mockEnvironment = new Mock<IServer>();
             _mockConnection = new Mock<IEnvironmentConnection>();
@@ -61,11 +60,12 @@ namespace Dev2.Core.Tests.Settings
             _mockEnvironment.Setup(model => model.AuthorizationService).Returns(authorizationService.Object);
             var activeServer = new Server(Guid.NewGuid(), _mockConnection.Object);
             ServerRepository.Instance.ActiveServer = activeServer;
-            var counters = new PrivateType(typeof(PerfcounterViewModel));
+            var privateCounters = new PerfcounterViewModel(new Mock<IPerformanceCounterTo>().Object, new Mock<IServer>().Object);
+            var counters = new Warewolf.Testing.PrivateObject(privateCounters);
             CustomContainer.Register(new Mock<IExplorerTooltips>().Object);
             //------------Setup for test------------------------
             //------------Execute Test--------------------------
-            var invokeStatic = counters.InvokeStatic("GetEnvironment");
+            var invokeStatic = counters.Invoke("GetEnvironment", true);
             //------------Assert Results-------------------------
             Assert.IsNotNull(invokeStatic);
         }
