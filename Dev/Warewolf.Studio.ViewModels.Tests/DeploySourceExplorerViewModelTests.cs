eb6bb2bf2c0741b43610d59746c61ad47b2a0c22
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2022-01-03 14:35:13 +0000

    6166-increasing this studio view model unit test timeout because of test flicker against it

--- Dev/Warewolf.Studio.ViewModels.Tests/DeploySourceExplorerViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/DeploySourceExplorerViewModelTests.cs
@@ -341,7 +341,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(150)]
+        [Timeout(500)]
         public void TestCalculateOnNullExplorerItems()
         {
             //arrange
