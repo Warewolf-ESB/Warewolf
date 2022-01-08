3232a06abea3b05b884ef1bdbdd27b92139821df
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-01-08 18:11:49 +0000

    DevOps-adding cover coverage entrypoint shim

---
+++ Dev/Dev2.Server/CoverageEntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using com.sun.tools.javadoc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev2
+{
+    [TestClass]
+    public class CoverageEntryPoint
+    {
+        [TestMethod]
+        public void CoverageEntrypoint()=>Main.main();
+    }
+}
