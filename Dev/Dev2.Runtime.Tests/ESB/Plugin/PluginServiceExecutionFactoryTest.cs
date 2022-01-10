5f96d6762ab4e1b7b1fbcc956c567878edc22676
Ashley Leewis <ashley.lewis@dev2.co.za>
2022-01-10 11:36:26 +0000

    DevOps-adding another timeout

--- Dev/Dev2.Runtime.Tests/ESB/Plugin/PluginServiceExecutionFactoryTest.cs
+++ Dev/Dev2.Runtime.Tests/ESB/Plugin/PluginServiceExecutionFactoryTest.cs
@@ -160,7 +160,7 @@ namespace Dev2.Tests.Runtime.ESB.Plugin
         [TestMethod]
         [Owner("Travis Frisinger")]
         [TestCategory("PluginServiceExecutionFactory_InvokePlugin")]
-        [Timeout(5000)]
+        [Timeout(30000)]
         public void PluginRuntimeHandler_InvokePlugin_WhenValidDll_ExpectValidResults()
         {
             //------------Setup for test--------------------------
