7851799f1d5fd1f89dc1b5245a3d92e45293e003
PREMIER\Njabulo.Nxele <njabulo.nxele@theunlimited.co.za>
2022-01-18 10:47:30 +0200

    implement WebView2 control and use WebView version generated by compiler

--- Dev/Warewolf.Studio.CustomControls/WebBrowserView.xaml.cs
+++ Dev/Warewolf.Studio.CustomControls/WebBrowserView.xaml.cs
@@ -8,11 +8,14 @@
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
 
+using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Web.WebView2.Core;
 using Warewolf.Studio.Core;
 
 namespace Warewolf.Studio.CustomControls
@@ -21,41 +24,35 @@ namespace Warewolf.Studio.CustomControls
     public partial class WebBrowserView : IView
     {
         private readonly Grid _blackoutGrid = new Grid();
+        private const string _pathFolder = "Microsoft.WebView2.FixedVersionRuntime.95.0.1020.44.x64";
 
         public WebBrowserView(string licenseType)
         {
             InitializeComponent();
             PopupViewManageEffects.AddBlackOutEffect(_blackoutGrid);
+            InitializeWebView(licenseType);
+        }
 
+        private void InitializeWebView(string licenseType)
+        {
+            var path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), _pathFolder);
+
+            webView.CreationProperties =
+                  new Microsoft.Web.WebView2.Wpf.CoreWebView2CreationProperties { BrowserExecutableFolder = path };
+            webView.Source = ScriptManager.GetSourceUri(licenseType);
+        }
+
+        private void webView_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
             var helper = new ScriptManager(this);
-            webBrowser.ObjectForScripting = helper;
-            webBrowser.AllowDrop = false;
-            webBrowser.Source = ScriptManager.GetSourceUri(licenseType);
+
+            webView.EnsureCoreWebView2Async().Wait();
+            webView.CoreWebView2.AddHostObjectToScript(nameof(helper), helper);
         }
 
-        private void WebBrowserOnNavigated(object sender, NavigationEventArgs e)
+        private void webView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
-            ScriptManager.SetSilent(webBrowser, true); // make it silent
-        }
 
-        private void WebBrowser_OnNavigating(object sender, NavigatingCancelEventArgs e)
-        {
-            var browser = sender as WebBrowser;
-
-            if (browser?.Document is null)
-            {
-                return;
-            }
-
-            dynamic document = browser.Document;
-
-            if (document.readyState != "complete")
-                return;
-
-            var script = document.createElement("script");
-            script.type = @"text/javascript";
-            script.text = @"window.onerror = function(msg,url,line){return true;}";
-            document.head.appendChild(script);
         }
 
         private void WebBrowserView_OnClosing(object sender, CancelEventArgs e)
