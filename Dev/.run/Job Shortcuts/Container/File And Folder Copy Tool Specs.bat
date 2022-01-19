6538fea19d87b03e20477c255bfc5428b6ac6e1b
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-01-19 19:23:37 +0000

    7084-this character needs to be escaped

--- Dev/.run/Job Shortcuts/Container/File And Folder Copy Tool Specs.bat
+++ Dev/.run/Job Shortcuts/Container/File And Folder Copy Tool Specs.bat
@@ -1,3 +1,3 @@
 echo Set-Location C:\BuildUnderTest>"%~dp0..\..\..\TestResults\RunTestsEntrypoint.ps1"
-echo &".\Job Shortcuts\TestRun.ps1" -RetryCount 6 -Projects Warewolf.Tools.Specs -Category FileAndFolderCopy>>"%~dp0..\..\..\TestResults\RunTestsEntrypoint.ps1"
+echo ^&".\Job Shortcuts\TestRun.ps1" -RetryCount 6 -Projects Warewolf.Tools.Specs -Category FileAndFolderCopy>>"%~dp0..\..\..\TestResults\RunTestsEntrypoint.ps1"
 docker run -i --rm --memory 4g -v "%~dp0..\..\..\TestResults:C:\BuildUnderTest\TestResults" registry.gitlab.com/warewolf/vstest:%1 powershell -File C:\BuildUnderTest\TestResults\RunTestsEntrypoint.ps1
