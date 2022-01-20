7340b4428aa42cf6c0fe93544e55e89d7d6c11c8
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-01-20 09:01:15 +0000

    7084-this shortcut script was missing the test execute line

--- Dev/.run/Job Shortcuts/Container/Server Startup Tests.bat
+++ Dev/.run/Job Shortcuts/Container/Server Startup Tests.bat
@@ -1,3 +1,4 @@
 mkdir "%~dp0..\..\..\TestResults"
 echo Set-Location C:\BuildUnderTest>"%~dp0..\..\..\TestResults\RunTestsEntrypoint.ps1"
-echo ^&".\Job Shortcuts\TestRun.ps1" -RetryCount 6 -Projects Dev2.Integration.Tests -Category "Server Startup" -PreTestRunScript "StartAsService.ps1 -Cleanup -ResourcesPath ServerTests" -PostTestRunScript ReverseDeployLog.ps1 -InContainer
+echo ^&".\Job Shortcuts\TestRun.ps1" -RetryCount 6 -Projects Dev2.Integration.Tests -Category "Server Startup" -PreTestRunScript "StartAsService.ps1 -Cleanup -ResourcesPath ServerTests" -PostTestRunScript ReverseDeployLog.ps1>>"%~dp0..\..\..\TestResults\RunTestsEntrypoint.ps1"
+docker run -i --rm --memory 4g -v "%~dp0..\..\..\TestResults:C:\BuildUnderTest\TestResults" registry.gitlab.com/warewolf/vstest:%1 powershell -File C:\BuildUnderTest\TestResults\RunTestsEntrypoint.ps1
