a66432ef4f7854d4eb33c143e89fa4c1a2bdba95
Siphamandla Dube <Siphamandla.Dube@theunlimited.co.za>
2022-01-26 07:30:33 +0200

    7025-BugFix, DebugLineItem: Only UI components should truncated the values for display so the value itself can be used at it's original format

--- Dev/Dev2.Common.Interfaces/Diagnostics/Debug/IDebugItemResult.cs
+++ Dev/Dev2.Common.Interfaces/Diagnostics/Debug/IDebugItemResult.cs
@@ -1,14 +1,12 @@
 /*
 *  Warewolf - Once bitten, there's no going back
-*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Copyright 2022 by Warewolf Ltd <alpha@warewolf.io>
 *  Licensed under GNU Affero General Public License 3.0 or later.
 *  Some rights reserved.
 *  Visit our website for more information <http://warewolf.io/>
 *  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
 *  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
 */
-
-using System.Xml.Serialization;
 
 namespace Dev2.Common.Interfaces.Diagnostics.Debug
 {
@@ -19,6 +17,7 @@ namespace Dev2.Common.Interfaces.Diagnostics.Debug
         string Operator { get; set; }
         string Variable { get; set; }
         string Value { get; set; }
+        string TruncatedValue { get; set; }
         string GroupName { get; set; }
         int GroupIndex { get; set; }
         string MoreLink { get; set; }
