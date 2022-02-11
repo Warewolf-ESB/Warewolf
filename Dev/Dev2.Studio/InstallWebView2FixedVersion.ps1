e46c272f70a467b4d96c6c5f9280f93a060fded5
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-02-11 05:43:13 +0000

    7104-try to solve reachability problem

--- Dev/Dev2.Studio/InstallWebView2FixedVersion.ps1
+++ Dev/Dev2.Studio/InstallWebView2FixedVersion.ps1
@@ -1,10 +1,13 @@
 if (!(Test-Path "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab")) {
-    Write-Host Downloading  Microsoft.WebView2.FixedVersionRuntime.cab
+    Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    Set-Location ZoneMap\Domains
+    New-Item example.com
+    Set-Location example.com
+    New-Item www
+    Set-Location www
+    New-ItemProperty . -Name http -Value 2 -Type DWORD
     Write-Host Running reachability test:
     ping msedge.sf.dl.delivery.mp.microsoft.com
-    Write-Host Downloading to $env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab
-    Write-Host running free space test:
-    Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }}
 	Invoke-WebRequest -Uri https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/f93ee51c-3968-426b-9c3e-4f3cba8b1a97/Microsoft.WebView2.FixedVersionRuntime.95.0.1020.44.x64.cab -OutFile "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab"
 }
 expand "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab" -F:* "$PSScriptRoot"
