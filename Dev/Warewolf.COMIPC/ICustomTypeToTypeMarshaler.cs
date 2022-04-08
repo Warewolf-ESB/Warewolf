d8c27deca57a6e5906fef0e6e8f562aa1e679940
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-04-08 21:51:52 +0100

    6968-patch for error: "Feature 'default interface implementation' is not available in C# 7.3."

--- Dev/Warewolf.COMIPC/ICustomTypeToTypeMarshaler.cs
+++ Dev/Warewolf.COMIPC/ICustomTypeToTypeMarshaler.cs
@@ -3,31 +3,31 @@ using System.Runtime.InteropServices;
 
 namespace WarewolfCOMIPC
 {
-    public interface ICustomTypeToTypeMarshaler : ICustomMarshaler
+    public class ICustomTypeToTypeMarshaler : ICustomMarshaler
     {
-        object MarshalNativeToManaged(IntPtr pNativeData)
+        public object MarshalNativeToManaged(IntPtr pNativeData)
         {
             Console.WriteLine("An attempt was made to marshal native object to managed.");
             return null;
         }
 
-        IntPtr MarshalManagedToNative(object ManagedObj)
+        public IntPtr MarshalManagedToNative(object ManagedObj)
         {
             Console.WriteLine("An attempt was made to marshal managed object to native.");
             return new IntPtr();
         }
 
-        void CleanUpNativeData(IntPtr pNativeData)
+        public void CleanUpNativeData(IntPtr pNativeData)
         {
             Console.WriteLine("An attempt was made to cleanup native data.");
         }
 
-        void CleanUpManagedData(object ManagedObj)
+        public void CleanUpManagedData(object ManagedObj)
         {
             Console.WriteLine("An attempt was made to cleanup manged data.");
         }
 
-        int GetNativeDataSize()
+        public int GetNativeDataSize()
         {
             Console.WriteLine("An attempt was made to get native data size.");
             return 0;
