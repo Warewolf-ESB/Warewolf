98e949600f3655cc260dac8f7683671bb053e6fd
PREMIER\Njabulo.Nxele <njabulo.nxele@theunlimited.co.za>
2022-05-19 10:53:35 +0200

    make test steps tree view draggable

--- Dev/Warewolf.Studio.Views/ServiceTestView.xaml.cs
+++ Dev/Warewolf.Studio.Views/ServiceTestView.xaml.cs
@@ -1,10 +1,12 @@
 #pragma warning disable
-﻿using System.Activities.Presentation;
+using System;
+using System.Activities.Presentation;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using Dev2.Common.Interfaces;
 using Dev2.Studio.Interfaces;
 using Dev2.UI;
 using Microsoft.Practices.Prism.Mvvm;
@@ -127,5 +129,203 @@ namespace Warewolf.Studio.Views
             RefreshCommands(routedEventArgs);
             e.Handled = true;
         }
+        
+        #region DragDrop
+        
+        Point _lastMouseDown;
+        IServiceTestStep draggedItem, _target;
+
+
+        private void TreeView_After_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                _lastMouseDown = e.GetPosition(StepTestDataTree);
+            }
+
+        }
+
+
+        private bool CheckGridSplitter(UIElement element)
+        {
+            if (element is GridSplitter)
+            {
+                return true;
+            }
+
+            GridSplitter GridSplitter = FindParent<GridSplitter>(element);
+
+            if (GridSplitter != null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+        private void TreeView_After_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    UIElement element = e.OriginalSource as UIElement;
+                    bool bGridSplitter = CheckGridSplitter(element);
+
+
+                    Point currentPosition = e.GetPosition(StepTestDataTree);
+
+
+                    if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
+                        (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                    {
+                        draggedItem = (IServiceTestStep)StepTestDataTree.SelectedItem;
+
+                        if ((draggedItem != null) && !bGridSplitter)
+                        {
+                            DragDropEffects finalDropEffect = DragDrop.DoDragDrop(StepTestDataTree, StepTestDataTree.SelectedValue,
+                                DragDropEffects.Move);
+                            //Checking target is not null and item is dragging(moving)
+                            if ((finalDropEffect == DragDropEffects.Move) && (_target != null))
+                            {
+
+                                // A Move drop was accepted
+                                if (draggedItem.ActivityID != _target.ActivityID)
+                                {
+                                    CopyItem(draggedItem, _target);
+                                    _target = null;
+                                    draggedItem = null;
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void TreeView_After_DragOver(object sender, DragEventArgs e)
+        {
+            try
+            {
+
+                Point currentPosition = e.GetPosition(StepTestDataTree);
+
+
+                if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
+                    (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                {
+                    // Verify that this is a valid drop and then store the drop target
+                    IServiceTestStep item = GetNearestContainer(e.OriginalSource as UIElement);
+                    if (CheckDropTarget(draggedItem, item))
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.None;
+                    }
+                }
+                e.Handled = true;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void TreeView_After_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+
+                // Verify that this is a valid drop and then store the drop target
+                IServiceTestStep TargetItem = GetNearestContainer(e.OriginalSource as UIElement);
+                if (TargetItem != null && draggedItem != null)
+                {
+                    _target = TargetItem;
+                    e.Effects = DragDropEffects.Move;
+
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+
+
+        }
+
+        private bool CheckDropTarget(IServiceTestStep _sourceItem, IServiceTestStep _targetItem)
+        {
+            //Check whether the target item is meeting your condition
+            bool _isEqual = false;
+
+            if (_sourceItem.ActivityID != _targetItem.ActivityID)
+            {
+                _isEqual = true;
+            }
+
+            return _isEqual;
+
+        }
+
+
+        private void CopyItem(IServiceTestStep _sourceItem, IServiceTestStep _targetItem)
+        {
+
+            //Asking user wether he want to drop the dragged TreeViewItem here or not
+            if (MessageBox.Show("Would you like to drop " + _sourceItem.StepDescription + " into " + _targetItem.StepDescription + "", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    _targetItem.Children.Add(_sourceItem);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+        }
+
+
+        private IServiceTestStep GetNearestContainer(UIElement element)
+        {
+            // Walk up the element tree to the nearest tree view item.
+            TreeViewItem UIContainer = FindParent<TreeViewItem>(element);
+            IServiceTestStep NVContainer = null;
+
+            if (UIContainer != null)
+            {
+                NVContainer = UIContainer.DataContext as IServiceTestStep;
+            }
+            return NVContainer;
+        }
+
+
+        private static Parent FindParent<Parent>(DependencyObject child)
+                where Parent : DependencyObject
+        {
+            DependencyObject parentObject = child;
+            parentObject = VisualTreeHelper.GetParent(parentObject);
+
+            //check if the parent matches the type we're looking for
+            if (parentObject is Parent || parentObject == null)
+            {
+                return parentObject as Parent;
+            }
+            else
+            {
+                //use recursion to proceed with next level
+                return FindParent<Parent>(parentObject);
+            }
+        }
+        
+        #endregion
     }
 }
