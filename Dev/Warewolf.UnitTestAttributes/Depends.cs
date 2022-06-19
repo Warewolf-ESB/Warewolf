69bb9b1374ecccb6d9bf19791e1761e582a8bdf0
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-06-19 16:38:48 +0100

    DevOps-using new mysql dependency container in the cloud

--- Dev/Warewolf.UnitTestAttributes/Depends.cs
+++ Dev/Warewolf.UnitTestAttributes/Depends.cs
@@ -151,8 +151,8 @@ namespace Warewolf.UnitTestAttributes
             }
             else if(_containerType == ContainerType.FTPS)
             {
-                Container.IP = "localhost";
-                Container.Port = "1010";
+                Container.IP = "102.37.115.89";
+                Container.Port = "3143";
             }
             else if (_containerType == ContainerType.RabbitMQ)
             {
@@ -196,8 +196,8 @@ namespace Warewolf.UnitTestAttributes
             }
             else if (_containerType == ContainerType.MySQL)
             {
-                Container.IP = "warewolf-mysql-connector-testing.southafricanorth.azurecontainer.io";
-                Container.Port = "3306";
+                Container.IP = "102.37.115.89";
+                Container.Port = "9300";
             }
             else if (_containerType == ContainerType.AnonymousRedis)
             {
