5f57ddff00b4fdcc4b19304ef07e156535455f75
Ashley Lewis <ashley.lewis@dev2.co.za>
2022-10-06 15:50:22 +0100

    DevOps-optimizing this script

--- Dev/Dev2.Studio/InstallWebView2FixedVersion.ps1
+++ Dev/Dev2.Studio/InstallWebView2FixedVersion.ps1
@@ -1,10 +1,6 @@
-if (!(Test-Path "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab")) {
-    Write-Host Downloading  Microsoft.WebView2.FixedVersionRuntime.cab
-    Write-Host Running reachability test:
-    ping msedge.sf.dl.delivery.mp.microsoft.com
-    Write-Host Downloading to $env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab
-    Write-Host running free space test:
-    Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }}
-	Invoke-WebRequest -Uri https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/f93ee51c-3968-426b-9c3e-4f3cba8b1a97/Microsoft.WebView2.FixedVersionRuntime.95.0.1020.44.x64.cab -OutFile "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab"
+if (!(Test-Path "$PSScriptRoot\Microsoft.WebView2.FixedVersionRuntime.95.0.1020.44.x64")) {
+	if (!(Test-Path "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab")) {
+		Invoke-WebRequest -Uri https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/f93ee51c-3968-426b-9c3e-4f3cba8b1a97/Microsoft.WebView2.FixedVersionRuntime.95.0.1020.44.x64.cab -OutFile "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab"
+	}
+	expand "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab" -F:* "$PSScriptRoot"
 }
-expand "$env:UserProfile\.nuget\packages\Microsoft.WebView2.FixedVersionRuntime.cab" -F:* "$PSScriptRoot"
