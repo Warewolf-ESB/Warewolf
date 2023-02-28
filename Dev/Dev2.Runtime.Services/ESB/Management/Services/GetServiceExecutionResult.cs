c6a3198219487daad056b6a35cffc5fa68f75ddb
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2023-02-28 13:27:34 +0000

    7339-update this service to work with current version of Warewolf.Auditing

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/GetServiceExecutionResult.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/GetServiceExecutionResult.cs
@@ -5,6 +5,7 @@ using Dev2.Communication;
 using Dev2.DynamicServices;
 using Dev2.Workspaces;
 using Dev2.Common;
+using Warewolf.Auditing.Drivers;
 
 namespace Dev2.Runtime.ESB.Management.Services
 {
@@ -15,9 +16,11 @@ namespace Dev2.Runtime.ESB.Management.Services
             var executionId = values["ExecutionId"];
             var trimExecutionId = executionId.ToString().Trim();
             var serializer = new Dev2JsonSerializer();
-            if (LogDataCache.CurrentResults == null || !LogDataCache.CurrentResults.Any())
+            var auditQueryable = new AuditQueryableSqlite();
+
+			if (LogDataCache.CurrentResults == null || !LogDataCache.CurrentResults.Any())
             {
-                LogDataCache.CurrentResults = BuildTempObjects(values);
+				LogDataCache.CurrentResults = auditQueryable.QueryLogData(values).ToList();
             }
             var tmpObjects = LogDataCache.CurrentResults.FirstOrDefault(r => r.ExecutionId == trimExecutionId && r.Message.StartsWith("Execution Result"));
             var replace = "";
@@ -29,7 +32,7 @@ namespace Dev2.Runtime.ESB.Management.Services
                 var lengthToSelect = message.Length - startIdx - endIdx;
                 replace = message.Substring(startIdx, lengthToSelect);
             }
-            var logEntry = new LogEntry { Result = replace };
+            var logEntry = new Common.LogEntry { Result = replace };
             return serializer.SerializeToBuilder(logEntry);
         }
 
