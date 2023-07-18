e93145b403b109a61915822e4894e3e88d74c9cc
Ashley Lewis <Ashley.lewis@theunlimited.co.za>
2023-07-18 10:12:02 +0100

    7472-removing unused files

--- Dev/.run/Job Shortcuts/Bare Metal/Conflicting View and Execute Permissions Security Specs.bat
+++
@@ -1,3 +0,0 @@
-mkdir "%~dp0..\..\..\..\bin\AcceptanceTesting"
-cd /d "%~dp0..\..\..\..\bin\AcceptanceTesting"
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -NoExit -File "%~dp0..\TestRun.ps1" -RetryCount 6 -Projects Dev2.*.Specs,Warewolf.*.Specs -ExcludeProjects Dev2.Activities.Specs,Warewolf.Tools.Specs,Warewolf.UI.Specs,Warewolf.UI.Load.Specs -Category ConflictingViewExecutePermissionsSecurity -PreTestRunScript "StartAsService.ps1 -Cleanup -ResourcesPath ServerTests" -PostTestRunScript ReverseDeployLog.ps1
