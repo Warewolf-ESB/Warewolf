fde1ff89fb90f136594a573b6142b47b83986415
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2024-09-30 19:32:22 +0000

    7742-bumping this timeout after repeated failures against it

--- Dev/Warewolf.Studio.ViewModels.Tests/MenuViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/MenuViewModelTests.cs
@@ -182,7 +182,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(100)]
+        [Timeout(250)]
         public void TestLockCommand()
         {
             //arrange
