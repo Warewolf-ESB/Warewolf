3521411b2bf21044eec41c48ed7ada76ed450019
Yogesh Rajpurohit <yogesh.rajpurohit@theunlimited.co.za>
2025-02-03 12:21:45 +0530

    7867 - #Fix - incorrect indentation

--- Dev/Warewolf.Language.Parser/DataStorage.fs
+++ Dev/Warewolf.Language.Parser/DataStorage.fs
@@ -38,23 +38,18 @@ type WarewolfAtom =
     | NullPlaceholder
     | PositionedValue of (int * WarewolfAtom)
     
-    override x.ToString() = 
-    match x with
-    | Float a -> 
-        let places = GetDecimalPlaces a
-        a.ToString(sprintf "F%i" places, System.Globalization.CultureInfo.InvariantCulture)
-    | Int a -> 
-        a.ToString(System.Globalization.CultureInfo.InvariantCulture)
-    | DataString a -> 
-        a
-    | JsonObject a -> 
-        a.ToString()
-    | Nothing -> 
-        ""
-    | NullPlaceholder -> 
-        ""
-    | PositionedValue(_, b) -> 
-        b.ToString()
+    override x.ToString() : string = 
+        match x with
+        | Float a -> 
+                let places = GetDecimalPlaces a
+                a.ToString(sprintf "F%i" places, System.Globalization.CultureInfo.InvariantCulture)
+        | Int a -> 
+                a.ToString(System.Globalization.CultureInfo.InvariantCulture)
+        | DataString a -> a
+        | JsonObject a -> a.ToString()
+        | Nothing -> ""
+        | NullPlaceholder -> ""
+        | PositionedValue(_, b) -> b.ToString()
 
     
     override x.Equals y = x.ToString() = y.ToString()
