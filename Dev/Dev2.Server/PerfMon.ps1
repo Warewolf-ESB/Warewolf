87b71b75975d40ea3ab462542eb01ca545e41bed
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-02-17 15:36:32 +0000

    7875-#Fix-Remove unnecessary function

--- Dev/Dev2.Server/PerfMon.ps1
+++ Dev/Dev2.Server/PerfMon.ps1
@@ -1,86 +1,45 @@
-$categoryName = "Warewolf"
-$categoryHelp = "Warewolf Performance Counters"
-$categoryType = [System.Diagnostics.PerformanceCounterCategoryType]::SingleInstance
-$instanceName = "*"
-
 function Convert-CounterType {
-    param (
-        [string]$counterType
-    )
-    
-    switch ($counterType) {
-        "AverageBase" { return [System.Diagnostics.PerformanceCounterType]::AverageBase }
-        "AverageTimer32" { return [System.Diagnostics.PerformanceCounterType]::AverageTimer32 }
-        "NumberOfItems32" { return [System.Diagnostics.PerformanceCounterType]::NumberOfItems32 }
-        "RateOfCountsPerSecond32" { return [System.Diagnostics.PerformanceCounterType]::RateOfCountsPerSecond32 }
-        default { throw "Unhandled PerformanceCounterType: $counterType" }
-    }
+	param (
+		[string]$counterType
+	)
+	
+	switch ($counterType) {
+		"AverageBase" { return [System.Diagnostics.PerformanceCounterType]::AverageBase }
+		"AverageTimer32" { return [System.Diagnostics.PerformanceCounterType]::AverageTimer32 }
+		"NumberOfItems32" { return [System.Diagnostics.PerformanceCounterType]::NumberOfItems32 }
+		"RateOfCountsPerSecond32" { return [System.Diagnostics.PerformanceCounterType]::RateOfCountsPerSecond32 }
+		default { throw "Unhandled PerformanceCounterType: $counterType" }
+	}
 }
 
-if (-Not [System.Diagnostics.PerformanceCounterCategory]::Exists($categoryName)) {
-    $counters = @(@{
-        CreationData = @(
-        @{ CounterName = "Concurrent requests currently executing"; CounterHelp = "Concurrent requests currently executing"; CounterType = "NumberOfItems32" },
-        @{ CounterName = "Total Errors"; CounterHelp = "Total Errors"; CounterType = "NumberOfItems32" },
-        @{ CounterName = "Request Per Second"; CounterHelp = "Request Per Second"; CounterType = "RateOfCountsPerSecond32" },
-        @{ CounterName = "Average workflow execution time"; CounterHelp = "Average workflow execution time"; CounterType = "AverageTimer32" },
-        @{ CounterName = "Average workflow execution time base"; CounterHelp = "Average workflow execution time base"; CounterType = "AverageBase" },
-        @{ CounterName = "Count of Not Authorised errors"; CounterHelp = "Count of Not Authorised errors"; CounterType = "NumberOfItems32" },
-        @{ CounterName = "Count of requests for workflows which don't exist"; CounterHelp = "Count of requests for workflows which don't exist"; CounterType = "NumberOfItems32" }
-        )
-    })
-    $counterCreationDataCollection = New-Object System.Diagnostics.CounterCreationDataCollection
-    
-    foreach ($counter in $counters) {
-        foreach ($counterData in $counter.CreationData) {
-            $systemCounterData = New-Object System.Diagnostics.CounterCreationData(
-                $counterData.CounterName,
-                $counterData.CounterHelp,
-                (Convert-CounterType $counterData.CounterType)
-            )
-            $counterCreationDataCollection.Add($systemCounterData)
-        }
-    }
-    [System.Diagnostics.PerformanceCounterCategory]::Create("Warewolf", "Warewolf Performance Counters", [System.Diagnostics.PerformanceCounterCategoryType]::MultiInstance, $counterCreationDataCollection)
+if (-Not [System.Diagnostics.PerformanceCounterCategory]::Exists("Warewolf")) {
+	$counters = @(@{
+		CreationData = @(
+		@{ CounterName = "Concurrent requests currently executing"; CounterHelp = "Concurrent requests currently executing"; CounterType = "NumberOfItems32" },
+		@{ CounterName = "Total Errors"; CounterHelp = "Total Errors"; CounterType = "NumberOfItems32" },
+		@{ CounterName = "Request Per Second"; CounterHelp = "Request Per Second"; CounterType = "RateOfCountsPerSecond32" },
+		@{ CounterName = "Average workflow execution time"; CounterHelp = "Average workflow execution time"; CounterType = "AverageTimer32" },
+		@{ CounterName = "Average workflow execution time base"; CounterHelp = "Average workflow execution time base"; CounterType = "AverageBase" },
+		@{ CounterName = "Count of Not Authorised errors"; CounterHelp = "Count of Not Authorised errors"; CounterType = "NumberOfItems32" },
+		@{ CounterName = "Count of requests for workflows which don't exist"; CounterHelp = "Count of requests for workflows which don't exist"; CounterType = "NumberOfItems32" }
+		)
+	})
+	$counterCreationDataCollection = New-Object System.Diagnostics.CounterCreationDataCollection
+	
+	foreach ($counter in $counters) {
+		foreach ($counterData in $counter.CreationData) {
+			$systemCounterData = New-Object System.Diagnostics.CounterCreationData(
+				$counterData.CounterName,
+				$counterData.CounterHelp,
+				(Convert-CounterType $counterData.CounterType)
+			)
+			$counterCreationDataCollection.Add($systemCounterData)
+		}
+	}
+	[System.Diagnostics.PerformanceCounterCategory]::Create("Warewolf", "Warewolf Performance Counters", [System.Diagnostics.PerformanceCounterCategoryType]::MultiInstance, $counterCreationDataCollection)
+	Write-Host Warewolf Performance Counters created.
+	exit 0
+} else {
+	Write-Host Warewolf Performance Counters already exist.
+	exit 1
 }
-
-$ErrorActionPreference = "Stop"
-try {
-    Get-Counter -Counter "\Warewolf(*)\Concurrent requests currently executing"
-    Write-Output "Concurrent requests currently executing counter exists."
-} catch {
-    # Instance might not exist.
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Concurrent requests currently executing", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-try {
-    Get-Counter -Counter "\Warewolf(*)\Total Errors"
-    Write-Output "Total Errors counter exists."
-} catch {
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Total Errors", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-try {
-    Get-Counter -Counter "\Warewolf(*)\Request Per Second"
-    Write-Output "Request Per Second counter exists."
-} catch {
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Request Per Second", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-try {
-    Get-Counter -Counter "\Warewolf(*)\Average workflow execution time"
-    Write-Output "Average workflow execution time counter exists."
-} catch {
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Average workflow execution time", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-try {
-    Get-Counter -Counter "\Warewolf(*)\Count of Not Authorised errors"
-    Write-Output "Count of Not Authorised errors counter exists."
-} catch {
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Count of Not Authorised errors", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-try {
-    Get-Counter -Counter "\Warewolf(*)\Count of requests for workflows which don't exist"
-    Write-Output "Count of requests for workflows which don't exist counter exists."
-} catch {
-    New-Object System.Diagnostics.PerformanceCounter($categoryName, "Count of requests for workflows which don't exist", $instanceName, $false);Get-Counter -ListSet "Warewolf"
-}
-$ErrorActionPreference = "Continue"
-Write-Output "$categoryHelp created and initialized for instance: $instanceName"
