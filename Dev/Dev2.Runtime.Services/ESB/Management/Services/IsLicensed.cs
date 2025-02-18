af31537478078d072cb8219c0ba6f07e6a813192
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-02-18 07:38:03 +0000

    7875-#Fix-add IsLicensed service to find if a server is licenced or not

---
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/IsLicensed.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using Dev2.Communication;
+using Dev2.DynamicServices;
+using Dev2.Runtime.Subscription;
+using Dev2.Workspaces;
+
+namespace Dev2.Runtime.ESB.Management.Services
+{
+	public class IsLicensed : DefaultEsbManagementEndpoint
+	{
+		public override StringBuilder Execute(Dictionary<string, StringBuilder> values, IWorkspace theWorkspace)
+		{
+			var serialiser = new Dev2JsonSerializer();
+			return serialiser.SerializeToBuilder(SubscriptionProvider.Instance.IsLicensed);
+		}
+
+		public override DynamicService CreateServiceEntry() => EsbManagementServiceEntry.CreateESBManagementServiceEntry(HandlesType(), "<DataList><Dev2System.ManagmentServicePayload ColumnIODirection=\"Both\"></Dev2System.ManagmentServicePayload></DataList>");
+
+		public override string HandlesType() => "IsLicensed";
+	}
+}
