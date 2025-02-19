b27e713d30512478b15d58164f9c34d6b9d8c277
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-02-19 14:02:10 +0000

    7875-#Fix-bumping this test timeout

--- Dev/Warewolf.Studio.ViewModels.Tests/MenuViewModelTests.cs
+++ Dev/Warewolf.Studio.ViewModels.Tests/MenuViewModelTests.cs
@@ -197,7 +197,7 @@ namespace Warewolf.Studio.ViewModels.Tests
         }
 
         [TestMethod]
-        [Timeout(250)]
+        [Timeout(500)]
         public void TestSlideOpenCommand_isOverLockfalseIsPanelLockedOpenfalse()
         {
             //arrange
