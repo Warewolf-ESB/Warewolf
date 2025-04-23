7a5d56d823a0a8fe7cfd53e801372b10b0f4f3f4
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-04-23 04:37:17 +0100

    7906-patch for error in the build: "STATestClass  is ambiguous"

--- Dev/Dev2.Studio.Core.Tests/TabGroupPaneBindingBehaviorTests.cs
+++ Dev/Dev2.Studio.Core.Tests/TabGroupPaneBindingBehaviorTests.cs
@@ -1,7 +1,6 @@
 ﻿using Dev2.Studio.AppResources.Behaviors;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Infragistics.Windows.DockManager;
-using Dev2.Net6.Compatibility;
 
 namespace Dev2.Core.Tests
 {
