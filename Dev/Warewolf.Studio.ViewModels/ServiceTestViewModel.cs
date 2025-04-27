09f292dbdc8ee612115f4a2b373f231c53a548f9
Ashley Lewis <ashley.lewis@theunlimited.co.za>
2025-04-27 22:07:10 +0000

    7955-remove this causing all sequence asserts to be treated as mocks

--- Dev/Warewolf.Studio.ViewModels/ServiceTestViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/ServiceTestViewModel.cs
@@ -881,7 +881,6 @@ namespace Warewolf.Studio.ViewModels
         {
             var sequence = GetCurrentActivity<DsfSequenceActivity>(modelItem);
             var testStep = BuildParentsFromModelItem(modelItem);
-            testStep.Type = StepType.Mock; //PBI: specs are failing of assert
             if (testStep != null)
             {
                 AddSequence(sequence, testStep, SelectedServiceTest.TestSteps);
