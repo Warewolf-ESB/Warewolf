bcd735e94c9c62f001ee80a3917a2a2c25cd769d
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-05-10 17:36:39 +0100

    Revert "7906-patch for compile failure"
    This reverts commit b7da8ffe2fa88f33353e9692ec89922d540eec79.

--- Dev/Warewolf.Core.Tests/Diagnostics/DebugStateTests.cs
+++ Dev/Warewolf.Core.Tests/Diagnostics/DebugStateTests.cs
@@ -200,7 +200,9 @@ namespace Dev2.Tests.Diagnostics
         #region Serialization
 
         [TestMethod]
+
         public void Serialized_Expected_CanBeDeserialized()
+
         {
             var rw = new MockByteReaderWriter();
 
@@ -212,12 +214,8 @@ namespace Dev2.Tests.Diagnostics
             itemToAdd.Add(new DebugItemResult { GroupIndex = 0, GroupName = "Group1", Type = DebugItemResultType.Value, Value = "MyValue" });
             debugStateIn.Inputs.Add(itemToAdd);
 
-            // Serialize debugStateIn to a byte array
-            var serializedData = SerializeDebugState(debugStateIn);
-            rw.Write(serializedData);
-
-            // Deserialize the byte array back to a DebugState object
-            var debugStateOut = DeserializeDebugState(rw);
+            debugStateIn.Write(rw);
+            var debugStateOut = new DebugState();
 
             Assert.AreEqual(debugStateIn.WorkspaceID, debugStateOut.WorkspaceID);
             Assert.AreEqual(debugStateIn.ID, debugStateOut.ID);
@@ -231,24 +229,12 @@ namespace Dev2.Tests.Diagnostics
             Assert.AreEqual(debugStateIn.HasError, debugStateOut.HasError);
             Assert.AreEqual(debugStateIn.ErrorMessage, debugStateOut.ErrorMessage);
             Assert.AreEqual(debugStateIn.Server, debugStateOut.Server);
+            Assert.AreEqual(debugStateIn.Server, debugStateOut.Server);
             Assert.AreEqual(debugStateIn.ServerID, debugStateOut.ServerID);
             Assert.AreEqual(debugStateIn.StartTime, debugStateOut.StartTime);
             Assert.AreEqual(debugStateIn.EndTime, debugStateOut.EndTime);
             Assert.AreEqual(debugStateIn.SessionID, debugStateOut.SessionID);
-        }
 
-        private byte[] SerializeDebugState(DebugState debugState)
-        {
-            // Implement serialization logic here
-            // This is a placeholder implementation
-            return new byte[0];
-        }
-
-        private DebugState DeserializeDebugState(MockByteReaderWriter rw)
-        {
-            // Implement deserialization logic here
-            // This is a placeholder implementation
-            return new DebugState();
         }
 
         
