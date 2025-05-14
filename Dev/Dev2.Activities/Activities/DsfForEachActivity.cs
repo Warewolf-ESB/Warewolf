6bd6b9207f38da200862af645114c2cdd76150db
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-05-14 10:57:15 +0100

    Revert "7969-patch foreach asserts over the whole foreach"
    This reverts commit 9cd4e9635dad31148631471bff5b26e547d1d504.

--- Dev/Dev2.Activities/Activities/DsfForEachActivity.cs
+++ Dev/Dev2.Activities/Activities/DsfForEachActivity.cs
@@ -363,11 +363,11 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                 allErrors.AddError(e.Message);
             }
             finally
-			{
-				RestoreValues(dataObject, itr);
-                UniqueID = _originalUniqueID.ToString();
+            {
+                RestoreValues(dataObject, itr);
                 var serviceTestStep = HandleServiceTestExecution(dataObject);
                 dataObject.ParentInstanceID = _previousParentId;
+                UniqueID = _originalUniqueID.ToString();
                 dataObject.ForEachNestingLevel--;
                 dataObject.IsDebugNested = false;
                 HandleDebug(dataObject, serviceTestStep);
@@ -403,12 +403,11 @@ namespace Unlimited.Applications.BusinessDesignStudio.Activities
                 {
                     var testRunResult = new TestRunResult();
                     GetFinalTestRunResult(serviceTestStep, testRunResult);
-					ServiceTestHelper.UpdateDebugStateWithAssertions(dataObject, dataObject.ServiceTest?.TestSteps, UniqueID);
-					serviceTestStep.Result = testRunResult;
+                    serviceTestStep.Result = testRunResult;
                 }
-			}
+            }
 
-			return serviceTestStep;
+            return serviceTestStep;
         }
 
         private void HandleDebug(IDSFDataObject dataObject, IServiceTestStep serviceTestStep)
