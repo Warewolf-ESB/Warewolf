24202bdcd75cc6f46474b850aa67e7a083a41b8b
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-06-23 16:36:37 +0100

    8029-reuse refactor to re-use existing code for PerfMonService from dev2.infrastructure

--- Dev/Warewolf.PerfMon/PerfMonService.cs
+++ Dev/Warewolf.PerfMon/PerfMonService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.ServiceProcess;
 using System.Threading;
+using Dev2.PerformanceCounters.Management;
+using Dev2.Common.Interfaces.Monitoring;
 
 namespace Warewolf.PerfMon
 {
@@ -28,7 +30,6 @@ namespace Warewolf.PerfMon
 			try
 			{
 				EnsureCategory();
-				InitializeCounters();
 
 				// Keep counters alive
 				while (true)
@@ -57,50 +58,9 @@ namespace Warewolf.PerfMon
 
 		private void EnsureCategory()
 		{
-			if (!PerformanceCounterCategory.Exists(Category))
-			{
-				var counters = new CounterCreationDataCollection
-				{
-					new CounterCreationData("Concurrent requests currently executing", "Concurrent requests currently executing", PerformanceCounterType.NumberOfItems32),
-					new CounterCreationData("Total Errors", "Total Errors", PerformanceCounterType.NumberOfItems32),
-					new CounterCreationData("Request Per Second", "Request Per Second", PerformanceCounterType.RateOfCountsPerSecond32),
-					new CounterCreationData("Average workflow execution time", "Average workflow execution time", PerformanceCounterType.AverageTimer32),
-					new CounterCreationData("Average workflow execution time base", "Average workflow execution time base", PerformanceCounterType.AverageBase),
-					new CounterCreationData("Count of Not Authorised errors", "Count of Not Authorised errors", PerformanceCounterType.NumberOfItems32),
-					new CounterCreationData("Count of requests for workflows which don't exist", "Count of requests for workflows which don't exist", PerformanceCounterType.NumberOfItems32)
-				};
-
-				PerformanceCounterCategory.Create(
-					Category,
-					"Warewolf Performance Counters",
-					PerformanceCounterCategoryType.MultiInstance,
-					counters
-				);
-			}
-		}
-
-		private void InitializeCounters()
-		{
-			_counters = new Dictionary<string, PerformanceCounter>
-			{
-				["Concurrent requests currently executing"] = Create("Concurrent requests currently executing"),
-				["Total Errors"] = Create("Total Errors"),
-				["Request Per Second"] = Create("Request Per Second"),
-				["Average workflow execution time"] = Create("Average workflow execution time"),
-				["Average workflow execution time base"] = Create("Average workflow execution time base"),
-				["Count of Not Authorised errors"] = Create("Count of Not Authorised errors"),
-				["Count of requests for workflows which don't exist"] = Create("Count of requests for workflows which don't exist")
-			};
-
-			foreach (var pc in _counters.Values)
-			{
-				pc.RawValue = 0;
-			}
-		}
-
-		private PerformanceCounter Create(string counterName)
-		{
-			return new PerformanceCounter(Category, counterName, InstanceName, false);
+			var allCounters = PerformanceCounterPersistence.DefaultCounters;
+			var register = new WarewolfPerformanceCounterRegister(allCounters, new List<IResourcePerformanceCounter>());
+			register.RegisterCountersOnMachine(allCounters, Category);
 		}
 	}
 }
