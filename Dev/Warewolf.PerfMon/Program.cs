eadde5412d07087f0dcc39ee642fa943e4ffea16
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-06-26 10:45:42 +0100

    8029-re-introduce powershell script tor run alongside the service

--- Dev/Warewolf.PerfMon/Program.cs
+++ Dev/Warewolf.PerfMon/Program.cs
@@ -2,7 +2,20 @@
 using System.ServiceProcess;
 using Warewolf.PerfMon;
 
-if (Environment.UserInteractive)
+bool runConsole = false;
+if (args != null && args.Length > 0)
+{
+	foreach (var arg in args)
+	{
+		if (string.Equals(arg, "-console", StringComparison.OrdinalIgnoreCase))
+		{
+			runConsole = true;
+			break;
+		}
+	}
+}
+
+if (Environment.UserInteractive || runConsole)
 {
 	var service = new PerfMonService();
 	Console.WriteLine("Running in console mode. Press Ctrl+C to exit...");
