89e4a8f23b771ee594862d0a2d2a6aeb829f3b30
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-09-24 12:44:31 +0100

    8154-update this unit test considering the recwent changes to the web source

--- Dev/Dev2.Runtime.Tests/ServiceModel/WebSourcesTests.cs
+++ Dev/Dev2.Runtime.Tests/ServiceModel/WebSourcesTests.cs
@@ -547,8 +547,8 @@ namespace Dev2.Tests.Runtime.ServiceModel
             }
 
             mockWebRequest.Verify(o => o.AddHeader(It.IsAny<string>()), Times.Exactly(2)); 
-            mockWebRequest.Verify(o => o.AddHeader("a:x"), Times.Once);
-            mockWebRequest.Verify(o => o.AddHeader("b:e"), Times.Once);
+            mockWebRequest.Verify(o => o.AddHeader("a: x"), Times.Once);
+            mockWebRequest.Verify(o => o.AddHeader("b: e"), Times.Once);
         }
 
         [TestMethod]
