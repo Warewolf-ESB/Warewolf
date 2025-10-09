755a34d3e6dd96c24adad7f2efdc571b7cc884cc
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-10-09 14:30:22 +0100

    8174-remove references to uipath

--- Dev/Dev2.Server/WebServerConfiguration.cs
+++ Dev/Dev2.Server/WebServerConfiguration.cs
@@ -13,9 +13,6 @@ using Dev2.Common;
 using Dev2.Common.Interfaces.Wrappers;
 using Dev2.Runtime.Security;
 using Dev2.Runtime.WebServer;
-#if !NETFRAMEWORK
-using ReflectionMagic;
-#endif
 using System;
 using System.Collections.Generic;
 using System.Configuration;
