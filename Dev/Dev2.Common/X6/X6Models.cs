aedda52aa073a40701825ccf3403765b54826062
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-10-23 14:02:53 +0100

    8184-fix IsObject and ObjectResult model property names

--- Dev/Dev2.Common/X6/X6Models.cs
+++ Dev/Dev2.Common/X6/X6Models.cs
@@ -122,8 +122,8 @@ namespace Dev2.Common.X6
         public const string WEBGET_SOURCEID = "sourceId";
         public const string WEBGET_INPUTS = "inputs";
         public const string WEBGET_OUTPUTS = "outputs";
-        public const string WEBGET_ISOBJECT = "isobject";
-        public const string WEBGET_OBJECTNAME = "objectname";
+        public const string WEBGET_ISOBJECT = "isOutputToObject";
+        public const string WEBGET_OBJECTNAME = "objectName";
         public const string WEBGET_OBJECTRESULT = "objectresult";
 
     }
