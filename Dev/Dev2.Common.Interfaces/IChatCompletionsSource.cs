0698951ddfdc53669b68bda92ff8ec8974db34f7
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-10-29 09:12:08 +0000

    8194-add missing properties to chat completions source interface

--- Dev/Dev2.Common.Interfaces/IChatCompletionsSource.cs
+++ Dev/Dev2.Common.Interfaces/IChatCompletionsSource.cs
@@ -1,3 +1,13 @@
+/*
+*  Warewolf - Once bitten, there's no going back
+*  Copyright 2019 by Warewolf Ltd <alpha@warewolf.io>
+*  Licensed under GNU Affero General Public License 3.0 or later.
+*  Some rights reserved.
+*  Visit our website for more information <http://warewolf.io/>
+*  AUTHORS <http://warewolf.io/authors.php> , CONTRIBUTORS <http://warewolf.io/contributors.php>
+*  @license GNU Affero General Public License <http://www.gnu.org/licenses/agpl-3.0.html>
+*/
+
 using System;
 
 namespace Dev2.Common.Interfaces
@@ -6,5 +16,8 @@ namespace Dev2.Common.Interfaces
     {
         string ApiKey { get; set; }
         string CompletionsEndpoint { get; set; }
+        string Name { get; set; }
+        string Path { get; set; }
+        Guid Id { get; set; }
     }
 }
