a4ee779ece3e1d8d6ce5a3d61e7a2a11fff2cb0b
Ashley Lewis <Ashley.lewis@dev2.co.za>
2025-11-12 13:37:13 +0000

    8194-add mapping for chatbot source to the view factory

--- Dev/Dev2.Studio/ViewFactory.cs
+++ Dev/Dev2.Studio/ViewFactory.cs
@@ -1,5 +1,5 @@
 #pragma warning disable
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using Dev2.Studio.Core;
 using Microsoft.Practices.Prism.Mvvm;
@@ -9,40 +9,41 @@ namespace Dev2
 {
     class ViewFactory : IViewFactory
     {
-        readonly ConcurrentDictionary<string, Func<IView>> _viewMap =
-            new ConcurrentDictionary<string, Func<IView>>();
+     readonly ConcurrentDictionary<string, Func<IView>> _viewMap =
+ new ConcurrentDictionary<string, Func<IView>>();
 
-        public ViewFactory()
+public ViewFactory()
         {
             _viewMap.TryAdd("Server", () => new ManageServerControl());
             _viewMap.TryAdd("Dev2Server", () => new ManageServerControl());
-            _viewMap.TryAdd("ServerSource", () => new ManageServerControl());
+     _viewMap.TryAdd("ServerSource", () => new ManageServerControl());
 
             _viewMap.TryAdd("RabbitMQSource", () => new ManageRabbitMQSourceControl());
-            _viewMap.TryAdd("OauthSource", () => new ManageOAuthSourceControl());
-            _viewMap.TryAdd("SharepointServerSource", () => new SharepointServerSource());
-            _viewMap.TryAdd("DropBoxSource", () => new ManageOAuthSourceControl());
-            _viewMap.TryAdd("ExchangeSource", () => new ManageExchangeSourceControl());
-            _viewMap.TryAdd("EmailSource", () => new ManageEmailSourceControl());
-            _viewMap.TryAdd("WcfSource", () => new ManageWcfSourceControl());
-            _viewMap.TryAdd("ComPluginSource", () => new ManageComPluginSourceControl());
+      _viewMap.TryAdd("OauthSource", () => new ManageOAuthSourceControl());
+        _viewMap.TryAdd("SharepointServerSource", () => new SharepointServerSource());
+_viewMap.TryAdd("DropBoxSource", () => new ManageOAuthSourceControl());
+       _viewMap.TryAdd("ExchangeSource", () => new ManageExchangeSourceControl());
+    _viewMap.TryAdd("EmailSource", () => new ManageEmailSourceControl());
+   _viewMap.TryAdd("WcfSource", () => new ManageWcfSourceControl());
+       _viewMap.TryAdd("ComPluginSource", () => new ManageComPluginSourceControl());
             _viewMap.TryAdd("PluginSource", () => new ManagePluginSourceControl());
-            _viewMap.TryAdd("WebSource", () => new ManageWebserviceSourceControl());
-            _viewMap.TryAdd("MySqlDatabase", () => new ManageDatabaseSourceControl());
+   _viewMap.TryAdd("WebSource", () => new ManageWebserviceSourceControl());
+_viewMap.TryAdd("MySqlDatabase", () => new ManageDatabaseSourceControl());
             _viewMap.TryAdd("PostgreSQL", () => new ManageDatabaseSourceControl());
-            _viewMap.TryAdd("Oracle", () => new ManageDatabaseSourceControl());
-            _viewMap.TryAdd("ODBC", () => new ManageDatabaseSourceControl());
+      _viewMap.TryAdd("Oracle", () => new ManageDatabaseSourceControl());
+      _viewMap.TryAdd("ODBC", () => new ManageDatabaseSourceControl());
             _viewMap.TryAdd("SqlDatabase", () => new ManageDatabaseSourceControl());
-			_viewMap.TryAdd("SqliteDatabase", () => new ManageDatabaseSourceControl());
-		}
+   _viewMap.TryAdd("SqliteDatabase", () => new ManageDatabaseSourceControl());
+            _viewMap.TryAdd("ChatbotSource", () => new ChatbotSource());
+        }
 
         public IView GetViewGivenServerResourceType(string resourceModel)
         {
-            if (_viewMap.TryGetValue(resourceModel, out Func<IView> funcView))
+   if (_viewMap.TryGetValue(resourceModel, out Func<IView> funcView))
             {
-                return funcView.Invoke();
+         return funcView.Invoke();
             }
-            return default(IView);
+       return default(IView);
         }
-    }
+  }
 }
