ed7d96346ebe5b6c4582a9f101e270d9ee05d9c1
aakashg <aakash.gaikwad@unlimited.co.za>
2025-12-17 11:57:06 +0530

    8242-Sort Records tool

---
+++ Dev/Dev2.Activities/WorkflowConverters/WorkflowToX6Converter_SortRecordsActivityHelper.cs
@@ -0,0 +1,25 @@
+﻿using Dev2.Common.X6;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Activities.WF
+{
+    public partial class WorkflowToX6Converter
+    {
+        public Cell CreateDsfSortRecordsActivity(DsfSortRecordsActivity sortRecordActivity, string nodeId)
+        {
+            var cell = new Cell
+            {
+                id = nodeId,
+                position = new Position(_currentX, _currentY),
+                label = sortRecordActivity.DisplayName ?? Constants.DISPLAYNAME_SORTACTIVITY,
+                data = new System.Collections.Generic.Dictionary<string, object>()
+            };
+
+            _currentY += 150;
+
+            sortRecordActivity.ToX6Json(cell);
+
+            return cell;
+        }
+    }
+}
--- Dev/Dev2.Activities/WorkflowConverters/X6ToWorkflowConverter.cs
+++ Dev/Dev2.Activities/WorkflowConverters/X6ToWorkflowConverter.cs
@@ -573,6 +573,8 @@ namespace Dev2.Activities.WF
                     return CreateRedisRemoveActivity(node);
                 case var t when t.Contains(Constants.DSFFINDRECORDSMULTIPLECRITERIAACTIVITY, StringComparison.OrdinalIgnoreCase):
                     return CreateFindRecordsMultipleCriteriaActivity(node);
+                case var t when t.Contains(Constants.DSFSORTRECORDSACTIVITY, StringComparison.OrdinalIgnoreCase):
+                    return CreateDsfSortRecordsActivity(node);
                 default:
                     return new WriteLine { Text = "Unknown type" };
             }
