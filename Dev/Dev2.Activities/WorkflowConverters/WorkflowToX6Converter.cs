24b5f48058fefabeac5ffd66a6c5c9a7067ff33e
aakashg <aakash.gaikwad@unlimited.co.za>
2026-01-16 19:03:56 +0530

    8275- Added support for UnZip tool

--- Dev/Dev2.Activities/WorkflowConverters/WorkflowToX6Converter.cs
+++ Dev/Dev2.Activities/WorkflowConverters/WorkflowToX6Converter.cs
@@ -681,6 +681,10 @@ namespace Dev2.Activities.WF
             {
                 cell = CreatePathCopyActivity(pathCopyActivity, nodeId);
             }
+            else if (activity is DsfUnZip unZipActivity)
+            {
+                cell = CreateUnZipActivity(unZipActivity, nodeId);
+            }
             else
             {
                 cell.shape = Constants.RECT;
--- Dev/Dev2.Activities/WorkflowConverters/X6ToWorkflowConverter.cs
+++ Dev/Dev2.Activities/WorkflowConverters/X6ToWorkflowConverter.cs
@@ -602,6 +602,8 @@ namespace Dev2.Activities.WF
                     return CreatePathCreateActivity(node);
                 case var t when t.Contains(Constants.DSFPATHCOPY, StringComparison.OrdinalIgnoreCase):
                     return CreatePathCopyActivity(node);
+                case var t when t.Contains(Constants.UNZIPACTIVITY, StringComparison.OrdinalIgnoreCase):
+                    return CreateUnZipActivity(node);
                 default:
                     return new WriteLine { Text = "Unknown type" };
             }
