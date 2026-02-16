4cbb56993b56b591b45981d65237a92b75b293bc
Ashley Lewis <ashley.lewis@dev2.co.za>
2026-02-16 17:46:51 +0000

    8301-add X6 to workflow serializaiton for calculate tool

---
+++ Dev/Dev2.Activities/WorkflowConverters/X6ToWorkflowConverter_CalculateActivityHelper.cs
@@ -0,0 +1,31 @@
+﻿using Dev2.Activities.SelectAndApply;
+using Dev2.Common.X6;
+using Dev2.WorkflowConverters;
+using System;
+using System.Activities.Statements;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using Unlimited.Applications.BusinessDesignStudio.Activities;
+
+namespace Dev2.Activities.WF
+{
+    /// <summary>
+    /// Creates Workflow from X6 Json data (nodes, edges and common data) 
+    /// </summary>
+    public partial class X6ToWorkflowConverter
+    {
+        private static DsfCalculateActivity CreateCalculateActivity(Cell node)
+        {
+            // Try both camelCase and lowercase variations for compatibility
+            var hasDisplayName = node.data.TryGetValue(Constants.DISPLAYNAME, out var displayObject);
+
+            if (!hasDisplayName || displayObject is not string displayName || string.IsNullOrWhiteSpace(displayName))
+                return null;
+
+            var activity = new DsfCalculateActivity();
+            activity.FromX6Json(node);
+            return activity;
+        }
+    }
+}
