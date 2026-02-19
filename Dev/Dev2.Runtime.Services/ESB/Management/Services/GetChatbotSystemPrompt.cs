f20918e38f5472b0437db5a17968503571e5e9c8
Ashley Lewis <ashley.lewis@dev2.co.za>
2026-02-19 14:43:58 +0000

    8194-typo

--- Dev/Dev2.Runtime.Services/ESB/Management/Services/GetChatbotSystemPrompt.cs
+++ Dev/Dev2.Runtime.Services/ESB/Management/Services/GetChatbotSystemPrompt.cs
@@ -16,6 +16,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
 using Warewolf.Configuration;
+using Warewolf.Resource.Errors;
 
 namespace Dev2.Runtime.ESB.Management.Services
 {
