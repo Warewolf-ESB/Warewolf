3b53d1f4941e4682a795017418bbcb689a731859
Yogesh Rajpurohit <yogesh.rajpurohit@theunlimited.co.za>
2026-06-12 13:53:32 +0530

    8412 - #Fixed : Added uncomitted changes

--- Dev/Warewolf.Execution.Lightweight.Tests/Execution/WorkflowExecutorLicenseGateTests.cs
+++ Dev/Warewolf.Execution.Lightweight.Tests/Execution/WorkflowExecutorLicenseGateTests.cs
@@ -125,8 +125,6 @@ namespace Warewolf.Execution.Lightweight.Tests.Execution
             public void LogError(string activityName, Exception ex, Guid executionId) { }
             public void LogFatal(string message, Guid executionId) { }
             public void LogFatal(string message, Exception exception, Guid executionId) { }
-            public void LogTrace(string message, Guid executionId) { }
-            public void LogTrace(string message, Exception exception, Guid executionId) { }
         }
     }
 }
